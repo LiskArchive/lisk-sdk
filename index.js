71b9ac4fb9308fe065bfe682f19300daeb33cfde
Oliver Beddows <oliver@lisk.io>
2016-03-15 16:55:07 +0000

    Normalizing quotes.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 lisk = {
-	contact: require('./lib/transactions/contact.js'),
+	contact: require("./lib/transactions/contact.js"),
 	crypto : require("./lib/transactions/crypto.js"),
-	dapp: require('./lib/transactions/dapp.js'),
+	dapp: require("./lib/transactions/dapp.js"),
 	delegate : require("./lib/transactions/delegate.js"),
 	signature : require("./lib/transactions/signature.js"),
 	transaction : require("./lib/transactions/transaction.js"),
-	username: require('./lib/transactions/username.js'),
+	username: require("./lib/transactions/username.js"),
 	vote : require("./lib/transactions/vote.js")
 }
 
