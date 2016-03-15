2adcc6115390d0cc1fbfcebfffece7b61cf3b974
Oliver Beddows <oliver@lisk.io>
2016-03-15 15:58:51 +0000

    Sorting properties.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 lisk = {
-	transaction : require("./lib/transactions/transaction.js"),
-	signature : require("./lib/transactions/signature.js"),
-	delegate : require("./lib/transactions/delegate.js"),
-	vote : require("./lib/transactions/vote.js"),
-	crypto : require("./lib/transactions/crypto.js"),
-	username: require('./lib/transactions/username.js'),
 	contact: require('./lib/transactions/contact.js'),
-	dapp: require('./lib/transactions/dapp.js')
+	crypto : require("./lib/transactions/crypto.js"),
+	dapp: require('./lib/transactions/dapp.js'),
+	delegate : require("./lib/transactions/delegate.js"),
+	signature : require("./lib/transactions/signature.js"),
+	transaction : require("./lib/transactions/transaction.js"),
+	username: require('./lib/transactions/username.js'),
+	vote : require("./lib/transactions/vote.js")
 }
 
 module.exports = lisk;
