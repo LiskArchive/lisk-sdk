1476835b55f0aab52a790e4311ce6cc61c28c47c
Olivier Beddows <olivier@lisk.io>
2016-05-19 19:04:02 +0100

    Sorting alphabetically.

--- helpers/constants.js
+++ helpers/constants.js
@@ -1,32 +1,32 @@
 module.exports = {
-	maxAmount: 100000000,
-	maxPayloadLength: 1024 * 1024,
-	blockHeaderLength: 248,
 	addressLength: 208,
-	maxAddressesLength: 208 * 128,
-	maxClientConnections: 100,
-	numberLength: 100000000,
-	feeStartVolume: 10000 * 100000000,
-	feeStart: 1,
+	blockHeaderLength: 248,
+	confirmationLength: 77,
 	fees:{
-		send:10000000,
-		vote:100000000,
-		secondsignature:500000000,
-		delegate:10000000000,
-		multisignature:500000000,
-		dapp:50000000000
+		send: 10000000,
+		vote: 100000000,
+		secondsignature: 500000000,
+		delegate: 10000000000,
+		multisignature: 500000000,
+		dapp: 50000000000
 	},
+	feeStart: 1,
+	feeStartVolume: 10000 * 100000000,
+	fixedPoint : Math.pow(10, 8),
+	maxAddressesLength: 208 * 128,
+	maxAmount: 100000000,
+	maxClientConnections: 100,
+	maxConfirmations : 77 * 100,
+	maxPayloadLength: 1024 * 1024,
+	maxRequests: 10000 * 12,
+	maxSignaturesLength: 196 * 256,
+	maxTxsPerBlock: 25,
+	numberLength: 100000000,
+	requestLength: 104,
 	rewards: {
 		offset: 2021,      // Start rewards at block (n)
 		distance: 3000000, // Distance between each milestone
 	},
-	maxRequests: 10000 * 12,
-	requestLength: 104,
 	signatureLength: 196,
-	maxSignaturesLength: 196 * 256,
-	maxConfirmations : 77 * 100,
-	confirmationLength: 77,
-	fixedPoint : Math.pow(10, 8),
-	totalAmount: 10000000000000000,
-	maxTxsPerBlock: 25
+	totalAmount: 10000000000000000
 }
