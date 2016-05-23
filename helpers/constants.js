31a7ae28d06afb55beb6a449cfb77bf73ad1cd5f
Olivier Beddows <olivier@lisk.io>
2016-05-23 21:51:16 +0100

    Setting rewardOfset to 60480.

--- helpers/constants.js
+++ helpers/constants.js
@@ -25,7 +25,7 @@ module.exports = {
 	numberLength: 100000000,
 	requestLength: 104,
 	rewards: {
-		offset: 2021,      // Start rewards at block (n)
+		offset: 60480,     // Start rewards at block (n)
 		distance: 3000000, // Distance between each milestone
 	},
 	signatureLength: 196,
