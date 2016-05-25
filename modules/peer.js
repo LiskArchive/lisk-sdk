d8504c50bb3fad253b864b236a07b522bc6792c2
Fix <fx.thoorens@gmail.com>
2016-05-25 22:17:33 +0200

    improvements

--- modules/peer.js
+++ modules/peer.js
@@ -12,6 +12,8 @@ var _ = require('underscore');
 // Private fields
 var modules, library, self, private = {}, shared = {};
 
+// the list of peers not behaving well
+// reset when we restart
 var removed=[];
 
 private.loopback = ["0.0.0.0", "127.0.0.1"];
@@ -77,14 +79,14 @@ private.updatePeerList = function (cb) {
 				return cb();
 			}
 
-			// Removing from the lists the nodes not working well
-			//library.logger.debug("removed peers list size: "+removed.length);
+			// Removing the nodes not working well
+			library.logger.debug("removed peers list size: "+removed.length);
 			var peers = data.body.peers.filter(function(peer){
 					return removed.indexOf(peer.ip);
 			});
 
 			// We update only a subset of the peers to decrease the noise on the network
-			// Default is 20 nodes. To be fined tuned. You get on average checked by a peer every 3s
+			// Default is 20 peers. To be fined tuned. You get checked by a peer every 3s on average
 			// Maybe increasing schedule (every 60s right now).
 			var maxUpdatePeers=library.config.peers.maxUpdatePeers || 20;
 			if(peers.length>maxUpdatePeers){
@@ -93,9 +95,11 @@ private.updatePeerList = function (cb) {
 
 			// We dropped one random peer from removed to give him a chance.
 			// This mitigate the fact that a node could be removed for ever if he was offline for long
-			// This is not harm for the node, but it prevent from network shrinking, but increase noise
-			// To fine tune decreasing random value threshold to reduce noise.
+			// This is not harm for the node, but it prevents from network shrinking, increasing noise.
+			// To fine tune: decreasing random value threshold -> reduce noise.
 			if(Math.random()<0.5){ // every 60/0.5=120s
+				// we want to remove the first element, ie the one that have been placed first
+				removed.shift();
 				removed.pop();
 			}
 
