83cb702f312240b1cf443901ac2b4fd49fe1c61e
Oliver Beddows <oliver@lisk.io>
2016-06-30 14:38:13 +0100

    Returning callback.

--- logic/account.js
+++ logic/account.js
@@ -603,7 +603,7 @@ Account.prototype.get = function (filter, fields, cb) {
 	}
 
 	this.getAll(filter, fields, function (err, data) {
-		cb(err, data && data.length ? data[0] : null)
+		return cb(err, data && data.length ? data[0] : null);
 	})
 }
 
