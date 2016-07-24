be91b27c077804bf6f6dbfffd21b9679be68181b
Oliver Beddows <oliver@lisk.io>
2016-07-24 14:25:29 +0200

    Adding upcaseAddresses migration.

---
+++ sql/migrations/20160724132825_upcaseAddresses.sql
@@ -0,0 +1,19 @@
+/* Upcase Addresses
+ *
+ */
+
+BEGIN;
+
+UPDATE "mem_accounts" SET "address" = UPPER("address") WHERE "address" LIKE '%l';
+
+UPDATE "mem_round" SET "address" = UPPER("address") WHERE "address" LIKE '%l';
+
+UPDATE "mem_accounts2delegates" SET "accountId" = UPPER("accountId") WHERE "accountId" LIKE '%l';
+
+UPDATE "mem_accounts2u_delegates" SET "accountId" = UPPER("accountId") WHERE "accountId" LIKE '%l';
+
+UPDATE "mem_accounts2multisignatures" SET "accountId" = UPPER("accountId") WHERE "accountId" LIKE '%l';
+
+UPDATE "mem_accounts2u_multisignatures" SET "accountId" = UPPER("accountId") WHERE "accountId" LIKE '%l';
+
+COMMIT;
