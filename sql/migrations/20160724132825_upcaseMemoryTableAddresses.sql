612b438b9dad7710862bf59ebe659b966f53ff86
Oliver Beddows <oliver@lisk.io>
2016-08-02 12:08:56 +0200

    Adjusting migration.
    - Merging balances between lower/upper cased accounts.
    - Deleting lowercased addresses without a public key.

--- sql/migrations/20160724132825_upcaseMemoryTableAddresses.sql
+++ sql/migrations/20160724132825_upcaseMemoryTableAddresses.sql
@@ -4,6 +4,18 @@
 
 BEGIN;
 
+UPDATE "mem_accounts" AS m
+   SET "balance" = (m."balance" + l."balance"),
+       "u_balance" = (m."u_balance" + l."u_balance")
+  FROM (
+    SELECT "address", "balance", "u_balance"
+      FROM "mem_accounts"
+     WHERE "address" LIKE '%l'
+  ) AS "l"
+ WHERE m."address" = UPPER(l."address");
+
+DELETE FROM "mem_accounts" WHERE "address" LIKE '%l' AND "publicKey" IS NULL;
+
 UPDATE "mem_accounts" SET "address" = UPPER("address") WHERE "address" LIKE '%l';
 
 UPDATE "mem_round" SET "address" = UPPER("address") WHERE "address" LIKE '%l';
