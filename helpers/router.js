f91ae84db0df205a3eb1cfb3744e986c8a68c935
Oliver Beddows <oliver@lisk.io>
2016-08-22 23:55:30 +0200

    Hinting file.

--- helpers/router.js
+++ helpers/router.js
@@ -1,18 +1,21 @@
+'use strict';
+
 var extend = require('extend');
 
-function map(root, config) {
+function Map (root, config) {
 	var router = this;
+
 	Object.keys(config).forEach(function (params) {
-		var route = params.split(" ");
-		if (route.length != 2 || ["post", "get", "put"].indexOf(route[0]) == -1) {
-			throw Error("Invalid map config");
+		var route = params.split(' ');
+		if (route.length !== 2 || ['post', 'get', 'put'].indexOf(route[0]) === -1) {
+			throw Error('Invalid map config');
 		}
 		router[route[0]](route[1], function (req, res, next) {
-			root[config[params]]({"body": route[0] == "get" ? req.query : req.body}, function (err, response) {
+			root[config[params]]({'body': route[0] === 'get' ? req.query : req.body}, function (err, response) {
 				if (err) {
-					res.json({"success": false, "error": err});
+					res.json({'success': false, 'error': err});
 				} else {
-					return res.json(extend({}, {"success": true}, response));
+					return res.json(extend({}, {'success': true}, response));
 				}
 			});
 		});
@@ -28,14 +31,14 @@ var Router = function () {
 	var router = require('express').Router();
 
 	router.use(function (req, res, next) {
-		res.header("Access-Control-Allow-Origin", "*");
-		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 		next();
 	});
 
-	router.map = map;
+	router.map = Map;
 
 	return router;
-}
+};
 
 module.exports = Router;
