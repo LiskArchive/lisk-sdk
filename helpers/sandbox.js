dafd3713f4c0c6eac829f9e845c2f03eb5060e4c
Oliver Beddows <oliver@lisk.io>
2016-08-22 23:51:59 +0200

    Hinting file.

--- helpers/sandbox.js
+++ helpers/sandbox.js
@@ -1,6 +1,8 @@
-function callMethod(shared, call, args, cb) {
-	if (typeof shared[call] !== "function") {
-		return cb("Function not found in module: " + call);
+'use strict';
+
+function callMethod (shared, call, args, cb) {
+	if (typeof shared[call] !== 'function') {
+		return cb('Function not found in module: ' + call);
 	}
 
 	var callArgs = [args, cb];
