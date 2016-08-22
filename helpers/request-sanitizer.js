197e12aa154c212c2bf2ef99582019ba2e054ec5
Oliver Beddows <oliver@lisk.io>
2016-08-23 00:10:35 +0200

    Hinting file.

--- helpers/request-sanitizer.js
+++ helpers/request-sanitizer.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var Validator = require('./validator/validator.js');
 var extend = require('extend');
 var inherits = require('util').inherits;
@@ -17,38 +19,38 @@ RequestSanitizer.options = extend({
     reporter : SanitizeReporter
 }, Validator.options);
 
-RequestSanitizer.addRule("empty", {
+RequestSanitizer.addRule('empty', {
     validate : function(accept, value, field) {
-        if (accept !== false) return;
+        if (accept !== false) { return; }
 
         return !field.isEmpty();
     }
 });
 
-RequestSanitizer.addRule("string", {
+RequestSanitizer.addRule('string', {
     filter : function(accept, value, field){
-        if (field.isEmpty() && field.rules.empty) return null;
+        if (field.isEmpty() && field.rules.empty) { return null; }
 
         return String(value||'');
     }
 });
 
-RequestSanitizer.addRule("regexp", {
-    message : "value should match template",
+RequestSanitizer.addRule('regexp', {
+    message : 'value should match template',
     validate : function(accept, value) {
-        if (typeof value !== 'string') return false;
+        if (typeof value !== 'string') { return false; }
 
         return accept.test(value);
     }
 });
 
-RequestSanitizer.addRule("boolean", {
+RequestSanitizer.addRule('boolean', {
     filter : function(accept, value, field){
-        if (field.isEmpty() && field.rules.empty) return null;
+        if (field.isEmpty() && field.rules.empty) { return null; }
 
         switch(String(value).toLowerCase()) {
-            case "false":
-            case "f":
+            case 'false':
+            case 'f':
                 return false;
             default:
                 return !!value;
@@ -56,21 +58,21 @@ RequestSanitizer.addRule("boolean", {
     }
 });
 
-RequestSanitizer.addRule("int", {
+RequestSanitizer.addRule('int', {
     filter : function(accept, value , field) {
-        if (field.isEmpty() && field.rules.empty) return null;
+        if (field.isEmpty() && field.rules.empty) { return null; }
 
-		if (isNaN(value) || parseInt(value) != value || isNaN(parseInt(value, 10))) {
-			return 0;
-		}
+        if (isNaN(value) || parseInt(value) !== value || isNaN(parseInt(value, 10))) {
+            return 0;
+        }
 
-		return parseInt(value);
+        return parseInt(value);
     }
 });
 
-RequestSanitizer.addRule("float", {
+RequestSanitizer.addRule('float', {
     filter : function(accept, value , field) {
-        if (field.isEmpty() && field.rules.empty) return null;
+        if (field.isEmpty() && field.rules.empty) { return null; }
 
         value = parseFloat(value);
 
@@ -78,19 +80,19 @@ RequestSanitizer.addRule("float", {
     }
 });
 
-RequestSanitizer.addRule("object", {
+RequestSanitizer.addRule('object', {
     filter : function(accept, value , field) {
-        if (field.isEmpty() && field.rules.empty) return null;
+        if (field.isEmpty() && field.rules.empty) { return null; }
 
-       return Object.prototype.toString.call(value) == "[object Object]" ? value : {};
+       return Object.prototype.toString.call(value) === '[object Object]' ? value : {};
     }
 });
 
-RequestSanitizer.addRule("array", {
+RequestSanitizer.addRule('array', {
     filter: function (accept, value, field) {
-        if (field.isEmpty() && field.rules.empty) return null;
+        if (field.isEmpty() && field.rules.empty) { return null; }
 
-        if (typeof value === "string" && (typeof accept === "string" || accept instanceof RegExp )) {
+        if (typeof value === 'string' && (typeof accept === 'string' || accept instanceof RegExp )) {
             return value.length ? value.split(accept) : [];
         } else if (Array.isArray(value)) {
             return value;
@@ -100,10 +102,10 @@ RequestSanitizer.addRule("array", {
     }
 });
 
-RequestSanitizer.addRule("arrayOf", {
+RequestSanitizer.addRule('arrayOf', {
     validate : function(accept, value, field) {
-        if (field.isEmpty() && field.rules.empty) return null;
-        if (! Array.isArray(value)) return false;
+        if (field.isEmpty() && field.rules.empty) { return null; }
+        if (! Array.isArray(value)) { return false; }
 
         var l = value.length;
         var i = -1;
@@ -115,22 +117,22 @@ RequestSanitizer.addRule("arrayOf", {
     }
 });
 
-RequestSanitizer.addRule("hex", {
+RequestSanitizer.addRule('hex', {
     filter : function(accept, value, field) {
-        if (field.isEmpty() && field.rules.empty) return null;
+        if (field.isEmpty() && field.rules.empty) { return null; }
 
         return value;
     },
     validate : function(accept, value, field) {
-        if (field.isEmpty() && field.rules.empty) return;
+        if (field.isEmpty() && field.rules.empty) { return; }
 
         return /^([A-Fa-f0-9]{2})*$/.test(String(value||''));
     }
 });
 
-RequestSanitizer.addRule("buffer", {
+RequestSanitizer.addRule('buffer', {
     filter : function(accept, value) {
-        if (typeof accept !== "string") {
+        if (typeof accept !== 'string') {
             accept = 'utf8';
         }
 
@@ -142,39 +144,39 @@ RequestSanitizer.addRule("buffer", {
     }
 });
 
-RequestSanitizer.addRule("variant", {
+RequestSanitizer.addRule('variant', {
     filter : function(accept, value, field) {
-        if (field.isEmpty() && field.rules.empty) return null;
+        if (field.isEmpty() && field.rules.empty) { return null; }
 
         return typeof value === 'undefined' ? '' : value;
     }
 });
 
-RequestSanitizer.addRule("required", {
-    message : "value is required",
+RequestSanitizer.addRule('required', {
+    message : 'value is required',
     validate : function(accept, value) {
-        return (typeof value !== 'undefined') == accept;
+        return (typeof value !== 'undefined') === accept;
     }
 });
 
-RequestSanitizer.addRule("default", {
+RequestSanitizer.addRule('default', {
     filter : function(accept, value) {
         return (typeof value === 'undefined') ? accept : value;
     }
 });
 
-RequestSanitizer.addRule("properties", {
+RequestSanitizer.addRule('properties', {
     validate : function(accept, value, field) {
-        if (! field.isObject()) return false;
+        if (! field.isObject()) { return false; }
 
         Object.getOwnPropertyNames(accept).forEach(function(name){
             var childAccept = accept[name];
-            if (typeof childAccept === "string") {
+            if (typeof childAccept === 'string') {
                 childAccept = convertStringRule(childAccept);
             }
             var child = field.child(name, value[name], childAccept, value);
             child.validate(function(err, report, output){
-                if (err) throw err;
+                if (err) { throw err; }
 
                 value[name] = output;
             });
@@ -182,41 +184,41 @@ RequestSanitizer.addRule("properties", {
     }
 });
 
-RequestSanitizer.addRule("minLength", {
-    message : "minimum length is ${accept}.",
+RequestSanitizer.addRule('minLength', {
+    message : 'minimum length is ${accept}.',
     validate : function(accept, value) {
         return value.length >= accept;
     }
 });
 
-RequestSanitizer.addRule("case", {
-    message : "case is ${accept}.",
+RequestSanitizer.addRule('case', {
+    message : 'case is ${accept}.',
     validate : function(accept, value) {
-        return typeof value === "string" && ((accept==="lower" && value===value.toLowerCase())||(accept==="upper" && value===value.toUpperCase()));
+        return typeof value === 'string' && ((accept==='lower' && value===value.toLowerCase())||(accept==='upper' && value===value.toUpperCase()));
     },
     filter : function(accept, value, field){
-        if (field.isEmpty() && field.rules.empty) return null;
+        if (field.isEmpty() && field.rules.empty) { return null; }
 
-        if(accept==="lower"){
+        if(accept==='lower'){
           return String(value||'').toLowerCase();
         }
-        else if(accept==="upper"){
+        else if(accept==='upper'){
           return String(value||'').toUpperCase();
         }
     }
 });
 
-RequestSanitizer.addRule("maxLength", {
-    message : "maximum length is ${accept}.",
+RequestSanitizer.addRule('maxLength', {
+    message : 'maximum length is ${accept}.',
     validate : function(accept, value) {
         return value.length <= accept;
     }
 });
 
-RequestSanitizer.addRule("maxByteLength", {
-    message : "maximum size is ${accept.length} bytes",
+RequestSanitizer.addRule('maxByteLength', {
+    message : 'maximum size is ${accept.length} bytes',
     accept : function(accept){
-        if (typeof accept !== "object") {
+        if (typeof accept !== 'object') {
             accept = {
                 encoding : 'utf8',
                 length : accept
@@ -225,7 +227,7 @@ RequestSanitizer.addRule("maxByteLength", {
         return accept;
     },
     validate : function(accept, value, field) {
-        if (field.isEmpty() && field.rules.empty) return;
+        if (field.isEmpty() && field.rules.empty) { return; }
 
         if (typeof value === 'object' && value !== null) {
             value = JSON.stringify(value);
@@ -236,10 +238,10 @@ RequestSanitizer.addRule("maxByteLength", {
     }
 });
 
-RequestSanitizer.addRule("minByteLength", {
-    message : "minimum size is ${accept.length} bytes",
+RequestSanitizer.addRule('minByteLength', {
+    message : 'minimum size is ${accept.length} bytes',
     accept : function(accept){
-        if (typeof accept !== "object") {
+        if (typeof accept !== 'object') {
             accept = {
                 encoding : 'utf8',
                 length : accept
@@ -249,7 +251,7 @@ RequestSanitizer.addRule("minByteLength", {
         return accept;
     },
     validate : function(accept, value, field) {
-        if (field.isEmpty() && field.rules.empty) return;
+        if (field.isEmpty() && field.rules.empty) { return; }
 
         if (typeof value === 'object' && value !== null) {
             value = JSON.stringify(value);
@@ -272,13 +274,13 @@ RequestSanitizer.express = function(options) {
 
         function sanitize(value, properties, callback) {
             var values = {};
-            if (typeof value === "string") {
+            if (typeof value === 'string') {
                 value = req[value] || {};
             }
 
             Object.getOwnPropertyNames(properties).forEach(function(name){
                 values[name] = value.hasOwnProperty(name) ? value[name] : undefined;
-                if (typeof properties[name] === "string") {
+                if (typeof properties[name] === 'string') {
                     properties[name] = convertStringRule(properties[name]);
                 }
             });
@@ -304,12 +306,12 @@ var rules = RequestSanitizer.prototype.rules;
     'buffer'
 ].forEach(function(name){
     var rule = rules[name];
-    if (typeof rule.filter !== 'function') return;
-    if (name in RequestSanitizer) return;
+    if (typeof rule.filter !== 'function') { return; }
+    if (name in RequestSanitizer) { return; }
 
     RequestSanitizer[name] = function filter(value, extra) {
         var rules = {};
-        if (typeof extra === "object") {
+        if (typeof extra === 'object') {
             extend(rules, extra);
         } else if (typeof extra !== 'undefined') {
             rules.empty = extra;
@@ -346,27 +348,27 @@ SanitizeReporter.prototype.convert = function(issues) {
 
     var grouped = issues.reduce(function(result, item){
         var path = item.path.join('.');
-        if (path in result === false) result[path] = [];
+        if (path in result === false) { result[path] = []; }
 
         result[path].push(item);
 
         return result;
     }, {});
 
-    var result = "";
+    var result = '';
 
     Object.getOwnPropertyNames(grouped).forEach(function(path){
-        result += "Property \"" + path + "\":\n";
+        result += 'Property \'' + path + '\':\n';
 
         grouped[path].forEach(function(item){
             var rule = self.validator.getRule(item.rule);
 
-            result += "\t- ";
+            result += '\t- ';
 
             if (rule.hasOwnProperty('message')) {
-                result += self.format(rule.message, item) + "\n";
+                result += self.format(rule.message, item) + '\n';
             } else {
-                result += "break rule \"" + item.rule + "\"\n";
+                result += 'break rule \'' + item.rule + '\'\n';
             }
         });
     });
@@ -394,10 +396,10 @@ function getByPath(target, path) {
 function convertStringRule(rule) {
     var result = {};
 
-    if (rule.charAt(rule.length-1) === "!") {
+    if (rule.charAt(rule.length-1) === '!') {
         result.required = true;
         rule = rule.slice(0, -1);
-    } else if (rule.charAt(rule.length-1) === "?") {
+    } else if (rule.charAt(rule.length-1) === '?') {
         result.empty = true;
         rule = rule.slice(0, -1);
     }
