6b3f6c067ec8f6a17c0e09885f9f5f09ec0632d9
Oliver Beddows <oliver@lisk.io>
2016-08-22 23:51:29 +0200

    Hinting file.

--- helpers/sequence.js
+++ helpers/sequence.js
@@ -1,11 +1,13 @@
+'use strict';
+
 var util = require('util');
 var extend = require('extend');
 
-function Sequence(config) {
+function Sequence (config) {
 	var _default = {
 		onWarning: null,
 		warningLimit: 50
-	}
+	};
 	_default = extend(_default, config);
 	var self = this;
 	this.sequence = [];
@@ -26,31 +28,33 @@ Sequence.prototype.__tick = function (cb) {
 		return setImmediate(cb);
 	}
 	var args = [function (err, res) {
-		task.done && setImmediate(task.done, err, res);
+		if (task.done) {
+			setImmediate(task.done, err, res);
+		}
 		setImmediate(cb);
 	}];
 	if (task.args) {
 		args = args.concat(task.args);
 	}
 	task.worker.apply(task.worker, args);
-}
+};
 
 Sequence.prototype.add = function (worker, args, done) {
-	if (!done && args && typeof(args) == 'function') {
+	if (!done && args && typeof(args) === 'function') {
 		done = args;
 		args = undefined;
 	}
-	if (worker && typeof(worker) == 'function') {
+	if (worker && typeof(worker) === 'function') {
 		var task = {worker: worker, done: done};
 		if (util.isArray(args)) {
 			task.args = args;
 		}
 		this.sequence.push(task);
 	}
-}
+};
 
 Sequence.prototype.count = function () {
 	return this.sequence.length;
-}
+};
 
 module.exports = Sequence;
