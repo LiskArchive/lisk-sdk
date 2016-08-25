e3298b4f7965c44d135455f81ed167dcfbf92418
Oliver Beddows <oliver@lisk.io>
2016-08-25 22:45:19 +0200

    Breaking up miscellaneous tests.
    Moving into separate blocks and peers tests.

--- test/api/blocks.js
+++ test/api/blocks.js
@@ -3,136 +3,14 @@
 var node = require("./../variables.js");
 
 var block = {
-	blockHeight : 0,
-	id : 0,
-	generatorPublicKey : "",
-	totalAmount : 0,
-	totalFee : 0
+	blockHeight: 0,
+	id: 0,
+	generatorPublicKey: "",
+	totalAmount: 0,
+	totalFee: 0
 };
 
 var testBlocksUnder101 = 0;
-
-describe("GET /peers/version", function () {
-
-	it("Should be ok", function (done) {
-		node.api.get("/peers/version")
-			.set("Accept", "application/json")
-			.expect("Content-Type", /json/)
-			.expect(200)
-			.end(function (err, res) {
-				// console.log(JSON.stringify(res.body));
-				node.expect(res.body).to.have.property("success").to.be.true;
-				node.expect(res.body).to.have.property("build").to.be.a("string");
-				node.expect(res.body).to.have.property("version").to.be.a("string");
-				done();
-			});
-	});
-});
-
-describe("GET /peers", function () {
-
-	it("Using empty parameters. Should fail", function (done) {
-		var state = "", os = "", shared = "", version = "", limit = "", offset = 0, orderBy = "";
-		node.api.get("/peers?state="+state+"&os="+os+"&shared="+true+"&version="+version+"&limit="+limit+"&offset="+offset+"orderBy="+orderBy)
-			.set("Accept", "application/json")
-			.expect("Content-Type", /json/)
-			.expect(200)
-			.end(function (err, res) {
-				// console.log(JSON.stringify(res.body));
-				node.expect(res.body).to.have.property("success").to.be.false;
-				node.expect(res.body).to.have.property("error");
-				done();
-			});
-	});
-
-	it("Using state. Should be ok", function (done) {
-		var state = 1;
-		node.api.get("/peers?state="+state)
-			.set("Accept", "application/json")
-			.expect("Content-Type", /json/)
-			.expect(200)
-			.end(function (err, res) {
-				// console.log(JSON.stringify(res.body));
-				node.expect(res.body).to.have.property("success").to.be.true;
-				node.expect(res.body).to.have.property("peers").that.is.an("array");
-				if (res.body.peers.length > 0) {
-					for (var i = 0; i < res.body.peers.length; i++) {
-						 node.expect(res.body.peers[i].state).to.equal(parseInt(state));
-					}
-				}
-				done();
-			});
-	});
-
-	it("Using limit. Should be ok", function (done) {
-		var limit = 3, offset = 0;
-		node.api.get("/peers?&limit="+limit+"&offset="+offset)
-			.set("Accept", "application/json")
-			.expect("Content-Type", /json/)
-			.expect(200)
-			.end(function (err, res) {
-				// console.log(JSON.stringify(res.body));
-				node.expect(res.body).to.have.property("success").to.be.true;
-				node.expect(res.body).to.have.property("peers").that.is.an("array");
-
-				// To check it need to have peers
-				node.expect(res.body.peers.length).to.be.at.most(limit);
-				done();
-			});
-	});
-
-	it("Using orderBy. Should be ok", function (done) {
-		var orderBy = "state:desc";
-		node.api.get("/peers?orderBy="+orderBy)
-			.set("Accept", "application/json")
-			.expect("Content-Type", /json/)
-			.expect(200)
-			.end(function (err, res) {
-				// console.log(JSON.stringify(res.body));
-				node.expect(res.body).to.have.property("success").to.be.true;
-				node.expect(res.body).to.have.property("peers").that.is.an("array");
-
-				if (res.body.peers.length > 0) {
-					for (var i = 0; i < res.body.peers.length; i++) {
-						if (res.body.peers[i+1] != null) {
-							node.expect(res.body.peers[i+1].state).to.at.most(res.body.peers[i].state);
-						}
-					}
-				}
-
-				done();
-			});
-
-	});
-
-	it("Using limit > 100. Should fail", function (done) {
-		var limit = 101;
-		node.api.get("/peers?&limit="+limit)
-			.set("Accept", "application/json")
-			.expect("Content-Type", /json/)
-			.expect(200)
-			.end(function (err, res) {
-				// console.log(JSON.stringify(res.body));
-				node.expect(res.body).to.have.property("success").to.be.false;
-				node.expect(res.body).to.have.property("error");
-				done();
-			});
-	});
-
-	it("Using invalid parameters. Should fail", function (done) {
-		var state = "invalid", os = "invalid", shared = "invalid", version = "invalid", limit = "invalid", offset = "invalid", orderBy = "invalid";
-		node.api.get("/peers?state="+state+"&os="+os+"&shared="+shared+"&version="+version+"&limit="+limit+"&offset="+offset+"orderBy="+orderBy)
-			.set("Accept", "application/json")
-			.expect("Content-Type", /json/)
-			.expect(200)
-			.end(function (err, res) {
-				// console.log(JSON.stringify(res.body));
-				node.expect(res.body).to.have.property("success").to.be.false;
-				node.expect(res.body).to.have.property("error");
-				done();
-			});
-	});
-});
 
 describe("GET /blocks/getEpoch", function () {
 
@@ -422,6 +300,7 @@ describe("GET /blocks", function () {
 	it("Using previousBlock. Should be ok", function (done) {
 		if (block.id != null) {
 			var previousBlock = block.id;
+
 			node.onNewBlock(function (err) {
 				node.expect(err).to.be.not.ok;
 				node.api.get("/blocks?previousBlock="+previousBlock)
@@ -442,6 +321,7 @@ describe("GET /blocks", function () {
 
 	it("Using orderBy. Should be ok", function (done) {
 		var orderBy = "height:desc";
+
 		node.api.get("/blocks?orderBy="+orderBy)
 			.set("Accept", "application/json")
 			.expect("Content-Type", /json/)
--- test/index.js
+++ test/index.js
@@ -1,13 +1,14 @@
-require("./api/accounts.js");
-require("./api/dapps.js");
-require("./api/delegates.js");
-require("./api/loader.js");
-require("./api/miscellaneous.js");
-require("./api/multisignatures.js");
-require("./api/peers.delegates.js");
-require("./api/peers.secondpassword.js");
-require("./api/peers.transactions.js");
-require("./api/peers.votes.js");
-require("./api/transactions.js");
+require('./api/accounts.js');
+require('./api/blocks.js');
+require('./api/dapps.js');
+require('./api/delegates.js');
+require('./api/loader.js');
+require('./api/multisignatures.js');
+require('./api/peers.js');
+require('./api/peers.delegates.js');
+require('./api/peers.secondpassword.js');
+require('./api/peers.transactions.js');
+require('./api/peers.votes.js');
+require('./api/transactions.js');
 
 require("./unit/logic/blockReward.js");
