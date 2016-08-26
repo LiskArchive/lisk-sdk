601cca5d4e823bf653ed462cd3514153dce26a98
Oliver Beddows <oliver@lisk.io>
2016-08-26 22:04:38 +0200

    Refactoring tests.

--- test/api/peers.delegates.js
+++ test/api/peers.delegates.js
@@ -6,214 +6,162 @@ var node = require('./../node.js');
 var account = node.randomAccount();
 var account2 = node.randomAccount();
 
+function openAccount (account, done) {
+	node.api.post('/accounts/open')
+		.set('Accept', 'application/json')
+		.set('version', node.version)
+		.set('nethash', node.config.nethash)
+		.set('port', node.config.port)
+		.send({
+			secret: account.password
+		})
+		.expect('Content-Type', /json/)
+		.expect(200)
+		.end(function (err, res) {
+			// console.log(JSON.stringify(res.body));
+			done(err, res);
+		});
+}
+
+function sendLISK (params, done) {
+	node.api.put('/transactions')
+		.set('Accept', 'application/json')
+		.set('version', node.version)
+		.set('nethash', node.config.nethash)
+		.set('port', node.config.port)
+		.send(params)
+		.expect('Content-Type', /json/)
+		.expect(200)
+		.end(function (err, res) {
+			// console.log(JSON.stringify(res.body));
+			node.onNewBlock(function (err) {
+				node.expect(err).to.be.not.ok;
+				done(err, res);
+			});
+		});
+}
+
+function postTransaction (transaction, done) {
+	node.peer.post('/transactions')
+		.set('Accept', 'application/json')
+		.set('version', node.version)
+		.set('nethash', node.config.nethash)
+		.set('port', node.config.port)
+		.send({
+			transaction: transaction
+		})
+		.expect('Content-Type', /json/)
+		.expect(200)
+		.end(function (err, res) {
+			// console.log(JSON.stringify(res.body));
+			done(err, res);
+		});
+}
+
 describe('POST /peer/transactions', function () {
 
 	describe('registering a delegate', function () {
 
-		it('using invalid username should fail', function (done) {
-			node.api.post('/accounts/open')
-				.set('Accept', 'application/json')
-				.set('version', node.version)
-				.set('nethash', node.config.nethash)
-				.set('port', node.config.port)
-				.send({
-					secret: account.password
-				})
-				.expect('Content-Type', /json/)
-				.expect(200)
-				.end(function (err, res) {
+		describe('when account has no funds', function () {
+
+			it('should fail', function (done) {
+				var transaction = node.lisk.delegate.createDelegate(node.randomPassword(), node.randomDelegateName().toLowerCase());
+				transaction.fee = node.Fees.delegateRegistrationFee;
+
+				postTransaction(transaction, function (err, res) {
+					node.expect(res.body).to.have.property('success').to.be.not.ok;
+					done();
+				});
+			});
+		});
+
+		describe('when account has funds', function () {
+
+			before(function (done) {
+				openAccount(account, function (err, res) {
 					account.address = res.body.account.address;
-					node.api.put('/transactions')
-						.set('Accept', 'application/json')
-						.set('version', node.version)
-						.set('nethash', node.config.nethash)
-						.set('port', node.config.port)
-						.send({
-							secret: node.Gaccount.password,
-							amount: node.Fees.delegateRegistrationFee,
-							recipientId: account.address
-						})
-						.expect('Content-Type', /json/)
-						.expect(200)
-						.end(function (err, res) {
-							node.onNewBlock(function (err) {
-								node.expect(err).to.be.not.ok;
-								var transaction = node.lisk.delegate.createDelegate(account.password, crypto.randomBytes(64).toString('hex'));
-								transaction.fee = node.Fees.delegateRegistrationFee;
-
-								node.peer.post('/transactions')
-									.set('Accept', 'application/json')
-									.set('version',node.version)
-									.set('nethash', node.config.nethash)
-									.set('port', node.config.port)
-									.send({
-										transaction: transaction
-									})
-									.expect('Content-Type', /json/)
-									.expect(200)
-									.end(function (err, res) {
-										// console.log(JSON.stringify(res.body));
-										node.expect(res.body).to.have.property('success').to.be.not.ok;
-										done();
-									});
-							});
-						});
+					sendLISK({
+						secret: node.Gaccount.password,
+						amount: node.Fees.delegateRegistrationFee,
+						recipientId: account.address
+					}, function (err, res) {
+						done();
+					});
 				});
-		});
+			});
 
-		it('when account has no funds should fail', function (done) {
-			var transaction = node.lisk.delegate.createDelegate(node.randomPassword(), node.randomDelegateName().toLowerCase());
-			transaction.fee = node.Fees.delegateRegistrationFee;
+			it('using invalid username should fail', function (done) {
+				var transaction = node.lisk.delegate.createDelegate(account.password, crypto.randomBytes(64).toString('hex'));
+				transaction.fee = node.Fees.delegateRegistrationFee;
 
-			node.peer.post('/transactions')
-				.set('Accept', 'application/json')
-				.set('version', node.version)
-				.set('nethash', node.config.nethash)
-				.set('port', node.config.port)
-				.send({
-					transaction: transaction
-				})
-				.expect('Content-Type', /json/)
-				.expect(200)
-				.end(function (err, res) {
-					// console.log(JSON.stringify(res.body));
+				postTransaction(transaction, function (err, res) {
 					node.expect(res.body).to.have.property('success').to.be.not.ok;
 					done();
 				});
-		});
+			});
 
-		it('when account has funds and username is uppercase should fail', function (done) {
-			account.username = node.randomDelegateName().toUpperCase();
-			var transaction = node.lisk.delegate.createDelegate(account.password, account.username);
+			it('using uppercase username should fail', function (done) {
+				account.username = node.randomDelegateName().toUpperCase();
+				var transaction = node.lisk.delegate.createDelegate(account.password, account.username);
 
-			node.peer.post('/transactions')
-				.set('Accept', 'application/json')
-				.set('version', node.version)
-				.set('nethash', node.config.nethash)
-				.set('port', node.config.port)
-				.send({
-					transaction: transaction
-				})
-				.expect('Content-Type', /json/)
-				.expect(200)
-				.end(function (err, res) {
-					// console.log(JSON.stringify(res.body));
+				postTransaction(transaction, function (err, res) {
 					node.expect(res.body).to.have.property('success').to.be.not.ok;
 					done();
 				});
-		});
+			});
 
-		it('when account has funds and username is uppercase and lowercase username already registered should fail', function (done) {
-			var transaction = node.lisk.delegate.createDelegate(account2.password, account.username.toUpperCase());
+			describe('when lowercased username already registered', function () {
+				it('using uppercase username should fail', function (done) {
+					var transaction = node.lisk.delegate.createDelegate(account2.password, account.username.toUpperCase());
 
-			node.peer.post('/transactions')
-				.set('Accept', 'application/json')
-				.set('version', node.version)
-				.set('nethash', node.config.nethash)
-				.set('port', node.config.port)
-				.send({
-					transaction: transaction
-				})
-				.expect('Content-Type', /json/)
-				.expect(200)
-				.end(function (err, res) {
-					// console.log(JSON.stringify(res.body));
-					node.expect(res.body).to.have.property('success').to.be.not.ok;
-					done();
+					postTransaction(transaction, function (err, res) {
+						node.expect(res.body).to.have.property('success').to.be.not.ok;
+						done();
+					});
 				});
-		});
+			});
 
-		it('when account has funds and username is lowercase should be ok', function (done) {
-			account.username = node.randomDelegateName().toLowerCase();
-			var transaction = node.lisk.delegate.createDelegate(account.password, account.username);
+			it('using lowercase username should be ok', function (done) {
+				account.username = node.randomDelegateName().toLowerCase();
+				var transaction = node.lisk.delegate.createDelegate(account.password, account.username);
 
-			node.peer.post('/transactions')
-				.set('Accept', 'application/json')
-				.set('version', node.version)
-				.set('nethash', node.config.nethash)
-				.set('port', node.config.port)
-				.send({
-					transaction: transaction
-				})
-				.expect('Content-Type', /json/)
-				.expect(200)
-				.end(function (err, res) {
-					// console.log(JSON.stringify(res.body));
+				postTransaction(transaction, function (err, res) {
 					node.expect(res.body).to.have.property('success').to.be.ok;
 					done();
 				});
+			});
 		});
 
-		it('twice within the same block should fail', function (done) {
-			node.api.post('/accounts/open')
-				.set('Accept', 'application/json')
-				.set('version', node.version)
-				.set('nethash', node.config.nethash)
-				.set('port', node.config.port)
-				.send({
-					secret: account2.password
-				})
-				.expect('Content-Type', /json/)
-				.expect(200)
-				.end(function (err, res) {
+		describe('twice within the same block', function () {
+
+			before(function (done) {
+				openAccount(account2, function (err, res) {
 					account2.address = res.body.account.address;
-					// console.log(account2);
-					node.api.put('/transactions')
-						.set('Accept', 'application/json')
-						.set('version', node.version)
-						.set('nethash', node.config.nethash)
-						.set('port', node.config.port)
-						.send({
-							secret: node.Gaccount.password,
-							amount: node.Fees.delegateRegistrationFee,
-							recipientId: account2.address
-						})
-						.expect('Content-Type', /json/)
-						.expect(200)
-						.end(function (err, res) {
-							// console.log(res.body);
-							node.onNewBlock(function (err) {
-								node.expect(err).to.be.not.ok;
-								account2.username = node.randomDelegateName().toLowerCase();
-								var transaction = node.lisk.delegate.createDelegate(account2.password, account2.username);
-								// console.log(transaction);
-
-								node.peer.post('/transactions')
-									.set('Accept', 'application/json')
-									.set('version', node.version)
-									.set('nethash', node.config.nethash)
-									.set('port', node.config.port)
-									.send({
-										transaction: transaction
-									})
-									.expect('Content-Type', /json/)
-									.expect(200)
-									.end(function (err, res) {
-										// console.log(res.body);
-										node.expect(res.body).to.have.property('success').to.be.ok;
-
-										account2.username = node.randomDelegateName().toLowerCase();
-										var transaction2 = node.lisk.delegate.createDelegate(account2.password, account2.username);
-
-										node.peer.post('/transactions')
-											.set('Accept', 'application/json')
-											.set('version', node.version)
-											.set('nethash', node.config.nethash)
-											.set('port', node.config.port)
-											.send({
-												transaction: transaction2
-											})
-											.expect('Content-Type', /json/)
-											.expect(200)
-											.end(function (err, res) {
-												// console.log(JSON.stringify(res.body));
-												node.expect(res.body).to.have.property('success').to.be.not.ok;
-												done();
-											});
-									});
-							});
-						});
+					sendLISK({
+						secret: node.Gaccount.password,
+						amount: (node.Fees.delegateRegistrationFee * 2),
+						recipientId: account2.address
+					}, function (err, res) {
+						done();
 					});
 				});
+			});
+
+			it('should fail', function (done) {
+				account2.username = node.randomDelegateName().toLowerCase();
+				var transaction = node.lisk.delegate.createDelegate(account2.password, account2.username);
+
+				account2.username = node.randomDelegateName().toLowerCase();
+				var transaction2 = node.lisk.delegate.createDelegate(account2.password, account2.username);
+
+				postTransaction(transaction, function (err, res) {
+					node.expect(res.body).to.have.property('success').to.be.ok;
+					postTransaction(transaction2, function (err, res) {
+						node.expect(res.body).to.have.property('success').to.be.not.ok;
+						done();
+					});
+				});
+			});
+		});
 	});
 });
