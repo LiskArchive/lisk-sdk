657fa7e950ad54ae5882e5dc8d3e318453847786
Oliver Beddows <oliver@lisk.io>
2016-08-26 23:06:33 +0200

    Refactoring tests.

--- test/api/peers.transactions.js
+++ test/api/peers.transactions.js
@@ -5,47 +5,57 @@ var crypto = require('crypto');
 
 var genesisblock = require('../../genesisBlock.json');
 
+function postTransaction (transaction, done) {
+	node.peer.post('/transactions')
+		.set('Accept', 'application/json')
+		.set('version', node.version)
+		.set('nethash', node.config.nethash)
+		.set('port', node.config.port)
+		.send({
+			transaction: transaction
+		})
+		.expect('Content-Type', /json/)
+		.expect(200)
+		.end(function (err, res) {
+			// console.log(JSON.stringify(res.body));
+			done(err, res);
+		});
+}
+
+function getAddress (address, done) {
+	node.api.get('/accounts?address=' + address)
+		.set('Accept', 'application/json')
+		.expect('Content-Type', /json/)
+		.expect(200)
+		.end(function (err, res) {
+			// console.log(JSON.stringify(res.body));
+			done(err, res);
+		});
+}
+
 describe('POST /peer/transactions', function () {
 
-	it('using valid transaction with wrong nethash in headers should fail', function (done) {
+	it('using incorrect nethash in headers should fail', function (done) {
 		var transaction = node.lisk.transaction.createTransaction('1L', 1, node.Gaccount.password);
 
-		node.peer.post('/transactions')
-			.set('Accept', 'application/json')
-			.set('version', node.version)
-			.set('nethash', 'wrongnethash')
-			.set('port', node.config.port)
-			.send({
-				transaction: transaction
-			})
-			.expect('Content-Type', /json/)
-			.expect(200)
-			.end(function (err, res) {
-				// console.log(JSON.stringify(res.body));
-				node.expect(res.body).to.have.property('success').to.be.not.ok;
-				node.expect(res.body.expected).to.equal(node.config.nethash);
-				done();
-			});
+		var validHash = node.config.nethash;
+		node.config.nethash = 'incorrect';
+
+		postTransaction(transaction, function (err, res) {
+			node.expect(res.body).to.have.property('success').to.be.not.ok;
+			node.expect(res.body.expected).to.equal(validHash);
+			node.config.nethash = validHash;
+			done();
+		});
 	});
 
-	it('using same valid transaction with correct nethash in headers should be ok', function (done) {
+	it('using correct nethash in headers should be ok', function (done) {
 		var transaction = node.lisk.transaction.createTransaction('1L', 1, node.Gaccount.password);
 
-		node.peer.post('/transactions')
-			.set('Accept', 'application/json')
-			.set('version', node.version)
-			.set('nethash', node.config.nethash)
-			.set('port', node.config.port)
-			.send({
-				transaction: transaction
-			})
-			.expect('Content-Type', /json/)
-			.expect(200)
-			.end(function (err, res) {
-				// console.log(JSON.stringify(res.body));
-				node.expect(res.body).to.have.property('success').to.be.ok;
-				done();
-			});
+		postTransaction(transaction, function (err, res) {
+			node.expect(res.body).to.have.property('success').to.be.ok;
+			done();
+		});
 	});
 
 	it('using varying recipientId casing should go to same address', function (done) {
@@ -53,45 +63,24 @@ describe('POST /peer/transactions', function () {
 		var keys = node.lisk.crypto.getKeys(account.password);
 		var address = node.lisk.crypto.getAddress(keys.publicKey);
 
-		function postTransaction (address, done) {
-			var transaction = node.lisk.transaction.createTransaction(address, 100000000, node.Gaccount.password);
+		var transaction = node.lisk.transaction.createTransaction(address, 100000000, node.Gaccount.password);
+		postTransaction(transaction, function (err, res) {
+			// console.log(JSON.stringify(res.body));
+			node.expect(res.body).to.have.property('success').to.be.ok;
+			node.onNewBlock(function (err) {
+				node.expect(err).to.be.not.ok;
 
-			node.peer.post('/transactions')
-				.set('Accept', 'application/json')
-				.set('version', node.version)
-				.set('nethash', node.config.nethash)
-				.set('port', node.config.port)
-				.send({
-					transaction: transaction
-				})
-				.expect('Content-Type', /json/)
-				.expect(200)
-				.end(function (err, res) {
-					done(err, res);
-				});
-		}
-
-		function getAddress (address, done) {
-			node.api.get('/accounts?address=' + address)
-				.set('Accept', 'application/json')
-				.expect('Content-Type', /json/)
-				.expect(200)
-				.end(function (err, res) {
+				var transaction2 = node.lisk.transaction.createTransaction(address.toLowerCase(), 100000000, node.Gaccount.password);
+				postTransaction(transaction2, function (err, res) {
 					// console.log(JSON.stringify(res.body));
 					node.expect(res.body).to.have.property('success').to.be.ok;
-					node.expect(res.body).to.have.property('account').that.is.an('object');
-					done(err, res);
-				});
-		}
+					node.onNewBlock(function (err) {
+						node.expect(err).to.be.not.ok;
 
-		postTransaction(address, function (err, res) {
-			// console.log(JSON.stringify(res.body));
-			node.onNewBlock(function () {
-				postTransaction(address.toLowerCase(), function (err, res) {
-					// console.log(JSON.stringify(res.body));
-					node.onNewBlock(function () {
 						getAddress(address, function (err, res) {
 							// console.log(JSON.stringify(res.body));
+							node.expect(res.body).to.have.property('success').to.be.ok;
+							node.expect(res.body).to.have.property('account').that.is.an('object');
 							node.expect(res.body.account).to.have.property('balance').to.eql('200000000');
 							done();
 						});
@@ -104,43 +93,21 @@ describe('POST /peer/transactions', function () {
 	it('using transaction with undefined recipientId should fail', function (done) {
 		var transaction = node.lisk.transaction.createTransaction(undefined, 1, node.Gaccount.password);
 
-		node.peer.post('/transactions')
-			.set('Accept', 'application/json')
-			.set('version', node.version)
-			.set('nethash', node.config.nethash)
-			.set('port', node.config.port)
-			.send({
-				transaction: transaction
-			})
-			.expect('Content-Type', /json/)
-			.expect(200)
-			.end(function (err, res) {
-				// console.log(JSON.stringify(res.body));
-				node.expect(res.body).to.have.property('success').to.be.not.ok;
-				node.expect(res.body).to.have.property('message');
-				done();
-			});
+		postTransaction(transaction, function (err, res) {
+			node.expect(res.body).to.have.property('success').to.be.not.ok;
+			node.expect(res.body).to.have.property('message');
+			done();
+		});
 	});
 
 	it('using transaction with negative amount should fail', function (done) {
 		var transaction = node.lisk.transaction.createTransaction('1L', -1, node.Gaccount.password);
 
-		node.peer.post('/transactions')
-			.set('Accept', 'application/json')
-			.set('version', node.version)
-			.set('nethash', node.config.nethash)
-			.set('port', node.config.port)
-			.send({
-				transaction: transaction
-			})
-			.expect('Content-Type', /json/)
-			.expect(200)
-			.end(function (err, res) {
-				// console.log(JSON.stringify(res.body));
-				node.expect(res.body).to.have.property('success').to.be.not.ok;
-				node.expect(res.body).to.have.property('message');
-				done();
-			});
+		postTransaction(transaction, function (err, res) {
+			node.expect(res.body).to.have.property('success').to.be.not.ok;
+			node.expect(res.body).to.have.property('message');
+			done();
+		});
 	});
 
 	it('using invalid passphrase should fail', function (done) {
@@ -148,43 +115,21 @@ describe('POST /peer/transactions', function () {
 		transaction.recipientId = '1L';
 		transaction.id = node.lisk.crypto.getId(transaction);
 
-		node.peer.post('/transactions')
-			.set('Accept', 'application/json')
-			.set('version', node.version)
-			.set('nethash', node.config.nethash)
-			.set('port', node.config.port)
-			.send({
-				transaction: transaction
-			})
-			.expect('Content-Type', /json/)
-			.expect(200)
-			.end(function (err, res) {
-				// console.log(JSON.stringify(res.body));
-				node.expect(res.body).to.have.property('success').to.be.not.ok;
-				node.expect(res.body).to.have.property('message');
-				done();
-			});
+		postTransaction(transaction, function (err, res) {
+			node.expect(res.body).to.have.property('success').to.be.not.ok;
+			node.expect(res.body).to.have.property('message');
+			done();
+		});
 	});
 
 	it('when sender has no funds should fail', function (done) {
 		var transaction = node.lisk.transaction.createTransaction('1L', 1, 'randomstring');
 
-		node.peer.post('/transactions')
-			.set('Accept', 'application/json')
-			.set('version', node.version)
-			.set('nethash', node.config.nethash)
-			.set('port', node.config.port)
-			.send({
-				transaction: transaction
-			})
-			.expect('Content-Type', /json/)
-			.expect(200)
-			.end(function (err, res) {
-				// console.log(JSON.stringify(res.body));
-				node.expect(res.body).to.have.property('success').to.be.not.ok;
-				node.expect(res.body).to.have.property('message');
-				done();
-			});
+		postTransaction(transaction, function (err, res) {
+			node.expect(res.body).to.have.property('success').to.be.not.ok;
+			node.expect(res.body).to.have.property('message');
+			done();
+		});
 	});
 
 	it('using fake signature should fail', function (done) {
@@ -192,22 +137,11 @@ describe('POST /peer/transactions', function () {
 		transaction.signature = crypto.randomBytes(64).toString('hex');
 		transaction.id = node.lisk.crypto.getId(transaction);
 
-		node.peer.post('/transactions')
-			.set('Accept', 'application/json')
-			.set('version', node.version)
-			.set('nethash', node.config.nethash)
-			.set('port', node.config.port)
-			.send({
-				transaction: transaction
-			})
-			.expect('Content-Type', /json/)
-			.expect(200)
-			.end(function (err, res) {
-				// console.log(JSON.stringify(res.body));
-				node.expect(res.body).to.have.property('success').to.be.not.ok;
-				node.expect(res.body).to.have.property('message');
-				done();
-			});
+		postTransaction(transaction, function (err, res) {
+			node.expect(res.body).to.have.property('success').to.be.not.ok;
+			node.expect(res.body).to.have.property('message');
+			done();
+		});
 	});
 
 	it('using invalid publicKey and signature should fail', function (done) {
@@ -215,85 +149,42 @@ describe('POST /peer/transactions', function () {
 		transaction.signature = node.randomPassword();
 		transaction.senderPublicKey = node.randomPassword();
 
-		node.peer.post('/transactions')
-			.set('Accept', 'application/json')
-			.set('version', node.version)
-			.set('nethash', node.config.nethash)
-			.set('port', node.config.port)
-			.send({
-				transaction: transaction
-			})
-			.expect('Content-Type', /json/)
-			.expect(200)
-			.end(function (err, res) {
-				// console.log(JSON.stringify(res.body));
-				node.expect(res.body).to.have.property('success').to.be.not.ok;
-				node.expect(res.body).to.have.property('message');
-				done();
-			});
+		postTransaction(transaction, function (err, res) {
+			node.expect(res.body).to.have.property('success').to.be.not.ok;
+			node.expect(res.body).to.have.property('message');
+			done();
+		});
 	});
 
 	it('using very large amount and genesis block id should fail', function (done) {
 		var transaction = node.lisk.transaction.createTransaction('12L', 10000000000000000, node.Gaccount.password);
 		transaction.blockId = genesisblock.id;
 
-		node.peer.post('/transactions')
-			.set('Accept', 'application/json')
-			.set('version', node.version)
-			.set('nethash', node.config.nethash)
-			.set('port', node.config.port)
-			.send({
-				transaction: transaction
-			})
-			.expect('Content-Type', /json/)
-			.expect(200)
-			.end(function (err, res) {
-				// console.log(JSON.stringify(res.body));
-				node.expect(res.body).to.have.property('success').to.be.not.ok;
-				done();
-			});
+		postTransaction(transaction, function (err, res) {
+			node.expect(res.body).to.have.property('success').to.be.not.ok;
+			node.expect(res.body).to.have.property('message');
+			done();
+		});
 	});
 
 	it('using overflown amount should fail', function (done) {
 		var transaction = node.lisk.transaction.createTransaction('12L', 184819291270000000012910218291201281920128129,
 		node.Gaccount.password);
 
-		node.peer.post('/transactions')
-			.set('Accept', 'application/json')
-			.set('version', node.version)
-			.set('nethash', node.config.nethash)
-			.set('port', node.config.port)
-			.send({
-				transaction: transaction
-			})
-			.expect('Content-Type', /json/)
-			.expect(200)
-			.end(function (err, res) {
-				// console.log(JSON.stringify(res.body));
-				node.expect(res.body).to.have.property('success').to.be.not.ok;
-				node.expect(res.body).to.have.property('message');
-				done();
-			});
+		postTransaction(transaction, function (err, res) {
+			node.expect(res.body).to.have.property('success').to.be.not.ok;
+			node.expect(res.body).to.have.property('message');
+			done();
+		});
 	});
 
 	it('using float amount should fail', function (done) {
 		var transaction = node.lisk.transaction.createTransaction('12L', 1.3, node.Gaccount.password);
 
-		node.peer.post('/transactions')
-			.set('Accept', 'application/json')
-			.set('version', node.version)
-			.set('nethash', node.config.nethash)
-			.set('port', node.config.port)
-			.send({
-				transaction: transaction
-			})
-			.expect('Content-Type', /json/)
-			.expect(200)
-			.end(function (err, res) {
-				// console.log(JSON.stringify(res.body));
-				node.expect(res.body).to.have.property('success').to.be.not.ok;
-				node.expect(res.body).to.have.property('message');
-				done();
-			});
+		postTransaction(transaction, function (err, res) {
+			node.expect(res.body).to.have.property('success').to.be.not.ok;
+			node.expect(res.body).to.have.property('message');
+			done();
+		});
 	});
 });
