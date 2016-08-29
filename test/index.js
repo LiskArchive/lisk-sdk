332693662f74ed1a058228d4517d1ba025cd80c7
Oliver Beddows <oliver@lisk.io>
2016-08-29 11:22:49 +0200

    Renaming file.

--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ require('./api/loader.js');
 require('./api/multisignatures.js');
 require('./api/peers.js');
 require('./api/peers.delegates.js');
-require('./api/peers.secondpassword.js');
+require('./api/peers.secondSignature.js');
 require('./api/peers.transactions.js');
 require('./api/peers.votes.js');
 require('./api/transactions.js');
