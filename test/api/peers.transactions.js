25d06e70ec18a8f70074938dfab18faf0c816da7
Oliver Beddows <oliver@lisk.io>
2016-09-01 11:40:21 +0200

    Sorting requires.

--- test/api/peers.transactions.js
+++ test/api/peers.transactions.js
@@ -1,7 +1,7 @@
 'use strict'; /*jslint mocha:true, expr:true */
 
-var node = require('./../node.js');
 var crypto = require('crypto');
+var node = require('./../node.js');
 
 var genesisblock = require('../../genesisBlock.json');
 
