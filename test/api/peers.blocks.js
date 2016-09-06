9af9972b8290f6b489d091e11eea834ba8c017fc
Oliver Beddows <oliver@lisk.io>
2016-09-06 15:42:49 +0200

    Refactoring tests using abstract API helpers.

--- test/api/peers.blocks.js
+++ test/api/peers.blocks.js
@@ -1,24 +1,17 @@
 'use strict'; /*jslint mocha:true, expr:true */
 
 var node = require('./../node.js');
+node.api = node.peer;
 
 var genesisblock = require('../../genesisBlock.json');
 
 describe('POST /peer/blocks', function () {
 
-	it('using invalid nethash in headers should fail', function (done) {
-		node.peer.post('/blocks')
-			.set('Accept', 'application/json')
-			.set('version',node.version)
-			.set('nethash', 'wrongnethash')
-			.set('port',node.config.port)
-			.send({
-				dummy: 'dummy'
-			})
-			.expect('Content-Type', /json/)
-			.expect(200)
+	it('using incorrect nethash in headers should fail', function (done) {
+		node.post('/blocks', { dummy: 'dummy' })
+			.set('nethash', 'incorrect')
 			.end(function (err, res) {
-				// console.log(JSON.stringify(res.body));
+				// node.debug('> Response:'.grey, JSON.stringify(res.body));
 				node.expect(res.body).to.have.property('success').to.be.not.ok;
 				node.expect(res.body.expected).to.equal(node.config.nethash);
 				done();
@@ -29,17 +22,11 @@ describe('POST /peer/blocks', function () {
 describe('GET /peer/blocks', function () {
 
 	it('using correct nethash in headers should be ok', function (done) {
-		node.peer.get('/blocks')
-			.set('Accept', 'application/json')
-			.set('version',node.version)
-			.set('port',node.config.port)
-			.expect('Content-Type', /json/)
-			.expect(200)
+		node.get('/blocks')
 			.end(function (err, res) {
-				// console.log(JSON.stringify(res.body.blocks));
-				node.expect(res.headers.nethash).to.equal(node.config.nethash);
-				node.expect(res.body.blocks.length).to.be.greaterThan(1);
+				// node.debug('> Response:'.grey, JSON.stringify(res.body));
+				node.expect(res.body).to.have.property('blocks').that.is.an('array');
 				done();
-			});
+		});
 	});
 });
