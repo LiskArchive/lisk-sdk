7433d26cb6690c7db0e6b89dab83eed2ef1c9ffb
Oliver Beddows <oliver@lisk.io>
2016-09-06 16:08:28 +0200

    Cleaning up params.

--- test/api/peers.js
+++ test/api/peers.js
@@ -17,9 +17,17 @@ describe('GET /peers/version', function () {
 describe('GET /peers', function () {
 
 	it('using empty parameters should fail', function (done) {
-		var state = '', os = '', shared = '', version = '', limit = '', offset = 0, orderBy = '';
+		var params = [
+			'state=',
+			'os=',
+			'shared=',
+			'version=',
+			'limit=',
+			'offset=',
+			'orderBy='
+		];
 
-		node.get('/peers?state='+state+'&os='+os+'&shared='+true+'&version='+version+'&limit='+limit+'&offset='+offset+'orderBy='+orderBy, function (err, res) {
+		node.get('/peers?' + params.join('&'), function (err, res) {
 			node.expect(res.body).to.have.property('success').to.be.not.ok;
 			node.expect(res.body).to.have.property('error');
 			done();
@@ -28,8 +36,9 @@ describe('GET /peers', function () {
 
 	it('using state should be ok', function (done) {
 		var state = 1;
+		var params = 'state=' + state;
 
-		node.get('/peers?state='+state, function (err, res) {
+		node.get('/peers?' + params, function (err, res) {
 			node.expect(res.body).to.have.property('success').to.be.ok;
 			node.expect(res.body).to.have.property('peers').that.is.an('array');
 			if (res.body.peers.length > 0) {
@@ -42,9 +51,10 @@ describe('GET /peers', function () {
 	});
 
 	it('using limit should be ok', function (done) {
-		var limit = 3, offset = 0;
+		var limit = 3;
+		var params = 'limit=' + limit;
 
-		node.get('/peers?&limit='+limit+'&offset='+offset, function (err, res) {
+		node.get('/peers?' + params, function (err, res) {
 			node.expect(res.body).to.have.property('success').to.be.ok;
 			node.expect(res.body).to.have.property('peers').that.is.an('array');
 			node.expect(res.body.peers.length).to.be.at.most(limit);
@@ -54,8 +64,9 @@ describe('GET /peers', function () {
 
 	it('using orderBy should be ok', function (done) {
 		var orderBy = 'state:desc';
+		var params = 'orderBy=' + orderBy;
 
-		node.get('/peers?orderBy='+orderBy, function (err, res) {
+		node.get('/peers?' + params, function (err, res) {
 			node.expect(res.body).to.have.property('success').to.be.ok;
 			node.expect(res.body).to.have.property('peers').that.is.an('array');
 
@@ -73,8 +84,9 @@ describe('GET /peers', function () {
 
 	it('using limit > 100 should fail', function (done) {
 		var limit = 101;
+		var params = 'limit=' + limit;
 
-		node.get('/peers?&limit='+limit, function (err, res) {
+		node.get('/peers?' + params, function (err, res) {
 			node.expect(res.body).to.have.property('success').to.be.not.ok;
 			node.expect(res.body).to.have.property('error');
 			done();
@@ -82,9 +94,17 @@ describe('GET /peers', function () {
 	});
 
 	it('using invalid parameters should fail', function (done) {
-		var state = 'invalid', os = 'invalid', shared = 'invalid', version = 'invalid', limit = 'invalid', offset = 'invalid', orderBy = 'invalid';
+		var params = [
+			'state=invalid',
+			'os=invalid',
+			'shared=invalid',
+			'version=invalid',
+			'limit=invalid',
+			'offset=invalid',
+			'orderBy=invalid'
+		];
 
-		node.get('/peers?state='+state+'&os='+os+'&shared='+shared+'&version='+version+'&limit='+limit+'&offset='+offset+'orderBy='+orderBy, function (err, res) {
+		node.get('/peers?' + params.join('&'), function (err, res) {
 			node.expect(res.body).to.have.property('success').to.be.not.ok;
 			node.expect(res.body).to.have.property('error');
 			done();
