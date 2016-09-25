c41ec7a0f40de9051f3f4eb089b6bf609c11438c
Oliver Beddows <oliver@lisk.io>
2016-09-25 22:31:12 +0200

    Fixing typo error in Blocks.prototype.lastReceipt.

--- modules/blocks.js
+++ modules/blocks.js
@@ -473,8 +473,8 @@ Blocks.prototype.lastReceipt = function (lastReceipt) {
 
 	if (__private.lastReceipt) {
 		var timeNow = new Date();
-		__private.lastReceipt.secondAgo = Math.floor((timeNow.getTime() - __private.lastReceipt.getTime()) / 1000);
-		__private.lastReceipt.stale = (__private.lastReceipt.secondAgo > 120);
+		__private.lastReceipt.secondsAgo = Math.floor((timeNow.getTime() - __private.lastReceipt.getTime()) / 1000);
+		__private.lastReceipt.stale = (__private.lastReceipt.secondsAgo > 120);
 	}
 
 	return __private.lastReceipt;
