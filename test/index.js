0f3d49867100cacff3121e982b3b703549615146
Oliver Beddows <oliver@lisk.io>
2016-09-25 23:57:40 +0200

    Sorting requires.

--- test/index.js
+++ test/index.js
@@ -4,11 +4,11 @@ require('./api/dapps.js');
 require('./api/delegates.js');
 require('./api/loader.js');
 require('./api/multisignatures.js');
-require('./api/peers.js');
 require('./api/peer.delegates.js');
 require('./api/peer.signatures.js');
 require('./api/peer.transactions.js');
 require('./api/peer.votes.js');
+require('./api/peers.js');
 require('./api/signatures.js');
 require('./api/transactions.js');
 
