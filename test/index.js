e17c3d91c1bf6f2547b3ce28daabafc64e4d498a
Oliver Beddows <oliver@lisk.io>
2016-09-25 23:09:48 +0200

    Renaming files according to endpoint.

--- test/index.js
+++ test/index.js
@@ -5,10 +5,10 @@ require('./api/delegates.js');
 require('./api/loader.js');
 require('./api/multisignatures.js');
 require('./api/peers.js');
-require('./api/peers.delegates.js');
-require('./api/peers.signatures.js');
-require('./api/peers.transactions.js');
-require('./api/peers.votes.js');
+require('./api/peer.delegates.js');
+require('./api/peer.signatures.js');
+require('./api/peer.transactions.js');
+require('./api/peer.votes.js');
 require('./api/signatures.js');
 require('./api/transactions.js');
 
