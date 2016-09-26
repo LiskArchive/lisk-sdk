f6302b80bf071b6c33cf066eb0a746250d0f88a1
Oliver Beddows <oliver@lisk.io>
2016-09-26 16:03:10 +0200

    Adding pending tests.

---
+++ test/api/peer.dapp.js
@@ -0,0 +1,11 @@
+'use strict'; /*jslint mocha:true, expr:true */
+
+var node = require('./../node.js');
+
+describe('POST /peer/dapp/message', function () {
+
+});
+
+describe('POST /peer/dapp/request', function () {
+
+});
