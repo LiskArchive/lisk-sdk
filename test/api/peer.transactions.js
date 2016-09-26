e67d9e24d91ac136c4180c66b518853fc9073ac9
Oliver Beddows <oliver@lisk.io>
2016-09-26 16:05:29 +0200

    Expanding test coverage.
    Describing GET /peer/transactions.

--- test/api/peer.transactions.js
+++ test/api/peer.transactions.js
@@ -15,6 +15,29 @@ function getAddress (address, done) {
 	node.get('/api/accounts?address=' + address, done);
 }
 
+describe('GET /peer/transactions', function () {
+
+	it('using incorrect nethash in headers should fail', function (done) {
+		node.get('/peer/transactions')
+			.set('nethash', 'incorrect')
+			.end(function (err, res) {
+				node.debug('> Response:'.grey, JSON.stringify(res.body));
+				node.expect(res.body).to.have.property('success').to.be.not.ok;
+				node.expect(res.body.expected).to.equal(node.config.nethash);
+				done();
+			});
+	});
+
+	it('using valid headers should be ok', function (done) {
+		node.get('/peer/transactions')
+			.end(function (err, res) {
+			node.expect(res.body).to.have.property('success').to.be.ok;
+			node.expect(res.body).to.have.property('transactions').to.be.an('array');
+			done();
+		});
+	});
+});
+
 describe('POST /peer/transactions', function () {
 
 	it('using incorrect nethash in headers should fail', function (done) {
