9d4aa2aaf070a5c75f0eb7339b21e0c6e55a5683
Oliver Beddows <oliver@lisk.io>
2016-10-13 17:41:28 +0200

    Removing command terminators.

--- sql/peers.js
+++ sql/peers.js
@@ -5,7 +5,7 @@ var PeersSql = {
 
   count: 'SELECT COUNT(*)::int FROM peers',
 
-  banManager: 'UPDATE peers SET "state" = 1, "clock" = null WHERE ("state" = 0 AND "clock" - ${now} < 0);',
+  banManager: 'UPDATE peers SET "state" = 1, "clock" = null WHERE ("state" = 0 AND "clock" - ${now} < 0)',
 
   getByFilter: function (params) {
     return [
@@ -24,17 +24,17 @@ var PeersSql = {
     ].filter(Boolean).join(' ');
   },
 
-  state: 'UPDATE peers SET "state" = ${state}, "clock" = ${clock} WHERE "ip" = ${ip} AND "port" = ${port};',
+  state: 'UPDATE peers SET "state" = ${state}, "clock" = ${clock} WHERE "ip" = ${ip} AND "port" = ${port}',
 
-  remove: 'DELETE FROM peers WHERE "ip" = ${ip} AND "port" = ${port};',
+  remove: 'DELETE FROM peers WHERE "ip" = ${ip} AND "port" = ${port}',
 
   getByIdPort: 'SELECT "id" FROM peers WHERE "ip" = ${ip} AND "port" = ${port}',
 
-  addDapp: 'INSERT INTO peers_dapp ("peerId", "dappid") VALUES (${peerId}, ${dappId}) ON CONFLICT DO NOTHING;',
+  addDapp: 'INSERT INTO peers_dapp ("peerId", "dappid") VALUES (${peerId}, ${dappId}) ON CONFLICT DO NOTHING',
 
   upsert: 'INSERT INTO peers ("ip", "port", "state", "os", "version") VALUES (${ip}, ${port}, ${state}, ${os}, ${version}) ON CONFLICT ("ip", "port") DO UPDATE SET ("ip", "port", "state", "os", "version") = (${ip}, ${port}, (CASE WHEN EXCLUDED."state" = 0 THEN EXCLUDED."state" ELSE ${state} END), ${os}, ${version})',
 
-  insertSeed: 'INSERT INTO peers("ip", "port", "state") VALUES(${ip}, ${port}, ${state}) ON CONFLICT DO NOTHING;',
+  insertSeed: 'INSERT INTO peers("ip", "port", "state") VALUES(${ip}, ${port}, ${state}) ON CONFLICT DO NOTHING',
 };
 
 module.exports = PeersSql;
