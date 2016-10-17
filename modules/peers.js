6b425751cba612053f9d463008f677f264a8eb16
Oliver Beddows <oliver@lisk.io>
2016-10-17 19:08:21 +0200

    Handling NaN port.

--- modules/peers.js
+++ modules/peers.js
@@ -216,6 +216,7 @@ Peers.prototype.inspect = function (peer) {
 	}
 
 	peer.port = parseInt(peer.port);
+	peer.port = isNaN(peer.port) ? 0 : peer.port;
 
 	if (peer.ip) {
 		peer.string = (peer.ip + ':' + peer.port || 'unknown');
