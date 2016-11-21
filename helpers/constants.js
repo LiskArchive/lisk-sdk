a9343c81d9478ffba60d54fbcc69bf8ba9ab53c4
Oliver Beddows <oliver@lisk.io>
2016-11-21 13:37:11 +0100

    Disabling forced forging for known nethashes.

--- helpers/constants.js
+++ helpers/constants.js
@@ -29,6 +29,12 @@ module.exports = {
 	maxTxsPerBlock: 25,
 	maxTxsPerQueue: 10000,
 	minBroadhashEfficiency: 10,
+	nethashes: [
+		// Mainnet
+		'ed14889723f24ecc54871d058d98ce91ff2f973192075c0155ba2b7b70ad2511',
+		// Testnet
+		'da3ed6a45429278bac2666961289ca17ad86595d33b31037615d4b8e8f158bba'
+	],
 	numberLength: 100000000,
 	requestLength: 104,
 	rewards: {
