b8dad253623a7b221b5ddba3e309e280bd1a2596
Oliver Beddows <oliver@lisk.io>
2016-11-23 01:40:11 +0100

    Converting space to tabs.

--- test/api/peer.signatures.js
+++ test/api/peer.signatures.js
@@ -4,110 +4,110 @@ var node = require('./../node.js');
 
 describe('GET /peer/signatures', function () {
 
-  it('using incorrect nethash in headers should fail', function (done) {
-    node.get('/peer/signatures')
-      .set('nethash', 'incorrect')
-      .end(function (err, res) {
-        node.debug('> Response:'.grey, JSON.stringify(res.body));
-        node.expect(res.body).to.have.property('success').to.be.not.ok;
-        node.expect(res.body.expected).to.equal(node.config.nethash);
-        done();
-      });
-  });
+	it('using incorrect nethash in headers should fail', function (done) {
+		node.get('/peer/signatures')
+			.set('nethash', 'incorrect')
+			.end(function (err, res) {
+				node.debug('> Response:'.grey, JSON.stringify(res.body));
+				node.expect(res.body).to.have.property('success').to.be.not.ok;
+				node.expect(res.body.expected).to.equal(node.config.nethash);
+				done();
+			});
+	});
 
-  it('using incompatible version in headers should fail', function (done) {
-    node.get('/peer/signatures')
-      .set('version', '0.1.0a')
-      .end(function (err, res) {
-        node.debug('> Response:'.grey, JSON.stringify(res.body));
-        node.expect(res.body).to.have.property('success').to.be.not.ok;
-        node.expect(res.body).to.have.property('message').to.eql('Request is made from incompatible version');
+	it('using incompatible version in headers should fail', function (done) {
+		node.get('/peer/signatures')
+			.set('version', '0.1.0a')
+			.end(function (err, res) {
+				node.debug('> Response:'.grey, JSON.stringify(res.body));
+				node.expect(res.body).to.have.property('success').to.be.not.ok;
+				node.expect(res.body).to.have.property('message').to.eql('Request is made from incompatible version');
 				node.expect(res.body).to.have.property('expected').to.eql('0.0.0a');
 				node.expect(res.body).to.have.property('received').to.eql('0.1.0a');
-        done();
-      });
-  });
+				done();
+			});
+	});
 
-  it('using valid headers should be ok', function (done) {
-    node.get('/peer/signatures')
-      .end(function (err, res) {
-        node.debug('> Response:'.grey, JSON.stringify(res.body));
-        node.expect(res.body).to.have.property('success').to.be.ok;
-        node.expect(res.body).to.have.property('signatures').that.is.an('array');
-        done();
-      });
-  });
+	it('using valid headers should be ok', function (done) {
+		node.get('/peer/signatures')
+			.end(function (err, res) {
+				node.debug('> Response:'.grey, JSON.stringify(res.body));
+				node.expect(res.body).to.have.property('success').to.be.ok;
+				node.expect(res.body).to.have.property('signatures').that.is.an('array');
+				done();
+			});
+	});
 });
 
 describe('POST /peer/signatures', function () {
 
-  var validParams;
+	var validParams;
 
-  var transaction = node.lisk.transaction.createTransaction('1L', 1, node.gAccount.password);
+	var transaction = node.lisk.transaction.createTransaction('1L', 1, node.gAccount.password);
 
-  beforeEach(function (done) {
-    validParams = {
-      signature: {
-        signature: transaction.signature,
-        transaction: transaction.id
-      }
-    };
-    done();
-  });
+	beforeEach(function (done) {
+		validParams = {
+			signature: {
+				signature: transaction.signature,
+				transaction: transaction.id
+			}
+		};
+		done();
+	});
 
-  it('using incorrect nethash in headers should fail', function (done) {
-    node.post('/peer/signatures')
-      .set('nethash', 'incorrect')
-      .end(function (err, res) {
-        node.debug('> Response:'.grey, JSON.stringify(res.body));
-        node.expect(res.body).to.have.property('success').to.be.not.ok;
-        node.expect(res.body.expected).to.equal(node.config.nethash);
-        done();
-      });
-  });
+	it('using incorrect nethash in headers should fail', function (done) {
+		node.post('/peer/signatures')
+			.set('nethash', 'incorrect')
+			.end(function (err, res) {
+				node.debug('> Response:'.grey, JSON.stringify(res.body));
+				node.expect(res.body).to.have.property('success').to.be.not.ok;
+				node.expect(res.body.expected).to.equal(node.config.nethash);
+				done();
+			});
+	});
 
-  it('using incompatible version in headers should fail', function (done) {
-    node.post('/peer/signatures')
-      .set('version', '0.1.0a')
-      .end(function (err, res) {
-        node.debug('> Response:'.grey, JSON.stringify(res.body));
-        node.expect(res.body).to.have.property('success').to.be.not.ok;
-        node.expect(res.body).to.have.property('message').to.eql('Request is made from incompatible version');
+	it('using incompatible version in headers should fail', function (done) {
+		node.post('/peer/signatures')
+			.set('version', '0.1.0a')
+			.end(function (err, res) {
+				node.debug('> Response:'.grey, JSON.stringify(res.body));
+				node.expect(res.body).to.have.property('success').to.be.not.ok;
+				node.expect(res.body).to.have.property('message').to.eql('Request is made from incompatible version');
 				node.expect(res.body).to.have.property('expected').to.eql('0.0.0a');
 				node.expect(res.body).to.have.property('received').to.eql('0.1.0a');
-        done();
-      });
-  });
+				done();
+			});
+	});
 
-  it('using invalid signature schema should fail', function (done) {
-    delete validParams.signature.transaction;
+	it('using invalid signature schema should fail', function (done) {
+		delete validParams.signature.transaction;
 
-    node.post('/peer/signatures', validParams)
-      .end(function (err, res) {
-        node.debug('> Response:'.grey, JSON.stringify(res.body));
-        node.expect(res.body).to.have.property('success').to.be.not.ok;
-        node.expect(res.body).to.have.property('message').to.equal('Signature validation failed');
-        done();
-      });
-  });
+		node.post('/peer/signatures', validParams)
+			.end(function (err, res) {
+				node.debug('> Response:'.grey, JSON.stringify(res.body));
+				node.expect(res.body).to.have.property('success').to.be.not.ok;
+				node.expect(res.body).to.have.property('message').to.equal('Signature validation failed');
+				done();
+			});
+	});
 
-  it('using unprocessable signature should fail', function (done) {
-    validParams.signature.transaction = '1';
+	it('using unprocessable signature should fail', function (done) {
+		validParams.signature.transaction = '1';
 
-    node.post('/peer/signatures', validParams)
-      .end(function (err, res) {
-        node.debug('> Response:'.grey, JSON.stringify(res.body));
-        node.expect(res.body).to.have.property('success').to.be.not.ok;
-        node.expect(res.body).to.have.property('message').to.equal('Error processing signature');
-        done();
-      });
-  });
+		node.post('/peer/signatures', validParams)
+			.end(function (err, res) {
+				node.debug('> Response:'.grey, JSON.stringify(res.body));
+				node.expect(res.body).to.have.property('success').to.be.not.ok;
+				node.expect(res.body).to.have.property('message').to.equal('Error processing signature');
+				done();
+			});
+	});
 
-  it('using processable signature should be ok');
+	it('using processable signature should be ok');
 
-  describe('using multiple signatures', function () {
-    it('with unprocessable signature should fail');
+	describe('using multiple signatures', function () {
+		it('with unprocessable signature should fail');
 
-    it('with processable signature should be ok');
-  });
+		it('with processable signature should be ok');
+	});
 });
