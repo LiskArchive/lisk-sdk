d75ca7d7efc17009363399f5ac23fda15969ac4b
Oliver Beddows <oliver@lisk.io>
2016-11-29 22:38:37 +0100

    Fixing support for multisignatures.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ lisk = {
 	crypto : require("./lib/transactions/crypto.js"),
 	dapp: require("./lib/transactions/dapp.js"),
 	delegate : require("./lib/transactions/delegate.js"),
+	multisignature : require("./lib/transactions/multisignature.js"),
 	signature : require("./lib/transactions/signature.js"),
 	transaction : require("./lib/transactions/transaction.js"),
 	vote : require("./lib/transactions/vote.js")
