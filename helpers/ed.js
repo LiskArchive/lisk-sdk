58cad963b16bbebd700cf3dfd117ab7e7432a529
Oliver Beddows <oliver@lisk.io>
2016-12-02 20:08:20 +0100

    Beautifying code.

--- helpers/ed.js
+++ helpers/ed.js
@@ -3,22 +3,21 @@
 var sodium = require('sodium').api;
 var ed = {};
 
-	ed.makeKeypair = function (hash) {
-		var keypair = sodium.crypto_sign_seed_keypair(hash);
+ed.makeKeypair = function (hash) {
+	var keypair = sodium.crypto_sign_seed_keypair(hash);
 
-		return {
-			publicKey: keypair.publicKey,
-			privateKey: keypair.secretKey
-		};
+	return {
+		publicKey: keypair.publicKey,
+		privateKey: keypair.secretKey
 	};
+};
 
-	ed.sign = function (hash, keypair) {
-		return sodium.crypto_sign_detached(hash, new Buffer(keypair.privateKey, 'hex'));
-	};
+ed.sign = function (hash, keypair) {
+	return sodium.crypto_sign_detached(hash, new Buffer(keypair.privateKey, 'hex'));
+};
 
-	ed.verify = function (hash, signatureBuffer, publicKeyBuffer) {
-		return sodium.crypto_sign_verify_detached(signatureBuffer, hash, publicKeyBuffer);
-	};
-
+ed.verify = function (hash, signatureBuffer, publicKeyBuffer) {
+	return sodium.crypto_sign_verify_detached(signatureBuffer, hash, publicKeyBuffer);
+};
 
 module.exports = ed;
