3d5c38e2a9591450aab74ad98d9d8a773872d3fc
Tobias Schwarz <tobias@Tobiass-MacBook-Pro.local>
2017-02-02 14:48:29 +0100

    Adding eslint ECMA5
    Adding ESLint and the configuration for ECMA5

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,22 @@
 module.exports = function(grunt) {
+
+    require('load-grunt-tasks')(grunt);
+
     grunt.initConfig({
+        eslint: {
+            options: {
+                configFile: 'eslint_ecma5.json',
+                reset: true
+            },
+            target: ['lib/**']
+        },
         pkg: grunt.file.readJSON('package.json'),
         browserify: {
             'app.js': ['index.js']
         }
-    })
-    grunt.loadNpmTasks('grunt-browserify')
+    });
+
+
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.registerTask("default", ["browserify", "eslint"]);
 }
