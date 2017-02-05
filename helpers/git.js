64907c785632a5908d45fd48e873607eed342355
4miners <mariusz@serek.net>
2017-02-05 05:58:33 +0100

    Retrieve git commit info only once on app start

--- helpers/git.js
+++ helpers/git.js
@@ -1,13 +1,23 @@
 'use strict';
 var childProcess = require('child_process');
 
-function getLastCommit() {
-    try {
-        return childProcess.execSync('git rev-parse HEAD').toString().trim();
-    }
-    catch (err) {
-        return '';
-    }
+/**
+ * Return hash of last git commit if available
+ *
+ * @method getLastCommit
+ * @public
+ * @return {String} Hash of last git commit
+ * @throws {Error} Throws error if cannot get last git commit
+ */
+function getLastCommit () {
+	var spawn = childProcess.spawnSync('git', ['rev-parse', 'HEAD']);
+	var err = spawn.stderr.toString().trim();
+
+	if (err) {
+		throw new Error(err);
+	} else {
+		return spawn.stdout.toString().trim();
+	}
 }
 
 module.exports = {
