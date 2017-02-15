1ec011be155ddf98743a9ac22e1eba8fa48ba0e1
Maciej Baj <macie.baj@gmail.com>
2017-02-15 23:34:41 +0100

    load all modules for unit tests, sync peers loader unit tests

--- test/unit/modules/peers.js
+++ test/unit/modules/peers.js
@@ -3,21 +3,12 @@
 var chai = require('chai');
 var express = require('express');
 var _  = require('lodash');
+var sinon = require('sinon');
 var node = require('../../node.js');
+var randomPeer = require('../../common/objectStubs').randomPeer;
 
 var modulesLoader = require('../../common/initModule').modulesLoader;
 var Peers = require('../../../modules/peers');
-
-var randomPeer = {
-	'broadhash': '198f2b61a8eb95fbeed58b8216780b68f697f26b849acf00c8c93bb9b24f783d',
-	'dappid': null,
-	'height': 1,
-	'ip': '40.40.40.40',
-	'os': 'unknown',
-	'port': 4000,
-	'state': 2,
-	'version': '0.1.2'
-};
 
 var currentPeers = [];
 
@@ -164,8 +155,6 @@ describe('peers', function () {
 				});
 			});
 		});
-
-
 	});
 
 	describe('ban', function () {
