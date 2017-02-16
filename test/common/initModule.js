c12f7eabf92ee4b6d2c4bd8befcab0c029651862
Maciej Baj <macie.baj@gmail.com>
2017-02-16 21:37:40 +0100

    add genesis block loading progress logger log applying transactions progress while loading genesis block. closes #290

--- test/common/initModule.js
+++ test/common/initModule.js
@@ -1,49 +1,96 @@
 'use strict';
 
 var express = require('express');
-var merge = require('lodash/merge');
-
 var path = require('path');
+var _ = require('lodash');
+
 var dirname = path.join(__dirname, '..', '..');
 var config = require(path.join(dirname, '/config.json'));
 var database = require(path.join(dirname, '/helpers', 'database.js'));
 var genesisblock = require(path.join(dirname, '/genesisBlock.json'));
 var Logger = require(dirname + '/logger.js');
+var z_schema = require('../../helpers/z_schema.js');
 
 var modulesLoader = new function() {
 
 	this.db = null;
 	this.logger = new Logger({ echo: null, errorLevel: config.fileLogLevel, filename: config.logFileName });
+	this.scope = {
+		config: config,
+		genesisblock: { block: genesisblock },
+		logger: this.logger,
+		network: {
+			app: express()
+		},
+		public: '../../public',
+		schema: new z_schema()
+	};
+
 	/**
+	 * Initializes Logic class with params
+	 *
+	 * @param {Function} Logic
+	 * @param {Object} scope
+	 * @param {Function} cb
+	 */
+	this.initLogic = function (Logic, scope, cb) {
+		new Logic(scope, cb);
+	};
+
+	/**
+	 * Initializes Module class with params
+	 *
 	 * @param {Function} Module
 	 * @param {Object} scope
 	 * @param {Function} cb
 	 */
-	this.init = function (Module, scope, cb) {
-		new Module(function (err, module) {
-			return cb(err, module);
-		}, merge({}, scope, {
-			network: {
-				app: express()
-			},
-			genesisblock: genesisblock
-		}));
+	this.initModule = function (Module, scope, cb) {
+		return new Module(cb, scope);
 	};
 
 	/**
+	 * Initializes Module class with basic conf
+	 *
 	 * @param {Function} Module
 	 * @param {Function} cb
+	 * @param {Object=} scope
 	 */
-	this.initWithDb = function (Module, cb) {
+	this.initModuleWithDb = function (Module, cb, scope) {
+		this.initWithDb(Module, this.initModule, cb, scope);
+	};
+
+	/**
+	 * Initializes Logic class with basic conf
+	 *
+	 * @param {Function} Logic
+	 * @param {Function} cb
+	 * @param {Object=} scope
+	 */
+	this.initLogicWithDb = function (Logic, cb, scope) {
+		this.initWithDb(Logic, this.initLogic, cb, scope);
+	};
+
+	/**
+	 * Accepts Class to invoke (Logic or Module) and fills the scope with basic conf
+	 *
+	 * @param {Function} Klass
+	 * @param {Function} moduleConstructor
+	 * @param {Function} cb
+	 * @param {Object=} scope
+	 */
+	this.initWithDb = function (Klass, moduleConstructor, cb, scope) {
 		this.getDbConnection(function (err, db) {
 			if (err) {
 				return cb(err);
 			}
-			this.init(Module, {db: db}, cb);
+
+			moduleConstructor(Klass, _.merge(this.scope, {db: db}, scope), cb);
 		}.bind(this));
 	};
 
 	/**
+	 * Starts and returns db connection
+	 *
 	 * @param {Function} cb
 	 */
 	this.getDbConnection = function (cb) {
@@ -58,7 +105,6 @@ var modulesLoader = new function() {
 			cb(null, this.db);
 		}.bind(this));
 	};
-
 };
 
 module.exports = {
