4720061452eb6905954db01656f677f6bb340b48
Oliver Beddows <oliver@lisk.io>
2017-02-28 16:01:10 +0100

    Reviewing example descriptions

--- test/unit/modules/rounds.js
+++ test/unit/modules/rounds.js
@@ -19,14 +19,14 @@ describe('rounds', function () {
 
 	describe('calc', function () {
 
-		it('should calculate round number for given block height', function () {
+		it('should calculate round number from given block height', function () {
 			node.expect(rounds.calc(100)).equal(1);
 			node.expect(rounds.calc(200)).equal(2);
 			node.expect(rounds.calc(303)).equal(3);
 			node.expect(rounds.calc(304)).equal(4);
 		});
 
-		it('should deal with great numbers', function () {
+		it('should calculate round number from Number.MAX_VALUE', function () {
 			var res = rounds.calc(Number.MAX_VALUE);
 			node.expect(_.isNumber(res)).to.be.ok;
 			node.expect(res).to.be.below(Number.MAX_VALUE);
