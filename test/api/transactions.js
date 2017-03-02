eb9ab7fc4a6d1390b00d2af92dfbd663d0bfdda1
Oliver Beddows <oliver@lisk.io>
2017-03-02 10:18:20 +0100

    Moving test into correct location

--- test/api/transactions.js
+++ test/api/transactions.js
@@ -715,30 +715,4 @@ describe('PUT /api/transactions', function () {
 			});
 		});
 	});
-
-	describe('from a genesis account', function () {
-
-		it('should be impossible to transfer the money from genesis account', function (done) {
-			var genesisSignedSendTransaction = {
-				type: 0,
-				amount: 1000,
-				senderPublicKey: 'c96dec3595ff6041c3bd28b76b8cf75dce8225173d1bd00241624ee89b50f2a8',
-				requesterPublicKey: null,
-				timestamp: 24259352,
-				asset: {},
-				recipientId: node.eAccount.address,
-				signature: 'f56a09b2f448f6371ffbe54fd9ac87b1be29fe29f27f001479e044a65e7e42fb1fa48dce6227282ad2a11145691421c4eea5d33ac7f83c6a42e1dcaa44572101',
-				id: '15307587316657110485',
-				fee: 10000000
-			};
-
-			node.post('/peer/transactions', {
-				transaction: genesisSignedSendTransaction
-			}, function (err, res) {
-				node.expect(res.body).to.have.property('success').to.be.not.ok;
-				node.expect(res.body).to.have.property('message').equals('Invalid sender. Can not send from genesis account');
-				done();
-			});
-		});
-	});
 });
