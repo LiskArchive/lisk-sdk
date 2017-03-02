dafa397ce47785813e9fc9c0cec237feaaee814f
Oliver Beddows <oliver@lisk.io>
2017-03-02 17:08:32 +0100

    Standardising code

--- index.js
+++ index.js
@@ -11,15 +11,14 @@ naclFactory.instantiate(function (nacl) {
 });
 
 lisk = {
-
-	crypto : require("./lib/transactions/crypto.js"),
-	dapp: require("./lib/transactions/dapp.js"),
-	delegate : require("./lib/transactions/delegate.js"),
-	multisignature : require("./lib/transactions/multisignature.js"),
-	signature : require("./lib/transactions/signature.js"),
-	transaction : require("./lib/transactions/transaction.js"),
-	vote : require("./lib/transactions/vote.js"),
-	api: require("./lib/api/liskApi")
-}
+	crypto : require('./lib/transactions/crypto.js'),
+	dapp: require('./lib/transactions/dapp.js'),
+	delegate : require('./lib/transactions/delegate.js'),
+	multisignature : require('./lib/transactions/multisignature.js'),
+	signature : require('./lib/transactions/signature.js'),
+	transaction : require('./lib/transactions/transaction.js'),
+	vote : require('./lib/transactions/vote.js'),
+	api: require('./lib/api/liskApi')
+};
 
 module.exports = lisk;
