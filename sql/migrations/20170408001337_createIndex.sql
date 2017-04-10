8345ccc3b020ab389878b799e3eb9b9d10fc3d8b
Oliver Beddows <oliver@lisk.io>
2017-04-10 21:27:39 +0200

    Standardising code

--- sql/migrations/20170408001337_createIndex.sql
+++ sql/migrations/20170408001337_createIndex.sql
@@ -5,6 +5,6 @@
 BEGIN;
 
 -- Add 'mem_accounts2delegates_depId' index for fast delegates voters counting
-CREATE INDEX IF NOT EXISTS "mem_accounts2delegates_depId" ON mem_accounts2delegates ("dependentId");
+CREATE INDEX IF NOT EXISTS "mem_accounts2delegates_depId" ON mem_accounts2delegates("dependentId");
 
 COMMIT;
