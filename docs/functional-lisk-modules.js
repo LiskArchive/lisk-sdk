d476e4b77e96565b8bcf29952a1c587ab70e47e4
lucasisasmendi <lucas.isasmendi@gmail.com>
2017-04-11 16:06:33 -0300

    add functional modules category

---
+++ docs/functional-lisk-modules.js
@@ -0,0 +1,66 @@
+
+/**
+ * Functional Module `accounts`
+ * 
+ * @module accounts
+ */
+
+/**
+ * Functional Module `blocks`
+ * 
+ * @module blocks
+ */
+
+/**
+ * Functional Module `dapss`
+ * 
+ * @module dapss
+ */
+
+/**
+ * Functional Module `delegates`
+ * 
+ * @module delegates
+ */
+
+/**
+ * Functional Module `loader`
+ * 
+ * @module loader
+ */
+
+/**
+ * Functional Module `multisignatures`
+ * 
+ * @module multisignatures
+ */
+
+/**
+ * Functional Module `peers`
+ * 
+ * @module peers
+ */
+
+/**
+ * Functional Module `server`
+ * 
+ * @module server
+ */
+
+/**
+ * Functional Module `signatures`
+ * 
+ * @module signatures
+ */
+
+/**
+ * Functional Module `transactions`
+ * 
+ * @module transactions
+ */
+
+/**
+ * Functional Module `transport`
+ * 
+ * @module transport
+ */
