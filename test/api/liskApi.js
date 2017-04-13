2fc439fed862802858848276dd9dc0323b33af6e
Tobias Schwarz <toschdev@gmail.com>
2017-04-13 14:06:34 +0200

    adds to list all pending multisignature transaction or single one

--- test/api/liskApi.js
+++ test/api/liskApi.js
@@ -685,4 +685,32 @@ describe('Lisk.api()', function () {
 		});
 	});
 
+	describe('#listMultisignatureTransactions', function () {
+
+		it('should list all current not signed multisignature transactions', function (done) {
+
+			lisk.api().listMultisignatureTransactions(function(result) {
+				(result).should.be.ok;
+				(result).should.be.type('object');
+				done();
+			});
+
+		});
+
+	});
+
+	describe('#getMultisignatureTransaction', function () {
+
+		it('should list all current not signed multisignature transactions', function (done) {
+
+			lisk.api().getMultisignatureTransaction('123', function(result) {
+				(result).should.be.ok;
+				(result).should.be.type('object');
+				done();
+			});
+
+		});
+
+	});
+
 });
