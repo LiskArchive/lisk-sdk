2c0d0b79367e469ac921e7122b20155da11ea8f1
lucasisasmendi <lucas.isasmendi@gmail.com>
2017-04-18 08:30:48 -0300

    module account helpers

--- helpers/exceptions.js
+++ helpers/exceptions.js
@@ -1,5 +1,15 @@
 'use strict';
-
+/**
+ * @namespace exceptions
+ * @memberof module:helpers
+ * @property {object} genesisPublicKey
+ * @property {string} genesisPublicKey.mainnet
+ * @property {string} genesisPublicKey.testnet
+ * @property {Strin[]} senderPublicKey
+ * @property {Strin[]} signatures
+ * @property {Strin[]} multisignatures
+ * @property {Strin[]} votes
+ */	
 module.exports = {
 	genesisPublicKey: {
 		mainnet: 'd121d3abf5425fdc0f161d9ddb32f89b7750b4bdb0bff7d18b191d4b4bafa6d4',
