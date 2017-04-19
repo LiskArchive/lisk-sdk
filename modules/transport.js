0b84cbc43f343d0ddfb4351bcecf85a89ea36b8a
Maciej Baj <macie.baj@gmail.com>
2017-04-19 17:47:49 +0200

    revert handshake in transport for http compatibility

--- modules/transport.js
+++ modules/transport.js
@@ -753,6 +753,56 @@ Transport.prototype.internal = {
 		});
 	},
 
+	handshake: function (ip, port, headers, validateHeaders, cb) {
+		var peer = library.logic.peers.create(
+			{
+				ip: ip,
+				port: port
+			}
+		);
+
+		var headers = peer.applyHeaders(headers);
+
+		validateHeaders(headers, function (error, extraMessage) {
+			if (error) {
+				// Remove peer
+				__private.removePeer({peer: peer, code: 'EHEADERS'}, extraMessage);
+
+				return setImmediate(cb, {success: false, error: error});
+			}
+
+			if (!modules.system.networkCompatible(headers.nethash)) {
+				// Remove peer
+				__private.removePeer({peer: peer, code: 'ENETHASH'}, extraMessage);
+
+				return setImmediate(cb, {
+					success: false,
+					message: 'Request is made on the wrong network',
+					expected: modules.system.getNethash(),
+					received: headers.nethash
+				});
+			}
+
+			if (!modules.system.versionCompatible(headers.version)) {
+				// Remove peer
+				__private.removePeer({
+					peer: peer,
+					code: 'EVERSION:' + headers.version
+				}, extraMessage);
+
+				return setImmediate(cb, {
+					success: false,
+					message: 'Request is made from incompatible version',
+					expected: modules.system.getMinVersion(),
+					received: headers.version
+				});
+			}
+
+			modules.peers.update(peer);
+
+			return setImmediate(cb, null, peer);
+		});
+	},
 
 	/**
 	 * @param {Peer} peer
