d5d7bbfaa2a13ccf83346bcd9d3d2bffe45d4b93
Maciej Baj <macie.baj@gmail.com>
2017-04-19 17:49:28 +0200

    add rpc client in every peer instance

--- api/ws/transport.js
+++ api/ws/transport.js
@@ -5,23 +5,26 @@ var workersController = require('./workersController');
 
 function TransportWSApi (transportModule, app, logger) {
 
-	console.log('\x1b[36m%s\x1b[0m', 'TransportWSApi ----- invoke registerWorkerReceiver', app.rpc.server.registerRPCEndpoints);
-
-	app.rpc.server.registerRPCEndpoints({
+	this.rpcEndpoints = {
 		dupaRpc: function (random, cb) {
 			console.log('\x1b[31m%s\x1b[0m', 'TRANSPORT API: dupaRPC invoked', 2 * random);
 			return cb(null, 'dupa rpc ' + 2 * random);
 		},
 		acceptPeer: transportModule.internal.acceptPeer,
-		removePeer: transportModule.internal.removePeer
-	});
+		removePeer: transportModule.internal.removePeer,
+		ping: transportModule.internal.ping
+	};
 
-	app.rpc.server.registerEventEndpoints({
+	this.eventEndpoints = {
 		dupaEmit: function (random) {
 			console.log('\x1b[31m%s\x1b[0m', 'TRANSPORT API: dupaEmit invoked', 2 * random);
 		}
-	});
+	};
 
+	console.log('\x1b[36m%s\x1b[0m', 'TransportWSApi ----- invoke registerWorkerReceiver', app.rpc.server.registerRPCEndpoints);
+
+	app.rpc.server.registerRPCEndpoints(this.rpcEndpoints);
+	app.rpc.server.registerEventEndpoints(this.eventEndpoints);
 }
 
 module.exports = TransportWSApi;
