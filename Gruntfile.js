cd02ac61a621e0a9623856d2c086f13dd7abb3f7
Tobias Schwarz <toschdev@gmail.com>
2017-05-08 16:38:11 +0200

    Adds grunt and grunt-mocha-tests

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+	// Add the grunt-mocha-test tasks.
+	grunt.loadNpmTasks('grunt-mocha-test');
+
+	grunt.initConfig({
+		// Configure a mochaTest task
+		mochaTest: {
+			test: {
+				options: {
+					reporter: 'spec',
+					quiet: false, // Optionally suppress output to standard out (defaults to false)
+					clearRequireCache: false, // Optionally clear the require cache before running tests (defaults to false)
+					noFail: false // Optionally set to not fail on failed tests (will still fail on other errors)
+				},
+				src: ['test/**/*.js']
+			}
+		}
+	});
+
+	grunt.registerTask('default', 'mochaTest');
+
+};
