78fa17785f30b31ca8cbb0549dca754f328584a9
Tobias Schwarz <toschdev@gmail.com>
2017-05-31 16:23:05 +0200

    Adds description to commands and fixes json vs table output

--- commands/get.js
+++ commands/get.js
@@ -48,7 +48,7 @@ module.exports = function getCommand (vorpal) {
 		.command('get <type> <input>')
 		.option('-j, --json', 'Sets output to json')
 		.option('--no-json', 'Default: sets output to text. You can change this in the config.js')
-		.description('Get information from <type> with parameter <input>')
+		.description('Get information from <type> with parameter <input>. \n Types available: account, adddress, block, delegate, transaction \n E.g. get delegate lightcurve \n e.g. get block 5510510593472232540')
 		.autocomplete(['account', 'address', 'block', 'delegate', 'transaction'])
 		.action(function(userInput) {
 
@@ -70,7 +70,7 @@ module.exports = function getCommand (vorpal) {
 
 			} else {
 
-				if(userInput.options.json === true) {
+				if( (userInput.options.json === true || config.json === true) && userInput.options.json !== false) {
 					return output.then((result) => {
 						if(result.error) {
 							vorpal.log(util.inspect(result));
