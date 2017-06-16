9a4dc170d66f377f32a7182befd7267d60afa1ad
Tobias Schwarz <toschdev@gmail.com>
2017-06-16 14:18:29 +0200

    Fixes vorpal version. Modifies to from config.js to config.json

--- commands/get.js
+++ commands/get.js
@@ -44,7 +44,7 @@ module.exports = function getCommand (vorpal) {
 		.command('get <type> <input>')
 		.option('-j, --json', 'Sets output to json')
 		.option('--no-json', 'Default: sets output to text. You can change this in the config.js')
-		.description('Get information from <type> with parameter <input>. \n Types available: account, adddress, block, delegate, transaction \n E.g. get delegate lightcurve \n e.g. get block 5510510593472232540')
+		.description('Get information from <type> with parameter <input>. \n Types available: account, address, block, delegate, transaction \n E.g. get delegate lightcurve \n e.g. get block 5510510593472232540')
 		.autocomplete(['account', 'address', 'block', 'delegate', 'transaction'])
 		.action(function(userInput) {
 
@@ -57,6 +57,8 @@ module.exports = function getCommand (vorpal) {
 			};
 
 			let output = getType[userInput.type](userInput.input);
+
+			console.log(userInput);
 
 			if(process.env.NODE_ENV === 'test') {
 
