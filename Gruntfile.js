9ac65c12a8349f79147c65dabeb8ddf4a66cd417
Oliver Beddows <oliver@lightcurve.io>
2017-06-28 18:12:55 +0200

    Fixing indent

--- Gruntfile.js
+++ Gruntfile.js
@@ -130,7 +130,7 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-compress');
 	grunt.loadNpmTasks('grunt-eslint');
 
-    grunt.registerTask('default', ['release']);
+	grunt.registerTask('default', ['release']);
 	grunt.registerTask('release', ['exec:folder', 'obfuscator', 'exec:package', 'exec:build', 'compress']);
 	grunt.registerTask('jenkins', ['exec:coverageSingle']);
 	grunt.registerTask('eslint-nofix', ['eslint']);
