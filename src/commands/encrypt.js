1c3972988d0cb4338176072b33417e8a196956c8
willclarktech <willclarktech@users.noreply.github.com>
2017-07-06 17:09:40 +0200

    Add placeholder encrypt command action

--- src/commands/encrypt.js
+++ src/commands/encrypt.js
@@ -1,6 +1,13 @@
+const cryptoModule = require('../utils/cryptoModule');
+
 module.exports = function encryptCommand (vorpal) {
 	'use strict';
 
+	function encrypt (userInput) {
+		return cryptoModule.encrypt();
+	}
+
 	vorpal
-		.command('encrypt <message> <secret> <recipient>');
+		.command('encrypt <message> <secret> <recipient>')
+		.action(encrypt);
 };
