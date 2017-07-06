7a3c7fba78f206ee3fac3487bbb78bcf106e3ff0
willclarktech <willclarktech@users.noreply.github.com>
2017-07-06 17:28:50 +0200

    Add description to encrypt command

--- src/commands/encrypt.js
+++ src/commands/encrypt.js
@@ -9,5 +9,6 @@ module.exports = function encryptCommand (vorpal) {
 
 	vorpal
 		.command('encrypt <message> <secret> <recipient>')
+		.description('Encrypt a message for a given recipient public key using your secret key. \n E.g. encrypt "Hello world" "my passphrase" bba7e2e6a4639c431b68e31115a71ffefcb4e025a4d1656405dfdcd8384719e0')
 		.action(encrypt);
 };
