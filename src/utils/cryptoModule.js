7aae20a462dff23ffd4971963ef9b352c8b8ca5b
willclarktech <willclarktech@users.noreply.github.com>
2017-07-06 17:43:00 +0200

    Add functionality for Crypto class encrypt method

--- src/utils/cryptoModule.js
+++ src/utils/cryptoModule.js
@@ -1,7 +1,10 @@
 'use strict';
+const lisk = require('lisk-js');
 
 class Crypto {
-	encrypt () {}
+	encrypt (message, secret, recipient) {
+		return lisk.crypto.encryptMessageWithSecret(message, secret, recipient);
+	}
 }
 
 module.exports = new Crypto();
