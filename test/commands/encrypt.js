9a265e6ff6bd61c799548c7a0e0c4656611623b2
willclarktech <willclarktech@users.noreply.github.com>
2017-07-06 16:37:15 +0200

    Add valid parameters unit test for encrypt command

--- test/commands/encrypt.js
+++ test/commands/encrypt.js
@@ -1,5 +1,6 @@
 const Vorpal = require('vorpal');
 const encrypt = require('../../src/commands/encrypt');
+const cryptoModule = require('../../src/utils/cryptoModule');
 
 describe('lisky encrypt command palette', () => {
 	let vorpal;
@@ -26,6 +27,21 @@ describe('lisky encrypt command palette', () => {
 		const encryptCommand = vorpal.commands.filter(commandFilter)[0];
 		const requiredArgs = encryptCommand._args.filter(argsFilter);
 		(requiredArgs).should.have.length(3);
+	});
+
+	it('should handle valid parameters', () => {
+		sinon.stub(cryptoModule, 'encrypt');
+
+		const message = 'Hello Lisker';
+		const secret = 'pass phrase';
+		const recipient = 'bba7e2e6a4639c431b68e31115a71ffefcb4e025a4d1656405dfdcd8384719e0';
+		const command = `encrypt "${ message }" "${ secret }" "${ recipient }"`;
+		vorpal.execSync(command);
+
+		(cryptoModule.encrypt.calledWith(message, secret, recipient))
+			.should.be.true();
+
+		cryptoModule.encrypt.restore();
 	});
 
 });
