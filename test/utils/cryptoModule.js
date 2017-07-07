c10bb4a9f5b5cd578d3f9baa8bb45675dd7f4353
willclarktech <willclarktech@users.noreply.github.com>
2017-07-07 14:13:25 +0200

    Add test for handling errors to crypto module encrypt function

--- test/utils/cryptoModule.js
+++ test/utils/cryptoModule.js
@@ -2,19 +2,34 @@ const lisk = require('lisk-js');
 const cryptoModule = require('../../src/utils/cryptoModule');
 
 describe('cryptoModule class', () => {
+	let stub;
+	const message = 'Hello Lisker';
+	const secret = 'pass phrase';
+	const recipient = 'bba7e2e6a4639c431b68e31115a71ffefcb4e025a4d1656405dfdcd8384719e0';
+
+	beforeEach(() => {
+		stub = sinon.stub(lisk.crypto, 'encryptMessageWithSecret');
+	});
+
+	afterEach(() => {
+		lisk.crypto.encryptMessageWithSecret.restore();
+	});
 
 	it('should use lisk-js encryptMessageWithSecret', () => {
-		sinon.stub(lisk.crypto, 'encryptMessageWithSecret');
-
-		const message = 'Hello Lisker';
-		const secret = 'pass phrase';
-		const recipient = 'bba7e2e6a4639c431b68e31115a71ffefcb4e025a4d1656405dfdcd8384719e0';
 		cryptoModule.encrypt(message, secret, recipient);
 
 		(lisk.crypto.encryptMessageWithSecret.calledWith(message, secret, recipient))
 			.should.be.true();
+	});
 
-		lisk.crypto.encryptMessageWithSecret.restore();
+	it('should handle error responses', () => {
+		const message = 'Cannot read property \'length\' of null';
+		const error = new TypeError(message);
+		stub.throws(error);
+
+		const result = cryptoModule.encrypt(message, secret, recipient);
+
+		(result).should.have.property('error', message);
 	});
 
 });
