016c2f7019536660247761bf8493899f692e5a70
willclarktech <willclarktech@users.noreply.github.com>
2017-07-07 15:09:18 +0200

    Add description of --no-json option to encrypt command

--- src/commands/encrypt.js
+++ src/commands/encrypt.js
@@ -16,6 +16,7 @@ module.exports = function encryptCommand (vorpal) {
 	vorpal
 		.command('encrypt <message> <secret> <recipient>')
 		.option('-j, --json', 'Sets output to json')
+		.option('--no-json', 'Default: sets output to text. You can change this in the config.json')
 		.description('Encrypt a message for a given recipient public key using your secret key. \n E.g. encrypt "Hello world" "my passphrase" bba7e2e6a4639c431b68e31115a71ffefcb4e025a4d1656405dfdcd8384719e0')
 		.action(encrypt);
 };
