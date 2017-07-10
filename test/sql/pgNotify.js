df30d35b5b7e9aa4c5385fb0f31c64ea4949f720
Mariusz Serek <mariusz@serek.net>
2017-07-11 00:46:18 +0200

    Add triggerNotify SQL query for pg-notify tests

--- test/sql/pgNotify.js
+++ test/sql/pgNotify.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var pgNotify = {
-	interruptConnection: 'SELECT pg_terminate_backend(${pid});'
+	interruptConnection: 'SELECT pg_terminate_backend(${pid});',
+	triggerNotify: 'SELECT pg_notify(${channel}, ${message});'
 };
 
 module.exports = pgNotify;
