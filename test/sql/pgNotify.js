6ac28a5d3c38c77f8f249eee57bfd7f44124629c
Mariusz Serek <mariusz@serek.net>
2017-07-14 20:50:11 +0200

    Reorganise pg-notify tests to prevent interrupt on working node

--- test/sql/pgNotify.js
+++ test/sql/pgNotify.js
@@ -2,8 +2,10 @@
 
 var pgNotify = {
 	interruptConnection: 'SELECT pg_terminate_backend(${pid});',
-	triggerNotify: 'SELECT pg_notify(${channel}, json_build_object(\'round\', CEIL((SELECT height FROM blocks ORDER BY height DESC LIMIT 1) / 101::float)::int, \'list\', generateDelegatesList(CEIL((SELECT height FROM blocks ORDER BY height DESC LIMIT 1) / 101::float)::int, ARRAY(SELECT ENCODE(pk, \'hex\') AS pk FROM delegates ORDER BY rank ASC LIMIT 101)))::text);',
-	triggerNotifyWithMessage: 'SELECT pg_notify(${channel}, ${message});'
+	triggerNotify: 'SELECT pg_notify(${channel}, json_build_object(\'round\', CEIL((SELECT height+1 FROM blocks ORDER BY height DESC LIMIT 1) / 101::float)::int, \'list\', getDelegatesList())::text);',
+	triggerNotifyWithMessage: 'SELECT pg_notify(${channel}, ${message});',
+	getDelegatesList: 'SELECT getDelegatesList() AS list;',
+	getRound: 'SELECT CEIL((SELECT height+1 FROM blocks ORDER BY height DESC LIMIT 1) / 101::float)::int AS round;'
 };
 
 module.exports = pgNotify;
