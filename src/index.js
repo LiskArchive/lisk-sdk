327a57b0136f13d6632b6342a1e3f95bf348f8a1
willclarktech <willclarktech@users.noreply.github.com>
2017-07-14 12:25:38 +0200

    Fix index.js for transpiling

--- src/index.js
+++ src/index.js
@@ -3,27 +3,28 @@
  * @module lisk
  * @main lisk
  */
+import naclFactory from 'js-nacl';
 
 global.Buffer = global.Buffer || require('buffer').Buffer;
-global.naclFactory = require('js-nacl');
+global.naclFactory = naclFactory;
 
-global.naclInstance;
+global.naclInstance = null;
 naclFactory.instantiate(function (nacl) {
 	naclInstance = nacl;
 });
 
-lisk = {
-	crypto : require('./lib/transactions/crypto.js'),
-	dapp: require('./lib/transactions/dapp.js'),
-	delegate : require('./lib/transactions/delegate.js'),
-	multisignature : require('./lib/transactions/multisignature.js'),
-	signature : require('./lib/transactions/signature.js'),
-	transaction : require('./lib/transactions/transaction.js'),
-	transfer: require('./lib/transactions/transfer'),
-	vote : require('./lib/transactions/vote.js'),
-	api: require('./lib/api/liskApi'),
-	slots: require('./lib/time/slots'),
-	mnemonic: require('./lib/utils/mnemonic.js'),
+var lisk = {
+	crypto : require('./transactions/crypto.js'),
+	dapp: require('./transactions/dapp.js'),
+	delegate : require('./transactions/delegate.js'),
+	multisignature : require('./transactions/multisignature.js'),
+	signature : require('./transactions/signature.js'),
+	transaction : require('./transactions/transaction.js'),
+	transfer: require('./transactions/transfer'),
+	vote : require('./transactions/vote.js'),
+	api: require('./api/liskApi'),
+	slots: require('./time/slots'),
+	mnemonic: require('./utils/mnemonic.js'),
 };
 
 module.exports = lisk;
