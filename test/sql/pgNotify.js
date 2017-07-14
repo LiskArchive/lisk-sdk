6c3086be5d0d67dfe86d3c4f58640e8c209e04c5
Mariusz Serek <mariusz@serek.net>
2017-07-14 17:31:07 +0200

    Changes expectations for pg-notify unit test, add test cases

--- test/sql/pgNotify.js
+++ test/sql/pgNotify.js
@@ -2,7 +2,8 @@
 
 var pgNotify = {
 	interruptConnection: 'SELECT pg_terminate_backend(${pid});',
-	triggerNotify: 'SELECT pg_notify(${channel}, ${message});'
+	triggerNotify: 'SELECT pg_notify(${channel}, json_build_object(\'round\', CEIL((SELECT height FROM blocks ORDER BY height DESC LIMIT 1) / 101::float)::int, \'list\', generateDelegatesList(CEIL((SELECT height FROM blocks ORDER BY height DESC LIMIT 1) / 101::float)::int, ARRAY(SELECT ENCODE(pk, \'hex\') AS pk FROM delegates ORDER BY rank ASC LIMIT 101)))::text);',
+	triggerNotifyWithMessage: 'SELECT pg_notify(${channel}, ${message});'
 };
 
 module.exports = pgNotify;
