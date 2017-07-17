f4b23c5cd4a3b5c323f82eec365fa49158b0c260
lucasisasmendi <lucas.isasmendi@gmail.com>
2017-07-17 00:18:27 -0300

    Adjust test

--- test/unit/modules/blocks/verify.js
+++ test/unit/modules/blocks/verify.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var expect = require('chai').expect;
-
-var express = require('express');
-var sinon = require('sinon');
+var async = require('async');
 
 var modulesLoader = require('../../../common/initModule').modulesLoader;
 var BlockLogic = require('../../../../logic/block.js');
 var exceptions = require('../../../../helpers/exceptions.js');
+var clearDatabaseTable = require('../../../common/globalBefore').clearDatabaseTable;
+
 var crypto = require('crypto');
 var BSON = require('bson');
 
@@ -125,34 +125,6 @@ var blockRewardInvalid = {
 	id: '15635779876149546284'
 };
 
-var validBlockReward1 = {
-	blockSignature: 'b626ff71d01ec0ce1f700253f11ea708c6d505e8613f94d42f4e21572d062ce8732169b14c515c201bbc0d21cf57329ea8ffc65608b263f8d54a985af8bb2e09',
-	generatorPublicKey: 'c76a0e680e83f47cf07c0f46b410f3b97e424171057a0f8f0f420c613da2f7b5',
-	numberOfTransactions: 1,
-	payloadHash: '215020db61a29a640397e39a2766fc467f7d12c95e814a5f6b150d562e6088ad',
-	payloadLength: 117,
-	previousBlock: '6524861224470851795',
-	reward: 999,
-	timestamp: 32578370,
-	totalAmount: 10000000000000000,
-	totalFee: 10000000,
-	transactions: [
-		{
-			'type': 0,
-			'amount': 10000000000000000,
-			'fee': 10000000,
-			'timestamp': 33514086,
-			'recipientId': '16313739661670634666L',
-			'senderId': '2737453412992791987L',
-			'senderPublicKey': 'c76a0e680e83f47cf07c0f46b410f3b97e424171057a0f8f0f420c613da2f7b5',
-			'signature': '57bc34c092189e6520b1fcb5b8a1e911d5aed56910ae75d8bbf6145b780dce539949ba86a0ae8d6a33b3a2a68ce8c16eb39b448b4e53f5ca8b04a0da3b438907',
-			'id': '7249285091378090017'
-		}
-	],
-	version: 0,
-	id: '2783858589203451895'
-};
-
 var testAccount = {
 	account: {
 		username: 'test_verify',
@@ -191,8 +163,8 @@ var previousBlock1 = {
 	transactions: [],
 	version:0,
 };
-var validBlock1;
-var transactionsValidBlock1 =	[
+var block1;
+var transactionsBlock1 =	[
 	{
 		'type': 0,
 		'amount': 10000000000000000,
@@ -206,8 +178,8 @@ var transactionsValidBlock1 =	[
 	}
 ];
 
-var validBlock2;
-var transactionsValidBlock2 = [
+var block2;
+var transactionsBlock2 = [
 	{
 		'type': 0,
 		'amount': 100000000,
@@ -221,38 +193,40 @@ var transactionsValidBlock2 = [
 	}
 ];
 
-var validBlock3;
-var transactionsValidBlock3 = [
-	{
-		'type': 0,
-		'amount': 100000000,
-		'fee': 10000000,
-		'timestamp': 33942637,
-		'recipientId': '2896019180726908125L',
-		'senderId': '2737453412992791987L',
-		'senderPublicKey': 'c76a0e680e83f47cf07c0f46b410f3b97e424171057a0f8f0f420c613da2f7b5',
-		'signature': '2e2fb92c17716f2e239148fc990cb712b76639301c945050b621d7568c781f1ad49991b173fff9e7fc8348818ac606d4069cf78ce2b873d86f48da37a4bf5f07',
-		'id': '5602023031121962294'
-	}
-];
+var block3;
 
 function createBlock (blocksModule, blockLogic, secret, timestamp, transactions, previousBlock) {
 	var keypair = blockLogic.scope.ed.makeKeypair(crypto.createHash('sha256').update(secret, 'utf8').digest());
 	blocksModule.lastBlock.set(previousBlock);
-	var newBLock = blockLogic.create({
+	var newBlock = blockLogic.create({
 		keypair: keypair,
 		timestamp: timestamp,
 		previousBlock: blocksModule.lastBlock.get(),
 		transactions: transactions
 	});
-	newBLock.id = blockLogic.getId(newBLock);
-	return newBLock;
+	//newBlock.id = blockLogic.getId(newBlock);
+	return newBlock;
 }
 
 function deleteBlockProperties (block) {
 	// see modules/blocks/verify.js for deleted fields reference.
-	delete block.version;
-	delete block.numberOfTransactions;
+	if (block.version === 0) {
+		delete block.version;
+	}
+	// verifyBlock ensures numberOfTransactions is transactions.length
+	if (block.numberOfTransactions === block.transactions.length) {
+		delete block.numberOfTransactions;
+	}
+	if (block.totalAmount === 0) {
+		delete block.totalAmount;
+	}
+	if (block.totalFee === 0) {
+		delete block.totalFee;
+	}
+	if (block.payloadLength === 0) {
+		delete block.payloadLength;
+	}
+	delete block.id;
 	return;
 }
 
@@ -577,6 +551,23 @@ describe('blocks/verify', function () {
 	// Sends a block to network, save it locally.
 	describe('processBlock() for valid block {broadcast: true, saveBlock: true}', function () {
 		
+		it('should clear database', function (done) {
+			async.every([
+				'blocks where height > 1',
+				'trs where "blockId" != \'6524861224470851795\'',
+				'mem_accounts where address in (\'2737453412992791987L\', \'2896019180726908125L\')',
+				'forks_stat',
+				'votes where "transactionId" = \'17502993173215211070\''
+			], function (table, seriesCb) {
+				clearDatabaseTable(modulesLoader.db, modulesLoader.logger, table, seriesCb);
+			}, function (err, result) {
+				if (err) {
+					return done(err);
+				}
+				return done();
+			});
+		});
+
 		it('should generate new account', function (done) {
 			accounts.setAccountAndGet(testAccount.account, function (err, newaccount) {
 				if (err) {
@@ -587,17 +578,25 @@ describe('blocks/verify', function () {
 			});
 		});
 
-		it('should create a block 1', function (done) {
+		it('should create block 1', function (done) {
 			var secret = 'famous weapon poverty blast announce observe discover prosper mystery adapt tuna office';
 			
-			validBlock1 = createBlock(blocks, blockLogic, secret, 32578370, transactionsValidBlock1, previousBlock1);
-			expect(validBlock1.version).to.equal(0);
+			block1 = createBlock(blocks, blockLogic, secret, 32578370, transactionsBlock1, previousBlock1);
+			expect(block1.version).to.equal(0);
+			expect(block1.timestamp).to.equal(32578370);
+			expect(block1.numberOfTransactions).to.equal(1);
+			expect(block1.reward).to.equal(0);
+			expect(block1.totalFee).to.equal(10000000);
+			expect(block1.totalAmount).to.equal(10000000000000000);
+			expect(block1.payloadLength).to.equal(117);
+			expect(block1.transactions).to.deep.equal(transactionsBlock1);
+			expect(block1.previousBlock).to.equal(previousBlock1.id);
 			done();
 		});
 
-		it('should processBlock ok', function (done) {
+		it('should process block 1', function (done) {
 
-			blocksVerify.processBlock(validBlock1, true, function (err, result) {
+			blocksVerify.processBlock(block1, true, function (err, result) {
 				if (err) {
 					return done(err);
 				}
@@ -607,10 +606,10 @@ describe('blocks/verify', function () {
 				onMessage[1] = bson.deserialize(onMessage[1]);
 				expect(onMessage[1].version).to.be.undefined;
 				expect(onMessage[1].numberOfTransactions).to.be.undefined;
-				expect(onMessage[1].id).to.equal(validBlock1.id);
+				expect(onMessage[1].id).to.be.undefined;
 				expect(onMessage[2]).to.be.true;
 				expect(onMessage[3]).to.equal('transactionsSaved');
-				expect(onMessage[4][0].id).to.equal(validBlock1.transactions[0].id);
+				expect(onMessage[4]).to.deep.equal(block1.transactions);
 				modulesLoader.scope.bus.clearMessages();
 				done();
 			}, true);
@@ -622,8 +621,8 @@ describe('blocks/verify', function () {
 		it('process same block again should fail (checkExists)', function (done) {
 			blocks.lastBlock.set(previousBlock1);
 
-			blocksVerify.processBlock(validBlock1, true, function (err, result) {
-				expect(err).to.equal(['Block', validBlock1.id, 'already exists'].join(' '));
+			blocksVerify.processBlock(block1, true, function (err, result) {
+				expect(err).to.equal(['Block', block1.id, 'already exists'].join(' '));
 				done();
 				modulesLoader.scope.bus.clearMessages();
 			}, true);
@@ -635,88 +634,105 @@ describe('blocks/verify', function () {
 		
 		var invalidBlock2;
 
-		it('should generate valid block2', function (done) {
+		it('should generate block 2 with invalid generator slot', function (done) {
 			var secret = 'flip relief play educate address plastic doctor fix must frown oppose segment';
-			validBlock2 = createBlock(blocks, blockLogic, secret, 33772862, transactionsValidBlock2, validBlock1);
-			expect(validBlock2.version).to.equal(0);
-			deleteBlockProperties(validBlock2);
-
+			block2 = createBlock(blocks, blockLogic, secret, 33772882, transactionsBlock2, block1);
+			expect(block2.version).to.equal(0);
+			expect(block2.timestamp).to.equal(33772882);
+			expect(block2.numberOfTransactions).to.equal(1);
+			expect(block2.reward).to.equal(0);
+			expect(block2.totalFee).to.equal(10000000);
+			expect(block2.totalAmount).to.equal(100000000);
+			expect(block2.payloadLength).to.equal(117);
+			expect(block2.transactions).to.deep.equal(transactionsBlock2);
+			expect(block2.previousBlock).to.equal(block1.id);
 			done();
 		});
 	
-		it('normalizeBlock should fail (block schema: timestamp)', function (done) {
-			invalidBlock2 = JSON.parse(JSON.stringify(validBlock2));
-			delete invalidBlock2.timestamp;
+		it('normalizeBlock should fail when deleted timestamp', function (done) {
+			deleteBlockProperties(block2);
+			var timestamp = block2.timestamp;
+			delete block2.timestamp;
 
-			blocksVerify.processBlock(invalidBlock2, false, function (err, result) {
+			blocksVerify.processBlock(block2, false, function (err, result) {
 				if (err) {
 					expect(err).equal('Failed to validate block schema: Missing required property: timestamp');
+					block2.timestamp = timestamp;
 					done();
 				}
 			}, true);
 		});
 
-		it('normalizeBlock should fail (block schema: transactions)', function (done) {
-			invalidBlock2.timestamp = validBlock2.timestamp;
-			delete invalidBlock2.transactions;
-			deleteBlockProperties(invalidBlock2);
+		it('normalizeBlock should fail when deleted transactions', function (done) {
+			var transactions = block2.transactions;
+			delete block2.transactions;
 
-			blocksVerify.processBlock(invalidBlock2, false, function (err, result) {
+			blocksVerify.processBlock(block2, false, function (err, result) {
 				if (err) {
 					expect(err).equal('Failed to validate block schema: Missing required property: transactions');
+					block2.transactions = transactions;
 					done();
 				}
 			}, true);
 		});
 
-		it('normalizeBlock should fail (transaction schema: type)', function (done) {
-			invalidBlock2.transactions = JSON.parse(JSON.stringify(validBlock2.transactions));
-			delete invalidBlock2.transactions[0].type;
-			deleteBlockProperties(invalidBlock2);
+		it('normalizeBlock should fail when deleted transaction type', function (done) {
+			var trsType = block2.transactions[0].type;
+			delete block2.transactions[0].type;
 
-			blocksVerify.processBlock(invalidBlock2, false, function (err, result) {
+			blocksVerify.processBlock(block2, false, function (err, result) {
 				if (err) {
 					expect(err).equal('Unknown transaction type undefined');
+					block2.transactions[0].type = trsType;
 					done();
 				}
 			}, true);
 		});
 
-		it('normalizeBlock should fail (transaction schema: timestamp)', function (done) {
-			invalidBlock2.transactions[0].type = validBlock2.transactions[0].type;
-			delete invalidBlock2.transactions[0].timestamp;
-			deleteBlockProperties(invalidBlock2);
+		it('normalizeBlock should fail when deleted transaction timestamp', function (done) {
+			var trsTimestamp = block2.transactions[0].timestamp;
+			delete block2.transactions[0].timestamp;
 
-			blocksVerify.processBlock(invalidBlock2, false, function (err, result) {
+			blocksVerify.processBlock(block2, false, function (err, result) {
 				if (err) {
 					expect(err).equal('Failed to validate transaction schema: Missing required property: timestamp');
+					block2.transactions[0].timestamp = trsTimestamp;
 					done();
 				}
 			}, true);
 		});
 
-		it('validateBlockSlot should fail (fork: 3)', function (done) {
-			invalidBlock2.transactions[0].timestamp = validBlock2.transactions[0].timestamp;
-			invalidBlock2.generatorPublicKey = 'invalid-public-key';
-			deleteBlockProperties(invalidBlock2);
+		it('validateBlockSlot should fail for invalid block generator (fork: 3)', function (done) {
 
-			blocksVerify.processBlock(invalidBlock2, false, function (err, result) {
+			blocksVerify.processBlock(block2, false, function (err, result) {
 				if (err) {
-					expect(err).equal('Failed to validate block schema: Object didn\'t pass validation for format publicKey: invalid-public-key');
+					expect(err).equal('Failed to verify slot: 3377288');
 					done();
 				}
 			}, true);
 		});
 
-		it('checkTransactions should fail (trs in table)', function (done) {
-			var secret = 'fortune project stable road outside spoil team quantum journey fall cloud great';
-			validBlock2.height = 489;
-			var invalidBlock3 = createBlock(blocks, blockLogic, secret, 33772874, transactionsValidBlock1, validBlock2);
-			deleteBlockProperties(invalidBlock3);
+		it('should generate block 2 with valid generator slot and processed trs', function (done) {
+			var secret = 'flip relief play educate address plastic doctor fix must frown oppose segment';
+			block2 = createBlock(blocks, blockLogic, secret, 33772862, transactionsBlock1, block1);
+			expect(block2.version).to.equal(0);
+			expect(block2.timestamp).to.equal(33772862);
+			expect(block2.numberOfTransactions).to.equal(1);
+			expect(block2.reward).to.equal(0);
+			expect(block2.totalFee).to.equal(10000000);
+			expect(block2.totalAmount).to.equal(10000000000000000);
+			expect(block2.payloadLength).to.equal(117);
+			expect(block2.transactions).to.deep.equal(transactionsBlock1);
+			expect(block2.previousBlock).to.equal(block1.id);
+			done();
+		});
 
-			blocksVerify.processBlock(invalidBlock3, false, function (err, result) {
+		it('checkTransactions should fail when trs is in table (fork: 2)', function (done) {
+			deleteBlockProperties(block2);
+
+			blocksVerify.processBlock(block2, false, function (err, result) {
 				if (err) {
-					expect(err).to.equal(['Transaction is already confirmed:', transactionsValidBlock1[0].id].join(' '));
+					expect(err).to.equal(['Transaction is already confirmed:', transactionsBlock1[0].id].join(' '));
 					done();
 				}
 			}, true);
@@ -725,28 +741,42 @@ describe('blocks/verify', function () {
 
 	describe('processBlock() for valid block {broadcast: false, saveBlock: true}', function () {
 
-		it('should be ok', function (done) {
-			blocks.lastBlock.set(validBlock1);
+		it('should generate block 2 with valid generator slot', function (done) {
+			var secret = 'flip relief play educate address plastic doctor fix must frown oppose segment';
+			block2 = createBlock(blocks, blockLogic, secret, 33772862, transactionsBlock2, block1);
+			expect(block2.version).to.equal(0);
+			expect(block2.timestamp).to.equal(33772862);
+			expect(block2.numberOfTransactions).to.equal(1);
+			expect(block2.reward).to.equal(0);
+			expect(block2.totalFee).to.equal(10000000);
+			expect(block2.totalAmount).to.equal(100000000);
+			expect(block2.payloadLength).to.equal(117);
+			expect(block2.transactions).to.deep.equal(transactionsBlock2);
+			expect(block2.previousBlock).to.equal(block1.id);
+			done();
+		});
 
-			blocksVerify.processBlock(validBlock2, false, function (err, result) {
+		it('should process block 2', function (done) {
+			blocks.lastBlock.set(block1);
+
+			blocksVerify.processBlock(block2, false, function (err, result) {
 				if (err) {
 					return done(err);
 				}
 				expect(result).to.be.undefined;
 				var onMessage = modulesLoader.scope.bus.getMessages();
 				expect(onMessage[0]).to.equal('transactionsSaved');
-				expect(onMessage[1][0].id).to.equal(validBlock2.transactions[0].id);
+				expect(onMessage[1][0].id).to.equal(block2.transactions[0].id);
 				modulesLoader.scope.bus.clearMessages();
 				done();
 			}, true);
 		});
 
-		it('process same block again should fail (checkExists)', function (done) {
-			blocks.lastBlock.set(validBlock1);
-			deleteBlockProperties(validBlock2);
+		it('process same block 2 again should fail (checkExists)', function (done) {
+			blocks.lastBlock.set(block1);
 
-			blocksVerify.processBlock(validBlock2, false, function (err, result) {
-				expect(err).to.equal(['Block', validBlock2.id, 'already exists'].join(' '));
+			blocksVerify.processBlock(block2, false, function (err, result) {
+				expect(err).to.equal(['Block', block2.id, 'already exists'].join(' '));
 				done();
 			}, true);
 		});
@@ -766,14 +796,14 @@ describe('blocks/verify', function () {
 		});
 		it('should generate valid block3', function (done) {
 			var secret = 'flavor type stone episode capable usage save sniff notable liar gas someone';
-			validBlock3 = createBlock(blocks, blockLogic, secret, 33942637, transactionsValidBlock3, validBlock2);
-			expect(validBlock3.version).to.equal(0);
+			block3 = createBlock(blocks, blockLogic, secret, 33942637, [], block2);
+			expect(block3.version).to.equal(0);
 			done();
 		});
 
 		it('processBlock() should broadcast block3', function (done) {
 
-			blocksVerify.processBlock(validBlock3, true, function (err, result) {
+			blocksVerify.processBlock(block3, true, function (err, result) {
 				if (err) {
 					return done(err);
 				}
@@ -783,7 +813,10 @@ describe('blocks/verify', function () {
 				onMessage[1] = bson.deserialize(onMessage[1]);
 				expect(onMessage[1].version).to.be.undefined;
 				expect(onMessage[1].numberOfTransactions).to.be.undefined;
-				expect(onMessage[1].id).to.equal(validBlock3.id);
+				expect(onMessage[1].totalAmount).to.be.undefined;
+				expect(onMessage[1].totalFee).to.be.undefined;
+				expect(onMessage[1].payloadLength).to.be.undefined;
+				expect(onMessage[1].id).to.be.undefined;
 				expect(onMessage[2]).to.be.true;
 				expect(onMessage[3]).to.be.undefined; // transactionsSaved
 				modulesLoader.scope.bus.clearMessages();
@@ -792,9 +825,9 @@ describe('blocks/verify', function () {
 		});
 
 		it('processBlock() broadcast block3 again should be ok (checkExists)', function (done) {
-			blocks.lastBlock.set(validBlock2);
+			blocks.lastBlock.set(block2);
 			
-			blocksVerify.processBlock(validBlock3, true, function (err, result) {
+			blocksVerify.processBlock(block3, true, function (err, result) {
 				if (err) {
 					return done(err);
 				}
@@ -804,7 +837,10 @@ describe('blocks/verify', function () {
 				onMessage[1] = bson.deserialize(onMessage[1]);
 				expect(onMessage[1].version).to.be.undefined;
 				expect(onMessage[1].numberOfTransactions).to.be.undefined;
-				expect(onMessage[1].id).to.equal(validBlock3.id);
+				expect(onMessage[1].totalAmount).to.be.undefined;
+				expect(onMessage[1].totalFee).to.be.undefined;
+				expect(onMessage[1].payloadLength).to.be.undefined;
+				expect(onMessage[1].id).to.be.undefined;
 				expect(onMessage[2]).to.be.true;
 				expect(onMessage[3]).to.be.undefined; // transactionsSaved
 				modulesLoader.scope.bus.clearMessages();
@@ -818,10 +854,10 @@ describe('blocks/verify', function () {
 	describe('processBlock() for valid block {broadcast: false, saveBlock: false}', function () {
 
 		it('processBlock() should receive block3', function (done) {
-			blocks.lastBlock.set(validBlock2);
-			deleteBlockProperties(validBlock3);
+			blocks.lastBlock.set(block2);
+			deleteBlockProperties(block3);
 
-			blocksVerify.processBlock(validBlock3, false, function (err, result) {
+			blocksVerify.processBlock(block3, false, function (err, result) {
 				if (err) {
 					return done(err);
 				}
@@ -833,10 +869,10 @@ describe('blocks/verify', function () {
 		});
 
 		it('processBlock() receive block3 again should be ok (checkExists)', function (done) {
-			blocks.lastBlock.set(validBlock2);
-			deleteBlockProperties(validBlock3);
+			blocks.lastBlock.set(block2);
+			deleteBlockProperties(block3);
 
-			blocksVerify.processBlock(validBlock3, false, function (err, result) {
+			blocksVerify.processBlock(block3, false, function (err, result) {
 				expect(result).to.be.undefined;
 				var onMessage = modulesLoader.scope.bus.getMessages();
 				expect(onMessage).to.be.an('array').that.is.empty;
