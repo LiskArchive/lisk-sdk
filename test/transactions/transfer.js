11eb93e961f8e3153a26977d9b5e33aa321af933
willclarktech <willclarktech@users.noreply.github.com>
2017-07-18 12:46:46 +0200

    Add createOutTransfer function

--- test/transactions/transfer.js
+++ test/transactions/transfer.js
@@ -23,41 +23,58 @@ describe('transfer.js', function () {
 		var dappId = '1234213';
 		var amount = 10e8;
 		var secret = 'secret';
+		var publicKey = '5d036a858ce89f844491762eb89e2bfbd50a4a0a0da658e4b2628b25b117ae09';
 		var secondSecret = 'secondSecret';
-		var transferTransaction = createInTransfer(dappId, amount, secret, secondSecret);
+		var inTransferTransaction = createInTransfer(dappId, amount, secret, secondSecret);
 
 		it('should be a function', function () {
 			(createInTransfer).should.be.type('function');
 		});
 
-		it('should create a transfer dapp transaction', function () {
-			(transferTransaction).should.be.type('object');
+		it('should create an in transfer dapp transaction', function () {
+			(inTransferTransaction).should.be.type('object');
 		});
 
-		it('should create a transfer dapp transaction type 6', function () {
-			(transferTransaction.type).should.be.equal(6);
+		it('should create an in transfer dapp transaction type 6', function () {
+			(inTransferTransaction).should.have.property('type').be.equal(6);
 		});
 
-		it('should create a transfer dapp transaction with dapp id in asset', function () {
-			(transferTransaction.asset.inTransfer.dappId).should.be.equal(dappId);
+		it('should create an in transfer dapp transaction with dapp id in asset', function () {
+			(inTransferTransaction)
+				.should.have.property('asset')
+				.with.property('inTransfer')
+				.with.property('dappId')
+				.equal(dappId);
 		});
 
-		it('should create a transfer dapp transaction with a provided amount', function () {
-			(transferTransaction.amount).should.be.equal(amount);
+		it('should create an in transfer dapp transaction with a provided amount', function () {
+			(inTransferTransaction).should.have.property('amount').equal(amount);
 		});
 
-		it('should create a transfer dapp transaction with first signature', function () {
-			(transferTransaction.signature).should.be.ok;
+		it('should create an in transfer dapp transaction with a default fee', function () {
+			(inTransferTransaction).should.have.property('fee').equal(0.1e8);
 		});
 
-		it('should create a transfer dapp transaction with second signature', function () {
-			(transferTransaction.signSignature).should.be.ok;
+		it('should create an in transfer dapp transaction with no recipient', function () {
+			(inTransferTransaction).should.have.property('recipientId').be.null();
 		});
 
-		it('should create a transfer dapp transaction with just one signature', function () {
-			var transferTransactionOneSignature = createInTransfer(dappId, amount, secret);
-			(transferTransactionOneSignature.signature).should.be.ok();
-			(transferTransactionOneSignature).should.not.have.property('secondSignature');
+		it('should create an in transfer dapp transaction with senderPublicKey', function () {
+			(inTransferTransaction).should.have.property('senderPublicKey').equal(publicKey);
+		});
+
+		it('should create an in transfer dapp transaction with first signature', function () {
+			(inTransferTransaction).should.have.property('signature').and.be.ok();
+		});
+
+		it('should create an in transfer dapp transaction with second signature', function () {
+			(inTransferTransaction).should.have.property('signSignature').and.be.ok();
+		});
+
+		it('should create an in transfer dapp transaction with just one signature', function () {
+			var inTransferTransactionOneSignature = createInTransfer(dappId, amount, secret);
+			(inTransferTransactionOneSignature).should.have.property('signature').and.be.ok();
+			(inTransferTransactionOneSignature).should.not.have.property('secondSignature');
 		});
 
 		it('should use time slots to get the time for the timestamp', function () {
@@ -83,6 +100,108 @@ describe('transfer.js', function () {
 			var stub = sinon.stub(slots, 'getTime').returns(time);
 
 			var trs = createInTransfer(dappId, amount, secret, null, offset);
+
+			(trs).should.have.property('timestamp').and.be.equal(time);
+			(stub.calledWithExactly(now.getTime() - offset)).should.be.true();
+
+			stub.restore();
+			clock.restore();
+		});
+
+	});
+
+	describe('#createOutTransfer', function () {
+		var createOutTransfer = transfer.createOutTransfer;
+		var dappId = '1234213';
+		var transactionId = '9876567';
+		var recipientId = '989234L';
+		var amount = 10e8;
+		var secret = 'secret';
+		var publicKey = '5d036a858ce89f844491762eb89e2bfbd50a4a0a0da658e4b2628b25b117ae09';
+		var secondSecret = 'secondSecret';
+		var outTransferTransaction = createOutTransfer(dappId, transactionId, recipientId, amount, secret, secondSecret);
+
+		it('should be a function', function () {
+			(createOutTransfer).should.be.type('function');
+		});
+
+		it('should create an out transfer dapp transaction', function () {
+			(outTransferTransaction).should.be.type('object');
+		});
+
+		it('should create an out transfer dapp transaction type 7', function () {
+			(outTransferTransaction).should.have.property('type').equal(7);
+		});
+
+		it('should create an out transfer dapp transaction with dapp id in asset', function () {
+			(outTransferTransaction)
+				.should.have.property('asset')
+				.with.property('outTransfer')
+				.with.property('dappId')
+				.equal(dappId);
+		});
+
+		it('should create an out transfer dapp transaction with transaction id in asset', function () {
+			(outTransferTransaction)
+				.should.have.property('asset')
+				.with.property('outTransfer')
+				.with.property('transactionId')
+				.equal(transactionId);
+		});
+
+		it('should create an out transfer dapp transaction with a provided amount', function () {
+			(outTransferTransaction).should.have.property('amount').equal(amount);
+		});
+
+		it('should create an out transfer dapp transaction with a default fee', function () {
+			(outTransferTransaction).should.have.property('fee').equal(0.1e8);
+		});
+
+		it('should create an out transfer dapp transaction with a provided recipient', function () {
+			(outTransferTransaction).should.have.property('recipientId').equal(recipientId);
+		});
+
+		it('should create an out transfer dapp transaction with senderPublicKey', function () {
+			(outTransferTransaction).should.have.property('senderPublicKey').equal(publicKey);
+		});
+
+		it('should create an out transfer dapp transaction with first signature', function () {
+			(outTransferTransaction).should.have.property('signature').be.ok();
+		});
+
+		it('should create an out transfer dapp transaction with second signature', function () {
+			(outTransferTransaction).should.have.property('signSignature').be.ok();
+		});
+
+		it('should create an out transfer dapp transaction with just one signature', function () {
+			var outTransferTransactionOneSignature = createOutTransfer(dappId, transactionId, recipientId, amount, secret);
+			(outTransferTransactionOneSignature).should.have.property('signature').be.ok();
+			(outTransferTransactionOneSignature).should.not.have.property('secondSignature');
+		});
+
+		it('should use time slots to get the time for the timestamp', function () {
+			var now = new Date();
+			var clock = sinon.useFakeTimers(now, 'Date');
+			var time = 36174862;
+			var stub = sinon.stub(slots, 'getTime').returns(time);
+
+			var trs = createOutTransfer(dappId, transactionId, recipientId, amount, secret);
+
+			(trs).should.have.property('timestamp').and.be.equal(time);
+			(stub.calledWithExactly(now.getTime())).should.be.true();
+
+			stub.restore();
+			clock.restore();
+		});
+
+		it('should use time slots with an offset to get the time for the timestamp', function () {
+			var now = new Date();
+			var clock = sinon.useFakeTimers(now, 'Date');
+			var offset = 10e3;
+			var time = 36174862;
+			var stub = sinon.stub(slots, 'getTime').returns(time);
+
+			var trs = createOutTransfer(dappId, transactionId, recipientId, amount, secret, null, offset);
 
 			(trs).should.have.property('timestamp').and.be.equal(time);
 			(stub.calledWithExactly(now.getTime() - offset)).should.be.true();
