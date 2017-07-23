2011949c06cfe7aa76cbce4598cd8728b1d82e7c
Mariusz Serek <mariusz@serek.net>
2017-07-23 14:17:40 +0200

    Add first round test to rounds-related tests

--- test/unit/sql/rounds.js
+++ test/unit/sql/rounds.js
@@ -6,15 +6,17 @@ var chai   = require('chai');
 var expect = require('chai').expect;
 var rewire = require('rewire');
 var sinon  = require('sinon');
+var Promise = require('bluebird');
 
-var config    = require('../../../config.json');
-var node      = require('../../node.js');
-var Sequence  = require('../../../helpers/sequence.js');
-var slots     = require('../../../helpers/slots.js');
+var config   = require('../../../config.json');
+var node     = require('../../node.js');
+var Sequence = require('../../../helpers/sequence.js');
+var slots    = require('../../../helpers/slots.js');
+var bignum   = require('../../../helpers/bignum.js');
 
 describe('Rounds-related SQL triggers', function () {
 	var db, logger, library, rewiredModules = {}, modules = [];
-	var mem_state;
+	var mem_state, delegates_state, round_blocks = [];
 
 	function normalizeMemAccounts (mem_accounts) {
 		var accounts = {};
@@ -27,14 +29,115 @@ describe('Rounds-related SQL triggers', function () {
 		return accounts;
 	}
 
+	function normalizeDelegates (db_delegates) {
+		var delegates = {};
+		_.map(db_delegates, function (d) {
+			d.pk = d.pk.toString('hex');
+			d.rank = Number(d.rank);
+			d.fees = Number(d.fees);
+			d.rewards = Number(d.rewards);
+			d.voters_balance = Number(d.voters_balance);
+			d.voters_cnt = Number(d.voters_cnt);
+			d.blocks_forged_cnt = Number(d.blocks_forged_cnt);
+			d.blocks_missed_cnt = Number(d.blocks_missed_cnt);
+			delegates[d.pk] = d;
+		});
+		return delegates;
+	}
+
+	function getMemAccounts () {
+		return db.query('SELECT * FROM mem_accounts').then(function (rows) {
+			rows = normalizeMemAccounts(rows);
+			mem_state = rows;
+			return rows;
+		});
+	}
+
+	function getDelegates (normalize) {
+		return db.query('SELECT * FROM delegates').then(function (rows) {
+			delegates_state = normalizeDelegates(rows);
+			return rows;
+		});
+	}
+
+	function getBlocks (round) {
+		return db.query('SELECT * FROM blocks WHERE CEIL(height / 101::float)::int = ${round} AND height > 1 ORDER BY height ASC', {round: round}).then(function (rows) {
+			return rows;
+		});
+	}
+
+	function getRoundRewards (round) {
+		return db.query('SELECT ENCODE(pk, \'hex\') AS pk, SUM(fees) AS fees, SUM(reward) AS rewards FROM rounds_rewards WHERE round = ${round} GROUP BY pk', {round: round}).then(function (rows) {
+			var rewards = {};
+			_.each(rows, function (row) {
+				rewards[row.pk] = {
+					pk: row.pk,
+					fees: Number(row.fees),
+					rewards: Number(row.rewards)
+				};
+			});
+			return rewards;
+		});
+	}
+
+	function getExpectedRoundRewards (blocks) {
+		var rewards = {};
+
+		var feesTotal = _.reduce(blocks, function (fees, block) {
+			return new bignum(fees).plus(block.totalFee);
+		}, 0);
+
+		var feesPerDelegate = new bignum(feesTotal.toPrecision(15)).dividedBy(slots.delegates).floor();
+		var feesRemaining   = new bignum(feesTotal.toPrecision(15)).minus(feesPerDelegate.times(slots.delegates));
+
+		node.debug('	Total fees: ' + feesTotal.toString() + ' Fees per delegates: ' + feesPerDelegate.toString() + ' Remaining fees: ' + feesRemaining);
+		
+		_.each(blocks, function (block, index) {
+			var pk = block.generatorPublicKey.toString('hex');
+			if (rewards[pk]) {
+				rewards[pk].fees = rewards[pk].fees.plus(feesPerDelegate);
+				rewards[pk].rewards = rewards[pk].rewards.plus(block.reward);
+			} else {
+				rewards[pk] = {
+					pk: pk,
+					fees: new bignum(feesPerDelegate),
+					rewards: new bignum(block.reward)
+				};
+			}
+
+			if (index === blocks.length - 1) {
+				// Apply remaining fees to last delegate
+				rewards[pk].fees = rewards[pk].fees.plus(feesRemaining);
+			}
+		});
+
+		_.each(rewards, function (delegate) {
+			delegate.fees = Number(delegate.fees.toFixed());
+			delegate.rewards = Number(delegate.rewards.toFixed());
+		});
+
+		return rewards;
+	};
+
 	before(function (done) {
 		// Init dummy connection with database - valid, used for tests here
-		var pgp = require('pg-promise')();
+		var options = {
+		    promiseLib: Promise
+		};
+		var pgp = require('pg-promise')(options);
 		config.db.user = config.db.user || process.env.USER;
 		db = pgp(config.db);
 
 		// Clear tables
-		db.none('DELETE FROM blocks; DELETE FROM mem_accounts;').then(done).catch(done);
+		db.task(function (t) {
+			return t.batch([
+				t.none('DELETE FROM blocks WHERE height > 1'),
+				t.none('DELETE FROM blocks'),
+				t.none('DELETE FROM mem_accounts')
+			]);
+		}).then(function () {
+			done();
+		}).catch(done);
 	});
 
 	before(function (done) {
@@ -221,14 +324,14 @@ describe('Rounds-related SQL triggers', function () {
 	describe('genesisBlock', function () {
 		var genesisBlock, delegatesList;
 		var genesisAccount;
-		var accounts;
+		var genesisAccounts;
 
 		before(function () {
 			// Get genesis accounts address - should be senderId from first transaction
 			genesisAccount = library.genesisblock.block.transactions[0].senderId;
 
 			// Get unique accounts from genesis block
-			accounts = _.reduce(library.genesisblock.block.transactions, function (accounts, tx) {
+			genesisAccounts = _.reduce(library.genesisblock.block.transactions, function (accounts, tx) {
 				if (tx.senderId && accounts.indexOf(tx.senderId) === -1) {
 					accounts.push(tx.senderId);
 				}
@@ -239,12 +342,10 @@ describe('Rounds-related SQL triggers', function () {
 			}, []);
 		})
 
-		it('should not populate mem_accounts', function (done) {
-			db.query('SELECT * FROM mem_accounts').then(function (rows) {
-				mem_state = normalizeMemAccounts(rows);
-				expect(rows.length).to.equal(0);
-				done();
-			}).catch(done);
+		it('should not populate mem_accounts', function () {
+			return getMemAccounts().then(function (accounts) {
+				expect(Object.keys(accounts).length).to.equal(0);
+			});
 		});
 
 		it('should load genesis block with transactions into database (native)', function (done) {
@@ -256,9 +357,9 @@ describe('Rounds-related SQL triggers', function () {
 			}).catch(done);
 		});
 
-		it('should populate delegates table (native) and set data (trigger block_insert->delegates_update_on_block)', function (done) {
-			db.query('SELECT * FROM delegates').then(function (rows) {
-				_.each(rows, function (delegate) {
+		it('should populate delegates table (native) and set data (trigger block_insert)', function () {
+			return getDelegates().then(function () {
+				_.each(delegates_state, function (delegate) {
 					expect(delegate.tx_id).that.is.an('string');
 
 					// Search for that transaction in genesis block
@@ -267,17 +368,16 @@ describe('Rounds-related SQL triggers', function () {
 
 					expect(delegate.name).to.equal(found.asset.delegate.username);
 					expect(delegate.address).to.equal(found.senderId);
-					expect(delegate.pk.toString('hex')).to.equal(found.senderPublicKey);
+					expect(delegate.pk).to.equal(found.senderPublicKey);
 					
 					// Data populated by trigger
 					expect(delegate.rank).that.is.an('number');
-					expect(Number(delegate.voters_balance)).to.equal(10000000000000000);
+					expect(delegate.voters_balance).to.equal(10000000000000000);
 					expect(delegate.voters_cnt).to.equal(1);
 					expect(delegate.blocks_forged_cnt).to.equal(0);
 					expect(delegate.blocks_missed_cnt).to.equal(0);
 				});
-				done();
-			}).catch(done);
+			});
 		});
 
 		it('should populate modules.delegates.__private.delegatesList with 101 public keys (pg-notify)', function () {
@@ -292,12 +392,13 @@ describe('Rounds-related SQL triggers', function () {
 
 		it('should apply transactions of genesis block to mem_accounts (native)', function (done) {
 			library.modules.blocks.chain.applyGenesisBlock(genesisBlock, function (err) {
-				db.query('SELECT * FROM mem_accounts').then(function (rows) {
-					mem_state = normalizeMemAccounts(rows);
-					// Number of returned accounts should be equal to number of unique accounts in genesis block
-					expect(rows.length).to.equal(accounts.length);
+				if (err) { done(err); }
 
-					_.each(mem_state, function (account) {
+				getMemAccounts().then(function (accounts) {
+					// Number of returned accounts should be equal to number of unique accounts in genesis block
+					expect(Object.keys(accounts).length).to.equal(genesisAccounts.length);
+
+					_.each(accounts, function (account) {
 						if (account.address === genesisAccount) {
 							// Genesis account should have negative balance
 							expect(account.balance).to.be.below(0);
@@ -309,8 +410,7 @@ describe('Rounds-related SQL triggers', function () {
 							expect(account.balance).to.be.above(0);
 						}
 					});
-					done();
-				}).catch(done);
+				}).then(done).catch(done);
 			})
 		});
 	});
@@ -319,6 +419,7 @@ describe('Rounds-related SQL triggers', function () {
 		var transactions = [];
 
 		function addTransaction (transaction, cb) {
+			node.debug('	Add transaction ID: ' + transaction.id);
 			// Add transaction to transactions pool - we use shortcut here to bypass transport module, but logic is the same
 			// See: modules.transport.__private.receiveTransaction
 			transaction = library.logic.transaction.objectNormalize(transaction);
@@ -358,32 +459,58 @@ describe('Rounds-related SQL triggers', function () {
 			});
 		}
 
+		function tickAndValidate (transactions) {
+			var last_block = library.modules.blocks.lastBlock.get();
+
+			return new Promise(function (resolve, reject) {
+				addTransactionsAndForge(transactions, function(err) {
+					if (err) { reject(err); }
+					resolve();
+				})
+			})
+				.then(function () {
+					var new_block = library.modules.blocks.lastBlock.get();
+					expect(new_block.id).to.not.equal(last_block.id);
+					last_block = new_block;
+					round_blocks.push(new_block);
+				})
+				.then(getMemAccounts)
+				.then(function (accounts) {
+					var expected_mem_state = expectedMemState(transactions);
+					expect(accounts).to.deep.equal(expected_mem_state);
+				})
+				.then(getDelegates)
+				.then(function () {
+					var expected_delegates_state = expectedDelegatesState();
+					expect(delegates_state).to.deep.equal(expected_delegates_state);
+				})
+		}
+
 		function expectedMemState (transactions) {
 			_.each(transactions, function (tx) {
-				var block_id = library.modules.blocks.lastBlock.get().id;
+				var last_block = library.modules.blocks.lastBlock.get();
 
 				var address = tx.senderId
 				if (mem_state[address]) {
 					// Update sender
 					mem_state[address].balance -= (tx.fee+tx.amount);
 					mem_state[address].u_balance -= (tx.fee+tx.amount);
-					mem_state[address].blockId = block_id;
+					mem_state[address].blockId = last_block.id;
 					mem_state[address].virgin = 0;
 				}
 
 				address = tx.recipientId;
 				if (mem_state[address]) {
 					// Update recipient
-					found = true;
 					mem_state[address].balance += tx.amount;
 					mem_state[address].u_balance += tx.amount;
-					mem_state[address].blockId = block_id;
+					mem_state[address].blockId = last_block.id;
 				} else {
 					// Funds sent to new account
 					mem_state[address] = {
 						address: address,
 						balance: tx.amount,
-						blockId: block_id,
+						blockId: last_block.id,
 						delegates: null,
 						fees: 0,
 						isDelegate: 0,
@@ -416,6 +543,16 @@ describe('Rounds-related SQL triggers', function () {
 			return mem_state;
 		}
 
+		function expectedDelegatesState () {
+			var last_block = library.modules.blocks.lastBlock.get();
+			_.each(delegates_state, function (delegate) {
+				if (delegate.pk === last_block.generatorPublicKey) {
+					delegate.blocks_forged_cnt += 1;
+				}
+			});
+			return delegates_state;
+		}
+
 		before(function () {
 			// Set delegates module as loaded to allow manual forging
 			rewiredModules.delegates.__set__('__private.loaded', true);
@@ -424,7 +561,6 @@ describe('Rounds-related SQL triggers', function () {
 		it('should load all secrets of 101 delegates and set modules.delegates.__private.keypairs (native)', function (done) {
 			var loadDelegates = rewiredModules.delegates.__get__('__private.loadDelegates');
 			loadDelegates(function (err) {
-				if (err) { done(err); }
 				var keypairs = rewiredModules.delegates.__get__('__private.keypairs');
 				expect(Object.keys(keypairs).length).to.equal(config.forging.secret.length);
 				_.each(keypairs, function (keypair, pk) {
@@ -432,11 +568,11 @@ describe('Rounds-related SQL triggers', function () {
 					expect(keypair.privateKey).to.be.instanceOf(Buffer);
 					expect(pk).to.equal(keypair.publicKey.toString('hex'));
 				});
-				done();
+				done(err);
 			});
 		});
 
-		it('should forge block with 1 TRANSFER transaction and update mem_accounts', function (done) {
+		it('should forge block with 1 TRANSFER transaction to random account, update mem_accounts (native) and delegates (trigger block_insert_delete) tables', function () {
 			var tx = node.lisk.transaction.createTransaction(
 				node.randomAccount().address,
 				node.randomNumber(100000000, 1000000000),
@@ -444,16 +580,83 @@ describe('Rounds-related SQL triggers', function () {
 			);
 			transactions.push(tx);
 
-			addTransactionsAndForge(transactions, function (err) {
-				if (err) { done(err); }
-				var expected_mem_state = expectedMemState(transactions);
-				db.query('SELECT * FROM mem_accounts').then(function (rows) {
-					mem_state = normalizeMemAccounts(rows);
-					expect(mem_state).to.deep.equal(expected_mem_state);
-					done();
-				}).catch(done);
-			});
+			return tickAndValidate(transactions);
 		});
 
+		it('should forge block with 25 TRANSFER transactions to random accounts, update mem_accounts (native) and delegates (trigger block_insert_delete) tables', function () {
+			var tx_cnt = 25;
+			var transactions = [];
+
+			for (var i = tx_cnt - 1; i >= 0; i--) {
+				var tx = node.lisk.transaction.createTransaction(
+					node.randomAccount().address,
+					node.randomNumber(100000000, 1000000000),
+					node.gAccount.password
+				);
+				transactions.push(tx);
+			}
+
+			return tickAndValidate(transactions);
+		});
+
+		it('should forge 98 blocks with 1 TRANSFER transaction each to random account, update mem_accounts (native) and delegates (trigger block_insert_delete) tables', function (done) {
+			var blocks_cnt = 98;
+			var blocks_processed = 0;
+			var tx_cnt = 1;
+
+			async.doUntil(function (untilCb) {
+				++blocks_processed;
+				var transactions = [];
+				for (var t = tx_cnt - 1; t >= 0; t--) {
+					var tx = node.lisk.transaction.createTransaction(
+						node.randomAccount().address,
+						node.randomNumber(100000000, 1000000000),
+						node.gAccount.password
+					);
+					transactions.push(tx);
+				}
+				node.debug('	Processing block ' + blocks_processed + ' of ' + blocks_cnt + ' with ' + transactions.length + ' transactions');
+
+				// We need to wait 1 second between forge because of block time
+				setTimeout(function () {
+					tickAndValidate(transactions).then(untilCb).catch(untilCb);
+				}, 1000);
+				
+			}, function (err) {
+				return err || blocks_processed >= blocks_cnt;
+			}, done);
+		});
+
+		it('should calculate rewards for round 1 correctly - all should be the same (native, rounds_rewards, delegates)', function () {
+			var round = 1;
+			var expectedRewards;
+
+			return getBlocks(round)
+				.then(function (blocks) {
+					expectedRewards = getExpectedRoundRewards(blocks);
+				})
+				.then(function () {
+					return getRoundRewards(round);
+				})
+				.then(function (rewards) {
+					expect(rewards).to.deep.equal(expectedRewards);
+				})
+				.then(function () {
+					return getDelegates();
+				})
+				.reduce(function (delegates, d) {
+					if (d.fees > 0 || d.rewards > 0) {
+						delegates[d.pk] = {
+							pk: d.pk,
+							fees: Number(d.fees),
+							rewards: Number(d.rewards)
+						}
+					}
+					return delegates;
+				}, {})
+				.then(function (delegates) {
+					expect(delegates).to.deep.equal(expectedRewards);
+				});
+		});
 	});
 });
