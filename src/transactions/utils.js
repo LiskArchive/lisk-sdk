db320c4f7088e4412692c2066e158204e95fe7f2
willclarktech <willclarktech@users.noreply.github.com>
2017-07-31 15:18:25 +0200

    Add utils for transactions

---
+++ src/transactions/utils.js
@@ -0,0 +1,28 @@
+const crypto = require('./crypto.js');
+
+const secondSignTransaction = (transactionObject, secondSecret) => {
+	const secondKeys = crypto.getKeys(secondSecret);
+	return Object.assign({}, transactionObject, {
+		signSignature: crypto.secondSign(transactionObject, secondKeys),
+	});
+};
+
+const processTransaction = (transaction, keys, secondSecret) => {
+	const singleSignedTransaction = Object.assign({}, transaction, {
+		signature: crypto.sign(transaction, keys),
+	});
+
+	const signedTransaction = secondSecret
+		? secondSignTransaction(singleSignedTransaction, secondSecret)
+		: singleSignedTransaction;
+
+	const transactionWithId = Object.assign({}, signedTransaction, {
+		id: crypto.getId(signedTransaction),
+	});
+
+	return transactionWithId;
+};
+
+module.exports = {
+	processTransaction,
+};
