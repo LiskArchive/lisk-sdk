268c9c1c880305de4edf210b6d2d140561280ad5
Mariusz Serek <mariusz@serek.net>
2017-08-08 11:25:20 +0200

    Remove newBlock events

--- modules/blocks/chain.js
+++ modules/blocks/chain.js
@@ -454,12 +454,10 @@ Chain.prototype.applyBlock = function (block, saveBlock, cb) {
 					}
 
 					library.logger.debug('Block applied correctly with ' + block.transactions.length + ' transactions');
-					library.bus.message('newBlock', block, broadcast);
 
 					return seriesCb();
 				});
 			} else {
-				library.bus.message('newBlock', block, broadcast);
 				return seriesCb();
 			}
 		},
