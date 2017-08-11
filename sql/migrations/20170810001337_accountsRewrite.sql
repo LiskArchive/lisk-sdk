7a386d5798e26d4616cd2d07729589ca95648fe3
Mariusz Serek <mariusz@serek.net>
2017-08-11 02:24:36 +0200

    Create on_transaction_insert SQL trigger and function

---
+++ sql/migrations/20170810001337_accountsRewrite.sql
@@ -0,0 +1,71 @@
+BEGIN;
+
+-- Consistency checks - 'mem_accounts' against blockchain
+DO $$
+DECLARE
+	diff int;
+BEGIN
+	RAISE NOTICE 'Accounts rewrite migration, please wait...';
+	SELECT COUNT(1) FROM validateMemBalances() INTO diff;
+
+	IF diff > 0 THEN
+		RAISE check_violation USING MESSAGE = 'Migration failed, mem_accounts are inconsistent';
+	END IF;
+END $$;
+
+-- Create table 'accounts'
+CREATE TABLE IF NOT EXISTS "accounts" (
+	"tx_id" VARCHAR(20) REFERENCES trs(id) ON DELETE CASCADE,
+	"pk" BYTEA DEFAULT NULL,
+	"pk_tx_id" VARCHAR(20) DEFAULT NULL REFERENCES trs(id) ON DELETE SET NULL,
+	"second_pk" BYTEA DEFAULT NULL,
+	"address" VARCHAR(22) NOT NULL UNIQUE PRIMARY KEY,
+	"balance" BIGINT NOT NULL DEFAULT 0
+);
+
+-- Create function for maintain 'accounts' table
+CREATE OR REPLACE FUNCTION on_transaction_insert() RETURNS TRIGGER LANGUAGE PLPGSQL AS $$
+	DECLARE
+		sender_address VARCHAR(22);
+		sender_pk BYTEA;
+		recipient_address VARCHAR(22);
+	BEGIN
+		-- Get accounts that are part of the transaction
+		SELECT address, pk INTO sender_address, sender_pk FROM accounts WHERE address = NEW."senderId";
+		SELECT address INTO recipient_address FROM accounts WHERE address = NEW."recipientId";
+
+		IF sender_address IS NULL THEN
+			-- No sender address in accounts - create new account
+			INSERT INTO accounts (tx_id, pk, pk_tx_id, address) VALUES (NEW.id, NEW."senderPublicKey", NEW.id, NEW."senderId");
+		ELSIF sender_pk IS NULL THEN
+			-- Sender address exists, but no PK - update pk
+			UPDATE accounts SET pk = NEW."senderPublicKey", pk_tx_id = NEW.id WHERE accounts.address = NEW."senderId";
+		ELSIF sender_pk !== NEW."senderPublicKey" THEN
+			RAISE check_violation USING MESSAGE = 'Transaction invalid - cannot change account public key';
+		END IF;
+
+		IF recipient_address IS NULL AND NEW."recipientId" IS NOT NULL THEN
+			-- No recipient address in accounts - create new account
+			INSERT INTO accounts (tx_id, address) VALUES (NEW.id, NEW."recipientId");
+		END IF;
+
+		-- Update sender balance
+		IF NEW."senderId" IS NOT NULL THEN
+			UPDATE accounts SET balance = accounts.balance-(NEW.amount+NEW.fee) WHERE accounts.address = NEW."senderId";
+		END IF;
+
+		-- Update recipient balance
+		IF NEW."recipientId" IS NOT NULL THEN
+			UPDATE accounts SET balance = accounts.balance+NEW.amount WHERE accounts.address = NEW."recipientId";
+		END IF;
+
+	RETURN NULL;
+END $$;
+
+-- Create trigger that will execute 'on_transaction_insert' function after insertion of transaction
+CREATE TRIGGER on_transaction_insert
+	AFTER INSERT ON trs
+	FOR EACH ROW
+	EXECUTE PROCEDURE on_transaction_insert();
+
+COMMIT;
