9b528fe605390c416b367cfd4a3edd4a20267703
Maciej Baj <macie.baj@gmail.com>
2017-08-25 11:50:25 +0200

    add types representatives

--- test/common/schemaDynamicTest.js
+++ test/common/schemaDynamicTest.js
@@ -1,50 +1,19 @@
 'use strict';
 
 var assign = require('lodash').assign;
-var async = require('async');
 var chai = require('chai');
 var expect = require('chai').expect;
 require('it-each')({ testPerIteration: true });
 
-var strings = [
-	{
-		input: '1',
-		description: 'string integer',
-		expectation: 'string'
-	}
-];
+var typesRepresentatives = require('./typesRepresentatives');
 
-var numbers = [
-	{
-		input: 1,
-		description: 'integer',
-		expectation: 'integer'
-	}
-];
-
-var objects = [
-	{
-		input: {},
-		description: 'empty object',
-		expectation: 'object'
-	}
-];
-
-var arrays = [
-	{
-		input: [],
-		description: 'empty array',
-		expectation: 'array'
-	}
-];
-
-var others = [
-	{
-		input: NaN,
-		description: 'Not a number',
-		expectation: 'not-a-number'
-	}
-];
+var arrays = typesRepresentatives.arrays;
+var booleans = typesRepresentatives.booleans;
+var integers = typesRepresentatives.integers;
+var numbers = typesRepresentatives.numbers;
+var objects = typesRepresentatives.objects;
+var others = typesRepresentatives.others;
+var strings = typesRepresentatives.strings;
 
 var self;
 
@@ -60,20 +29,28 @@ function SchemaDynamicTest (config) {
 	self.schema = {
 		shouldFailAgainst: {
 			nonArray: {
-				arguments: self.testArgument.bind(self, 'array', numbers.concat(strings).concat(others).concat(objects)),
-				property: self.testProperty.bind(self, 'array', numbers.concat(strings).concat(others).concat(objects))
+				arguments: self.testArgument.bind(self, 'array', booleans.concat(integers).concat(numbers).concat(objects).concat(others).concat(strings)),
+				property: self.testProperty.bind(self, 'array', booleans.concat(integers).concat(numbers).concat(objects).concat(others).concat(strings))
+			},
+			nonBoolean: {
+				arguments: self.testArgument.bind(self, 'boolean', arrays.concat(integers).concat(numbers).concat(objects).concat(others).concat(strings)),
+				property: self.testProperty.bind(self, 'boolean', arrays.concat(integers).concat(numbers).concat(objects).concat(others).concat(strings))
 			},
 			nonInteger: {
-				arguments: self.testArgument.bind(self, 'integer', objects.concat(strings).concat(others).concat(arrays)),
-				property: self.testProperty.bind(self, 'integer', objects.concat(strings).concat(others).concat(arrays))
+				arguments: self.testArgument.bind(self, 'integer', arrays.concat(booleans).concat(numbers).concat(objects).concat(others).concat(strings)),
+				property: self.testProperty.bind(self, 'integer', arrays.concat(booleans).concat(numbers).concat(objects).concat(others).concat(strings))
+			},
+			nonNumber: {
+				arguments: self.testArgument.bind(self, 'number', arrays.concat(booleans).concat(integers).concat(objects).concat(others).concat(strings)),
+				property: self.testProperty.bind(self, 'number', arrays.concat(booleans).concat(integers).concat(objects).concat(others).concat(strings))
 			},
 			nonObject: {
-				arguments: self.testArgument.bind(self, 'object', numbers.concat(strings).concat(others).concat(arrays)),
-				property: self.testProperty.bind(self, 'object', numbers.concat(strings).concat(others).concat(arrays))
+				arguments: self.testArgument.bind(self, 'object', arrays.concat(booleans).concat(integers).concat(numbers).concat(others).concat(strings)),
+				property: self.testProperty.bind(self, 'object', arrays.concat(booleans).concat(integers).concat(numbers).concat(others).concat(strings))
 			},
 			nonString: {
-				arguments: self.testArgument.bind(self, 'string', numbers.concat(objects).concat(others).concat(arrays)),
-				property: self.testProperty.bind(self, 'string', numbers.concat(objects).concat(others).concat(arrays))
+				arguments: self.testArgument.bind(self, 'string', arrays.concat(booleans).concat(integers).concat(numbers).concat(objects).concat(others)),
+				property: self.testProperty.bind(self, 'string', arrays.concat(booleans).concat(integers).concat(numbers).concat(objects).concat(others))
 			}
 		},
 		shouldFailWithoutRequiredProperties: self.testRequired
@@ -140,90 +117,3 @@ SchemaDynamicTest.prototype.testRequired = function (testedFunction, validArgume
 };
 
 module.exports = SchemaDynamicTest;
-/*
-
-********************************************
-********************************************
-********************************************
-********************************************
-********************************************
-********************************************
-********************************************
-********************************************
-********************************************
-********************************************
-********************************************
-*/
-
-//
-// var myAsyncMock;
-// var schemaDynamicTest;
-//
-// before(function () {
-// 	var myArgumentMock = function (input, cb) {
-// 		return cb([
-// 			{
-// 				message: 'Expected type object but found type string'
-// 			}
-// 		]);
-// 	};
-// 	myAsyncMock = myArgumentMock;
-//
-// 	schemaDynamicTest = new SchemaDynamicTest({
-// 		customInput: 'shghdjasvhjasdjhvdasjhvads'
-// 	});
-//
-// 	describe('schema', function () {
-// 		schemaDynamicTest.schema.shouldFailAgainst.nonObject.arguments(myAsyncMock);
-// 	});
-//
-// });
-
-//
-// describe('dupa', function () {
-// });
-
-
-
-//
-//
-//
-//
-// var myParameterMock = function (input, cb) {
-// 	return cb([
-// 		{
-// 			message: 'Expected type string but found type string'
-// 		}
-// 	]);
-// };
-//
-// schemaDynamicTest = new SchemaDynamicTest({
-// 	testedFunction: myParameterMock
-// });
-//
-// var myArg = {
-// 	a: 'A'
-// };
-//
-// schemaDynamicTest.schema.shouldFailAgainst.nonString.property(myParameterMock, myArg, 'a');
-//
-//
-// var myMissingPropMock = function (input, cb) {
-// 	return cb([
-// 		{
-// 			message: 'Missing required property: a'
-// 		}
-// 	]);
-// };
-//
-// schemaDynamicTest = new SchemaDynamicTest({
-// 	testedFunction: myMissingPropMock
-// });
-//
-// var myArg = {
-// 	a: 'A',
-// 	b: 'B',
-// 	c: 'c'
-// };
-//
-// schemaDynamicTest.schema.shouldFailWithoutRequiredProperties(myMissingPropMock, myArg, ['a']);
