629c3286a8f9cc0a98d906665c365c0223c1c6e1
willclarktech <willclarktech@users.noreply.github.com>
2017-08-25 18:23:27 +0200

    Update encrypt command tests for passphrase file

--- test/commands/encrypt.js
+++ test/commands/encrypt.js
@@ -13,6 +13,7 @@
  * Removal or modification of this copyright notice is prohibited.
  *
  */
+import fse from 'fs-extra';
 import encrypt from '../../src/commands/encrypt';
 import cryptoModule from '../../src/utils/cryptoModule';
 import tablify from '../../src/utils/tablify';
@@ -40,12 +41,12 @@ describe('lisky encrypt command palette', () => {
 			(encryptCommands).should.have.length(1);
 		});
 
-		it('should require 3 inputs', () => {
+		it('should require 2 inputs', () => {
 			const argsFilter = arg => arg.required;
 			const encryptCommand = vorpal.commands.filter(commandFilter)[0];
 			// eslint-disable-next-line no-underscore-dangle
 			const requiredArgs = encryptCommand._args.filter(argsFilter);
-			(requiredArgs).should.have.length(3);
+			(requiredArgs).should.have.length(2);
 		});
 	});
 
@@ -53,7 +54,14 @@ describe('lisky encrypt command palette', () => {
 		const message = 'Hello Lisker';
 		const secret = 'pass phrase';
 		const recipient = 'bba7e2e6a4639c431b68e31115a71ffefcb4e025a4d1656405dfdcd8384719e0';
-		const command = `encrypt "${message}" "${secret}" "${recipient}"`;
+		const command = `encrypt "${message}" "${recipient}"`;
+		const jsonCommand = `${command} --json`;
+		const jCommand = `${command} -j`;
+		const noJsonCommand = `${command} --no-json`;
+		const passPhraseFileCommand = `${command} --passphrase-file ~/path/to/secret.txt`;
+		const passPhraseFileJsonCommand = `${passPhraseFileCommand} --json`;
+		const passPhraseFileJCommand = `${passPhraseFileCommand} --j`;
+		const passPhraseFileNoJsonCommand = `${passPhraseFileCommand} --no-json`;
 
 		const nonce = '60ee6cbb5f9f0ee3736a6ffd20317f59ebfee2083e819909';
 		const encryptedMessage = '4ba04a1c568b66fe5f6e670295cd9945730013f4e3feb5ac0b4e3c';
@@ -76,35 +84,144 @@ describe('lisky encrypt command palette', () => {
 			encryptStub.restore();
 		});
 
-		it('should handle valid parameters', () => {
-			return vorpal.exec(command)
-				.then(() => {
-					(encryptStub.calledWithExactly(message, secret, recipient))
-						.should.be.true();
+		describe('with prompt for password', () => {
+			let promptStub;
+
+			beforeEach(() => {
+				promptStub = sinon.stub(vorpal, 'prompt').resolves({ secret });
+			});
+
+			afterEach(() => {
+				promptStub.restore();
+			});
+
+			it('should call the crypto module encrypt method with correct parameters', () => {
+				return vorpal.exec(command)
+					.then(() => (encryptStub.calledWithExactly(message, secret, recipient))
+						.should.be.true(),
+					);
+			});
+
+			describe('output', () => {
+				it('should print the returned object', () => {
+					return vorpal.exec(command)
+						.then(() => (capturedOutput[0]).should.equal(tableOutput));
 				});
+
+				it('should print json with --json option', () => {
+					return vorpal.exec(jsonCommand)
+						.then(() => (capturedOutput[0]).should.equal(jsonOutput));
+				});
+
+				it('should handle a -j shorthand for --json option', () => {
+					return vorpal.exec(jCommand)
+						.then(() => (capturedOutput[0]).should.equal(jsonOutput));
+				});
+
+				it('should print a table with --no-json option', () => {
+					return vorpal.exec(noJsonCommand)
+						.then(() => (capturedOutput[0]).should.equal(tableOutput));
+				});
+			});
 		});
 
-		it('should print the returned object', () => {
-			return vorpal.exec(command)
-				.then(() => (capturedOutput[0]).should.equal(tableOutput));
-		});
+		describe('with passphrase file passed as option', () => {
+			let readFileStub;
 
-		it('should print json with --json option', () => {
-			const jsonCommand = `${command} --json`;
-			return vorpal.exec(jsonCommand)
-				.then(() => (capturedOutput[0]).should.equal(jsonOutput));
-		});
+			describe('if file does not exist', () => {
+				beforeEach(() => {
+					readFileStub = sinon.stub(fse, 'readFile').callsArgWith(1, new Error('ENOENT: no such file or directory'));
+					return vorpal.exec(passPhraseFileCommand);
+				});
 
-		it('should handle a -j shorthand for --json option', () => {
-			const jCommand = `${command} -j`;
-			return vorpal.exec(jCommand)
-				.then(() => (capturedOutput[0]).should.equal(jsonOutput));
-		});
+				afterEach(() => {
+					readFileStub.restore();
+				});
 
-		it('should print a table with --no-json option', () => {
-			const noJsonCommand = `${command} --no-json`;
-			return vorpal.exec(noJsonCommand)
-				.then(() => (capturedOutput[0]).should.equal(tableOutput));
+				it('should inform the user that the file does not exist', () => {
+					(capturedOutput[0]).should.equal('Could not encrypt: passphrase file does not exist.');
+				});
+
+				it('should not call the crypto module encrypt method', () => {
+					(encryptStub.called).should.be.false();
+				});
+			});
+
+			describe('if file cannot be read', () => {
+				beforeEach(() => {
+					readFileStub = sinon.stub(fse, 'readFile').callsArgWith(1, new Error('EACCES: permission denied'));
+					return vorpal.exec(passPhraseFileCommand);
+				});
+
+				afterEach(() => {
+					readFileStub.restore();
+				});
+
+				it('should inform the user that the file cannot be read', () => {
+					(capturedOutput[0]).should.equal('Could not encrypt: passphrase file could not be read.');
+				});
+
+				it('should not call the crypto module encrypt method', () => {
+					(encryptStub.called).should.be.false();
+				});
+			});
+
+			describe('if an unexpected error occurs', () => {
+				const unknownError = new Error('unknown error');
+
+				beforeEach(() => {
+					readFileStub = sinon.stub(fse, 'readFile').callsArgWith(1, unknownError);
+				});
+
+				afterEach(() => {
+					readFileStub.restore();
+				});
+
+				it('should throw an error', () => {
+					return vorpal.exec(passPhraseFileCommand)
+						.then(result => (result).should.fail(), error => (error).should.equal(unknownError));
+				});
+			});
+
+			describe('if file can be read', () => {
+				beforeEach(() => {
+					readFileStub = sinon.stub(fse, 'readFile').callsArgWith(1, null, `${secret}\n`);
+				});
+
+				afterEach(() => {
+					readFileStub.restore();
+				});
+
+				it('should call the crypto module encrypt method with correct parameters', () => {
+					return vorpal.exec(passPhraseFileCommand)
+						.then(() => {
+							(encryptStub.calledWithExactly(message, secret, recipient))
+								.should.be.true();
+						});
+				});
+
+				describe('output', () => {
+					it('should print the returned object', () => {
+						return vorpal.exec(passPhraseFileCommand)
+							.then(() => (capturedOutput[0]).should.equal(tableOutput));
+					});
+
+					it('should print json with --json option', () => {
+						return vorpal.exec(passPhraseFileJsonCommand)
+							.then(() => (capturedOutput[0]).should.equal(jsonOutput));
+					});
+
+					it('should handle a -j shorthand for --json option', () => {
+						return vorpal.exec(passPhraseFileJCommand)
+							.then(() => (capturedOutput[0]).should.equal(jsonOutput));
+					});
+
+					it('should print a table with --no-json option', () => {
+						return vorpal.exec(passPhraseFileNoJsonCommand)
+							.then(() => (capturedOutput[0]).should.equal(tableOutput));
+					});
+				});
+			});
 		});
 	});
 });
