aba51265ad6d5d12e2e19fbda6182079e0b6e7a1
Usman Khan <usman@lightcurve.io>
2017-08-29 12:16:44 +0200

    fixed internal api schema check for dapps, and added its tests

---
+++ test/unit/schema/dapp.js
@@ -0,0 +1,41 @@
+var ZSchema = require('../../../helpers/z_schema.js');
+var schema = require('../../../schema/dapps.js');
+var expect = require('chai').expect;
+
+var validator = new ZSchema();
+
+describe('dapp', function () {
+
+	describe('launch', function () {
+		var testBody;
+
+		beforeEach(function () {
+			testBody = {
+				params: ['-x', 'localhost'],
+				id: '1465651642158264047',
+				master: 'pluto'
+			};
+		});
+
+		it('should return error when params field is not an array', function () {
+			testBody.params = '';
+			validator.validate(testBody, schema.launch);
+			expect(validator.getLastErrors().map(function (e) {
+				return e.message;
+			})).to.eql(['Expected type array but found type string']);
+		});
+
+		it('should return error when params field length is less than minimum length', function () {
+			testBody.params = [];
+			validator.validate(testBody, schema.launch);
+			expect(validator.getLastErrors().map(function (e) {
+				return e.message;
+			})).to.eql(['Array is too short (0), minimum 1']);
+		});
+
+		it('should be ok when params field length valid', function () {
+			validator.validate(testBody, schema.launch);
+			expect(validator.getLastErrors()).to.not.exist;
+		});
+	});
+});
