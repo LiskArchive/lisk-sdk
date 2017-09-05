7c4c5828c291f550d74dedc514fcf0f0b600d852
Tobias Schwarz <tobias@lightcurve.io>
2017-09-05 11:39:20 +0200

    Newline on yuidoc documentation for eslint max-len

--- src/crypto/sign.js
+++ src/crypto/sign.js
@@ -327,7 +327,8 @@ function encryptAES256CBCWithPassword(passphrase, password) {
 /**
  * @method decryptAES256CBCWithPassword
  * @param {Object} cipherAndNonce.
- * The cipher text resulting from the AES-256-CBC encryption, including the nonce { cipher: ..., nonce: ..., }
+ * The cipher text resulting from the AES-256-CBC encryption,
+ * including the nonce { cipher: ..., nonce: ..., }
  * @param {String} password utf8 - the password used to encrypt the passphrase
  *
  * @return {String}
@@ -358,7 +359,8 @@ export function encryptPassphraseWithPassword(passphrase, password) {
 /**
  * @method decryptPassphraseWithPassword
  * @param {String} cipherAndNonce.
- * The cipher text resulting from the AES-256-CBC encryption, including the nonce { cipher: ..., nonce: ..., }
+ * The cipher text resulting from the AES-256-CBC encryption,
+ * including the nonce { cipher: ..., nonce: ..., }
  * @param {String} password utf8 - the password used to encrypt the passphrase
  *
  * @return {String}
