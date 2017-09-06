5d9cbdecac973d68b7b53fb9378482a4b9b3d9b5
reasonthearchitect <rhys.personal@gmail.com>
2017-09-05 21:00:59 -0400

    New unit tests for helpers/config.js and helpers/checkIpInList.js

--- test/unit/helpers/config.js
+++ test/unit/helpers/config.js
@@ -1,23 +1,25 @@
 'use strict';
 
-var chai          = require('chai');
-var expect        = require('chai').expect;
-var sinon         = require('sinon');
+var chai = require('chai');
+var expect = require('chai').expect;
+var sinon = require('sinon');
 
 var config = require('../../../helpers/config');
-var argvclone = JSON.parse(JSON.stringify(process.argv));
+
+var argvcloneStr = JSON.stringify(process.argv);
 
 describe('config', function () {
 
+	const VERSION = '0.0.0a';
+
 	afterEach(() => {
-		process.argv = argvclone;
-		argvclone = JSON.parse(JSON.stringify(process.argv));
+		process.argv = JSON.parse(argvcloneStr);
 	});
 
 	it('should load the referenced config file double dash', function () {
 		process.argv.push('--config');
 		process.argv.push('./test/unit/helpers/config.mock.data.json');
-		var appconfig = config({version: '0.0.0a'});
+		var appconfig = config({version: VERSION});
 		expect(appconfig).to.not.be.null;
 		expect(appconfig.port).to.eq(1234);
 		expect(appconfig.httpPort).to.be.eq(4321);
@@ -32,7 +34,7 @@ describe('config', function () {
 	it('should override the port with simple dash', function () {
 		process.argv.push('-p');
 		process.argv.push('9999');
-		var appconfig = config({version: '0.0.0a'});
+		var appconfig = config({version: VERSION});
 		expect(appconfig).to.not.be.null;
 		expect(appconfig.port).to.eq(9999);
 	});
@@ -40,7 +42,7 @@ describe('config', function () {
 	it('should override the port with double dash', function () {
 		process.argv.push('--port');
 		process.argv.push('9998');
-		var appconfig = config({version: '0.0.0a'});
+		var appconfig = config({version: VERSION});
 		expect(appconfig).to.not.be.null;
 		expect(appconfig.port).to.eq(9998);
 	});
@@ -48,7 +50,7 @@ describe('config', function () {
 	it('should override the httpport with simple dash', function () {
 		process.argv.push('-h');
 		process.argv.push('1999');
-		var appconfig = config({version: '0.0.0a'});
+		var appconfig = config({version: VERSION});
 		expect(appconfig).to.not.be.null;
 		expect(appconfig.httpPort).to.eq(1999);
 	});
@@ -56,7 +58,7 @@ describe('config', function () {
 	it('should override the database name', function () {
 		process.argv.push('--database');
 		process.argv.push('unittestdb');
-		var appconfig = config({version: '0.0.0a'});
+		var appconfig = config({version: VERSION});
 		expect(appconfig).to.not.be.null;
 		expect(appconfig.db.database).to.eq('unittestdb');
 	});
@@ -64,23 +66,23 @@ describe('config', function () {
 	it('should override the ipaddress with simple dash', function () {
 		process.argv.push('-a');
 		process.argv.push('198.0.0.1');
-		var appconfig = config({version: '0.0.0a'});
+		var appconfig = config({version: VERSION});
 		expect(appconfig).to.not.be.null;
 		expect(appconfig.address).to.eq('198.0.0.1');
 	});
 
 	it('should override the ipaddress with double dash', function () {
 		process.argv.push('--address');
-		process.argv.push('198.0.0.1');
-		var appconfig = config({version: '0.0.0a'});
+		process.argv.push('198.0.0.2');
+		var appconfig = config({version: VERSION});
 		expect(appconfig).to.not.be.null;
-		expect(appconfig.address).to.eq('198.0.0.1');
+		expect(appconfig.address).to.eq('198.0.0.2');
 	});
 
 	it('should override the log with simple dash', function () {
 		process.argv.push('-l');
 		process.argv.push('UNIT_TEST_ORG');
-		var appconfig = config({version: '0.0.0a'});
+		var appconfig = config({version: VERSION});
 		expect(appconfig).to.not.be.null;
 		expect(appconfig.consoleLogLevel).to.eq('UNIT_TEST_ORG');
 	});
@@ -88,7 +90,7 @@ describe('config', function () {
 	it('should override the log with double dash', function () {
 		process.argv.push('--log');
 		process.argv.push('UNIT_TEST_ORG');
-		var appconfig = config({version: '0.0.0a'});
+		var appconfig = config({version: VERSION});
 		expect(appconfig).to.not.be.null;
 		expect(appconfig.consoleLogLevel).to.eq('UNIT_TEST_ORG');
 	});
@@ -96,7 +98,7 @@ describe('config', function () {
 	it('should override the snapshot with double dash', function () {
 		process.argv.push('--snapshot');
 		process.argv.push('1');
-		var appconfig = config({version: '0.0.0a'});
+		var appconfig = config({version: VERSION});
 		expect(appconfig).to.not.be.null;
 		expect(appconfig.loading.snapshot).to.eq(1);
 	});
@@ -104,7 +106,7 @@ describe('config', function () {
 	it('should override the peers with double dash', function () {
 		process.argv.push('--peers');
 		process.argv.push('127.0.0.1:4008,127.0.0.2:6789');
-		var appconfig = config({version: '0.0.0a'});
+		var appconfig = config({version: VERSION});
 		expect(appconfig).to.not.be.null;
 		expect(appconfig.peers.list.length).to.eq(2);
 		expect(appconfig.peers.list[0].ip).to.eq('127.0.0.1');
@@ -118,11 +120,11 @@ describe('config', function () {
 		process.argv.push('-p');
 		process.argv.push('9997');
 		process.argv.push('--peers');
-		process.argv.push('127.0.0.2');
-		var appconfig = config({version: '0.0.0a'});
+		process.argv.push('127.0.0.19');
+		var appconfig = config({version: VERSION});
 		expect(appconfig).to.not.be.null;
 		expect(appconfig.peers.list.length).to.eq(1);
-		expect(appconfig.peers.list[0].ip).to.eq('127.0.0.2');
+		expect(appconfig.peers.list[0].ip).to.eq('127.0.0.19');
 		expect(appconfig.peers.list[0].port).to.eq(9997);
 	});
 });
