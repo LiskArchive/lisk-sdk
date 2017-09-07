d04037ea25f5cffb93556e2a5b06ed853da7056e
reasonthearchitect <rhys.personal@gmail.com>
2017-09-07 17:03:53 -0400

    Updated the code based on the comments recieved by SargeKhan.

--- test/unit/helpers/cache.js
+++ test/unit/helpers/cache.js
@@ -9,7 +9,36 @@ var cache = require('../../../helpers/cache');
 
 describe('cache', function () {
 
+	var ERROR = new Error('Cache Test Callback Error');
+
+	var mockedLogger = {
+		'info': function (info) {expect(info).to.eq('App connected with redis server');},
+		'error': function (message, err) {
+			expect(message).to.eq('Redis:');
+			expect(err).to.eql(ERROR);
+		}
+	};
+
+	var mockRedisClientReady = {
+		'on': function (type, cb) {
+			if ( 'ready' === type ) {
+				cb();
+			}
+		}
+	};
+
+	var mockedRedis;
+
+	beforeEach( function () {
+		mockedRedis = sinon.mock(redis);
+	});
+
+	afterEach( function () {
+		mockedRedis.verify();
+	});
+
 	it('is not enabled', function () {
+		mockedRedis.expects('createClient').never();
 		cache.connect(false, {}, {}, function (err, result) {
 			expect(result.cacheEnabled).to.eq( false );
 			expect(result.client).to.be.null;
@@ -17,84 +46,39 @@ describe('cache', function () {
 	});
 
 	it('is enabled redis client callback ready', function () {
-
-		var logger = {
-			'info': function (info) {expect(info).to.eq('App connected with redis server');}
-		};
-
-		var mockredisclient = {
-			'on': function (type, cb) {
-				if ( 'ready' === type ) {
-					cb();
-				}
-			}
-		};
-
-		var mockedRedis = sinon.mock(redis);
-		mockedRedis.expects('createClient').once().withArgs(sinon.match.any).returns(mockredisclient);
-		cache.connect(true, {}, logger, function (err, result) {
+		mockedRedis.expects('createClient').once().withArgs(sinon.match.any).returns(mockRedisClientReady);
+		cache.connect(true, {}, mockedLogger, function (err, result) {
 			expect(result.cacheEnabled).to.eq( true );
-			expect(result.client).to.be.not.null;
-
-			mockedRedis.restore();
-			mockedRedis.verify();
+			expect(result.client).to.eql(mockRedisClientReady);
 		});
 	});
 
 	it('is enabled redis client callback error', function () {
-
-		var logger = {
-			'error': function (message, err) {
-				expect(message).to.eq('Redis:');
-				expect(err + '').to.eq('Error: Cache Test Callback Error');
-
-			}
-		};
-
-		var mockredisclient = {
+		// note that this cannot be integrated with the declared scope mock client.
+		// This is because both ready and error are called by the client under Test
+		// so test results will not pass.
+		var mockRedisClientError = {
 			'on': function (type, cb) {
 				if ( 'error' === type ) {
-					cb(new Error('Cache Test Callback Error'));
+					cb(ERROR);
 				}
 			}
 		};
-
-		var mockedRedis = sinon.mock(redis);
-		mockedRedis.expects('createClient').once().withArgs(sinon.match.any).returns(mockredisclient);
-		cache.connect(true, {}, logger, function (err, result) {
+		mockedRedis.expects('createClient').once().withArgs(sinon.match.any).returns(mockRedisClientError);
+		cache.connect(true, {}, mockedLogger, function (err, result) {
 			expect(result.cacheEnabled).to.eq( true );
 			expect(result.client).to.be.null;
-
-			mockedRedis.restore();
-			mockedRedis.verify();
 		});
 	});
 
 	it('deletes the password if it is null', function () {
-
-		var logger = {
-			'info': function (info) {expect(info).to.eq('App connected with redis server');}
-		};
-
-		var mockredisclient = {
-			'on': function (type, cb) {
-				if ( 'ready' === type ) {
-					cb();
-				}
-			}
-		};
-
 		var config = {password: null};
 		expect(config).to.have.property('password');
-		var mockedRedis = sinon.mock(redis);
-		mockedRedis.expects('createClient').once().withArgs(sinon.match.any).returns(mockredisclient);
-		cache.connect(true, config, logger, function (err, result) {
+		mockedRedis.expects('createClient').once().withArgs(sinon.match.any).returns(mockRedisClientReady);
+		cache.connect(true, config, mockedLogger, function (err, result) {
 			expect(result.cacheEnabled).to.eq( true );
-			expect(result.client).to.be.not.null;
+			expect(result.client).to.eql(mockRedisClientReady);
 			expect(config).to.not.have.property('password');
-
-			mockedRedis.restore();
-			mockedRedis.verify();
 		});
 	});
 });
