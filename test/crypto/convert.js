df548b2b3fa04fa1cf9c30625a06df9a1a87ac8b
willclarktech <willclarktech@users.noreply.github.com>
2017-09-07 17:22:00 +0200

    :white_check_mark: Improve crypto convert tests

--- test/crypto/convert.js
+++ test/crypto/convert.js
@@ -24,119 +24,120 @@ import {
 	convertPrivateKeyEd2Curve,
 } from '../../src/crypto/convert';
 
-import {
-	getRawPrivateAndPublicKeyFromSecret,
-	getKeys,
-} from '../../src/crypto/keys';
+const hash = require('../../src/crypto/hash');
+const utils = require('../../src/transactions/utils');
 
 describe('convert', () => {
-	const defaultSecret = 'secret';
-	const defaultBuffer = naclInstance.encode_utf8('\xe5\xe4\xf6');
-	const defaultHex = 'c3a5c3a4c3b6';
+	// keys for secret 'secret';
+	const defaultPrivateKey = '2bb80d537b1da3e38bd30361aa855686bde0eacd7162fef6a25fe97bf527a25b5d036a858ce89f844491762eb89e2bfbd50a4a0a0da658e4b2628b25b117ae09';
+	const defaultPublicKey = '5d036a858ce89f844491762eb89e2bfbd50a4a0a0da658e4b2628b25b117ae09';
+	const defaultPublicKeyHash = Buffer.from('3a971fd02b4a07fc20aad1936d3cb1d263b96e0ffd938625e5c0db1ad8ba2a29', 'hex');
+	const defaultPrivateKeyCurve = Buffer.from('6073c8f6198112b558bb5a98d150f3a0e35fb2b7a9c192cae1bbf37752df1950', 'hex');
+	const defaultPublicKeyCurve = Buffer.from('d4e56ce5d0c7e2d4a9f05813ba37882985ee13a3f511bc6f99b905b2f87cdf11', 'hex');
 	const defaultAddress = '18160565574430594874L';
-	const expectedPublicKey = '5d036a858ce89f844491762eb89e2bfbd50a4a0a0da658e4b2628b25b117ae09';
+	const defaultBuffer = Buffer.from('\xe5\xe4\xf6');
+	const defaultHex = 'c3a5c3a4c3b6';
+	const defaultTransactionId = '13987348420913138422';
+	const defaultTransactionHash = Buffer.from('f60a26da470b1dc233fd526ed7306c1d84836f9e2ecee82c9ec47319e0910474', 'hex');
+	const defaultStringWithMoreThanEightCharacters = '0123456789';
+	const defaultFirstEightCharactersReversed = '76543210';
+	const defaultDataForBuffer = 'Hello!';
+	const defaultAddressFromBuffer = '79600447942433L';
+	const defaultAmount = 1000;
+	const defaultTimestamp = 141738;
+	const defaultRecipientId = '58191285901858109L';
+	const defaultSignature = '618a54975212ead93df8c881655c625544bce8ed7ccdfe6f08a42eecfb1adebd051307be5014bb051617baf7815d50f62129e70918190361e5d4dd4796541b0a';
 
 	describe('#bufferToHex', () => {
-		it('should create Hex from Buffer type', () => {
+		it('should create a hex string from a Buffer', () => {
 			const hex = bufferToHex(defaultBuffer);
 			(hex).should.be.equal(defaultHex);
 		});
 	});
 
 	describe('#hexToBuffer', () => {
-		it('should create Buffer from Hex type', () => {
+		it('should create a Buffer from a hex string', () => {
 			const buffer = hexToBuffer(defaultHex);
-			(naclInstance.decode_utf8(buffer)).should.be.equal('åäö');
+			(buffer).should.be.eql(defaultBuffer);
 		});
 	});
 
 	describe('#getFirstEightBytesReversed', () => {
-		it('should use a Buffer, cut after first 8 entries and reverse them', () => {
-			const bufferEntry = Buffer.from('0123456789');
+		it('should get the first eight bytes reversed from a Buffer', () => {
+			const bufferEntry = Buffer.from(defaultStringWithMoreThanEightCharacters);
 			const reversedAndCut = getFirstEightBytesReversed(bufferEntry);
-			(reversedAndCut).should.be.eql(Buffer.from('76543210'));
+			(reversedAndCut).should.be.eql(Buffer.from(defaultFirstEightCharactersReversed));
+		});
+
+		it('should get the first eight bytes reversed from a string', () => {
+			const reversedAndCut = getFirstEightBytesReversed(defaultStringWithMoreThanEightCharacters);
+			(reversedAndCut).should.be.eql(Buffer.from(defaultFirstEightCharactersReversed));
 		});
 	});
 
 	describe('#toAddress', () => {
-		const bufferInit = Buffer.from('Hello!');
-		const address = toAddress(bufferInit);
-
-		(address).should.be.eql('79600447942433L');
-	});
-
-	describe('#getId', () => {
-		it('should be ok', () => {
-			(getId).should.be.ok();
-		});
-
-		it('should be a function', () => {
-			(getId).should.be.type('function');
-		});
-
-		it('should return string id and be equal to 13987348420913138422', () => {
-			const transaction = {
-				type: 0,
-				amount: 1000,
-				recipientId: '58191285901858109L',
-				timestamp: 141738,
-				asset: {},
-				senderPublicKey: '5d036a858ce89f844491762eb89e2bfbd50a4a0a0da658e4b2628b25b117ae09',
-				signature: '618a54975212ead93df8c881655c625544bce8ed7ccdfe6f08a42eecfb1adebd051307be5014bb051617baf7815d50f62129e70918190361e5d4dd4796541b0a',
-			};
-
-			const id = getId(transaction);
-			(id).should.be.type('string').and.equal('13987348420913138422');
+		it('should create an address from a buffer', () => {
+			const bufferInit = Buffer.from(defaultDataForBuffer);
+			const address = toAddress(bufferInit);
+			(address).should.be.eql(defaultAddressFromBuffer);
 		});
 	});
 
 	describe('#getAddressFromPublicKey', () => {
-		const address = getAddressFromPublicKey(expectedPublicKey);
+		beforeEach(() => {
+			sandbox.stub(hash, 'getSha256Hash').returns(defaultPublicKeyHash);
+		});
 
 		it('should generate address from publicKey', () => {
+			const address = getAddressFromPublicKey(defaultPublicKey);
 			(address).should.be.equal(defaultAddress);
 		});
 	});
 
 	describe('#getAddress', () => {
-		it('should be ok', () => {
-			(getAddress).should.be.ok();
+		beforeEach(() => {
+			sandbox.stub(hash, 'getSha256Hash').returns(defaultPublicKeyHash);
 		});
 
-		it('should be a function', () => {
-			(getAddress).should.be.type('function');
-		});
-
-		it('should generate address by publicKey', () => {
-			const keys = getKeys(defaultSecret);
-			const address = getAddress(keys.publicKey);
-
-			(address).should.be.ok();
-			(address).should.be.type('string');
-			(address).should.be.equal('18160565574430594874L');
+		it('should generate address from publicKey', () => {
+			const address = getAddress(defaultPublicKey);
+			(address).should.be.equal(defaultAddress);
 		});
 	});
 
+	describe('#getId', () => {
+		beforeEach(() => {
+			sandbox.stub(hash, 'getSha256Hash').returns(defaultTransactionHash);
+			sandbox.stub(utils, 'getTransactionBytes');
+		});
+
+		it('should return an id of 13987348420913138422 for a transaction', () => {
+			const transaction = {
+				type: 0,
+				amount: defaultAmount,
+				recipientId: defaultRecipientId,
+				timestamp: defaultTimestamp,
+				asset: {},
+				senderPublicKey: defaultPublicKey,
+				signature: defaultSignature,
+			};
+			const id = getId(transaction);
+
+			(id).should.be.equal(defaultTransactionId);
+		});
+	});
 
 	describe('#convertPublicKeyEd2Curve', () => {
-		const keyPair = getRawPrivateAndPublicKeyFromSecret('123');
-
 		it('should convert publicKey ED25519 to Curve25519 key', () => {
-			let curveRepresentation = convertPublicKeyEd2Curve(keyPair.publicKey);
-			curveRepresentation = bufferToHex(curveRepresentation);
-
-			(curveRepresentation).should.be.equal('f65170b330e5ae94fe6372e0ff8b7c709eb8dfe78c816ffac94e7d3ed1729715');
+			const curveRepresentation = convertPublicKeyEd2Curve(defaultPublicKey);
+			(defaultPublicKeyCurve.equals(Buffer.from(curveRepresentation))).should.be.true();
 		});
 	});
 
 	describe('#convertPrivateKeyEd2Curve', () => {
-		const keyPair = getRawPrivateAndPublicKeyFromSecret('123');
-
 		it('should convert privateKey ED25519 to Curve25519 key', () => {
-			let curveRepresentation = convertPrivateKeyEd2Curve(keyPair.privateKey);
-			curveRepresentation = bufferToHex(curveRepresentation);
-
-			(curveRepresentation).should.be.equal('a05621ba2d3f69f054abb1f3c155338bb44ec8b718928cf9d5b206bafd364356');
+			const curveRepresentation = convertPrivateKeyEd2Curve(defaultPrivateKey);
+			(defaultPrivateKeyCurve.equals(Buffer.from(curveRepresentation))).should.be.true();
 		});
 	});
 });
