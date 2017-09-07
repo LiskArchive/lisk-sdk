112140ef96d9ef6881744a943f3eac233834681f
reasonthearchitect <rhys.personal@gmail.com>
2017-09-07 00:10:09 -0400

    Adding the test/helpers/router.js file to the test/unit/router.js file.

--- test/unit/index.js
+++ test/unit/index.js
@@ -14,6 +14,7 @@ require('./helpers/jobs-queue');
 require('./helpers/peersManager');
 require('./helpers/pg-notify');
 require('./helpers/request-limiter');
+require('./helpers/router');
 require('./helpers/RPC');
 require('./helpers/slots');
 require('./helpers/RPC');
