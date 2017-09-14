341e4f872d9bc0edc381199bb131b23b0d4bf5b9
Usman Khan <usman@lightcurve.io>
2017-09-14 18:45:57 +0200

    Removed usage of `this` var in outTransfer logic

--- logic/outTransfer.js
+++ logic/outTransfer.js
@@ -31,12 +31,10 @@ function OutTransfer (db, schema, logger) {
 /**
  * Binds input modules to private variable module.
  * @param {Accounts} accounts
- * @param {Dapps} dapps
  */
-OutTransfer.prototype.bind = function (accounts, dapps) {
+OutTransfer.prototype.bind = function (accounts) {
 	modules = {
 		accounts: accounts,
-		dapps: dapps,
 	};
 };
 
@@ -261,7 +259,7 @@ OutTransfer.prototype.objectNormalize = function (trs) {
 	var report = library.schema.validate(trs.asset.outTransfer, OutTransfer.prototype.schema);
 
 	if (!report) {
-		throw 'Failed to validate outTransfer schema: ' + this.scope.schema.getLastErrors().map(function (err) {
+		throw 'Failed to validate outTransfer schema: ' + library.schema.getLastErrors().map(function (err) {
 			return err.message;
 		}).join(', ');
 	}
