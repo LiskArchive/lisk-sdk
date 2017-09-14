0e9343ce01d82ec9efb4a53c748dcd4c046024f8
willclarktech <willclarktech@users.noreply.github.com>
2017-09-14 10:13:37 +0200

    Complete tests for input utils

--- src/utils/input.js
+++ src/utils/input.js
@@ -13,15 +13,54 @@
  * Removal or modification of this copyright notice is prohibited.
  *
  */
-export const ERROR_PASSPHRASE_VERIFICATION_FAIL = 'Passphrase verification failed.';
+import readline from 'readline';
+import fse from 'fs-extra';
 
-const createPromptOptions = message => ({
+export const ERROR_PASSPHRASE_VERIFICATION_FAIL = 'Passphrase verification failed.';
+const ERROR_PASSPHRASE_SOURCE_TYPE_UNKNOWN = 'Unknown passphrase source type. Must be one of `env`, `file`, or `stdin`. Leave blank for prompt.';
+const ERROR_PASSPHRASE_ENV_VARIABLE_NOT_SET = 'Passphrase environmental variable not set.';
+const ERROR_FILE_DOES_NOT_EXIST = 'File does not exist.';
+const ERROR_FILE_UNREADABLE = 'File could not be read.';
+
+export const splitSource = (source) => {
+	const delimiter = ':';
+	const sourceParts = source.split(delimiter);
+	return {
+		sourceType: sourceParts[0],
+		sourceIdentifier: sourceParts.slice(1).join(delimiter),
+	};
+};
+
+export const getStdIn = ({ dataIsRequired, passphraseIsRequired }) => new Promise((resolve) => {
+	if (!dataIsRequired && !passphraseIsRequired) return resolve({});
+
+	const lines = [];
+	const rl = readline.createInterface({ input: process.stdin });
+
+	const handleLine = line => (
+		dataIsRequired
+			? lines.push(line)
+			: resolve({ passphrase: line }) || rl.close()
+	);
+	const handleClose = () => {
+		const messageLines = passphraseIsRequired ? lines.slice(1) : lines;
+		return resolve({
+			data: dataIsRequired ? messageLines.join('\n') : null,
+			passphrase: passphraseIsRequired ? lines[0] : null,
+		});
+	};
+
+	return rl
+		.on('line', handleLine)
+		.on('close', handleClose);
+});
+
+export const createPromptOptions = message => ({
 	type: 'password',
 	name: 'passphrase',
 	message,
 });
 
-// eslint-disable-next-line import/prefer-default-export
 export const getPassphraseFromPrompt = (vorpal) => {
 	// IMPORTANT: prompt will exit if UI has no parent, but calling
 	// ui.attach(vorpal) will start a prompt, which will complain when we call
@@ -39,4 +78,61 @@ export const getPassphraseFromPrompt = (vorpal) => {
 				return passphrase;
 			}),
 		);
+};
+
+export const getPassphraseFromEnvVariable = async (key) => {
+	const passphrase = process.env[key];
+	if (!passphrase) {
+		throw new Error(ERROR_PASSPHRASE_ENV_VARIABLE_NOT_SET);
+	}
+	return passphrase;
+};
+
+
+export const getPassphraseFromFile = path => new Promise((resolve, reject) => {
+	const stream = fse.createReadStream(path);
+	const handleReadError = (error) => {
+		stream.close();
+		const { message } = error;
+
+		if (message.match(/ENOENT/)) {
+			return reject(new Error(ERROR_FILE_DOES_NOT_EXIST));
+		}
+		if (message.match(/EACCES/)) {
+			return reject(new Error(ERROR_FILE_UNREADABLE));
+		}
+
+		return reject(error);
+	};
+	const handleLine = (line) => {
+		stream.close();
+		resolve(line);
+	};
+
+	stream.on('error', handleReadError);
+
+	readline.createInterface({ input: stream })
+		.on('error', handleReadError)
+		.on('line', handleLine);
+});
+
+export const getPassphraseFromSource = async (source) => {
+	const { sourceType, sourceIdentifier } = splitSource(source);
+
+	switch (sourceType) {
+	case 'env':
+		return getPassphraseFromEnvVariable(sourceIdentifier);
+	case 'file':
+		return getPassphraseFromFile(sourceIdentifier);
+	case 'pass':
+		return sourceIdentifier;
+	default:
+		throw new Error(ERROR_PASSPHRASE_SOURCE_TYPE_UNKNOWN);
+	}
+};
+
+export const getPassphrase = async (vorpal, passphraseSource, { passphrase } = {}) => {
+	if (passphrase) return passphrase;
+	if (!passphraseSource) return getPassphraseFromPrompt(vorpal);
+	return getPassphraseFromSource(passphraseSource);
 };
--- test/utils/input.js
+++ test/utils/input.js
@@ -13,16 +13,39 @@
  * Removal or modification of this copyright notice is prohibited.
  *
  */
+import readline from 'readline';
+import fse from 'fs-extra';
 import {
+	splitSource,
+	getStdIn,
+	createPromptOptions,
 	getPassphraseFromPrompt,
+	getPassphraseFromEnvVariable,
+	getPassphraseFromFile,
+	getPassphraseFromSource,
+	getPassphrase,
 } from '../../src/utils/input';
 
-describe('prompt utils', () => {
+const createStreamStub = on => ({
+	resume: () => {},
+	close: () => {},
+	on,
+});
+
+describe('input utils', () => {
 	const passphrase = 'minute omit local rare sword knee banner pair rib museum shadow juice';
 	const badPassphrase = `${passphrase.slice(0, -1)}y`;
+	const envVariable = 'TEST_PASSPHRASE';
+	const path = '/path/to/my/file.txt';
+	const data = `${passphrase}\nsome other stuff on a new line`;
 
 	let promptStub;
 	let vorpal;
+	let initialEnvValue;
+
+	before(() => {
+		initialEnvValue = process.env[envVariable];
+	});
 
 	beforeEach(() => {
 		promptStub = sinon.stub().resolves({ passphrase });
@@ -32,6 +55,98 @@ describe('prompt utils', () => {
 			},
 			ui: {},
 		};
+	});
+
+	after(() => {
+		if (typeof initialEnvValue !== 'undefined') {
+			process.env[envVariable] = initialEnvValue;
+		}
+	});
+
+	describe('#splitSource', () => {
+		it('should split a source with delimiter', () => {
+			const result = splitSource('someSource: this has spaces: and more colons ');
+			const expected = {
+				sourceType: 'someSource',
+				sourceIdentifier: ' this has spaces: and more colons ',
+			};
+			(result).should.eql(expected);
+		});
+
+		it('should split a source without delimiter', () => {
+			const result = splitSource('stdin');
+			const expected = {
+				sourceType: 'stdin',
+				sourceIdentifier: '',
+			};
+			(result).should.eql(expected);
+		});
+	});
+
+	describe('#getStdin', () => {
+		const createFakeInterface = value => ({
+			on: ((type, callback) => {
+				if (type === 'line') {
+					value.split('\n').forEach(callback);
+				}
+				if (type === 'close') {
+					callback();
+				}
+				return createFakeInterface(value);
+			}),
+		});
+		let createInterfaceStub;
+
+		beforeEach(() => {
+			createInterfaceStub = sinon.stub(readline, 'createInterface');
+		});
+
+		afterEach(() => {
+			createInterfaceStub.restore();
+		});
+
+		describe('if neither passphrase nor data is requested', () => {
+			it('should return an empty object if neither passphrase nor data is requested', () => {
+				return (getStdIn({})).should.be.fulfilledWith({});
+			});
+		});
+
+		describe('if the passphrase is requested', () => {
+			it('should get just the passphrase', () => {
+				createInterfaceStub.returns(createFakeInterface(passphrase));
+				return (getStdIn({ passphraseIsRequired: true }))
+					.should.be.fulfilledWith({ passphrase });
+			});
+		});
+
+		describe('if the data is requested', () => {
+			it('should get just the data', () => {
+				createInterfaceStub.returns(createFakeInterface(data));
+				return (getStdIn({ dataIsRequired: true }))
+					.should.be.fulfilledWith({ data, passphrase: null });
+			});
+		});
+
+		describe('if both the passphrase and the data are requested', () => {
+			it('should get both the passphrase and the data', () => {
+				createInterfaceStub.returns(createFakeInterface(`${passphrase}\n${data}`));
+				return (getStdIn({ passphraseIsRequired: true, dataIsRequired: true }))
+					.should.be.fulfilledWith({ passphrase, data });
+			});
+		});
+	});
+
+	describe('#createPromptOptions', () => {
+		it('should construct an options object for a message', () => {
+			const message = 'Some message.';
+			const result = createPromptOptions(message);
+			const expected = {
+				type: 'password',
+				name: 'passphrase',
+				message,
+			};
+			(result).should.eql(expected);
+		});
 	});
 
 	describe('#getPassphraseFromPrompt', () => {
@@ -49,17 +164,114 @@ describe('prompt utils', () => {
 				});
 		});
 
-		it('should return the pass phrase if successfully repeated', () => {
-			return getPassphraseFromPrompt(vorpal)
-				.then((result) => {
-					(result).should.be.equal(passphrase);
-				});
+		it('should resolve to the passphrase if successfully repeated', () => {
+			return (getPassphraseFromPrompt(vorpal))
+				.should.be.fulfilledWith(passphrase);
 		});
 
-		it('should throw an error if the pass phrase is not successfully repeated', () => {
+		it('should complain if the pass phrase is not successfully repeated', () => {
 			promptStub.onSecondCall().resolves(badPassphrase);
-			return getPassphraseFromPrompt(vorpal)
+			return (getPassphraseFromPrompt(vorpal))
 				.should.be.rejectedWith({ message: 'Passphrase verification failed.' });
+		});
+	});
+
+	describe('#getPassphraseFromEnvVariable', () => {
+		it('should complain if the environmental variable is not set', () => {
+			delete process.env[envVariable];
+			return (getPassphraseFromEnvVariable(envVariable)).should.be.rejectedWith('Passphrase environmental variable not set.');
+		});
+
+		it('should resolve to the passphrase if the environmental variable is set', () => {
+			process.env[envVariable] = passphrase;
+			return (getPassphraseFromEnvVariable(envVariable)).should.be.fulfilled(passphrase);
+		});
+	});
+
+	describe('#getPassphraseFromFile', () => {
+		let streamStub;
+		let createReadStreamStub;
+
+		afterEach(() => {
+			createReadStreamStub.restore();
+		});
+
+		it('should complain if the file does not exist', () => {
+			const doesNotExistError = new Error('ENOENT: no such file or directory');
+			streamStub = createStreamStub((type, callback) => type === 'error' && callback(doesNotExistError));
+			createReadStreamStub = sinon.stub(fse, 'createReadStream').returns(streamStub);
+
+			return (getPassphraseFromFile(path)).should.be.rejectedWith('File does not exist.');
+		});
+
+		it('should complain if the file cannot be read', () => {
+			const permissionError = new Error('EACCES: permission denied');
+			streamStub = createStreamStub((type, callback) => type === 'error' && callback(permissionError));
+			createReadStreamStub = sinon.stub(fse, 'createReadStream').returns(streamStub);
+
+			return (getPassphraseFromFile(path)).should.be.rejectedWith('File could not be read.');
+		});
+
+		it('should complain if an unknown error occurs', () => {
+			const unknownMessage = 'unknown error';
+			const unknownError = new Error(unknownMessage);
+			streamStub = createStreamStub((type, callback) => type === 'error' && callback(unknownError));
+			createReadStreamStub = sinon.stub(fse, 'createReadStream').returns(streamStub);
+
+			return (getPassphraseFromFile(path)).should.be.rejectedWith(unknownMessage);
+		});
+
+		it('should resolve to the first line of the file if the file can be read', () => {
+			streamStub = createStreamStub((type, callback) => type === 'data' && setImmediate(() => callback(data)));
+			createReadStreamStub = sinon.stub(fse, 'createReadStream').returns(streamStub);
+
+			return (getPassphraseFromFile(path)).should.be.fulfilledWith(passphrase);
+		});
+	});
+
+	describe('#getPassphraseFromSource', () => {
+		let streamStub;
+		let createReadStreamStub;
+
+		beforeEach(() => {
+			streamStub = createStreamStub((type, callback) => type === 'data' && setImmediate(() => callback(data)));
+			createReadStreamStub = sinon.stub(fse, 'createReadStream').returns(streamStub);
+		});
+
+		afterEach(() => {
+			createReadStreamStub.restore();
+		});
+
+		it('should complain about an unknown source', () => {
+			return (getPassphraseFromSource('unknown')).should.be.rejectedWith('Unknown passphrase source type. Must be one of `env`, `file`, or `stdin`. Leave blank for prompt.');
+		});
+
+		it('should get passphrase from an environmental variable', () => {
+			return (getPassphraseFromSource(`env:${envVariable}`)).should.be.fulfilledWith(passphrase);
+		});
+
+		it('should get passphrase from a file', () => {
+			return (getPassphraseFromSource(`file:${path}`)).should.be.fulfilledWith(passphrase);
+		});
+
+		it('should resolve to a plaintext passphrase', () => {
+			return (getPassphraseFromSource(`pass:${passphrase}`)).should.be.fulfilledWith(passphrase);
+		});
+	});
+
+	describe('#getPassphrase', () => {
+		it('should get a passphrase from stdin if provided', () => {
+			const stdIn = { passphrase };
+			return (getPassphrase(null, null, stdIn)).should.be.fulfilledWith(passphrase);
+		});
+
+		it('should get a passphrase from a source if no stdin is provided', () => {
+			const source = `pass:${passphrase}`;
+			return (getPassphrase(null, source, {})).should.be.fulfilledWith(passphrase);
+		});
+
+		it('should get a passphrase from prompt if no stdin and no source is provided', () => {
+			return (getPassphrase(vorpal, null, {})).should.be.fulfilledWith(passphrase);
 		});
 	});
 });
