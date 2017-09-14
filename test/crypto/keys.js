a142a848d833eb50dae7f4edf9e8aba6e908e43f
Tobias Schwarz <tobias@lightcurve.io>
2017-09-14 16:31:09 +0200

    Renames variable defaultAddress in test for keys to defaultAddressAndPublicKey

--- test/crypto/keys.js
+++ test/crypto/keys.js
@@ -26,7 +26,7 @@ describe('keys', () => {
 	const defaultSecret = 'secret';
 	const defaultPublicKey = '5d036a858ce89f844491762eb89e2bfbd50a4a0a0da658e4b2628b25b117ae09';
 	const defaultPrivateKey = '2bb80d537b1da3e38bd30361aa855686bde0eacd7162fef6a25fe97bf527a25b5d036a858ce89f844491762eb89e2bfbd50a4a0a0da658e4b2628b25b117ae09';
-	const defaultAddress = {
+	const defaultAddressAndPublicKey = {
 		publicKey: defaultPublicKey,
 		address: '18160565574430594874L',
 	};
@@ -72,7 +72,7 @@ describe('keys', () => {
 
 	describe('#getAddressAndPublicKeyFromSecret', () => {
 		it('should create correct address and publicKey', () => {
-			(getAddressAndPublicKeyFromSecret(defaultSecret)).should.eql(defaultAddress);
+			(getAddressAndPublicKeyFromSecret(defaultSecret)).should.eql(defaultAddressAndPublicKey);
 		});
 	});
 });
