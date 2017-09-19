2e070284103c62e0b58361a5c416890064f1b811
Tobias Schwarz <tobias@lightcurve.io>
2017-09-19 13:48:38 +0200

    getAssetBytes function

--- src/transactions/transactionBytes.js
+++ src/transactions/transactionBytes.js
@@ -15,8 +15,8 @@
 /* eslint-disable no-plusplus */
 import bignum from 'browserify-bignum';
 
-const typeSizes = {
-	TRANSACTION_TYPE: 1,
+const byteSizes = {
+	TYPE: 1,
 	TIMESTAMP: 4,
 	MULTISIGNATURE_PUBLICKEY: 32,
 	RECIPIENT_ID: 8,
@@ -26,183 +26,191 @@ const typeSizes = {
 	DATA: 64,
 };
 
-function getAssetData(transaction) {
-	/**
-	 * @method isSendTransaction
-	 * @return {object}
-	 */
+class Transaction {
 
-	function isSendTransaction() {
-		return transaction.asset.data
-			? Buffer.from(transaction.asset.data, 'utf8')
+	constructor(transaction) {
+
+		this.transaction = transaction;
+
+		this.transactionType = Buffer.alloc(byteSizes.TYPE);
+		this.transactionType.writeInt8(transaction.type)
+
+		this.transactionTimestamp = Buffer.alloc(byteSizes.TIMESTAMP)
+		this.transactionTimestamp.writeIntLE(transaction.timestamp, 0, byteSizes.TIMESTAMP);
+
+		this.transactionSenderPublicKey = Buffer.from(transaction.senderPublicKey, 'hex');
+		this.transactionRequesterPublicKey = transaction.requesterPublicKey
+			? Buffer.from(transaction.requesterPublicKey, 'hex')
 			: Buffer.alloc(0);
+
+		this.transactionRecipientID = transaction.recipientId
+			? Buffer.from(
+				bignum(
+					transaction.recipientId.slice(0, -1),
+				).toBuffer({ size: byteSizes.RECIPIENT_ID }),
+			)
+			: Buffer.alloc(byteSizes.RECIPIENT_ID).fill(0);
+
+		this.transactionAmount = Buffer.alloc(byteSizes.AMOUNT);
+		this.transactionAmount.writeInt32LE(transaction.amount, 0, byteSizes.AMOUNT);
+
+		this.transactionAssetData = this.getAssetBytes(this.transaction);
+
+		this.transactionSignature = transaction.signature
+			? Buffer.from(transaction.signature, 'hex')
+			: Buffer.alloc(0);
+
+		this.transactionSecondSignature = transaction.signSignature
+			? Buffer.from(transaction.signSignature, 'hex')
+			: Buffer.alloc(0);
+
 	}
 
-	/**
-	 * @method isSignatureTransaction
-	 * @return {object}
-	 */
-
-	function isSignatureTransaction() {
-		return Buffer.from(transaction.asset.signature.publicKey, 'hex');
+	get transactionBytes() {
+		return this.concatTransactionBytes();
 	}
 
-	/**
-	 * @method isDelegateTransaction
-	 * @return {object}
-	 */
-
-	function isDelegateTransaction() {
-		return Buffer.from(transaction.asset.delegate.username, 'utf8');
-	}
-
-	/**
-	 * @method isVoteTransaction
-	 * @return {object}
-	 */
-
-	function isVoteTransaction() {
-		return Buffer.from(transaction.asset.votes.join(''));
-	}
-
-	/**
-	 * @method isMultisignatureTransaction
-	 * @return {object}
-	 */
-
-	function isMultisignatureTransaction() {
-		const multisigTransactionAsset = transaction.asset.multisignature;
-		const minBuffer = Buffer.alloc(1).fill(multisigTransactionAsset.min);
-		const lifetimeBuffer = Buffer.alloc(1).fill(multisigTransactionAsset.lifetime);
-		const keysgroupBuffer = Buffer.from(multisigTransactionAsset.keysgroup.join(''));
-		const assetBuffer = Buffer.concat([minBuffer, lifetimeBuffer, keysgroupBuffer]);
-
-		return assetBuffer;
-	}
-
-	/**
-	 * @method isDappTransaction
-	 * @return {object}
-	 */
-
-	function isDappTransaction() {
-		const dapp = transaction.asset.dapp;
-		const dappNameBuffer = Buffer.from(dapp.name);
-		const dappDescriptionBuffer = dapp.description ? Buffer.from(dapp.description) : Buffer.from('');
-		const dappTagsBuffer = dapp.tags ? Buffer.from(dapp.tags) : Buffer.from('');
-		const dappLinkBuffer = Buffer.from(dapp.link);
-		const dappIconBuffer = dapp.icon ? Buffer.from(dapp.icon) : Buffer.from('');
-		const dappTypeBuffer = Buffer.alloc(4).fill(dapp.type);
-		const dappCategoryBuffer = Buffer.alloc(4).fill(dapp.category);
-		const dappBuffer = Buffer.concat([
-			dappNameBuffer,
-			dappDescriptionBuffer,
-			dappTagsBuffer, dappLinkBuffer,
-			dappIconBuffer,
-			dappTypeBuffer,
-			dappCategoryBuffer,
+	concatTransactionBytes () {
+		return Buffer.concat([
+			this.transactionType,
+			this.transactionTimestamp,
+			this.transactionSenderPublicKey,
+			this.transactionRequesterPublicKey,
+			this.transactionRecipientID,
+			this.transactionAmount,
+			this.transactionAssetData,
+			this.transactionSignature,
+			this.transactionSecondSignature,
 		]);
-
-		return dappBuffer;
 	}
 
 	/**
-	 * @method isDappInTransferTransaction
-	 * @return {object}
+	 * @method getAssetBytes
+	 * @return {Buffer}
 	 */
 
-	function isDappInTransferTransaction() {
-		return Buffer.from(transaction.asset.inTransfer.dappId);
+	getAssetBytes(transaction) {
+		/**
+		 * @method isSendTransaction
+		 * @return {Buffer}
+		 */
+
+		function isSendTransaction() {
+			return transaction.asset.data
+				? Buffer.from(transaction.asset.data, 'utf8')
+				: Buffer.alloc(0);
+		}
+
+		/**
+		 * @method isSignatureTransaction
+		 * @return {Buffer}
+		 */
+
+		function isSignatureTransaction() {
+			return Buffer.from(transaction.asset.signature.publicKey, 'hex');
+		}
+
+		/**
+		 * @method isDelegateTransaction
+		 * @return {Buffer}
+		 */
+
+		function isDelegateTransaction() {
+			return Buffer.from(transaction.asset.delegate.username, 'utf8');
+		}
+
+		/**
+		 * @method isVoteTransaction
+		 * @return {Buffer}
+		 */
+
+		function isVoteTransaction() {
+			return Buffer.from(transaction.asset.votes.join(''));
+		}
+
+		/**
+		 * @method isMultisignatureTransaction
+		 * @return {Buffer}
+		 */
+
+		function isMultisignatureTransaction() {
+			const multisigTransactionAsset = transaction.asset.multisignature;
+			const minBuffer = Buffer.alloc(1);
+			minBuffer.writeInt8(multisigTransactionAsset.min);
+			const lifetimeBuffer = Buffer.alloc(1);
+			lifetimeBuffer.writeInt8(multisigTransactionAsset.lifetime);
+			const keysgroupBuffer = Buffer.from(multisigTransactionAsset.keysgroup.join(''));
+			const assetBuffer = Buffer.concat([minBuffer, lifetimeBuffer, keysgroupBuffer]);
+
+			return assetBuffer;
+		}
+
+		/**
+		 * @method isDappTransaction
+		 * @return {Buffer}
+		 */
+
+		function isDappTransaction() {
+			const dapp = transaction.asset.dapp;
+			const dappNameBuffer = Buffer.from(dapp.name);
+			const dappDescriptionBuffer = dapp.description ? Buffer.from(dapp.description) : Buffer.from('');
+			const dappTagsBuffer = dapp.tags ? Buffer.from(dapp.tags) : Buffer.from('');
+			const dappLinkBuffer = Buffer.from(dapp.link);
+			const dappIconBuffer = dapp.icon ? Buffer.from(dapp.icon) : Buffer.from('');
+			const dappTypeBuffer = Buffer.alloc(4);
+			dappTypeBuffer.writeInt8(dapp.type);
+			const dappCategoryBuffer = Buffer.alloc(4);
+			dappCategoryBuffer.writeInt8(dapp.category);
+			const dappBuffer = Buffer.concat([
+				dappNameBuffer,
+				dappDescriptionBuffer,
+				dappTagsBuffer, dappLinkBuffer,
+				dappIconBuffer,
+				dappTypeBuffer,
+				dappCategoryBuffer,
+			]);
+
+			return dappBuffer;
+		}
+
+		/**
+		 * @method isDappInTransferTransaction
+		 * @return {Buffer}
+		 */
+
+		function isDappInTransferTransaction() {
+			return Buffer.from(transaction.asset.inTransfer.dappId);
+		}
+
+		/**
+		 * @method isDappOutTransferTransaction
+		 * @return {Buffer}
+		 */
+
+		function isDappOutTransferTransaction() {
+			const dappOutAppIdBuffer = Buffer.from(transaction.asset.outTransfer.dappId);
+			const dappOutTransactionIdBuffer = Buffer.from(transaction.asset.outTransfer.transactionId);
+			const transactionAssetBuffer = Buffer.concat([dappOutAppIdBuffer, dappOutTransactionIdBuffer]);
+
+			return transactionAssetBuffer;
+		}
+
+		const transactionType = {
+			0: isSendTransaction,
+			1: isSignatureTransaction,
+			2: isDelegateTransaction,
+			3: isVoteTransaction,
+			4: isMultisignatureTransaction,
+			5: isDappTransaction,
+			6: isDappInTransferTransaction,
+			7: isDappOutTransferTransaction,
+		};
+
+		return transactionType[transaction.type]();
 	}
 
-	/**
-	 * @method isDappOutTransferTransaction
-	 * @return {object}
-	 */
-
-	function isDappOutTransferTransaction() {
-		const dappOutAppIdBuffer = Buffer.from(transaction.asset.outTransfer.dappId);
-		const dappOutTransactionIdBuffer = Buffer.from(transaction.asset.outTransfer.transactionId);
-		const transactionAssetBuffer = Buffer.concat([dappOutAppIdBuffer, dappOutTransactionIdBuffer]);
-
-		return transactionAssetBuffer;
-	}
-
-	const transactionType = {
-		0: isSendTransaction,
-		1: isSignatureTransaction,
-		2: isDelegateTransaction,
-		3: isVoteTransaction,
-		4: isMultisignatureTransaction,
-		5: isDappTransaction,
-		6: isDappInTransferTransaction,
-		7: isDappOutTransferTransaction,
-	};
-
-	return transactionType[transaction.type]();
 }
 
-
-function getTransactionBuffer(transaction) {
-	const transactionType = Buffer.alloc(typeSizes.TRANSACTION_TYPE).fill(transaction.type);
-
-	const transactionTimestamp = Buffer.alloc(typeSizes.TIMESTAMP);
-	transactionTimestamp.writeIntLE(transaction.timestamp, 0, typeSizes.TIMESTAMP);
-
-	const transactionSenderPublicKey = Buffer.from(transaction.senderPublicKey, 'hex');
-
-	const transactionRequesterPublicKey = transaction.requesterPublicKey
-		? Buffer.from(transaction.requesterPublicKey, 'hex')
-		: Buffer.alloc(0);
-
-	const transactionRecipientID = transaction.recipientId
-		? Buffer.from(
-			bignum(
-				transaction.recipientId.slice(0, -1),
-			).toBuffer({ size: typeSizes.RECIPIENT_ID }),
-		)
-		: Buffer.alloc(typeSizes.RECIPIENT_ID).fill(0);
-
-	const transactionAmount = Buffer.alloc(typeSizes.AMOUNT);
-	transactionAmount.writeInt32LE(transaction.amount, 0, typeSizes.AMOUNT);
-
-	const transactionAssetData = getAssetData(transaction);
-
-	const transactionSignature = transaction.signature
-		? Buffer.from(transaction.signature, 'hex')
-		: Buffer.alloc(0);
-
-	const transactionSecondSignature = transaction.signSignature
-		? Buffer.from(transaction.signSignature, 'hex')
-		: Buffer.alloc(0);
-
-	const transactionBuffer = Buffer.concat([
-		transactionType,
-		transactionTimestamp,
-		transactionSenderPublicKey,
-		transactionRequesterPublicKey,
-		transactionRecipientID,
-		transactionAmount,
-		transactionAssetData,
-		transactionSignature,
-		transactionSecondSignature,
-	]);
-
-	return transactionBuffer;
+export function getTransactionBytes(transaction) {
+	return new Transaction(transaction).transactionBytes;
 }
-
-
-/**
- * @method getBytes
- * @param transaction Object
- *
- * @return {buffer}
- */
-
-function getTransactionBytes(transaction) {
-	return getTransactionBuffer(transaction);
-}
-
-module.exports = {
-	getTransactionBytes,
-};
