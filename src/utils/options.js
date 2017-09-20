4c471354e46c6056827310c07e5792007470e85a
willclarktech <willclarktech@users.noreply.github.com>
2017-09-20 16:24:32 +0200

    Add password option to options file

--- src/utils/options.js
+++ src/utils/options.js
@@ -11,6 +11,15 @@ const passphraseDescription = `Specifies a source for your secret passphrase. Li
 	- --passphrase stdin (takes the first line only)
 `;
 
+const passwordDescription = `Specifies a source for your secret password. Lisky will prompt you for input if this option is not set. Source must be one of \`env\`, \`file\` or \`stdin\`. Except for \`stdin\`, a corresponding identifier must also be provided.
+
+	Examples:
+	- --password pass:password123 (should only be used where security is not important)
+	- --password env:PASSWORD
+	- --password file:/path/to/my/password.txt (takes the first line only)
+	- --password stdin (takes the first line only)
+`;
+
 const dataDescription = `Specifies a source for providing data to the command. If a string is provided directly as an argument, this option will be ignored. The data must be provided via an argument or via this option. Sources must be one of \`file\` or \`stdin\`. In the case of \`file\`, a corresponding identifier must also be provided.
 
 	Note: if both secret passphrase and data are passed via stdin, the passphrase must be the first line.
@@ -24,6 +33,7 @@ const options = {
 	json: ['-j, --json', jsonDescription],
 	noJson: ['-t, --no-json', noJsonDescription],
 	passphrase: ['-p, --passphrase <source>', passphraseDescription],
+	password: ['-w, --password <source>', passwordDescription],
 	data: ['-d, --data <source>', dataDescription],
 };
 
