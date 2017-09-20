02c621c0e828c4201926b40be3b9b9347f758715
willclarktech <willclarktech@users.noreply.github.com>
2017-09-20 10:23:51 +0200

    Specify babel plugins for browsertest build

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,7 +26,7 @@ module.exports = function configureGrunt(grunt) {
 			prepareDistNode: 'rm -r dist-node/* || mkdir dist-node | echo',
 			prepareDistBrowser: 'rm -r dist-browser/* || mkdir dist-browser | echo',
 			babel: './node_modules/.bin/babel src --out-dir ./dist-node',
-			babelTest: './node_modules/.bin/babel src --out-dir ./browsertest/src && ./node_modules/.bin/babel test --ignore test/transactions/dapp.js --out-dir ./browsertest/test',
+			babelTest: './node_modules/.bin/babel src --out-dir ./browsertest/src && BABEL_ENV=browsertest ./node_modules/.bin/babel test --ignore test/transactions/dapp.js --out-dir ./browsertest/test',
 			tidyTest: 'rm -r browsertest/{src,test}',
 		},
 
