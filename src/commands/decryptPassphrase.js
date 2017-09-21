b899b6bd267476470fd012343cae29a3ad468e8f
willclarktech <willclarktech@users.noreply.github.com>
2017-09-21 15:16:24 +0200

    Fill out decrypt passphrase option description

--- src/commands/decryptPassphrase.js
+++ src/commands/decryptPassphrase.js
@@ -24,7 +24,14 @@ import {
 	getData,
 } from '../utils/input';
 
-const encryptedPassphraseOptionDescription = `Hello${' you'}`;
+const passphraseOptionDescription = `Specifies a source for providing an encrypted passphrase to the command. If a string is provided directly as an argument, this option will be ignored. The encrypted passphrase must be provided via an argument or via this option. Sources must be one of \`file\` or \`stdin\`. In the case of \`file\`, a corresponding identifier must also be provided.
+
+	Note: if both an encrypted passphrase and the password are passed via stdin, the password must be the first line.
+
+	Examples:
+	- --passphrase file:/path/to/my/encrypted_passphrase.txt (takes the first line only)
+	- --passphrase stdin (takes the first line only)
+`;
 
 const handleInput = iv => ([cipher, password]) =>
 	cryptoModule.decryptPassphrase({ cipher, iv }, password);
@@ -50,7 +57,7 @@ function decryptPassphraseCommand(vorpal) {
 	vorpal
 		.command('decrypt passphrase <iv> [passphrase]')
 		.option(...commonOptions.password)
-		.option(commonOptions.passphrase[0], encryptedPassphraseOptionDescription)
+		.option(commonOptions.passphrase[0], passphraseOptionDescription)
 		.action(decryptPassphrase(vorpal));
 }
 
