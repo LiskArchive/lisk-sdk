6e18b569a5c39388ecdfabbe4e6b815392e79d03
Usman Khan <usman@lightcurve.io>
2017-09-25 16:39:42 +0200

    Added functional tests for all transaction types

--- test/api/bug.multisig.dapp-0.9.js
+++ test/api/bug.multisig.dapp-0.9.js
@@ -2,6 +2,7 @@
 
 var async = require('async');
 var node = require('./../node.js');
+var expect = node.expect;
 
 var constants = require('../../helpers/constants.js');
 
@@ -18,8 +19,14 @@ function sendLISK (account, i, done) {
 		amount: randomLISK,
 		recipientId: account.address
 	}, function (err, res) {
-		node.expect(res.body).to.have.property('success').to.be.ok;
+		expect(res.body).to.have.property('success').to.be.ok;
 		done();
+	});
+}
+
+function createAccountWithLisk (account, done) {
+	sendLISK(account, 100000, function (err, res) {
+		node.onNewBlock(done);
 	});
 }
 
@@ -56,7 +63,7 @@ function confirmTransaction (transactionId, passphrases, done) {
 				if (err || !res.body.success) {
 					return untilCb(err || res.body.error);
 				}
-				node.expect(res.body).to.have.property('transactionId').to.equal(transactionId);
+				expect(res.body).to.have.property('transactionId').to.equal(transactionId);
 				count++;
 				return untilCb();
 			});
@@ -67,65 +74,94 @@ function confirmTransaction (transactionId, passphrases, done) {
 	);
 }
 
-describe('registering and signing multisig transaction and sending different type of transaction should be ok', function () {
+function createDapp (params, done) {
+	var params = {
+		secret: params.account.password,
+		category: node.randomProperty(node.dappCategories),
+		name: params.applicationName,
+		type: node.dappTypes.DAPP,
+		description: 'A dapp added via API autotest',
+		tags: 'handy dizzy pear airplane alike wonder nifty curve young probable tart concentrate',
+		link: 'https://github.com/' + params.applicationName + '/master.zip',
+		icon: node.guestbookDapp.icon
+	};
 
-	describe('in the same block', function () {
+	node.put('/api/dapps', params, done);
+}
 
-		var totalMembers = 15;
-		var requiredSignatures = 15;
-		var validParams;
+function createIntransfer (params, done) {
+	node.put('/api/dapps/transaction', params, done);
+}
 
-		var multisigAccount;
-		var multisigTransactionId;
-		var passphrases;
+function createOutTransfer (params, cb) {
+	node.put('/api/dapps/withdrawal', params, cb);
+}
 
-		beforeEach(function () {
-			multisigAccount = node.randomAccount();
-			var accounts = [];
-			var keysgroup = [];
-
-			for (var i = 0; i < totalMembers; i++) {
-				accounts[i] = node.randomAccount();
-				var member = '+' + accounts[i].publicKey;
-				keysgroup.push(member);
-			}
-
-			passphrases = accounts.map(function (account) {
-				return account.password;
-			});
-
-			validParams = {
-				secret: multisigAccount.password,
-				lifetime: parseInt(node.randomNumber(1,72)),
-				min: requiredSignatures,
-				keysgroup: keysgroup
-			};
+function checkConfirmedTransactions (ids, cb) {
+	async.each(ids, function (id, eachCb) {
+		getTransactionById(id, function (err, res) {
+			expect(err).to.not.exist;
+			expect(res.body).to.have.property('success').to.be.ok;
+			expect(res.body).to.have.property('transaction').that.is.an('object');
+			expect(res.body.transaction.id).to.equal(id);
+			eachCb(err);
 		});
+	}, function (err) {
+		cb(err);
+	});
+}
 
-		function checkConfirmedTransactions (ids, cb) {
-			async.each(ids, function (id, eachCb) {
-				getTransactionById(id, function (err, res) {
-					node.expect(err).to.not.exist;
-					node.expect(res.body).to.have.property('success').to.be.ok;
-					node.expect(res.body).to.have.property('transaction').that.is.an('object');
-					node.expect(res.body.transaction.id).to.equal(id);
-					eachCb();
-				});
-			}, function (err) {
-				cb(err);
-			});
+
+describe('registering and signing multisig transaction and another trasaction from same account', function () {
+
+	var totalMembers = 15;
+	var requiredSignatures = 15;
+
+	var multisigAccount;
+	var multisigTransactionId;
+	var passphrases;
+	var accounts = [];
+	var keysgroup = [];
+
+	function createMultisignatureAndConfirm (account, cb) {
+		keysgroup = [];
+		accounts = [];
+
+		for (var i = 0; i < totalMembers; i++) {
+			accounts[i] = node.randomAccount();
+			var member = '+' + accounts[i].publicKey;
+			keysgroup.push(member);
 		}
 
-		beforeEach('send lisk to multisig account', function (done) {
-			sendLISK(multisigAccount, 0, function () {
-				node.onNewBlock(function (err) {
-					node.put('/api/multisignatures', validParams, function (err, res) {
-						node.expect(res.body).to.have.property('success').to.be.ok;
-						node.expect(res.body).to.have.property('transactionId').that.is.not.empty;
-						multisigTransactionId = res.body.transactionId;
-						confirmTransaction(res.body.transactionId, passphrases, done);
-					});
-				});
+		passphrases = accounts.map(function (account) {
+			return account.password;
+		});
+
+		var params = {
+			secret: account.password,
+			lifetime: parseInt(node.randomNumber(1,72)),
+			min: requiredSignatures,
+			keysgroup: keysgroup
+		};
+
+		node.put('/api/multisignatures', params, function (err, res) {
+			expect(res.body).to.have.property('success').to.be.ok;
+			expect(res.body).to.have.property('transactionId').that.is.not.empty;
+			multisigTransactionId = res.body.transactionId;
+			confirmTransaction(res.body.transactionId, passphrases, function (err, result) {
+				expect(err).to.not.exist;
+				cb(err, res);
+			});
+		});
+	}
+
+	describe('in the same block for transactions 0 to 5', function () {
+
+		beforeEach(function (done) {
+			multisigAccount = node.randomAccount();
+			createAccountWithLisk(multisigAccount, function (err, res) {
+				expect(err).to.not.exist;
+				createMultisignatureAndConfirm(multisigAccount, done);
 			});
 		});
 
@@ -135,7 +171,7 @@ describe('registering and signing multisig transaction and sending different typ
 				amount: 1,
 				recipientId: node.eAccount.address
 			}, function (err, res) {
-				node.expect(res.body).to.have.property('success').to.be.ok;
+				expect(res.body).to.have.property('success').to.be.ok;
 
 				node.onNewBlock(function () {
 					var transactionInCheckId = res.body.transactionId;
@@ -146,13 +182,13 @@ describe('registering and signing multisig transaction and sending different typ
 		});
 
 		it('TYPE 1 registering second password with valid params should be ok', function (done) {
-			validParams = {
+			var params = {
 				secret: multisigAccount.password,
 				secondSecret: multisigAccount.secondPassword
 			};
 
-			putSignature(validParams, function (err, res) {
-				node.expect(res.body).to.have.property('success').to.be.ok;
+			putSignature(params, function (err, res) {
+				expect(res.body).to.have.property('success').to.be.ok;
 
 				node.onNewBlock(function () {
 					var transactionInCheckId = res.body.transaction.id;
@@ -163,13 +199,13 @@ describe('registering and signing multisig transaction and sending different typ
 		});
 
 		it('TYPE 2 registering delegate with valid params should be ok', function (done) {
-			validParams = {
+			var params = {
 				secret: multisigAccount.password,
 				username: multisigAccount.username
 			};
 
-			putDelegates(validParams, function (err, res) {
-				node.expect(res.body).to.have.property('success').to.be.ok;
+			putDelegates(params, function (err, res) {
+				expect(res.body).to.have.property('success').to.be.ok;
 
 				node.onNewBlock(function () {
 					var transactionInCheckId = res.body.transaction.id;
@@ -184,7 +220,7 @@ describe('registering and signing multisig transaction and sending different typ
 				secret: multisigAccount.password,
 				delegates: ['+' + node.eAccount.publicKey]
 			}, function (err, res) {
-				node.expect(res.body).to.have.property('success').to.be.ok;
+				expect(res.body).to.have.property('success').to.be.ok;
 
 				node.onNewBlock(function () {
 					var transactionInCheckId = res.body.transaction.id;
@@ -194,26 +230,127 @@ describe('registering and signing multisig transaction and sending different typ
 			});
 		});
 
-		it('TYPE 5 registering dapp should be ok', function (done) {
-			var applicationName = node.randomApplicationName();
-			validParams = {
+		it('TYPE 4 creating second multisignature should fail', function (done) {
+			var params = {
 				secret: multisigAccount.password,
-				category: node.randomProperty(node.dappCategories),
-				name: applicationName,
-				type: node.dappTypes.DAPP,
-				description: 'A dapp added via API autotest',
-				tags: 'handy dizzy pear airplane alike wonder nifty curve young probable tart concentrate',
-				link: 'https://github.com/' + applicationName + '/master.zip',
-				icon: node.guestbookDapp.icon
+				lifetime: parseInt(node.randomNumber(1,72)),
+				min: requiredSignatures,
+				keysgroup: keysgroup
 			};
 
-			node.put('/api/dapps', validParams, function (err, res) {
-				node.expect(res.body).to.have.property('success').to.be.ok;
+			node.put('/api/multisignatures', params, function (err, res) {
+				expect(res.body).to.have.property('success').to.be.ok;
+				expect(res.body).to.have.property('transactionId').that.is.not.empty;
+				confirmTransaction(res.body.transactionId, passphrases, function (err) {
+					expect(err).to.not.exist;
+
+					var transactionInCheckId = res.body.transactionId;
+					node.onNewBlock(function () {
+						async.parallel([
+							function (cb) {
+								checkConfirmedTransactions([transactionInCheckId], cb);
+							},
+							function (cb) {
+								getTransactionById(multisigTransactionId, function (err, res) {
+									expect(res.body.success).to.equal(false);
+									expect(res.body.error).to.equal('Transaction not found');
+									cb(err, res);
+								});
+							}
+						],done);
+					});
+				});
+			});
+		});
+
+		it('TYPE 5 registering dapp should be ok', function (done) {
+			var applicationName = node.randomApplicationName();
+			createDapp({
+				account: multisigAccount,
+				applicationName: applicationName,
+			}, function (err, res) {
+				expect(err).to.not.exist;
+				var transactionInCheckId = res.body.transaction.id;
 
 				node.onNewBlock(function () {
-					var transactionInCheckId = res.body.transaction.id;
-
 					checkConfirmedTransactions([transactionInCheckId, multisigTransactionId], done);
+				});
+			});
+		});
+	});
+
+	describe('In the same block for transaction types 6 and 7', function () {
+
+		var dappId;
+
+		beforeEach(function (done) {
+			multisigAccount = node.randomAccount();
+			createAccountWithLisk(multisigAccount, function (err, res) {
+				var applicationName = node.randomApplicationName();
+
+				createDapp({
+					account: multisigAccount,
+					applicationName: applicationName,
+				}, function (err, res) {
+					expect(err).to.not.exist;
+
+					dappId = res.body.transaction.id;
+					node.onNewBlock(done);
+				});
+			});
+		});
+
+		it('TYPE 6 sending IN_TRANSFER transaction should be ok', function (done) {
+			async.parallel({
+				inTransfer: function (cb) {
+					var params = {
+						secret: multisigAccount,
+						dappId: dappId,
+						amount: 10000
+					};
+					createIntransfer(params, cb);
+				},
+				multisignature: function (cb) {
+					createMultisignatureAndConfirm(multisigAccount, cb);
+				}
+			}, function (err, res) {
+				expect(err).to.not.exist;
+
+				var inTransferTransactionId = res.inTransfer.body.transactionId;
+
+				node.onNewBlock(function () {
+					checkConfirmedTransactions([inTransferTransactionId, multisigTransactionId], done);
+				});
+			});
+		});
+
+		it('TYPE 7 sending OUT_TRANSFER transaction should be ok', function (done) {
+			var params = {
+				secret: multisigAccount,
+				dappId: dappId,
+				amount: 10000
+			};
+
+			createIntransfer(params, function (err, res) {
+				expect(err).to.not.exist;
+
+				var inTransferId = res.body.transactionId;
+
+				var outTransferParams = {
+					amount: 1000,
+					recipientId: '16313739661670634666L',
+					dappId: dappId,
+					transactionId: inTransferId,
+					secret: multisigAccount.password
+				};
+
+				createOutTransfer(outTransferParams, function (err, res) {
+					expect(err).to.not.exist;
+
+					var transactionInCheckId = res.body.transactionId;
+					node.onNewBlock(function () {
+						checkConfirmedTransactions([transactionInCheckId, multisigTransactionId], done);
+					});
 				});
 			});
 		});
