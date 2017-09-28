be80d8529a3a83fdd3a5f34bab430575eea5e749
Maciej Baj <macie.baj@gmail.com>
2017-09-28 13:17:25 +0200

    Add mem_accounts protection sql unit tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 require('./unit/helpers/request-limiter.js');
 require('./unit/logic/blockReward.js');
 require('./unit/sql/blockRewards.js');
+require('./unit/sql/memAccountsProtection.js');
 require('./unit/modules/peers.js');
 require('./unit/modules/blocks.js');
 require('./unit/modules/blocks/verify.js');
