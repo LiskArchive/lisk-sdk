034991a10957ac89613a476f052e7626bfdef934
Diego Garcia <diego@lightcurve.io>
2017-10-02 14:46:49 +0200

    Shared functional test phases

---
+++ test/functional/shared.js
@@ -0,0 +1,193 @@
+'use strict';
+
+var node = require('../node');
+
+var sendTransactionPromise = require('../common/complexTransactions').sendTransactionPromise;
+var getTransactionPromise = require('../common/complexTransactions').getTransactionPromise;
+var getUnconfirmedTransactionPromise = require('../common/complexTransactions').getUnconfirmedTransactionPromise;
+var getPendingMultisignaturePromise = require('../common/complexTransactions').getPendingMultisignaturePromise;
+var onNewBlockPromise = node.Promise.promisify(node.onNewBlock);
+
+var tests = [
+	{describe: 'null',							args: null},
+	{describe: 'undefined',					args: undefined},
+	{describe: 'NaN',								args: NaN},
+	{describe: 'Infinity',					args: Infinity},
+	{describe: '0 integer',					args: 0},
+	{describe: 'negative integer',	args: -1},
+	{describe: 'float',							args: 1.2},
+	{describe: 'negative float',		args: -1.2},
+	{describe: 'empty string',			args: ''},
+	{describe: '0 as string',				args: '0'},
+	{describe: 'regular string',		args: String('abc')},
+	{describe: 'uppercase string',	args: String('ABC')},
+	{describe: 'invalid chars',			args: String('/')},
+	{describe: 'date',							args: new Date()},
+	{describe: 'true boolean',			args: true},
+	{describe: 'false boolean',			args: false},
+	{describe: 'empty array',				args: []},
+	{describe: 'empty object',			args: {}}
+];
+
+function confirmationPhase (goodTransactions, badTransactions, pendingMultisignatures) {
+
+	describe('before new block', function () {
+
+		it('good transactions should remain unconfirmed', function () {
+			return node.Promise.map(goodTransactions, function (tx) {
+				return getTransactionPromise(tx.id).then(function (res) {
+					node.expect(res).to.have.property('success').to.be.not.ok;
+					node.expect(res).to.have.property('error').equal('Transaction not found');
+				});
+			});
+		});
+
+		if (pendingMultisignatures) {
+			it('pendingMultisignatures should remain in the pending queue', function () {
+				return node.Promise.map(pendingMultisignatures, function (tx) {
+					return getPendingMultisignaturePromise(tx).then(function (res) {
+						node.expect(res).to.have.property('success').to.be.ok;
+						node.expect(res).to.have.property('transactions').to.be.an('array').to.have.lengthOf(1);
+					});
+				});
+			});
+
+			it('pendingMultisignatures should not be confirmed', function () {
+				return node.Promise.map(pendingMultisignatures, function (tx) {
+					return getTransactionPromise(tx.id).then(function (res) {
+						node.expect(res).to.have.property('success').to.be.not.ok;
+						node.expect(res).to.have.property('error').equal('Transaction not found');
+					});
+				});
+			});
+	  };
+	});
+
+	describe('after new block', function () {
+
+		before(function () {
+			return onNewBlockPromise();
+		});
+
+		it('bad transactions should not be confirmed', function () {
+			return node.Promise.map(badTransactions, function (tx) {
+				return getTransactionPromise(tx.id).then(function (res) {
+					node.expect(res).to.have.property('success').to.be.not.ok;
+					node.expect(res).to.have.property('error').equal('Transaction not found');
+				});
+			});
+		});
+
+		it('good transactions should not be unconfirmed', function () {
+			return node.Promise.map(goodTransactions, function (tx) {
+				return getUnconfirmedTransactionPromise(tx.id).then(function (res) {
+					node.expect(res).to.have.property('success').to.be.not.ok;
+					node.expect(res).to.have.property('error').equal('Transaction not found');
+				});
+			});
+		});
+
+		it('good transactions should be confirmed', function () {
+			return node.Promise.map(goodTransactions, function (tx) {
+				return getTransactionPromise(tx.id).then(function (res) {
+					node.expect(res).to.have.property('success').to.be.ok;
+					node.expect(res).to.have.property('transaction').to.have.property('id').equal(tx.id);
+				});
+			});
+		});
+
+		if (pendingMultisignatures) {
+			it('pendingMultisignatures should remain in the pending queue', function () {
+				return node.Promise.map(pendingMultisignatures, function (tx) {
+					return getPendingMultisignaturePromise(tx).then(function (res) {
+						node.expect(res).to.have.property('success').to.be.ok;
+						node.expect(res).to.have.property('transactions').to.be.an('array').to.have.lengthOf(1);
+					});
+				});
+			});
+
+			it('pendingMultisignatures should not be confirmed', function () {
+				return node.Promise.map(pendingMultisignatures, function (tx) {
+					return getTransactionPromise(tx.id).then(function (res) {
+						node.expect(res).to.have.property('success').to.be.not.ok;
+						node.expect(res).to.have.property('error').equal('Transaction not found');
+					});
+				});
+			});
+		};
+	});
+};
+
+function invalidTxs () {
+
+	tests.forEach(function (test) {
+		it('using ' + test.describe + ' should fail', function () {
+			return sendTransactionPromise(test.args).then(function (res) {
+				node.expect(res).to.have.property('success').to.not.be.ok;
+				node.expect(res).to.have.property('message').that.is.not.empty;
+			});
+		});
+	});
+};
+
+function invalidAssets (account, option, badTransactions) {
+
+	var transaction;
+
+	beforeEach(function () {
+		switch(option) {
+			case 'signature':
+				transaction = node.lisk.signature.createSignature(account.password, node.randomPassword());
+				break;
+			case 'delegate':
+				transaction = node.lisk.delegate.createDelegate(account.password, node.randomDelegateName());
+				break;
+			case 'votes':
+				transaction = node.lisk.vote.createVote(account.password, []);
+				break;
+			case 'multisignature':
+				transaction = node.lisk.multisignature.createMultisignature(account.password, null, ['+' + node.eAccount.publicKey], 1, 2);
+				break;
+			case 'dapp':
+				transaction = node.lisk.dapp.createDapp(account.password, null, node.guestbookDapp);
+				break;
+		};
+	});
+
+	describe('using invalid asset values', function () {
+
+		tests.forEach(function (test) {
+			it('using ' + test.describe + ' should fail', function () {
+				transaction.asset = test.args;
+
+				return sendTransactionPromise(transaction).then(function (res) {
+					node.expect(res).to.have.property('success').to.be.not.ok;
+					node.expect(res).to.have.property('message').that.is.not.empty;
+					badTransactions.push(transaction);
+				});
+			});
+		});
+	});
+
+	describe('using invalid asset.' + option + ' values', function () {
+
+		tests.forEach(function (test) {
+			it('using ' + test.describe + ' should fail', function () {
+				transaction.asset[option] = test.args;
+
+				return sendTransactionPromise(transaction).then(function (res) {
+					node.expect(res).to.have.property('success').to.be.not.ok;
+					node.expect(res).to.have.property('message').that.is.not.empty;
+					badTransactions.push(transaction);
+				});
+			});
+		});
+	});
+}
+
+module.exports = {
+	tests: tests,
+	confirmationPhase: confirmationPhase,
+	invalidTxs: invalidTxs,
+	invalidAssets: invalidAssets
+};
