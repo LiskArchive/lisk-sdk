e41f1ee057fa788ea568a3335f84cb93f7c93e30
Mona <tschakki@posteo.de>
2017-10-05 16:35:32 +0200

    Further tests

--- test/unit/modules/transport.js
+++ test/unit/modules/transport.js
@@ -120,23 +120,86 @@ describe('transport', function () {
 		
 		describe('receiveSignature', function () {
 			
+			it('should call library.schema.validate');
+			
 			it('should call library.schema.validate with {signature: query}');
 			
-			it('should call library.schema.validate with shcema.signature');
+			it('should call library.schema.validate with schema.signature');
 			
-			describe('callback of library.schema.validate', function () {
+			describe('when library.schema.validate fails', function () {
 				
-				describe('when error is defined', function () {
-					
-					it('should call callback with "Invalid signature body"');
-				});
+				it('should call callback with "Invalid signature body"');
+			});
+			
+			describe('when library.schema.validate succeeds', function () {
+				
+				it('should call modules.multisignatures.processSignature');
 				
 				it('should call modules.multisignatures.processSignature with query');
 				
-				describe('callback of modules.multisignatures.processSignature', function () {
+				describe('when modules.multisignatures.processSignature fails', function (){
 					
-					describe('when error is defined', function () {
+					it('should call callback with "Error processing signature:" + err');
+				});
+				
+				describe('when modules.multisignatures.processSignature succeeds', function (){
+					
+					it('should call callback with error = undefined');
+					
+					it('should call callback with result = undefined');
+				});
+			});
+		});
+		
+		describe('receiveTransactions', function () {
+			
+			it('should call library.schema.validate');
+			
+			it('should call library.schema.validate with query');
+			
+			it('should call library.schema.validate with schema.transactions');
+			
+			describe('when library.schema.validate fails', function (){
+				
+				it('should call callback with error = "Invalid transactions body"');
+			});
+			
+			describe('when library.schema.validate succeeds', function (){
+				
+				describe('for every transaction in transactions', function () {
+					
+					describe('when transaction is undefined', function () {
 						
+						it('should call callback with error = "Unable to process signature. Signature is undefined."');
+					});
+					
+					describe('when transaction is defined', function () {
+						
+						it('should set transaction.bundle = true');
+						
+						it('should call __private.receiveTransaction');
+						
+						it('should call __private.receiveTransaction with transaction');
+						
+						it('should call __private.receiveTransaction with peer');
+						
+						it('should call __private.receiveTransaction with extraLogMessage');
+						
+						describe('when call __private.receiveTransaction fails', function () {
+							
+							it('should call library.logger.debug with error');
+							
+							it('should call library.logger.debug with transaction');
+							
+							it('should call callback with error');
+						});
+						
+						describe('when call __private.receiveTransaction succeeds', function () {
+							
+							it('should call callback with error = undefined');
+							
+							it('should call callback with result = undefined');
+						});
 					});
 				});
 			});
