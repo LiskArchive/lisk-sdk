362a3c37228492d9563d9e5ab8fa52c81e538d65
willclarktech <willclarktech@users.noreply.github.com>
2017-10-16 11:18:39 +0200

    :house: Remove vorpal ui listeners in global afterEach hook

--- test/_globalHooks.js
+++ test/_globalHooks.js
@@ -1,1 +1,9 @@
-afterEach(() => sandbox.restore());
+afterEach(function globalAfterEach() {
+	const { vorpal } = this.test.ctx;
+	// See https://github.com/dthree/vorpal/issues/230
+	if (vorpal && vorpal.ui) {
+		vorpal.ui.removeAllListeners();
+	}
+
+	sandbox.restore();
+});
