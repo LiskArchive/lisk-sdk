34f14512415187c83d9161bbf7ee1397ae0e1e0b
Tobias Schwarz <tobias@lightcurve.io>
2017-10-16 13:03:56 +0200

    :recycle: Rename registerSecondSignature to registerSecondPassphrase

--- test/transactions/1_registerSecondPassphrase.js
+++ test/transactions/1_registerSecondPassphrase.js
@@ -12,14 +12,14 @@
  * Removal or modification of this copyright notice is prohibited.
  *
  */
-import registerSecondSignature from '../../src/transactions/1_registerSecondSignature';
+import registerSecondPassphrase from '../../src/transactions/1_registerSecondPassphrase';
 import cryptoModule from '../../src/crypto';
 
 const time = require('../../src/transactions/utils/time');
 
 afterEach(() => sandbox.restore());
 
-describe('#registerSecondSignature transaction', () => {
+describe('#registerSecondPassphrase transaction', () => {
 	const secret = 'secret';
 	const secondSecret = 'second secret';
 	const publicKey = '5d036a858ce89f844491762eb89e2bfbd50a4a0a0da658e4b2628b25b117ae09';
@@ -29,15 +29,15 @@ describe('#registerSecondSignature transaction', () => {
 	const timeWithOffset = 38350076;
 
 	let getTimeWithOffsetStub;
-	let registerSecondSignatureTransaction;
+	let registerSecondPassphraseTransaction;
 
 	beforeEach(() => {
 		getTimeWithOffsetStub = sandbox.stub(time, 'getTimeWithOffset').returns(timeWithOffset);
-		registerSecondSignatureTransaction = registerSecondSignature({ secret, secondSecret });
+		registerSecondPassphraseTransaction = registerSecondPassphrase({ secret, secondSecret });
 	});
 
 	it('should create a register second signature transaction', () => {
-		(registerSecondSignatureTransaction).should.be.ok();
+		(registerSecondPassphraseTransaction).should.be.ok();
 	});
 
 	it('should use time.getTimeWithOffset to calculate the timestamp', () => {
@@ -46,90 +46,90 @@ describe('#registerSecondSignature transaction', () => {
 
 	it('should use time.getTimeWithOffset with an offset of -10 seconds to calculate the timestamp', () => {
 		const offset = -10;
-		registerSecondSignature({ secret, secondSecret, timeOffset: offset });
+		registerSecondPassphrase({ secret, secondSecret, timeOffset: offset });
 
 		(getTimeWithOffsetStub.calledWithExactly(offset)).should.be.true();
 	});
 
 	describe('returned register second signature transaction', () => {
 		it('should be an object', () => {
-			(registerSecondSignatureTransaction).should.be.type('object');
+			(registerSecondPassphraseTransaction).should.be.type('object');
 		});
 
 		it('should have an id string', () => {
-			(registerSecondSignatureTransaction).should.have.property('id').and.be.type('string');
+			(registerSecondPassphraseTransaction).should.have.property('id').and.be.type('string');
 		});
 
 		it('should have type number equal to 1', () => {
-			(registerSecondSignatureTransaction).should.have.property('type').and.be.type('number').and.equal(1);
+			(registerSecondPassphraseTransaction).should.have.property('type').and.be.type('number').and.equal(1);
 		});
 
 		it('should have amount number equal to 0', () => {
-			(registerSecondSignatureTransaction).should.have.property('amount').and.be.type('number').and.equal(0);
+			(registerSecondPassphraseTransaction).should.have.property('amount').and.be.type('number').and.equal(0);
 		});
 
 		it('should have fee number equal to signature fee', () => {
-			(registerSecondSignatureTransaction).should.have.property('fee').and.be.type('number').and.equal(signatureFee);
+			(registerSecondPassphraseTransaction).should.have.property('fee').and.be.type('number').and.equal(signatureFee);
 		});
 
 		it('should have recipientId equal to null', () => {
-			(registerSecondSignatureTransaction).should.have.property('recipientId').and.be.null();
+			(registerSecondPassphraseTransaction).should.have.property('recipientId').and.be.null();
 		});
 
 		it('should have senderPublicKey hex string equal to sender public key', () => {
-			(registerSecondSignatureTransaction).should.have.property('senderPublicKey').and.be.hexString().and.equal(publicKey);
+			(registerSecondPassphraseTransaction).should.have.property('senderPublicKey').and.be.hexString().and.equal(publicKey);
 		});
 
 		it('should have timestamp number equal to result of time.getTimeWithOffset', () => {
-			(registerSecondSignatureTransaction).should.have.property('timestamp').and.be.type('number').and.equal(timeWithOffset);
+			(registerSecondPassphraseTransaction).should.have.property('timestamp').and.be.type('number').and.equal(timeWithOffset);
 		});
 
 		it('should have signature hex string', () => {
-			(registerSecondSignatureTransaction).should.have.property('signature').and.be.hexString();
+			(registerSecondPassphraseTransaction).should.have.property('signature').and.be.hexString();
 		});
 
 		it('should be signed correctly', () => {
-			const result = cryptoModule.verifyTransaction(registerSecondSignatureTransaction);
+			const result = cryptoModule.verifyTransaction(registerSecondPassphraseTransaction);
 			(result).should.be.ok();
 		});
 
 		it('should not be signed correctly if modified', () => {
-			registerSecondSignatureTransaction.amount = 100;
-			const result = cryptoModule.verifyTransaction(registerSecondSignatureTransaction);
+			registerSecondPassphraseTransaction.amount = 100;
+			const result = cryptoModule.verifyTransaction(registerSecondPassphraseTransaction);
 			(result).should.be.not.ok();
 		});
 
 		it('should have asset object', () => {
-			(registerSecondSignatureTransaction).should.have.property('asset').and.not.be.empty();
+			(registerSecondPassphraseTransaction).should.have.property('asset').and.not.be.empty();
 		});
 
 		it('should not have a signSignature property', () => {
-			(registerSecondSignatureTransaction).should.not.have.property('signSignature');
+			(registerSecondPassphraseTransaction).should.not.have.property('signSignature');
 		});
 
 		describe('signature asset', () => {
 			it('should be an object', () => {
-				(registerSecondSignatureTransaction.asset).should.have.property('signature')
+				(registerSecondPassphraseTransaction.asset).should.have.property('signature')
 					.and.be.type('object')
 					.and.not.be.empty();
 			});
 
 			it('should have a 32-byte publicKey hex string', () => {
-				(registerSecondSignatureTransaction.asset).should.have.property('signature')
+				(registerSecondPassphraseTransaction.asset).should.have.property('signature')
 					.with.property('publicKey')
 					.and.be.hexString();
-				(Buffer.from(registerSecondSignatureTransaction.asset.signature.publicKey, 'hex')).should.have.length(32);
+				(Buffer.from(registerSecondPassphraseTransaction.asset.signature.publicKey, 'hex')).should.have.length(32);
 			});
 
 			it('should have a publicKey equal to the public key for the provided second secret', () => {
-				(registerSecondSignatureTransaction.asset).should.have.property('signature')
+				(registerSecondPassphraseTransaction.asset).should.have.property('signature')
 					.with.property('publicKey')
 					.and.equal(secondPublicKey);
 			});
 
 			it('should have the correct publicKey if the provided second secret is an empty string', () => {
-				registerSecondSignatureTransaction = registerSecondSignature({ secret, secondSecret: '' });
-				(registerSecondSignatureTransaction.asset.signature.publicKey).should.be.equal(
+				registerSecondPassphraseTransaction = registerSecondPassphrase({ secret, secondSecret: '' });
+				(registerSecondPassphraseTransaction.asset.signature.publicKey).should.be.equal(
 					emptyStringPublicKey,
 				);
 			});
