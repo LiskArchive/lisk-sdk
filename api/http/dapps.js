f9743af3bc9cf490a671ce53c4e8588b521ec8c4
Maciej Baj <macie.baj@gmail.com>
2017-10-18 07:37:07 +0200

    Provide options.responseWithCode parameter to sanitize middleware

--- api/http/dapps.js
+++ api/http/dapps.js
@@ -7,11 +7,8 @@ var schema = require('../../schema/dapps');
 /**
  * Binds api with modules and creates common url.
  * - End point: `/api/dapps`
- * - Private API:
- * 	- get	/categories
  * - Sanitized
  * 	- get	/
- * 	- get	/get
  * @memberof module:dapps
  * @requires helpers/Router
  * @requires helpers/httpApi
@@ -24,11 +21,7 @@ function DappsHttpApi (dappsModule, app) {
 
 	var router = new Router();
 
-	router.map(dappsModule.internal, {
-		'get /categories': 'categories'
-	});
-
-	router.get('/', httpApi.middleware.sanitize('query', schema.list, dappsModule.internal.list));
+	router.get('/', httpApi.middleware.sanitize('query', schema.list, {}, dappsModule.internal.list));
 
 	httpApi.registerEndpoint('/api/dapps', app, router, dappsModule.isLoaded);
 }
