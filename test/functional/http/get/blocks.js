5b8dce2c1df5ff678ba138318f362abfa67324e7
Maciej Baj <macie.baj@gmail.com>
2017-10-18 13:44:12 +0200

    Fix blocks/getFees functional tests

--- test/functional/http/get/blocks.js
+++ test/functional/http/get/blocks.js
@@ -66,15 +66,16 @@ describe('GET /api/blocks', function () {
 		});
 	});
 
-	describe('/getfees', function () {
-
+	describe('GET /api/blocks/getFees', function () {
 		it('should be ok', function (done) {
 			http.get('/api/blocks/getFees', function (err, res) {
 				node.expect(res.body).to.have.property('success').to.be.ok;
 				node.expect(res.body).to.have.property('fees');
 				node.expect(res.body.fees.send).to.equal(node.fees.transactionFee);
 				node.expect(res.body.fees.vote).to.equal(node.fees.voteFee);
-				node.expect(res.body.fees.dapp).to.equal(node.fees.dappRegistrationFee);
+				node.expect(res.body.fees.dappRegistration).to.equal(node.fees.dappRegistrationFee);
+				node.expect(res.body.fees.dappDeposit).to.equal(node.fees.dappDepositFee);
+				node.expect(res.body.fees.dappWithdrawal).to.equal(node.fees.dappWithdrawalFee);
 				node.expect(res.body.fees.secondSignature).to.equal(node.fees.secondPasswordFee);
 				node.expect(res.body.fees.delegate).to.equal(node.fees.delegateRegistrationFee);
 				node.expect(res.body.fees.multisignature).to.equal(node.fees.multisignatureRegistrationFee);
