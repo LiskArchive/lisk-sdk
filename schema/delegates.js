b5ddd72d3199974066c288904208a0536007c6ee
Maciej Baj <macie.baj@gmail.com>
2017-10-20 18:41:27 +0200

    Update get delegates schema

--- schema/delegates.js
+++ schema/delegates.js
@@ -29,52 +29,51 @@ module.exports = {
 		},
 		required: ['key', 'publicKey']
 	},
-	getDelegate: {
-		id: 'delegates.getDelegate',
+	getDelegates: {
+		id: 'delegates.getDelegates',
 		type: 'object',
 		properties: {
+
+			address: {
+				type: 'string',
+				format: 'address',
+				minLength: 21,
+				maxLength: 21
+			},
 			publicKey: {
-				type: 'string'
+				type: 'string',
+				format: 'publicKey'
+			},
+			secondPublicKey: {
+				type: 'string',
+				format: 'publicKey'
 			},
 			username: {
 				type: 'string',
 				format: 'username',
 				minLength: 1,
 				maxLength: 20
-			}
-		}
-	},
-	search: {
-		id: 'delegates.search',
-		type: 'object',
-		properties: {
-			q: {
+			},
+			search: {
 				type: 'string',
+				format: 'username',
 				minLength: 1,
 				maxLength: 20
 			},
-			limit: {
-				type: 'integer',
-				minimum: 1,
-				maximum: 1000
-			}
-		},
-		required: ['q']
-	},
-	getDelegates: {
-		id: 'delegates.getDelegates',
-		type: 'object',
-		properties: {
+			rank: {
+				type: 'number',
+				minimum: 1
+			},
 			orderBy: {
 				type: 'string'
 			},
 			limit: {
-				type: 'integer',
+				type: 'number',
 				minimum: 1,
 				maximum: constants.activeDelegates
 			},
 			offset: {
-				type: 'integer',
+				type: 'number',
 				minimum: 0
 			}
 		}
