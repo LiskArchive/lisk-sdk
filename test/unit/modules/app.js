ad4392345db7c0145ed75a4c2661b6c214a496ee
Mona <tschakki@posteo.de>
2017-10-25 14:08:40 +0200

    Fix indents

--- test/unit/modules/app.js
+++ test/unit/modules/app.js
@@ -9,13 +9,12 @@ describe('app', function () {
 
 	before(function (done) {
 		// Run the app on a different than default port
-        process.argv.splice(2,0,'--');
-        process.argv.splice(2,0,config.httpPort += 1);
-        process.argv.splice(2,0,'-h');
-        process.argv.splice(2,0,config.port += 1);
-        process.argv.splice(2,0,'-p');
+		process.argv.splice(2,0,'--');
+		process.argv.splice(2,0,config.httpPort += 1);
+		process.argv.splice(2,0,'-h');
+		process.argv.splice(2,0,config.port += 1);
+		process.argv.splice(2,0,'-p');
 
-        console.log(process.argv);
 		require('../../../app');
 		// Wait for modules to be initialized
 		setTimeout(done, 5000);
