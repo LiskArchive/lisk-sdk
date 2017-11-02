feb5f226e5f834e7a20cf9db8312c3e2d537b91d
Mona <tschakki@posteo.de>
2017-11-02 15:08:37 +0100

    Redescribe tests for helpers/httpApi.js

--- test/unit/helpers/httpApi.js
+++ test/unit/helpers/httpApi.js
@@ -171,39 +171,46 @@ describe('httpApi', function () {
 
 		describe('sanitize', function () {
 
-			it('should return error when report is not valid', function (){
-				var calledSanitize = false;
-				var calledCallback = false;
-				var cb = function (sanitize, binded) {
-					calledCallback = true;
-					return false;
-				};
-				var rfunction = httpApi.middleware.sanitize('prop', null, cb);
-				var req = {
-					prop: 'prop',
-					sanitize: function (propname, schema, fn) {
-						expect(propname).to.equal('prop');
-						var report = {
-							isValid: false,
-							issues: 'a mock error'
-						};
-						expect(fn(null, report, false)).to.be.true;
-						calledSanitize = true;
-					}
-				};
-				var res = {
-					json: function (json) {
-						expect(json).to.eql({success: false, error: 'a mock error'});
-						return true;
-					}
-				};
+			it('should call req.sanitize');
 
-				rfunction(req, res, null);
-				expect(calledSanitize).to.be.true;
-				expect(calledCallback).to.be.false;
+			describe('when report is not valid', function () {
+
+				it('should return error', function (){
+					var calledSanitize = false;
+					var calledCallback = false;
+					var cb = function (sanitize, binded) {
+						calledCallback = true;
+						return false;
+					};
+					var rfunction = httpApi.middleware.sanitize('prop', null, cb);
+					var req = {
+						prop: 'prop',
+						sanitize: function (propname, schema, fn) {
+							expect(propname).to.equal('prop');
+							var report = {
+								isValid: false,
+								issues: 'a mock error'
+							};
+							expect(fn(null, report, false)).to.be.true;
+							calledSanitize = true;
+						}
+					};
+					var res = {
+						json: function (json) {
+							expect(json).to.eql({success: false, error: 'a mock error'});
+							return true;
+						}
+					};
+
+					rfunction(req, res, null);
+					expect(calledSanitize).to.be.true;
+					expect(calledCallback).to.be.false;
+				});
 			});
 
-			it('should return success when report is valid', function (){
+			describe('when report is valid', function () {
+
+				it('should return success');
 
 				var calledSanitize = false;
 				var calledCallback = false;
@@ -239,7 +246,7 @@ describe('httpApi', function () {
 
 		describe('attachResponseHeader', function () {
 
-			it('should add single header when setHeaderCalled is true', function () {
+			it('should add single header', function () {
 				var setHeaderCalled = false;
 				var cbCalled = false;
 				var res = {
@@ -249,12 +256,134 @@ describe('httpApi', function () {
 						setHeaderCalled = true;
 					}
 				};
-				httpApi.middleware.attachResponseHeader('key', 'value', {}, res, function () {cbCalled = true;});
+				httpApi.middleware.attachResponseHeader('key', 'value', {}, res, function () {
+					cbCalled = true;
+				});
 				expect(setHeaderCalled).to.be.true;
 				expect(cbCalled).to.be.true;
 			});
+		});
 
-			it('should add all headers when setHeaderCalled is true', function () {
+		describe('applyAPIAccessRules', function () {
+
+			it('should return true when peering is enabled', function () {
+				var req = {
+					url: {
+						match: function (regex) {
+							return true;
+						}
+					},
+					ip: '127.0.0.1'
+				};
+				var config = {
+					peers: {
+						enabled: false,
+						access: {
+							blackList: false
+						}
+					}
+				};
+				var next = function () {return true;};
+				var res = {
+					status: function (httpCode) {
+						return {
+							send: function (obj) {
+
+							}
+						};
+					}
+				};
+				httpApi.middleware.applyAPIAccessRules(config, req, res, next);
+			});
+
+			it('should return API Access Denied when public is disabled', function () {
+				var sendCalled = false;
+				var nextCalled = false;
+				var matchCalled = false;
+				var req = {
+					url: {
+						match: function (regex) {
+							matchCalled = true;
+							return false;
+						}
+					},
+					ip: '127.0.0.1'
+				};
+				var config = {
+					api: {
+						enabled: true,
+						access: {
+							public: false
+						}
+					}
+				};
+				var next = function () {
+					nextCalled = true;
+					return true;
+				};
+				var res = {
+					status: function (httpCode) {
+						return {
+							send: function (obj) {
+								expect(obj).to.eql({success:false,error:'API access denied'});
+								sendCalled = true;
+							}
+						};
+					}
+				};
+
+				httpApi.middleware.applyAPIAccessRules(config, req, res, next);
+				expect(sendCalled).to.be.true;
+				expect(nextCalled).to.be.false;
+				expect(matchCalled).to.be.true;
+			});
+
+			it('should be ok when ip is whitelisted', function () {
+
+				var sendCalled = false;
+				var nextCalled = false;
+				var matchCalled = false;
+				var req = {
+					url: {
+						match: function (regex) {
+							matchCalled = true;
+							return false;
+						}
+					},
+					ip: '127.0.0.1'
+				};
+				var config = {
+					api: {
+						enabled: true,
+						access: {
+							public: true
+						}
+					}
+				};
+				var next = function () {
+					nextCalled = true;
+					return true;
+				};
+				var res = {
+					status: function (httpCode) {
+						return {
+							send: function (obj) {
+								sendCalled = true;
+							}
+						};
+					}
+				};
+
+				httpApi.middleware.applyAPIAccessRules(config, req, res, next);
+				expect(sendCalled).to.be.false;
+				expect(nextCalled).to.be.true;
+				expect(matchCalled).to.be.true;
+			});
+		});
+
+		describe('attachResponseHeaders', function () {
+
+			it('should add all headers', function () {
 				var headers = {header1: 'header1', header2: 'header2'};
 				var getHeaders = function () { return headers;};
 				var setHeaderCalled = false;
@@ -273,212 +402,191 @@ describe('httpApi', function () {
 
 		describe('useCache', function () {
 
-			it('should return error when useCache is false', function () {
+			describe('when cache.isReady() is false', function () {
+				it('should call next ', function () {
+					var nextCalled = false;
+					var next = function () {
+						nextCalled = true;
+					};
+					httpApi.middleware.useCache(null, {isReady: function () {return false;}}, null, null, next);
+					expect(nextCalled).to.be.true;
+				});
+			});
+
+			it('should call cache.getJsonForKey with key');
+
+			describe('when error is defined', function () {
+				var debugCalled = false;
 				var nextCalled = false;
+				var resJsonCalled = false;
+				var cachedValue = 'cachedValue';
+				var response = {success: true};
+				var req = {
+					originalUrl: 'org/url',
+					url: 'new/url'
+				};
+				var res = {
+					json: function (res,thisresponse) {
+						resJsonCalled = true;
+					}
+				};
+				var logger = {
+					debug: function (str, url) {
+						expect(str).to.equal('cached response for key: ');
+						expect(url).to.equal(req.url);
+						debugCalled = true;
+					}
+				};
 				var next = function () {
 					nextCalled = true;
 				};
-				httpApi.middleware.useCache(null, {isReady: function () {return false;}}, null, null, next);
+				var cache = {
+					isReady: function () {return true;},
+					getJsonForKey: function (key, cb) {
+						cb(null, null);
+					},
+					setJsonForKey: function (key, thisresponse) {
+						expect(key).to.equal(req.originalUrl);
+						expect(thisresponse).to.eql(response);
+					}
+				};
+
+				httpApi.middleware.useCache(logger, cache, req, res, next);
+				res.json({success: true});
 				expect(nextCalled).to.be.true;
+				expect(resJsonCalled).to.be.true;
+				expect(debugCalled).to.be.true;
 			});
 
-			describe('is ready', function () {
+			describe('when cachedValue is undefined', function () {
+				var debugCalled = false;
+				var nextCalled = false;
+				var resJsonCalled = false;
+				var cachedValue = 'cachedValue';
+				var response = {success: true};
+				var req = {
+					originalUrl: 'org/url',
+					url: 'new/url'
+				};
+				var res = {
+					json: function (res,thisresponse) {
+						resJsonCalled = true;
+					}
+				};
+				var logger = {
+					debug: function (str, url) {
+						expect(str).to.equal('cached response for key: ');
+						expect(url).to.equal(req.url);
+						debugCalled = true;
+					}
+				};
+				var next = function () {
+					nextCalled = true;
+				};
+				var cache = {
+					isReady: function () {return true;},
+					getJsonForKey: function (key, cb) {
+						cb(null, null);
+					},
+					setJsonForKey: function (key, thisresponse) {
+						expect(key).to.equal(req.originalUrl);
+						expect(thisresponse).to.eql(response);
+					}
+				};
 
-				it('should return cached value without reqJsonCalled', function () {
-					var debugCalled = false;
-					var nextCalled = false;
-					var cachedValue = 'cachedValue';
-					var req = {
-						originalUrl: 'org/url',
-						url: 'new/url'
-					};
-					var res = {
-						json: function (val) {
-							expect(val).to.equal(cachedValue);
-						}
-					};
-					var logger = {
-						debug: function (str) {
-							expect(str).to.equal('serving response for url: ' + req.url + ' from cache');
-							debugCalled = true;
-						}
-					};
-					var next = function () {
-						nextCalled = true;
-					};
-					var cache = {
-						isReady: function () {return true;},
-						getJsonForKey: function (key, cb) {
-							cb(null, cachedValue);
-						}
-					};
-
-					httpApi.middleware.useCache(logger, cache, req, res, next);
-					expect(nextCalled).to.be.false;
-					expect(debugCalled).to.be.true;
-				});
-
-				it('should return cached value with reqJesonCalled', function () {
-
-					var debugCalled = false;
-					var nextCalled = false;
-					var resJsonCalled = false;
-					var cachedValue = 'cachedValue';
-					var response = {success: true};
-					var req = {
-						originalUrl: 'org/url',
-						url: 'new/url'
-					};
-					var res = {
-						json: function (res,thisresponse) {
-							resJsonCalled = true;
-						}
-					};
-					var logger = {
-						debug: function (str, url) {
-							expect(str).to.equal('cached response for key: ');
-							expect(url).to.equal(req.url);
-							debugCalled = true;
-						}
-					};
-					var next = function () {
-						nextCalled = true;
-					};
-					var cache = {
-						isReady: function () {return true;},
-						getJsonForKey: function (key, cb) {
-							cb(null, null);
-						},
-						setJsonForKey: function (key, thisresponse) {
-							expect(key).to.equal(req.originalUrl);
-							expect(thisresponse).to.eql(response);
-						}
-					};
-
-					httpApi.middleware.useCache(logger, cache, req, res, next);
-					res.json({success: true});
-					expect(nextCalled).to.be.true;
-					expect(resJsonCalled).to.be.true;
-					expect(debugCalled).to.be.true;
-				});
+				httpApi.middleware.useCache(logger, cache, req, res, next);
+				res.json({success: true});
+				expect(nextCalled).to.be.true;
+				expect(resJsonCalled).to.be.true;
+				expect(debugCalled).to.be.true;
 			});
 
-			describe('applyAPIAccessRules', function () {
+			describe('when error is undefined and cachedValue is defined', function () {
+				var debugCalled = false;
+				var nextCalled = false;
+				var cachedValue = 'cachedValue';
+				var req = {
+					originalUrl: 'org/url',
+					url: 'new/url'
+				};
+				var res = {
+					json: function (val) {
+						expect(val).to.equal(cachedValue);
+					}
+				};
+				var logger = {
+					debug: function (str) {
+						expect(str).to.equal('serving response for url: ' + req.url + ' from cache');
+						debugCalled = true;
+					}
+				};
+				var next = function () {
+					nextCalled = true;
+				};
+				var cache = {
+					isReady: function () {return true;},
+					getJsonForKey: function (key, cb) {
+						cb(null, cachedValue);
+					}
+				};
 
-				it('should return true when peering is enabled', function () {
-					var req = {
-						url: {
-							match: function (regex) {
-								return true;
-							}
-						},
-						ip: '127.0.0.1'
-					};
-					var config = {
-						peers: {
-							enabled: false,
-							access: {
-								blackList: false
-							}
-						}
-					};
-					var next = function () {return true;};
-					var res = {
-						status: function (httpCode) {
-							return {
-								send: function (obj) {
-
-								}
-							};
-						}
-					};
-					httpApi.middleware.applyAPIAccessRules(config, req, res, next);
-				});
-
-				it('should return API Access Denied when public is disabled', function () {
-					var sendCalled = false;
-					var nextCalled = false;
-					var matchCalled = false;
-					var req = {
-						url: {
-							match: function (regex) {
-								matchCalled = true;
-								return false;
-							}
-						},
-						ip: '127.0.0.1'
-					};
-					var config = {
-						api: {
-							enabled: true,
-							access: {
-								public: false
-							}
-						}
-					};
-					var next = function () {
-						nextCalled = true;
-						return true;
-					};
-					var res = {
-						status: function (httpCode) {
-							return {
-								send: function (obj) {
-									expect(obj).to.eql({success:false,error:'API access denied'});
-									sendCalled = true;
-								}
-							};
-						}
-					};
-
-					httpApi.middleware.applyAPIAccessRules(config, req, res, next);
-					expect(sendCalled).to.be.true;
-					expect(nextCalled).to.be.false;
-					expect(matchCalled).to.be.true;
-				});
-
-				it('should be ok when ip is whitelisted', function () {
-
-					var sendCalled = false;
-					var nextCalled = false;
-					var matchCalled = false;
-					var req = {
-						url: {
-							match: function (regex) {
-								matchCalled = true;
-								return false;
-							}
-						},
-						ip: '127.0.0.1'
-					};
-					var config = {
-						api: {
-							enabled: true,
-							access: {
-								public: true
-							}
-						}
-					};
-					var next = function () {
-						nextCalled = true;
-						return true;
-					};
-					var res = {
-						status: function (httpCode) {
-							return {
-								send: function (obj) {
-									sendCalled = true;
-								}
-							};
-						}
-					};
-
-					httpApi.middleware.applyAPIAccessRules(config, req, res, next);
-					expect(sendCalled).to.be.false;
-					expect(nextCalled).to.be.true;
-					expect(matchCalled).to.be.true;
-				});
+				httpApi.middleware.useCache(logger, cache, req, res, next);
+				expect(nextCalled).to.be.false;
+				expect(debugCalled).to.be.true;
 			});
+		});
+	});
+
+	describe('respond', function () {
+
+		describe('when err is defined', function () {
+
+			it('should return error');
+
+			var error = 'an error occured';
+			var resJsonCalled = false;
+			var res = {
+				json: function (json) {
+					expect(json.success).to.be.false;
+					expect(json.error).to.equal(error);
+					resJsonCalled = true;
+				}
+			};
+
+			httpApi.respond(res, error, null);
+			expect(resJsonCalled).to.be.true;
+		});
+
+		describe('when err is undefined', function () {
+
+			it('should return success');
+
+			var success = {message: 'successful'};
+			var resJsonCalled = false;
+			var res = {
+				json: function (json) {
+					expect(json.success).to.be.true;
+					expect(json.message).to.equal(success.message);
+					resJsonCalled = true;
+				}
+			};
+
+			httpApi.respond(res, null, success);
+			expect(resJsonCalled).to.be.true;
+
+		});
+	});
+
+	describe('respondWithCode', function () {
+		describe('when err is defined', function () {
+
+			it('should return error');
+		});
+
+		describe('when err is undefined', function () {
+
+			it('should return success');
+
 		});
 	});
 
@@ -505,39 +613,6 @@ describe('httpApi', function () {
 			httpApi.registerEndpoint('route', app, router, false);
 			expect(called).to.equal(2);
 			expect(appUseCalled).to.be.true;
-		});
-	});
-
-	describe('respond', function () {
-
-		it('should return an error when error is provided', function () {
-			var error = 'an error occured';
-			var resJsonCalled = false;
-			var res = {
-				json: function (json) {
-					expect(json.success).to.be.false;
-					expect(json.error).to.equal(error);
-					resJsonCalled = true;
-				}
-			};
-
-			httpApi.respond(res, error, null);
-			expect(resJsonCalled).to.be.true;
-		});
-
-		it('should return success when success is provided', function () {
-			var success = {message: 'successful'};
-			var resJsonCalled = false;
-			var res = {
-				json: function (json) {
-					expect(json.success).to.be.true;
-					expect(json.message).to.equal(success.message);
-					resJsonCalled = true;
-				}
-			};
-
-			httpApi.respond(res, null, success);
-			expect(resJsonCalled).to.be.true;
 		});
 	});
 });
