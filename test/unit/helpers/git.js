dfe434287a62ecc21f5333a7a0d8047efda2fa63
Mona <tschakki@posteo.de>
2017-11-02 12:31:55 +0100

    Redescribe tests for helpers/git.js

--- test/unit/helpers/git.js
+++ test/unit/helpers/git.js
@@ -6,8 +6,11 @@ var sinon = require('sinon');
 var git = require('../../../helpers/git');
 var childProcess = require('child_process');
 
-describe('git', function () {
-	describe('has an error', function () {
+describe('getLastCommit', function () {
+
+	it('should call childProcess.spawnSync');
+
+	describe('when childProcess.spawnSync has an error', function () {
 		var errorMessage = 'an error';
 		beforeEach(function () {
 			sinon.stub(childProcess, 'spawnSync').callsFake(function (name, anArray) {
@@ -21,12 +24,12 @@ describe('git', function () {
 			childProcess.spawnSync.restore();
 		});
 
-		it('validates the error', function () {
+		it('should validate the error', function () {
 			expect(function () { git.getLastCommit();}).to.throw(Error, errorMessage);
 		});
 	});
 
-	describe('does not have an error', function () {
+	describe('when childProcess.spawnSync does not have an error', function () {
 		var standardOut = 'standard out';
 		beforeEach(function () {
 			sinon.stub(childProcess, 'spawnSync').callsFake(function (name, anArray) {
@@ -41,7 +44,7 @@ describe('git', function () {
 			childProcess.spawnSync.restore();
 		});
 
-		it('validates the message to standard out', function () {
+		it('should validate the message to standard out', function () {
 			expect(git.getLastCommit()).to.equal(standardOut);
 		});
 	});
