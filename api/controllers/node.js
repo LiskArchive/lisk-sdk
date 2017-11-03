6477f456797152f825a01f66f26f47bae3574b3d
Nazar Hussain <nazarhussain@gmail.com>
2017-11-03 11:11:58 +0100

    Add feature to validate API responses as per swagger schema

--- api/controllers/node.js
+++ api/controllers/node.js
@@ -16,12 +16,32 @@ function NodeController (scope) {
 
 NodeController.getConstants = function (req, res) {
 	modules.node.shared.getConstants(null, function (err, data){
+
+		// Typecast required integer/bignum attributes while sending data in API
+		data.supply = data.supply.toString();
+		data.reward = data.reward.toString();
+		data.fees.dappDeposit = data.fees.dappDeposit.toString();
+		data.fees.dappWithdrawal = data.fees.dappWithdrawal.toString();
+		data.fees.dappRegistration = data.fees.dappRegistration.toString();
+		data.fees.multisignature = data.fees.multisignature.toString();
+		data.fees.delegate = data.fees.delegate.toString();
+		data.fees.secondSignature = data.fees.secondSignature.toString();
+		data.fees.vote = data.fees.vote.toString();
+		data.fees.send = data.fees.send.toString();
+		data.fees.data = data.fees.data.toString();
+
 		res.send(data);
 	});
 };
 
 NodeController.getStatus = function (req, res) {
 	modules.node.shared.getStatus(null, function (err, data){
+
+		// Check if attributes are null, then set it to 0
+		// as per schema defined for these attributes in swagger
+		data.networkHeight = data.networkHeight || 0;
+		data.consensus = data.consensus || 0;
+
 		res.send(data);
 	});
 };
---
+++ test/common/swaggerSpec.js
@@ -0,0 +1,171 @@
+var node = require('../node');
+var apiSpec = node.swaggerDef;
+var	_ = require('lodash');
+var refsResolved = false;
+var swaggerHelper = require('../../helpers/swagger');
+var validator = swaggerHelper.getValidator();
+
+// Extend Chai assertion with a new method validResponse
+// to facilitate the validation of swagger response body
+// e.g. res.body.should.be.validResponse
+node.chai.use(function (_chai, utils) {
+	_chai.Assertion.addMethod('validResponse', function (responsePath) {
+		var result = validator.validate(utils.flag(this,'object'), apiSpec, {schemaPath: responsePath});
+		var errorDetail = '';
+
+		if (!result) {
+			utils.flag(this, 'message', 'InvalidResponseBody');
+
+			errorDetail = _.map(validator.getLastErrors(), function (object) {
+				return object.code + ': ' + object.path.join('.') + ' | ' + object.message;
+			}).join('\n');
+		}
+
+		this.assert(result, errorDetail);
+	});
+});
+
+/**
+ * A class to make test spec for swagger based endpoint
+ * Can be called with three parameters or only with one in a string form
+ * > new SwaggerTestSpec('GET', '/node/status', 200)
+ * > new SwaggerTestSpec('GET /node/status 200')
+ *
+ * @param {String} method - HTTP method e.g. GET, PUT, POST
+ * @param {String} [apiPath] - API endpoint excluding the base path
+ * @param {Integer} [responseCode] - Expected status code from endpoint
+ * @constructor
+ */
+function SwaggerTestSpec (method, apiPath, responseCode) {
+	if(apiPath && method && responseCode) {
+		this.path = apiPath;
+		this.method = method.toLowerCase();
+		this.responseCode = responseCode;
+	} else if (method) {
+		// Considering that object was created with single param format
+		// 'GET /node/status 200'
+		var specParam = method.split(' ');
+
+		this.path = _.trim(specParam[1]);
+		this.method = _.trim(specParam[0]).toLowerCase();
+		this.responseCode = parseInt(specParam[2]);
+	} else {
+		throw 'SwaggerTestSpec was created with invalid params';
+	}
+
+	this.spec = apiSpec.paths[this.path][this.method];
+	this.responseSpecPath = ['paths', this.path, this.method, 'responses', this.responseCode, 'schema'].join('.');
+	this.responseSpec = this.spec.responses[this.responseCode];
+
+	this.describe = this.method.toUpperCase() + ' ' + apiSpec.basePath + this.path;
+	this.it = 'should respond with status code ' + this.responseCode;
+
+	var self = this;
+
+	this.resolveJSONRefs = function () {
+		if (refsResolved) {
+			return node.Promise.resolve();
+		}
+
+		return swaggerHelper.getResolvedSwaggerSpec().then(function (results) {
+			apiSpec = results;
+			refsResolved = true;
+
+			self.spec = apiSpec.paths[self.path][self.method];
+			self.responseSpec = self.spec.responses[self.responseCode];
+		});
+	};
+}
+
+/**
+ * Perform the actual HTTP call with the spec of current instance
+ *
+ * @param {Object} [parameters] - JSON object of all parameters, including query, post
+ * @return {*|Promise<any>}
+ */
+SwaggerTestSpec.prototype.makeRequest = function (parameters){
+	var query = {};
+	var post = {};
+	var self = this;
+	var callPath = self.path;
+
+	return this.resolveJSONRefs().then(function () {
+
+		_.each(_.keys(parameters), function (param){
+			var p = _.find(self.spec.parameters, {name: param});
+			if(p.in === 'query') {
+				query[param] = parameters[param];
+			} else if (p.in === 'body') {
+				post = parameters[param];
+			} else if (p.in === 'path') {
+				callPath = callPath.replace('{' + param + '}', parameters[param]);
+			}
+		});
+
+		var req = node.supertest(node.baseUrl);
+
+		if (self.method === 'post') {
+			req = req.post(apiSpec.basePath + callPath);
+		} else if (self.method === 'put') {
+			req = req.put(apiSpec.basePath + callPath);
+		} else if (self.method === 'get') {
+			req = req.get(apiSpec.basePath + callPath);
+		}
+
+		req = req.set('Accept', 'application/json')
+			.query(query);
+
+		if (self.method === 'post' || self.method === 'put') {
+			req = req.send(post);
+		}
+
+		node.debug(['> URI:'.grey, req.method, req.url].join(' '));
+
+		if(!_.isEmpty(query)) {
+			node.debug(['> Query:'.grey, JSON.stringify(query)].join(' '));
+		}
+		if(!_.isEmpty(post)) {
+			node.debug(['> Data:'.grey, JSON.stringify(post)].join(' '));
+		}
+		return req;
+	}).then(function (res) {
+
+		node.debug('> Response:'.grey, JSON.stringify(res.body));
+
+		describe(self.describe, function () {
+			it('should response with status code ' + self.responseCode, function () {
+				res.statusCode.should.be.eql(self.responseCode);
+			});
+
+			it('should response json content type ', function () {
+				res.headers['content-type'].should.match(/json/);
+			});
+
+			it('should match the response schema defined in swagger spec', function () {
+				res.body.should.be.validResponse(self.responseSpecPath);
+				//validator.validate(res.body, apiSpec, {schemaPath: self.responseSpecPath}).should.be.true;
+			});
+		});
+
+		return res;
+	})
+		.catch(function (eror){
+			console.log(validator.getLastErrors());
+			throw eror;
+		});
+};
+
+
+/**
+ * A helper method to create an object swagger test spec
+ * Can be called with three parameters or only with one in a string form
+ * > ('GET', '/node/status', 200)
+ * > ('GET /node/status 200')
+ * @param {String} method - HTTP method e.g. GET, PUT, POST
+ * @param {String} [path] - API endpoint excluding the base path
+ * @param {Integer} [responseCode] - Expected status code from endpoint
+ * @return {SwaggerTestSpec}
+ */
+module.exports = function (method, path, responseCode) {
+	return new SwaggerTestSpec(method, method, responseCode);
+};
