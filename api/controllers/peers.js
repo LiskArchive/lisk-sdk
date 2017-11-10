15b88e4bc65e5e08563579a8ae19039298ae17bd
Nazar Hussain <nazar@lightcurve.io>
2017-11-10 12:11:33 +0100

    Update /peers endpoint for standardise response

--- api/controllers/peers.js
+++ api/controllers/peers.js
@@ -16,20 +16,22 @@ function PeersController (scope) {
 	modules = scope.modules;
 }
 
-PeersController.getPeers = function (req, res) {
+PeersController.getPeers = function (context, next) {
+
+	var params = context.request.swagger.params;
 
 	var filters = {
-		ip: req.swagger.params.ip.value,
-		port: req.swagger.params.wsPort.value,
-		httpPort: req.swagger.params.httpPort.value,
-		state: req.swagger.params.state.value,
-		os: req.swagger.params.os.value,
-		version: req.swagger.params.version.value,
-		broadhash: req.swagger.params.broadhash.value,
-		height: req.swagger.params.height.value,
-		limit: req.swagger.params.limit.value,
-		offset: req.swagger.params.offset.value,
-		orderBy: req.swagger.params.sort.value
+		ip: params.ip.value,
+		port: params.wsPort.value,
+		httpPort: params.httpPort.value,
+		state: params.state.value,
+		os: params.os.value,
+		version: params.version.value,
+		broadhash: params.broadhash.value,
+		height: params.height.value,
+		limit: params.limit.value,
+		offset: params.offset.value,
+		orderBy: params.sort.value
 	};
 
 	// Remove filters with null values
@@ -38,6 +40,8 @@ PeersController.getPeers = function (req, res) {
 	});
 
 	modules.peers.shared.getPeers(filters, function (err, data) {
+
+		if (err) { return next(err); }
 
 		data = _.cloneDeep(data);
 
@@ -49,7 +53,14 @@ PeersController.getPeers = function (req, res) {
 			return peer;
 		});
 
-		res.send({peers: data});
+		next(null, {
+			data: data,
+			meta: {
+				offset: filters.offset,
+				limit: filters.limit,
+				total: modules.peers.shared.getPeersCount()
+			}
+		});
 	});
 };
 
