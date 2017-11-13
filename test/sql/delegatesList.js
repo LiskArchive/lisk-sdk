89b56c42026491642e6f6427a107db5eab885b2d
Mariusz Serek <mariusz@serek.net>
2017-11-13 17:11:13 +0100

    Fix delegates list SQL tests

--- test/sql/delegatesList.js
+++ test/sql/delegatesList.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var DelegatesList = {
-	generateDelegatesList: 'SELECT generate_delegates_list AS delegates FROM generateDelegatesList(${round}, ${delegates});',
+	generateDelegatesList: 'SELECT generate_delegates_list AS delegates FROM generate_delegates_list(${round}, ${delegates});',
 
-	generateDelegatesListCast: 'SELECT generate_delegates_list AS delegates FROM generateDelegatesList(${round}, ${delegates}::text[]);',
+	generateDelegatesListCast: 'SELECT generate_delegates_list AS delegates FROM generate_delegates_list(${round}, ${delegates}::text[]);',
 
 	getDelegatesList: 'SELECT get_delegates_list() AS list;',
 
-	getActiveDelegates: 'SELECT ARRAY(SELECT ENCODE(pk, \'hex\') AS pk FROM delegates ORDER BY rank ASC LIMIT 101) AS delegates',
+	getActiveDelegates: 'SELECT ARRAY(SELECT ENCODE(public_key, \'hex\') AS pk FROM delegates ORDER BY rank ASC LIMIT 101) AS delegates',
 
 	getRound: 'SELECT CEIL((SELECT height+1 FROM blocks ORDER BY height DESC LIMIT 1) / 101::float)::int AS round;'
 };
