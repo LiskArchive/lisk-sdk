579676f8eb2ecae43d0c702636ac0a8bd6c2485c
isabello <isabella@lightcurve.io>
2017-11-14 17:46:36 +0100

    Rewrite getVoters query for /api/voters

--- sql/voters.js
+++ sql/voters.js
@@ -7,7 +7,7 @@ var VotersSql = {
 		'publicKey'
 	],
 
-	getVoters: 'SELECT ARRAY_AGG("accountId") AS "accountIds" FROM mem_accounts2delegates WHERE "dependentId" = ${publicKey}'
+	getVoters: 'SELECT ARRAY_AGG(v.voter_address) AS "accountIds" FROM(SELECT DISTINCT ON (voter_address) voter_address, delegate_public_key, type FROM votes_details WHERE delegate_public_key = DECODE(${publicKey}, \'hex\') ORDER BY voter_address, timestamp DESC) v WHERE v.type = \'add\''
 };
 
 module.exports = VotersSql;
