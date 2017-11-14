f2cb7d0e3439c3b1b465713b72264bc17c0d0264
Isabello <isabella@lightcurve.io>
2017-11-15 00:48:25 +0100

    Correct blocks_forged_count column name

--- logic/account.js
+++ logic/account.js
@@ -218,7 +218,7 @@ function Account (db, schema, logger, cb) {
 			name: 'producedBlocks',
 			type: 'BigInt',
 			conv: Number,
-			expression: '(d."blocks_forged_cnt")::bigint'
+			expression: '(d."blocks_forged_count")::bigint'
 		},
 		{
 			name: 'missedBlocks',
