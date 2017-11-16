c0d12843b14f530777ffb716ece60505b72cb29b
isabello <isabella@lightcurve.io>
2017-11-16 13:43:21 +0100

    Use new query to get multisignature members using view

--- sql/multisignatures.js
+++ sql/multisignatures.js
@@ -1,11 +1,8 @@
 'use strict';
 
 var MultisignaturesSql = {
-    // TODO: Need to join this with both multisignatures_master and member
-    getAccountIds: 'SELECT ARRAY_AGG("accountId") AS "accountIds" FROM mem_accounts2multisignatures WHERE "dependentId" = ${publicKey}',
-
-    // TODO: This will replace getAccountIds in 1.1.0
-    getMultisignaturesGroup: 'SELECT ARRAY_AGG(ENCODE("memberPublicKey", \'hex\')) AS "members" FROM multisignatures_list WHERE  ENCODE("masterPublicKey", \'hex\') = ${publicKey}'
+    getAccountIds: 'SELECT ARRAY_AGG(ENCODE("memberPublicKey", \'hex\')) AS "members" FROM multisignatures_list WHERE  ENCODE("masterPublicKey", \'hex\') = ${publicKey}',
+    
 };
 
 module.exports = MultisignaturesSql;
