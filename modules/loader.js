e844d6146bf4534c7098f3e400946318268bfe1a
Lucas Isasmendi <lucas@lightcurve.io>
2017-11-17 12:41:48 +0100

    Add peer parameter to processUnconfirmedTransaction

--- modules/loader.js
+++ modules/loader.js
@@ -265,7 +265,7 @@ __private.loadTransactions = function (cb) {
 			async.eachSeries(transactions, function (transaction, eachSeriesCb) {
 				library.balancesSequence.add(function (cb) {
 					transaction.bundled = true;
-					modules.transactions.processUnconfirmedTransaction(transaction, false, cb);
+					modules.transactions.processUnconfirmedTransaction('peer', transaction, false, cb);
 				}, function (err) {
 					if (err) {
 						// TODO: Validate if must include error propagation.
