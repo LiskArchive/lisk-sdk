d396d8fd8f37d39b6c63a861a272e15fbe346cc1
Maciej Baj <macie.baj@gmail.com>
2017-11-23 18:17:01 +0100

    Add integration tests for blocks propagation

--- test/integration/scenarios/propagation/blocks.js
+++ test/integration/scenarios/propagation/blocks.js
@@ -1,1 +1,46 @@
 'use strict';
+
+var _ = require('lodash');
+var expect = require('chai').expect;
+var Promise = require('bluebird');
+var utils = require('../../utils');
+
+module.exports = function (params) {
+
+	describe('blocks', function () {
+
+		var nodesBlocks;
+
+		before(function () {
+			return Promise.all(params.configurations.map(function (configuration) {
+				return utils.http.getBlocks(configuration.httpPort).then(function (blocksResults) {
+					nodesBlocks = blocksResults;
+				});
+			}));
+		});
+
+		it('should be able to get blocks list from every peer', function () {
+			expect(nodesBlocks).to.have.lengthOf(params.configurations.length);
+		});
+
+		it('should contain non empty blocks after running functional tests', function () {
+			nodesBlocks.forEach(function (blocks) {
+				expect(blocks).to.be.an('array').and.not.empty;
+			});
+		});
+
+		it('should have all peers at the same height', function () {
+			var uniquePeersHeights = _(nodesBlocks).map('length').uniq().value();
+			expect(uniquePeersHeights).to.have.lengthOf.at.least(1);
+		});
+
+		it('should have all blocks the same at all peers', function () {
+			var patternBlocks = nodesBlocks[0];
+			for (var i = 0; i < patternBlocks.length; i += 1) {
+				for (var j = 1; j < nodesBlocks.length; j += 1) {
+					expect(_.isEqual(nodesBlocks[j][i], patternBlocks[i]));
+				}
+			}
+		});
+	});
+};
