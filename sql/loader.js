98bc2b17a5615c05ab694de8794328278b696c25
Oliver Beddows <oliver@lightcurve.io>
2017-11-24 15:05:22 +0100

    Review sql/loader.js

--- sql/loader.js
+++ sql/loader.js
@@ -5,7 +5,7 @@ var LoaderSql = {
 
 	getGenesisBlock: 'SELECT "block_id" AS "id", "payload_hash" AS "payloadHash", "signature" AS "blockSignature" FROM blocks WHERE "height" = 1',
 
-//TODO: Trash these, or reuse for something else if logic is ok.
+	// TODO: Trash these, or reuse for something else if logic is ok
 	countMemAccounts: 'SELECT COUNT(*)::int FROM accounts a WHERE a."transaction_id" IS NOT NULL',
 
 	updateMemAccounts: 'UPDATE mem_accounts SET "u_isDelegate" = "isDelegate", "u_secondSignature" = "secondSignature", "u_username" = "username", "u_balance" = "balance", "u_delegates" = "delegates", "u_multisignatures" = "multisignatures", "u_multimin" = "multimin", "u_multilifetime" = "multilifetime" WHERE "u_isDelegate" <> "isDelegate" OR "u_secondSignature" <> "secondSignature" OR "u_username" <> "username" OR "u_balance" <> "balance" OR "u_delegates" <> "delegates" OR "u_multisignatures" <> "multisignatures" OR "u_multimin" <> "multimin" OR "u_multilifetime" <> "multilifetime";',
