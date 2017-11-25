2a6ad03be70e20b5ed43844ea84c5b2f23e93806
Oliver Beddows <oliver@lightcurve.io>
2017-11-25 17:25:15 +0100

    Review sql/migrations/20171022144049_create_constaints.sql

--- sql/migrations/20171022144049_create_constaints.sql
+++ sql/migrations/20171022144049_create_constaints.sql
@@ -2,39 +2,50 @@ BEGIN;
 
 DO language plpgsql $$
 BEGIN
-	RAISE NOTICE 'Creating constraints tables, please wait...';
+	RAISE NOTICE 'Adding constraints to tables, please wait...';
 END
 $$;
 
+ALTER TABLE votes
+	ADD CONSTRAINT fkey_votes_transaction_id
+	FOREIGN KEY(transaction_id)
+	REFERENCES transactions(transaction_id)
+	ON DELETE CASCADE;
 
-  -- Create new Foreign Key relations
-  ALTER TABLE "public".votes ADD CONSTRAINT "fkey_votes_transaction_id" FOREIGN KEY ( "transaction_id" ) REFERENCES "public".transactions( "transaction_id" )
-  ON
-  DELETE CASCADE;
+ALTER TABLE intransfer
+	ADD CONSTRAINT fkey_intransfer_transaction_id
+	FOREIGN KEY(transaction_id)
+	REFERENCES transactions(transaction_id)
+	ON DELETE CASCADE;
 
-  ALTER TABLE "public".intransfer ADD CONSTRAINT "fkey_intransfer_transaction_id" FOREIGN KEY ( "transaction_id" ) REFERENCES "public".transactions( "transaction_id" )
-  ON
-  DELETE CASCADE;
+ALTER TABLE outtransfer
+	ADD CONSTRAINT fkey_outtransfer_transaction_id
+	FOREIGN KEY(transaction_id)
+	REFERENCES transactions(transaction_id)
+	ON DELETE CASCADE;
 
-  ALTER TABLE "public".outtransfer ADD CONSTRAINT "fkey_outtransfer_transaction_id" FOREIGN KEY ( "transaction_id" ) REFERENCES "public".transactions( "transaction_id" )
-  ON
-  DELETE CASCADE;
+ALTER TABLE multisignatures_master
+	ADD CONSTRAINT fkey_multisignatures_master_transaction_id
+	FOREIGN KEY(transaction_id)
+	REFERENCES transactions(transaction_id)
+	ON DELETE CASCADE;
 
-  ALTER TABLE "public".multisignatures_master ADD CONSTRAINT "fkey_multisignatures_master_transaction_id" FOREIGN KEY ( "transaction_id" ) REFERENCES "public".transactions( "transaction_id" )
-  ON
-  DELETE CASCADE;
+ALTER TABLE multisignatures_member
+	ADD CONSTRAINT fkey_multisignatures_member_transaction_id
+	FOREIGN KEY(transaction_id)
+	REFERENCES transactions(transaction_id)
+	ON DELETE CASCADE;
 
-  ALTER TABLE "public".multisignatures_member ADD CONSTRAINT "fkey_multisignatures_member_transaction_id" FOREIGN KEY ( "transaction_id" ) REFERENCES "public".transactions( "transaction_id" )
-  ON
-  DELETE CASCADE;
+ALTER TABLE second_signature
+	ADD CONSTRAINT fkey_second_signature_transaction_id
+	FOREIGN KEY(transaction_id)
+	REFERENCES transactions(transaction_id)
+	ON DELETE CASCADE;
 
-  ALTER TABLE "public".second_signature ADD CONSTRAINT "fkey_second_signature_transaction_id" FOREIGN KEY ( "transaction_id" ) REFERENCES "public".transactions( "transaction_id" )
-  ON
-  DELETE CASCADE;
+ALTER TABLE delegates
+	ADD CONSTRAINT fkey_delegates_transaction_id
+	FOREIGN KEY(transaction_id)
+	REFERENCES transactions(transaction_id)
+	ON DELETE CASCADE;
 
-  ALTER TABLE "public".delegates ADD CONSTRAINT "fkey_delegates_transaction_id" FOREIGN KEY ( "transaction_id" ) REFERENCES "public".transactions( "transaction_id" )
-  ON
-  DELETE CASCADE;
-
-
-END;
+COMMIT;
