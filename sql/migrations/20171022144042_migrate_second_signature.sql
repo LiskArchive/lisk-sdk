3722f86adee000b899c01033c6cf929c188f1057
Oliver Beddows <oliver@lightcurve.io>
2017-11-25 17:23:02 +0100

    Review sql/migrations/20171022144042_migrate_second_signature.sql

--- sql/migrations/20171022144042_migrate_second_signature.sql
+++ sql/migrations/20171022144042_migrate_second_signature.sql
@@ -6,33 +6,33 @@ BEGIN
 END
 $$;
 
-/* Begin second signature migration */
-CREATE TABLE "public".second_signature(transaction_id varchar(20) NOT NULL,
-  public_key bytea NOT NULL,
-  second_public_key bytea NOT NULL,
-  CONSTRAINT pk_second_signature PRIMARY KEY(public_key));
+-- Create 'second_signature' table
+CREATE TABLE second_signature(
+	transaction_id varchar(20) NOT NULL,
+	public_key bytea NOT NULL,
+	second_public_key bytea NOT NULL,
+	CONSTRAINT pk_second_signature PRIMARY KEY(public_key)
+);
 
-
+-- Insert data into 'second_signature' table from 'transactions' table
 INSERT INTO second_signature(transaction_id, public_key, second_public_key)
-SELECT t.transaction_id,
-  t."sender_public_key",
-  ma."secondPublicKey"
-FROM "public".transactions t,  mem_accounts ma
-WHERE ma."secondPublicKey"
-IS NOT NULL
-AND t."sender_address" = ma."address"
-AND t.type = 1;
+	SELECT
+		t.transaction_id,
+		t.sender_public_key,
+		ma."secondPublicKey"
+	FROM transactions t, mem_accounts ma
+	WHERE ma."secondPublicKey" IS NOT NULL
+	AND t.sender_address = ma.address
+	AND t.type = 1;
 
+-- Create indexes
+CREATE INDEX idx_second_signature_transaction_id
+ON second_signature(transaction_id);
 
-  -- Second signature indexes
-  CREATE INDEX idx_second_signature_transaction_id
-  ON "public".second_signature ( "transaction_id" );
+CREATE INDEX idx_public_key
+ON second_signature(public_key);
 
-  CREATE INDEX idx_public_key
-  ON "public".second_signature ( "public_key" );
+CREATE INDEX idx_second_public_key
+ON second_signature(second_public_key);
 
-  CREATE INDEX idx_second_public_key
-  ON "public".second_signature ( "second_public_key" );
-
-
-END;
+COMMIT;
