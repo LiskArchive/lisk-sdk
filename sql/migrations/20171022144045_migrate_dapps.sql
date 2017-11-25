4008f26b27c96bce494b2e6b18178f00aca6d33e
Oliver Beddows <oliver@lightcurve.io>
2017-11-25 17:23:54 +0100

    Review sql/migrations/20171022144045_migrate_dapps.sql

--- sql/migrations/20171022144045_migrate_dapps.sql
+++ sql/migrations/20171022144045_migrate_dapps.sql
@@ -6,47 +6,49 @@ BEGIN
 END
 $$;
 
-/* Begin outtransfer migration */
+-- Rename columns on 'outtransfer' table
 ALTER TABLE outtransfer RENAME "transactionId" TO "transaction_id";
 ALTER TABLE outtransfer RENAME "dappId" TO "dapp_id";
 ALTER TABLE outtransfer RENAME "outTransactionId" TO "out_transaction_id";
 
-/* Begin intransfer migration */
+-- Rename columns on 'intransfer' table
 ALTER TABLE intransfer RENAME "dappId" TO "dapp_id";
 ALTER TABLE intransfer RENAME "transactionId" TO "transaction_id";
 
-/* Begin dapps migration */
+-- Rename 'dapps' table to 'dapps_old'
 ALTER TABLE dapps RENAME TO dapps_old;
 
-CREATE TABLE "public".dapps(transaction_id varchar(20) NOT NULL,
-  name varchar(32) NOT NULL,
-  description varchar(160),
-  tags varchar(160),
-  LINK text, TYPE integer NOT NULL,
-  category integer NOT NULL,
-  icon text, owner_public_key bytea NOT NULL,
-  CONSTRAINT pk_dapps_transaction_id PRIMARY KEY(transaction_id));
+CREATE TABLE dapps(
+	transaction_id varchar(20) NOT NULL,
+	name varchar(32) NOT NULL,
+	description varchar(160),
+	tags varchar(160),
+	LINK text, TYPE integer NOT NULL,
+	category integer NOT NULL,
+	icon text, owner_public_key bytea NOT NULL,
+	CONSTRAINT pk_dapps_transaction_id PRIMARY KEY(transaction_id)
+);
 
-/* Populate new dapps table */
-INSERT INTO "public".dapps(transaction_id, name, description, tags, LINK, TYPE, category, icon, owner_public_key)
-SELECT DISTINCT d."transactionId",
-  d.name,
-  d.description,
-  d.tags,
-  d.link,
-  d.type,
-  d.category,
-  d.icon,
-  t."sender_public_key"
-FROM transactions t,dapps_old d
-WHERE t.type = 5
-AND d."transactionId" = t."transaction_id";
+-- Insert data into 'dapps' table from 'transactions' table
+INSERT INTO dapps(transaction_id, name, description, tags, LINK, TYPE, category, icon, owner_public_key)
+	SELECT DISTINCT d."transactionId",
+		d.name,
+		d.description,
+		d.tags,
+		d.link,
+		d.type,
+		d.category,
+		d.icon,
+		t."sender_public_key"
+	FROM transactions t,dapps_old d
+	WHERE t.type = 5
+	AND d."transactionId" = t."transaction_id";
 
-  -- Dapps indexes
-  CREATE INDEX idx_dapps_name
-  ON "public".dapps ( name );
+-- Create indexes
+CREATE INDEX idx_dapps_name
+ON dapps(name);
 
-  CREATE INDEX idx_dapps_transactions_id
-  ON "public".dapps ( "transaction_id" );
+CREATE INDEX idx_dapps_transactions_id
+ON dapps(transaction_id);
 
-END;
+COMMIT;
