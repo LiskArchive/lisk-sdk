c6dfbe7f3061bc792d4e06192fa1a753bb2f59a5
Oliver Beddows <oliver@lightcurve.io>
2017-11-25 17:25:58 +0100

    Review sql/multisignatures.js

--- sql/multisignatures.js
+++ sql/multisignatures.js
@@ -1,8 +1,7 @@
 'use strict';
 
 var MultisignaturesSql = {
-    getAccountIds: 'SELECT ARRAY_AGG(ENCODE("memberPublicKey", \'hex\')) AS "members" FROM multisignatures_list WHERE  ENCODE("masterPublicKey", \'hex\') = ${publicKey}',
-    
+	getAccountIds: 'SELECT ARRAY_AGG(ENCODE("memberPublicKey", \'hex\')) AS "members" FROM multisignatures_list WHERE ENCODE("masterPublicKey", \'hex\') = ${publicKey}',
 };
 
 module.exports = MultisignaturesSql;
