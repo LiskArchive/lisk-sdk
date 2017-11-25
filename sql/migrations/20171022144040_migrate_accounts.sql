087e8f293deef078fbc04b48c23802bf243ea5e4
Oliver Beddows <oliver@lightcurve.io>
2017-11-25 17:22:27 +0100

    Review sql/migrations/20171022144040_migrate_accounts.sql

--- sql/migrations/20171022144040_migrate_accounts.sql
+++ sql/migrations/20171022144040_migrate_accounts.sql
@@ -6,58 +6,72 @@ BEGIN
 END
 $$;
 
---Apply transactions into new table, to populate new accounts table
-INSERT INTO "public".accounts(address, public_key, balance)
-SELECT
-  mem.address,
-  mem."publicKey",
-  mem.balance
-FROM mem_accounts mem;
+-- Insert data into 'accounts' table from 'mem_accounts' table
+INSERT INTO accounts(address, public_key, balance)
+	SELECT
+		mem.address,
+		mem."publicKey",
+		mem.balance
+	FROM mem_accounts mem;
 
---UPDATE all accounts with first transaction
-UPDATE "public".accounts AS a set transaction_id = t.transaction_id
-FROM(
-  SELECT t."transaction_id", t."recipient_address"
-  FROM transactions AS t group by t."recipient_address", t."transaction_id"
-) AS t
-WHERE UPPER(t."recipient_address") = a."address";
+-- Update all accounts with first transaction
+UPDATE accounts AS a SET transaction_id = t.transaction_id
+	FROM (
+		SELECT
+			t.transaction_id,
+			t.recipient_address
+		FROM transactions AS t
+		GROUP BY t.recipient_address, t.transaction_id
+	) AS t
+	WHERE UPPER(t.recipient_address) = a.address;
 
---UPDATE genesis transaction ID
-UPDATE "public".accounts AS a set transaction_id = t.transaction_id
-FROM(
-  SELECT t."transaction_id", t."sender_address"
-  FROM transactions AS t WHERE t.type = '2'
-  group by t."recipient_address", t."transaction_id"
-) AS t
-WHERE UPPER(t."sender_address") = a."address"
-AND a.transaction_id IS NULL;
+--- Update all accounts with first transaction
+UPDATE accounts AS a SET transaction_id = t.transaction_id
+	FROM (
+		SELECT
+			t.transaction_id,
+			t.sender_address
+		FROM transactions AS t
+		WHERE t.type = '2'
+		GROUP BY t.recipient_address, t.transaction_id
+	) AS t
+	WHERE UPPER(t.sender_address) = a.address
+	AND a.transaction_id IS NULL;
 
---UPDATE all acounts with publickeys
-UPDATE "public".accounts AS a set public_key_transaction_id = t.transaction_id
-FROM(
-  SELECT t."transaction_id", t."sender_public_key"
-  FROM transactions AS t group by t."sender_public_key", t."transaction_id"
-) AS t
-WHERE t."sender_public_key" = a."public_key";
+-- Update all accounts with public keys
+UPDATE accounts AS a SET public_key_transaction_id = t.transaction_id
+	FROM (
+		SELECT
+			t.transaction_id,
+			t.sender_public_key
+		FROM transactions AS t
+		GROUP BY
+			t.sender_public_key,
+			t.transaction_id
+	) AS t
+	WHERE t.sender_public_key = a.public_key;
 
---UPDATE all acounts with transaction ids
-UPDATE "public".accounts AS a set transaction_id = t.transaction_id
-FROM(
-  SELECT t."transaction_id", t."recipient_address"
-  FROM transactions AS t
-	group by t."recipient_address", t."transaction_id"
-) AS t
-WHERE t."recipient_address" = a."address";
+-- Update all accounts with transaction IDs
+UPDATE accounts AS a SET transaction_id = t.transaction_id
+	FROM (
+		SELECT
+			t.transaction_id,
+			t.recipient_address
+		FROM transactions AS t
+		GROUP BY
+			t.recipient_address,
+			t.transaction_id
+	) AS t
+	WHERE t.recipient_address = a.address;
 
+-- Create indexes
+CREATE INDEX idx_accounts_public_key_transaction_id
+ON accounts(public_key_transaction_id);
 
--- Create new indexes
-  CREATE INDEX idx_accounts_public_key_transaction_id
-  ON "public".accounts( public_key_transaction_id );
+CREATE INDEX idx_accounts_address_upper
+ON accounts(upper((address)::text));
 
-  CREATE INDEX idx_accounts_address_upper
-  ON "public".accounts( upper((address)::text) );
+CREATE INDEX idx_accounts_balance
+ON accounts(balance);
 
-  CREATE INDEX idx_accounts_balance
-  ON "public".accounts( balance );
-
-END;
+COMMIT;
