be5c6a037dc56aad103c2b3a47dac2b4191f13fa
Oliver Beddows <oliver@lightcurve.io>
2017-11-27 20:08:15 +0100

    Standardise code

--- api/controllers/voters.js
+++ api/controllers/voters.js
@@ -40,11 +40,12 @@ VotersController.getVoters = function (context, next) {
 
 	if (!(filters.username || filters.address || filters.publicKey || filters.secondPublicKey)) {
 		var error = generateParamsErrorObject(
-			[params.address, params.publicKey, params.secondPublicKey, params.username], [
-				'address is required if publicKey, secondPublicKey and username not provided.',
-				'publicKey is required if address, secondPublicKey and username not provided.',
-				'secondPublicKey is required if address, publicKey and username not provided.',
-				'username is required if publicKey, secondPublicKey and address not provided.']);
+			[params.address, params.publicKey, params.secondPublicKey, params.username],
+			['address is required if publicKey, secondPublicKey and username not provided.',
+			 'publicKey is required if address, secondPublicKey and username not provided.',
+			 'secondPublicKey is required if address, publicKey and username not provided.',
+			 'username is required if publicKey, secondPublicKey and address not provided.']
+		);
 
 		return next(error);
 	}
