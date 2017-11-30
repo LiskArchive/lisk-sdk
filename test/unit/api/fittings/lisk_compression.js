63b1d8db7832cd44cc52cc6dff73e4a84131e19d
Nazar Hussain <nazar@lightcurve.io>
2017-11-30 16:53:39 +0100

    Add placeholder test files for differnt fitting modules

---
+++ test/unit/api/fittings/lisk_compression.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe('lisk_compression', function () {
+
+	it('should be a factory function that names 2 argument');
+
+	it('should create a middleware accepting 2 arguments');
+
+	it('should accept level, chunkSize, memLevel as config options');
+
+	it('should compress the response as per specified level');
+});
