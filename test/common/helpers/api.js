f3ec9a62510f666ebcc3cb5a9c613ee1c9c5abfa
Lucas Isasmendi <lucas@lightcurve.io>
2017-12-11 11:56:27 +0100

    Fix merge issue with api get

--- test/common/helpers/api.js
+++ test/common/helpers/api.js
@@ -101,7 +101,7 @@ function getTransactions (params, cb) {
 }
 
 function getUnconfirmedTransaction (transaction, cb) {
-	http.get('/api/transactions/unconfirmed/?id=' + transaction, httpCallbackHelper.bind(null, cb));
+	http.get('/api/transactions/unconfirmed/get?id=' + transaction, httpCallbackHelper.bind(null, cb));
 }
 
 function getUnconfirmedTransactions (cb) {
@@ -109,7 +109,7 @@ function getUnconfirmedTransactions (cb) {
 }
 
 function getQueuedTransaction (transaction, cb) {
-	http.get('/api/transactions/queued/?id=' + transaction, httpCallbackHelper.bind(null, cb));
+	http.get('/api/transactions/queued/get?id=' + transaction, httpCallbackHelper.bind(null, cb));
 }
 
 function getQueuedTransactions (cb) {
