c9440f08510e924cddb757f99bc2329377990f25
Lucas Isasmendi <lucas@lightcurve.io>
2017-12-11 18:41:57 +0100

    Remove applyUnconfirmed and undoUnconfirmed from attachAssetType

--- logic/transaction.js
+++ logic/transaction.js
@@ -69,7 +69,6 @@ Transaction.prototype.attachAssetType = function (typeId, instance) {
 		typeof instance.calculateFee === 'function' && typeof instance.verify === 'function' &&
 		typeof instance.objectNormalize === 'function' && typeof instance.dbRead === 'function' &&
 		typeof instance.apply === 'function' && typeof instance.undo === 'function' &&
-		typeof instance.applyUnconfirmed === 'function' && typeof instance.undoUnconfirmed === 'function' &&
 		typeof instance.ready === 'function' && typeof instance.process === 'function'
 	) {
 		__private.types[typeId] = instance;
