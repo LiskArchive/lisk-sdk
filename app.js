2fb12b5be7ccd4bcb3189e4883fd02c84dc5dd3f
Nazar Hussain <nazar@lightcurve.io>
2017-12-15 11:47:03 +0100

    Update list of params to be ignored by parsing

--- app.js
+++ app.js
@@ -325,7 +325,7 @@ d.run(function () {
 			scope.network.app.use(bodyParser.json({limit: '2mb'}));
 			scope.network.app.use(methodOverride());
 
-			var ignore = ['id', 'name', 'lastBlockId', 'blockId', 'transactionId', 'address', 'recipientId', 'senderId', 'previousBlock'];
+			var ignore = ['id', 'name', 'username', 'blockId', 'transactionId', 'address', 'recipientAddress', 'senderAddress'];
 
 			scope.network.app.use(queryParser({
 				parser: function (value, radix, name) {
