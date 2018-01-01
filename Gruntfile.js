2ca365da34c30b1bca5a57a98dd6e3c72e21e872
Oliver Beddows <oliver@lightcurve.io>
2018-01-01 22:57:30 +0100

    Fail mocha task fatally

--- Gruntfile.js
+++ Gruntfile.js
@@ -142,7 +142,7 @@ module.exports = function (grunt) {
 
 	grunt.registerTask('mocha', 'Run test suite.', function (tag, suite, section) {
 		if (['unit', 'functional', 'integration'].indexOf(suite) < 0) {
-			grunt.fail.warn('Please specify a test suite to run.\n\nExample: `grunt mocha:<tag>:<suite>:[section]` or `npm test -- mocha:<tag>:<suite>:[section]`\n\n- Where tag can be one of slow | unstable | untagged | extensive (required)\n- Where suite can be one of unit | functional | integration (required)\n- Where section can be one of get | post | ws | system (optional)');
+			grunt.fail.fatal('Please specify a test suite to run.\n\nExample: `grunt mocha:<tag>:<suite>:[section]` or `npm test -- mocha:<tag>:<suite>:[section]`\n\n- Where tag can be one of slow | unstable | untagged | extensive (required)\n- Where suite can be one of unit | functional | integration (required)\n- Where section can be one of get | post | ws | system (optional)');
 		} else {
 			grunt.task.run('eslint');
 			var toExecute = [
