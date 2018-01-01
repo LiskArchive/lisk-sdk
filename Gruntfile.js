219bca7300b501bcadd3e98d36f08dfaf1f291b5
Oliver Beddows <oliver@lightcurve.io>
2018-01-01 23:06:44 +0100

    Move mocha task into separate file

--- Gruntfile.js
+++ Gruntfile.js
@@ -140,20 +140,6 @@ module.exports = function (grunt) {
 	grunt.registerTask('coverageReport', ['exec:coverageReport']);
 	grunt.registerTask('eslint-nofix', ['eslint']);
 
-	grunt.registerTask('mocha', 'Run test suite.', function (tag, suite, section) {
-		if (['unit', 'functional', 'integration'].indexOf(suite) < 0) {
-			grunt.fail.fatal('Please specify a test suite to run.\n\nExample: `grunt mocha:<tag>:<suite>:[section]` or `npm test -- mocha:<tag>:<suite>:[section]`\n\n- Where tag can be one of slow | unstable | untagged | extensive (required)\n- Where suite can be one of unit | functional | integration (required)\n- Where section can be one of get | post | ws | system (optional)');
-		} else {
-			grunt.task.run('eslint');
-			var toExecute = [
-				tag,
-				suite,
-				section
-			].filter(function (val) { return val; }).join(':');
-			grunt.task.run('exec:mocha:' + toExecute);
-		}
-	});
-
 	grunt.registerTask('eslint-fix', 'Run eslint and fix formatting', function () {
 		grunt.config.set('eslint.options.fix', true);
 		grunt.task.run('eslint');
