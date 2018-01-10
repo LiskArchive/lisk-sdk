eae467d9f8326a699122528024f5326512955e87
tosch110 <tobias@lightcurve.io>
2018-01-10 12:45:38 +0100

    :books: Add license header to sql

--- sql/migrations/20171207000007_recreateFullBlockListView.sql
+++ sql/migrations/20171207000007_recreateFullBlockListView.sql
@@ -1,8 +1,21 @@
+/*
+ * Copyright © 2018 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
 /**
  * Recreate full_blocks_list, add join for transfer table.
  */
 
-BEGIN; 
+BEGIN;
 
 DROP VIEW IF EXISTS full_blocks_list;
 
