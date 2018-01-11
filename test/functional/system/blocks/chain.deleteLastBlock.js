562fa39e9b0dae8f76cf0c12276bf2a7d7a761dc
Lucas Isasmendi <lucas@lightcurve.io>
2018-01-11 17:21:10 +0100

    Add test for single transaction type 5

--- test/functional/system/blocks/chain.deleteLastBlock.js
+++ test/functional/system/blocks/chain.deleteLastBlock.js
@@ -486,6 +486,74 @@ describe('blocks/chain', function () {
 					});
 				});
 			});
+
+			describe('dapps' ,function () {
+
+				before('create account with funds', function (done) {
+					createAccountWithFunds(done);
+					fieldsToCompare = ['balance', 'u_balance', 'blockId', 'virgin', 'publicKey'];
+				});
+
+				describe('(type 5) register dapp', function () {
+	
+					it('should validate account data from sender', function (done) {
+						library.logic.account.get({address: testAccount.address}, fieldsToCompare, function (err, res) {
+							testAccountData = res;
+							expect(res.virgin).to.equal(1);
+							expect(res.publicKey).to.be.null;
+							done();
+						});
+					});
+	
+					it('should forge a block', function (done) {
+						var dappTransaction = lisk.dapp.createDapp(testAccount.password, null, randomUtil.guestbookDapp);
+						randomUtil.guestbookDapp.id = dappTransaction.id;
+						localCommon.addTransactionsAndForge(library, [dappTransaction], done);
+					});
+	
+					it('should validate account data from sender', function (done) {
+						library.logic.account.get({address: testAccount.address}, fieldsToCompare, function (err, res) {
+							testAccountDataAfterBlock = res;
+							expect(res.virgin).to.equal(0);
+							expect(res.publicKey).to.not.be.null;
+							done();
+						});
+					});
+	
+					it('should delete last block', function (done) {
+						library.modules.blocks.chain.deleteLastBlock(function (err, res) {
+							expect(err).to.not.exist;
+							done();
+						});
+					});
+	
+					it('should validate account data from sender', function (done) {
+						library.logic.account.get({address: testAccount.address}, fieldsToCompare, function (err, res) {
+							expect(res.balance).to.equal(testAccountData.balance);
+							expect(res.u_balance).to.equal(testAccountData.u_balance);
+							// FIXME: incorrect blockId
+							// CHECKME: publicKey should be null
+							// CHECKME: virgin should be 1
+							done();
+						});
+					});
+	
+					it('should forge a block with pool transaction', function (done) {
+						localCommon.addTransactionsAndForge(library, [], done);
+					});
+	
+					it('should validate account data from sender', function (done) {
+						library.logic.account.get({address: testAccount.address}, fieldsToCompare, function (err, res) {
+							expect(res.balance).to.equal(testAccountDataAfterBlock.balance);
+							expect(res.u_balance).to.equal(testAccountDataAfterBlock.u_balance);
+							expect(res.publicKey).to.equal(testAccountDataAfterBlock.publicKey);
+							expect(res.virgin).to.equal(0);
+							// CHECKME: blockId
+							done();
+						});
+					});
+				});
+			});
 		});
 
 		describe('multiple transactions scenarios: create transactions, forge, delete block, forge again', function () {
