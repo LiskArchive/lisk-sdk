2eda4575111d183707e7d2aae27c4d2f531a1b48
Maciej Baj <macie.baj@gmail.com>
2018-01-22 17:25:35 +0100

    Add tests to check forge and sync process exclusiveness

--- test/system/synchronousTasks.js
+++ test/system/synchronousTasks.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var expect = require('chai').expect;
+var Rx = require('rx');
 var application = require('./../common/application');
 
 describe('synchronousTasks', function () {
@@ -17,13 +19,76 @@ describe('synchronousTasks', function () {
 		application.cleanup(done);
 	});
 
-	describe('when "attempt to forge" synchronous tasks runs every 100 ms and takes 200 ms', function () {
+	describe('when events are emitted after any of synchronous task starts', function () {
 
-		describe('when "blockchain synchronization" synchronous tasks runs every 100 ms and takes 200 ms', function () {
+		var attemptToForgeRunningSubject = new Rx.BehaviorSubject();
+		var synchronizeBlockchainRunningSubject = new Rx.BehaviorSubject();
 
-			it('"attempt to forge" task should never start when "blockchain synchronization" task is running');
+		describe('when "attempt to forge" synchronous tasks runs every 100 ms and takes 101 ms', function () {
 
-			it('"blockchain synchronization" task should never start when "attempt to forge" task is running');
+			var intervalMs = 100;
+			var durationMs = intervalMs + 1;
+
+			before(function () {
+				library.modules.delegates.onBlockchainReady = library.rewiredModules.delegates.prototype.onBlockchainReady;
+				library.rewiredModules.delegates.__set__('__private.forgeAttemptInterval', intervalMs);
+				library.rewiredModules.delegates.__set__('__private.nextForge', function (nextForgeCb) {
+					attemptToForgeRunningSubject.onNext(true);
+					setTimeout(function () {
+						attemptToForgeRunningSubject.onNext(false);
+						nextForgeCb();
+					}, durationMs);
+				});
+				library.modules.delegates.onBlockchainReady();
+			});
+
+			describe('when "blockchain synchronization" synchronous tasks runs every 100 ms and takes 101 ms', function () {
+
+				before(function () {
+					library.rewiredModules.loader.__set__('__private.syncInterval', intervalMs);
+					library.rewiredModules.loader.__set__('__private.sync', function (nextForgeCb) {
+						synchronizeBlockchainRunningSubject.onNext(true);
+						setTimeout(function () {
+							synchronizeBlockchainRunningSubject.onNext(false);
+							nextForgeCb();
+						}, durationMs);
+					});
+					var jobsQueue = require('../../helpers/jobsQueue');
+					var originalLoaderSyncTimerJob = jobsQueue.jobs['loaderSyncTimer'];
+					clearTimeout(originalLoaderSyncTimerJob);  // Terminate original job
+					jobsQueue.jobs['loaderSyncTimer'] = null;  // Remove original job
+					library.modules.loader.onPeersReady();     // Execute the mocked blockchain synchronization process
+				});
+
+				describe('Within 5000 ms', function () {
+
+					beforeEach(function () {
+						setTimeout(function () {
+							attemptToForgeRunningSubject.onCompleted();
+							synchronizeBlockchainRunningSubject.onCompleted();
+							attemptToForgeRunningSubject = new Rx.BehaviorSubject();
+							synchronizeBlockchainRunningSubject = new Rx.BehaviorSubject();
+						}, 5000);
+					});
+
+					after(function () {
+						attemptToForgeRunningSubject.dispose();
+						synchronizeBlockchainRunningSubject.dispose();
+					});
+
+					it('"attempt to forge" task should never start when "blockchain synchronization" task is running', function (done) {
+						attemptToForgeRunningSubject
+							.filter(function (isStarting) {return isStarting;})
+							.subscribe(function () {expect(synchronizeBlockchainRunningSubject.getValue()).to.be.false;}, done, done);
+					});
+
+					it('"blockchain synchronization" task should never start when "attempt to forge" task is running', function (done) {
+						synchronizeBlockchainRunningSubject
+							.filter(function (isStarting) {return isStarting;})
+							.subscribe(function () {expect(attemptToForgeRunningSubject.getValue()).to.be.false;}, done, done);
+					});
+				});
+			});
 		});
 	});
 });
