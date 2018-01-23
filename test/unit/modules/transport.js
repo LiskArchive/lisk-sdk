df2e19aa8b880a861f7184c86cce4ac8251261ec
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2018-01-23 21:08:54 +0000

    Transport tests - receiveTransaction method, check debugger

--- test/unit/modules/transport.js
+++ test/unit/modules/transport.js
@@ -106,7 +106,9 @@ describe('transport', () => {
 			},
 		};
 
-		peerStub = {};
+		peerStub = {
+			nonce: 'sYHEDBKcScaAAAYg'
+		};
 
 		swaggerHelper.getResolvedSwaggerSpec().then(resolvedSpec => {
 			defaultScope.swagger = {
@@ -858,18 +860,24 @@ describe('transport', () => {
 				});
 			});
 
-			describe('when peer is undefined', () => {
-				it(
-					'should call library.logger.debug with "Received transaction " + transaction.id + " from public client"'
-				);
+			describe('when peer is undefined', function () {
+
+				it('should call library.logger.debug with "Received transaction " + transaction.id + " from public client"', function (done) {
+					__private.receiveTransaction(transaction, undefined, 'This is a log message', function (err) {
+						expect(library.logger.debug.calledWith('Received transaction ' + transaction.id + ' from public client')).to.be.true;
+						done();
+					});
+				});
 			});
 
-			describe('when peer is defined', () => {
-				it(
-					'should call library.logger.debug with "Received transaction " + transaction.id + " from peer"'
-				);
+			describe('when peer is defined', function () {
 
-				it('should call library.logic.peers.peersManager.getAddress');
+				it('should call library.logger.debug with "Received transaction " + transaction.id + " from peer ..."', function (done) {
+					__private.receiveTransaction(transaction, peerStub, 'This is a log message', function (err) {
+						expect(library.logger.debug.calledWith('Received transaction ' + transaction.id + ' from peer ' + peerAddressString)).to.be.true;
+						done();
+					});
+				});
 
 				it(
 					'should call library.logic.peers.peersManager.getAddress with peer.nonce'
