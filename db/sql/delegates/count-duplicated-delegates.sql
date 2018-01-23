28c93fe8260d7cb6b6db172a45bee1315e34adaa
vitaly <vitaly.tomilov@gmail.com>
2018-01-22 18:42:18 -0800

    white-space removing

--- db/sql/delegates/count-duplicated-delegates.sql
+++ db/sql/delegates/count-duplicated-delegates.sql
@@ -7,9 +7,9 @@
 WITH duplicates AS
   (
     SELECT count(1)
-     FROM ${schema~}.delegates
-     GROUP BY "transactionId"
-     HAVING count(1) > 1
+    FROM ${schema~}.delegates
+    GROUP BY "transactionId"
+    HAVING count(1) > 1
   )
 SELECT count(1)
 FROM ${schema~}.duplicates
