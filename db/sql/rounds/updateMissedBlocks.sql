6d1c39e33b1cdfb4890b650de800b5b261fae223
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2018-01-24 17:08:31 +0000

    Exporting updateMissedBlocks sql

---
+++ db/sql/rounds/updateMissedBlocks.sql
@@ -0,0 +1,11 @@
+/*
+  DESCRIPTION: ?
+
+  PARAMETERS:
+    - change - can be either '+ 1' or '- 1'
+    - outsiders - array of something?
+*/
+
+UPDATE mem_accounts
+SET missedblocks = missedblocks ${change:raw}
+WHERE address IN (${outsiders}:csv)
