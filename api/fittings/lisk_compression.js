683adf3e8e6102d7335ea32ed6c0d6036bda1995
Mona Baerenfaenger <mona@lightcurve.io>
2018-01-24 12:23:53 +0100

    :books: Standardize code documentation for lisk_compression.js

--- api/fittings/lisk_compression.js
+++ api/fittings/lisk_compression.js
@@ -17,6 +17,23 @@ var debug = require('debug')('swagger:lisk:compression');
 var _ = require('lodash');
 var compression = require('compression');
 
+/**
+ * Description of the module.
+ * @module
+ * @requires debug
+ * @requires lodash
+ * @requires compression
+ * @todo: add description of the module
+ */
+
+/**
+ * Description of the function.
+ * @func create
+ * @param {Object} fittingDef - Description of the param.
+ * @param {Object} bagpipes - Description of the param.
+ * @returns {function} lisk_compression
+ * @todo: add description of the function and its parameters
+ */
 module.exports = function create (fittingDef, bagpipes) {
 
 	debug('config: %j', fittingDef);
@@ -24,6 +41,13 @@ module.exports = function create (fittingDef, bagpipes) {
 	var validCorsOptions = ['level', 'chunkSize', 'memLevel'];
 	var middleware = compression(_.pick(fittingDef, validCorsOptions));
 
+	/**
+	 * Description of the function.
+	 * @func lisk_compression
+	 * @param {Object} context - Description of the param.
+	 * @param {function} cb - Description of the param.
+	 * @todo: add description of the function and its parameters
+	 */
 	return function lisk_compression (context, cb) {
 		debug('exec');
 		middleware(context.request, context.response, cb);
