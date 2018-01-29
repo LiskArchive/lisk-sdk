b91f4619167baebcff3532b50303960d27b50336
Nazar Hussain <nazar@lightcurve.io>
2018-01-29 12:08:26 +0100

    Resolve some merge conflicts occured during rebase

--- db/repos/accounts.js
+++ db/repos/accounts.js
@@ -299,7 +299,7 @@ class AccountsRepo {
 	 * @return {Promise}
 	 */
 	resetMemTables () {
-		return this.db.none(Queries.resetMemTables);
+		return this.db.none(migrationsSql.resetMemoryTables);
 	}
 
 	/**
@@ -454,29 +454,11 @@ class AccountsRepo {
 const Queries = {
 	countMemAccounts: 'SELECT COUNT(*)::int FROM mem_accounts WHERE "blockId" = (SELECT "id" FROM "blocks" ORDER BY "height" DESC LIMIT 1)',
 
-<<<<<<< HEAD
-/**
- * Clear data in memory tables
- * - mem_round
- * - mem_accounts2delegates
- * - mem_accounts2u_delegates
- * - mem_accounts2multisignatures
- * - mem_accounts2u_multisignatures
- *
- * @return {Promise}
- */
-AccountsRepo.prototype.resetMemTables = function () {
-	return this.db.none(migrationsSql.resetMemoryTables);
-};
-=======
 	updateMemAccounts: 'UPDATE mem_accounts SET "u_isDelegate" = "isDelegate", "u_secondSignature" = "secondSignature", "u_username" = "username", "u_balance" = "balance", "u_delegates" = "delegates", "u_multisignatures" = "multisignatures", "u_multimin" = "multimin", "u_multilifetime" = "multilifetime" WHERE "u_isDelegate" <> "isDelegate" OR "u_secondSignature" <> "secondSignature" OR "u_username" <> "username" OR "u_balance" <> "balance" OR "u_delegates" <> "delegates" OR "u_multisignatures" <> "multisignatures" OR "u_multimin" <> "multimin" OR "u_multilifetime" <> "multilifetime";',
->>>>>>> Update db/reps/accounts to use ES6 syntax
 
 	getOrphanedMemAccounts: 'SELECT a."blockId", b."id" FROM mem_accounts a LEFT OUTER JOIN blocks b ON b."id" = a."blockId" WHERE a."blockId" IS NOT NULL AND a."blockId" != \'0\' AND b."id" IS NULL',
 
 	getDelegates: 'SELECT ENCODE("publicKey", \'hex\') FROM mem_accounts WHERE "isDelegate" = 1',
-
-	resetMemTables: new QF(path.join(process.cwd(), './db/sql/init/resetMemoryTables.sql'), {minify: true, params: {schema: 'public'}}),
 
 	increment: 'UPDATE ${table:name} SET ${field:name} = ${field:name} + ${value}::bigint WHERE "address"=${address}',
 
