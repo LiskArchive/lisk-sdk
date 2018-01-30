e33486897eecdf202ceedff2379993305310b55b
tosch110 <tobias@lightcurve.io>
2018-01-30 16:21:24 +0100

    :recycle: Change accept header to only use json format

--- test/api/liskApi.js
+++ test/api/liskApi.js
@@ -34,7 +34,7 @@ describe('Lisk API module', () => {
 		version: '1.0.0',
 		minVersion: '>=0.5.0',
 		port: sslPort,
-		Accept: 'text/plain, text/html, application/json',
+		Accept: 'application/json',
 	};
 	const testnetNethash = Object.assign({}, defaultNethash, {
 		nethash: testnetHash,
@@ -203,7 +203,7 @@ describe('Lisk API module', () => {
 					version: '0.0.0a',
 					minVersion: '>=0.5.0',
 					port: sslPort,
-					Accept: 'text/plain, text/html, application/json',
+					Accept: 'application/json',
 				};
 				LSK = new LiskAPI({ nethash: ownNethash });
 				return LSK.should.have.property('nethash').be.eql(expectedDevNethash);
