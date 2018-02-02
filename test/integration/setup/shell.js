4af6b4507c2475a0217b9e1a4803b3325c19851d
Maciej Baj <macie.baj@gmail.com>
2018-02-02 15:05:46 +0100

    Include setup file while running mocha functional tests

--- test/integration/setup/shell.js
+++ test/integration/setup/shell.js
@@ -50,7 +50,13 @@ module.exports = {
 	runMochaTests: function(testsPaths, cb) {
 		var child = child_process.spawn(
 			'node_modules/.bin/_mocha',
-			['--timeout', (8 * 60 * 1000).toString(), '--exit'].concat(testsPaths),
+			[
+				'--timeout',
+				(8 * 60 * 1000).toString(),
+				'--exit',
+				'--require',
+				'./test/setup.js',
+			].concat(testsPaths),
 			{
 				cwd: `${__dirname}/../../..`,
 			}
