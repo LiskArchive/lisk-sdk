05925f1f75dba004ded65ef996c876b975e4afd7
Oliver Beddows <oliver@lightcurve.io>
2018-02-06 23:39:42 +0100

    :nail_care: Fix import/newline-after-import

--- test/unit/modules/blocks/utils.js
+++ test/unit/modules/blocks/utils.js
@@ -17,6 +17,7 @@
 
 var rewire = require('rewire');
 var modulesLoader = require('../../../common/modules_loader');
+
 var BlocksUtils = rewire('../../../../modules/blocks/utils.js');
 
 var viewRow_full_blocks_list = [
