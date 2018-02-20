56f910e650023d44f73980f51015c7a0189c47ab
Maciej Baj <macie.baj@gmail.com>
2018-02-20 16:04:45 +0100

    Align test WS client and WS server instances

--- test/common/ws/client.js
+++ test/common/ws/client.js
@@ -14,97 +14,43 @@
 
 'use strict';
 
-var scClient = require('socketcluster-client');
-var Promise = require('bluebird');
-var testConfig = require('../../data/config.json');
+const testConfig = require('../../data/config.json');
+const System = require('../../../modules/system');
+const connect = require('../../../api/ws/rpc/connect');
+const WSServerMaster = require('./server_master');
 
 /**
  * WSClient
  * Create a web socket client to be connected to server
  *
  * @param {object} headers - headers object to be passed a query param to client
- * @param {object} handlers - object containing callback handlers for native socket client
  * @constructor
  */
-function WSClient(headers, handlers) {
-	handlers = handlers || {};
-
-	this.id = null;
+function WSClient(headers = WSServerMaster.generatePeerHeaders()) {
 	this.headers = headers;
-
-	this.socketOptions = {
-		protocol: 'http',
-		hostname: '127.0.0.1',
-		port: testConfig.wsPort,
-		query: headers,
-		autoConnect: false,
-		connectTimeout: 1000,
-		ackTimeout: 1000,
-		pingTimeout: 1000,
-		connectAttempts: 1,
-		autoReconnect: false,
-	};
-	this.client = null;
-
-	this.getHandlers = function() {
-		return handlers;
-	};
 }
 
 /**
  * Start the client and register the handlers
- *
- * @return {Promise}
  */
 WSClient.prototype.start = function() {
-	var self = this;
-
-	return new Promise((resolve, reject) => {
-		self.client = scClient.connect(self.socketOptions);
-
-		self.client.on('connecting', () => {
-			console.info('Client Socket: Connecting...');
-		});
-
-		self.client.on('connect', data => {
-			self.id = data.id;
-			console.info('Client Socket: Connected... id: ', self.id);
-			resolve(data);
-		});
-
-		self.client.on('close', () => {
-			reject();
-		});
-
-		self.client.on('error', () => {
-			reject();
-		});
-
-		self.client.on('disconnect', () => {
-			self.stop();
-		});
-
-		self.client.on('connectAbort', () => {
-			self.stop();
-		});
-
-		self.client.on('fail', () => {
-			self.stop();
-		});
-
-		Object.keys(self.getHandlers()).forEach(k => {
-			self.client.on(k, self.getHandlers()[k]);
-		});
-
-		self.client.connect();
-	}).timeout(2000, "Timeout: Can't connect.");
+	System.setHeaders(this.headers);
+	this.client = connect(
+		{
+			ip: testConfig.address,
+			wsPort: testConfig.wsPort,
+		},
+		this.stop.bind(this)
+	);
+	// Emit random message to initialize connection
+	this.client.socket.emit('randomProcedure');
 };
 
 /**
  * Stop the web socket client
  */
 WSClient.prototype.stop = function() {
-	this.client.disconnect();
+	this.client.socket.disconnect();
 };
 
 module.exports = WSClient;
