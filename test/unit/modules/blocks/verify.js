3c0e969301b0f7becfe1eace986c962b08ef76f9
Lucas Isasmendi <lucas@lightcurve.io>
2018-02-26 14:32:15 +0100

    Add test for __private.checkTransaction

--- test/unit/modules/blocks/verify.js
+++ test/unit/modules/blocks/verify.js
@@ -20,13 +20,15 @@ var rewire = require('rewire');
 var BlocksVerify = rewire('../../../../modules/blocks/verify.js');
 
 describe('blocks/verify', () => {
-	var library;
-	var __private;
-	var loggerStub;
-	var dbStub;
-	var logicBlockStub;
-	var logicTransactionStub;
-	var blocksVerifyModule;
+	let library;
+	let __private;
+	let loggerStub;
+	let dbStub;
+	let logicBlockStub;
+	let logicTransactionStub;
+	let blocksVerifyModule;
+	let modulesStub;
+	let modules;
 
 	beforeEach(done => {
 		// Logic
@@ -39,7 +41,11 @@ describe('blocks/verify', () => {
 		};
 		dbStub = sinonSandbox.stub();
 		logicBlockStub = sinonSandbox.stub();
-		logicTransactionStub = sinonSandbox.stub();
+		logicTransactionStub = {
+			getId: sinonSandbox.stub(),
+			checkConfirmed: sinonSandbox.stub(),
+			verify: sinonSandbox.stub(),
+		};
 
 		blocksVerifyModule = new BlocksVerify(
 			loggerStub,
@@ -51,12 +57,30 @@ describe('blocks/verify', () => {
 		library = BlocksVerify.__get__('library');
 		__private = BlocksVerify.__get__('__private');
 
+		// Modules
+		const modulesAccountsStub = {
+			getAccount: sinonSandbox.stub(),
+		};
+		const modulesDelegatesStub = {
+			fork: sinonSandbox.stub(),
+		};
+		const modulesTransactionsStub = {
+			undoUnconfirmed: sinonSandbox.stub(),
+			removeUnconfirmedTransaction: sinonSandbox.stub(),
+		};
+		modulesStub = {
+			accounts: modulesAccountsStub,
+			delegates: modulesDelegatesStub,
+			transactions: modulesTransactionsStub,
+		};
+
+		blocksVerifyModule.onBind(modulesStub);
+		modules = BlocksVerify.__get__('modules');
 		done();
 	});
 
-	afterEach(done => {
-		sinonSandbox.restore();
-		done();
+	afterEach(() => {
+		return sinonSandbox.restore();
 	});
 
 	describe('constructor', () => {
@@ -88,6 +112,182 @@ describe('blocks/verify', () => {
 			expect(blocksVerifyModule.deleteBlockProperties).to.be.a('function');
 			expect(blocksVerifyModule.processBlock).to.be.a('function');
 			return expect(blocksVerifyModule.onBind).to.be.a('function');
+		});
+	});
+
+	describe('__private.checkTransaction', () => {
+		const dummyBlock = { id: '5', height: 5 };
+		const dummyTransaction = { id: '5', type: 0 };
+		describe('library.logic.transaction.getId', () => {
+			describe('when fails', () => {
+				beforeEach(() => {
+					return library.logic.transaction.getId.throws('getId-ERR');
+				});
+				it('should call a callback with error', done => {
+					__private.checkTransaction(dummyBlock, dummyTransaction, err => {
+						expect(err).to.equal('getId-ERR');
+						done();
+					});
+				});
+			});
+			describe('when succeeds', () => {
+				beforeEach(() => {
+					return library.logic.transaction.getId.returns('4');
+				});
+				describe('library.logic.transaction.checkConfirmed', () => {
+					describe('when fails', () => {
+						beforeEach(() => {
+							return library.logic.transaction.checkConfirmed.callsArgWith(
+								1,
+								'checkConfirmed-ERR',
+								null
+							);
+						});
+						afterEach(() => {
+							expect(modules.delegates.fork.calledOnce).to.be.true;
+							expect(modules.delegates.fork.args[0][0]).to.deep.equal(
+								dummyBlock
+							);
+							return expect(modules.delegates.fork.args[0][1]).to.equal(2);
+						});
+						describe('modules.transactions.undoUnconfirmed', () => {
+							describe('when fails', () => {
+								beforeEach(() => {
+									return modules.transactions.undoUnconfirmed.callsArgWith(
+										1,
+										'undoUnconfirmed-ERR',
+										null
+									);
+								});
+								afterEach(() => {
+									expect(
+										modules.transactions.removeUnconfirmedTransaction.calledOnce
+									).to.be.true;
+									return expect(
+										modules.transactions.removeUnconfirmedTransaction.args[0][0]
+									).to.equal('4');
+								});
+								it('should call a callback with error', done => {
+									__private.checkTransaction(
+										dummyBlock,
+										dummyTransaction,
+										err => {
+											expect(err).to.equal('undoUnconfirmed-ERR');
+											done();
+										}
+									);
+								});
+							});
+							describe('when succeeds', () => {
+								beforeEach(() => {
+									return modules.transactions.undoUnconfirmed.callsArgWith(
+										1,
+										null,
+										true
+									);
+								});
+								afterEach(() => {
+									expect(
+										modules.transactions.removeUnconfirmedTransaction.calledOnce
+									).to.be.true;
+									return expect(
+										modules.transactions.removeUnconfirmedTransaction.args[0][0]
+									).to.equal('4');
+								});
+								it('should call a callback with error', done => {
+									__private.checkTransaction(
+										dummyBlock,
+										dummyTransaction,
+										err => {
+											expect(err).to.equal('checkConfirmed-ERR');
+											done();
+										}
+									);
+								});
+							});
+						});
+					});
+					describe('when succeeds', () => {
+						beforeEach(() => {
+							return library.logic.transaction.checkConfirmed.callsArgWith(
+								1,
+								null,
+								true
+							);
+						});
+						describe('modules.accounts.getAccount', () => {
+							describe('when fails', () => {
+								beforeEach(() => {
+									return modules.accounts.getAccount.callsArgWith(
+										1,
+										'getAccount-ERR',
+										null
+									);
+								});
+								it('should call a callback with error', done => {
+									__private.checkTransaction(
+										dummyBlock,
+										dummyTransaction,
+										err => {
+											expect(err).to.equal('getAccount-ERR');
+											done();
+										}
+									);
+								});
+							});
+							describe('when succeeds', () => {
+								beforeEach(() => {
+									return modules.accounts.getAccount.callsArgWith(
+										1,
+										null,
+										true
+									);
+								});
+								describe('library.logic.transaction.verify', () => {
+									describe('when fails', () => {
+										beforeEach(() => {
+											return library.logic.transaction.verify.callsArgWith(
+												2,
+												'verify-ERR',
+												null
+											);
+										});
+										it('should call a callback with error', done => {
+											__private.checkTransaction(
+												dummyBlock,
+												dummyTransaction,
+												err => {
+													expect(err).to.equal('verify-ERR');
+													done();
+												}
+											);
+										});
+									});
+									describe('when succeeds', () => {
+										beforeEach(() => {
+											return library.logic.transaction.verify.callsArgWith(
+												2,
+												null,
+												true
+											);
+										});
+										it('should call a callback with no error', done => {
+											__private.checkTransaction(
+												dummyBlock,
+												dummyTransaction,
+												err => {
+													expect(err).to.be.null;
+													done();
+												}
+											);
+										});
+									});
+								});
+							});
+						});
+					});
+				});
+			});
 		});
 	});
 });
