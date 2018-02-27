4adf2a0d91560eaec013bc60f517d736cd5d6e6f
Maciej Baj <macie.baj@gmail.com>
2018-02-27 11:22:57 +0100

    Merge ws_rpc conficts

--- api/ws/rpc/ws_rpc.js
+++ api/ws/rpc/ws_rpc.js
@@ -14,15 +14,7 @@
 
 'use strict';
 
-var _ = require('lodash');
-var scClient = require('socketcluster-client');
-var WAMPClient = require('wamp-socket-cluster/WAMPClient');
-var PromiseDefer = require('../../../helpers/promise_defer');
-var System = require('../../../modules/system');
-var failureCodes = require('./failure_codes');
-var PeerUpdateError = require('./failure_codes').PeerUpdateError;
-
-var wsServer = null;
+let wsServer = null;
 
 /**
  * Description of the module.
@@ -33,29 +25,19 @@ var wsServer = null;
  * @requires socketcluster-client
  * @requires wamp-socket-cluster/WAMPClient
  * @requires wamp-socket-cluster/MasterWAMPServe
- * @requires api/ws/rpc/failure_codes
- * @requires {@link helpers.PromiseDefer}
+ * @requires api/ws/rpc/failureCodes
+ * @requires helpers/promiseDefer
  * @requires modules/system
  * @todo Add description for the module
  */
 
 /**
- * Description of the namespace.
- *
- * @namespace wsRPC
- * @memberof module:api/ws/rpc/ws_rpc
- * @see Parent: {@link module:api/ws/rpc/ws_rpc}
- * @todo Add description for the namespace
+ * @alias module:api/ws/rpc/wsRPC
  */
-var wsRPC = {
-	clientsConnectionsMap: {},
-	scClient,
-	wampClient: new WAMPClient(),
-
+const wsRPC = {
 	/**
 	 * Description of the function.
 	 *
-	 * @memberof module:api/ws/rpc/ws_rpc.wsRPC
 	 * @param {Object} __wsServer
 	 * @todo Add description for the function and the params
 	 */
@@ -66,7 +48,6 @@ var wsRPC = {
 	/**
 	 * Description of the function.
 	 *
-	 * @memberof module:api/ws/rpc/ws_rpc.wsRPC
 	 * @throws {Error} If WS server has not been initialized yet
 	 * @returns {MasterWAMPServer} wsServer
 	 * @todo Add description for the function
@@ -77,40 +58,10 @@ var wsRPC = {
 		}
 		return wsServer;
 	},
-	/**
-	 * Description of the function.
-	 *
-	 * @memberof module:api/ws/rpc/ws_rpc.wsRPC
-	 * @param {string} ip
-	 * @param {number} port
-	 * @returns {Object} Map where keys are all procedures registered
-	 * @todo Add description for the function and the params
-	 */
-	getClientRPCStub(ip, port) {
-		if (!ip || !port) {
-			throw new Error(
-				`RPC client needs ip and port to establish WS connection with: ${ip}:${port}`
-			);
-		}
-
-		var address = `${ip}:${port}`;
-		var connectionState = this.clientsConnectionsMap[address];
-
-		// first time init || previously rejected
-		if (
-			!connectionState ||
-			connectionState.status === ConnectionState.STATUS.DISCONNECTED
-		) {
-			connectionState = new ConnectionState(ip, port);
-			this.clientsConnectionsMap[address] = connectionState;
-		}
-		return connectionState.stub;
-	},
 
 	/**
 	 * Description of the function.
 	 *
-	 * @memberof module:api/ws/rpc/ws_rpc.wsRPC
 	 * @throws {Error} If WS server has not been initialized yet
 	 * @returns {Object} wsServer
 	 * @todo Add description for the function
@@ -123,205 +74,15 @@ var wsRPC = {
 	},
 };
 
-ConnectionState.STATUS = {
-	NEW: 1,
-	PENDING: 2,
-	ESTABLISHED: 3,
-	DISCONNECTED: 4,
-};
-
-/**
- * Description of the function.
- *
- * @class
- * @memberof module:api/ws/rpc/ws_rpc
- * @see Parent: {@link api/ws/rpc/ws_rpc}
- * @param {string} ip
- * @param {number} port
- * @todo Add description for the function and the params
- */
-function ConnectionState(ip, port) {
-	this.ip = ip;
-	this.port = +port;
-	this.status = ConnectionState.STATUS.NEW;
-	this.socketDefer = PromiseDefer();
-	this.stub = new ClientRPCStub(this);
-}
-
-/**
- * Description of the function.
- *
- * @memberof module:api/ws/rpc/ws_rpc.ConnectionState
- * @param {string} ip
- * @param {number} port
- * @todo Add description for the function and the params
- */
-ConnectionState.prototype.reconnect = function() {
-	this.status = ConnectionState.STATUS.PENDING;
-	this.socketDefer = PromiseDefer();
-};
-
-/**
- * Description of the function.
- *
- * @memberof module:api/ws/rpc/ws_rpc.ConnectionState
- * @param {string} ip
- * @param {number} port
- * @todo Add description for the function and the params
- */
-ConnectionState.prototype.reject = function(reason) {
-	this.status = ConnectionState.STATUS.DISCONNECTED;
-	this.socketDefer.reject(reason);
-};
-
-/**
- * Description of the function.
- *
- * @memberof module:api/ws/rpc/ws_rpc.ConnectionState
- * @param {string} ip
- * @param {number} port
- * @todo Add description for the function and the params
- */
-ConnectionState.prototype.resolve = function(socket) {
-	this.status = ConnectionState.STATUS.ESTABLISHED;
-	this.socketDefer.resolve(socket);
-};
-
-/**
- * The stub of all RPC methods registered on WS server.
- *
- * @class ClientRPCStub
- * @memberof module:api/ws/rpc/ws_rpc
- * @see Parent: {@link api/ws/rpc/ws_rpc}
- * @param {Object} connectionState
- * @example
- * // methodA registered on WS server can be called by a client by simply:
- * sampleClientStub.methodA(exampleArg, cb);
- * @returns {Object} clientStub
- * @todo Add description for the params
- */
-var ClientRPCStub = function(connectionState) {
-	var wsServer;
-	try {
-		wsServer = wsRPC.getServer();
-	} catch (wsServerNotInitializedException) {
-		return {};
-	}
-
-	return _.reduce(
-		Object.assign({}, wsServer.endpoints.rpc, wsServer.endpoints.event),
-		(availableCalls, procedureHandler, procedureName) => {
-			availableCalls[procedureName] = this.sendAfterSocketReadyCb(
-				connectionState
-			)(procedureName);
-			return availableCalls;
-		},
-		{}
-	);
-};
-
-/**
- * Description of the function.
- *
- * @memberof module:api/ws/rpc/ws_rpc.ClientRPCStub
- * @param {Object} connectionState
- * @todo Add description for the function and the params
- * @todo Document this as an instance method (not static)
- */
-ClientRPCStub.prototype.initializeNewConnection = function(connectionState) {
-	var options = {
-		hostname: connectionState.ip,
-		port: connectionState.port,
-		protocol: 'http',
-		autoReconnect: true,
-		query: System.getHeaders(),
-	};
-
-	var clientSocket = wsRPC.scClient.connect(options);
-	wsRPC.wampClient.upgradeToWAMP(clientSocket);
-
-	clientSocket.on('error', () => {
-		clientSocket.disconnect();
-	});
-
-	clientSocket.on('connectAbort', () => {
-		connectionState.reject(
-			new PeerUpdateError(
-				failureCodes.HANDSHAKE_ERROR,
-				failureCodes.errorMessages[failureCodes.HANDSHAKE_ERROR]
-			)
-		);
-	});
-
-	clientSocket.on('disconnect', (code, description) => {
-		connectionState.reject(
-			new PeerUpdateError(code, failureCodes.errorMessages[code], description)
-		);
-	});
-
-	clientSocket.on('connect', () => {
-		connectionState.resolve(clientSocket);
-	});
-};
-
-/**
- * Description of the function.
- *
- * @memberof module:api/ws/rpc/ws_rpc.ClientRPCStub
- * @param {Object} connectionState
- * @returns {function} To be called with procedure, to be then called with optional argument and/or callback
- * @todo Add description for the function and the params
- * @todo Document this as an instance method (not static)
- */
-ClientRPCStub.prototype.sendAfterSocketReadyCb = function(connectionState) {
-	return function(procedureName) {
-		/**
-		 * @param {Object} data [data={}] Argument passed to procedure
-		 */
-		return function(data, cb) {
-			cb = _.isFunction(cb) ? cb : _.isFunction(data) ? data : function() {};
-			data = data && !_.isFunction(data) ? data : {};
-
-			if (
-				connectionState.status === ConnectionState.STATUS.NEW ||
-				connectionState.status === ConnectionState.STATUS.DISCONNECTED
-			) {
-				connectionState.reconnect();
-				ClientRPCStub.prototype.initializeNewConnection(connectionState);
-			}
-
-			connectionState.socketDefer.promise
-				.timeout(1000)
-				.then(socket =>
-					socket
-						.call(procedureName, data)
-						.then(res => setImmediate(cb, null, res))
-						.catch(err => setImmediate(cb, err))
-				)
-				.catch(err => {
-					if (err && err.name === 'TimeoutError') {
-						err = new PeerUpdateError(
-							failureCodes.CONNECTION_TIMEOUT,
-							failureCodes.errorMessages[failureCodes.CONNECTION_TIMEOUT]
-						);
-					}
-					return setImmediate(cb, err);
-				});
-		};
-	};
-};
-
-var remoteAction = function() {
+const remoteAction = function() {
 	throw new Error('Function invoked on master instead of slave process');
 };
 
-var slaveRPCStub = {
+const slaveRPCStub = {
 	updateMyself: remoteAction,
 };
 
 module.exports = {
 	wsRPC,
-	ConnectionState,
-	ClientRPCStub,
 	slaveRPCStub,
 };
