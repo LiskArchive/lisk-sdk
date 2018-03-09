8e8fae0784f06cf74a391c76353fc3f4818e8628
Shusetsu Toda <shusetsu@lightcurve.io>
2018-03-09 15:51:11 +0100

    :nail_care: Fix lint

--- src/api/lisk_api.js
+++ src/api/lisk_api.js
@@ -13,7 +13,6 @@
  *
  */
 import { TESTNET_NETHASH, MAINNET_NETHASH } from 'lisk-constants';
-import { LIVE_PORT, SSL_PORT, TEST_PORT } from './constants';
 import config from '../../config.json';
 import {
 	AccountsResource,
@@ -27,6 +26,7 @@ import {
 	VotersResource,
 	VotesResource,
 } from './resources';
+import { LIVE_PORT, SSL_PORT, TEST_PORT } from './constants';
 
 const commonHeaders = {
 	'Content-Type': 'application/json',
