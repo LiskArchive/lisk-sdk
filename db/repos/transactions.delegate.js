82c0d69d901f8c4144447c59df883d45b65d50cf
vitaly <vitaly.tomilov@gmail.com>
2018-03-10 12:40:48 -0800

    refactoring DelegateTransactionsRepo

--- db/repos/transactions.delegate.js
+++ db/repos/transactions.delegate.js
@@ -41,12 +41,8 @@ function DelegateTransactionsRepo(db, pgp) {
 
 	if (!columnSet) {
 		columnSet = {};
-		var table = new pgp.helpers.TableName({
-			table: this.dbTable,
-			schema: 'public',
-		});
 		columnSet.insert = new pgp.helpers.ColumnSet(this.dbFields, {
-			table,
+			table: this.dbTable,
 		});
 	}
 
@@ -70,7 +66,9 @@ DelegateTransactionsRepo.prototype.save = function(transactions) {
 		username: transaction.asset.delegate.username,
 	}));
 
-	return this.db.none(this.pgp.helpers.insert(transactions, this.cs.insert));
+	const query = () => this.pgp.helpers.insert(transactions, this.cs.insert);
+
+	return this.db.none(query);
 };
 
 module.exports = DelegateTransactionsRepo;
