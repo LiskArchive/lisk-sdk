8a3603747898a73fab52c416838769682c1c62e6
Manu Nelamane Siddalingegowda <manu@lightcurve.io>
2018-03-22 11:24:30 +0100

    Update constants documentation

--- helpers/constants.js
+++ helpers/constants.js
@@ -20,10 +20,10 @@
  * @namespace constants
  * @memberof helpers
  * @see Parent: {@link helpers}
- * @property {number} activeDelegates - The default number of delegates allowed to forge a block
- * @property {number} maxVotesPerTransaction - The maximum number of votes allowed in transaction type(3) votes
- * @property {number} blockSlotWindow - The default number of previous blocks to keep in memory
- * @property {number} blockReceiptTimeOut - Seconds to check if the block is fresh or not
+ * @property {number} activeDelegates - The default number of delegates allowed to forge a block.
+ * @property {number} maxVotesPerTransaction - The maximum number of votes allowed in transaction type(3) votes.
+ * @property {number} blockSlotWindow - The default number of previous blocks to keep in memory.
+ * @property {number} blockReceiptTimeOut - Seconds to check if the block is fresh or not.
  * @property {Date} epochTime	- Timestamp indicating the start of lisk core.
  * @property {Object} fees - Object representing amount of fees for different types of transactions.
  * @property {number} fees.send	- Fee for sending a transaction.
@@ -32,20 +32,23 @@
  * @property {number} fees.delegate - Fee for registering as a delegate.
  * @property {number} fees.multisignature - Fee for multisignature transaction.
  * @property {number} fees.dapp	- Fee for registering as a dapp.
- * @property {number} maxAmount - @// TODO: DK
- * @property {number} maxPayloadLength - @// TODO: DK
- * @property {number} maxPeers - @// TODO: DK
- * @property {number} maxSharedTxs - @// TODO: DK
- * @property {number} maxTxsPerBlock -	Maximum Number of transactions allowed per block
- * @property {number} minBroadhashConsensus - @// TODO: DK
- * @property {string[]} nethashes - For mainnet and testnet
+ * @property {number} maxAmount - Maximum amount of LSK that can be transfered in a transaction.
+ // TODO: Needs additional check to revise max payload length
+ // for each transaction type for consistency
+ // FYI: https://lisk.io/documentation/the-lisk-protocol/blocks {Block Payload Section}
+ * @property {number} maxPayloadLength - Maximum transaction bytes length for 25 transactions in a single block.
+ * @property {number} maxPeers - Maximum number of peers allowed to connect while broadcasting a block.
+ * @property {number} maxSharedTxs - Maximum number of in-memory transactions/signatures shared accros peers.
+ * @property {number} maxTxsPerBlock -	Maximum Number of transactions allowed per block.
+ * @property {number} minBroadhashConsensus - Minimum broadhash consensus(%) among connected {maxPeers} peers.
+ * @property {string[]} nethashes - For mainnet and testnet.
  * @property {Object} rewards - Object representing LSK rewards milestone.
- * @property {number[]} rewards.milestones - Initial 5, and decreasing until 1
- * @property {number} rewards.offset - Start rewards at block (n)
- * @property {number} rewards.distance - Distance between each milestone
- * @property {number} totalAmount - @// TODO: DK
- * @property {number} unconfirmedTransactionTimeOut - 1080 blocks
- * @todo Add description for the namespace and the properties
+ * @property {number[]} rewards.milestones - Initial 5, and decreasing until 1.
+ * @property {number} rewards.offset - Start rewards at block (n).
+ * @property {number} rewards.distance - Distance between each milestone.
+ * @property {number} totalAmount - Total amount of LSK available in network before rewards milestone started.
+ * @property {number} unconfirmedTransactionTimeOut - Expiration time for unconfirmed transaction/signatures in transaction pool.
+ * @todo Add description for the namespace and the properties.
  */
 var constants = {
 	activeDelegates: 101,
