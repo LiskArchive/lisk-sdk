b1f95a0582d278090d532f3368bfcf4f9a8ccae1
willclarktech <willclarktech@users.noreply.github.com>
2018-04-18 15:24:35 +0200

    :white_check_mark: Update passphrase encryption tests for new errors

--- test/unit/modules/delegates.js
+++ test/unit/modules/delegates.js
@@ -126,7 +126,9 @@ describe('delegates', () => {
 
 				loadDelegates(err => {
 					expect(err).to.equal(
-						`Invalid encryptedSecret for publicKey: ${accountDetails.publicKey}`
+						`Invalid encryptedSecret for publicKey: ${
+							accountDetails.publicKey
+						}. Invalid encryption method 1: currently only method 5 (SHA-256) is supported.`
 					);
 					expect(Object.keys(__private.keypairs).length).to.equal(0);
 					done();
@@ -146,7 +148,9 @@ describe('delegates', () => {
 
 				loadDelegates(err => {
 					expect(err).to.equal(
-						`Invalid encryptedSecret for publicKey: ${accountDetails.publicKey}`
+						`Invalid encryptedSecret for publicKey: ${
+							accountDetails.publicKey
+						}. Unsupported state or unable to authenticate data.`
 					);
 					expect(Object.keys(__private.keypairs).length).to.equal(0);
 					done();
@@ -166,34 +170,38 @@ describe('delegates', () => {
 
 				loadDelegates(err => {
 					expect(err).to.equal(
-						`Invalid encryptedSecret for publicKey: ${accountDetails.publicKey}`
+						`Invalid encryptedSecret for publicKey: ${
+							accountDetails.publicKey
+						}. Unsupported state or unable to authenticate data.`
 					);
 					expect(Object.keys(__private.keypairs).length).to.equal(0);
 					done();
 				});
 			});
 
-			it('should return error if encrypted secret does not decrypt with default secret', done => {
+			it('should return error if encrypted secret has a modified ciphertext', done => {
 				var accountDetails = {
 					publicKey:
 						'9d3058175acab969f41ad9b86f7a2926c74258670fe56b37c429c01fca9f2f0f',
-					// cipher text is one character short
+					// cipher text is 1 character different
 					encryptedSecret:
-						'$5$rounds=1$5d502e8156eeeaf2acf23d801351dda7$f856ab7731c046da5bf7b9f11fdf20fbc1870845633627b82f84151e432d4e4ca5f69a9c40f15bc2a749e28a66c2e11bbdc45049e0a1f53087fbdd542536c19059e294d231f7aaf30a0600a79ef7851$iv=4a500624cba9c3cb3b6e380556e197d6$tag=24a118a1abab32d256a5de013c20539e$version=1.0.0',
+						'$5$rounds=1$5d502e8156eeeaf2acf23d801351dda7$f856ab7731c046da5bf7b9f11fdf20fbc1870845633627b82f84151e432d4e4ca5f69a9c40f15bc2a749e28a66c2e11bbdc45049e0a1f53087fbdd542536c19059e294d231f7aaf30a0600a79ef78510$iv=4a500624cba9c3cb3b6e380556e197d6$tag=24a118a1abab32d256a5de013c20539e$version=1.0.0',
 				};
 
 				config.forging.secret = [accountDetails];
 
 				loadDelegates(err => {
 					expect(err).to.equal(
-						`Invalid encryptedSecret for publicKey: ${accountDetails.publicKey}`
+						`Invalid encryptedSecret for publicKey: ${
+							accountDetails.publicKey
+						}. Unsupported state or unable to authenticate data.`
 					);
 					expect(Object.keys(__private.keypairs).length).to.equal(0);
 					done();
 				});
 			});
 
-			it('should return error if encrypted secret has invalid iv', done => {
+			it('should return error if encrypted secret has invalid iv hex string', done => {
 				var accountDetails = {
 					publicKey:
 						'9d3058175acab969f41ad9b86f7a2926c74258670fe56b37c429c01fca9f2f0f',
@@ -206,7 +214,9 @@ describe('delegates', () => {
 
 				loadDelegates(err => {
 					expect(err).to.equal(
-						`Invalid encryptedSecret for publicKey: ${accountDetails.publicKey}`
+						`Invalid encryptedSecret for publicKey: ${
+							accountDetails.publicKey
+						}. Unsupported state or unable to authenticate data.`
 					);
 					expect(Object.keys(__private.keypairs).length).to.equal(0);
 					done();
@@ -226,7 +236,9 @@ describe('delegates', () => {
 
 				loadDelegates(err => {
 					expect(err).to.equal(
-						`Invalid encryptedSecret for publicKey: ${accountDetails.publicKey}`
+						`Invalid encryptedSecret for publicKey: ${
+							accountDetails.publicKey
+						}. Unsupported state or unable to authenticate data.`
 					);
 					expect(Object.keys(__private.keypairs).length).to.equal(0);
 					done();
@@ -246,7 +258,9 @@ describe('delegates', () => {
 
 				loadDelegates(err => {
 					expect(err).to.equal(
-						`Invalid encryptedSecret for publicKey: ${accountDetails.publicKey}`
+						`Invalid encryptedSecret for publicKey: ${
+							accountDetails.publicKey
+						}. Tag must be 16 bytes.`
 					);
 					expect(Object.keys(__private.keypairs).length).to.equal(0);
 					done();
@@ -264,7 +278,11 @@ describe('delegates', () => {
 				config.forging.secret = [accountDetails];
 
 				loadDelegates(err => {
-					expect(err).to.equal('Public keys do not match');
+					expect(err).to.equal(
+						`Invalid encryptedSecret for publicKey: ${
+							accountDetails.publicKey
+						}. Public keys do not match.`
+					);
 					expect(Object.keys(__private.keypairs).length).to.equal(0);
 					done();
 				});
@@ -316,6 +334,37 @@ describe('delegates', () => {
 
 			it('should load secrets in encrypted format with the key', done => {
 				config.forging.secret = encryptedSecret;
+
+				loadDelegates(err => {
+					expect(err).to.not.exist;
+					expect(Object.keys(__private.keypairs).length).to.equal(
+						encryptedSecret.length
+					);
+					done();
+				});
+			});
+
+			it('should load secrets in encrypted format with the key with default 1e6 rounds if not set', done => {
+				config.forging.secret = [
+					{
+						publicKey:
+							'9d3058175acab969f41ad9b86f7a2926c74258670fe56b37c429c01fca9f2f0f',
+						encryptedSecret:
+							'$5$f26a3c93e355ed1586f03e8525cdb8d2$3469ac7aa4c550b4d762db025950d7e3d7c2f14db7a07c053a2b02fa10029d909749ba0915f2b1556b1032c8c1d0fe1475b08d986bcf86950cffec444f6ca0ca38c5f2c76fa7bb4e8e38e9610fa251d8$iv=b75ad88c97c8bb9ebe9a9ebdc43bf256$tag=a53e556141adbc8769692e9e88d1e00b$version=1.0.0',
+					},
+					{
+						publicKey:
+							'141b16ac8d5bd150f16b1caa08f689057ca4c4434445e56661831f4e671b7c0a',
+						encryptedSecret:
+							'$5$1d450ddb2fb22b0a1f92532856436f07$ef32a34327d18e1e475a343aded72ce745b0cf55fed8f16d67f7fd314d2eeb1810322fd6c2ec746d833f77bf0814a4f3ecfcdc5ffb5bffc1c2e028bb5ef2f8fbf8fefe816cdfb5e323ada8f3$iv=dc58976175a594ed7bb80ffc23417b58$tag=ab3da063670a7a6c364e401cb78865ad$version=1.0.0',
+					},
+					{
+						publicKey:
+							'3ff32442bb6da7d60c1b7752b24e6467813c9b698e0f278d48c43580da972135',
+						encryptedSecret:
+							'$5$cc745aafe1a7ece0048244588860018d$a36b96a34c93c8bcfb3a0d32f0d1590f1327064c2d487d3c7f6a78b504fb2811e3b3366767b7ef09d3ee8450b194bf2629ab8e0d82d3ec2d9b7856084a56ae2c1f62f997e3eef95e9b01f029d823$iv=d8a8c2cfc08ae42b6b9621a6a0cf7d97$tag=fda19cef0e31d3b0be1f371476b9fba7$version=1.0.0',
+					},
+				];
 
 				loadDelegates(err => {
 					expect(err).to.not.exist;
