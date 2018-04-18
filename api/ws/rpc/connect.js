b80d36dec80fe49d032009433fe519d156a93cb4
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2018-04-18 20:24:05 +0200

    Use better timeout values

--- api/ws/rpc/connect.js
+++ api/ws/rpc/connect.js
@@ -23,6 +23,8 @@ const System = require('../../../modules/system');
 const wsRPC = require('../../../api/ws/rpc/ws_rpc').wsRPC;
 
 const TIMEOUT = 2000;
+const INITIAL_PING_TIMEOUT = 8000;
+
 const wampClient = new WAMPClient(TIMEOUT); // Timeout failed requests after 1 second
 const socketConnections = {};
 
@@ -47,7 +49,7 @@ const connectSteps = {
 			autoReconnect: false,
 			connectTimeout: TIMEOUT,
 			ackTimeout: TIMEOUT,
-			pingTimeout: TIMEOUT,
+			pingTimeout: INITIAL_PING_TIMEOUT,
 			connectAttempts: 1,
 			port: peer.wsPort,
 			hostname: peer.ip,
