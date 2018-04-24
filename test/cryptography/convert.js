adb294e5750625e878eef028ac02dee9e51d479d
willclarktech <willclarktech@users.noreply.github.com>
2018-04-24 14:57:46 +0200

    :white_check_mark: Add tests for parsing/formatting encrypted secrets

--- test/cryptography/convert.js
+++ test/cryptography/convert.js
@@ -22,6 +22,8 @@ import {
 	convertPrivateKeyEd2Curve,
 	bigNumberToBuffer,
 	bufferToBigNumberString,
+	stringifyEncryptedPassphrase,
+	parseEncryptedPassphrase,
 } from 'cryptography/convert';
 // Require is used for stubbing
 const hash = require('cryptography/hash');
@@ -200,6 +202,77 @@ describe('convert', () => {
 			const bigNumber = '58191285901858109';
 			const buffer = Buffer.from('00cebcaa8d34153d', 'hex');
 			return expect(bufferToBigNumberString(buffer)).to.be.equal(bigNumber);
+		});
+	});
+
+	describe('#stringifyEncryptedPassphrase', () => {
+		it('should format an encrypted passphrase as a string', () => {
+			const encryptedPassphrase = {
+				salt: 'e8c7dae4c893e458e0ebb8bff9a36d84',
+				cipherText:
+					'c0fab123d83c386ffacef9a171b6e0e0e9d913e58b7972df8e5ef358afbc65f99c9a2b6fe7716f708166ed72f59f007d2f96a91f48f0428dd51d7c9962e0c6a5fc27ca0722038f1f2cf16333',
+				iv: '1a2206e426c714091b7e48f6',
+				tag: '3a9d9f9f9a92c9a58296b8df64820c15',
+				version: '1',
+			};
+			const stringifiedEncryptedPassphrase =
+				'salt=e8c7dae4c893e458e0ebb8bff9a36d84&cipherText=c0fab123d83c386ffacef9a171b6e0e0e9d913e58b7972df8e5ef358afbc65f99c9a2b6fe7716f708166ed72f59f007d2f96a91f48f0428dd51d7c9962e0c6a5fc27ca0722038f1f2cf16333&iv=1a2206e426c714091b7e48f6&tag=3a9d9f9f9a92c9a58296b8df64820c15&version=1';
+			return expect(stringifyEncryptedPassphrase(encryptedPassphrase)).to.equal(
+				stringifiedEncryptedPassphrase,
+			);
+		});
+
+		it('should format an encrypted passphrase with custom iterations as a string', () => {
+			const encryptedPassphrase = {
+				iterations: 1,
+				salt: 'e8c7dae4c893e458e0ebb8bff9a36d84',
+				cipherText:
+					'c0fab123d83c386ffacef9a171b6e0e0e9d913e58b7972df8e5ef358afbc65f99c9a2b6fe7716f708166ed72f59f007d2f96a91f48f0428dd51d7c9962e0c6a5fc27ca0722038f1f2cf16333',
+				iv: '1a2206e426c714091b7e48f6',
+				tag: '3a9d9f9f9a92c9a58296b8df64820c15',
+				version: '1',
+			};
+			const stringifiedEncryptedPassphrase =
+				'iterations=1&salt=e8c7dae4c893e458e0ebb8bff9a36d84&cipherText=c0fab123d83c386ffacef9a171b6e0e0e9d913e58b7972df8e5ef358afbc65f99c9a2b6fe7716f708166ed72f59f007d2f96a91f48f0428dd51d7c9962e0c6a5fc27ca0722038f1f2cf16333&iv=1a2206e426c714091b7e48f6&tag=3a9d9f9f9a92c9a58296b8df64820c15&version=1';
+			return expect(stringifyEncryptedPassphrase(encryptedPassphrase)).to.equal(
+				stringifiedEncryptedPassphrase,
+			);
+		});
+	});
+
+	describe('#parseEncryptedPassphrase', () => {
+		it('should parse an encrypted passphrase string', () => {
+			const stringifiedEncryptedPassphrase =
+				'salt=e8c7dae4c893e458e0ebb8bff9a36d84&cipherText=c0fab123d83c386ffacef9a171b6e0e0e9d913e58b7972df8e5ef358afbc65f99c9a2b6fe7716f708166ed72f59f007d2f96a91f48f0428dd51d7c9962e0c6a5fc27ca0722038f1f2cf16333&iv=1a2206e426c714091b7e48f6&tag=3a9d9f9f9a92c9a58296b8df64820c15&version=1';
+			const encryptedPassphrase = {
+				iterations: null,
+				salt: 'e8c7dae4c893e458e0ebb8bff9a36d84',
+				cipherText:
+					'c0fab123d83c386ffacef9a171b6e0e0e9d913e58b7972df8e5ef358afbc65f99c9a2b6fe7716f708166ed72f59f007d2f96a91f48f0428dd51d7c9962e0c6a5fc27ca0722038f1f2cf16333',
+				iv: '1a2206e426c714091b7e48f6',
+				tag: '3a9d9f9f9a92c9a58296b8df64820c15',
+				version: '1',
+			};
+			return expect(
+				parseEncryptedPassphrase(stringifiedEncryptedPassphrase),
+			).to.eql(encryptedPassphrase);
+		});
+
+		it('should parse an encrypted passphrase string with custom iterations', () => {
+			const stringifiedEncryptedPassphrase =
+				'iterations=1&salt=e8c7dae4c893e458e0ebb8bff9a36d84&cipherText=c0fab123d83c386ffacef9a171b6e0e0e9d913e58b7972df8e5ef358afbc65f99c9a2b6fe7716f708166ed72f59f007d2f96a91f48f0428dd51d7c9962e0c6a5fc27ca0722038f1f2cf16333&iv=1a2206e426c714091b7e48f6&tag=3a9d9f9f9a92c9a58296b8df64820c15&version=1';
+			const encryptedPassphrase = {
+				iterations: 1,
+				salt: 'e8c7dae4c893e458e0ebb8bff9a36d84',
+				cipherText:
+					'c0fab123d83c386ffacef9a171b6e0e0e9d913e58b7972df8e5ef358afbc65f99c9a2b6fe7716f708166ed72f59f007d2f96a91f48f0428dd51d7c9962e0c6a5fc27ca0722038f1f2cf16333',
+				iv: '1a2206e426c714091b7e48f6',
+				tag: '3a9d9f9f9a92c9a58296b8df64820c15',
+				version: '1',
+			};
+			return expect(
+				parseEncryptedPassphrase(stringifiedEncryptedPassphrase),
+			).to.eql(encryptedPassphrase);
 		});
 	});
 });
