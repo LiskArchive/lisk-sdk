bf95a01292f1a13004ac2f7df16a84ea7fd29629
Mariusz Serek <mariusz@serek.net>
2018-04-25 12:24:54 +0200

    Rename password to secret, key to password for accountFixtures

--- test/fixtures/accounts.js
+++ test/fixtures/accounts.js
@@ -23,7 +23,7 @@ const accounts = {};
 accounts.existingDelegate = {
 	address: '10881167371402274308L',
 	publicKey: 'addb0e15a44b0fdc6ff291be28d8c98f5551d0cd9218d749e30ddb87c6e31ca9',
-	password:
+	secret:
 		'actress route auction pudding shiver crater forum liquid blouse imitate seven front',
 	balance: '0',
 	delegateName: 'genesis_100',
@@ -33,12 +33,12 @@ accounts.existingDelegate = {
 accounts.genesis = {
 	address: '16313739661670634666L',
 	publicKey: 'c094ebee7ec0c50ebee32918655e089f6e1a604b83bcaa760293c61e0f18ab6f',
-	password:
+	secret:
 		'wagon stock borrow episode laundry kitten salute link globe zero feed marble',
 	balance: '10000000000000000',
 	encryptedSecret:
 		'iterations=1&salt=e8c7dae4c893e458e0ebb8bff9a36d84&cipherText=c0fab123d83c386ffacef9a171b6e0e0e9d913e58b7972df8e5ef358afbc65f99c9a2b6fe7716f708166ed72f59f007d2f96a91f48f0428dd51d7c9962e0c6a5fc27ca0722038f1f2cf16333&iv=1a2206e426c714091b7e48f6&tag=3a9d9f9f9a92c9a58296b8df64820c15&version=1',
-	key: 'elephant tree paris dragon chair galaxy',
+	password: 'elephant tree paris dragon chair galaxy',
 };
 
 accounts.mem_accountsFields = [
