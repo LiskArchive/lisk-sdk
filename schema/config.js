8b43af85bf53467aed6c125b759044f910b22d95
Mariusz Serek <mariusz@serek.net>
2018-05-03 11:46:01 +0200

    Rename encryptedSecret to encryptedPassphrase for config.json schema

--- schema/config.js
+++ schema/config.js
@@ -269,9 +269,9 @@ module.exports = {
 						type: 'array',
 						items: {
 							properties: {
-								encryptedSecret: {
+								encryptedPassphrase: {
 									type: 'string',
-									format: 'encryptedSecret',
+									format: 'encryptedPassphrase',
 								},
 								publicKey: {
 									type: 'string',
