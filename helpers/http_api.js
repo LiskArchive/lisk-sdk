655cf2f68fc2c921507cbf435681cbec547724e9
Nazar Hussain <nazar@lightcurve.io>
2018-05-07 18:09:47 +0200

    Remove unused middlewares from http layer

--- helpers/http_api.js
+++ helpers/http_api.js
@@ -14,9 +14,7 @@
 
 'use strict';
 
-var _ = require('lodash');
-var extend = require('extend');
-var apiCodes = require('./api_codes');
+var queryParser = require('express-query-int');
 var checkIpInList = require('./check_ip_in_list');
 
 /**
@@ -44,25 +42,6 @@ var checkIpInList = require('./check_ip_in_list');
  * @memberof module:helpers/http_api
  */
 var middleware = {
-	/**
-	 * Adds CORS header to all requests.
-	 *
-	 * @memberof module:helpers/http_api.middleware
-	 * @param {Object} req
-	 * @param {Object} res
-	 * @param {function} next
-	 * @todo Add description for the params
-	 * @todo Add @returns tag
-	 */
-	cors(req, res, next) {
-		res.header('Access-Control-Allow-Origin', '*');
-		res.header(
-			'Access-Control-Allow-Headers',
-			'Origin, X-Objected-With, Content-Type, Accept'
-		);
-		return next();
-	},
-
 	/**
 	 * Logs all api errors.
 	 *
@@ -119,62 +98,6 @@ var middleware = {
 		logger.log(`${req.method} ${req.url} from ${req.ip}`);
 
 		return next();
-	},
-
-	/**
-	 * Resends error msg when blockchain is not loaded.
-	 *
-	 * @memberof module:helpers/http_api.middleware
-	 * @param {function} isLoaded
-	 * @param {Object} req
-	 * @param {Object} res
-	 * @param {function} next
-	 * @todo Add description for the params
-	 * @todo Add @returns tag
-	 */
-	blockchainReady(isLoaded, req, res, next) {
-		if (isLoaded()) {
-			return next();
-		}
-		res.status(500).send({ success: false, error: 'Blockchain is loading' });
-	},
-
-	/**
-	 * Resends error if API endpoint doesn't exists.
-	 *
-	 * @memberof module:helpers/http_api.middleware
-	 * @param {Object} req
-	 * @param {Object} res
-	 * @param {function} next
-	 * @todo Add description for the params
-	 * @todo Add @returns tag
-	 */
-	notFound(req, res) {
-		return res
-			.status(500)
-			.send({ success: false, error: 'API endpoint not found' });
-	},
-
-	/**
-	 * Uses req.sanitize for particular endpoint.
-	 *
-	 * @memberof module:helpers/http_api.middleware
-	 * @param {string} property
-	 * @param {Object} schema
-	 * @param {function} cb
-	 * @returns {function}
-	 * @todo Add description for the params
-	 */
-	sanitize(property, schema, cb) {
-		// TODO: Remove optional error codes response handler choice as soon as all modules will be conformed to new REST API standards
-		return function(req, res) {
-			req.sanitize(req[property], schema, (err, report, sanitized) => {
-				if (!report.isValid) {
-					return res.json({ success: false, error: report.issues });
-				}
-				return cb(sanitized, respond.bind(null, res));
-			});
-		};
 	},
 
 	/**
@@ -239,131 +162,44 @@ var middleware = {
 		}
 	},
 
-	/**
-	 * Passes getter for headers and assign then to response.
-	 *
-	 * @memberof module:helpers/http_api.middleware
-	 * @param {function} getHeaders
-	 * @param {Object} req
-	 * @param {Object} res
-	 * @param {function} next
-	 * @todo Add description for the params
-	 * @todo Add @returns tag
-	 */
-	attachResponseHeaders(getHeaders, req, res, next) {
-		res.set(getHeaders());
-		return next();
-	},
+	queryParser() {
+		const ignoredPramList = [
+			'id',
+			'name',
+			'username',
+			'blockId',
+			'transactionId',
+			'address',
+			'recipientId',
+			'senderId',
+			'search',
+		];
 
-	/**
-	 * Lookup cache, and reply with cached response if it's a hit.
-	 * If it's a miss, forward the request but cache the response if it's a success.
-	 *
-	 * @memberof module:helpers/http_api.middleware
-	 * @param {Object} req
-	 * @param {Object} res
-	 * @param {function} next
-	 * @todo Add description for the params
-	 * @todo Add @returns tag
-	 */
-	useCache(logger, cache, req, res, next) {
-		if (!cache.isReady()) {
-			return next();
-		}
+		return queryParser({
+			parser(value, radix, name) {
+				if (ignoredPramList.indexOf(name) >= 0) {
+					return value;
+				}
 
-		var key = req.originalUrl;
-		cache.getJsonForKey(key, (err, cachedValue) => {
-			// There was an error or value doesn't exist for key
-			if (err || !cachedValue) {
-				// Monkey patching res.json function only if we expect to cache response
-				var expressSendJson = res.json;
-				res.json = function(response) {
-					// ToDo: Remove response.success check when API refactor is done (#225)
-					if (
-						response.success ||
-						(response.success === undefined && res.statusCode === apiCodes.OK)
-					) {
-						logger.debug('Cache - Response for key:', req.url);
-						cache.setJsonForKey(key, response);
-					}
-					expressSendJson.call(res, response);
-				};
-				next();
-			} else {
-				logger.debug('Cache - Response for url:', req.url);
-				res.json(cachedValue);
-			}
+				// Ignore conditional fields for transactions list
+				if (/^.+?:(blockId|recipientId|senderId)$/.test(name)) {
+					return value;
+				}
+
+				if (
+					isNaN(value) ||
+					parseInt(value) != value ||
+					isNaN(parseInt(value, radix))
+				) {
+					return value;
+				}
+
+				return parseInt(value);
+			},
 		});
 	},
 };
 
-/**
- * Adds 'success' field to every response and attach error message if needed.
- *
- * @param {Object} res
- * @param {string} err
- * @param {Object} response
- * @todo Add description for the params
- * @todo Add @returns tag
- */
-function respond(res, err, response) {
-	if (err) {
-		res.json({ success: false, error: err });
-	} else {
-		return res.json(extend({}, { success: true }, response));
-	}
-}
-
-/**
- * Adds code status to responses for every failed request.
- * Default error code is 500.
- * Success code is 200.
- * Success code for empty data is 204.
- *
- * @param {Object} res
- * @param {ApiError} err
- * @param {Object} response
- * @todo Add description for the params
- * @todo Add @returns tag
- */
-function respondWithCode(res, err, response) {
-	if (err) {
-		return res
-			.status(err.code || apiCodes.INTERNAL_SERVER_ERROR)
-			.json(err.toJson());
-	}
-	var isResponseEmpty = function(response) {
-		var firstValue = _(response)
-			.values()
-			.first();
-		return _.isArray(firstValue) && _.isEmpty(firstValue);
-	};
-	return res
-		.status(
-			isResponseEmpty(response) ? apiCodes.EMPTY_RESOURCES_OK : apiCodes.OK
-		)
-		.json(response);
-}
-
-/**
- * Register router in express app using default middleware.
- *
- * @param {string} route
- * @param {Object} app
- * @param {Object} router
- * @param {function} isLoaded
- * @todo Add description for the params
- * @todo Add @returns tag
- */
-function registerEndpoint(route, app, router, isLoaded) {
-	router.use(middleware.notFound);
-	router.use(middleware.blockchainReady.bind(null, isLoaded));
-	app.use(route, router);
-}
-
 module.exports = {
 	middleware,
-	registerEndpoint,
-	respond,
-	respondWithCode,
 };
--- test/unit/helpers/http_api.js
+++ test/unit/helpers/http_api.js
@@ -15,8 +15,9 @@
 
 'use strict';
 
-var httpApi = require('../../../helpers/http_api');
-const apiCodes = require('../../../helpers/api_codes');
+var rewire = require('rewire');
+
+var httpApi = rewire('../../../helpers/http_api');
 
 var validUrl = 'api/url';
 var validOriginalUrl = 'org/url';
@@ -49,6 +50,7 @@ describe('httpApi', () => {
 			};
 			validNextSpy = sinonSandbox.spy();
 			spyConsoleTrace = sinonSandbox.spy(console, 'trace');
+			checkIpInListStub = sinonSandbox.spy();
 
 			loggerMock = {
 				trace: sinonSandbox.spy(),
@@ -90,35 +92,6 @@ describe('httpApi', () => {
 			resMock.status.reset();
 			resMock.send.reset();
 			done();
-		});
-
-		describe('cors', () => {
-			beforeEach(done => {
-				httpApi.middleware.cors(validReq, resMock, validNextSpy);
-				done();
-			});
-
-			it('should call res.header with "Access-Control-Allow-Origin" and "*"', done => {
-				expect(
-					resMock.header.calledWith('Access-Control-Allow-Origin', '*')
-				).to.eql(true);
-				done();
-			});
-
-			it('should call res.header "Access-Control-Allow-Headers" and "Origin, X-Objected-With, Content-Type, Accept"', done => {
-				expect(
-					resMock.header.calledWith(
-						'Access-Control-Allow-Headers',
-						'Origin, X-Objected-With, Content-Type, Accept'
-					)
-				).to.eql(true);
-				done();
-			});
-
-			it('should call next()', done => {
-				expect(validNextSpy.calledOnce).to.be.true;
-				done();
-			});
 		});
 
 		describe('errorLogger', () => {
@@ -206,163 +179,6 @@ describe('httpApi', () => {
 			});
 		});
 
-		describe('blockchainReady', () => {
-			var validIsLoaded;
-
-			before(done => {
-				validIsLoaded = sinonSandbox.stub();
-				validSendObject = { success: false, error: 'Blockchain is loading' };
-				done();
-			});
-
-			beforeEach(done => {
-				httpApi.middleware.blockchainReady(
-					validIsLoaded,
-					validReq,
-					resMock,
-					validNextSpy
-				);
-				done();
-			});
-
-			describe('when isLoaded returns true', () => {
-				before(done => {
-					validIsLoaded.returns(true);
-					done();
-				});
-
-				it('should call next function', done => {
-					expect(validNextSpy.calledOnce).to.be.true;
-					done();
-				});
-			});
-
-			describe('when isLoaded returns false', () => {
-				before(done => {
-					validIsLoaded.returns(false);
-					done();
-				});
-
-				it('should send status 500 and error-object', done => {
-					expect(resMock.status.calledWith(500)).to.be.true;
-					expect(resMock.status().send.calledWith(validSendObject)).to.be.true;
-					done();
-				});
-			});
-		});
-
-		describe('notFound', () => {
-			before(done => {
-				validSendObject = { success: false, error: 'API endpoint not found' };
-				done();
-			});
-
-			beforeEach(done => {
-				httpApi.middleware.notFound(validReq, resMock, validNextSpy);
-				done();
-			});
-
-			it('should send status 500 and error-object', done => {
-				expect(resMock.status.calledWith(500)).to.be.true;
-				expect(resMock.status().send.calledWith(validSendObject)).to.be.true;
-				done();
-			});
-		});
-
-		describe('sanitize', () => {
-			var validProperty;
-			var validSchema;
-			var validCbSpy;
-			var sanitizeResultFunction;
-
-			before(done => {
-				validProperty = 'url';
-				validSchema = null;
-				validCbSpy = sinonSandbox.spy();
-				done();
-			});
-
-			beforeEach(done => {
-				sanitizeResultFunction = httpApi.middleware.sanitize(
-					validProperty,
-					validSchema,
-					validCbSpy
-				);
-				done();
-			});
-
-			it('should return a function', done => {
-				expect(sanitizeResultFunction).to.be.a('function');
-				done();
-			});
-
-			describe('when sanitize result is called', () => {
-				var validReqMock;
-				var validSanitizeError;
-				var validSanitizeReport;
-				var validSanitizeSanitized;
-				var validValue = 'validValue';
-				var validSanitizeCallback;
-
-				before(done => {
-					validRes = {
-						json: sinonSandbox.stub(),
-					};
-					validSanitizeReport = { isValid: true };
-					validReqMock = {
-						sanitize: sinonSandbox.stub(),
-					};
-					validReqMock.sanitize.yields(
-						validSanitizeError,
-						validSanitizeReport,
-						validSanitizeSanitized
-					);
-					validReqMock[validProperty] = validValue;
-					validSanitizeCallback = sinonSandbox.stub();
-					done();
-				});
-
-				beforeEach(done => {
-					sanitizeResultFunction(validReqMock, validRes, validSanitizeCallback);
-					done();
-				});
-
-				it('should call req.sanitize with req[property], schema and cb as arguments', done => {
-					expect(
-						validReqMock.sanitize.calledWith(
-							validReqMock[validProperty],
-							validSchema
-						)
-					).to.be.true;
-					done();
-				});
-
-				describe('when report.isValid = false', () => {
-					before(done => {
-						validSanitizeReport.isValid = false;
-						done();
-					});
-
-					it('should call res.json', done => {
-						expect(validCbSpy.called).to.be.true;
-						done();
-					});
-				});
-
-				describe('when report.isValid = true', () => {
-					before(done => {
-						validSanitizeReport.isValid = true;
-						done();
-					});
-
-					it('should call callback', done => {
-						expect(validCbSpy.called).to.be.true;
-						done();
-					});
-				});
-			});
-		});
-
 		describe('attachResponseHeader', () => {
 			var validHeaderKey;
 			var validHeaderValue;
@@ -397,7 +213,7 @@ describe('httpApi', () => {
 			});
 		});
 
-		describe.skip('applyAPIAccessRules', () => {
+		describe('applyAPIAccessRules', () => {
 			var validConfig;
 
 			before(done => {
@@ -406,12 +222,21 @@ describe('httpApi', () => {
 						enabled: true,
 						access: { blacklist: [] },
 					},
-					api: sinonSandbox.stub(),
+					api: {
+						enabled: true,
+						access: {
+							public: true,
+							whiteList: ['127.0.0.1'],
+						},
+					},
 				};
 				done();
 			});
 
 			beforeEach(done => {
+				checkIpInListStub = sinonSandbox.stub();
+				httpApi.__set__('checkIpInList', checkIpInListStub);
+
 				httpApi.middleware.applyAPIAccessRules(
 					validConfig,
 					validReq,
@@ -428,14 +253,12 @@ describe('httpApi', () => {
 				});
 
 				it('should call checkIpInList with parameters: config.peers.access.blackList, req.ip, false', done => {
-					sinonSandbox.assert.called(checkIpInListStub);
-					expect(
-						checkIpInListStub.calledWith(
-							validConfig.peers.access.blacklist,
-							validReq.ip,
-							false
-						)
-					).to.be.true;
+					expect(checkIpInListStub).to.be.calledOnce;
+					expect(checkIpInListStub).to.be.calledWith(
+						validConfig.api.access.blacklist,
+						validReq.ip,
+						false
+					);
 					done();
 				});
 
@@ -483,318 +306,6 @@ describe('httpApi', () => {
 			});
 		});
 
-		describe.skip('useCache', () => {
-			var validCache;
-			var validCachedValue;
-			var validErr;
-
-			before(done => {
-				validCache = {
-					isReady: sinonSandbox.stub(),
-					getJsonForKey: sinonSandbox.stub(),
-					setJsonForKey: sinonSandbox.stub(),
-				};
-				validRes = {};
-				validErr = 'error';
-				validCachedValue = 'cachedValue';
-				done();
-			});
-
-			beforeEach(done => {
-				httpApi.middleware.useCache(
-					loggerMock,
-					validCache,
-					validReq,
-					validRes,
-					validNextSpy
-				);
-				done();
-			});
-
-			afterEach(done => {
-				loggerMock.debug.reset();
-				done();
-			});
-
-			describe('when cache.isReady() = false', () => {
-				before(done => {
-					validCache.isReady.returns(false);
-					done();
-				});
-
-				it('should call next function', done => {
-					expect(validNextSpy.calledOnce).to.be.true;
-					done();
-				});
-			});
-
-			describe('when cache.isReady() = true', () => {
-				before(done => {
-					validCache.isReady.returns(true);
-					validCache.getJsonForKey.yields(validErr, validCachedValue);
-					done();
-				});
-
-				it('should call cache.getJsonForKey with key = req.originalUrl', done => {
-					expect(validCache.getJsonForKey.calledWith(validReq.originalUrl)).to
-						.be.true;
-					done();
-				});
-
-				describe('when err = true', () => {
-					before(done => {
-						validErr = true;
-						done();
-					});
-
-					it('should add json function to response', done => {
-						expect(validRes.json).to.be.a('function');
-						done();
-					});
-
-					// Not tested, because defined function is not executed at this point
-					describe.skip('when res.statusCode = 200 (OK)', () => {
-						before(done => {
-							validRes.statusCode = 200;
-							done();
-						});
-
-						it('should call logger.debug with "cached response for key: api/url"', done => {
-							expect(
-								loggerMock.debug.calledWith('cached response for key: api/url')
-							).to.be.true;
-							done();
-						});
-
-						it('should call cache.setJsonForKey with key, response as arguments', done => {
-							// expect(validCache.setJsonForKey.calledWith(validReq.url, validCachedValue));
-							expect(
-								validCache.setJsonForKey.calledWith(
-									validReq.url,
-									validCachedValue
-								)
-							).to.be.true;
-							done();
-						});
-					});
-
-					it('should call next function', done => {
-						expect(validNextSpy.calledOnce).to.be.true;
-						done();
-					});
-				});
-
-				describe('when cachedValue  = false', () => {
-					before(done => {
-						validCachedValue = false;
-						done();
-					});
-
-					it('should add json function to response', done => {
-						expect(validRes.json).to.be.a('function');
-						done();
-					});
-
-					it('should call next function', done => {
-						expect(validNextSpy.calledOnce).to.be.true;
-						done();
-					});
-				});
-
-				describe('when cachedValue = true and err = false', () => {
-					before(done => {
-						validCachedValue = 'cachedValue';
-						validErr = false;
-						validRes = {
-							json: sinonSandbox.stub(),
-						};
-						validRes.json.withArgs(validCachedValue);
-						validCache.getJsonForKey.yields(validErr, validCachedValue);
-						done();
-					});
-
-					it('should call logger.debug', done => {
-						expect(loggerMock.debug.called).to.be.true;
-						done();
-					});
-
-					it('should call res.json with cachedValue', done => {
-						expect(validRes.json.calledWith(validCachedValue)).to.be.true;
-						done();
-					});
-				});
-			});
-		});
-	});
-
-	describe('respond', () => {
-		var validResponse = {};
-
-		before(done => {
-			validRes = {
-				json: sinonSandbox.stub(),
-			};
-			done();
-		});
-
-		beforeEach(done => {
-			httpApi.respond(validRes, validError, validResponse);
-			done();
-		});
-
-		afterEach(done => {
-			validRes.json.reset();
-			done();
-		});
-
-		describe('when error is defined', () => {
-			it('should call res.json with {"success": false, "error": err}', done => {
-				expect(validRes.json.calledWith({ success: false, error: validError }))
-					.to.be.true;
-				done();
-			});
-		});
-
-		describe('when error is undefined', () => {
-			before(done => {
-				validError = null;
-				done();
-			});
-
-			it('should call res.json with extend({}, {"success": true}, response)', done => {
-				validResponse.success = true;
-				expect(validRes.json.calledWith(validResponse)).to.be.true;
-				done();
-			});
-		});
-	});
-
-	describe('respondWithCode', () => {
-		var validResponse;
-
-		before(done => {
-			validResponse = {
-				data: [],
-			};
-			validError = {
-				message: 'validError',
-				toJson: sinonSandbox.stub(),
-			};
-
-			validRes = {
-				json: sinonSandbox.stub(),
-				status: sinonSandbox.stub(),
-			};
-			validRes.status.returns(validRes);
-			done();
-		});
-
-		beforeEach(done => {
-			httpApi.respondWithCode(validRes, validError, validResponse);
-			done();
-		});
-
-		afterEach(done => {
-			validRes.json.reset();
-			done();
-		});
-
-		describe('when error is defined', () => {
-			before(done => {
-				validError = {
-					message: 'validError',
-					toJson: sinonSandbox.stub(),
-				};
-				done();
-			});
-
-			it('should call res.status(500).json() with error in json format', done => {
-				var tmp_error = validError.code
-					? validError.code
-					: apiCodes.INTERNAL_SERVER_ERROR;
-
-				expect(validRes.status.calledWith(tmp_error)).to.be.true;
-				expect(validRes.json.calledOnce).to.be.true;
-				done();
-			});
-		});
-
-		describe('when error is undefined', () => {
-			before(done => {
-				validError = null;
-				done();
-			});
-
-			describe('when response is empty', () => {
-				it('should call res.status with code = 204 and res.json', done => {
-					expect(validRes.status.calledWith(apiCodes.EMPTY_RESOURCES_OK)).to.be
-						.true;
-					expect(validRes.json.calledOnce).to.be.true;
-					done();
-				});
-			});
-
-			describe('when response is not empty', () => {
-				before(done => {
-					validResponse = {
-						data: [1, 2, 3],
-					};
-					done();
-				});
-
-				it('should call res.status with code = 200 and res.json', done => {
-					expect(validRes.status.calledWith(apiCodes.OK)).to.be.true;
-					expect(validRes.json.calledOnce).to.be.true;
-					done();
-				});
-			});
-		});
-	});
-
-	describe('registerEndpoint', () => {
-		var validRoute;
-		var validApp;
-		var validRouter;
-		var validIsLoaded;
-
-		before(done => {
-			validRoute = null;
-			validApp = {
-				use: sinonSandbox.stub(),
-			};
-			validRouter = {
-				use: sinonSandbox.stub(),
-			};
-			validIsLoaded = true;
-			done();
-		});
-
-		beforeEach(done => {
-			httpApi.registerEndpoint(
-				validRoute,
-				validApp,
-				validRouter,
-				validIsLoaded
-			);
-			done();
-		});
-
-		it('should call router.use with middleware.notFound', done => {
-			expect(validRouter.use.calledWith(httpApi.middleware.notFound)).to.be
-				.true;
-			done();
-		});
-
-		it('should call router.use with middleware.blockchainReady.bind(null, validIsLoaded)', done => {
-			expect(validRouter.use.args[1][0].toString()).to.equal(
-				httpApi.middleware.blockchainReady.bind(null, validIsLoaded).toString()
-			);
-			done();
-		});
-
-		it('should call app.use with route and router as arguments', done => {
-			expect(validApp.use.calledWith(validRoute, validRouter));
-			done();
-		});
+		describe('queryParser', () => {});
 	});
 });
