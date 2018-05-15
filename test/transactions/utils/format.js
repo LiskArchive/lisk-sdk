ccc94dfb48432b4f65c9a22fc63f11a1e427f66e
willclarktech <willclarktech@users.noreply.github.com>
2018-05-15 13:25:37 +0200

    :seedling: Add functions for converting between LSK and Beddows
    Closes #635

--- test/transactions/utils/format.js
+++ test/transactions/utils/format.js
@@ -13,11 +13,69 @@
  *
  */
 import {
+	convertBeddowsToLSK,
+	convertLSKToBeddows,
 	prependPlusToPublicKeys,
 	prependMinusToPublicKeys,
 } from 'transactions/utils/format';
 
 describe('format', () => {
+	describe('#convertBeddowsToLSK', () => {
+		it('should error on 18446744073709551615.1', () => {
+			return expect(
+				convertBeddowsToLSK.bind(null, '18446744073709551615.1'),
+			).to.throw('Beddows amount should not have decimal points');
+		});
+		it('should error on 0.1', () => {
+			return expect(convertBeddowsToLSK.bind(null, '0.1')).to.throw(
+				'Beddows amount should not have decimal points',
+			);
+		});
+		it('should error on 18446744073709551616', () => {
+			return expect(
+				convertBeddowsToLSK.bind(null, '18446744073709551616'),
+			).to.throw('Beddows amount out of range');
+		});
+		it('should convert 100000000 to 1', () => {
+			return expect(convertBeddowsToLSK('100000000')).to.equal('1');
+		});
+		it('should convert 1 to 0.00000001', () => {
+			return expect(convertBeddowsToLSK('1')).to.equal('0.00000001');
+		});
+		it('should convert 18446744073709551615 to 184467440737.09551615', () => {
+			return expect(convertBeddowsToLSK('18446744073709551615')).to.equal(
+				'184467440737.09551615',
+			);
+		});
+	});
+	describe('#convertLSKToBeddows', () => {
+		it('should error on 184467440737.095516151', () => {
+			return expect(
+				convertLSKToBeddows.bind(null, '184467440737.095516151'),
+			).to.throw('LSK amount has too many decimal points');
+		});
+		it('should error on 0.000000001', () => {
+			return expect(convertLSKToBeddows.bind(null, '0.000000001')).to.throw(
+				'LSK amount has too many decimal points',
+			);
+		});
+		it('should error on 184467440737.09551616', () => {
+			return expect(
+				convertLSKToBeddows.bind(null, '184467440737.09551616'),
+			).to.throw('LSK amount out of range');
+		});
+		it('should convert 1 to 100000000', () => {
+			return expect(convertLSKToBeddows('1')).to.equal('100000000');
+		});
+		it('should convert 0.00000001 to 1', () => {
+			return expect(convertLSKToBeddows('0.00000001')).to.equal('1');
+		});
+		it('should convert 184467440737.09551615 to 18446744073709551615', () => {
+			return expect(convertLSKToBeddows('184467440737.09551615')).to.equal(
+				'18446744073709551615',
+			);
+		});
+	});
 	describe('#prependPlusToPublicKeys', () => {
 		describe('Given an array of public keys', () => {
 			it('should append plus to each public key', () => {
