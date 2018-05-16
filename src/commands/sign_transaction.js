40cee73f45ec400a1684297f9559c0d00cb8a423
Shusetsu Toda <shusetsu@lightcurve.io>
2018-05-16 09:16:11 +0200

    :seedling: Add sign transaction command

---
+++ src/commands/sign_transaction.js
@@ -0,0 +1,94 @@
+/*
+ * LiskHQ/lisky
+ * Copyright © 2017 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import transactions from '../utils/transactions';
+import { ValidationError } from '../utils/error';
+import { createCommand } from '../utils/helpers';
+import getInputsFromSources from '../utils/input';
+import { getStdIn } from '../utils/input/utils';
+import commonOptions from '../utils/options';
+
+const description = `Sign a transaction using your secret passphrase.
+
+	Example: sign transaction '{"amount":"100","recipientId":"13356260975429434553L","senderPublicKey":null,"timestamp":52871598,"type":0,"fee":"10000000","recipientPublicKey":null,"asset":{}}'
+`;
+
+const getTransactionInput = ({ transaction, stdin, shouldUseStdIn }) => {
+	const hasStdIn = stdin && stdin[0];
+	if (shouldUseStdIn && !hasStdIn) {
+		return null;
+	}
+	return shouldUseStdIn ? stdin[0] : transaction;
+};
+
+const getStdInOnNonInteractiveMode = async () => {
+	// We should only get normal stdin for NON_INTERACTIVE_MODE
+	if (process.env.NON_INTERACTIVE_MODE) {
+		const stdin = await getStdIn({ dataIsRequired: true });
+		return stdin.data;
+	}
+	return null;
+};
+
+export const actionCreator = vorpal => async ({
+	transaction,
+	stdin,
+	options = {},
+}) => {
+	const shouldUseStdIn = !transaction;
+	const transactionSource = getTransactionInput({
+		transaction,
+		stdin,
+		shouldUseStdIn,
+	});
+	const transactionInput =
+		transactionSource || (await getStdInOnNonInteractiveMode());
+
+	if (!transactionInput) {
+		throw new ValidationError('No transaction was provided.');
+	}
+
+	let transactionObject;
+	try {
+		transactionObject = JSON.parse(transactionInput);
+	} catch (error) {
+		throw new ValidationError('Could not parse transaction JSON.');
+	}
+
+	const passphraseSource = options.passphrase;
+
+	const { passphrase } = await getInputsFromSources(vorpal, {
+		passphrase: {
+			source: passphraseSource,
+			repeatPrompt: true,
+		},
+	});
+
+	const signature = transactions.utils.signTransaction(
+		transactionObject,
+		passphrase,
+	);
+	return Object.assign({}, transactionObject, { signature });
+};
+
+const signTransaction = createCommand({
+	command: 'sign transaction [transaction]',
+	description,
+	actionCreator,
+	options: [commonOptions.passphrase],
+	errorPrefix: 'Could not sign transaction',
+});
+
+export default signTransaction;
