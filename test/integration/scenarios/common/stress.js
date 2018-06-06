90465c42f916b2f98b16289508c2c71e301011c9
Manu Nelamane Siddalingegowda <manu@lightcurve.io>
2018-06-06 09:53:15 +0200

    Tidy up after review comments

--- test/integration/scenarios/common/stress.js
+++ test/integration/scenarios/common/stress.js
@@ -1,22 +1,19 @@
 'use strict';
 
 const getTransaction = require('../../utils/http').getTransaction;
-const { confirmTransaction } = require('../../../common/utils/wait_for');
 
 const confirmTransactionsOnAllNodes = function(transactions, params) {
-	return confirmTransaction().then(() => {
-		return Promise.all(
-			_.flatMap(params.configurations, configuration => {
-				return transactions.map(transaction => {
-					return getTransaction(transaction.id, configuration.httpPort);
-				});
-			})
-		).then(results => {
-			results.forEach(transaction => {
-				expect(transaction)
-					.to.have.property('id')
-					.that.is.an('string');
+	return Promise.all(
+		_.flatMap(params.configurations, configuration => {
+			return transactions.map(transaction => {
+				return getTransaction(transaction.id, configuration.httpPort);
 			});
+		})
+	).then(results => {
+		results.forEach(transaction => {
+			expect(transaction)
+				.to.have.property('id')
+				.that.is.an('string');
 		});
 	});
 };
