ad5881cb9ea9a14072c575e47200a8cd448f009f
Nazar Hussain <nazar@lightcurve.io>
2018-06-12 02:01:35 +0200

    Fix keypair fix to make buffer from string

--- helpers/ed.js
+++ helpers/ed.js
@@ -52,6 +52,9 @@ ed.makeKeypair = function(hash) {
  * @todo Add description for the params and the return value
  */
 ed.sign = function(hash, privateKey) {
+	if (typeof privateKey === 'string') {
+		privateKey = Buffer.from(privateKey, 'hex');
+	}
 	return sodium.crypto_sign_detached(hash, privateKey);
 };
 
