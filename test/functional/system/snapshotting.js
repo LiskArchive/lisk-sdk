42eaee1e5d140575f12d156e93aed8b70574d9bc
Mariusz Serek <mariusz@serek.net>
2018-06-13 17:23:01 +0200

    Extend snapshotting functional system tests with sending transaction at round 1

--- test/functional/system/snapshotting.js
+++ test/functional/system/snapshotting.js
@@ -15,6 +15,9 @@
 'use strict';
 
 const Promise = require('bluebird');
+const elements = require('lisk-elements').default;
+const randomUtil = require('../../common/utils/random');
+const accountsFixtures = require('../../fixtures/accounts');
 const constants = require('../../../helpers/constants');
 const queriesHelper = require('../common/sql/queriesHelper.js');
 const localCommon = require('./common');
@@ -51,18 +54,32 @@ describe('snapshotting', () => {
 
 		before(() => {
 			// Forge 201 blocks to reach height 202 (genesis block is already tyhere)
-			return Promise.mapSeries([...Array(201)], () => {
+			return Promise.mapSeries([...Array(99)], () => {
 				return addTransactionsAndForgePromise(library, [], 0);
-			}).then(() => {
-				return getMemAccounts().then(_accounts => {
-					// Save copy of mem_accounts table
-					memAccountsBeforeSnapshot = _.cloneDeep(_accounts);
-					// Forge one more round (blocks from that round should be deleted during snapshotting process)
+			})
+				.then(() => {
+					const transaction = elements.transaction.transfer({
+						recipientId: randomUtil.account().address,
+						amount: randomUtil.number(100000000, 1000000000),
+						passphrase: accountsFixtures.genesis.passphrase,
+					});
+					return addTransactionsAndForgePromise(library, [transaction], 0);
+				})
+				.then(() => {
 					return Promise.mapSeries([...Array(101)], () => {
 						return addTransactionsAndForgePromise(library, [], 0);
 					});
+				})
+				.then(() => {
+					return getMemAccounts().then(_accounts => {
+						// Save copy of mem_accounts table
+						memAccountsBeforeSnapshot = _.cloneDeep(_accounts);
+						// Forge one more round (blocks from that round should be deleted during snapshotting process)
+						return Promise.mapSeries([...Array(101)], () => {
+							return addTransactionsAndForgePromise(library, [], 0);
+						});
+					});
 				});
-			});
 		});
 
 		it('mem_accounts states after snapshotting should match copy taken after round 2', done => {
