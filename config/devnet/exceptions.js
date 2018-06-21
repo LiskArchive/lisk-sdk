e81601d74d6378d359affd2e2db25f4ab00d52b7
Nazar Hussain <nazar@lightcurve.io>
2018-06-21 12:40:04 +0200

    Add exceptions for devnet

---
+++ config/devnet/exceptions.js
@@ -0,0 +1,49 @@
+/*
+ * Copyright © 2018 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+'use strict';
+
+/**
+ * Description of the namespace.
+ *
+ * @namespace exceptions
+ * @memberof config
+ * @see Parent: {@link config}
+ * @property {Array} blockRewards
+ * @property {Array} delegates
+ * @property {Object} genesisPublicKey
+ * @property {string} genesisPublicKey.mainnet
+ * @property {string} genesisPublicKey.testnet
+ * @property {Object} rounds
+ * @property {string[]} senderPublicKey
+ * @property {string[]} signatures
+ * @property {string[]} multisignatures
+ * @property {string[]} votes
+ * @todo Add description for the namespace and the properties
+ */
+module.exports = {
+	blockRewards: [],
+	delegates: [],
+	genesisPublicKey: {
+		mainnet: 'd121d3abf5425fdc0f161d9ddb32f89b7750b4bdb0bff7d18b191d4b4bafa6d4',
+		testnet: 'c96dec3595ff6041c3bd28b76b8cf75dce8225173d1bd00241624ee89b50f2a8',
+	},
+	rounds: {},
+	senderPublicKey: [],
+	signatures: [],
+	multisignatures: [],
+	votes: [],
+	inertTransactions: [],
+	transactionFee: [],
+};
