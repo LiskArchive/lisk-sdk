1ad3ece63afa57f8c1b9eade8d00b79f47ce5bf2
Nazar Hussain <nazar@lightcurve.io>
2018-06-21 11:26:04 +0200

    Update app.js to load dynamic configuration and genesis block

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ var extend = require('extend');
 var SocketCluster = require('socketcluster');
 var async = require('async');
 var randomstring = require('randomstring');
-var genesisblock = require('./genesis_block.json');
 var Logger = require('./logger.js');
 var wsRPC = require('./api/ws/rpc/ws_rpc').wsRPC;
 var wsTransport = require('./api/ws/transport');
@@ -41,7 +40,6 @@ var swaggerHelper = require('./helpers/swagger');
  * @requires extend
  * @requires fs
  * @requires socketcluster
- * @requires genesis_block.json
  * @requires logger.js
  * @requires api/ws/rpc/ws_rpc.wsRPC
  * @requires helpers/config
@@ -65,7 +63,7 @@ var swaggerHelper = require('./helpers/swagger');
  * @property {undefined} connect
  * @property {Object} db
  * @property {Object} ed
- * @property {Object} genesisblock
+ * @property {Object} genesisBlock
  * @property {string} lastCommit
  * @property {Object} listen
  * @property {Object} logger
@@ -201,7 +199,7 @@ d.run(() => {
 			config(cb) {
 				try {
 					appConfig.nethash = Buffer.from(
-						genesisblock.payloadHash,
+						appConfig.genesisBlock.payloadHash,
 						'hex'
 					).toString('hex');
 
@@ -233,9 +231,9 @@ d.run(() => {
 				cb(null, lastCommit);
 			},
 
-			genesisblock(cb) {
+			genesisBlock(cb) {
 				cb(null, {
-					block: genesisblock,
+					block: appConfig.genesisBlock,
 				});
 			},
 
@@ -535,7 +533,7 @@ d.run(() => {
 				'db',
 				'bus',
 				'schema',
-				'genesisblock',
+				'genesisBlock',
 				/**
 				 * Description of the function.
 				 *
@@ -567,9 +565,9 @@ d.run(() => {
 							schema(cb) {
 								cb(null, scope.schema);
 							},
-							genesisblock(cb) {
+							genesisBlock(cb) {
 								cb(null, {
-									block: genesisblock,
+									block: scope.genesisBlock,
 								});
 							},
 							account: [
@@ -577,7 +575,7 @@ d.run(() => {
 								'bus',
 								'ed',
 								'schema',
-								'genesisblock',
+								'genesisBlock',
 								'logger',
 								function(scope, cb) {
 									new Account(scope.db, scope.schema, scope.logger, cb);
@@ -588,7 +586,7 @@ d.run(() => {
 								'bus',
 								'ed',
 								'schema',
-								'genesisblock',
+								'genesisBlock',
 								'account',
 								'logger',
 								function(scope, cb) {
@@ -596,7 +594,7 @@ d.run(() => {
 										scope.db,
 										scope.ed,
 										scope.schema,
-										scope.genesisblock,
+										scope.genesisBlock,
 										scope.account,
 										scope.logger,
 										cb
@@ -608,7 +606,7 @@ d.run(() => {
 								'bus',
 								'ed',
 								'schema',
-								'genesisblock',
+								'genesisBlock',
 								'account',
 								'transaction',
 								function(scope, cb) {
