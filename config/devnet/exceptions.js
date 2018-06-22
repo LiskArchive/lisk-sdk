9bf89137b13d2867a5147ab93ff8a3b09f748894
Nazar Hussain <nazar@lightcurve.io>
2018-06-22 15:26:32 +0200

    Fix devnet expectations with correct testnet public key

--- config/devnet/exceptions.js
+++ config/devnet/exceptions.js
@@ -37,7 +37,7 @@ module.exports = {
 	delegates: [],
 	genesisPublicKey: {
 		mainnet: 'd121d3abf5425fdc0f161d9ddb32f89b7750b4bdb0bff7d18b191d4b4bafa6d4',
-		testnet: 'c96dec3595ff6041c3bd28b76b8cf75dce8225173d1bd00241624ee89b50f2a8',
+		testnet: '73ec4adbd8f99f0d46794aeda3c3d86b245bd9d27be2b282cdd38ad21988556b',
 	},
 	rounds: {},
 	senderPublicKey: [],
