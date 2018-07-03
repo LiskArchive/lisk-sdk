bb690a729609ba3ed2d984b530f396cc45b40fbc
Diego Garcia <diego@lightcurve.io>
2018-07-03 16:06:14 +0200

    Clean main file from integration test suite

--- test/integration/utils/index.js
+++ test/integration/utils/index.js
@@ -25,16 +25,6 @@ module.exports = {
 		filename: `${__dirname}/integrationTestsLogger.logs`,
 		echo: 'log',
 	}),
-	getOpenConnections: (ports, cb) => {
-		// lsof -i :5000 -i :5001 -P -n | wc -l
-		// tail -n +2 to strip the headers of lsof so we can count the rows
-		childProcess.exec(
-			`lsof ${ports.map(p => `-i :${p}`).join(' ')} -P -n | tail -n +2 | wc -l`,
-			(err, stdout) => {
-				cb(err, parseInt(stdout.toString().trim()));
-			}
-		);
-	},
 	getListeningConnections: (ports, cb) => {
 		// lsof -i :5000 -i :5001 -P -n -s TCP:LISTEN -t | wc -l
 		// tail -n +2 to strip the headers of lsof so we can count the rows
