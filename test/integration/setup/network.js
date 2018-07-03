9193094d3b87e31fb9d62486e717d4cce6375474
Nazar Hussain <nazar@lightcurve.io>
2018-07-03 15:13:12 +0200

    Undo an unwanted commit to change reties for integration tests

--- test/integration/setup/network.js
+++ test/integration/setup/network.js
@@ -22,7 +22,7 @@ var utils = require('../utils');
 
 module.exports = {
 	waitForAllNodesToBeReady(configurations, cb) {
-		const retries = 40;
+		const retries = 20;
 		const timeout = 3000;
 		async.forEachOf(
 			configurations,
