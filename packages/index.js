dangolbeeker/lisk-elements

:recycle: Rename packages to include lisk prefix

packages/index.js
@@ -13,11 +13,11 @@
  *
  */
 import naclFactory from 'js-nacl';
-import APIClient from './api_client';
-import cryptography from './cryptography';
-import * as constants from './lisk-constants';
-import passphrase from './passphrase';
-import transaction from './transactions';
+import APIClient from 'lisk-api-client';
+import cryptography from 'lisk-cryptography';
+import * as constants from 'lisk-constants';
+import passphrase from 'lisk-passphrase';
+import transaction from 'lisk-transactions';
 
 global.naclFactory = naclFactory;
 

LiskArchive/lisk-sdk

packages/index.js
@@ -12,11 +12,11 @@
  * Removal or modification of this copyright notice is prohibited.
  *
  */
-import APIClientModule from './api_client';
-import * as constantsModule from './lisk-constants';
-import cryptographyModule from './cryptography';
-import passphraseModule from './passphrase';
-import transactionModule from './transactions';
+import APIClientModule from 'lisk-api-client';
+import * as constantsModule from 'lisk-constants';
+import cryptographyModule from 'lisk-cryptography';
+import passphraseModule from 'lisk-passphrase';
+import transactionModule from 'lisk-transactions';
 
 export const APIClient = APIClientModule;
 export const constants = constantsModule;