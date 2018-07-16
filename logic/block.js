b43a578a937d45b94e1a6c8f82a326217e238e84
Mariusz Serek <mariusz@serek.net>
2018-07-16 11:51:26 +0200

    Make block.version dynamic during block creation

--- logic/block.js
+++ logic/block.js
@@ -19,6 +19,7 @@ const ByteBuffer = require('bytebuffer');
 const bignum = require('../helpers/bignum.js');
 const constants = require('../helpers/constants.js');
 const transactionTypes = require('../helpers/transaction_types.js');
+const blockVersion = require('../helpers/block_version.js');
 const BlockReward = require('./block_reward.js');
 
 const __private = {};
@@ -125,7 +126,7 @@ class Block {
 		}
 
 		let block = {
-			version: 0,
+			version: blockVersion.get(nextHeight),
 			totalAmount,
 			totalFee,
 			reward,
