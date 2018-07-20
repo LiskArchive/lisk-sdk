8a7cefd330b8330e48593950c28c4e9fc6efbd48
Manu Nelamane Siddalingegowda <manu@lightcurve.io>
2018-07-20 10:09:12 +0200

    Fix system round functional test

--- test/functional/system/rounds.js
+++ test/functional/system/rounds.js
@@ -1071,7 +1071,8 @@ describe('rounds', () => {
 
 				it('block just before rewards start should have reward = 0', () => {
 					const lastBlock = library.modules.blocks.lastBlock.get();
-					return expect(lastBlock.reward.equals(expectedRewardsPerBlock));
+					return expect(lastBlock.reward.equals(expectedRewardsPerBlock)).to.be
+						.true;
 				});
 			});
 
@@ -1114,7 +1115,8 @@ describe('rounds', () => {
 						describe('rewards check', () => {
 							it('all blocks from now until round end should have proper rewards (5 LSK)', () => {
 								const lastBlock = library.modules.blocks.lastBlock.get();
-								return expect(lastBlock.reward.equals(expectedRewardsPerBlock));
+								return expect(lastBlock.reward.equals(expectedRewardsPerBlock))
+									.to.be.true;
 							});
 						});
 
