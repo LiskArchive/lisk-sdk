1c1bb6e2882ac1204e2fae8be06fe055eb388bf8
yatki <mehmet@yatki.com>
2018-07-30 17:09:08 +0200

    :recycle: rename global variable `LiskElements` to `lisk`

--- packages/lisk-elements/scripts/start.sh
+++ packages/lisk-elements/scripts/start.sh
@@ -1,1 +1,1 @@
-node -i -e "global.LiskElements = require('./dist-node').default; console.log('\\x1b[34m', 'The global \`LiskElements\` was initialised', '\\x1b[0m')"
+node -i -e "global.lisk = require('./dist-node').default; console.log('\\x1b[34m', 'The global \`lisk\` was initialised', '\\x1b[0m')"
