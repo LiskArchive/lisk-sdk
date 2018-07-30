8cbe218e91d3780525b8f8cc04a52334059955bd
Usman <usman@lightcurve.io>
2018-07-30 17:19:05 +0200

    add migration for dependentId column of mem_accounts2delegates table

---
+++ db/sql/migrations/updates/20180730162905_change_dependentId_type_in_mem_accounts2delegates.sql
@@ -0,0 +1,23 @@
+
+/*
+ * Copyright © 2018 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+
+/*
+  DESCRIPTION: Alter 'mem_account2delegate' table - change data type of 'dependentId' from VARCHAR(64) to bytea.
+
+  PARAMETERS: None
+*/
+
+ALTER TABLE mem_accounts2delegates ALTER COLUMN "dependentId" TYPE bytea USING DECODE("dependentId", 'hex');
