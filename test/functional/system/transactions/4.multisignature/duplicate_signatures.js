29b10f2d2c40c148e39f8112beb80c659aaf04c4
Mariusz Serek <mariusz@serek.net>
2018-08-01 18:05:27 +0200

    Update test expectation for duplicate_signature functional system tests

--- test/functional/system/transactions/4.multisignature/duplicate_signatures.js
+++ test/functional/system/transactions/4.multisignature/duplicate_signatures.js
@@ -361,7 +361,9 @@ describe('duplicate_signatures', () => {
 					(err, results) => {
 						// There should be an error from processing only for duplicated signature
 						expect(results[0].value).to.be.undefined;
-						expect(results[1].error).to.eql('Signature already exists');
+						expect(results[1].error).to.eql(
+							'Unable to process signature, signature already exists'
+						);
 						expect(results[2].value).to.be.undefined;
 
 						// Get multisignature transaction from pool
