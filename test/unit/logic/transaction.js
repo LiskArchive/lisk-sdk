9ce23c2e14880d53c2b6af67633475148a267922
yatki <mehmet@yatki.com>
2018-09-06 13:30:32 +0200

    :white_check_mark: Update transaction logic test for totalAmount

--- test/unit/logic/transaction.js
+++ test/unit/logic/transaction.js
@@ -751,7 +751,7 @@ describe('transaction', () => {
 			});
 		});
 
-		it('should return error when transaction amount is invalid', done => {
+		it('should not return "Invalid transaction amount" error when transaction amount is bigger than total amount of Lisk tokens', done => {
 			// lisk-elements cannot create a transaction with higher amount than max amount. So, this test needs to use hardcoded transaction object
 			var transaction = {
 				type: 0,
@@ -769,7 +769,8 @@ describe('transaction', () => {
 			};
 
 			transactionLogic.verify(transaction, sender, null, null, err => {
-				expect(err).to.include('Invalid transaction amount');
+				expect(err).to.not.include('Invalid transaction amount');
+				expect(err).to.include('Account does not have enough LSK:');
 				done();
 			});
 		});
