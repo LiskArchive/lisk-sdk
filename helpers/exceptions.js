1e872fb4dfa3c9ce432591ec3abac69f69dc86d5
Maciej Baj <macie.baj@gmail.com>
2018-09-11 12:51:32 +0200

    Remove blockVersion from devnet exceptions

--- helpers/exceptions.js
+++ helpers/exceptions.js
@@ -47,7 +47,5 @@ module.exports = {
 	inertTransactions: [],
 	transactionFee: [],
 	// <version>: { start: <start_height>, end: <end_height> }
-	blockVersions: {
-		0: { start: 1, end: 6901027 },
-	},
+	blockVersions: {},
 };
