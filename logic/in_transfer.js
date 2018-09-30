b3560c4b2855a33058546464a65998c2a26236c9
Mariusz Serek <mariusz@serek.net>
2018-10-01 01:24:53 +0200

    Change condition for verify amount of IN_TRANSFER tx type

--- logic/in_transfer.js
+++ logic/in_transfer.js
@@ -93,7 +93,7 @@ InTransfer.prototype.verify = function(transaction, sender, cb, tx) {
 
 	const amount = new Bignum(transaction.amount);
 
-	if (!amount.equals(0)) {
+	if (amount.equals(0)) {
 		return setImmediate(cb, 'Invalid transaction amount');
 	}
 
