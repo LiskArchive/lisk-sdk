416adb6772b0a3a972e3fb2b64f0da6751ed997a
Pablo Vicente <pvicente@gmail.com>
2018-10-18 16:27:48 +0200

    Improves startup time

--- db/sql/migrations/runtime.sql
+++ db/sql/migrations/runtime.sql
@@ -19,11 +19,12 @@
   PARAMETERS: None
 */
 
-UPDATE "peers" SET "state" = 1, "clock" = NULL WHERE "state" != 0;
-
 -- Overwrite unconfirmed tables with state from confirmed tables
-DELETE FROM mem_accounts2u_delegates;
-INSERT INTO mem_accounts2u_delegates ("accountId", "dependentId") SELECT "accountId", "dependentId" FROM mem_accounts2delegates;
+DROP TABLE mem_accounts2u_delegates;
+CREATE TABLE mem_accounts2u_delegates AS TABLE mem_accounts2delegates;
+ALTER TABLE mem_accounts2u_delegates ADD CONSTRAINT "mem_accounts2u_delegates_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES mem_accounts(address) ON DELETE CASCADE;
+CREATE INDEX "mem_accounts2u_delegates_accountId" ON mem_accounts2u_delegates("accountId");
 
 DELETE FROM mem_accounts2u_multisignatures;
 INSERT INTO mem_accounts2u_multisignatures ("accountId", "dependentId") SELECT "accountId", "dependentId" FROM mem_accounts2multisignatures;
+
