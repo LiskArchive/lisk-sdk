927f013084d93ca3a4512f246e9de70bc56f7d06
Diego Garcia <diego@lightcurve.io>
2018-10-26 14:03:45 +0200

    Fix unit test on transaction logic with expect without expectation

--- test/unit/logic/transaction.js
+++ test/unit/logic/transaction.js
@@ -198,7 +198,7 @@ describe('transaction', () => {
 			inValidTransaction.senderPublicKey =
 				'c094ebee7ec0c50ebee32918655e089f6e1a604b83bcaa760293c61e0f18ab6fx';
 			try {
-				expect(transactionLogic.sign(keyPair, inValidTransaction));
+				expect(transactionLogic.sign(keyPair, inValidTransaction)).to.be.error;
 			} catch (e) {
 				err = e.message;
 			} finally {
