3eb3b1b168c85d2b5f5a41e0dd0b83154f7c59d1
Nazar Hussain <nazar@lightcurve.io>
2018-10-29 07:16:57 +0100

    Add base entity for storage

---
+++ storage/entities/base_entity.js
@@ -0,0 +1,133 @@
+'use strict';
+
+const assert = require('assert');
+const {
+	ImplementationPendingError,
+	NonSupportedFilterTypeError,
+} = require('../errors');
+const filterTypes = require('./filter_types');
+
+class BaseEntity {
+	constructor() {
+		this.defaultFieldSet = null;
+		this.fields = {};
+		this.filters = {};
+	}
+
+	/**
+	 * Get one object from persistence layer
+	 *
+	 * @param {String | Object} filters - Multiple filters or just primary key
+	 * @param {String} fieldSet - Field sets defining collection of fields to get
+	 * @param {Object} options - Extended options
+	 *
+	 * @return {Promise}
+	 */
+	// eslint-disable-next-line class-methods-use-this,no-unused-vars
+	get(filters, fieldSet, options) {
+		throw new ImplementationPendingError();
+	}
+
+	// eslint-disable-next-line class-methods-use-this
+	getAll() {
+		throw new ImplementationPendingError();
+	}
+
+	// eslint-disable-next-line class-methods-use-this
+	count() {
+		throw new ImplementationPendingError();
+	}
+
+	// eslint-disable-next-line class-methods-use-this
+	create() {
+		throw new ImplementationPendingError();
+	}
+
+	// eslint-disable-next-line class-methods-use-this
+	update() {
+		throw new ImplementationPendingError();
+	}
+
+	// eslint-disable-next-line class-methods-use-this
+	save() {
+		throw new ImplementationPendingError();
+	}
+
+	// eslint-disable-next-line class-methods-use-this
+	isPersisted() {
+		throw new ImplementationPendingError();
+	}
+
+	/**
+	 * Returns the available fields sets
+	 * @return Array
+	 */
+	// eslint-disable-next-line class-methods-use-this
+	getFieldSets() {
+		throw new ImplementationPendingError();
+	}
+
+	getFilters() {
+		return Object.keys(this.filters);
+	}
+
+	addField(fieldName, filterType = filterTypes.NUMBER, fieldSets = []) {
+		assert(fieldSets, 'Must provide array of field sets');
+
+		// eslint-disable-next-line no-return-assign
+		fieldSets.forEach(fs => (this.fields[fs] = this.fields[fs] || []));
+		// eslint-disable-next-line no-return-assign
+		fieldSets.forEach(fs => this.fields[fs].push(fieldName));
+
+		switch (filterType) {
+			case filterTypes.BOOLEAN:
+				// eslint-disable-next-line no-useless-escape
+				this.filters[fieldName] = `${fieldName} = $\{${fieldName}\}`;
+				this.filters[`${fieldName}_ne`] = ' <> ${fieldName}';
+				break;
+
+			case filterTypes.TEXT:
+				this.filters[fieldName] = ' = ${fieldName}';
+				this.filters[`${fieldName}_ne`] = ' <> ${fieldName}';
+				this.filters[`${fieldName}_in`] = ' IN (${fieldName:csv})';
+				this.filters[`${fieldName}_like`] = ' LIKE ${fieldName}';
+				break;
+
+			case filterTypes.NUMBER:
+				this.filters[fieldName] = ' = ${fieldName}';
+				this.filters[`${fieldName}_gt`] = ' > ${fieldName}';
+				this.filters[`${fieldName}_gte`] = ' >= ${fieldName}';
+				this.filters[`${fieldName}_lt`] = ' < ${fieldName}';
+				this.filters[`${fieldName}_lte`] = ' <= ${fieldName}';
+				this.filters[`${fieldName}_ne`] = ' <> ${fieldName}';
+				this.filters[`${fieldName}_in`] = ' IN (${fieldName:csv})';
+				break;
+
+			case filterType.BINARY:
+				this.filters[`${fieldName}_like`] =
+					" = DECODE(${fieldName}:val, 'hex')";
+				break;
+
+			default:
+				throw new NonSupportedFilterTypeError();
+		}
+	}
+
+	parseFilters(filters) {
+		const parseFilterObject = object =>
+			Object.keys(object)
+				.map(key => this.filters[key])
+				.join(' AND ');
+
+		if (Array.isArray(filters)) {
+			return filters
+				.map(filterObject => parseFilterObject(filterObject))
+				.join(' OR ');
+		} else if (typeof filters === 'object') {
+			return parseFilterObject(filters);
+		}
+		throw new NonSupportedFilterTypeError();
+	}
+}
+
+module.exports = BaseEntity;
