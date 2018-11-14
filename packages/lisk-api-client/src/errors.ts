72e8457b1b8446d343067ffac497970ad4c5bdaf
mitsujutsu <mitsuaki.uchimoto@gmail.com>
2018-11-14 09:54:43 +0100

    :recycle: Add default errno

--- packages/lisk-api-client/src/errors.ts
+++ packages/lisk-api-client/src/errors.ts
@@ -1,9 +1,11 @@
 import { VError } from 'verror';
 
+const defaultErrorNo = 500;
+
 export class APIError extends VError {
-	public errno: number;
-	public constructor(message: string, errno: number) {
+	public errno?: number;
+	public constructor(message: string, errno?: number) {
 		super(message);
-		this.errno = errno;
+		this.errno = errno || defaultErrorNo;
 	}
 }
