4581448b6744fc8b9a3f1b73e51bf1abe5208af0
Usman <usman@lightcurve.io>
2018-11-23 14:54:56 +0100

    cleanup clean.sh script

--- packages/lisk-transaction-pool/scripts/clean.sh
+++ packages/lisk-transaction-pool/scripts/clean.sh
@@ -8,9 +8,7 @@ IFS=$'\n\t'
 # npm run clean
 
 rm -rf .nyc_output/
-rm -rf browsertest.build/
 rm -rf coverage/
-rm -rf dist-browser/
 rm -rf dist-node/
 rm -rf lerna-debug.log
 rm -rf npm-debug.log
