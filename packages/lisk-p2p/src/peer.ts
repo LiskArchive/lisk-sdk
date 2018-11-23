df20d3d32fc02a077e7cd66f7fcf74cbf208bc66
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2018-11-23 17:39:47 +0100

    Add more basic functionality to Peer class

--- packages/lisk-p2p/src/peer.ts
+++ packages/lisk-p2p/src/peer.ts
@@ -13,45 +13,65 @@
  *
  */
 /* tslint:disable:interface-name */
+import { ILogger, PeerState } from './p2p_types';
+
+// TODO: Use to create outbound socket connection inside peer object.
+// TODO: const socketClusterClient = require('socketcluster-client');
 
 export interface IPeerConfig {
 	readonly clock?: Date;
 	readonly height: number;
 	readonly httpPort?: number;
+	readonly id: string;
+	readonly inboundSocket?: any; // TODO: Type SCServerSocket
 	readonly ip: string;
+	readonly logger: ILogger;
 	readonly os?: string;
-	readonly state?: number;
 	readonly version?: string;
 	readonly wsPort: number;
 }
 
 export class Peer {
 	private height: number;
-	private ip: string;
-	private wsPort: number;
+	private readonly id: string;
+	private readonly inboundSocket: any;
+	private readonly ip: string;
+	private readonly wsPort: number;
 
 	public constructor(peerConfig: IPeerConfig) {
-		this.height = peerConfig.height;
+		this.id = peerConfig.id;
 		this.ip = peerConfig.ip;
 		this.wsPort = peerConfig.wsPort;
+		this.inboundSocket = peerConfig.inboundSocket;
+		this.height = peerConfig.height;
 	}
 
 	public getHeight(): number {
 		return this.height;
 	}
+
+	public getId(): string {
+		return this.id;
+	}
+
 	public getIp(): string {
 		return this.ip;
 	}
+
+	// TODO: Return BANNED when appropriate.
+	public getState(): PeerState {
+		if (this.inboundSocket.state === this.inboundSocket.OPEN) {
+			return PeerState.CONNECTED;
+		}
+
+		return PeerState.DISCONNECTED;
+	}
+
 	public getWsPort(): number {
 		return this.wsPort;
 	}
+
 	public setHeight(height: number): void {
 		this.height = height;
-	}
-	public setIp(ip: string): void {
-		this.ip = ip;
-	}
-	public setWsPort(wsPort: number): void {
-		this.wsPort = wsPort;
 	}
 }
