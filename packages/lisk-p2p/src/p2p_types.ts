4b6df230d010b3db18f7ab03da0bf661d0b181f4
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2018-11-23 17:36:47 +0100

    Add types and interfaces

--- packages/lisk-p2p/src/p2p_types.ts
+++ packages/lisk-p2p/src/p2p_types.ts
@@ -13,6 +13,7 @@
  *
  */
 /* tslint:disable:interface-name no-empty-interface */
+
 export interface IP2PMessagePacket {}
 
 export interface IP2PRequestPacket {}
@@ -21,6 +22,46 @@ export interface IP2PResponsePacket {}
 
 export interface IP2PNodeStatus {}
 
+export interface ILogger {
+	readonly error: (message: string) => void;
+	readonly info: (message: string) => void;
+	readonly log: (message: string) => void;
+	readonly trace: (message: string) => void;
+	readonly warn: (message: string) => void;
+}
+
+// ---- Start IP2PConfig interfaces ----
+
+export interface IP2PConfigPeersAccess {
+	readonly blacklist: ReadonlyArray<string>;
+}
+
+export interface IP2PConfigPeersOptions {
+	readonly broadhashConsensusCalculationInterval: number;
+	readonly timeout: number;
+	readonly wsEngine: string;
+}
+
+export interface IP2PConfigPeers {
+	readonly access: IP2PConfigPeersAccess;
+	readonly enabled: boolean;
+	readonly list: ReadonlyArray<string>;
+	readonly options: IP2PConfigPeersOptions;
+}
+
+export interface IP2PConfig {
+	readonly logger?: ILogger;
+	readonly peers: IP2PConfigPeers;
+}
+
+// ---- End IP2PConfig interfaces ----
+
 export interface IP2PPenalty {}
 
 export interface INetworkStatus {}
+
+export enum PeerState {
+	BANNED = 0,
+	DISCONNECTED = 1,
+	CONNECTED = 2,
+}
