08dada35b9e80aa0b7d686d6e01046fc746ab134
Usman <usman@lightcurve.io>
2018-11-23 16:18:34 +0100

    implement queue datastructure for lisk-transaction-pool

--- packages/lisk-transaction-pool/src/queue.ts
+++ packages/lisk-transaction-pool/src/queue.ts
@@ -1,13 +1,23 @@
 import { Transaction } from  './transaction_pool';
 
-//* tslint:disable */
 interface QueueIndex {
 	[index: string]: Transaction;
-}
+};
+
+interface RemoveForReduceObject {
+	readonly effected: Transaction[];
+	readonly uneffected: Transaction[];
+};
+
+interface dequeueUntilReduceObject {
+	readonly effected: Transaction[];
+	readonly uneffected: Transaction[];
+	readonly conditionFailedOnce: boolean;
+};
 
 export class Queue {
-	private _transactions: ReadonlyArray<Transaction>;
 	private _index: QueueIndex;
+	private _transactions: ReadonlyArray<Transaction>;
 
 	public get transactions(): ReadonlyArray<Transaction> {
 		return this._transactions;
@@ -22,18 +32,67 @@ export class Queue {
 		this._index = {};
 	}
 
+	public dequeueUntil(condition: (transaction: Transaction) => boolean): ReadonlyArray<Transaction> {
+		const { effected, uneffected } = this._transactions.reduceRight(({effected, uneffected, conditionFailedOnce}: dequeueUntilReduceObject, transaction: Transaction) => {
+			// Add transaction to the uneffected list if the condition failed for this transaction or any previous transaction
+			if (conditionFailedOnce || !condition(transaction)) {
+				return {
+					effected,
+					uneffected: [transaction, ...uneffected],
+					conditionFailedOnce: true,
+				};
+			}
+
+			// delete the index of the transaction which passed the condition
+			delete this._index[transaction.id];
+
+			return {
+				effected: [transaction, ...effected],
+				uneffected,
+				conditionFailedOnce: false,
+			};
+		}, {
+			effected: [],
+			uneffected: [],
+			conditionFailedOnce: false
+		});
+
+		this._transactions = uneffected;
+
+		return effected;
+	}
+
 	public enqueueMany(transactions: ReadonlyArray<Transaction>): void {
+		this._transactions = [...transactions, ...this._transactions];
+
+		transactions.forEach((transaction: Transaction) => {
+			this._index[transaction.id] = transaction;
+		});
 	}
 
 	public enqueueOne(transaction: Transaction): void {
+		this._transactions = [transaction, ...this._transactions];
+		this._index[transaction.id] = transaction;
 	}
 
 	public exists(transaction: Transaction): boolean {
+		return !!this._index[transaction.id];
 	}
 
 	public removeFor(condition: (transaction: Transaction) => boolean): ReadonlyArray<Transaction> {
-	}
+		const { uneffected, effected } = this._transactions.reduce((reduceObject: RemoveForReduceObject, transaction: Transaction) => {
+			if (condition(transaction)) {
+				reduceObject.effected.push(transaction)
+				// tslint:disable-next-line
+				delete this._index[transaction.id];
+			} else {
+				reduceObject.uneffected.push(transaction);
+			}
 
-	public dequeueUntil(condition: (transaction: Transaction) => boolean): ReadonlyArray<Transaction> {
+			return reduceObject;
+		}, {uneffected: [], effected: []});
+		this._transactions = uneffected;
+
+		return effected;
 	}
 }
