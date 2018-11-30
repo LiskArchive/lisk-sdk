6900c3a0de8c19e485e2a3e6e8384b7d9a6d19b8
Lucas Silvestre <lukas.silvestre@gmail.com>
2018-11-30 19:08:55 +0100

    Add block filters properties comment

--- storage/entities/block.js
+++ storage/entities/block.js
@@ -24,6 +24,106 @@ const { blocks: { toEntity } } = require('../mappers');
 const ft = require('../utils/filter_types');
 const BaseEntity = require('./base_entity');
 
+/**
+ * Block Filters
+ * @typedef {Object} filters.Block
+ * @property {string} [id]
+ * @property {string} [id_eql]
+ * @property {string} [id_ne]
+ * @property {string} [id_in]
+ * @property {string} [id_like]
+ * @property {string} [height]
+ * @property {string} [height_eql]
+ * @property {string} [height_ne]
+ * @property {string} [height_gt]
+ * @property {string} [height_gte]
+ * @property {string} [height_lt]
+ * @property {string} [height_lte]
+ * @property {string} [height_in]
+ * @property {string} [blockSignature]
+ * @property {string} [blockSignature_eql]
+ * @property {string} [blockSignature_ne]
+ * @property {string} [blockSignature_in]
+ * @property {string} [blockSignature_like]
+ * @property {string} [generatorPublicKey]
+ * @property {string} [generatorPublicKey_eql]
+ * @property {string} [generatorPublicKey_ne]
+ * @property {string} [generatorPublicKey_in]
+ * @property {string} [generatorPublicKey_like]
+ * @property {string} [payloadHash]
+ * @property {string} [payloadHash_eql]
+ * @property {string} [payloadHash_ne]
+ * @property {string} [payloadHash_in]
+ * @property {string} [payloadHash_like]
+ * @property {string} [payloadLength]
+ * @property {string} [payloadLength_eql]
+ * @property {string} [payloadLength_ne]
+ * @property {string} [payloadLength_gt]
+ * @property {string} [payloadLength_gte]
+ * @property {string} [payloadLength_lt]
+ * @property {string} [payloadLength_lte]
+ * @property {string} [payloadLength_in]
+ * @property {string} [numberOfTransactions]
+ * @property {string} [numberOfTransactions_eql]
+ * @property {string} [numberOfTransactions_ne]
+ * @property {string} [numberOfTransactions_gt]
+ * @property {string} [numberOfTransactions_gte]
+ * @property {string} [numberOfTransactions_lt]
+ * @property {string} [numberOfTransactions_lte]
+ * @property {string} [numberOfTransactions_in]
+ * @property {string} [previousBlockId]
+ * @property {string} [previousBlockId_eql]
+ * @property {string} [previousBlockId_ne]
+ * @property {string} [previousBlockId_in]
+ * @property {string} [previousBlockId_like]
+ * @property {string} [timestamp]
+ * @property {string} [timestamp_eql]
+ * @property {string} [timestamp_ne]
+ * @property {string} [timestamp_gt]
+ * @property {string} [timestamp_gte]
+ * @property {string} [timestamp_lt]
+ * @property {string} [timestamp_lte]
+ * @property {string} [timestamp_in]
+ * @property {string} [totalAmount]
+ * @property {string} [totalAmount_eql]
+ * @property {string} [totalAmount_ne]
+ * @property {string} [totalAmount_gt]
+ * @property {string} [totalAmount_gte]
+ * @property {string} [totalAmount_lt]
+ * @property {string} [totalAmount_lte]
+ * @property {string} [totalAmount_in]
+ * @property {string} [totalFee]
+ * @property {string} [totalFee_eql]
+ * @property {string} [totalFee_ne]
+ * @property {string} [totalFee_gt]
+ * @property {string} [totalFee_gte]
+ * @property {string} [totalFee_lt]
+ * @property {string} [totalFee_lte]
+ * @property {string} [totalFee_in]
+ * @property {string} [reward]
+ * @property {string} [reward_eql]
+ * @property {string} [reward_ne]
+ * @property {string} [reward_gt]
+ * @property {string} [reward_gte]
+ * @property {string} [reward_lt]
+ * @property {string} [reward_lte]
+ * @property {string} [reward_in]
+ * @property {string} [version]
+ * @property {string} [version_eql]
+ * @property {string} [version_ne]
+ * @property {string} [version_gt]
+ * @property {string} [version_gte]
+ * @property {string} [version_lt]
+ * @property {string} [version_lte]
+ * @property {string} [version_in]
+ */
+
+/**
+ * @typedef {string} fieldSets.Block
+ * @enum
+ * @value 'FIELD_SET_SIMPLE'
+ */
+
 class Block extends BaseEntity {
 	constructor() {
 		super();
@@ -56,7 +156,6 @@ class Block extends BaseEntity {
 		this.addField('totalAmount', 'string', { filter: ft.NUMBER });
 		this.addField('totalFee', 'string', { filter: ft.NUMBER });
 		this.addField('reward', 'string', { filter: ft.NUMBER });
-		this.addField('transactions', 'array', {});
 		this.addField('version', 'number', { filter: ft.NUMBER });
 
 		this.SQLs = {
