1c7e33ad17e21b6bb0db5d530f829ea203a1df26
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2018-12-11 10:07:14 +0100

    Add new types related to peer RPC and node info

--- packages/lisk-p2p/src/p2p_types.ts
+++ packages/lisk-p2p/src/p2p_types.ts
@@ -13,28 +13,56 @@
  *
  */
 /* tslint:disable:no-empty-interface*/
-export interface P2PMessagePacket {}
 
-export interface P2PRequestPacket {}
+export interface P2PRequestPacket<T> {
+	readonly procedure: string;
+	readonly params?: T;
+}
 
-export interface P2PResponsePacket {}
+export interface P2PResponsePacket {
+	readonly data: unknown;
+}
 
-export interface P2PNodeStatus {}
+export interface P2PMessagePacket<T> {
+	readonly event: string;
+	readonly data: T;
+}
+
+export interface PeerInfo {
+	readonly ipAddress: string;
+	readonly wsPort: number;
+}
+
+export interface NodeInfo {
+	readonly wsPort: number;
+	readonly os: string;
+	readonly version: string;
+}
 
 export interface P2PPenality {}
 
 export interface P2PConfig {
-	readonly blacklistedPeers: ReadonlyArray<string>;
+	readonly blacklistedPeers: ReadonlyArray<PeerInfo>;
 	readonly connectTimeout: number;
 	readonly ipAddress?: string;
-	readonly seedPeers: ReadonlyArray<string>;
+	readonly seedPeers: ReadonlyArray<PeerInfo>;
 	readonly wsEngine?: string;
 	readonly wsPort: number;
+	readonly version: string;
 }
 
-export enum PeerState {
-	BANNED = 0,
-	DISCONNECTED = 1,
-	CONNECTED = 2,
+export interface RPCResponsePeerInfo {
+	readonly ip: string;
+	readonly wsPort: number;
+	readonly os: string;
+	readonly version: string;
+	readonly broadhash: string;
+	readonly height: number;
+	readonly nonce: string;
+}
+
+export interface RPCResponsePeerList {
+	readonly success: boolean;
+	readonly peers: ReadonlyArray<RPCResponsePeerInfo>;
 }
 export interface NetworkStatus {}
