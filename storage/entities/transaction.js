26b08fd3c002d39d0083f44a747c2d6da454c562
Nazar Hussain <nazar@lightcurve.io>
2018-12-20 15:27:37 +0100

    Add typedef for all transaction types

--- storage/entities/transaction.js
+++ storage/entities/transaction.js
@@ -16,6 +16,95 @@
 
 const BaseEntity = require('./base_entity');
 
+/**
+ * Basic Transaction
+ * @typedef {Object} BasicTransaction
+ * @property {string} id
+ * @property {string} blockId
+ * @property {Integer} type
+ * @property {Number} timestamp
+ * @property {string} senderPublicKey
+ * @property {string} requesterPublicKey
+ * @property {string} senderId
+ * @property {string} recipientId
+ * @property {string} amount
+ * @property {string} fee
+ * @property {string} signature
+ * @property {string} signSignature
+ * @property {Array.<string>} signatures
+ */
+
+/**
+ * Transfer Transaction
+ * @typedef {BasicTransaction} TransferTransaction
+ * @property {Object} asset
+ * @property {string} asset.data
+ */
+
+/**
+ * Second Passphrase Transaction
+ * @typedef {BasicTransaction} SecondPassphraseTransaction
+ * @property {Object} asset
+ * @property {Object} asset.signature
+ * @property {string} asset.signature.publicKey
+ */
+
+/**
+ * Delegate Transaction
+ * @typedef {BasicTransaction} DelegateTransaction
+ * @property {Object} asset
+ * @property {Object} asset.delegate
+ * @property {string} asset.delegate.username
+ */
+
+/**
+ * Vote Transaction
+ * @typedef {BasicTransaction} VoteTransaction
+ * @property {Object} asset
+ * @property {Array.<string>} asset.votes
+ */
+
+/**
+ * Multisig Registration Transaction
+ * @typedef {BasicTransaction} MultisigRegistrationTransaction
+ * @property {Object} asset
+ * @property {Object} asset.multisignature
+ * @property {Integer} asset.multisignature.min
+ * @property {Integer} asset.multisignature.lifetime
+ * @property {Array.<string>} asset.multisignature.keysgroup
+ */
+
+/**
+ * Dapp Registration Transaction
+ * @typedef {BasicTransaction} DappRegistrationTransaction
+ * @property {Object} asset
+ * @property {Object} asset.dapp
+ * @property {Integer} asset.dapp.type
+ * @property {string} asset.dapp.name
+ * @property {string} asset.dapp.description
+ * @property {string} asset.dapp.tags
+ * @property {text} asset.dapp.link
+ * @property {text} asset.dapp.icon
+ * @property {Integer} asset.dapp.category
+ */
+
+/**
+ * InTransfer Transaction
+ * @typedef {BasicTransaction} InTransferTransaction
+ * @property {Object} asset
+ * @property {Object} asset.inTransfer
+ * @property {string} asset.inTransfer.dappId
+ */
+
+/**
+ * OutTransfer Transaction
+ * @typedef {BasicTransaction} OutTransfer
+ * @property {Object} asset
+ * @property {Object} asset.outTransfer
+ * @property {string} asset.outTransfer.dappId
+ * @property {string} asset.outTransfer.transactionId
+ */
+
 class Transaction extends BaseEntity {}
 
 module.exports = Transaction;
