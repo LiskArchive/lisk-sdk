f5ad9feb11b89ba475e61ea50e860bc9b4dc2c27
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2019-01-08 11:51:37 +0100

    Convert PeerConfig to PeerInfo and add support for getting and sending P2PNodeInfo

--- packages/lisk-p2p/src/p2p.ts
+++ packages/lisk-p2p/src/p2p.ts
@@ -22,10 +22,10 @@ import { attach, SCServer, SCServerSocket } from 'socketcluster-server';
 import { Peer, PeerInfo } from './peer';
 
 import {
-	// TODO ASAP: NetworkStatus,
 	P2PConfig,
 	P2PMessagePacket,
-	P2PNodeStatus,
+	P2PNetworkStatus,
+	P2PNodeInfo,
 	P2PPenalty,
 	P2PRequestPacket,
 	P2PResponsePacket,
@@ -49,22 +49,25 @@ const BASE_10_RADIX = 10;
 
 export class P2P extends EventEmitter {
 	private readonly _config: P2PConfig;
-	private readonly _peerPool: PeerPool;
 	private readonly _httpServer: Server;
-	private readonly _scServer: SCServerUpdated; // Until we get comeplete definition for SCServer
+	private _isActive: boolean;
 	private readonly _newPeers: Set<PeerInfo>;
-	// TODO ASAP: private readonly _triedPeers: Set<PeerInfo>;
-	private _nodeStatus: P2PNodeStatus;
+
+	// TODO ASAP: private readonly _triedPeers: Set<PeerConfig>;
+	private _nodeInfo: P2PNodeInfo;
+	private readonly _peerPool: PeerPool;
+	private readonly _scServer: SCServerUpdated;
 
 	public constructor(config: P2PConfig) {
 		super();
 		this._config = config;
-		this._nodeStatus = {
+		this._nodeInfo = {
 			wsPort: config.wsPort,
 			os: platform(),
 			version: config.version,
 			height: 0,
 		};
+		this._isActive = false;
 		this._newPeers = new Set();
 		// TODO ASAP: this._triedPeers = new Set();
 
@@ -73,16 +76,43 @@ export class P2P extends EventEmitter {
 		this._scServer = attach(this._httpServer) as SCServerUpdated;
 	}
 
+	public get isActive(): boolean {
+		return this._isActive;
+	}
+
+	public get nodeInfo(): P2PNodeInfo {
+		return this._nodeInfo;
+	}
+
+	/**
+	 * This is not a declared as a setter because this method will need
+	 * invoke an async RPC on Peers to give them our new node status.
+	 */
+	public applyNodeInfo(nodeInfo: P2PNodeInfo): void {
+		this._nodeInfo = nodeInfo;
+		const peerList = this._peerPool.getAllPeers();
+		peerList.forEach(peer => {
+			peer.applyNodeInfo(nodeInfo);
+		});
+	}
+
 	/* tslint:disable:next-line: prefer-function-over-method */
 	public applyPenalty(penalty: P2PPenalty): void {
 		penalty;
 	}
 
-	// TODO ASAP: public getNetworkStatus(): NetworkStatus {};
+	public getNetworkStatus(): P2PNetworkStatus {
+		// TODO ASAP: Add additional properties.
+		return {
+			peers: this._peerPool.getAllPeerInfos(),
+		};
+	}
+
 	/* tslint:disable:next-line: prefer-function-over-method */
 	public async request<T>(
 		packet: P2PRequestPacket<T>,
 	): Promise<P2PResponsePacket> {
+		// TODO ASAP
 		return Promise.resolve({ data: packet });
 	}
 
@@ -90,19 +120,6 @@ export class P2P extends EventEmitter {
 	public send<T>(message: P2PMessagePacket<T>): void {
 		message;
 		// TODO ASAP
-	}
-
-	public get nodeStatus(): P2PNodeStatus {
-		return this._nodeStatus;
-	}
-
-	/**
-	 * This is not a declared as a setter because this method will need
-	 * invoke an async RPC on Peers to give them our new node status.
-	 */
-	public applyNodeStatus(value: P2PNodeStatus): void {
-		this._nodeStatus = value;
-		// TODO ASAP: Pass to PeerPool -> connected Peer objects
 	}
 
 	private async _startPeerServer(): Promise<void> {
@@ -126,17 +143,14 @@ export class P2P extends EventEmitter {
 					const peerId = Peer.constructPeerId(socket.remoteAddress, wsPort);
 					const existingPeer = this._peerPool.getPeer(peerId);
 					if (existingPeer === undefined) {
-						const peer = new Peer(
-							{
-								ipAddress: socket.remoteAddress,
-								os: queryObject.os,
-								version: queryObject.version,
-								wsPort,
-								nodeStatus: this._nodeStatus,
-								height: queryObject.height ? +queryObject.height : 0,
-							},
-							socket,
-						);
+						const peer = new Peer({
+							inboundSocket: socket,
+							ipAddress: socket.remoteAddress,
+							os: queryObject.os,
+							version: queryObject.version,
+							wsPort,
+						});
+						peer.applyNodeInfo(this._nodeInfo);
 						this._peerPool.addPeer(peer);
 						super.emit(EVENT_NEW_INBOUND_PEER, peer);
 						super.emit(EVENT_NEW_PEER, peer);
@@ -152,6 +166,7 @@ export class P2P extends EventEmitter {
 
 		return new Promise<void>(resolve => {
 			this._scServer.once('ready', () => {
+				this._isActive = true;
 				resolve();
 			});
 		});
@@ -161,48 +176,46 @@ export class P2P extends EventEmitter {
 		// TODO ASAP: Test this and check for potential failure scenarios.
 		return new Promise<void>(resolve => {
 			this._scServer.close(() => {
+				this._isActive = false;
 				resolve();
 			});
 		});
 	}
 
+	// TODO ASAP: This should be refactored to use the discovery function.
 	private async _loadListOfPeerListsFromSeeds(
 		seedList: ReadonlyArray<PeerInfo>,
 	): Promise<ReadonlyArray<ReadonlyArray<Peer>>> {
 		return Promise.all(
-			seedList.map(async (seedPeer: PeerInfo) => {
-				const peer = new Peer({
-					ipAddress: seedPeer.ipAddress,
-					wsPort: seedPeer.wsPort,
-					nodeStatus: this._nodeStatus,
-					height: seedPeer.height,
-					os: seedPeer.os,
-					version: seedPeer.version,
-				});
-				this._newPeers.add(peer.peerInfo);
+			seedList.map(async (seedPeerInfo: PeerInfo) => {
+				const seedPeer = new Peer(seedPeerInfo);
+				seedPeer.applyNodeInfo(this._nodeInfo);
+				this._newPeers.add(seedPeer.peerInfo);
 				/**
-				 * TODO LATER: For the LIP phase, we shouldn't add the seed peers to our
+				 * TODO later: For the LIP phase, we shouldn't add the seed peers to our
 				 * _peerPool and we should disconnect from them as soon as we've loaded their peer lists.
 				 */
-				this._peerPool.addPeer(peer);
-				// TODO LATER: For the LIP phase, the 'list' request will need to be renamed.
-				const seedNodePeerListResponse = await peer.request<void>({
+				this._peerPool.addPeer(seedPeer);
+				// TODO later: For the LIP phase, the 'list' request will need to be renamed.
+				const seedNodePeerListResponse = await seedPeer.request<void>({
 					procedure: 'list',
 				});
 				const peerListResponse = seedNodePeerListResponse.data as ProtocolPeerList;
+				
 				// TODO ASAP: Validate the response before returning. Check that seedNodePeerListResponse.data.peers exists.
 
-				return peerListResponse.peers.map(
-					(peerObject: ProtocolPeerInfo) =>
-						new Peer({
-							ipAddress: peerObject.ip,
-							wsPort: peerObject.wsPort, // TODO ASAP: Add more properties
-							nodeStatus: this._nodeStatus,
-							height: seedPeer.height,
-							os: seedPeer.os,
-							version: seedPeer.version,
-						}),
-				);
+				return peerListResponse.peers.map((peerInfo: ProtocolPeerInfo) => {
+					const peer = new Peer({
+						ipAddress: peerInfo.ip,
+						wsPort: peerInfo.wsPort, // TODO ASAP: Add more properties
+						height: peerInfo.height,
+						os: peerInfo.os,
+						version: peerInfo.version,
+					});
+					peer.applyNodeInfo(this._nodeInfo);
+
+					return peer;
+				});
 			}),
 		);
 	}
