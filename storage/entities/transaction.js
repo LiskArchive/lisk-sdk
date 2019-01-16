a923611512501718ff309d57aec16fb543c26c9a
Pablo Vicente <pvicente@gmail.com>
2019-01-16 14:21:59 +0100

    Remove redundant code from transaction entity

--- storage/entities/transaction.js
+++ storage/entities/transaction.js
@@ -379,16 +379,7 @@ class Transaction extends BaseEntity {
 				)
 			);
 
-			// Not all transactions of type 0 might have an `asset` so remove the ones without `asset` before storing related data
-			const needSubtransactions = transactions.filter(
-				transaction =>
-					transaction.type !== 0 ||
-					(transaction.type === 0 &&
-						transaction.asset &&
-						transaction.asset.data)
-			);
-
-			const groupedTransactions = _.groupBy(needSubtransactions, 'type');
+			const groupedTransactions = _.groupBy(transactions, 'type');
 
 			Object.keys(groupedTransactions).forEach(type => {
 				batch.push(
