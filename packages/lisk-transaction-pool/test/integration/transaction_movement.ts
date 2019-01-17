8a12827dec4c82fc3c0b51ba326d9f9667383427
Usman <usman@lightcurve.io>
2019-01-17 17:04:52 +0100

    add integration tests for lisk-transaction-pool

---
+++ packages/lisk-transaction-pool/test/integration/transaction_movement.ts
@@ -0,0 +1,183 @@
+import { TransactionPool, TransactionPoolConfiguration, Transaction } from '../../src/transaction_pool';
+import { dummyCheckFunctionGenerator, dummyCheckerFunctionGenerator,  wrapExpectationInNextTick} from './common';
+import { returnTrueUntilLimit } from '../../src/queue_checkers';
+import transactionObjects from '../../fixtures/transactions.json';
+import { wrapTransaction } from '../utils/add_transaction_functions';
+import { SinonFakeTimers } from 'sinon';
+import { expect } from 'chai';
+
+describe('transaction movement between queues', () => {
+    const transactions: ReadonlyArray<Transaction> = transactionObjects.map(wrapTransaction);
+    let transactionPool: TransactionPool;
+    let clock: SinonFakeTimers;
+
+    const configuration: TransactionPoolConfiguration = {
+        expireTransactionsInterval: 100,
+        maxTransactionsPerQueue: 1000,
+        receivedTransactionsLimitPerProcessing: 25,
+        receivedTransactionsProcessingInterval: 100,
+        validatedTransactionsLimitPerProcessing: 25,
+        validatedTransactionsProcessingInterval: 100,
+        verifiedTransactionsLimitPerProcessing: 25,
+        verifiedTransactionsProcessingInterval: 100,
+    };
+
+    const validateTransactionFunction = dummyCheckFunctionGenerator(['1']);
+    const verifyTransactionFunction = dummyCheckFunctionGenerator(['2']);
+    const processTransactionsFunction = dummyCheckFunctionGenerator(['3']);
+
+    const dependencies = {
+        processTransactions: dummyCheckerFunctionGenerator(processTransactionsFunction),
+        validateTransactions: dummyCheckerFunctionGenerator(validateTransactionFunction),
+        verifyTransactions: dummyCheckerFunctionGenerator(verifyTransactionFunction),
+    };
+
+    beforeEach(async () => {
+        clock = sandbox.useFakeTimers();
+        transactionPool = new TransactionPool({
+            ...configuration,
+            ...dependencies,
+        });
+    });
+
+    afterEach(async () => {
+        transactionPool.cleanup();
+    });
+
+    describe('validate received transactions', () => {
+        let transactionsToValidate: ReadonlyArray<Transaction>;
+        let validTransactions: ReadonlyArray<Transaction>;
+        let invalidTransactions: ReadonlyArray<Transaction>;
+
+        beforeEach(async () => {
+            transactions.forEach(transaction => {
+                transactionPool.addTransaction(transaction);
+            });
+            transactionsToValidate = transactionPool.queues.received.peekUntil(returnTrueUntilLimit(configuration.receivedTransactionsLimitPerProcessing));
+            const { passedTransactions, failedTransactions } = validateTransactionFunction(transactionsToValidate);
+            
+            validTransactions = passedTransactions;
+            invalidTransactions = failedTransactions;
+            clock.tick(configuration.receivedTransactionsProcessingInterval + 1);
+        });
+
+        it('should remove transactions from the received queue', async () => {
+            return wrapExpectationInNextTick(() => {
+                transactionsToValidate.forEach(transaction => {
+                    expect(transactionPool.queues.received.exists(transaction)).to.equal(false);
+                });
+            });
+        });
+
+        it('should move valid transactions to the validated queue', async () => {
+            return wrapExpectationInNextTick(() => {
+                validTransactions.forEach(transaction => {
+                    expect(transactionPool.queues.validated.exists(transaction)).to.equal(true);
+                });
+                expect(transactionPool.queues.validated.size()).to.equal(validTransactions.length);
+            });
+        });
+
+        it('should remove invalid transactions from the transaction pool', async () => {
+            return wrapExpectationInNextTick(() => {
+                invalidTransactions.forEach(transaction => {
+                    expect(transactionPool.existsInTransactionPool(transaction)).to.equal(false);
+                });
+            });
+        });
+
+        describe('from validated to the verified and pending queues', () => {
+            let transactionsToVerify: ReadonlyArray<Transaction>;
+            let verifiableTransactions: ReadonlyArray<Transaction>;
+            let unverifiableTransactions: ReadonlyArray<Transaction>;
+
+            beforeEach(async () => {
+                transactionsToVerify = validTransactions;
+                const { passedTransactions, failedTransactions } = verifyTransactionFunction(transactionsToVerify);
+
+                verifiableTransactions = passedTransactions;
+                unverifiableTransactions = failedTransactions;
+                clock.tick(configuration.validatedTransactionsProcessingInterval + 1);
+            });
+
+            it('should remove transactions from the validated queue', async () => {
+                return wrapExpectationInNextTick(() => {
+                    transactionsToVerify.forEach(transaction => {
+                        expect(transactionPool.queues.validated.exists(transaction)).to.equal(false);
+                    });
+                });
+            });
+
+            it('should move verifiable transactions to the verfied queue', async () => {
+                return wrapExpectationInNextTick(() => {
+                    verifiableTransactions.forEach(transaction => {
+                        expect(transactionPool.queues.verified.exists(transaction)).to.equal(true);
+                    });
+                    expect(transactionPool.queues.verified.size()).to.equal(verifiableTransactions.length);
+                });
+            });
+
+            it('should remove unverifiable transactions from the transaction pool', async () => {
+                return wrapExpectationInNextTick(() => {
+                    unverifiableTransactions.forEach(transaction => {
+                        expect(transactionPool.existsInTransactionPool(transaction)).to.equal(false);
+                    });
+                });
+            });
+
+            describe('from pending and verified to the ready queue', () => {
+                let transactionsToProcess: ReadonlyArray<Transaction>;
+                let processableTransactions: ReadonlyArray<Transaction>;
+                let unprocessableTransactions: ReadonlyArray<Transaction>;
+
+                beforeEach(async () => {
+                    transactionsToProcess = verifiableTransactions;
+                    const { passedTransactions, failedTransactions } = processTransactionsFunction(transactionsToProcess);
+
+                    processableTransactions = passedTransactions;
+                    unprocessableTransactions = failedTransactions;
+                    clock.tick(configuration.verifiedTransactionsProcessingInterval + 1);
+                });
+
+                it('should remove transactions from the verified queue', async () => {
+                    return wrapExpectationInNextTick(() => {
+                        transactionsToProcess.forEach(transaction => {
+                            expect(transactionPool.queues.verified.exists(transaction)).to.equal(false);
+                        });
+                    });
+                });
+
+                it('should move processable transactions to the ready queue', async () => {
+                    return wrapExpectationInNextTick(() => {
+                        processableTransactions.forEach(transaction => {
+                            expect(transactionPool.queues.ready.exists(transaction)).to.equal(true);
+                        });
+                        expect(transactionPool.queues.ready.size()).to.equal(processableTransactions.length);
+                    });
+                });
+
+                it('should remove unverifiable transactions from the transaction pool', async () => {
+                    return wrapExpectationInNextTick(() => {
+                        unprocessableTransactions.forEach(transaction => {
+                            expect(transactionPool.existsInTransactionPool(transaction)).to.equal(false);
+                        });
+                    });
+                });
+
+                it('should keep the transactions after running the job twice', async () => {
+                    let transactionsInReadyQueue: ReadonlyArray<Transaction>;
+                    await wrapExpectationInNextTick(() => {
+                        transactionsInReadyQueue = transactionPool.queues.ready.transactions;
+                        clock.tick(configuration.verifiedTransactionsProcessingInterval);
+                    });
+
+                    await wrapExpectationInNextTick(() => {
+                        transactionsInReadyQueue.forEach(transaction => {
+                            expect(transactionPool.queues.ready.exists(transaction)).to.equal(true);
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
