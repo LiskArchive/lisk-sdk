1a786675868225175bd77db05f2a91ebfb099cd3
ish@n <ishantiw.quasar@gmail.com>
2019-01-17 15:40:28 +0100

    :seedling: Add functions for drop inbound-outbound connections

--- packages/lisk-p2p/src/peer.ts
+++ packages/lisk-p2p/src/peer.ts
@@ -214,6 +214,19 @@ export class Peer extends EventEmitter {
 		}
 	}
 
+	public dropInboundConnection(code: number = 1000, reason?: string): void {
+		if (this._inboundSocket) {
+			this._inboundSocket.destroy(code, reason);
+			this._stopListeningForRPCs(this._inboundSocket);
+		}
+	}
+
+	public dropOutboundConnection(code: number = 1000, reason?: string): void {
+		if (this._outboundSocket) {
+			this._outboundSocket.destroy(code, reason);
+		}
+	}
+
 	public send<T>(packet: P2PMessagePacket<T>): void {
 		if (!this._outboundSocket) {
 			this._outboundSocket = this._createOutboundSocket();
