695a88c92ac56006e7eb9455f0728a6ac764e737
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2019-01-17 11:37:49 +0100

    Fix integration test setup

--- packages/lisk-p2p/test/integration/p2p.ts
+++ packages/lisk-p2p/test/integration/p2p.ts
@@ -1,11 +1,14 @@
 import { expect } from 'chai';
 import { P2P } from '../../src/index';
+import { wait } from '../utils/helpers';
 
-describe.skip('Integration tests for P2P library', () => {
+const NETWORK_START_PORT = 5000;
+
+describe('Integration tests for P2P library', () => {
 	const NETWORK_PEER_COUNT = 10;
 	let p2pNodeList: ReadonlyArray<P2P> = [];
 
-	describe('Disconnected network', () => {
+	describe.skip('Disconnected network', () => {
 		beforeEach(async () => {
 			p2pNodeList = [...Array(NETWORK_PEER_COUNT).keys()].map(index => {
 				return new P2P({
@@ -13,7 +16,7 @@ describe.skip('Integration tests for P2P library', () => {
 					connectTimeout: 5000,
 					seedPeers: [],
 					wsEngine: 'ws',
-					wsPort: 5000 + index,
+					wsPort: NETWORK_START_PORT + index,
 					version: '1.0.0',
 				});
 			});
@@ -41,15 +44,23 @@ describe.skip('Integration tests for P2P library', () => {
 		});
 	});
 
-	describe.skip('Fully connected network', () => {
+	describe('Fully connected network', () => {
 		beforeEach(async () => {
 			p2pNodeList = [...Array(NETWORK_PEER_COUNT).keys()].map(index => {
+				// Each node will have the next node in the sequence as a seed peer.
+				const seedPeers = [
+					{
+						ipAddress: '127.0.0.1',
+						wsPort: NETWORK_START_PORT + ((index + 1) % NETWORK_PEER_COUNT),
+						height: 0,
+					},
+				];
 				return new P2P({
 					blacklistedPeers: [],
 					connectTimeout: 5000,
-					seedPeers: [],
+					seedPeers,
 					wsEngine: 'ws',
-					wsPort: 5000 + index,
+					wsPort: NETWORK_START_PORT + index,
 					version: '1.0.0',
 				});
 			});
@@ -60,6 +71,7 @@ describe.skip('Integration tests for P2P library', () => {
 				},
 			);
 			await Promise.all(peerStartPromises);
+			await wait(500);
 		});
 
 		afterEach(async () => {
@@ -73,6 +85,9 @@ describe.skip('Integration tests for P2P library', () => {
 		describe('Seed peer discovery', () => {
 			it('should discover seed peers', () => {
 				// TODO: Check that nodes are running and connected to seed peers using p2p.getNetworkStatus()
+				p2pNodeList.forEach(p2p => {
+					let networkStatus = p2p.getNetworkStatus();
+				});
 			});
 		});
 	});
