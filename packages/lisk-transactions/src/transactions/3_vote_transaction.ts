406fbfef453c15b768aadcc8bf2f770fedc2c3bb
mitsujutsu <mitsuaki.uchimoto@gmail.com>
2019-01-23 14:34:49 +0100

    :recycle: Remove recipientPublicKey format check

--- packages/lisk-transactions/src/transactions/3_vote_transaction.ts
+++ packages/lisk-transactions/src/transactions/3_vote_transaction.ts
@@ -300,16 +300,6 @@ export class VoteTransaction extends BaseTransaction {
 			);
 		}
 
-		if (!this.recipientPublicKey) {
-			errors.push(
-				new TransactionError(
-					'RecipientPublicKey must be set for vote transaction',
-					this.id,
-					'.recipientPublicKey',
-				),
-			);
-		}
-
 		if (
 			this.recipientPublicKey &&
 			this.recipientId !== getAddressFromPublicKey(this.recipientPublicKey)
