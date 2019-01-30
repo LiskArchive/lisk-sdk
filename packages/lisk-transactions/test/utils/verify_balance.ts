cfc6f322d136ac2e8ae6c6515298c5305946ad84
shuse2 <shus.toda@gmail.com>
2019-01-30 21:16:26 +0100

    :recycle: Fix test import syntax

--- packages/lisk-transactions/test/utils/verify_balance.ts
+++ packages/lisk-transactions/test/utils/verify_balance.ts
@@ -12,7 +12,7 @@
  * Removal or modification of this copyright notice is prohibited.
  *
  */
-import BigNum from 'browserify-bignum';
+import * as BigNum from 'browserify-bignum';
 import { expect } from 'chai';
 import { verifyBalance } from '../../src/utils';
 import { TransactionError } from '../../src/errors';
