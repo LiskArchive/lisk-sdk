d4e14c6fc689f123e17027ed6a42e403562590c8
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2019-02-06 17:13:43 +0100

    Add error code and reason related to incompatible nethash

--- packages/lisk-p2p/src/disconnect_status_codes.ts
+++ packages/lisk-p2p/src/disconnect_status_codes.ts
@@ -3,3 +3,6 @@ export const INVALID_CONNECTION_URL_REASON = 'Peer did not provide a valid URL a
 
 export const INVALID_CONNECTION_QUERY_CODE = 4502;
 export const INVALID_CONNECTION_QUERY_REASON = 'Peer did not provide valid query parameters as part of the WebSocket connection';
+
+export const INCOMPATIBLE_NETWORK_CODE = 4102;
+export const INCOMPATIBLE_NETWORK_REASON = 'Peer nethash did not match our own';
