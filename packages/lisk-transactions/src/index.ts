2aa623a2220b94089cc25783029e8fc3169a5f4e
Pablo Vicente <pvicente@gmail.com>
2019-02-06 11:01:05 +0100

    :bug: Export all transaction types in index.ts

--- packages/lisk-transactions/src/index.ts
+++ packages/lisk-transactions/src/index.ts
@@ -13,7 +13,11 @@
  *
  */
 import { transfer } from './0_transfer';
+import { registerSecondPassphrase } from './1_register_second_passphrase';
+import { registerDelegate } from './2_register_delegate';
+import { castVotes } from './3_cast_votes';
 import { registerMultisignature } from './4_register_multisignature_account';
+import { createDapp } from './5_create_dapp';
 import * as constants from './constants';
 import { createSignatureObject } from './create_signature_object';
 import {
@@ -30,6 +34,8 @@ import * as utils from './utils';
 
 export {
 	BaseTransaction,
+	castVotes,
+	createDapp,
 	MultisignatureTransaction,
 	TransferTransaction,
 	SecondSignatureTransaction,
@@ -38,7 +44,9 @@ export {
 	InTransferTransaction,
 	OutTransferTransaction,
 	transfer,
+	registerDelegate,
 	registerMultisignature,
+	registerSecondPassphrase,
 	createSignatureObject,
 	utils,
 	constants,
