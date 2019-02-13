35ab7ffaff236434ea58968790feb38888271923
Nazar Hussain <nazar@lightcurve.io>
2019-02-13 17:32:31 +0100

    Add step to init logic classes

---
+++ framework/src/modules/chain/init_steps/init_logic_structs.js
@@ -0,0 +1,52 @@
+module.exports = async ({
+	config,
+	storage,
+	logger,
+	ed,
+	schema,
+	genesisBlock,
+}) => {
+	const Transaction = require('../logic/transaction.js');
+	const Block = require('../logic/block.js');
+	const Account = require('../logic/account.js');
+	const Peers = require('../logic/peers.js');
+
+	const accountLogic = await new Promise((resolve, reject) => {
+		new Account(storage, schema, logger, (err, object) => {
+			err ? reject(err) : resolve(object);
+		});
+	});
+
+	const transactionLogic = await new Promise((resolve, reject) => {
+		new Transaction(
+			storage,
+			ed,
+			schema,
+			genesisBlock,
+			accountLogic,
+			logger,
+			(err, object) => {
+				err ? reject(err) : resolve(object);
+			}
+		);
+	});
+
+	const blockLogic = await new Promise((resolve, reject) => {
+		new Block(ed, schema, transactionLogic, (err, object) => {
+			err ? reject(err) : resolve(object);
+		});
+	});
+
+	const peersLogic = await new Promise((resolve, reject) => {
+		new Peers(logger, config, (err, object) => {
+			err ? reject(err) : resolve(object);
+		});
+	});
+
+	return {
+		account: accountLogic,
+		transaction: transactionLogic,
+		block: blockLogic,
+		peers: peersLogic,
+	};
+};
