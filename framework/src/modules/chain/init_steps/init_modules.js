58d4dc1edc860e06c22c08a9540e3411994d2c0f
Nazar Hussain <nazar@lightcurve.io>
2019-02-13 17:32:16 +0100

    Add step to init modules

---
+++ framework/src/modules/chain/init_steps/init_modules.js
@@ -0,0 +1,57 @@
+const util = require('util');
+const async = require('async');
+
+const promisifyParallel = util.promisify(async.parallel);
+
+module.exports = async ({
+	modulesList,
+	config,
+	storage,
+	logger,
+	schema,
+	genesisBlock,
+	network,
+	webSocket,
+	bus,
+	sequence,
+	balanceSequence,
+	logic,
+}) => {
+	const modules = [];
+	const tasks = {};
+
+	Object.keys(modulesList).forEach(name => {
+		tasks[name] = function(configModulesCb) {
+			const domain = require('domain').create();
+
+			domain.on('error', err => {
+				logger.fatal(`Domain ${name}`, {
+					message: err.message,
+					stack: err.stack,
+				});
+			});
+
+			domain.run(() => {
+				logger.debug('Loading module', name);
+				// eslint-disable-next-line import/no-dynamic-require
+				const DynamicModule = require(`../${modulesList[name]}`);
+				const obj = new DynamicModule(configModulesCb, {
+					config,
+					storage,
+					logger,
+					schema,
+					genesisBlock,
+					network,
+					webSocket,
+					bus,
+					sequence,
+					balanceSequence,
+					logic,
+				});
+				modules.push(obj);
+			});
+		};
+	});
+
+	return promisifyParallel(tasks);
+};
