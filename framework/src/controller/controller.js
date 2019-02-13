0540e5131738b76fde382304bb070396fd8cebf0
michielmulders <michielmulders1@gmail.com>
2019-02-13 17:29:38 +0100

    Refactor controller and event class for JSDocs

--- framework/src/controller/controller.js
+++ framework/src/controller/controller.js
@@ -30,166 +30,160 @@ const validateModuleSpec = moduleSpec => {
  * @requires helpers/config
  * @requires channels/event_emitter
  * @requires module.Bus
+ * @param {Array} modules - All registered modules
+ * @param {Object} componentConfig - Configurations for components
+ * @param {component.Logger} logger - Logger component responsible for writing all logs to output
  */
-module.exports = class Controller {
-	/**
-	 * Create controller object.
-	 *
-	 * @param {Array} modules - All registered modules
-	 * @param {Object} componentConfig - Configurations for components
-	 * @param {component.Logger} logger - Logger component responsible for writing all logs to output
-	 */
-	constructor(modules, componentConfig, logger) {
-		this.logger = logger;
-		this.componentConfig = componentConfig;
-		this.logger.info('Initializing controller');
-		this.channel = null; // Channel for controller
-		this.channels = {}; // Keep track of all channels for modules
-		this.bus = null;
-		this.modules = modules;
-	}
+function Controller(modules, componentConfig, logger) {
+	this.logger = logger;
+	this.componentConfig = componentConfig;
+	this.logger.info('Initializing controller');
+	this.channel = null; // Channel for controller
+	this.channels = {}; // Keep track of all channels for modules
+	this.bus = null;
+	this.modules = modules;
+}
 
-	/**
-	 * Load the initial state and start listening for events or triggering actions.
-	 * Publishes 'lisk:ready' state on the bus.
-	 *
-	 * @async
-	 */
-	async load() {
-		this.logger.info('Loading controller');
-		await this._setupDirectories();
-		await this._setupBus();
-		await this._setupControllerActions();
-		await this._loadModules();
+/**
+ * Load the initial state and start listening for events or triggering actions.
+ * Publishes 'lisk:ready' state on the bus.
+ *
+ * @async
+ */
+Controller.prototype.load = async function() {
+	this.logger.info('Loading controller');
+	await this._setupDirectories();
+	await this._setupBus();
+	await this._setupControllerActions();
+	await this._loadModules();
 
-		this.logger.info('Bus listening to events', this.bus.getEvents());
-		this.logger.info('Bus ready for actions', this.bus.getActions());
+	this.logger.info('Bus listening to events', this.bus.getEvents());
+	this.logger.info('Bus ready for actions', this.bus.getActions());
 
-		this.channel.publish('lisk:ready', {});
-	}
+	this.channel.publish('lisk:ready', {});
+};
 
-	/**
-	 * Verify existence of required directories.
-	 *
-	 * @async
-	 */
-	// eslint-disable-next-line class-methods-use-this
-	async _setupDirectories() {
-		// Make sure all directories exists
-		fs.emptyDirSync(config.dirs.temp);
-		fs.ensureDirSync(config.dirs.sockets);
-		fs.ensureDirSync(config.dirs.pids);
-		fs.writeFileSync(`${config.dirs.pids}/controller.pid`, process.pid);
-	}
+/**
+ * Verify existence of required directories.
+ *
+ * @async
+ */
+// eslint-disable-next-line class-methods-use-this
+Controller.prototype._setupDirectories = async function() {
+	// Make sure all directories exists
+	fs.emptyDirSync(config.dirs.temp);
+	fs.ensureDirSync(config.dirs.sockets);
+	fs.ensureDirSync(config.dirs.pids);
+	fs.writeFileSync(`${config.dirs.pids}/controller.pid`, process.pid);
+};
 
-	/**
-	 * Initialize bus
-	 *
-	 * @async
-	 */
-	async _setupBus() {
-		this.bus = new Bus(this, {
-			wildcard: true,
-			delimiter: ':',
-			maxListeners: 1000,
-		});
-		await this.bus.setup();
+/**
+ * Initialize bus
+ *
+ * @async
+ */
+Controller.prototype._setupBus = async function() {
+	this.bus = new Bus(this, {
+		wildcard: true,
+		delimiter: ':',
+		maxListeners: 1000,
+	});
+	await this.bus.setup();
 
-		this.channel = new EventEmitterChannel(
-			'lisk',
-			['ready'],
-			['getComponentConfig'],
-			this.bus,
-			{ skipInternalEvents: true }
-		);
-		await this.channel.registerToBus();
+	this.channel = new EventEmitterChannel(
+		'lisk',
+		['ready'],
+		['getComponentConfig'],
+		this.bus,
+		{ skipInternalEvents: true }
+	);
+	await this.channel.registerToBus();
 
-		// If log level is greater than info
-		if (this.logger.level && this.logger.level() < 30) {
-			this.bus.onAny((name, event) => {
-				this.logger.debug(
-					`MONITOR: ${event.source} -> ${event.module}:${event.name}`,
-					event.data
-				);
-			});
-		}
-	}
-
-	/**
-	 * Setup Controller actions.
-	 * Create action for getting component config.
-	 *
-	 * @async
-	 */
-	async _setupControllerActions() {
-		this.channel.action(
-			'getComponentConfig',
-			action => this.componentConfig[action.params]
-		);
-	}
-
-	async _loadModules() {
-		return Promise.each(Object.keys(this.modules), m =>
-			this._loadInMemoryModule(m)
-		);
-	}
-
-	async _loadInMemoryModule(alias) {
-		const module = this.modules[alias].spec;
-		const moduleConfig = this.modules[alias].config;
-		validateModuleSpec(module);
-
-		this.logger.info(
-			`Loading module with alias: ${module.alias}(${module.info.name}:${
-				module.info.version
-			})`
-		);
-
-		const channel = new EventEmitterChannel(
-			module.alias,
-			module.events,
-			Object.keys(module.actions),
-			this.bus,
-			{}
-		);
-		this.channels[alias] = channel;
-
-		await channel.registerToBus();
-
-		Object.keys(module.actions).forEach(action => {
-			channel.action(action, module.actions[action]);
-		});
-
-		channel.publish(`${module.alias}:registeredToBus`);
-		channel.publish(`${module.alias}:loading:started`);
-		await module.load(channel, moduleConfig);
-		channel.publish(`${module.alias}:loading:finished`);
-		this.logger.info(
-			`Module ready with alias: ${module.alias}(${module.info.name}:${
-				module.info.version
-			})`
-		);
-	}
-
-	async unloadModules(modules = null) {
-		return Promise.mapSeries(modules || Object.keys(this.modules), async m => {
-			await this.modules[m].spec.unload();
-			delete this.modules[m];
+	// If log level is greater than info
+	if (this.logger.level && this.logger.level() < 30) {
+		this.bus.onAny((name, event) => {
+			this.logger.debug(
+				`MONITOR: ${event.source} -> ${event.module}:${event.name}`,
+				event.data
+			);
 		});
 	}
+};
 
-	async cleanup(code, reason) {
-		this.logger.info('Cleanup controller...');
+/**
+ * Setup Controller actions.
+ * Create action for getting component config.
+ *
+ * @async
+ */
+Controller.prototype._setupControllerActions = async function() {
+	this.channel.action(
+		'getComponentConfig',
+		action => this.componentConfig[action.params]
+	);
+};
 
-		if (reason) {
-			this.logger.error(reason);
-		}
+Controller.prototype._loadModules = async function() {
+	return Promise.each(Object.keys(this.modules), m =>
+		this._loadInMemoryModule(m)
+	);
+};
 
-		try {
-			await this.unloadModules();
-			this.logger.info('Unload completed');
-		} catch (error) {
-			this.logger.error('Caused error during cleanup', error);
-		}
+Controller.prototype._loadInMemoryModule = async function(alias) {
+	const module = this.modules[alias].spec;
+	const moduleConfig = this.modules[alias].config;
+	validateModuleSpec(module);
+
+	this.logger.info(
+		`Loading module with alias: ${module.alias}(${module.info.name}:${
+			module.info.version
+		})`
+	);
+
+	const channel = new EventEmitterChannel(
+		module.alias,
+		module.events,
+		Object.keys(module.actions),
+		this.bus,
+		{}
+	);
+	this.channels[alias] = channel;
+
+	await channel.registerToBus();
+
+	Object.keys(module.actions).forEach(action => {
+		channel.action(action, module.actions[action]);
+	});
+
+	channel.publish(`${module.alias}:registeredToBus`);
+	channel.publish(`${module.alias}:loading:started`);
+	await module.load(channel, moduleConfig);
+	channel.publish(`${module.alias}:loading:finished`);
+	this.logger.info(
+		`Module ready with alias: ${module.alias}(${module.info.name}:${
+			module.info.version
+		})`
+	);
+};
+
+Controller.prototype.unloadModules = async function(modules = null) {
+	return Promise.mapSeries(modules || Object.keys(this.modules), async m => {
+		await this.modules[m].spec.unload();
+		delete this.modules[m];
+	});
+};
+
+Controller.prototype.cleanup = async function(code, reason) {
+	this.logger.info('Cleanup controller...');
+
+	if (reason) {
+		this.logger.error(reason);
+	}
+
+	try {
+		await this.unloadModules();
+		this.logger.info('Unload completed');
+	} catch (error) {
+		this.logger.error('Caused error during cleanup', error);
 	}
 };
