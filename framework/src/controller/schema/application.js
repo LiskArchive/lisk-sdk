53e270fa4acab91a1ebf015e34ae7c360f98957f
michielmulders <michielmulders1@gmail.com>
2019-02-14 16:55:52 +0100

    Add schema genesis block

--- framework/src/controller/schema/application.js
+++ framework/src/controller/schema/application.js
@@ -6,8 +6,122 @@ module.exports = {
 	},
 
 	genesisBlock: {
-		id: '#genesisBlock',
+		$id: '#genesisBlock',
 		type: 'object',
+		required: ['xxx'],
+		properties: {
+			version: {
+				type: 'integer',
+			},
+			totalAmount: {
+				type: 'integer',
+			},
+			totalFee: {
+				type: 'integer',
+			},
+			reward: {
+				type: 'integer',
+			},
+			payloadHash: {
+				type: 'string',
+				minLength: 64,
+				maxLength: 64,
+			},
+			timestamp: {
+				type: 'integer',
+			},
+			numberOfTransactions: {
+				type: 'integer',
+			},
+			payloadLength: {
+				type: 'integer',
+			},
+			previousBlock: {
+				type: 'null',
+			},
+			generatorPublicKey: {
+				type: 'string',
+				pattern: '^[0-9a-z]{64}$',
+			},
+			transactions: {
+				type: 'array',
+				items: {
+					type: 'object',
+					$ref: '#/transactions',
+				},
+				uniqueItems: true,
+			},
+			height: {
+				type: 'integer',
+			},
+			blockSignature: {
+				type: 'string',
+				pattern: '^[0-9a-z]{128}$',
+			},
+			id: {
+				type: 'string',
+				pattern: '^[0-9]+$',
+			},
+		},
+		additionalProperties: false,
+	},
+
+	transactions: {
+		id: '#/transactions',
+		type: 'object',
+		required: [
+			'type',
+			'amount',
+			'fee',
+			'timestamp',
+			'recipientId',
+			'senderId',
+			'senderPublicKey',
+			'signature',
+			'id',
+		],
+		properties: {
+			type: {
+				type: 'integer',
+				minimum: 0,
+				maximum: 7,
+			},
+			amount: {
+				type: 'integer',
+			},
+			fee: {
+				type: 'integer',
+			},
+			timestamp: {
+				type: 'integer',
+			},
+			recipientId: {
+				type: ['string', 'null'],
+				pattern: '^[0-9]+L$',
+			},
+			senderId: {
+				type: 'string',
+				pattern: '^[0-9]+L$',
+			},
+			senderPublicKey: {
+				type: 'string',
+				pattern: '^[0-9a-z]{64}$',
+			},
+			asset: {
+				type: 'object',
+				description:
+					'Send relevant data with transaction like delegate, vote, signature, ...',
+			},
+			signature: {
+				type: 'string',
+				pattern: '^[0-9a-z]{128}$',
+			},
+			id: {
+				type: 'string',
+				pattern: '^[0-9]+$',
+			},
+		},
+		additionalProperties: false,
 	},
 
 	logger: {
