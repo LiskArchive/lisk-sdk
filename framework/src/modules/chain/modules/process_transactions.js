02e101fe7de67ed299311f2ae8a5ea7074c28e9e
Usman <usman@lightcurve.io>
2019-02-15 15:00:50 +0100

    add process_transactions module

---
+++ framework/src/modules/chain/modules/process_transactions.js
@@ -0,0 +1,57 @@
+/*
+ * Copyright © 2018 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+'use strict';
+
+const initTransaction = require('../helpers/init_transaction.js');
+
+let library;
+
+class ProcessTransactions {
+	constructor(cb, scope) {
+		library = {
+			logic: {
+				stateManager: scope.logic.stateManager,
+			},
+		};
+
+		setImmediate(cb, null, this);
+	}
+
+	// eslint-disable-next-line class-methods-use-this
+	async verifyTransactions(transactions) {
+		// Get data required for verifying transactions
+		const stateStore = library.logic.stateManager.createStore({
+			mutate: false,
+		});
+		const transactionInstances = transactions
+			.map(transaction => ({
+				...transaction,
+				fee: transaction.fee.toString(),
+				amount: transaction.amount.toString(),
+			}))
+			.map(initTransaction);
+
+		await Promise.all(transactionInstances.map(t => t.prepare(stateStore)));
+
+		return transactionInstances.map(transaction => {
+			library.logic.stateManager.createSnapshot();
+			const transactionResponse = transaction.apply(stateStore);
+			library.logic.stateManager.restoreSnapshot();
+			return transactionResponse;
+		});
+	}
+}
+
+module.exports = ProcessTransactions;
---
+++ framework/test/integration/process_transactions.js
@@ -0,0 +1,122 @@
+/*
+ * Copyright © 2018 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+'use strict';
+
+const liskTransactions = require('@liskhq/lisk-transactions');
+const accountFixtures = require('../fixtures/accounts');
+const application = require('../common/application');
+const random = require('../common/utils/random');
+const Bignum = require('../../src/modules/chain/helpers/bignum.js');
+
+const genesisBlock = __testContext.config.genesisBlock;
+const { NORMALIZER } = global.constants;
+const transactionStatus = liskTransactions.Status;
+
+describe('blocks/verifyTransactions', async () => {
+	let library;
+	let verifyTransactions;
+	const account = random.account();
+
+	const verifiableTransactions = [
+		liskTransactions.transfer({
+			amount: new Bignum(NORMALIZER).multipliedBy(1000),
+			recipientId: account.address,
+			passphrase: accountFixtures.genesis.passphrase,
+		}),
+		liskTransactions.registerDelegate({
+			passphrase: accountFixtures.genesis.passphrase,
+			username: account.username,
+		}),
+		liskTransactions.registerSecondPassphrase({
+			passphrase: accountFixtures.genesis.passphrase,
+			secondPassphrase: account.secondPassphrase,
+		}),
+		liskTransactions.castVotes({
+			passphrase: accountFixtures.genesis.passphrase,
+			votes: [`${accountFixtures.existingDelegate.publicKey}`],
+		}),
+		liskTransactions.createDapp({
+			passphrase: accountFixtures.genesis.passphrase,
+			options: random.application(),
+		}),
+	].map(transaction => ({
+		...transaction,
+		senderId: accountFixtures.genesis.address,
+	}));
+
+	const nonVerifiableTransactions = [
+		liskTransactions.transfer({
+			amount: new Bignum(NORMALIZER).multipliedBy(1000),
+			recipientId: accountFixtures.genesis.address,
+			passphrase: account.passphrase,
+		}),
+	].map(transaction => ({ ...transaction, senderId: account.address }));
+
+	const keysgroup = new Array(4).fill(0).map(() => random.account().publicKey);
+
+	const pendingTransactions = [
+		liskTransactions.registerMultisignature({
+			passphrase: accountFixtures.genesis.passphrase,
+			keysgroup,
+			lifetime: 10,
+			minimum: 2,
+		}),
+	].map(transaction => ({
+		...transaction,
+		senderId: accountFixtures.genesis.address,
+	}));
+
+	before(done => {
+		application.init(
+			{
+				sandbox: {
+					name: 'lisk_test_blocks_verify_transactions',
+				},
+			},
+			(err, scope) => {
+				library = scope;
+				verifyTransactions =
+					scope.modules.processTransactions.verifyTransactions;
+				library.modules.blocks.lastBlock.set(genesisBlock);
+				done();
+			}
+		);
+	});
+
+	it('should return transactionResponses with status OK for verified transactions', async () => {
+		const transactionResponses = await verifyTransactions(
+			verifiableTransactions
+		);
+		transactionResponses.forEach(transactionResponse => {
+			expect(transactionResponse.status).to.equal(transactionStatus.OK);
+		});
+	});
+
+	it('should return transactionResponse with status FAIL for unverifiable transaction', async () => {
+		const transactionResponses = await verifyTransactions(
+			nonVerifiableTransactions
+		);
+		transactionResponses.forEach(transactionResponse => {
+			expect(transactionResponse.status).to.equal(transactionStatus.FAIL);
+		});
+	});
+
+	it('should return transactionResponse with status PENDING for transactions waiting multi-signatures', async () => {
+		const transactionResponses = await verifyTransactions(pendingTransactions);
+		transactionResponses.forEach(transactionResponse => {
+			expect(transactionResponse.status).to.equal(transactionStatus.PENDING);
+		});
+	});
+});
