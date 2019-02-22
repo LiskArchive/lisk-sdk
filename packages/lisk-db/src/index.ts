8dfe788b1e6572920e77d969d0572a23759b27b7
shuse2 <shus.toda@gmail.com>
2019-02-22 08:12:51 +0100

    Add simple DB

--- packages/lisk-db/src/index.ts
+++ packages/lisk-db/src/index.ts
@@ -1,7 +1,26 @@
+import { AbstractBatch } from 'abstract-leveldown';
 import levelup, { LevelUp } from 'levelup';
 import RocksDB from 'rocksdb';
 
 const delimitor = ':';
+export type Value = string | Buffer;
+export interface BatchCommand {
+	readonly type: 'put' | 'del';
+	readonly bucket: string;
+	readonly key: string | number;
+	readonly value?: Value;
+}
+
+export interface ReadStreamOption {
+	readonly gt?: Value;
+	readonly gte?: Value;
+	readonly lt?: Value;
+	readonly lte?: Value;
+	readonly reverse?: boolean;
+	readonly limit?: number;
+	readonly keys?: boolean;
+	readonly values?: boolean;
+}
 
 export class DB {
 	private _db: LevelUp<RocksDB>;
@@ -10,13 +29,13 @@ export class DB {
 		this._db = levelup(RocksDB(file));
 	}
 
-	public async get(bucket: string, key: string): Promise<RocksDB.Bytes> {
+	public async get(bucket: string, key: string): Promise<Value> {
 		const fullKey = `${bucket}${delimitor}${key}`;
 
 		return this._db.get(fullKey);
 	}
 
-	public async put(bucket: string, key: string, val: any): Promise<void> {
+	public async put(bucket: string, key: string, val: Value): Promise<void> {
 		const fullKey = `${bucket}${delimitor}${key}`;
 
 		return this._db.put(fullKey, val);
@@ -26,5 +45,19 @@ export class DB {
 		const fullKey = `${bucket}${delimitor}${key}`;
 
 		return this._db.del(fullKey);
+	}
+
+	public createReadStream(options?: ReadStreamOption): NodeJS.ReadableStream {
+		return this._db.createReadStream(options);
+	}
+
+	public async batch(tasks: ReadonlyArray<BatchCommand>): Promise<void> {
+		const execTasks = tasks.map(t => ({
+			type: t.type,
+			key: `${t.bucket}${typeof t.key === 'string' ? t.key : t.key.toString()}`,
+			value: t.value,
+		}))
+
+		return this._db.batch(execTasks as AbstractBatch[]);
 	}
 }
