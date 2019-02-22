5eb5fd37eb98a8efe9ffab1b78ce6c204814d01d
yatki <mehmet@yatki.com>
2019-02-22 12:08:38 +0100

    Add example test files

---
+++ framework/test/jest/specs/functional/example.spec.js
@@ -0,0 +1,5 @@
+describe('A simple test', () => {
+	it('should pass', () => {
+		expect(2 + 4).toBe(6);
+	});
+});
---
+++ framework/test/jest/specs/unit/another.example.spec.js
@@ -0,0 +1,5 @@
+describe('Another simple test', () => {
+	it('should pass', () => {
+		expect(2 + 4).toBe(6);
+	});
+});
