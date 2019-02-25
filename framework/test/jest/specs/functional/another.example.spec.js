afaf5dee5366ef9fc0917b69f9695eb915329177
yatki <mehmet@yatki.com>
2019-02-25 14:23:03 +0100

    Remove example tests

--- framework/test/jest/specs/functional/another.example.spec.js
+++
@@ -1,5 +0,0 @@
-describe('Another simple test', () => {
-	it('should pass', () => {
-		expect(2 + 4).toBe(6);
-	});
-});
--- framework/test/jest/specs/functional/example.spec.js
+++
@@ -1,5 +0,0 @@
-describe('A simple test', () => {
-	it('should pass', () => {
-		expect(2 + 4).toBe(6);
-	});
-});
