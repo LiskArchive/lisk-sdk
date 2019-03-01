c1058755789781705792e57522b1cbb498617840
Nazar Hussain <nazar@lightcurve.io>
2019-03-01 10:42:36 +0100

    Add formatters env variables and args

--- framework/src/controller/helpers/validator/keywords/formatters.js
+++ framework/src/controller/helpers/validator/keywords/formatters.js
@@ -1,3 +1,27 @@
 module.exports = {
-	format1: () => {},
+	stringToIpPortSet: value => {
+		if (typeof value === 'string') {
+			return value.split(',').map(peer => {
+				const [ip, wsPort] = peer.split(':');
+				return {
+					ip,
+					wsPort: wsPort || 5000,
+				};
+			});
+		}
+		return [];
+	},
+
+	stringToDelegateList: value => {
+		if (typeof value === 'string') {
+			return value.split(',').map(delegate => {
+				const [publicKey, encryptedPassphrase] = delegate.split('|');
+				return {
+					publicKey,
+					encryptedPassphrase,
+				};
+			});
+		}
+		return [];
+	},
 };
