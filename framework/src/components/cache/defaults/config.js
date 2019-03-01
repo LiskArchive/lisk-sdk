f2f17d1cdde2e3cb9bb44b53384c32c7b6dc733a
Nazar Hussain <nazar@lightcurve.io>
2019-03-01 10:40:39 +0100

    Add env, arg support for cache component

--- framework/src/components/cache/defaults/config.js
+++ framework/src/components/cache/defaults/config.js
@@ -4,27 +4,33 @@ const defaultConfig = {
 		enabled: {
 			type: 'boolean',
 			default: false,
+			env: 'LISK_CACHE_ENABLED',
 		},
 		host: {
 			type: 'string',
 			format: 'ipOrFQDN',
 			default: '127.0.0.1',
+			env: 'LISK_REDIS_HOST',
+			arg: '-r,--redis',
 		},
 		port: {
 			type: 'integer',
 			minimum: 1,
 			maximum: 65535,
 			default: 6380,
+			env: 'LISK_REDIS_PORT',
 		},
 		db: {
 			type: 'integer',
 			minimum: 0,
 			maximum: 15,
 			default: 0,
+			env: 'LISK_REDIS_DB_NAME',
 		},
 		password: {
 			type: ['string', 'null'],
 			default: null,
+			env: 'LISK_REDIS_DB_PASSWORD',
 		},
 	},
 	required: ['enabled', 'host', 'port', 'db', 'password'],
