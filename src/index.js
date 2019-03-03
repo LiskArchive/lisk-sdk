32a1d7b0ce3c6082fdb871b83764991e34784dba
Nazar Hussain <nazar@lightcurve.io>
2019-03-03 02:05:52 +0100

    Add support for network and custom configuration

--- src/index.js
+++ src/index.js
@@ -1,32 +1,64 @@
+const fs = require('fs');
 const path = require('path');
-const { Application } = require('../framework/src');
-
-// TODO: Remove the use this config helper
 const packageJSON = require('../package');
-let config = require('../config/devnet/config');
-const constants = require('../config/devnet/constants');
-const exceptions = require('../config/devnet/exceptions');
-const genesisBlock = require('../config/devnet/genesis_block');
+const { Application, helpers: { validator } } = require('../framework/src');
 
-// TODO: WIll be resolved with issue https://github.com/LiskHQ/lisk/issues/2976
-const args = process.argv.slice(2);
-const index = args.indexOf('-c');
-if (index >= 0) {
-	// eslint-disable-next-line import/no-dynamic-require
-	config = require(path.resolve(args[index + 1]));
-}
+const packageInfo = {
+	version: packageJSON.version,
+	minVersion: packageJSON.lisk.minVersion,
+	protocolVersion: packageJSON.lisk.protocolVersion,
+};
 
-const appName = () => `devnet-${config.modules.http_api.httpPort}`;
+const appSchema = {
+	type: 'object',
+	properties: {
+		NETWORK: {
+			type: 'string',
+			description:
+				'lisk network [devnet|betanet|mainnet|testnet]. Defaults to "devnet"',
+			default: 'devnet',
+			enum: ['devnet', 'alphanet', 'betanet', 'testnet', 'mainnet'],
+			env: 'LISK_NETWORK',
+			arg: '-n,--network',
+		},
+		CUSTOM_CONFIG_FILE: {
+			type: ['string', 'null'],
+			description: 'Custom configuration file path',
+			default: null,
+			env: 'LISK_CONFIG_FILE',
+			arg: '-c,--config',
+		},
+	},
+};
 
 try {
+	const { NETWORK, CUSTOM_CONFIG_FILE } = validator.validateWithDefaults(
+		appSchema,
+		{}
+	);
+
+	/* eslint-disable import/no-dynamic-require */
+	let customConfig = {};
+	const networkConfig = require(`../config/${NETWORK}/config`);
+	const constants = require(`../config/${NETWORK}/constants`);
+	const exceptions = require(`../config/${NETWORK}/exceptions`);
+	const genesisBlock = require(`../config/${NETWORK}/genesis_block`);
+
+	if (CUSTOM_CONFIG_FILE) {
+		customConfig = JSON.parse(
+			fs.readFileSync(path.resolve(CUSTOM_CONFIG_FILE), 'utf8')
+		);
+	}
+
 	// To run multiple applications for same network for integration tests
-	// TODO: Refactored the way to find unique name for the app
-	const app = new Application(appName, genesisBlock, constants, {
-		...config,
-		version: packageJSON.version,
-		minVersion: packageJSON.lisk.minVersion,
-		protocolVersion: packageJSON.lisk.protocolVersion,
-	});
+	const appName = config =>
+		`lisk-${NETWORK}-${config.modules.http_api.httpPort}`;
+
+	const app = new Application(appName, genesisBlock, constants, [
+		networkConfig,
+		customConfig,
+		packageInfo,
+	]);
 
 	app.overrideModuleOptions('chain', { exceptions });
 
