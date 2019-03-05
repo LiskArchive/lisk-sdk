de42d1c0ea7c0d49e48f6ca83c0bf94c0c16dd05
Daniel Limia <limiaspasdaniel@gmail.com>
2019-03-05 17:20:50 +0100

    Add test for bootstrap_storage.js on http api module

--- framework/test/mocha/unit/modules/http_api/init_steps/bootstrap_storage.js
+++ framework/test/mocha/unit/modules/http_api/init_steps/bootstrap_storage.js
@@ -1,9 +1,48 @@
 /* eslint-disable mocha/no-pending-tests */
+const bootstrapStorage = require('../../../../../../src/modules/http_api/init_steps/bootstrap_storage');
 
 describe('init_steps/bootstrap_storage', () => {
-	it('should bootstrap the storage');
-	it('should extend account entity limit');
-	it('should apply all migrations');
-	it('should apply runtime sql');
-	it('should log error if there is any');
+	let argument;
+	const accountLimit = 3;
+
+	beforeEach(async () => {
+		argument = {
+			components: {
+				storage: {
+					bootstrap: sinonSandbox.stub().resolves(true),
+					entities: {
+						Account: {
+							extendDefaultOptions: sinonSandbox.stub(),
+						},
+					},
+				},
+				logger: {
+					error: sinonSandbox.stub(),
+				},
+			},
+		};
+		return bootstrapStorage(argument, accountLimit);
+	});
+
+	it('should bootstrap the storage', async () => {
+		expect(argument.components.storage.bootstrap).to.be.called;
+	});
+	it('should extend account entity limit', async () => {
+		expect(
+			argument.components.storage.entities.Account.extendDefaultOptions
+		).to.be.calledWith({
+			limit: accountLimit,
+		});
+	});
+	it('should log error if there is any', async () => {
+		const error = new Error('error');
+		argument.components.storage.bootstrap.rejects(error);
+		try {
+			await bootstrapStorage(argument, accountLimit);
+		} catch (err) {
+			expect(err).to.have.property('message');
+			expect(err.message).to.equal(error.message);
+		}
+		expect(argument.components.logger.error).to.be.calledWith(error);
+	});
 });
