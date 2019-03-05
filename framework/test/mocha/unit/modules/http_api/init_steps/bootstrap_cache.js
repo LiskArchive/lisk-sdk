c0bc1e22c7f9b547b8a1e641683db7cbff9d7009
Daniel Limia <limiaspasdaniel@gmail.com>
2019-03-05 16:44:55 +0100

    Add test for bootstrap_cache.js on http_api module

--- framework/test/mocha/unit/modules/http_api/init_steps/bootstrap_cache.js
+++ framework/test/mocha/unit/modules/http_api/init_steps/bootstrap_cache.js
@@ -1,7 +1,39 @@
-/* eslint-disable mocha/no-pending-tests */
+const _ = require('lodash');
+const bootstrapCache = require('../../../../../../src/modules/http_api/init_steps/bootstrap_cache');
 
 describe('init_steps/bootstrap_cache', () => {
-	it('should bootstrap the cache if it is enabled');
-	it('should resolved without error if cache id disabled');
-	it('should log message if cache is disabled');
+	const argument = {
+		components: {
+			cache: {
+				options: {
+					enabled: true,
+				},
+				bootstrap: {},
+			},
+			logger: {},
+		},
+	};
+
+	beforeEach(async () => {
+		argument.components.cache.bootstrap = sinonSandbox.stub();
+		argument.components.logger.debug = sinonSandbox.stub();
+		return bootstrapCache(argument);
+	});
+
+	afterEach(async () => {
+		sinonSandbox.restore();
+	});
+
+	it('should bootstrap the cache if it is enabled', async () => {
+		expect(argument.components.cache.bootstrap).to.be.called;
+	});
+
+	it('should log message if cache is disabled', async () => {
+		const cacheDisabled = _.cloneDeep(argument);
+		cacheDisabled.components.cache.options.enabled = false;
+		await bootstrapCache(cacheDisabled);
+		expect(argument.components.logger.debug).to.be.calledWithExactly(
+			'Cache not enabled'
+		);
+	});
 });
