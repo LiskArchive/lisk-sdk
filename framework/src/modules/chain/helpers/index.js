148bcefbf1b40304d4a970d04d42bcbea113d0a0
Diego Garcia <diego@lightcurve.io>
2019-03-11 16:43:58 +0100

    Clean chain helpers index

--- framework/src/modules/chain/helpers/index.js
+++ framework/src/modules/chain/helpers/index.js
@@ -1,6 +1,5 @@
 /**
  * @namespace helpers
- * @property {module:helpers/cache} Cache
  * @property {module:helpers/config} Config
  * @property {module:helpers/diff} Diff
  * @property {module:helpers/ed} Ed
@@ -8,6 +7,5 @@
  * @property {module:helpers/jobs_queue} Jobs_queue
  * @property {module:helpers/slots} Slots
  * @property {module:helpers/sort_by} Sort_by
- * @property {module:helpers/ws_api} WS_API
  * @property {module:helpers/z_schema} Z_schema
  */
