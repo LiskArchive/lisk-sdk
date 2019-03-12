089845ec29fdaee808de910cd09e959525b447f6
Manu Nelamane Siddalingegowda <manu@lightcurve.io>
2019-03-12 09:30:56 +0100

    :seedling: Implement node list command

---
+++ src/commands/node/list.ts
@@ -0,0 +1,58 @@
+/*
+ * LiskHQ/lisk-commander
+ * Copyright © 2017–2018 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import Listr from 'listr';
+import BaseCommand from '../../base';
+import { listApplication, Pm2Env } from '../../utils/node/pm2';
+
+export default class ListCommand extends BaseCommand {
+	static description = 'List Lisk Core';
+
+	static examples = ['node:list'];
+
+	static flags = {
+		...BaseCommand.flags,
+	};
+
+	async run(): Promise<void> {
+		this.parse(ListCommand);
+
+		const tasks = new Listr([
+			{
+				title: 'Lisk Core Instances',
+				task: async () => {
+					const apps = await listApplication();
+					this.print(apps.map(app => {
+						const { name, pm2_env } = app;
+						const {
+							status,
+							pm_uptime,
+							unstable_restarts,
+						} = pm2_env as Pm2Env;
+
+						return {
+							name,
+							status,
+							uptime: new Date(pm_uptime).toISOString(),
+							restart_count: unstable_restarts,
+						};
+					}));
+				},
+			},
+		]);
+
+		await tasks.run();
+	}
+}
