177abc7290fcf5d3478c5993ee1dc9e4b970923b
Maciej Baj <macie.baj@gmail.com>
2019-03-12 15:45:48 +0100

    Add documentation for env and arg validation keywords
    Signed-off-by: Maciej Baj <macie.baj@gmail.com>

--- framework/src/controller/helpers/validator/keywords/index.js
+++ framework/src/controller/helpers/validator/keywords/index.js
@@ -1,3 +1,11 @@
+/**
+ * Adds a possibility to validate the app parameters for Lisk Framework provided through:
+ * 1. Environment variables (env) like "npm start LISK_NETWORK=test"
+ * 2. Command line arguments (arg) like "npm start --network test"
+ * These keywords extend Ajv Validator and follow the format:
+ * @type {{compile, errors, modifying, valid, metaSchema}}
+ */
+
 const env = require('./env');
 const arg = require('./arg');
 
