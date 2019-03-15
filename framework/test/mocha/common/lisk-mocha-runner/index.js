b70959fd659c1c347cad67bf70cc4813ad62930d
yatki <mehmet@yatki.com>
2019-03-15 13:21:35 +0100

    Add Licence headers to lisk mocha runner

--- framework/test/mocha/common/lisk-mocha-runner/index.js
+++ framework/test/mocha/common/lisk-mocha-runner/index.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2018 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+'use strict';
+
 /* eslint-disable no-console */
 
 const { cpus } = require('os');
@@ -89,6 +105,8 @@ const processNext = () => {
 
 const executeTests = async (testType, testPathPattern, mochaCliOptions) => {
 	const allTestFiles = getTestFiles(testType, testPathPattern);
+	console.log(`${allTestFiles.length} test files were found`);
+
 	state.numberOfTestFiles = allTestFiles.length;
 	state.mochaCliOptions = mochaCliOptions;
 	state.queue = [...allTestFiles];
