e92bdee77604b751cc678e76024fb0e751d2f5e5
Usman <usman@lightcurve.io>
2019-03-19 10:26:16 +0100

    add round store to state store on initialization

--- framework/src/modules/chain/logic/state_store/index.js
+++ framework/src/modules/chain/logic/state_store/index.js
@@ -1,11 +1,13 @@
 const AccountStore = require('./account_store');
 const TransactionStore = require('./transaction_store');
+const RoundStore = require('./round_store');
 
 class StateStoreManager {
 	constructor(storage, cb) {
 		this.entities = {
 			Account: storage.entities.Account,
 			Transaction: storage.entities.Transaction,
+			Round: storage.entities.Round,
 		};
 
 		this.stateStore = {};
@@ -16,6 +18,7 @@ class StateStoreManager {
 	createStore(options) {
 		this.stateStore = {
 			account: new AccountStore(this.entities.Account, options),
+			round: new RoundStore(this.entities.Round, options),
 			transaction: new TransactionStore(this.entities.Transaction, options),
 		};
 		return this.stateStore;
