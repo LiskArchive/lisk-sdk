38e529a64c46345723aea8b20c5cd5283ead17b5
michielmulders <michielmulders1@gmail.com>
2019-03-21 15:13:21 +0100

    Reuse rootPath in config.js

--- framework/src/modules/chain/helpers/config.js
+++ framework/src/modules/chain/helpers/config.js
@@ -89,11 +89,11 @@ function Config(packageJson, parseCommandLineOptions = true) {
 
 	const genesisBlock = loadJSONFile(`config/${network}/genesis_block.json`);
 
-	const defaultConstants = require('../../../../../config/default/constants');
-	const networkConstants = require(`../../../../../config/${network}/constants.js`); // eslint-disable-line import/no-dynamic-require
+	const defaultConstants = require(`${rootPath}/config/default/constants`); // eslint-disable-line import/no-dynamic-require
+	const networkConstants = require(`${rootPath}/config/${network}/constants.js`); // eslint-disable-line import/no-dynamic-require
 
-	const defaultExceptions = require('../../../../../config/default/exceptions');
-	const networkExceptions = require(`../../../../../config/${network}/exceptions.js`); // eslint-disable-line import/no-dynamic-require
+	const defaultExceptions = require(`${rootPath}/config/default/exceptions`); // eslint-disable-line import/no-dynamic-require
+	const networkExceptions = require(`${rootPath}/config/${network}/exceptions.js`); // eslint-disable-line import/no-dynamic-require
 
 	const defaultConfig = loadJSONFile('config/default/config.json');
 	const networkConfig = loadJSONFile(`config/${network}/config.json`);
