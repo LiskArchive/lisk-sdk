LiskArchive/lisk-elements

update transactions to use new store

packages/lisk-transactions/src/1_second_signature_transaction.ts
@@ -15,12 +15,12 @@
 import { hash, hexToBuffer, signData } from '@liskhq/lisk-cryptography';
 import {
 	BaseTransaction,
+	ENTITY_ACCOUNT,
 	StateStore,
-	StateStorePrepare,
 } from './base_transaction';
 import { SIGNATURE_FEE } from './constants';
 import { TransactionError, TransactionMultiError } from './errors';
-import { TransactionJSON } from './transaction_types';
+import { Account, TransactionJSON } from './transaction_types';
 import { getId, validator } from './utils';
 
 const TRANSACTION_SIGNATURE_TYPE = 1;
@@ -102,14 +102,6 @@ export class SecondSignatureTransaction extends BaseTransaction {
 		};
 	}
 
-	public async prepare(store: StateStorePrepare): Promise<void> {
-		await store.account.cache([
-			{
-				address: this.senderId,
-			},
-		]);
-	}
-
 	protected verifyAgainstTransactions(
 		transactions: ReadonlyArray<TransactionJSON>,
 	): ReadonlyArray<TransactionError> {
@@ -184,9 +176,11 @@ export class SecondSignatureTransaction extends BaseTransaction {
 		return errors;
 	}
 
-	protected applyAsset(store: StateStore): ReadonlyArray<TransactionError> {
+	protected async applyAsset(
+		store: StateStore,
+	): Promise<ReadonlyArray<TransactionError>> {
 		const errors: TransactionError[] = [];
-		const sender = store.account.get(this.senderId);
+		const sender = await store.get<Account>(ENTITY_ACCOUNT, this.senderId);
 		// Check if secondPublicKey already exists on account
 		if (sender.secondPublicKey) {
 			errors.push(
@@ -202,21 +196,17 @@ export class SecondSignatureTransaction extends BaseTransaction {
 			secondPublicKey: this.asset.signature.publicKey,
 			secondSignature: 1,
 		};
-		store.account.set(updatedSender.address, updatedSender);
+		await store.set(ENTITY_ACCOUNT, updatedSender.address, updatedSender);
 
 		return errors;
 	}
 
-	protected undoAsset(store: StateStore): ReadonlyArray<TransactionError> {
-		const sender = store.account.get(this.senderId);
-		const strippedSender = {
-			...sender,
-			// tslint:disable-next-line no-null-keyword - Exception for compatibility with Core 1.4
-			secondPublicKey: null,
-			secondSignature: 0,
-		};
-
-		store.account.set(strippedSender.address, strippedSender);
+	protected async undoAsset(
+		store: StateStore,
+	): Promise<ReadonlyArray<TransactionError>> {
+		const sender = await store.get<Account>(ENTITY_ACCOUNT, this.senderId);
+		const { secondPublicKey, ...strippedSender } = sender;
+		await store.set(ENTITY_ACCOUNT, strippedSender.address, strippedSender);
 
 		return [];
 	}

packages/lisk-transactions/src/2_delegate_transaction.ts
@@ -14,15 +14,16 @@
  */
 import {
 	BaseTransaction,
+	ENTITY_ACCOUNT,
 	StateStore,
-	StateStorePrepare,
 } from './base_transaction';
 import { DELEGATE_FEE } from './constants';
 import { TransactionError, TransactionMultiError } from './errors';
 import { Account, TransactionJSON } from './transaction_types';
-import { CreateBaseTransactionInput, validator } from './utils';
+import { validator } from './utils';
 
 const TRANSACTION_DELEGATE_TYPE = 2;
+const ENTITY_ACCOUNT_USERNAME = 'username:accountId';
 
 export interface DelegateAsset {
 	readonly delegate: {
@@ -65,13 +66,6 @@ export const delegateAssetFormatSchema = {
 	},
 };
 
-export interface CreateDelegateRegistrationInput {
-	readonly username: string;
-}
-
-export type RegisterDelegateInput = CreateBaseTransactionInput &
-	CreateDelegateRegistrationInput;
-
 export class DelegateTransaction extends BaseTransaction {
 	public readonly asset: DelegateAsset;
 	public readonly containsUniqueData: boolean;
@@ -110,17 +104,6 @@ export class DelegateTransaction extends BaseTransaction {
 		return {
 			...this.asset,
 		};
-	}
-
-	public async prepare(store: StateStorePrepare): Promise<void> {
-		await store.account.cache([
-			{
-				address: this.senderId,
-			},
-			{
-				username: this.asset.delegate.username,
-			},
-		]);
 	}
 
 	protected verifyAgainstTransactions(
@@ -197,11 +180,14 @@ export class DelegateTransaction extends BaseTransaction {
 		return errors;
 	}
 
-	protected applyAsset(store: StateStore): ReadonlyArray<TransactionError> {
+	protected async applyAsset(
+		store: StateStore,
+	): Promise<ReadonlyArray<TransactionError>> {
 		const errors: TransactionError[] = [];
-		const sender = store.account.get(this.senderId);
-		const usernameExists = store.account.find(
-			(account: Account) => account.username === this.asset.delegate.username,
+		const sender = await store.get<Account>(ENTITY_ACCOUNT, this.senderId);
+		const usernameExists = await store.exists(
+			ENTITY_ACCOUNT_USERNAME,
+			this.asset.delegate.username,
 		);
 
 		if (usernameExists) {
@@ -227,15 +213,23 @@ export class DelegateTransaction extends BaseTransaction {
 			isDelegate: true,
 			username: this.asset.delegate.username,
 		};
-		store.account.set(updatedSender.address, updatedSender);
+		await store.set(ENTITY_ACCOUNT, updatedSender.address, updatedSender);
+		await store.set(
+			ENTITY_ACCOUNT_USERNAME,
+			this.asset.delegate.username,
+			updatedSender.address,
+		);
 
 		return errors;
 	}
 
-	protected undoAsset(store: StateStore): ReadonlyArray<TransactionError> {
-		const sender = store.account.get(this.senderId);
+	protected async undoAsset(
+		store: StateStore,
+	): Promise<ReadonlyArray<TransactionError>> {
+		const sender = await store.get<Account>(ENTITY_ACCOUNT, this.senderId);
 		const { username, ...strippedSender } = sender;
-		store.account.set(strippedSender.address, strippedSender);
+		await store.set(ENTITY_ACCOUNT, strippedSender.address, strippedSender);
+		await store.unset(ENTITY_ACCOUNT_USERNAME, this.asset.delegate.username);
 
 		return [];
 	}

packages/lisk-transactions/src/4_multisignature_transaction.ts
@@ -16,9 +16,9 @@ import { hexToBuffer } from '@liskhq/lisk-cryptography';
 import * as BigNum from 'browserify-bignum';
 import {
 	BaseTransaction,
+	ENTITY_ACCOUNT,
 	MultisignatureStatus,
 	StateStore,
-	StateStorePrepare,
 } from './base_transaction';
 import { MULTISIGNATURE_FEE } from './constants';
 import {
@@ -27,7 +27,7 @@ import {
 	TransactionPendingError,
 } from './errors';
 import { createResponse, Status, TransactionResponse } from './response';
-import { TransactionJSON } from './transaction_types';
+import { Account, TransactionJSON } from './transaction_types';
 import { validateMultisignatures, validator } from './utils';
 
 const TRANSACTION_MULTISIGNATURE_TYPE = 4;
@@ -144,14 +144,6 @@ export class MultisignatureTransaction extends BaseTransaction {
 		};
 	}
 
-	public async prepare(store: StateStorePrepare): Promise<void> {
-		await store.account.cache([
-			{
-				address: this.senderId,
-			},
-		]);
-	}
-
 	protected verifyAgainstTransactions(
 		transactions: ReadonlyArray<TransactionJSON>,
 	): ReadonlyArray<TransactionError> {
@@ -242,7 +234,9 @@ export class MultisignatureTransaction extends BaseTransaction {
 		return errors;
 	}
 
-	public processMultisignatures(_: StateStore): TransactionResponse {
+	public async processMultisignatures(
+		_: StateStore,
+	): Promise<TransactionResponse> {
 		const transactionBytes = this.signSignature
 			? Buffer.concat([this.getBasicBytes(), hexToBuffer(this.signature)])
 			: this.getBasicBytes();
@@ -281,9 +275,11 @@ export class MultisignatureTransaction extends BaseTransaction {
 		return createResponse(this.id, errors);
 	}
 
-	protected applyAsset(store: StateStore): ReadonlyArray<TransactionError> {
+	protected async applyAsset(
+		store: StateStore,
+	): Promise<ReadonlyArray<TransactionError>> {
 		const errors: TransactionError[] = [];
-		const sender = store.account.get(this.senderId);
+		const sender = await store.get<Account>(ENTITY_ACCOUNT, this.senderId);
 
 		// Check if multisignatures already exists on account
 		if (sender.membersPublicKeys && sender.membersPublicKeys.length > 0) {
@@ -315,13 +311,15 @@ export class MultisignatureTransaction extends BaseTransaction {
 			multiMin: this.asset.multisignature.min,
 			multiLifetime: this.asset.multisignature.lifetime,
 		};
-		store.account.set(updatedSender.address, updatedSender);
+		await store.set(ENTITY_ACCOUNT, updatedSender.address, updatedSender);
 
 		return errors;
 	}
 
-	protected undoAsset(store: StateStore): ReadonlyArray<TransactionError> {
-		const sender = store.account.get(this.senderId);
+	protected async undoAsset(
+		store: StateStore,
+	): Promise<ReadonlyArray<TransactionError>> {
+		const sender = await store.get<Account>(ENTITY_ACCOUNT, this.senderId);
 
 		const {
 			membersPublicKeys,
@@ -330,7 +328,7 @@ export class MultisignatureTransaction extends BaseTransaction {
 			...strippedSender
 		} = sender;
 
-		store.account.set(strippedSender.address, strippedSender);
+		await store.set(ENTITY_ACCOUNT, strippedSender.address, strippedSender);
 
 		return [];
 	}

LiskArchive/lisk-sdk

packages/lisk-transactions/src/1_second_signature_transaction.ts
@@ -15,12 +15,12 @@
 import { hash, hexToBuffer, signData } from '@liskhq/lisk-cryptography';
 import {
 	BaseTransaction,
+	ENTITY_ACCOUNT,
 	StateStore,
-	StateStorePrepare,
 } from './base_transaction';
 import { SIGNATURE_FEE } from './constants';
 import { convertToTransactionError, TransactionError } from './errors';
-import { TransactionJSON } from './transaction_types';
+import { Account, TransactionJSON } from './transaction_types';
 import { getId, validator } from './utils';
 
 const TRANSACTION_SIGNATURE_TYPE = 1;
@@ -71,14 +71,6 @@ export class SecondSignatureTransaction extends BaseTransaction {
 		return {
 			...this.asset,
 		};
-	}
-
-	public async prepare(store: StateStorePrepare): Promise<void> {
-		await store.account.cache([
-			{
-				address: this.senderId,
-			},
-		]);
 	}
 
 	protected verifyAgainstTransactions(
@@ -169,9 +161,11 @@ export class SecondSignatureTransaction extends BaseTransaction {
 		return errors;
 	}
 
-	protected applyAsset(store: StateStore): ReadonlyArray<TransactionError> {
+	protected async applyAsset(
+		store: StateStore,
+	): Promise<ReadonlyArray<TransactionError>> {
 		const errors: TransactionError[] = [];
-		const sender = store.account.get(this.senderId);
+		const sender = await store.get<Account>(ENTITY_ACCOUNT, this.senderId);
 		// Check if secondPublicKey already exists on account
 		if (sender.secondPublicKey) {
 			errors.push(
@@ -187,21 +181,20 @@ export class SecondSignatureTransaction extends BaseTransaction {
 			secondPublicKey: this.asset.signature.publicKey,
 			secondSignature: 1,
 		};
-		store.account.set(updatedSender.address, updatedSender);
+		await store.set(ENTITY_ACCOUNT, updatedSender.address, updatedSender);
 
 		return errors;
 	}
 
-	protected undoAsset(store: StateStore): ReadonlyArray<TransactionError> {
-		const sender = store.account.get(this.senderId);
+	protected async undoAsset(store: StateStore): Promise<ReadonlyArray<TransactionError>> {
+		const sender = await store.get<Account>(ENTITY_ACCOUNT, this.senderId);
 		const resetSender = {
 			...sender,
 			// tslint:disable-next-line no-null-keyword - Exception for compatibility with Core 1.4
 			secondPublicKey: null,
 			secondSignature: 0,
 		};
-
-		store.account.set(resetSender.address, resetSender);
+		await store.set(ENTITY_ACCOUNT, resetSender.address, resetSender);
 
 		return [];
 	}

packages/lisk-transactions/src/2_delegate_transaction.ts
@@ -14,8 +14,8 @@
  */
 import {
 	BaseTransaction,
+	ENTITY_ACCOUNT,
 	StateStore,
-	StateStorePrepare,
 } from './base_transaction';
 import { DELEGATE_FEE } from './constants';
 import { convertToTransactionError, TransactionError } from './errors';
@@ -23,6 +23,7 @@ import { Account, TransactionJSON } from './transaction_types';
 import { validator } from './utils';
 
 const TRANSACTION_DELEGATE_TYPE = 2;
+const ENTITY_ACCOUNT_USERNAME = 'username:accountId';
 
 export interface DelegateAsset {
 	readonly delegate: {
@@ -74,17 +75,6 @@ export class DelegateTransaction extends BaseTransaction {
 		return {
 			...this.asset,
 		};
-	}
-
-	public async prepare(store: StateStorePrepare): Promise<void> {
-		await store.account.cache([
-			{
-				address: this.senderId,
-			},
-			{
-				username: this.asset.delegate.username,
-			},
-		]);
 	}
 
 	protected verifyAgainstTransactions(
@@ -172,11 +162,14 @@ export class DelegateTransaction extends BaseTransaction {
 		return errors;
 	}
 
-	protected applyAsset(store: StateStore): ReadonlyArray<TransactionError> {
+	protected async applyAsset(
+		store: StateStore,
+	): Promise<ReadonlyArray<TransactionError>> {
 		const errors: TransactionError[] = [];
-		const sender = store.account.get(this.senderId);
-		const usernameExists = store.account.find(
-			(account: Account) => account.username === this.asset.delegate.username,
+		const sender = await store.get<Account>(ENTITY_ACCOUNT, this.senderId);
+		const usernameExists = await store.exists(
+			ENTITY_ACCOUNT_USERNAME,
+			this.asset.delegate.username,
 		);
 
 		if (usernameExists) {
@@ -203,14 +196,20 @@ export class DelegateTransaction extends BaseTransaction {
 			vote: 0,
 			isDelegate: 1,
 		};
-		store.account.set(updatedSender.address, updatedSender);
+		await store.set(ENTITY_ACCOUNT, updatedSender.address, updatedSender);
+		await store.set(
+			ENTITY_ACCOUNT_USERNAME,
+			this.asset.delegate.username,
+			updatedSender.address,
+		);
 
 		return errors;
 	}
 
-	protected undoAsset(store: StateStore): ReadonlyArray<TransactionError> {
-		const sender = store.account.get(this.senderId);
-		const { username, ...strippedSender } = sender;
+	protected async undoAsset(
+		store: StateStore,
+	): Promise<ReadonlyArray<TransactionError>> {
+		const sender = await store.get<Account>(ENTITY_ACCOUNT, this.senderId);
 		const resetSender = {
 			...sender,
 			// tslint:disable-next-line no-null-keyword - Exception for compatibility with Core 1.4
@@ -218,7 +217,8 @@ export class DelegateTransaction extends BaseTransaction {
 			vote: 0,
 			isDelegate: 0,
 		};
-		store.account.set(strippedSender.address, resetSender);
+		await store.set(ENTITY_ACCOUNT, resetSender.address, resetSender);
+		await store.unset(ENTITY_ACCOUNT_USERNAME, this.asset.delegate.username);
 
 		return [];
 	}

packages/lisk-transactions/src/4_multisignature_transaction.ts
@@ -15,9 +15,9 @@
 import * as BigNum from '@liskhq/bignum';
 import {
 	BaseTransaction,
+	ENTITY_ACCOUNT,
 	MultisignatureStatus,
 	StateStore,
-	StateStorePrepare,
 } from './base_transaction';
 import { MULTISIGNATURE_FEE } from './constants';
 import { SignatureObject } from './create_signature_object';
@@ -27,7 +27,7 @@ import {
 	TransactionPendingError,
 } from './errors';
 import { createResponse, Status, TransactionResponse } from './response';
-import { TransactionJSON } from './transaction_types';
+import { Account, TransactionJSON } from './transaction_types';
 import { validateMultisignatures, validateSignature, validator } from './utils';
 
 const TRANSACTION_MULTISIGNATURE_TYPE = 4;
@@ -104,14 +104,6 @@ export class MultisignatureTransaction extends BaseTransaction {
 				keysgroup: [...this.asset.multisignature.keysgroup],
 			},
 		};
-	}
-
-	public async prepare(store: StateStorePrepare): Promise<void> {
-		await store.account.cache([
-			{
-				address: this.senderId,
-			},
-		]);
 	}
 
 	protected verifyAgainstTransactions(
@@ -217,7 +209,9 @@ export class MultisignatureTransaction extends BaseTransaction {
 		return errors;
 	}
 
-	public processMultisignatures(_: StateStore): TransactionResponse {
+	public async processMultisignatures(
+		_: StateStore,
+	): Promise<TransactionResponse> {
 		const transactionBytes = this.getBasicBytes();
 
 		const { valid, errors } = validateMultisignatures(
@@ -254,9 +248,11 @@ export class MultisignatureTransaction extends BaseTransaction {
 		return createResponse(this.id, errors);
 	}
 
-	protected applyAsset(store: StateStore): ReadonlyArray<TransactionError> {
+	protected async applyAsset(
+		store: StateStore,
+	): Promise<ReadonlyArray<TransactionError>> {
 		const errors: TransactionError[] = [];
-		const sender = store.account.get(this.senderId);
+		const sender = await store.get<Account>(ENTITY_ACCOUNT, this.senderId);
 
 		// Check if multisignatures already exists on account
 		if (sender.membersPublicKeys && sender.membersPublicKeys.length > 0) {
@@ -288,13 +284,15 @@ export class MultisignatureTransaction extends BaseTransaction {
 			multiMin: this.asset.multisignature.min,
 			multiLifetime: this.asset.multisignature.lifetime,
 		};
-		store.account.set(updatedSender.address, updatedSender);
+		await store.set(ENTITY_ACCOUNT, updatedSender.address, updatedSender);
 
 		return errors;
 	}
 
-	protected undoAsset(store: StateStore): ReadonlyArray<TransactionError> {
-		const sender = store.account.get(this.senderId);
+	protected async undoAsset(
+		store: StateStore,
+	): Promise<ReadonlyArray<TransactionError>> {
+		const sender = await store.get<Account>(ENTITY_ACCOUNT, this.senderId);
 
 		const resetSender = {
 			...sender,
@@ -303,15 +301,15 @@ export class MultisignatureTransaction extends BaseTransaction {
 			multiLifetime: 0,
 		};
 
-		store.account.set(resetSender.address, resetSender);
+		await store.set(ENTITY_ACCOUNT, resetSender.address, resetSender);
 
 		return [];
 	}
 
-	public addMultisignature(
+	public async addMultisignature(
 		store: StateStore,
 		signatureObject: SignatureObject,
-	): TransactionResponse {
+	): Promise<TransactionResponse> {
 		// Validate signature key belongs to pending multisig registration transaction
 		const keysgroup = this.asset.multisignature.keysgroup.map((aKey: string) =>
 			aKey.slice(1),