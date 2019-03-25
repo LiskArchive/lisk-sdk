2ee678038bcf39c37006dd659fca350e44069ad9
Shusetsu Toda <shusetsu@lightcurve.io>
2019-03-25 18:54:41 +0100

    :bug: Accept first error from redis

--- framework/src/components/cache/cache.js
+++ framework/src/components/cache/cache.js
@@ -41,17 +41,16 @@ class Cache {
 		// TODO: implement retry_strategy
 		// TOFIX: app crashes with FTL error when launchin app with CACHE_ENABLE=true
 		// but cache server is not available.
-		const self = this;
-		return new Promise((resolve, reject) => {
-			self.client = redis.createClient(self.options);
-			self.client.once('error', err => {
+		return new Promise(resolve => {
+			this.client = redis.createClient(this.options);
+			this.client.once('error', err => {
 				// Called if the "error" event occured before "ready" event
-				self.logger.warn('App was unable to connect to Cache server', err);
-				return reject(err);
+				this.logger.warn('App was unable to connect to Cache server', err);
+				resolve();
 			});
-			self.client.once('ready', () => {
-				self._onReady();
-				return resolve();
+			this.client.once('ready', () => {
+				this._onReady();
+				resolve();
 			});
 		});
 	}
