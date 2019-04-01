27051e42d21df5a2ac61a44df62ebfa3f2b3b64a
Nazar Hussain <nazar@lightcurve.io>
2019-04-01 13:53:44 +0200

    Disable a test file which going to be replaced in coming PR

--- framework/test/mocha/unit/modules/chain/helpers/config.js
+++ framework/test/mocha/unit/modules/chain/helpers/config.js
@@ -14,7 +14,10 @@
 
 'use strict';
 
-const config = require('../../../../../../src/modules/chain/helpers/config');
+/* eslint-disable */
+
+// This file is not used any more will be replaced completely with https://github.com/LiskHQ/lisk/issues/2976
+// const config = require('../../../../../../src/modules/chain/helpers/config');
 const {
 	ZSchema,
 } = require('../../../../../../src/controller/helpers/validator');
