1dd941cce6bd7a9cf7fbc3073236f2aa8949d362
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2019-04-01 22:22:03 +0200

    Fix network module compatibility issues with v1 lisk-core network

--- framework/src/modules/network/network.js
+++ framework/src/modules/network/network.js
@@ -12,7 +12,10 @@ const {
 	EVENT_REQUEST_RECEIVED,
 	EVENT_MESSAGE_RECEIVED,
 } = require('@liskhq/lisk-p2p');
+const randomstring = require('randomstring');
 const { createLoggerComponent } = require('../../components/logger');
+
+const hasNamespaceReg = /:/;
 
 /**
  * Network Module
@@ -37,20 +40,32 @@ module.exports = class Network {
 
 		this.logger = createLoggerComponent(loggerConfig);
 
-		const initialNodeInfo = await this.channel.invoke('chain:getNodeInfo');
+		// TODO: Nonce overwrite should be removed once the Network module has been fully integreated into core and the old peer system has been fully removed.
+		// We need this because the old peer system which runs in parallel will conflict with the new one if they share the same nonce.
+		const moduleNonce = randomstring.generate(16);
+		const sanitizeNodeInfo = nodeInfo => (
+			{
+				...nodeInfo,
+				state: 2, // TODO: Delete state property
+				nonce: moduleNonce,
+				wsPort: this.options.nodeInfo.wsPort,
+			}
+		);
+
+		const initialNodeInfo = sanitizeNodeInfo(
+			await this.channel.invoke('chain:getNodeInfo')
+		);
 
 		const p2pConfig = {
 			...this.options,
-			nodeInfo: {
-				...initialNodeInfo,
-				wsPort: this.options.nodeInfo.wsPort,
-			},
+			nodeInfo: initialNodeInfo,
 		};
 
 		this.p2p = new P2P(p2pConfig);
 
 		this._handleUpdateNodeInfo = event => {
-			this.p2p.applyNodeInfo(event.data);
+			const newNodeInfo = sanitizeNodeInfo(event.data);
+			this.p2p.applyNodeInfo(newNodeInfo);
 		};
 
 		this.channel.subscribe(
@@ -69,28 +84,35 @@ module.exports = class Network {
 				}`
 			);
 		});
+
 		this.p2p.on(EVENT_CONNECT_OUTBOUND, peerInfo => {
 			this.logger.info(
 				`Connected to peer ${peerInfo.ipAddress}:${peerInfo.wsPort}`
 			);
 		});
+
 		this.p2p.on(EVENT_DISCOVERED_PEER, peerInfo => {
 			this.logger.info(
 				`Discovered peer ${peerInfo.ipAddress}:${peerInfo.wsPort}`
 			);
 		});
+
 		this.p2p.on(EVENT_FAILED_TO_FETCH_PEER_INFO, error => {
-			this.logger.debug(error.message);
+			this.logger.debug(error.message || error);
 		});
+
 		this.p2p.on(EVENT_FAILED_TO_PUSH_NODE_INFO, error => {
-			this.logger.debug(error.message);
+			this.logger.debug(error.message || error);
 		});
+
 		this.p2p.on(EVENT_OUTBOUND_SOCKET_ERROR, error => {
-			this.logger.debug(error.message);
+			this.logger.debug(error.message || error);
 		});
+
 		this.p2p.on(EVENT_INBOUND_SOCKET_ERROR, error => {
-			this.logger.debug(error.message);
+			this.logger.debug(error.message || error);
 		});
+
 		this.p2p.on(EVENT_UPDATED_PEER_INFO, peerInfo => {
 			this.logger.info(
 				`Updated info of peer ${peerInfo.ipAddress}:${
@@ -98,34 +120,47 @@ module.exports = class Network {
 				} to ${JSON.stringify(peerInfo)}`
 			);
 		});
+
 		this.p2p.on(EVENT_FAILED_PEER_INFO_UPDATE, error => {
-			this.logger.debug(error.message);
+			this.logger.debug(error.message || error);
 		});
 
 		this.p2p.on(EVENT_REQUEST_RECEIVED, async request => {
 			this.logger.info(
 				`Received inbound request for procedure ${request.procedure}`
 			);
+			// If the request has already been handled internally by the P2P library, we ignore.
+			if (request.wasResponseSent) {
+				return;
+			}
+			const hasTargetModule = hasNamespaceReg.test(request.procedure);
+			// If the request has no target module, default to chain (to support legacy protocol).
+			const sanitizedProcedure = hasTargetModule ?
+				request.procedure : `chain:${request.procedure}`;
 			try {
 				const result = await this.channel.invoke(
-					request.procedure,
+					sanitizedProcedure,
 					request.data
 				);
-				request.end(result); // Send the response back to the peer.
 				this.logger.info(`Responsed to peer request ${request.procedure}`);
+				request.end(result); // Send the response back to the peer.
 			} catch (error) {
-				request.error(error); // Send an error back to the peer.
 				this.logger.debug(
 					`Could not respond to peer request ${
 						request.procedure
-					} because of error: ${error.message}`
+					} because of error: ${error.message || error.message}`
 				);
+				request.error(error); // Send an error back to the peer.
 			}
 		});
 
 		this.p2p.on(EVENT_MESSAGE_RECEIVED, async packet => {
+			const hasSourceModule = hasNamespaceReg.test(packet.event);
+			// If the request has no source module, default to chain (to support legacy protocol).
+			const sanitizedEvent = hasSourceModule ?
+				packet.event : `chain:${packet.event}`;
 			this.logger.info(`Received inbound message for event ${packet.event}`);
-			this.channel.publish(`network:${packet.event}`, packet.data);
+			this.channel.publish(`network:${sanitizedEvent}`, packet.data);
 		});
 
 		// ---- END: Bind event handlers ----
