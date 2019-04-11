27e17a839732d243e59e9c3bc8d96fec5d788a8c
Daniel Limia <limiaspasdaniel@gmail.com>
2019-04-11 16:58:24 +0200

    Update tests for application.js

--- framework/test/jest/specs/unit/controller/application.spec.js
+++ framework/test/jest/specs/unit/controller/application.spec.js
@@ -1,113 +1,129 @@
+const _ = require('lodash');
 const Application = require('../../../../../src/controller/application');
 const validator = require('../../../../../src/controller/helpers/validator');
 const applicationSchema = require('../../../../../src/controller/schema/application');
 const constantsSchema = require('../../../../../src/controller/schema/constants');
 
 jest.mock('../../../../../src/components/logger');
-jest.mock('../../../../../src/controller/helpers/validator');
 jest.mock('../../../../../src/components/logger');
+
+const appSchema = {
+	type: 'object',
+	properties: {
+		NETWORK: {
+			type: 'string',
+			description:
+				'lisk network [devnet|betanet|mainnet|testnet]. Defaults to "devnet"',
+			enum: ['devnet', 'alphanet', 'betanet', 'testnet', 'mainnet'],
+			env: 'LISK_NETWORK',
+			arg: '-n,--network',
+		},
+		CUSTOM_CONFIG_FILE: {
+			type: ['string', 'null'],
+			description: 'Custom configuration file path',
+			default: null,
+			env: 'LISK_CONFIG_FILE',
+			arg: '-c,--config',
+		},
+	},
+	default: {
+		NETWORK: 'devnet',
+		CUSTOM_CONFIG_FILE: null,
+	},
+};
+
+const { NETWORK } = validator.parseEnvArgAndValidate(appSchema, {});
+
+const appConfig = {
+	app: {
+		version: '1.6.0',
+		minVersion: '1.0.0',
+		protocolVersion: '1.0',
+	},
+};
+
+// eslint-disable-next-line import/no-dynamic-require
+const networkConfig = require(`../../../../../../config/${NETWORK}/config`);
+// eslint-disable-next-line import/no-dynamic-require
+const genesisBlock = require(`../../../../../../config/${NETWORK}/genesis_block`);
 
 describe('Application', () => {
 	// Arrange
 	const params = {
 		label: 'jest-unit',
-		genesisBlock: {},
-		constants: {},
-		config: { components: { logger: null }, modules: {} },
+		genesisBlock,
+		config: [networkConfig, appConfig],
 	};
 
 	describe('#constructor', () => {
+		afterEach(() => {
+			// So we can start a fresh schema each time Application is instantiated
+			validator.validator.removeSchema();
+			validator.parserAndValidator.removeSchema();
+		});
+
 		it('should accept function as label argument', () => {
 			// Arrange
 			const labelFn = () => 'jest-unit';
 
 			// Act
-			const app = new Application(
-				labelFn,
-				params.genesisBlock,
-				params.constants,
-				params.config
-			);
-
-			expect(app.label).toBe(labelFn());
+			const app = new Application(labelFn, params.genesisBlock, params.config);
+			expect(app.label).toBe(labelFn);
 		});
 
 		it('should set filename for logger if logger component was not provided', () => {
 			// Arrange
-			const config = { components: {}, modules: {} };
-
-			validator.validate.mockReturnValue(config);
+			const configWithoutLogger = _.cloneDeep(params.config);
+			delete configWithoutLogger[0].components.logger;
 
 			// Act
-			// eslint-disable-next-line no-unused-vars
-			const app = new Application(
-				params.label,
-				params.genesisBlock,
-				params.constants,
-				config
-			);
+			new Application(params.label, params.genesisBlock, configWithoutLogger);
 
-			expect(config.components.logger.logFileName).toBe(
+			expect(configWithoutLogger[0].components.logger.logFileName).toBe(
 				`${process.cwd()}/logs/${params.label}/lisk.log`
 			);
 		});
 
-		it('should load applicationSchema', () => {
+		it('should load applicationSchema and constantsSchema', () => {
+			const loadSchemaSpy = jest.spyOn(validator, 'loadSchema');
+
 			// Act
-			// eslint-disable-next-line no-unused-vars
-			const app = new Application(
-				params.label,
-				params.genesisBlock,
-				params.constants,
-				params.config
-			);
+			new Application(params.label, params.genesisBlock, params.config);
 
 			// Assert
-			expect(validator.loadSchema).toHaveBeenCalledWith(applicationSchema);
-		});
-
-		it('should load constantsSchema', () => {
-			// Act
-			// eslint-disable-next-line no-unused-vars
-			const app = new Application(
-				params.label,
-				params.genesisBlock,
-				params.constants,
-				params.config
-			);
-
-			// Assert
-			expect(validator.loadSchema).toHaveBeenCalledWith(constantsSchema);
+			expect(loadSchemaSpy).toHaveBeenCalledWith(applicationSchema);
+			expect(loadSchemaSpy).toHaveBeenCalledWith(constantsSchema);
 		});
 
 		it('should validate constructor arguments', () => {
 			// Act
-			// eslint-disable-next-line no-unused-vars
-			const app = new Application(
-				params.label,
-				params.genesisBlock,
-				params.constants,
-				params.config
+			const validateSpy = jest.spyOn(validator, 'validate');
+			const parseEnvArgAndValidateSpy = jest.spyOn(
+				validator,
+				'parseEnvArgAndValidate'
 			);
+			new Application(params.label, params.genesisBlock, params.config);
 
 			// Assert
-			expect(validator.validate).toHaveBeenCalledWith(
+			expect(validateSpy).toHaveBeenNthCalledWith(
+				1,
 				applicationSchema.appLabel,
 				params.label
 			);
-
-			expect(validator.parseEnvArgAndValidate).toHaveBeenCalledWith(
-				constantsSchema.constants
-			);
-
-			expect(validator.validate).toHaveBeenCalledWith(
-				applicationSchema.config,
-				params.config
-			);
-
-			expect(validator.validate).toHaveBeenCalledWith(
+			expect(validateSpy).toHaveBeenNthCalledWith(
+				2,
 				applicationSchema.genesisBlock,
 				params.genesisBlock
+			);
+			expect(parseEnvArgAndValidateSpy).toHaveBeenNthCalledWith(
+				1,
+				applicationSchema.config,
+				_.defaultsDeep(...params.config)
+			);
+			expect(parseEnvArgAndValidateSpy).toHaveBeenNthCalledWith(
+				2,
+				constantsSchema.constants,
+				expect.any(Object)
 			);
 		});
 
@@ -116,7 +132,6 @@ describe('Application', () => {
 			const app = new Application(
 				params.label,
 				params.genesisBlock,
-				params.constants,
 				params.config
 			);
 
@@ -124,7 +139,7 @@ describe('Application', () => {
 			// Investigate if these are implementation details
 			expect(app.genesisBlock).toBe(params.genesisBlock);
 			expect(app.label).toBe(params.label);
-			expect(app.config).toEqual(params.config);
+			expect(app.config).toEqual(_.defaultsDeep(...params.config));
 			expect(app.controller).toBeNull();
 		});
 	});
