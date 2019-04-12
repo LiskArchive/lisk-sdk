f056e85797c82fa9a00c17c139225b004a577d77
Shusetsu Toda <shusetsu@lightcurve.io>
2019-04-12 11:26:10 +0200

    :recycle: Fix for merge deevelopment

--- framework/src/modules/network/defaults/index.js
+++ framework/src/modules/network/defaults/index.js
@@ -1,12 +1,5 @@
+const config = require('./config');
+
 module.exports = {
-	blacklistedPeers: [],
-	connectTimeout: 5000,
-	ackTimeout: 10000,
-	discoveryInterval: 30000,
-	seedPeers: [],
-	wsEngine: 'ws',
-	nodeInfo: {
-		wsPort: 5001, // TODO: Change to 5000
-		height: 0,
-	},
+	config,
 };
