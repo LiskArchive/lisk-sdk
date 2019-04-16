24cd1deeca3deb4b19fd8994a2b43ebda1242062
Manu Nelamane Siddalingegowda <manu@lightcurve.io>
2019-04-16 13:48:09 +0200

    Fix lint errors

--- framework/test/test_app/index.js
+++ framework/test/test_app/index.js
@@ -1,9 +1,5 @@
-const fs = require('fs');
-const path = require('path');
-
 const {
 	Application,
-	helpers: { validator },
 	/* eslint-disable import/no-unresolved */
 } = require('../../src');
 
@@ -19,10 +15,10 @@ const appConfig = {
 
 try {
 	// TODO: I would convert config.json to .JS
-	const networkConfig = require(`../fixtures/config/devnet/config`);
+	const networkConfig = require('../fixtures/config/devnet/config');
 	// TODO: Merge constants and exceptions with the above config.
-	const exceptions = require(`../fixtures/config/devnet/exceptions`);
-	const genesisBlock = require(`../fixtures/config/devnet/genesis_block`);
+	const exceptions = require('../fixtures/config/devnet/exceptions');
+	const genesisBlock = require('../fixtures/config/devnet/genesis_block');
 
 	// To run multiple applications for same network for integration tests
 	const appName = config => `lisk-devnet-${config.modules.http_api.httpPort}`;
