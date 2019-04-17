a9dfd85f51db6349caa33c13401ec399b34cc4a6
Nazar Hussain <nazar@lightcurve.io>
2019-04-17 16:17:24 +0200

    Fix the unit tests for schema snapshots

--- framework/test/jest/unit/specs/controller/schema/application.spec.js
+++
@@ -1,7 +0,0 @@
-const application = require('../../../../../../src/controller/schema/application');
-
-describe('schema/application.js', () => {
-	it('application schema must match to the snapshot.', () => {
-		expect(application).toMatchSnapshot();
-	});
-});
