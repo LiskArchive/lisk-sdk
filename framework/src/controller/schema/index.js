04a9bc59e378074a831ca5b3659b0261e8aedf24
Nazar Hussain <nazar@lightcurve.io>
2019-04-17 10:12:52 +0200

    Restructure the schema files for controller

--- framework/src/controller/schema/index.js
+++ framework/src/controller/schema/index.js
@@ -2,3 +2,13 @@
  * @namespace framework.controller.schema
  * @see Parent: {@link controller}
  */
+
+const applicationSchema = require('./application_schema');
+const constantsSchema = require('./constants_schema');
+const genesisBlockSchema = require('./genesis_block_schema');
+
+module.exports = {
+	applicationSchema,
+	constantsSchema,
+	genesisBlockSchema,
+};
