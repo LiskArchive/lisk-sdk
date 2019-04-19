4f048b898974113d5fb2567fed33b1de703a26f0
shuse2 <shus.toda@gmail.com>
2019-04-19 16:00:32 +0200

    :seedling: Replace logger with bunyan

--- framework/src/components/logger/logger.js
+++ framework/src/components/logger/logger.js
@@ -14,140 +14,51 @@
 
 'use strict';
 
-const fs = require('fs');
-const util = require('util');
-const child_process = require('child_process');
 const path = require('path');
-const strftime = require('strftime').utc();
+const fs = require('fs');
+const bunyan = require('bunyan');
 
-require('colors');
-
-class Logger {
-	/**
-	 *
-	 * @param {Object} config
-	 * @param {string} config.logFileName
-	 * @param {string} config.fileLogLevel
-	 * @param {string} config.consoleLogLevel
-	 */
-	constructor(config) {
-		// Not exposed via defaultConfig
-		this.levels = config.levels || {
-			none: 99,
-			trace: 0,
-			debug: 1,
-			log: 2,
-			info: 3,
-			warn: 4,
-			error: 5,
-			fatal: 6,
-		};
-
-		// Not exposed via defaultConfig
-		this.level_abbr = config.level_abbr || {
-			trace: 'trc',
-			debug: 'dbg',
-			log: 'log',
-			info: 'inf',
-			warn: 'WRN',
-			error: 'ERR',
-			fatal: 'FTL',
-		};
-
-		this.logFileName = `${process.cwd()}/${config.logFileName || 'logs.log'}`;
-		this.fileLogLevel = config.fileLogLevel || 'log';
-		this.consoleLogLevel = config.consoleLogLevel;
+const createDirIfNotExist = filePath => {
+	const dir = path.dirname(filePath);
+	if (fs.existsSync(dir)) {
+		return;
 	}
+	fs.mkdirSync(dir);
+};
 
-	bootstrap() {
-		const logs = {};
-		child_process.execSync(`mkdir -p ${path.dirname(this.logFileName)}`);
-		const log_file = fs.createWriteStream(this.logFileName, {
-			flags: 'a',
-		});
+/**
+ *
+ * @param {Object} config
+ * @param {string} config.logFileName
+ * @param {string} config.fileLogLevel
+ * @param {string} config.consoleLogLevel
+ */
+const createLogger = ({ fileLogLevel, consoleLogLevel, logFileName }) => {
+	const consoleSrc = consoleLogLevel === 'debug' || consoleLogLevel === 'trace';
+	const consoleStream = consoleLogLevel !== 'none' ? [{
+		level: consoleLogLevel,
+		stream: process.stdout,
+	}] : [];
+	const filePath = path.join(process.cwd(), logFileName);
+	createDirIfNotExist(filePath);
+	const fileSrc = fileLogLevel === 'debug' || fileLogLevel === 'trace';
+	const fileStream = fileLogLevel !== 'none' ? [{
+		type: 'rotating-file',
+		level: fileLogLevel,
+		path: filePath,
+	}] : [];
+	const streams = [
+		...consoleStream,
+		...fileStream,
+	];
+	return bunyan.createLogger({
+		name: 'lisk-framework',
+		streams,
+		src: consoleSrc || fileSrc,
+	});
+};
 
-		Object.keys(this.levels).forEach(name => {
-			logs[name] = (message, data) => {
-				const logContext = {
-					level: name,
-					timestamp: strftime('%F %T', new Date()),
-				};
-
-				if (message instanceof Error) {
-					logContext.message = message.stack;
-				} else {
-					logContext.message = message;
-				}
-
-				if (data && util.isObject(data)) {
-					logContext.data = JSON.stringify(Logger.snipFragileData(data));
-				} else {
-					logContext.data = data;
-				}
-
-				logContext.symbol = this.level_abbr[logContext.level]
-					? this.level_abbr[logContext.level]
-					: '???';
-
-				if (this.levels[this.fileLogLevel] <= this.levels[logContext.level]) {
-					if (logContext.data) {
-						log_file.write(
-							util.format(
-								'[%s] %s | %s - %s\n',
-								logContext.symbol,
-								logContext.timestamp,
-								logContext.message,
-								logContext.data
-							)
-						);
-					} else {
-						log_file.write(
-							util.format(
-								'[%s] %s | %s\n',
-								logContext.symbol,
-								logContext.timestamp,
-								logContext.message
-							)
-						);
-					}
-				}
-
-				if (
-					this.consoleLogLevel &&
-					this.levels[this.consoleLogLevel] <= this.levels[logContext.level]
-				) {
-					if (logContext.data) {
-						console.info(
-							`[${logContext.symbol.bgYellow.black}]`,
-							logContext.timestamp.grey,
-							'|',
-							logContext.message,
-							'-',
-							logContext.data
-						);
-					} else {
-						console.info(
-							`[${logContext.symbol.bgYellow.black}]`,
-							logContext.timestamp.grey,
-							'|',
-							logContext.message
-						);
-					}
-				}
-			};
-		});
-
-		return logs;
-	}
-
-	static snipFragileData(data) {
-		Object.keys(data).forEach(key => {
-			if (key.search(/passphrase|password/i) > -1) {
-				data[key] = 'XXXXXXXXXX';
-			}
-		});
-		return data;
-	}
-}
-
-module.exports = Logger;
+module.exports = {
+	createDirIfNotExist,
+	createLogger,
+};
