820428be043754a1e5b09fc8b4bad0fc439319d3
shuse2 <shus.toda@gmail.com>
2019-04-27 10:58:51 +0200

    :bug: Change state store not to be singleton

--- framework/src/modules/chain/logic/state_store/index.js
+++ framework/src/modules/chain/logic/state_store/index.js
@@ -2,37 +2,28 @@ const AccountStore = require('./account_store');
 const TransactionStore = require('./transaction_store');
 const RoundStore = require('./round_store');
 
-class StateStoreManager {
-	constructor(storage, cb) {
+class StateStore {
+	constructor(storage, options) {
 		this.entities = {
 			Account: storage.entities.Account,
 			Transaction: storage.entities.Transaction,
 			Round: storage.entities.Round,
 		};
 
-		this.stateStore = {};
-
-		return setImmediate(cb, null, this);
-	}
-
-	createStore(options) {
-		this.stateStore = {
-			account: new AccountStore(this.entities.Account, options),
-			round: new RoundStore(this.entities.Round, options),
-			transaction: new TransactionStore(this.entities.Transaction, options),
-		};
-		return this.stateStore;
+		this.account = new AccountStore(this.entities.Account, options);
+		this.round = new RoundStore(this.entities.Round, options);
+		this.transaction = new TransactionStore(this.entities.Transaction, options);
 	}
 
 	createSnapshot() {
-		this.stateStore.account.createSnapshot();
-		this.stateStore.transaction.createSnapshot();
+		this.account.createSnapshot();
+		this.transaction.createSnapshot();
 	}
 
 	restoreSnapshot() {
-		this.stateStore.account.restoreSnapshot();
-		this.stateStore.transaction.restoreSnapshot();
+		this.account.restoreSnapshot();
+		this.transaction.restoreSnapshot();
 	}
 }
 
-module.exports = StateStoreManager;
+module.exports = StateStore;
