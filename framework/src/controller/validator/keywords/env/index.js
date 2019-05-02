b728dbfacbccbc4e6a75f0f8e23e0bfeb05fb7f1
michielmulders <michielmulders1@gmail.com>
2019-05-02 11:02:34 +0200

    Convert env vars to correc type

--- framework/src/controller/validator/keywords/env/index.js
+++ framework/src/controller/validator/keywords/env/index.js
@@ -18,7 +18,21 @@ const compile = (schema, parentSchema) => {
 			  };
 
 	return function(data, dataPath, object, key) {
-		const variableValue = process.env[envVariable.name];
+		let variableValue = process.env[envVariable.name];
+
+		if (process.env[envVariable.name] !== undefined) {
+			// eslint-disable-next-line default-case
+			switch (parentSchema.type) {
+				case 'array':
+					variableValue = process.env[envVariable.name].split(',');
+					break;
+				case 'integer':
+					variableValue = parseInt(process.env[envVariable.name]);
+					break;
+				case 'boolean':
+					variableValue = JSON.parse(process.env[envVariable.name]);
+			}
+		}
 
 		if (variableValue) {
 			object[key] = envVariable.formatter
