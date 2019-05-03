e8218a0dedd7436c98a332f9a2d619fb86c3344f
Pablo Vicente <pvicente@gmail.com>
2019-05-03 14:44:17 +0200

    Remove usages of prototype from modules/chain/submodules/rounds.js

--- framework/src/modules/chain/submodules/rounds.js
+++ framework/src/modules/chain/submodules/rounds.js
@@ -58,375 +58,379 @@ class Rounds {
 
 		setImmediate(cb, null, self);
 	}
-}
-
-// Public methods
-/**
- * @returns {boolean} __private.loaded
- * @todo Add description of the function
- */
-Rounds.prototype.loaded = function() {
-	return __private.loaded;
-};
-
-/**
- * @returns {boolean} __private.ticking
- * @todo Add description of the function
- */
-Rounds.prototype.ticking = function() {
-	return __private.ticking;
-};
-
-/**
- * Performs backward tick on round.
- *
- * @param {block} block - Current block
- * @param {block} previousBlock - Previous block
- * @param {function} done - Callback function
- * @param {function} tx - SQL transaction
- * @returns {function} Calling done with error if any
- */
-Rounds.prototype.backwardTick = function(block, previousBlock, done, tx) {
-	const round = slots.calcRound(block.height);
-	const prevRound = slots.calcRound(previousBlock.height);
-	const nextRound = slots.calcRound(block.height + 1);
-
-	const scope = {
-		library,
-		modules,
-		block,
-		round,
-		backwards: true,
-	};
-
-	// Establish if finishing round or not
-	scope.finishRound =
-		(prevRound === round && nextRound !== round) ||
-		(block.height === 1 || block.height === 101);
 
 	/**
-	 * Description of backwardTick.
-	 *
-	 * @todo Add @param tags
-	 * @todo Add @returns tag
+	 * @returns {boolean} __private.loaded
 	 * @todo Add description of the function
 	 */
-	function backwardTick(backwardTickTx) {
-		const newRound = new Round(scope, backwardTickTx);
-
-		library.logger.debug('Performing backward tick');
-		library.logger.trace(scope);
-		return newRound
-			.mergeBlockGenerator()
-			.then(() => (scope.finishRound ? newRound.backwardLand() : newRound));
+	// eslint-disable-next-line class-methods-use-this
+	loaded() {
+		return __private.loaded;
 	}
-
-	// Start round ticking
-	__private.ticking = true;
-	async.series(
-		[
-			function(cb) {
-				// Sum round if finishing round
-				if (scope.finishRound) {
-					return __private.sumRound(scope, cb, tx);
-				}
-				return setImmediate(cb);
-			},
-			function(cb) {
-				// Get outsiders if finishing round
-				if (scope.finishRound) {
-					return __private.getOutsiders(scope, cb, tx);
-				}
-				return setImmediate(cb);
-			},
-			function(cb) {
-				// Perform round tick
-				library.storage.adapter
-					.task('rounds:backwardTick', backwardTick, tx)
-					.then(() => setImmediate(cb))
-					.catch(err => {
-						library.logger.error(err.stack);
-						return setImmediate(cb, err);
-					});
-			},
-		],
-		err => {
-			// Stop round ticking
-			__private.ticking = false;
-			if (err) {
-				return done(err);
-			}
-
-			/**
-			 * If we delete first block of the round,
-			 * that means we go to last block of the previous round
-			 * That's why we need to clear the cache to recalculate
-			 * delegate list.
-			 * */
-			if (scope.finishRound) {
-				modules.delegates.clearDelegateListCache();
-			}
-
-			return done();
-		}
-	);
-};
-
-/**
- * Performs forward tick on round.
- *
- * @param {block} block - Current block
- * @param {function} done - Callback function
- * @param {Object} tx - Database transaction/task object
- * @returns {function} Calling done with error if any
- */
-Rounds.prototype.tick = function(block, done, tx) {
-	const round = slots.calcRound(block.height);
-	const nextRound = slots.calcRound(block.height + 1);
-
-	const scope = {
-		library,
-		modules,
-		block,
-		round,
-		backwards: false,
-	};
-
-	// Establish if finishing round or not
-	scope.finishRound =
-		round !== nextRound || (block.height === 1 || block.height === 101);
 
 	/**
-	 * Description of Tick.
-	 *
-	 * @class
-	 * @todo Add @param tags
-	 * @todo Add @returns tag
-	 * @todo Add description of the class
+	 * @returns {boolean} __private.ticking
+	 * @todo Add description of the function
 	 */
-	function Tick(t) {
-		const promised = new Round(scope, t);
+	// eslint-disable-next-line class-methods-use-this
+	ticking() {
+		return __private.ticking;
+	}
 
-		library.logger.debug('Performing forward tick');
-		library.logger.trace(scope);
+	/**
+	 * Performs backward tick on round.
+	 *
+	 * @param {block} block - Current block
+	 * @param {block} previousBlock - Previous block
+	 * @param {function} done - Callback function
+	 * @param {function} tx - SQL transaction
+	 * @returns {function} Calling done with error if any
+	 */
+	// eslint-disable-next-line class-methods-use-this
+	backwardTick(block, previousBlock, done, tx) {
+		const round = slots.calcRound(block.height);
+		const prevRound = slots.calcRound(previousBlock.height);
+		const nextRound = slots.calcRound(block.height + 1);
 
-		return promised
-			.mergeBlockGenerator()
-			.then(() => {
-				if (scope.finishRound) {
-					return promised.land().then(() => {
-						library.bus.message('finishRound', round);
-					});
-				}
-				return true;
-			})
-			.then(() => {
-				// Check if we are one block before last block of round, if yes - perform round snapshot
-				if ((block.height + 1) % ACTIVE_DELEGATES === 0) {
-					library.logger.debug('Performing round snapshot...');
+		const scope = {
+			library,
+			modules,
+			block,
+			round,
+			backwards: true,
+		};
 
-					return t
-						.batch([
-							library.storage.entities.Round.clearRoundSnapshot(t),
-							library.storage.entities.Round.performRoundSnapshot(t),
-							library.storage.entities.Round.clearVotesSnapshot(t),
-							library.storage.entities.Round.performVotesSnapshot(t),
-						])
-						.then(() => {
-							library.logger.trace('Round snapshot done');
-						})
+		// Establish if finishing round or not
+		scope.finishRound =
+			(prevRound === round && nextRound !== round) ||
+			(block.height === 1 || block.height === 101);
+
+		/**
+		 * Description of backwardTick.
+		 *
+		 * @todo Add @param tags
+		 * @todo Add @returns tag
+		 * @todo Add description of the function
+		 */
+		function backwardTick(backwardTickTx) {
+			const newRound = new Round(scope, backwardTickTx);
+
+			library.logger.debug('Performing backward tick');
+			library.logger.trace(scope);
+			return newRound
+				.mergeBlockGenerator()
+				.then(() => (scope.finishRound ? newRound.backwardLand() : newRound));
+		}
+
+		// Start round ticking
+		__private.ticking = true;
+		async.series(
+			[
+				function(cb) {
+					// Sum round if finishing round
+					if (scope.finishRound) {
+						return __private.sumRound(scope, cb, tx);
+					}
+					return setImmediate(cb);
+				},
+				function(cb) {
+					// Get outsiders if finishing round
+					if (scope.finishRound) {
+						return __private.getOutsiders(scope, cb, tx);
+					}
+					return setImmediate(cb);
+				},
+				function(cb) {
+					// Perform round tick
+					library.storage.adapter
+						.task('rounds:backwardTick', backwardTick, tx)
+						.then(() => setImmediate(cb))
 						.catch(err => {
-							library.logger.error('Round snapshot failed', err);
-							throw err;
+							library.logger.error(err.stack);
+							return setImmediate(cb, err);
 						});
+				},
+			],
+			err => {
+				// Stop round ticking
+				__private.ticking = false;
+				if (err) {
+					return done(err);
 				}
-				return true;
-			});
+
+				/**
+				 * If we delete first block of the round,
+				 * that means we go to last block of the previous round
+				 * That's why we need to clear the cache to recalculate
+				 * delegate list.
+				 * */
+				if (scope.finishRound) {
+					modules.delegates.clearDelegateListCache();
+				}
+
+				return done();
+			}
+		);
 	}
 
-	// Start round ticking
-	__private.ticking = true;
-	async.series(
-		[
-			function(cb) {
-				// Sum round if finishing round
-				if (scope.finishRound) {
-					return __private.sumRound(scope, cb, tx);
-				}
-				return setImmediate(cb);
-			},
-			function(cb) {
-				// Get outsiders if finishing round
-				if (scope.finishRound) {
-					return __private.getOutsiders(scope, cb, tx);
-				}
-				return setImmediate(cb);
-			},
-			// Perform round tick
-			function(cb) {
-				library.storage.adapter
-					.task('rounds:tick', Tick, tx)
-					.then(() => setImmediate(cb))
-					.catch(err => {
-						library.logger.error(err.stack);
-						return setImmediate(cb, err);
-					});
-			},
-		],
-		err => {
-			// Stop round ticking
-			__private.ticking = false;
-			return done(err);
+	/**
+	 * Performs forward tick on round.
+	 *
+	 * @param {block} block - Current block
+	 * @param {function} done - Callback function
+	 * @param {Object} tx - Database transaction/task object
+	 * @returns {function} Calling done with error if any
+	 */
+	// eslint-disable-next-line class-methods-use-this
+	tick(block, done, tx) {
+		const round = slots.calcRound(block.height);
+		const nextRound = slots.calcRound(block.height + 1);
+
+		const scope = {
+			library,
+			modules,
+			block,
+			round,
+			backwards: false,
+		};
+
+		// Establish if finishing round or not
+		scope.finishRound =
+			round !== nextRound || (block.height === 1 || block.height === 101);
+
+		/**
+		 * Description of Tick.
+		 *
+		 * @class
+		 * @todo Add @param tags
+		 * @todo Add @returns tag
+		 * @todo Add description of the class
+		 */
+		function Tick(t) {
+			const promised = new Round(scope, t);
+
+			library.logger.debug('Performing forward tick');
+			library.logger.trace(scope);
+
+			return promised
+				.mergeBlockGenerator()
+				.then(() => {
+					if (scope.finishRound) {
+						return promised.land().then(() => {
+							library.bus.message('finishRound', round);
+						});
+					}
+					return true;
+				})
+				.then(() => {
+					// Check if we are one block before last block of round, if yes - perform round snapshot
+					if ((block.height + 1) % ACTIVE_DELEGATES === 0) {
+						library.logger.debug('Performing round snapshot...');
+
+						return t
+							.batch([
+								library.storage.entities.Round.clearRoundSnapshot(t),
+								library.storage.entities.Round.performRoundSnapshot(t),
+								library.storage.entities.Round.clearVotesSnapshot(t),
+								library.storage.entities.Round.performVotesSnapshot(t),
+							])
+							.then(() => {
+								library.logger.trace('Round snapshot done');
+							})
+							.catch(err => {
+								library.logger.error('Round snapshot failed', err);
+								throw err;
+							});
+					}
+					return true;
+				});
 		}
-	);
-};
 
-/**
- * Create round information record into mem_rounds.
- *
- * @param {string} address - Address of the account
- * @param {Number} round - Associated round number
- * @param {Number} amount - Amount updated on account
- * @param {Object} [tx] - Database transaction
- * @returns {Promise}
- */
-Rounds.prototype.createRoundInformationWithAmount = function(
-	address,
-	round,
-	amount,
-	tx
-) {
-	return library.storage.entities.Account.getOne(
-		{ address },
-		{ extended: true },
-		tx
-	).then(account => {
-		if (!account.votedDelegatesPublicKeys) return true;
-
-		const roundData = account.votedDelegatesPublicKeys.map(
-			delegatePublicKey => ({
-				address,
-				amount,
-				round,
-				delegatePublicKey,
-			})
+		// Start round ticking
+		__private.ticking = true;
+		async.series(
+			[
+				function(cb) {
+					// Sum round if finishing round
+					if (scope.finishRound) {
+						return __private.sumRound(scope, cb, tx);
+					}
+					return setImmediate(cb);
+				},
+				function(cb) {
+					// Get outsiders if finishing round
+					if (scope.finishRound) {
+						return __private.getOutsiders(scope, cb, tx);
+					}
+					return setImmediate(cb);
+				},
+				// Perform round tick
+				function(cb) {
+					library.storage.adapter
+						.task('rounds:tick', Tick, tx)
+						.then(() => setImmediate(cb))
+						.catch(err => {
+							library.logger.error(err.stack);
+							return setImmediate(cb, err);
+						});
+				},
+			],
+			err => {
+				// Stop round ticking
+				__private.ticking = false;
+				return done(err);
+			}
 		);
+	}
 
-		return library.storage.entities.Round.create(roundData, {}, tx);
-	});
-};
+	/**
+	 * Create round information record into mem_rounds.
+	 *
+	 * @param {string} address - Address of the account
+	 * @param {Number} round - Associated round number
+	 * @param {Number} amount - Amount updated on account
+	 * @param {Object} [tx] - Database transaction
+	 * @returns {Promise}
+	 */
+	// eslint-disable-next-line class-methods-use-this
+	createRoundInformationWithAmount(address, round, amount, tx) {
+		return library.storage.entities.Account.getOne(
+			{ address },
+			{ extended: true },
+			tx
+		).then(account => {
+			if (!account.votedDelegatesPublicKeys) return true;
 
-/**
- * Create round information record into mem_rounds.
- *
- * @param {string} address - Address of the account
- * @param {Number} round - Associated round number
- * @param {string} delegatePublicKey - Associated delegate id
- * @param {string} mode - Possible values of '+' or '-' represents behaviour of adding or removing delegate
- * @param {Object} [tx] - Databaes transaction
- * @returns {Promise}
- */
-Rounds.prototype.createRoundInformationWithDelegate = function(
-	address,
-	round,
-	delegatePublicKey,
-	mode,
-	tx
-) {
-	const balanceFactor = mode === '-' ? -1 : 1;
-	return library.storage.entities.Account.getOne({ address }, {}, tx).then(
-		account => {
-			const balance = new Bignumber(account.balance)
-				.multipliedBy(balanceFactor)
-				.toString();
+			const roundData = account.votedDelegatesPublicKeys.map(
+				delegatePublicKey => ({
+					address,
+					amount,
+					round,
+					delegatePublicKey,
+				})
+			);
 
-			const roundData = {
-				address,
-				delegatePublicKey,
-				round,
-				amount: balance,
-			};
 			return library.storage.entities.Round.create(roundData, {}, tx);
-		}
-	);
-};
-
-// Events
-/**
- * Assigns modules to private constant `modules`.
- *
- * @param {modules} scope - Loaded modules
- */
-Rounds.prototype.onBind = function(scope) {
-	components = {
-		cache: scope.components ? scope.components.cache : undefined,
-	};
-
-	modules = {
-		blocks: scope.modules.blocks,
-		accounts: scope.modules.accounts,
-		delegates: scope.modules.delegates,
-	};
-};
-
-/**
- * Sets private constant loaded to true.
- *
- * @listens module:loader~event:blockchainReady
- */
-Rounds.prototype.onBlockchainReady = function() {
-	__private.loaded = true;
-};
-
-/**
- * Clear all cache entries related to delegate and emits a 'rounds/change' socket message.
- *
- * @param {number} round
- * @emits rounds/change
- * @todo Add description for the params
- */
-Rounds.prototype.onFinishRound = async function(round) {
-	if (components && components.cache && components.cache.isReady()) {
-		library.logger.debug(
-			['Cache - onFinishRound', '| Status:', components.cache.isReady()].join(
-				' '
-			)
-		);
-		const pattern = CACHE_KEYS_DELEGATES;
-		try {
-			await components.cache.removeByPattern(pattern);
-			library.logger.debug(
-				[
-					'Cache - Keys with pattern:',
-					pattern,
-					'cleared from cache on new Round',
-				].join(' ')
-			);
-		} catch (removeByPatternErr) {
-			library.logger.error(
-				[
-					'Cache - Error clearing keys with pattern:',
-					pattern,
-					'when round finish',
-				].join(' ')
-			);
-		}
+		});
 	}
 
-	return library.channel.publish('chain:rounds:change', { number: round });
-};
+	/**
+	 * Create round information record into mem_rounds.
+	 *
+	 * @param {string} address - Address of the account
+	 * @param {Number} round - Associated round number
+	 * @param {string} delegatePublicKey - Associated delegate id
+	 * @param {string} mode - Possible values of '+' or '-' represents behaviour of adding or removing delegate
+	 * @param {Object} [tx] - Databaes transaction
+	 * @returns {Promise}
+	 */
+	// eslint-disable-next-line class-methods-use-this
+	createRoundInformationWithDelegate(
+		address,
+		round,
+		delegatePublicKey,
+		mode,
+		tx
+	) {
+		const balanceFactor = mode === '-' ? -1 : 1;
+		return library.storage.entities.Account.getOne({ address }, {}, tx).then(
+			account => {
+				const balance = new Bignumber(account.balance)
+					.multipliedBy(balanceFactor)
+					.toString();
 
-/**
- * Sets private constant `loaded` to false.
- *
- * @param {function} cb
- * @returns {setImmediateCallback} cb
- * @todo Add description for the params
- */
-Rounds.prototype.cleanup = function(cb) {
-	__private.loaded = false;
-	return setImmediate(cb);
-};
+				const roundData = {
+					address,
+					delegatePublicKey,
+					round,
+					amount: balance,
+				};
+				return library.storage.entities.Round.create(roundData, {}, tx);
+			}
+		);
+	}
+
+	// Events
+	/**
+	 * Assigns modules to private constant `modules`.
+	 *
+	 * @param {modules} scope - Loaded modules
+	 */
+	// eslint-disable-next-line class-methods-use-this
+	onBind(scope) {
+		components = {
+			cache: scope.components ? scope.components.cache : undefined,
+		};
+
+		modules = {
+			blocks: scope.modules.blocks,
+			accounts: scope.modules.accounts,
+			delegates: scope.modules.delegates,
+		};
+	}
+
+	/**
+	 * Sets private constant loaded to true.
+	 *
+	 * @listens module:loader~event:blockchainReady
+	 */
+	// eslint-disable-next-line class-methods-use-this
+	onBlockchainReady() {
+		__private.loaded = true;
+	}
+
+	/**
+	 * Clear all cache entries related to delegate and emits a 'rounds/change' socket message.
+	 *
+	 * @param {number} round
+	 * @emits rounds/change
+	 * @todo Add description for the params
+	 */
+	// eslint-disable-next-line class-methods-use-this
+	async onFinishRound(round) {
+		if (components && components.cache && components.cache.isReady()) {
+			library.logger.debug(
+				['Cache - onFinishRound', '| Status:', components.cache.isReady()].join(
+					' '
+				)
+			);
+			const pattern = CACHE_KEYS_DELEGATES;
+			try {
+				await components.cache.removeByPattern(pattern);
+				library.logger.debug(
+					[
+						'Cache - Keys with pattern:',
+						pattern,
+						'cleared from cache on new Round',
+					].join(' ')
+				);
+			} catch (removeByPatternErr) {
+				library.logger.error(
+					[
+						'Cache - Error clearing keys with pattern:',
+						pattern,
+						'when round finish',
+					].join(' ')
+				);
+			}
+		}
+
+		return library.channel.publish('chain:rounds:change', { number: round });
+	}
+
+	/**
+	 * Sets private constant `loaded` to false.
+	 *
+	 * @param {function} cb
+	 * @returns {setImmediateCallback} cb
+	 * @todo Add description for the params
+	 */
+	// eslint-disable-next-line class-methods-use-this
+	cleanup(cb) {
+		__private.loaded = false;
+		return setImmediate(cb);
+	}
+}
 
 // Private methods
 /**
