953ac8c9859cf38e1cf44dcfea536d3ed1100e4b
Pablo Vicente <pvicente@gmail.com>
2019-05-07 15:28:03 +0200

    Fix chain unit tests due to changes on cache cleanup

--- framework/test/mocha/unit/modules/chain/submodules/blocks/chain.js
+++ framework/test/mocha/unit/modules/chain/submodules/blocks/chain.js
@@ -442,8 +442,10 @@ describe('blocks/chain', () => {
 	describe('__private.afterSave', () => {
 		it('should call afterSave for all transactions', done => {
 			const spy = sinonSandbox.spy();
+			library.channel.publish = sinonSandbox.spy();
 			__private.afterSave(blockWithTransactions, spy);
 			expect(spy.calledOnce).to.be.true;
+			expect(library.channel.publish.calledOnce).to.be.true;
 			done();
 		});
 	});
