b28b459b7b1e1a4b1ab536da45ef02aeb1f5d162
yatki <mehmet@yatki.com>
2019-05-08 11:30:07 +0200

    Imporove comments for Application constructor

--- framework/src/controller/application.js
+++ framework/src/controller/application.js
@@ -101,8 +101,8 @@ class Application {
 	 * @param {string} [config.app.version] - Version of the application
 	 * @param {string} [config.app.minVersion] - Minimum compatible version on the network
 	 * @param {string} [config.app.protocolVersion] - Compatible protocol version application is using
-	 * @param {string} [config.app.lastCommitId] - lastCommitId
-	 * @param {string} [config.app.buildVersion] - buildVersion
+	 * @param {string} [config.app.lastCommitId] - Last commit id coming from application repository
+	 * @param {string} [config.app.buildVersion] - Build version of the application
 	 * @param {Object} [config.components] - Configurations for components
 	 * @param {Object} [config.components.logger] - Configuration for logger component
 	 * @param {Object} [config.components.cache] - Configuration for cache component
