99ca448b9c115d374a088502b772a52c8f86d9e7
mitsujutsu <mitsuaki.uchimoto@gmail.com>
2019-05-31 14:54:43 +0200

    Fix registerTransaction input

--- framework/src/controller/application.js
+++ framework/src/controller/application.js
@@ -225,7 +225,7 @@ class Application {
 	 * @param {number} transactionType - Unique integer that identifies the transaction type
 	 * @param {constructor} Transaction - Implementation of @liskhq/lisk-transactions/base_transaction
 	 */
-	registerTransaction(Transaction, matcher) {
+	registerTransaction(Transaction, { matcher }) {
 		assert(Transaction, 'Transaction implementation is required');
 
 		assert(
@@ -250,7 +250,7 @@ class Application {
 		}
 		const transactions = this.getTransactions();
 
-		transactions[Transaction.type] = Object.freeze(Transaction);
+		transactions[Transaction.TYPE] = Object.freeze(Transaction);
 		__private.transactions.set(this, transactions);
 	}
 
