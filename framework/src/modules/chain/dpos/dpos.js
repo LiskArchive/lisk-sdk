1d9ad1a855216f601135167ba1a7a737aa62f247
yatki <mehmet@yatki.com>
2019-06-11 15:17:24 +0200

    Remove WeakMap usage from Dpos module

--- framework/src/modules/chain/dpos/dpos.js
+++ framework/src/modules/chain/dpos/dpos.js
@@ -1,15 +1,11 @@
 const Delegates = require('./delegates');
 
-const __methods = {
-	delegates: new WeakMap(),
-};
-
 module.exports = class Dpos {
 	constructor({ storage, logger }) {
-		__methods.delegates.set(this, new Delegates({ storage, logger }));
+		this.delegate = new Delegates({ storage, logger });
 	}
 
 	async getRoundDelegates(round) {
-		return __methods.delegates.get(this).getRoundDelegates(round);
+		return this.delegates.getRoundDelegates(round);
 	}
 };
