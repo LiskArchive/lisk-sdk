1d1ede5e08651405609ed4875a65cfce261c7bcb
Daniel Limia <limiaspasdaniel@gmail.com>
2019-06-19 11:22:03 +0200

    Update framework/src/components/logger/logger.js
    Co-Authored-By: Pablo Vicente <pablitovicente@users.noreply.github.com>

--- framework/src/components/logger/logger.js
+++ framework/src/components/logger/logger.js
@@ -61,7 +61,7 @@ const createLogger = ({ fileLogLevel, consoleLogLevel, logFileName }) => {
 		name: 'lisk-framework',
 		streams,
 		src: consoleSrc || fileSrc,
-		serializers: bunyan.stdSerializers,
+		serializers: {err: bunyan.stdSerializers.err},
 	});
 };
 
