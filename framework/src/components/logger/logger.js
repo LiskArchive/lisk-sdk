b9880eaa3bfe15aaf9f8f699788f0a9d982d0cfe
Pablo Vicente <pvicente@gmail.com>
2019-06-20 09:09:51 +0200

    Fix linting issues with logger change

--- framework/src/components/logger/logger.js
+++ framework/src/components/logger/logger.js
@@ -61,7 +61,7 @@ const createLogger = ({ fileLogLevel, consoleLogLevel, logFileName }) => {
 		name: 'lisk-framework',
 		streams,
 		src: consoleSrc || fileSrc,
-		serializers: {err: bunyan.stdSerializers.err},
+		serializers: { err: bunyan.stdSerializers.err },
 	});
 };
 
