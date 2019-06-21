5b9fa7697f6dabd4484628aecb5852c23f843b6b
Daniel Limia <limiaspasdaniel@gmail.com>
2019-06-21 09:57:45 +0200

    Add JSDoc for Synchronizer

--- framework/src/modules/chain/blocks/synchronizer/synchronizer.js
+++ framework/src/modules/chain/blocks/synchronizer/synchronizer.js
@@ -4,31 +4,40 @@ const FastChainSwitchingMechanism = require('./fast_chain_switching_mechanism');
 class Synchronizer {
 	constructor() {
 		this.currentStrategy = null;
+		this.blockSynchronizationMechanism = new BlockSynchronizationMechanism();
+		this.fastChainSwitchingMechanism = new FastChainSwitchingMechanism();
 	}
 
+	/**
+	 * Start the syncing mechanism
+	 * @return {*}
+	 */
 	run() {
 		if (this.currentStrategy && this.currentStrategy.isActive()) {
 			throw new Error('Blocks Sychronizer is already running');
 		}
 
 		this.currentStrategy = this._determineStrategy();
+
 		return this.currentStrategy.run();
 	}
 
+	/**
+	 * Check if the current syncing mechanism is active
+	 * @return {*|never|boolean}
+	 */
 	isActive() {
 		return this.currentStrategy.isActive();
 	}
 
 	/**
-	 * Determine and return the mechanism strategy to follow
+	 * Determine and return the syncing mechanism strategy to follow
 	 * @private
 	 */
 	// eslint-disable-next-line class-methods-use-this
 	_determineStrategy() {
-		if (1) {
-			return new BlockSynchronizationMechanism();
-		}
-		return new FastChainSwitchingMechanism();
+		// Return blockSynchronizationMechanism or fastChainSwitchingMechanism depending on
+		// Moving to a Different Chain conditions defined in LIP-0014
 	}
 }
 
