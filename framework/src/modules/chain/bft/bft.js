0ea05e38fb01c3e074b9167d65e1c449b19da7ef
Nazar Hussain <nazar@lightcurve.io>
2019-06-24 10:28:50 +0200

    Add the init logic to BFT module

---
+++ framework/src/modules/chain/bft/bft.js
@@ -0,0 +1,110 @@
+/*
+ * Copyright © 2018 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+'use strict';
+
+const { FinalityManager } = require('./finality_manager');
+
+const KEYS = {
+	FINALIZED_HEIGHT: 'BFT.finalizedHeight',
+};
+
+const TWO_ROUNDS = 101 * 2;
+
+const extractBlockHeaderFromBlock = block => {
+	if (block.version !== 2) {
+		return null;
+	}
+
+	return {
+		blockId: block.id,
+		height: block.height,
+		maxHeightPreviouslyForged: block.maxHeightPreviouslyForged,
+		prevotedConfirmedUptoHeight: block.prevotedConfirmedUptoHeight,
+		delegatePublicKey: block.generatorPublicKey,
+		activeSinceRound: 0, // TODO: Link the new DPOS with BFT here
+	};
+};
+
+class BFT {
+	constructor({ storage, logger, activeDelegates, startingHeight }) {
+		this._finalityManager = null;
+
+		this.logger = logger;
+		this.storage = storage;
+		this.constants = {
+			activeDelegates,
+			startingHeight,
+		};
+	}
+
+	async init() {
+		try {
+			// Initialize the finality manager
+			// =====================================
+
+			// Check what finalized height was stored last time
+			const finalizedHeightStored =
+				parseInt(
+					await this.storage.entities.ChainMeta.getKey(KEYS.FINALIZED_HEIGHT)
+				) || 0;
+
+			// Check BFT migration height
+			// https://github.com/LiskHQ/lips/blob/master/proposals/lip-0014.md#backwards-compatibility
+			const bftMigrationHeight = this.constants.startingHeight - TWO_ROUNDS;
+
+			// Choose
+			const finalizedHeight = Math.max(
+				finalizedHeightStored,
+				bftMigrationHeight
+			);
+
+			this._finalityManager = new FinalityManager({
+				finalizedHeight,
+				activeDelegates: this.constants.activeDelegates,
+			});
+
+			const lastBlock = await this.storage.entities.Block.get(
+				{},
+				{ limit: 1, sort: 'height:desc' }
+			);
+			const lastBlockHeight = lastBlock.length ? lastBlock[0].height : 0;
+
+			const loadUptoHeight = Math.max(
+				lastBlockHeight - TWO_ROUNDS,
+				finalizedHeight
+			);
+
+			const rows = await this.storage.entities.Block.get(
+				{ height_gte: loadUptoHeight },
+				{ limit: 1, sort: 'height:asc' }
+			);
+
+			rows.map(row =>
+				this._finalityManager.addBlockHeader(
+					exportedInterface.extractBlockHeaderFromBlock(row)
+				)
+			);
+		} catch (error) {
+			this.logger.error(error, 'Unable to init BFT module');
+		}
+	}
+}
+
+const exportedInterface = {
+	extractBlockHeaderFromBlock,
+	BFT,
+};
+
+module.exports = exportedInterface;
