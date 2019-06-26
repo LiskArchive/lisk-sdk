0f43ce6b54fb6715321d3ab2e0bc2e05355fb6e1
Daniel Limia <limiaspasdaniel@gmail.com>
2019-06-26 09:48:58 +0200

    Remove broadhash from application state

--- framework/src/modules/http_api/controllers/node.js
+++ framework/src/modules/http_api/controllers/node.js
@@ -141,7 +141,6 @@ NodeController.getStatus = async (context, next) => {
 			);
 
 		const data = {
-			broadhash: library.applicationState.broadhash,
 			consensus: consensus || 0,
 			currentTime: Date.now(),
 			secondsSinceEpoch,
