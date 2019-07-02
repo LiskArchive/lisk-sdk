73cca0d74f4b43dc6cceef8ca85b8799187ad6e9
Manu Nelamane Siddalingegowda <manu@lightcurve.io>
2019-07-02 15:24:27 +0200

    :bug: Check current version against upgrade version

--- commander/src/commands/core/upgrade.ts
+++ commander/src/commands/core/upgrade.ts
@@ -120,12 +120,12 @@ export default class UpgradeCommand extends BaseCommand {
 			{
 				title: 'Validate Version Input',
 				task: async () => {
-					await validateVersion(releaseUrl, upgradeVersion);
 					if (semver.lte(upgradeVersion, currentVersion)) {
 						throw new Error(
 							`Upgrade version:${upgradeVersion} should be greater than current version: ${currentVersion}`,
 						);
 					}
+					await validateVersion(releaseUrl, upgradeVersion);
 				},
 			},
 			{
