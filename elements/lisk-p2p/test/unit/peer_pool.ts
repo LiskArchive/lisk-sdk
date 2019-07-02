0d3e3dcea40096b3b7a9bdc920f5adc9ccb2e786
Ishan <ishantiw.quasar@gmail.com>
2019-07-02 11:39:09 +0200

    Add inbound/outbound unit tests for PeerPool

--- elements/lisk-p2p/test/unit/peer_pool.ts
+++ elements/lisk-p2p/test/unit/peer_pool.ts
@@ -23,7 +23,7 @@ import { Peer, ConnectionState } from '../../src/peer';
 import { initializePeerList } from '../utils/peers';
 import { P2PDiscoveredPeerInfo } from '../../src/p2p_types';
 
-describe('peerPool', () => {
+describe.only('peerPool', () => {
 	const peerPool = new PeerPool({
 		peerSelectionForConnection: selectPeersForConnection,
 		peerSelectionForRequest: selectPeersForRequest,
@@ -40,66 +40,302 @@ describe('peerPool', () => {
 	});
 
 	describe('#getConnectedPeers', () => {
-		const peerList: ReadonlyArray<Peer> = initializePeerList();
+		describe('when there are some active peers in inbound and outbound', () => {
+			const peerList: ReadonlyArray<Peer> = initializePeerList();
+			let activePeersInfoList: ReadonlyArray<P2PDiscoveredPeerInfo>;
 
-		let activePeersInfoList: ReadonlyArray<P2PDiscoveredPeerInfo>;
-		beforeEach(async () => {
-			sandbox.stub(peerList[0], 'state').get(() => {
-				return {
-					inbound: ConnectionState.CONNECTED,
-					outbound: ConnectionState.CONNECTED,
-				};
-			});
-			sandbox.stub(peerList[1], 'state').get(() => {
-				return {
-					inbound: ConnectionState.CONNECTED,
-					outbound: ConnectionState.CONNECTED,
-				};
-			});
-			activePeersInfoList = [peerList[0], peerList[1]].map(
-				peer => peer.peerInfo,
-			);
+			beforeEach(async () => {
+				sandbox.stub(peerList[0], 'state').get(() => {
+					return {
+						inbound: ConnectionState.CONNECTED,
+						outbound: ConnectionState.CONNECTED,
+					};
+				});
+				sandbox.stub(peerList[1], 'state').get(() => {
+					return {
+						inbound: ConnectionState.CONNECTED,
+						outbound: ConnectionState.CONNECTED,
+					};
+				});
+				sandbox.stub(peerList[2], 'state').get(() => {
+					return {
+						inbound: ConnectionState.DISCONNECTED,
+						outbound: ConnectionState.DISCONNECTED,
+					};
+				});
 
-			sandbox.stub(peerPool, 'getAllPeers').returns(peerList);
+				activePeersInfoList = [peerList[0], peerList[1]].map(
+					peer => peer.peerInfo,
+				);
+				sandbox.stub(peerPool, 'getAllPeers').returns(peerList);
+			});
+
+			it('should return active peers', async () => {
+				expect(peerPool.getConnectedPeers().map(peer => peer.peerInfo)).eql(
+					activePeersInfoList,
+				);
+			});
 		});
 
-		it('should return active peers', async () => {
-			expect(peerPool.getConnectedPeers().map(peer => peer.peerInfo)).eql(
-				activePeersInfoList,
-			);
+		describe('when there are some active peers only in inbound', () => {
+			const peerList: ReadonlyArray<Peer> = initializePeerList();
+			let activePeersInfoList: ReadonlyArray<P2PDiscoveredPeerInfo>;
+
+			beforeEach(async () => {
+				sandbox.stub(peerList[0], 'state').get(() => {
+					return {
+						inbound: ConnectionState.CONNECTED,
+						outbound: ConnectionState.DISCONNECTED,
+					};
+				});
+				sandbox.stub(peerList[1], 'state').get(() => {
+					return {
+						inbound: ConnectionState.CONNECTED,
+						outbound: ConnectionState.DISCONNECTED,
+					};
+				});
+				sandbox.stub(peerList[2], 'state').get(() => {
+					return {
+						inbound: ConnectionState.CONNECTED,
+						outbound: ConnectionState.DISCONNECTED,
+					};
+				});
+				sandbox.stub(peerList[3], 'state').get(() => {
+					return {
+						inbound: ConnectionState.DISCONNECTED,
+						outbound: ConnectionState.DISCONNECTED,
+					};
+				});
+
+				activePeersInfoList = [peerList[0], peerList[1], peerList[2]].map(
+					peer => peer.peerInfo,
+				);
+				sandbox.stub(peerPool, 'getAllPeers').returns(peerList);
+			});
+
+			it('should return active peers having inbound connection', async () => {
+				expect(peerPool.getConnectedPeers().map(peer => peer.peerInfo)).eql(
+					activePeersInfoList,
+				);
+			});
+		});
+
+		describe('when there are some active peers only in outbound', () => {
+			const peerList: ReadonlyArray<Peer> = initializePeerList();
+			let activePeersInfoList: ReadonlyArray<P2PDiscoveredPeerInfo>;
+
+			beforeEach(async () => {
+				sandbox.stub(peerList[0], 'state').get(() => {
+					return {
+						inbound: ConnectionState.DISCONNECTED,
+						outbound: ConnectionState.CONNECTED,
+					};
+				});
+				sandbox.stub(peerList[1], 'state').get(() => {
+					return {
+						inbound: ConnectionState.DISCONNECTED,
+						outbound: ConnectionState.CONNECTED,
+					};
+				});
+				sandbox.stub(peerList[2], 'state').get(() => {
+					return {
+						inbound: ConnectionState.DISCONNECTED,
+						outbound: ConnectionState.CONNECTED,
+					};
+				});
+				sandbox.stub(peerList[3], 'state').get(() => {
+					return {
+						inbound: ConnectionState.DISCONNECTED,
+						outbound: ConnectionState.DISCONNECTED,
+					};
+				});
+
+				activePeersInfoList = [peerList[0], peerList[1], peerList[2]].map(
+					peer => peer.peerInfo,
+				);
+				sandbox.stub(peerPool, 'getAllPeers').returns(peerList);
+			});
+
+			it('should return active peers having outbound connection', async () => {
+				expect(peerPool.getConnectedPeers().map(peer => peer.peerInfo)).eql(
+					activePeersInfoList,
+				);
+			});
+		});
+
+		describe('when there are no active peers', () => {
+			const peerList: ReadonlyArray<Peer> = initializePeerList();
+
+			beforeEach(async () => {
+				peerList.forEach(peer =>
+					sandbox.stub(peer, 'state').get(() => {
+						return {
+							inbound: ConnectionState.DISCONNECTED,
+							outbound: ConnectionState.DISCONNECTED,
+						};
+					}),
+				);
+
+				sandbox.stub(peerPool, 'getAllPeers').returns(peerList);
+			});
+
+			it('should return an empty array', async () => {
+				expect(peerPool.getConnectedPeers().map(peer => peer.peerInfo)).eql([]);
+			});
 		});
 	});
 
 	describe('#getAllConnectedPeerInfos', () => {
-		const peerList: ReadonlyArray<Peer> = initializePeerList();
+		describe('when there are some active peers in inbound and outbound', () => {
+			const peerList: ReadonlyArray<Peer> = initializePeerList();
+			let activePeersInfoList: ReadonlyArray<P2PDiscoveredPeerInfo>;
 
-		let activePeersInfoList: ReadonlyArray<P2PDiscoveredPeerInfo>;
-		beforeEach(async () => {
-			sandbox.stub(peerList[0], 'state').get(() => {
-				return {
-					inbound: ConnectionState.CONNECTED,
-					outbound: ConnectionState.CONNECTED,
-				};
-			});
-			sandbox.stub(peerList[1], 'state').get(() => {
-				return {
-					inbound: ConnectionState.CONNECTED,
-					outbound: ConnectionState.CONNECTED,
-				};
+			beforeEach(async () => {
+				sandbox.stub(peerList[0], 'state').get(() => {
+					return {
+						inbound: ConnectionState.CONNECTED,
+						outbound: ConnectionState.CONNECTED,
+					};
+				});
+				sandbox.stub(peerList[1], 'state').get(() => {
+					return {
+						inbound: ConnectionState.CONNECTED,
+						outbound: ConnectionState.CONNECTED,
+					};
+				});
+				sandbox.stub(peerList[2], 'state').get(() => {
+					return {
+						inbound: ConnectionState.DISCONNECTED,
+						outbound: ConnectionState.DISCONNECTED,
+					};
+				});
+
+				sandbox
+					.stub(peerPool, 'getConnectedPeers')
+					.returns(
+						peerList.filter(
+							peer =>
+								peer.state.inbound === ConnectionState.CONNECTED ||
+								peer.state.outbound === ConnectionState.CONNECTED,
+						),
+					);
+				activePeersInfoList = [peerList[0].peerInfo, peerList[1].peerInfo];
 			});
 
-			sandbox
-				.stub(peerPool, 'getConnectedPeers')
-				.returns(
-					peerList.filter(
-						peer => peer.state.inbound === 1 || peer.state.outbound === 1,
-					),
-				);
-			activePeersInfoList = [peerList[0].peerInfo, peerList[1].peerInfo];
+			it('should returns list of peerInfos of active peers', async () => {
+				expect(peerPool.getAllConnectedPeerInfos()).eql(activePeersInfoList);
+			});
 		});
 
-		it('should returns list of peerInfos of active peers', async () => {
-			expect(peerPool.getAllConnectedPeerInfos()).eql(activePeersInfoList);
+		describe('when there are some active peers in inbound only', () => {
+			const peerList: ReadonlyArray<Peer> = initializePeerList();
+			let activePeersInfoList: ReadonlyArray<P2PDiscoveredPeerInfo>;
+
+			beforeEach(async () => {
+				sandbox.stub(peerList[0], 'state').get(() => {
+					return {
+						inbound: ConnectionState.CONNECTED,
+						outbound: ConnectionState.DISCONNECTED,
+					};
+				});
+				sandbox.stub(peerList[1], 'state').get(() => {
+					return {
+						inbound: ConnectionState.CONNECTED,
+						outbound: ConnectionState.DISCONNECTED,
+					};
+				});
+				sandbox.stub(peerList[2], 'state').get(() => {
+					return {
+						inbound: ConnectionState.DISCONNECTED,
+						outbound: ConnectionState.DISCONNECTED,
+					};
+				});
+
+				sandbox
+					.stub(peerPool, 'getConnectedPeers')
+					.returns(
+						peerList.filter(
+							peer =>
+								peer.state.inbound === ConnectionState.CONNECTED ||
+								peer.state.outbound === ConnectionState.CONNECTED,
+						),
+					);
+				activePeersInfoList = [peerList[0].peerInfo, peerList[1].peerInfo];
+			});
+
+			it('should returns list of peerInfos of active peers only in inbound', async () => {
+				expect(peerPool.getAllConnectedPeerInfos()).eql(activePeersInfoList);
+			});
+		});
+
+		describe('when there are some active peers in outbound only', () => {
+			const peerList: ReadonlyArray<Peer> = initializePeerList();
+			let activePeersInfoList: ReadonlyArray<P2PDiscoveredPeerInfo>;
+
+			beforeEach(async () => {
+				sandbox.stub(peerList[0], 'state').get(() => {
+					return {
+						inbound: ConnectionState.DISCONNECTED,
+						outbound: ConnectionState.CONNECTED,
+					};
+				});
+				sandbox.stub(peerList[1], 'state').get(() => {
+					return {
+						inbound: ConnectionState.DISCONNECTED,
+						outbound: ConnectionState.CONNECTED,
+					};
+				});
+				sandbox.stub(peerList[2], 'state').get(() => {
+					return {
+						inbound: ConnectionState.DISCONNECTED,
+						outbound: ConnectionState.DISCONNECTED,
+					};
+				});
+
+				sandbox
+					.stub(peerPool, 'getConnectedPeers')
+					.returns(
+						peerList.filter(
+							peer =>
+								peer.state.inbound === ConnectionState.CONNECTED ||
+								peer.state.outbound === ConnectionState.CONNECTED,
+						),
+					);
+				activePeersInfoList = [peerList[0].peerInfo, peerList[1].peerInfo];
+			});
+
+			it('should returns list of peerInfos of active peers only in outbound', async () => {
+				expect(peerPool.getAllConnectedPeerInfos()).eql(activePeersInfoList);
+			});
+		});
+
+		describe('when there are no active peers', () => {
+			const peerList: ReadonlyArray<Peer> = initializePeerList();
+
+			beforeEach(async () => {
+				peerList.forEach(peer =>
+					sandbox.stub(peer, 'state').get(() => {
+						return {
+							inbound: ConnectionState.DISCONNECTED,
+							outbound: ConnectionState.DISCONNECTED,
+						};
+					}),
+				);
+
+				sandbox
+					.stub(peerPool, 'getConnectedPeers')
+					.returns(
+						peerList.filter(
+							peer =>
+								peer.state.inbound === ConnectionState.CONNECTED ||
+								peer.state.outbound === ConnectionState.CONNECTED,
+						),
+					);
+			});
+
+			it('should return an empty array', async () => {
+				expect(peerPool.getAllConnectedPeerInfos()).eql([]);
+			});
 		});
 	});
 });
