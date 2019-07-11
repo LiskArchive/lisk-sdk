89f918aa85e630ae6680dc951174a481746de154
Daniel Limia <limiaspasdaniel@gmail.com>
2019-07-11 14:21:18 +0200

    Prevent throwing error when is active=true and receiveBlockFromNetwork is triggered

--- framework/src/modules/chain/blocks/blocks.js
+++ framework/src/modules/chain/blocks/blocks.js
@@ -313,7 +313,12 @@ class Blocks extends EventEmitter {
 	// Process a block from the P2P
 	async receiveBlockFromNetwork(block) {
 		return this.sequence.add(async () => {
-			this._shouldNotBeActive();
+			if (this._isActive) {
+				this.logger.info(
+					'Ignoring received block from network, node is syncing or forging'
+				);
+				return;
+			}
 			this._isActive = true;
 			// set active to true
 			if (this.blocksVerify.isSaneBlock(block, this._lastBlock)) {
