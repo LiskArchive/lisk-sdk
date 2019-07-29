ac0f121b6b6f7e59d1bf532dd49aa331c47f9b5e
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2019-07-29 20:35:02 +0200

    Only send back tried peers when peer requests our peer list

--- elements/lisk-p2p/src/p2p.ts
+++ elements/lisk-p2p/src/p2p.ts
@@ -642,7 +642,6 @@ export class P2P extends EventEmitter {
 		count: number,
 	): ReadonlyArray<P2PDiscoveredPeerInfo> {
 		const discoveredPeerList: ReadonlyArray<P2PDiscoveredPeerInfo> = [
-			...this._newPeers.values(),
 			...this._triedPeers.values(),
 		]; // Peers whose values has been updated atleast once.
 
