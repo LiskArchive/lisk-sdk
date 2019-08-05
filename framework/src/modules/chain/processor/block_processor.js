ab0385378065157dad03627310b0ddff785d2e04
shuse2 <shus.toda@gmail.com>
2019-08-05 19:15:46 +0200

    :seedling: Init commit for processor

---
+++ framework/src/modules/chain/processor/block_processor.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright © 2019 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+'use strict';
+
+const { Pipeline } = require('./pipeline');
+
+class BlockProcessor {
+	constructor() {
+		this.validate = new Pipeline();
+		this.verify = new Pipeline();
+		this.apply = new Pipeline();
+		this.undo = new Pipeline();
+	}
+}
+
+module.exports = {
+	BlockProcessor,
+};
