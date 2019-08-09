f39d73875ca0b8e63fdfdee1df995f62a584d9e6
Usman <usman@lightcurve.io>
2019-08-09 14:49:00 +0200

    create initial implementation of block_processor_v2

--- framework/src/modules/chain/block_processor_v2.js
+++ framework/src/modules/chain/block_processor_v2.js
@@ -14,11 +14,306 @@
 
 'use strict';
 
+const BigNum = require('@liskhq/bignum');
+const { validator } = require('@liskhq/lisk-validator');
+const {
+	BIG_ENDIAN,
+	hash,
+	signDataWithPrivateKey,
+	hexToBuffer,
+	intToBuffer,
+	LITTLE_ENDIAN,
+} = require('@liskhq/lisk-cryptography');
 const { BlockProcessor } = require('./processor');
+const { sortTransactions } = require('./transactions');
 
-class BlockProcessorV2 extends BlockProcessor {}
+const SIZE_INT32 = 4;
+const SIZE_INT64 = 8;
 
-BlockProcessorV2.VERSION = 2;
+const blockSchema = {
+	type: 'object',
+	properties: {
+		id: {
+			type: 'string',
+			format: 'id',
+			minLength: 1,
+			maxLength: 20,
+		},
+		height: {
+			type: 'integer',
+		},
+		maxHeightPreviouslyForged: {
+			type: 'integer',
+		},
+		prevotedConfirmedUptoHeight: {
+			type: 'integer',
+		},
+		blockSignature: {
+			type: 'string',
+			format: 'signature',
+		},
+		generatorPublicKey: {
+			type: 'string',
+			format: 'publicKey',
+		},
+		numberOfTransactions: {
+			type: 'integer',
+		},
+		payloadHash: {
+			type: 'string',
+			format: 'hex',
+		},
+		payloadLength: {
+			type: 'integer',
+		},
+		previousBlock: {
+			type: 'string',
+			format: 'id',
+			minLength: 1,
+			maxLength: 20,
+		},
+		timestamp: {
+			type: 'integer',
+		},
+		totalAmount: {
+			type: 'object',
+			format: 'amount',
+		},
+		totalFee: {
+			type: 'object',
+			format: 'amount',
+		},
+		reward: {
+			type: 'object',
+			format: 'amount',
+		},
+		transactions: {
+			type: 'array',
+			uniqueItems: true,
+		},
+		version: {
+			type: 'integer',
+			minimum: 0,
+		},
+	},
+	required: [
+		'blockSignature',
+		'generatorPublicKey',
+		'numberOfTransactions',
+		'payloadHash',
+		'payloadLength',
+		'timestamp',
+		'totalAmount',
+		'totalFee',
+		'reward',
+		'transactions',
+		'version',
+	],
+};
+
+/**
+ * Creates bytebuffer out of block data used for signatures.
+ *
+ * @param {block} block
+ * @throws {Error}
+ * @returns {!Array} Contents as an ArrayBuffer
+ * @todo Add description for the function and the params
+ */
+const getBytes = block => {
+	const blockVersionBuffer = intToBuffer(
+		block.version,
+		SIZE_INT32,
+		LITTLE_ENDIAN,
+	);
+
+	const timestampBuffer = intToBuffer(
+		block.timestamp,
+		SIZE_INT32,
+		LITTLE_ENDIAN,
+	);
+
+	const previousBlockBuffer = block.previousBlock
+		? intToBuffer(block.previousBlock, SIZE_INT64, BIG_ENDIAN)
+		: Buffer.alloc(SIZE_INT64);
+
+	const heightBuffer = intToBuffer(block.height, SIZE_INT32, LITTLE_ENDIAN);
+
+	const maxHeightPreviouslyForgedBuffer = intToBuffer(
+		block.maxHeightPreviouslyForged,
+		SIZE_INT32,
+		LITTLE_ENDIAN,
+	);
+
+	const prevotedConfirmedUptoHeightBuffer = intToBuffer(
+		block.prevotedConfirmedUptoHeight,
+		SIZE_INT32,
+		LITTLE_ENDIAN,
+	);
+
+	const numTransactionsBuffer = intToBuffer(
+		block.numberOfTransactions,
+		SIZE_INT32,
+		LITTLE_ENDIAN,
+	);
+
+	const totalAmountBuffer = intToBuffer(
+		block.totalAmount.toString(),
+		SIZE_INT64,
+		LITTLE_ENDIAN,
+	);
+
+	const totalFeeBuffer = intToBuffer(
+		block.totalFee.toString(),
+		SIZE_INT64,
+		LITTLE_ENDIAN,
+	);
+
+	const rewardBuffer = intToBuffer(
+		block.reward.toString(),
+		SIZE_INT64,
+		LITTLE_ENDIAN,
+	);
+
+	const payloadLengthBuffer = intToBuffer(
+		block.payloadLength,
+		SIZE_INT32,
+		LITTLE_ENDIAN,
+	);
+
+	const payloadHashBuffer = hexToBuffer(block.payloadHash);
+
+	const generatorPublicKeyBuffer = hexToBuffer(block.generatorPublicKey);
+
+	const blockSignatureBuffer = block.blockSignature
+		? hexToBuffer(block.blockSignature)
+		: Buffer.alloc(0);
+
+	return Buffer.concat([
+		blockVersionBuffer,
+		timestampBuffer,
+		previousBlockBuffer,
+		heightBuffer,
+		maxHeightPreviouslyForgedBuffer,
+		prevotedConfirmedUptoHeightBuffer,
+		numTransactionsBuffer,
+		totalAmountBuffer,
+		totalFeeBuffer,
+		rewardBuffer,
+		payloadLengthBuffer,
+		payloadHashBuffer,
+		generatorPublicKeyBuffer,
+		blockSignatureBuffer,
+	]);
+};
+
+const validateSchema = ({ block }) => {
+	const errors = validator.validate(blockSchema, block);
+	if (errors.length) {
+		throw errors;
+	}
+};
+
+class BlockProcessorV2 extends BlockProcessor {
+	constructor({ blocksModule, logger, constants, exceptions }) {
+		super();
+		this.blocksModule = blocksModule;
+		this.logger = logger;
+		this.constants = constants;
+		this.exceptions = exceptions;
+
+		this.getBytes.pipe([({ block }) => getBytes(block)]);
+
+		this.validate.pipe([
+			this._validateVersion.bind(this),
+			validateSchema,
+			// validate BFT related data
+			this.blocksModule.validate.bind(this), // validate common block header
+		]);
+
+		this.fork.pipe([
+			this.blocksModule.forkChoice.bind(this), // validate common block header
+		]);
+
+		this.validateNew.pipe([this.blocksModule.validateNew.bind(this)]);
+
+		this.verify.pipe([this.blocksModule.verify.bind(this)]);
+
+		this.apply.pipe([this.blocksModule.apply.bind(this)]);
+
+		this.undo.pipe([this.blocksModule.undo.bind(this)]);
+
+		this.create.pipe([this._create.bind(this)]);
+	}
+
+	create({
+		blockReward,
+		transactions,
+		previousBlock,
+		keypair,
+		timestamp,
+		maxHeightPreviouslyForged,
+		prevotedConfirmedUptoHeight,
+	}) {
+		// TODO: move to transactions module logic
+		const sortedTransactions = sortTransactions(transactions);
+
+		const nextHeight = previousBlock ? previousBlock.height + 1 : 1;
+
+		const reward = blockReward.calculateReward(nextHeight);
+		let totalFee = new BigNum(0);
+		let totalAmount = new BigNum(0);
+		let size = 0;
+
+		const blockTransactions = [];
+		const transactionsBytesArray = [];
+
+		// eslint-disable-next-line no-plusplus
+		for (let i = 0; i < sortedTransactions.length; i++) {
+			const transaction = sortedTransactions[i];
+			const transactionBytes = transaction.getBytes(transaction);
+
+			if (size + transactionBytes.length > this.constants.maxPayloadLength) {
+				break;
+			}
+
+			size += transactionBytes.length;
+
+			totalFee = totalFee.plus(transaction.fee);
+			totalAmount = totalAmount.plus(transaction.amount);
+
+			blockTransactions.push(transaction);
+			transactionsBytesArray.push(transactionBytes);
+		}
+
+		const transactionsBuffer = Buffer.concat(transactionsBytesArray);
+		const payloadHash = hash(transactionsBuffer).toString('hex');
+
+		const block = {
+			version: BlockProcessorV2.VERSION,
+			totalAmount,
+			totalFee,
+			reward,
+			payloadHash,
+			timestamp,
+			numberOfTransactions: blockTransactions.length,
+			payloadLength: size,
+			previousBlock: previousBlock.id,
+			generatorPublicKey: keypair.publicKey.toString('hex'),
+			transactions: blockTransactions,
+			height: nextHeight,
+			maxHeightPreviouslyForged,
+			prevotedConfirmedUptoHeight,
+		};
+
+		return {
+			...block,
+			blockSignature: signDataWithPrivateKey(
+				hash(getBytes(block)),
+				keypair.privateKey,
+			),
+		};
+	}
+}
 
 module.exports = {
 	BlockProcessorV2,
