63854bccd9b9667e4dfd159d8ae0b060ef818222
Pablo Vicente <pvicente@gmail.com>
2019-08-26 17:01:05 +0200

    Add initial implementation of second passphrase generator. Fix some bugs in scripts

--- protocol-specs/generators/base_generator.js
+++ protocol-specs/generators/base_generator.js
@@ -24,7 +24,7 @@ const runGenerator = (generatorName, suiteCreators) => {
 
 		const dirPath = path.join(
 			__dirname,
-			`./generator_outputs/${suiteResult.runner}`,
+			`../generator_outputs/${suiteResult.runner}`,
 		); // TODO: remove hard path and maybe expose it to a CLI
 
 		if (!fs.existsSync(dirPath)) {
