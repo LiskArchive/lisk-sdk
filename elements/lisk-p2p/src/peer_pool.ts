4458a8521dab24420ca41de473e1eaf9c0432c9c
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2019-08-30 12:59:34 +0200

    Improve the creation of peer configs in peer pool

--- elements/lisk-p2p/src/peer_pool.ts
+++ elements/lisk-p2p/src/peer_pool.ts
@@ -60,6 +60,7 @@ import {
 	InboundPeer,
 	OutboundPeer,
 	Peer,
+	PeerConfig,
 } from './peer';
 import { getUniquePeersbyIp } from './peer_selection';
 import { constructPeerIdFromPeerInfo } from './utils';
@@ -188,11 +189,37 @@ export class PeerPool extends EventEmitter {
 	private readonly _peerSelectForConnection: P2PPeerSelectionForConnectionFunction;
 	private readonly _sendPeerLimit: number;
 	private readonly _outboundShuffleIntervalId: NodeJS.Timer | undefined;
+	private readonly _inboundPeerConfig: PeerConfig;
+	private readonly _outboundPeerConfig: PeerConfig;
 
 	public constructor(peerPoolConfig: PeerPoolConfig) {
 		super();
 		this._peerMap = new Map();
 		this._peerPoolConfig = peerPoolConfig;
+		this._inboundPeerConfig = {
+			connectTimeout: this._peerPoolConfig.connectTimeout,
+			ackTimeout: this._peerPoolConfig.ackTimeout,
+			wsMaxMessageRate: this._peerPoolConfig.wsMaxMessageRate,
+			wsMaxMessageRatePenalty: this._peerPoolConfig.wsMaxMessageRatePenalty,
+			maxPeerDiscoveryResponseLength: this._peerPoolConfig
+				.maxPeerDiscoveryResponseLength,
+			maxPeerInfoSize: this._peerPoolConfig.maxPeerInfoSize,
+			rateCalculationInterval: this._peerPoolConfig.rateCalculationInterval,
+			secret: this._peerPoolConfig.secret,
+		};
+		this._outboundPeerConfig = {
+			connectTimeout: this._peerPoolConfig.connectTimeout,
+			ackTimeout: this._peerPoolConfig.ackTimeout,
+			banTime: this._peerPoolConfig.peerBanTime,
+			wsMaxMessageRate: this._peerPoolConfig.wsMaxMessageRate,
+			wsMaxMessageRatePenalty: this._peerPoolConfig.wsMaxMessageRatePenalty,
+			maxPeerDiscoveryResponseLength: this._peerPoolConfig
+				.maxPeerDiscoveryResponseLength,
+			rateCalculationInterval: this._peerPoolConfig.rateCalculationInterval,
+			wsMaxPayload: this._peerPoolConfig.wsMaxPayload,
+			maxPeerInfoSize: this._peerPoolConfig.maxPeerInfoSize,
+			secret: this._peerPoolConfig.secret,
+		};
 		this._peerSelectForSend = peerPoolConfig.peerSelectionForSend;
 		this._peerSelectForRequest = peerPoolConfig.peerSelectionForRequest;
 		this._peerSelectForConnection = peerPoolConfig.peerSelectionForConnection;
@@ -308,6 +335,14 @@ export class PeerPool extends EventEmitter {
 
 	public get nodeInfo(): P2PNodeInfo | undefined {
 		return this._nodeInfo;
+	}
+
+	public get outboundPeerConfig(): PeerConfig {
+		return { ...this._outboundPeerConfig };
+	}
+
+	public get inboundPeerConfig(): PeerConfig {
+		return { ...this._inboundPeerConfig };
 	}
 
 	public async request(packet: P2PRequestPacket): Promise<P2PResponsePacket> {
@@ -428,18 +463,9 @@ export class PeerPool extends EventEmitter {
 			this._evictPeer(InboundPeer);
 		}
 
-		const peerConfig = {
-			connectTimeout: this._peerPoolConfig.connectTimeout,
-			ackTimeout: this._peerPoolConfig.ackTimeout,
-			wsMaxMessageRate: this._peerPoolConfig.wsMaxMessageRate,
-			wsMaxMessageRatePenalty: this._peerPoolConfig.wsMaxMessageRatePenalty,
-			maxPeerDiscoveryResponseLength: this._peerPoolConfig
-				.maxPeerDiscoveryResponseLength,
-			maxPeerInfoSize: this._peerPoolConfig.maxPeerInfoSize,
-			rateCalculationInterval: this._peerPoolConfig.rateCalculationInterval,
-			secret: this._peerPoolConfig.secret,
-		};
-		const peer = new InboundPeer(peerInfo, socket, peerConfig);
+		const peer = new InboundPeer(peerInfo, socket, {
+			...this._inboundPeerConfig,
+		});
 
 		// Throw an error because adding a peer multiple times is a common developer error which is very difficult to identify and debug.
 		if (this._peerMap.has(peer.id)) {
@@ -461,20 +487,7 @@ export class PeerPool extends EventEmitter {
 			return existingPeer;
 		}
 
-		const peerConfig = {
-			connectTimeout: this._peerPoolConfig.connectTimeout,
-			ackTimeout: this._peerPoolConfig.ackTimeout,
-			banTime: this._peerPoolConfig.peerBanTime,
-			wsMaxMessageRate: this._peerPoolConfig.wsMaxMessageRate,
-			wsMaxMessageRatePenalty: this._peerPoolConfig.wsMaxMessageRatePenalty,
-			maxPeerDiscoveryResponseLength: this._peerPoolConfig
-				.maxPeerDiscoveryResponseLength,
-			rateCalculationInterval: this._peerPoolConfig.rateCalculationInterval,
-			wsMaxPayload: this._peerPoolConfig.wsMaxPayload,
-			maxPeerInfoSize: this._peerPoolConfig.maxPeerInfoSize,
-			secret: this._peerPoolConfig.secret,
-		};
-		const peer = new OutboundPeer(peerInfo, peerConfig);
+		const peer = new OutboundPeer(peerInfo, { ...this._outboundPeerConfig });
 
 		this._peerMap.set(peer.id, peer);
 		this._bindHandlersToPeer(peer);
