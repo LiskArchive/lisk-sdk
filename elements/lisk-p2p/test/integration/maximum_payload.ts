c0a9543bf11fa6cc63eb1b4a8c1a1852b55c8d0a
Ishan <ishantiw.quasar@gmail.com>
2019-09-10 15:35:15 +0200

    Capture disconnect reason-code and update the test for maxPayload

--- elements/lisk-p2p/test/integration/maximum_payload.ts
+++ elements/lisk-p2p/test/integration/maximum_payload.ts
@@ -20,7 +20,7 @@ import { platform } from 'os';
 describe('Maximum payload', () => {
 	let p2pNodeList: ReadonlyArray<P2P> = [];
 	let collectedMessages: Array<any> = [];
-	let closedPeers: Map<number, any>;
+	let disconnectReasons: Array<any> = [];
 	let dataLargerThanMaxPayload: Array<string>;
 	const NETWORK_START_PORT = 5000;
 	const NETWORK_PEER_COUNT = 10;
@@ -34,11 +34,11 @@ describe('Maximum payload', () => {
 		for (let i = 0; i < 6000; i++) {
 			dataLargerThanMaxPayload.push(`message${i}`);
 		}
-		p2pNodeList = [...new Array(NETWORK_PEER_COUNT).keys()].map(index => {
+		p2pNodeList = [...new Array(NETWORK_PEER_COUNT / 2).keys()].map(index => {
 			const seedPeers = [
 				{
 					ipAddress: '127.0.0.1',
-					wsPort: NETWORK_START_PORT + ((index - 1) % NETWORK_PEER_COUNT),
+					wsPort: NETWORK_START_PORT + ((index - 2) % NETWORK_PEER_COUNT),
 				},
 			];
 
@@ -46,8 +46,8 @@ describe('Maximum payload', () => {
 
 			return new P2P({
 				blacklistedPeers: [],
-				connectTimeout: 200,
-				ackTimeout: 200,
+				connectTimeout: 500,
+				ackTimeout: 500,
 				seedPeers,
 				populatorInterval: 30,
 				maxOutboundConnections: 10,
@@ -72,9 +72,7 @@ describe('Maximum payload', () => {
 
 		await Promise.all(p2pNodeList.map(async p2p => await p2p.start()));
 		await wait(300);
-
 		collectedMessages = [];
-		closedPeers = new Map();
 		p2pNodeList.forEach(p2p => {
 			p2p.on('messageReceived', message => {
 				collectedMessages.push({
@@ -84,21 +82,19 @@ describe('Maximum payload', () => {
 			});
 
 			p2p.on('closeInbound', packet => {
-				let peers = [];
-				if (closedPeers.has(p2p.nodeInfo.wsPort)) {
-					peers = closedPeers.get(p2p.nodeInfo.wsPort);
-				}
-				peers.push(packet.peerInfo);
-				closedPeers.set(p2p.nodeInfo.wsPort, peers);
+				disconnectReasons.push({
+					peerPort: packet.peerInfo.wsPort,
+					code: packet.code,
+					reason: packet.reason,
+				});
 			});
 
 			p2p.on('closeOutbound', packet => {
-				let peers = [];
-				if (closedPeers.has(p2p.nodeInfo.wsPort)) {
-					peers = closedPeers.get(p2p.nodeInfo.wsPort);
-				}
-				peers.push(packet.peerInfo);
-				closedPeers.set(p2p.nodeInfo.wsPort, peers);
+				disconnectReasons.push({
+					peerPort: packet.peerInfo.wsPort,
+					code: packet.code,
+					reason: packet.reason,
+				});
 			});
 		});
 	});
@@ -133,20 +129,20 @@ describe('Maximum payload', () => {
 
 		await wait(300);
 
-		const firstPeerDisconnectedList =
-			closedPeers.get(firstP2PNode.nodeInfo.wsPort) || [];
-		for (const p2pNode of p2pNodeList) {
-			const disconnectedList = closedPeers.get(p2pNode.nodeInfo.wsPort) || [];
-			const wasFirstPeerDisconnected =
-				disconnectedList.some((peerInfo: any) => peerInfo.wsPort === 5000) ||
-				firstPeerDisconnectedList.some(
-					(peerInfo: any) => peerInfo.wsPort === p2pNode.nodeInfo.wsPort,
-				);
-			if (p2pNode.nodeInfo.wsPort === 5000) {
-				expect(disconnectedList.length).to.be.gte(9);
-			} else {
-				expect(wasFirstPeerDisconnected).to.be.true;
-			}
-		}
+		const disconnectMaxPayload = disconnectReasons.filter(
+			packet =>
+				packet.reason === 'Message was too big to process' &&
+				packet.code === 1009 &&
+				packet.peerPort === 5000,
+		);
+
+		expect(disconnectMaxPayload).length.gt(0);
+		expect(disconnectMaxPayload[0])
+			.is.an('object')
+			.has.property('code')
+			.eql(1009);
+		expect(disconnectMaxPayload[0])
+			.has.property('reason')
+			.eql('Message was too big to process');
 	});
 });
