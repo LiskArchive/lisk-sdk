eb72db2f10376ba6a29e762aab74a237882ec71b
Ishan <ishantiw.quasar@gmail.com>
2019-09-13 13:44:01 +0200

    Declare all the constants in p2p config for network creation

--- elements/lisk-p2p/test/integration/setup.ts
+++ elements/lisk-p2p/test/integration/setup.ts
@@ -20,8 +20,24 @@ export const NETWORK_PEER_COUNT = 10;
 export const POPULATOR_INTERVAL = 50;
 export const DEFAULT_MAX_OUTBOUND_CONNECTIONS = 20;
 export const DEFAULT_MAX_INBOUND_CONNECTIONS = 100;
+export const DEFAULT_CONNECTION_TIMEOUT = 100;
+export const DEFAULT_ACK_TIMEOUT = 200;
+export const RATE_CALCULATION_INTERVAL = 10000;
+export const WEB_SOCKET_ENGINE = 'ws';
+export const SEED_PEER_IP = '127.0.0.1';
 
-export interface P2PTestConfig {
+export const nodeInfoConstants = {
+	nethash: 'da3ed6a45429278bac2666961289ca17ad86595d33b31037615d4b8e8f158bba',
+	version: '1.0.1',
+	protocolVersion: '1.1',
+	minVersion: '1.0.0',
+	os: platform(),
+	height: 0,
+	broadhash: '2768b267ae621a9ed3b3034e2e8a1bed40895c621bbb1bbd613d92b9d24e54b5',
+	nonce: `O2wTkjqplHII`,
+};
+
+interface TestNetworkConfig {
 	networkSize?: number;
 	startNodePort?: number;
 	customConfig?: (
@@ -47,7 +63,7 @@ export const createNetwork = async ({
 	customConfig,
 	customNodeInfo,
 	customSeedPeers,
-}: P2PTestConfig) => {
+}: TestNetworkConfig) => {
 	const numberOfPeers = networkSize ? networkSize : NETWORK_PEER_COUNT;
 	const startPort = startNodePort ? startNodePort : NETWORK_START_PORT;
 
@@ -59,7 +75,7 @@ export const createNetwork = async ({
 			? []
 			: [
 					{
-						ipAddress: '127.0.0.1',
+						ipAddress: SEED_PEER_IP,
 						wsPort: NETWORK_START_PORT + index - 1,
 					},
 			  ];
@@ -73,26 +89,24 @@ export const createNetwork = async ({
 			: {};
 
 		const p2pConfig = {
-			connectTimeout: 100,
-			ackTimeout: 200,
-			rateCalculationInterval: 10000,
+			connectTimeout: DEFAULT_CONNECTION_TIMEOUT,
+			ackTimeout: DEFAULT_ACK_TIMEOUT,
+			rateCalculationInterval: RATE_CALCULATION_INTERVAL,
 			seedPeers,
-			wsEngine: 'ws',
+			wsEngine: WEB_SOCKET_ENGINE,
 			populatorInterval: POPULATOR_INTERVAL,
 			maxOutboundConnections: DEFAULT_MAX_OUTBOUND_CONNECTIONS,
 			maxInboundConnections: DEFAULT_MAX_INBOUND_CONNECTIONS,
 			nodeInfo: {
 				wsPort: nodePort,
-				nethash:
-					'da3ed6a45429278bac2666961289ca17ad86595d33b31037615d4b8e8f158bba',
-				version: '1.0.1',
-				protocolVersion: '1.1',
-				minVersion: '1.0.0',
-				os: platform(),
-				height: 0,
-				broadhash:
-					'2768b267ae621a9ed3b3034e2e8a1bed40895c621bbb1bbd613d92b9d24e54b5',
-				nonce: `O2wTkjqplHII${nodePort}`,
+				nethash: nodeInfoConstants.nethash,
+				version: nodeInfoConstants.version,
+				protocolVersion: nodeInfoConstants.protocolVersion,
+				minVersion: nodeInfoConstants.minVersion,
+				os: nodeInfoConstants.os,
+				height: nodeInfoConstants.height,
+				broadhash: nodeInfoConstants.broadhash,
+				nonce: `${nodeInfoConstants.nonce}${nodePort}`,
 				...customNodeInfoObject,
 			},
 			...customConfigObject,
