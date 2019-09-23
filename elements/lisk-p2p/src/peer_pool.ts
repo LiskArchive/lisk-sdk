6a0132817506ee180b3f7b4050d08f101a9be879
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2019-09-23 09:58:25 +0200

    Simplify import

--- elements/lisk-p2p/src/peer_pool.ts
+++ elements/lisk-p2p/src/peer_pool.ts
@@ -72,7 +72,7 @@ import {
 	Peer,
 	PeerConfig,
 } from './peer';
-import { constructPeerIdFromPeerInfo, getUniquePeersbyIp } from './utils/misc';
+import { constructPeerIdFromPeerInfo, getUniquePeersbyIp } from './utils';
 
 interface FilterPeersOptions {
 	readonly category: PROTECTION_CATEGORY;
