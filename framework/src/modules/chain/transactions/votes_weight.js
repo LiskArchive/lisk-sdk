b84c06719a9d3fdf7dad1dbdbf0068a1690561ed
Pablo Vicente <pvicente@gmail.com>
2019-10-15 12:26:25 +0200

    Change default for amount from string to number

--- framework/src/modules/chain/transactions/votes_weight.js
+++ framework/src/modules/chain/transactions/votes_weight.js
@@ -124,7 +124,7 @@ const updateSenderDelegateVotes = (
 	undo = false,
 ) => {
 	// use the ammount or default to zero as LIP-0012 removes the 'amount' property from all transactions but transfer
-	const amount = transaction.fee.plus(transaction.asset.amount || '0');
+	const amount = transaction.fee.plus(transaction.asset.amount || 0);
 	const method = undo ? 'add' : 'sub';
 	const senderAccount = stateStore.account.getOrDefault(transaction.senderId);
 	let votedDelegatesPublicKeys = senderAccount.votedDelegatesPublicKeys || [];
