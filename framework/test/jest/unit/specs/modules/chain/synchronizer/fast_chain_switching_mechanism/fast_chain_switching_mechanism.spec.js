85285af1b1b7876feb76305537ed3988b0d76d13
Daniel Limia <limiaspasdaniel@gmail.com>
2019-10-25 11:12:22 +0200

    Start testing run method of FCS

--- framework/test/jest/unit/specs/modules/chain/synchronizer/fast_chain_switching_mechanism/fast_chain_switching_mechanism.spec.js
+++ framework/test/jest/unit/specs/modules/chain/synchronizer/fast_chain_switching_mechanism/fast_chain_switching_mechanism.spec.js
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2018 Lisk Foundation
+ * Copyright © 2019 Lisk Foundation
  *
  * See the LICENSE file at the top-level directory of this distribution
  * for licensing information.
@@ -14,150 +14,333 @@
 
 'use strict';
 
+const { when } = require('jest-when');
+const { Blocks } = require('../../../../../../../../src/modules/chain/blocks');
+const { BFT } = require('../../../../../../../../src/modules/chain/bft');
 const {
+	BlockProcessorV2,
+} = require('../../../../../../../../src/modules/chain/block_processor_v2');
+const {
+	Synchronizer,
 	FastChainSwitchingMechanism,
-} = require('../../../../../../../../src/modules/chain/synchronizer/fast_chain_switching_mechanism');
+} = require('../../../../../../../../src/modules/chain/synchronizer');
+const { Slots } = require('../../../../../../../../src/modules/chain/dpos');
 const {
-	Block: blockFixture,
-} = require('../../../../../../../mocha/fixtures/blocks');
+	Sequence,
+} = require('../../../../../../../../src/modules/chain/utils/sequence');
+const {
+	Processor,
+} = require('../../../../../../../../src/modules/chain/processor');
+const { constants } = require('../../../../../../utils');
+const { newBlock } = require('../../../chain/blocks/utils');
+const synchronizerUtils = require('../../../../../../../../src/modules/chain/synchronizer/utils');
+
+const genesisBlockDevnet = require('../../../../../../../fixtures/config/devnet/genesis_block');
+
+const ChannelMock = jest.genMockFromModule(
+	'../../../../../../../../src/controller/channels/in_memory_channel',
+);
+
+// describe.skip('fast_chain_switching_mechanism', () => {
+// 	afterEach(async () => {
+// 		jest.clearAllMocks();
+// 	});
+//
+// 	describe('FastChainSwitchingMechanism', () => {
+// 		const activeDelegates = 101;
+// 		const channelMock = { invoke: jest.fn() };
+//
+// 		const lastBlockGetterMock = jest.fn();
+// 		const blocksMock = {};
+// 		Object.defineProperty(blocksMock, 'lastBlock', {
+// 			get: lastBlockGetterMock,
+// 		});
+//
+// 		const storageMock = {
+// 			entities: {
+// 				Block: {
+// 					getLastBlock: jest.fn(),
+// 				},
+// 			},
+// 		};
+// 		const dposMock = {
+// 			getForgerPublicKeysForRound: jest.fn().mockReturnValue([]),
+// 		};
+// 		const slotsMock = {
+// 			getSlotNumber: jest.fn(),
+// 			calcRound: jest.fn(),
+// 		};
+// 		const syncParams = {
+// 			channel: channelMock,
+// 			blocks: blocksMock,
+// 			storage: storageMock,
+// 			slots: slotsMock,
+// 			dpos: dposMock,
+// 			activeDelegates,
+// 		};
+//
+// 		let syncMechanism;
+//
+// 		beforeEach(() => {
+// 			syncMechanism = new FastChainSwitchingMechanism(syncParams);
+// 		});
+//
+// 		describe('#constructor', () => {
+// 			it('should create instance of FastChainSwitchingMechanism', async () => {
+// 				expect(syncMechanism).toBeInstanceOf(FastChainSwitchingMechanism);
+// 			});
+//
+// 			it('should assign dependencies', async () => {
+// 				expect(syncMechanism.storage).toBe(syncParams.storage);
+// 				expect(syncMechanism.logger).toBe(syncParams.logger);
+// 				expect(syncMechanism.slots).toBe(syncParams.slots);
+// 				expect(syncMechanism.blocks).toBe(syncParams.blocks);
+// 				expect(syncMechanism.dpos).toBe(syncParams.dpos);
+// 				expect(syncMechanism.constants).toEqual({
+// 					activeDelegates,
+// 				});
+// 				expect(syncMechanism.active).toBeFalsy();
+// 			});
+// 		});
+//
+// 		describe('async isValidFor()', () => {
+// 			const lastBlockHeight = 200;
+// 			const finalizedBlockHeight = 100;
+// 			const lastBlock = blockFixture({ height: lastBlockHeight });
+// 			const finalizedBlock = blockFixture({ height: finalizedBlockHeight });
+//
+// 			beforeEach(async () => {
+// 				lastBlockGetterMock.mockReturnValue(lastBlock);
+// 			});
+//
+// 			it('should get the last block from blocks module', async () => {
+// 				const receivedBlock = blockFixture({ height: lastBlockHeight + 1 });
+// 				await syncMechanism.isValidFor(receivedBlock);
+//
+// 				expect(lastBlockGetterMock).toHaveBeenCalledTimes(1);
+// 			});
+//
+// 			it('should return false if gap between received block and last block is more than two rounds', async () => {
+// 				const currentSlot = 500;
+// 				const finalizedBlockSlot = 200; // Within three rounds
+// 				const receivedBlockHeight = lastBlockHeight + 203; // Ahead more than 2 rounds
+// 				const receivedBlock = blockFixture({ height: receivedBlockHeight });
+// 				slotsMock.getSlotNumber.mockImplementation(timestamp => {
+// 					if (timestamp === finalizedBlock.timestamp) {
+// 						return finalizedBlockSlot;
+// 					}
+//
+// 					return currentSlot;
+// 				});
+//
+// 				const result = await syncMechanism.isValidFor(receivedBlock);
+//
+// 				expect(result).toBeFalsy();
+// 				expect(slotsMock.calcRound).not.toHaveBeenCalled();
+// 			});
+//
+// 			it('should return true if received block delegate is part of that round delegate list', async () => {
+// 				const currentSlot = 500;
+// 				const finalizedBlockSlot = 200; // Within three rounds
+// 				const receivedBlockHeight = lastBlockHeight + 180; // Within two rounds
+// 				const receivedBlock = blockFixture({ height: receivedBlockHeight });
+// 				slotsMock.getSlotNumber.mockImplementation(timestamp => {
+// 					if (timestamp === finalizedBlock.timestamp) {
+// 						return finalizedBlockSlot;
+// 					}
+//
+// 					return currentSlot;
+// 				});
+// 				dposMock.getForgerPublicKeysForRound.mockReturnValue([
+// 					receivedBlock.generatorPublicKey,
+// 				]);
+//
+// 				const result = await syncMechanism.isValidFor(receivedBlock);
+//
+// 				expect(result).toBeTruthy();
+// 			});
+//
+// 			it('should return false if received block delegate is not part of that round delegate list', async () => {
+// 				const currentSlot = 500;
+// 				const finalizedBlockSlot = 200; // Within three rounds
+// 				const receivedBlockHeight = lastBlockHeight + 180; // Within two rounds
+// 				const receivedBlock = blockFixture({ height: receivedBlockHeight });
+// 				slotsMock.getSlotNumber.mockImplementation(timestamp => {
+// 					if (timestamp === finalizedBlock.timestamp) {
+// 						return finalizedBlockSlot;
+// 					}
+//
+// 					return currentSlot;
+// 				});
+// 				dposMock.getForgerPublicKeysForRound.mockReturnValue([]);
+//
+// 				const result = await syncMechanism.isValidFor(receivedBlock);
+//
+// 				expect(result).toBeFalsy();
+// 			});
+// 		});
+// 	});
+// });
 
 describe('fast_chain_switching_mechanism', () => {
-	afterEach(async () => {
-		jest.clearAllMocks();
-	});
+	let bftModule;
+	let blockProcessorV2;
+	let blocksModule;
+	let processorModule;
+	let fastChainSwitchingMechanism;
+	let slots;
 
-	describe('FastChainSwitchingMechanism', () => {
-		const activeDelegates = 101;
-		const channelMock = { invoke: jest.fn() };
+	let channelMock;
+	let dposModuleMock;
+	let exceptions;
+	let loggerMock;
+	let storageMock;
 
-		const lastBlockGetterMock = jest.fn();
-		const blocksMock = {};
-		Object.defineProperty(blocksMock, 'lastBlock', {
-			get: lastBlockGetterMock,
-		});
-
-		const storageMock = {
+	beforeEach(() => {
+		loggerMock = {
+			info: jest.fn(),
+			debug: jest.fn(),
+			error: jest.fn(),
+		};
+		storageMock = {
 			entities: {
+				TempBlock: {
+					get: jest.fn(),
+					truncate: jest.fn(),
+					isEmpty: jest.fn(),
+				},
 				Block: {
-					getLastBlock: jest.fn(),
+					get: jest.fn(),
+					begin: jest.fn(),
+				},
+				Account: {
+					get: jest.fn(),
 				},
 			},
 		};
-		const dposMock = {
-			getForgerPublicKeysForRound: jest.fn().mockReturnValue([]),
-		};
-		const slotsMock = {
-			getSlotNumber: jest.fn(),
-			calcRound: jest.fn(),
-		};
-		const syncParams = {
-			channel: channelMock,
-			blocks: blocksMock,
+		channelMock = new ChannelMock();
+
+		slots = new Slots({
+			epochTime: constants.EPOCH_TIME,
+			interval: constants.BLOCK_TIME,
+			blocksPerRound: constants.ACTIVE_DELEGATES,
+		});
+
+		bftModule = new BFT({
 			storage: storageMock,
-			slots: slotsMock,
-			dpos: dposMock,
-			activeDelegates,
-		};
-
-		let syncMechanism;
-
-		beforeEach(() => {
-			syncMechanism = new FastChainSwitchingMechanism(syncParams);
+			logger: loggerMock,
+			activeDelegates: constants.ACTIVE_DELEGATES,
+			startingHeight: 1,
 		});
 
-		describe('#constructor', () => {
-			it('should create instance of FastChainSwitchingMechanism', async () => {
-				expect(syncMechanism).toBeInstanceOf(FastChainSwitchingMechanism);
-			});
+		blocksModule = new Blocks({
+			logger: loggerMock,
+			storage: storageMock,
+			slots,
+			genesisBlock: genesisBlockDevnet,
+			sequence: new Sequence(),
 
-			it('should assign dependencies', async () => {
-				expect(syncMechanism.storage).toBe(syncParams.storage);
-				expect(syncMechanism.logger).toBe(syncParams.logger);
-				expect(syncMechanism.slots).toBe(syncParams.slots);
-				expect(syncMechanism.blocks).toBe(syncParams.blocks);
-				expect(syncMechanism.dpos).toBe(syncParams.dpos);
-				expect(syncMechanism.constants).toEqual({
-					activeDelegates,
-				});
-				expect(syncMechanism.active).toBeFalsy();
-			});
+			blockReceiptTimeout: constants.BLOCK_RECEIPT_TIMEOUT,
+			loadPerIteration: 1000,
+			maxPayloadLength: constants.MAX_PAYLOAD_LENGTH,
+			maxTransactionsPerBlock: constants.MAX_TRANSACTIONS_PER_BLOCK,
+			activeDelegates: constants.ACTIVE_DELEGATES,
+			rewardDistance: constants.REWARDS.DISTANCE,
+			rewardOffset: constants.REWARDS.OFFSET,
+			rewardMileStones: constants.REWARDS.MILESTONES,
+			totalAmount: constants.TOTAL_AMOUNT,
+			blockSlotWindow: constants.BLOCK_SLOT_WINDOW,
 		});
 
-		describe('async isValidFor()', () => {
-			const lastBlockHeight = 200;
-			const finalizedBlockHeight = 100;
-			const lastBlock = blockFixture({ height: lastBlockHeight });
-			const finalizedBlock = blockFixture({ height: finalizedBlockHeight });
+		blockProcessorV2 = new BlockProcessorV2({
+			blocksModule,
+			bftModule,
+			dposModule: dposModuleMock,
+			logger: loggerMock,
+			constants,
+			exceptions,
+		});
 
-			beforeEach(async () => {
-				lastBlockGetterMock.mockReturnValue(lastBlock);
-			});
+		processorModule = new Processor({
+			channel: channelMock,
+			storage: storageMock,
+			blocksModule,
+			logger: loggerMock,
+		});
+		processorModule.processValidated = jest.fn();
+		processorModule.deleteLastBlock = jest.fn();
+		processorModule.register(blockProcessorV2);
 
-			it('should get the last block from blocks module', async () => {
-				const receivedBlock = blockFixture({ height: lastBlockHeight + 1 });
-				await syncMechanism.isValidFor(receivedBlock);
+		fastChainSwitchingMechanism = new FastChainSwitchingMechanism({
+			storage: storageMock,
+			logger: loggerMock,
+			channel: channelMock,
+			slots,
+			blocks: blocksModule,
+			bft: bftModule,
+			processor: processorModule,
+			dpos: dposModuleMock,
+			activeDelegates: constants.ACTIVE_DELEGATES,
+		});
+	});
 
-				expect(lastBlockGetterMock).toHaveBeenCalledTimes(1);
-			});
+	describe('async run()', () => {
+		const aPeer = '127.0.0.1:5000';
+		let aBlock;
 
-			it('should return false if gap between received block and last block is more than two rounds', async () => {
-				const currentSlot = 500;
-				const finalizedBlockSlot = 200; // Within three rounds
-				const receivedBlockHeight = lastBlockHeight + 203; // Ahead more than 2 rounds
-				const receivedBlock = blockFixture({ height: receivedBlockHeight });
-				slotsMock.getSlotNumber.mockImplementation(timestamp => {
-					if (timestamp === finalizedBlock.timestamp) {
-						return finalizedBlockSlot;
-					}
+		beforeEach(async () => {
+			aBlock = newBlock();
+			when(storageMock.entities.Block.begin)
+				.calledWith('loader:checkMemTables')
+				.mockResolvedValue({ genesisBlock: genesisBlockDevnet });
+			when(storageMock.entities.Account.get)
+				.calledWith({ isDelegate: true }, { limit: null })
+				.mockResolvedValue([{ publicKey: 'aPublicKey' }]);
+			await blocksModule.init();
+		});
 
-					return currentSlot;
-				});
+		it('should request the last common block with the given peer', async () => {
+			when(storageMock.entities.Block.get)
+				.calledWith({}, { sort: 'height:desc', limit: 1, extended: true })
+				.mockResolvedValue([genesisBlockDevnet]);
 
-				const result = await syncMechanism.isValidFor(receivedBlock);
+			when(storageMock.entities.Block.get)
+				.calledWith({
+					height_in: [1], //  We only have  genesis block present
+					sort: 'height:asc',
+				})
+				.mockResolvedValueOnce([genesisBlockDevnet.id]);
 
-				expect(result).toBeFalsy();
-				expect(slotsMock.calcRound).not.toHaveBeenCalled();
-			});
+			when(channelMock.invoke)
+				.calledWith('network:requestFromPeer', {
+					procedure: 'getHighestCommonBlock',
+					peerId: aPeer,
+					data: {
+						ids: [genesisBlockDevnet.id],
+					},
+				})
+				.mockResolvedValueOnce(genesisBlockDevnet);
 
-			it('should return true if received block delegate is part of that round delegate list', async () => {
-				const currentSlot = 500;
-				const finalizedBlockSlot = 200; // Within three rounds
-				const receivedBlockHeight = lastBlockHeight + 180; // Within two rounds
-				const receivedBlock = blockFixture({ height: receivedBlockHeight });
-				slotsMock.getSlotNumber.mockImplementation(timestamp => {
-					if (timestamp === finalizedBlock.timestamp) {
-						return finalizedBlockSlot;
-					}
+			await fastChainSwitchingMechanism.run(aBlock, aPeer);
 
-					return currentSlot;
-				});
-				dposMock.getForgerPublicKeysForRound.mockReturnValue([
-					receivedBlock.generatorPublicKey,
-				]);
+			expect(channelMock.invoke).toHaveBeenCalledWith(
+				'network:requestFromPeer',
+				{
+					procedure: 'getHighestCommonBlock',
+					peerId: aPeer,
+					data: {
+						ids: [genesisBlockDevnet.id],
+					},
+				},
+			);
 
-				const result = await syncMechanism.isValidFor(receivedBlock);
-
-				expect(result).toBeTruthy();
-			});
-
-			it('should return false if received block delegate is not part of that round delegate list', async () => {
-				const currentSlot = 500;
-				const finalizedBlockSlot = 200; // Within three rounds
-				const receivedBlockHeight = lastBlockHeight + 180; // Within two rounds
-				const receivedBlock = blockFixture({ height: receivedBlockHeight });
-				slotsMock.getSlotNumber.mockImplementation(timestamp => {
-					if (timestamp === finalizedBlock.timestamp) {
-						return finalizedBlockSlot;
-					}
-
-					return currentSlot;
-				});
-				dposMock.getForgerPublicKeysForRound.mockReturnValue([]);
-
-				const result = await syncMechanism.isValidFor(receivedBlock);
-
-				expect(result).toBeFalsy();
-			});
+			expect(loggerMock.debug).toHaveBeenCalledWith(
+				{
+					blockId: genesisBlockDevnet.id,
+					height: genesisBlockDevnet.height,
+				},
+				'Common block found',
+			);
 		});
 	});
 });
