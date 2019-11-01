ef8c93607a28a9685539c28741b5f50350aa02c7
Lucas Silvestre <lukas.silvestre@gmail.com>
2019-11-01 14:29:56 +0100

    Code refactoring

--- framework/src/modules/http_api/controllers/blocks.js
+++ framework/src/modules/http_api/controllers/blocks.js
@@ -22,7 +22,6 @@ const apiCodes = require('../api_codes');
 const swaggerHelper = require('../helpers/swagger');
 
 let library;
-let sortFields;
 
 /**
  * Parse raw block data from database into expected API response type for blocks
@@ -30,7 +29,7 @@ let sortFields;
  * @param {Object} raw Raw block data from database
  * @return {block} Block formatted according to API specification
  */
-function parseBlockFromDatabase(raw) {
+const _parseBlock = raw => {
 	if (!raw.id) {
 		return null;
 	}
@@ -61,33 +60,10 @@ function parseBlockFromDatabase(raw) {
 	}
 
 	return block;
-}
+};
 
-/**
- * Get filtered list of blocks (without transactions - BasicBlock).
- *
- * @private
- * @func _list
- * @param {Object} filter - Conditions to filter with
- * @param {string} filter.id - Block id
- * @param {string} filter.generatorPublicKey - Public key of delegate who generates the block
- * @param {number} filter.numberOfTransactions - Number of transactions
- * @param {string} filter.previousBlock - Previous block ID
- * @param {number} filter.height - Block height
- * @param {number} filter.totalAmount - Total amount of block's transactions
- * @param {number} filter.totalFee - Block total fees
- * @param {number} filter.reward - Block reward
- * @param {number} filter.limit - Limit of blocks to retrieve, default: 100, max: 100
- * @param {number} filter.offset - Offset from where to start
- * @param {string} filter.sort - Sort order, default: height:desc
- * @param {function} cb - Callback function
- * @returns {function} cb - Callback function from params (through setImmediate)
- * @returns {Object} cb.err - Error if occurred
- * @returns {Object} cb.data - List of normalized blocks
- */
-function _list(params, cb) {
-	const options = {};
-	const parsedFilters = {
+const _parseFilters = params => {
+	const filters = {
 		id: params.id,
 		generatorPublicKey: params.generatorPublicKey,
 		numberOfTransactions: params.numberOfTransactions,
@@ -100,52 +76,14 @@ function _list(params, cb) {
 		reward: params.reward,
 	};
 
-	// Remove filters with undefined/null values
-	const filters = _.omitBy(
-		parsedFilters,
-		value => value === undefined || value === null,
-	);
+	return _.omitBy(filters, value => value === undefined || value === null);
+};
 
-	options.limit = params.limit ? Math.abs(params.limit) : 100;
-	options.offset = params.offset ? Math.abs(params.offset) : 0;
-	options.sort = params.sort || 'height:desc';
-
-	if (options.limit > 100) {
-		return setImmediate(
-			cb,
-			new ApiError(
-				'Invalid limit. Maximum is 100',
-				apiCodes.INTERNAL_SERVER_ERROR,
-			),
-		);
-	}
-
-	const [sortField, sortMethod = 'ASC'] = options.sort.split(':');
-	if (
-		!sortFields.includes(sortField) ||
-		!['ASC', 'DESC'].includes(sortMethod.toUpperCase())
-	) {
-		return setImmediate(
-			cb,
-			new ApiError('Invalid sort field', apiCodes.INTERNAL_SERVER_ERROR),
-		);
-	}
-
-	return (
-		library.storage.entities.Block.get(filters, options)
-			// FIXME: Can have poor performance because it performs SHA256 hash calculation for each block
-			.then(async rows =>
-				setImmediate(cb, null, rows.map(parseBlockFromDatabase)),
-			)
-			.catch(err => {
-				library.logger.error(err.stack);
-				return setImmediate(
-					cb,
-					new ApiError('Blocks#list error', apiCodes.INTERNAL_SERVER_ERROR),
-				);
-			})
-	);
-}
+const _parseOptions = params => ({
+	limit: params.limit ? Math.abs(params.limit) : 100,
+	offset: params.offset ? Math.abs(params.offset) : 0,
+	sort: params.sort || 'height:desc',
+});
 
 /**
  * Description of the function.
@@ -162,18 +100,6 @@ function BlocksController(scope) {
 		logger: scope.components.logger,
 		channel: scope.channel,
 	};
-
-	sortFields = [
-		'id',
-		'timestamp',
-		'height',
-		'previousBlock',
-		'totalAmount',
-		'totalFee',
-		'reward',
-		'numberOfTransactions',
-		'generatorPublicKey',
-	];
 }
 
 /**
@@ -183,7 +109,7 @@ function BlocksController(scope) {
  * @param {function} next
  * @todo Add description for the function and the params
  */
-BlocksController.getBlocks = function(context, next) {
+BlocksController.getBlocks = async (context, next) => {
 	const invalidParams = swaggerHelper.invalidParams(context.request);
 
 	if (invalidParams.length) {
@@ -191,8 +117,7 @@ BlocksController.getBlocks = function(context, next) {
 	}
 
 	const { params } = context.request.swagger;
-
-	let parsedParams = {
+	const parsedParams = {
 		id: params.blockId.value,
 		height: params.height.value,
 		generatorPublicKey: params.generatorPublicKey.value,
@@ -203,25 +128,25 @@ BlocksController.getBlocks = function(context, next) {
 		offset: params.offset.value,
 	};
 
-	// Remove params with undefined/null values
-	parsedParams = _.omitBy(
-		parsedParams,
-		value => value === undefined || value === null,
-	);
+	const filters = _parseFilters(parsedParams);
+	const options = _parseOptions(parsedParams);
 
-	return _list(_.clone(parsedParams), (err, data) => {
-		if (err) {
-			return next(err);
-		}
+	try {
+		const blocks = await library.storage.entities.Block.get(filters, options);
 
 		return next(null, {
-			data,
+			data: blocks.map(_parseBlock),
 			meta: {
-				offset: parsedParams.offset,
-				limit: parsedParams.limit,
+				offset: options.offset,
+				limit: options.limit,
 			},
 		});
-	});
+	} catch (error) {
+		library.logger.error(error.stack);
+		return next(
+			new ApiError('Blocks#list error', apiCodes.INTERNAL_SERVER_ERROR),
+		);
+	}
 };
 
 module.exports = BlocksController;
