01ebbf6c880c3683f71c83bb565f189623c2a285
Ishan <ishantiw.quasar@gmail.com>
2019-11-08 15:04:17 +0100

    Add a counter for unwanted messages

--- elements/lisk-p2p/src/peer/base.ts
+++ elements/lisk-p2p/src/peer/base.ts
@@ -35,6 +35,7 @@ import {
 	EVENT_FAILED_PEER_INFO_UPDATE,
 	EVENT_FAILED_TO_FETCH_PEER_INFO,
 	EVENT_FAILED_TO_FETCH_PEERS,
+	EVENT_INBOUND_SOCKET_ERROR,
 	EVENT_INVALID_MESSAGE_RECEIVED,
 	EVENT_INVALID_REQUEST_RECEIVED,
 	EVENT_MESSAGE_RECEIVED,
@@ -70,6 +71,7 @@ export const socketErrorStatusCodes = {
 
 // Can be used to convert a rate which is based on the rateCalculationInterval into a per-second rate.
 const RATE_NORMALIZATION_FACTOR = 1000;
+const NUMBER_OF_INVALID_MESSAGES = 3;
 
 interface Productivity {
 	readonly requestCounter: number;
@@ -129,6 +131,7 @@ export class Peer extends EventEmitter {
 	protected _nodeInfo: P2PNodeInfo | undefined;
 	protected _wsMessageCount: number;
 	protected _wsMessageRate: number;
+	protected _invalidMessageCount: number;
 	protected _rateInterval: number;
 	protected readonly _handleRawRPC: (
 		packet: unknown,
@@ -155,6 +158,7 @@ export class Peer extends EventEmitter {
 		this._messageRates = new Map();
 		this._wsMessageCount = 0;
 		this._wsMessageRate = 0;
+		this._invalidMessageCount = 0;
 		this._rateInterval = this._peerConfig.rateCalculationInterval;
 		this._counterResetInterval = setInterval(() => {
 			this._resetCounters();
@@ -207,7 +211,28 @@ export class Peer extends EventEmitter {
 			this.emit(EVENT_REQUEST_RECEIVED, request);
 		};
 
-		this._handleWSMessage = () => {
+		this._handleWSMessage = (message: any) => {
+			if (message === SOCKET_PING_MESSAGE || message === SOCKET_PONG_MESSAGE) {
+				this._invalidMessageCount += 1;
+				// TODO: Determine the right number of exptected messages used in socket cluster for heartbeat
+				if (this._invalidMessageCount > NUMBER_OF_INVALID_MESSAGES) {
+					if (this._socket) {
+						this._socket.destroy(
+							INVALID_MESSAGE_RECEIVED_CODE,
+							INVALID_MESSAGE_RECEIVED_REASON,
+						);
+					}
+
+					this._banPeer();
+					this.emit(
+						EVENT_INBOUND_SOCKET_ERROR,
+						`Peer ${this.ipAddress}:${
+							this.wsPort
+						} was disconnected due to unwanted to messages`,
+					);
+				}
+			}
+
 			this._wsMessageCount += 1;
 		};
 
@@ -475,6 +500,7 @@ export class Peer extends EventEmitter {
 		this._wsMessageRate =
 			(this._wsMessageCount * RATE_NORMALIZATION_FACTOR) / this._rateInterval;
 		this._wsMessageCount = 0;
+		this._invalidMessageCount = 0;
 
 		if (this._wsMessageRate > this._peerConfig.wsMaxMessageRate) {
 			this.applyPenalty(this._peerConfig.wsMaxMessageRatePenalty);
