ada9841b6c580202ecd1a2b118e969dde6d2aa60
Valamidev <52665225+valamidev@users.noreply.github.com>
2020-01-02 14:26:05 +0100

    Update jest.config.base.js

--- elements/lisk-p2p/test/config/jest.config.base.js
+++ elements/lisk-p2p/test/config/jest.config.base.js
@@ -21,15 +21,6 @@ module.exports = {
 	rootDir: '../../../../',
 	setupFilesAfterEnv: ['<rootDir>/framework/test/jest/config/setup.js'],
 
-	coverageThreshold: {
-		global: {
-			branches: 50,
-			functions: 50,
-			lines: 50,
-			statements: 50,
-		},
-	},
-
 	/**
 	 * restoreMocks [boolean]
 	 *
