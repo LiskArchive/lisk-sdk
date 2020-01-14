a2ce30ef5d0396c637aec13bc302fa8878329954
Manu Nelamane Siddalingegowda <manu@lightcurve.io>
2020-01-14 16:45:52 +0100

    :truck: Restructure the DPoS test
    Move the unit test files under unit folder
    
    Move fixtures and utils to its folders

---
+++ elements/lisk-dpos/test/fixtures/constants.ts
@@ -0,0 +1,22 @@
+/*
+ * Copyright © 2019 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+'use strict';
+
+module.exports = {
+	ACTIVE_DELEGATES: 101,
+	EPOCH_TIME: '2016-05-24T17:00:00.000Z',
+	BLOCK_TIME: 10,
+	DELEGATE_LIST_ROUND_OFFSET: 2,
+};
