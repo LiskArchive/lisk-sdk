22783c411916ea869c5422c7d8b6354927980803
shuse2 <shus.toda@gmail.com>
2020-01-15 20:29:02 +0100

    :recycle: Update lisk-cryptography not to use bignum

--- elements/lisk-cryptography/src/buffer.ts
+++ elements/lisk-cryptography/src/buffer.ts
@@ -12,19 +12,38 @@
  * Removal or modification of this copyright notice is prohibited.
  *
  */
-import * as BigNum from '@liskhq/bignum';
 
 export const BIG_ENDIAN = 'big';
 export const LITTLE_ENDIAN = 'little';
+const MAX_NUMBER_BYTE_LENGTH = 6;
 
 export const intToBuffer = (
 	value: number | string,
 	byteLength: number,
 	endianness: string = BIG_ENDIAN,
-) => new BigNum(value).toBuffer({ size: byteLength, endian: endianness });
+) => {
+	const buffer = Buffer.alloc(byteLength);
+	if (endianness === 'big') {
+		if (byteLength <= MAX_NUMBER_BYTE_LENGTH) {
+			buffer.writeUIntBE(Number(value), 0, byteLength);
+		} else {
+			buffer.writeBigUInt64BE(BigInt(value));
+		}
+	} else {
+		if (byteLength <= MAX_NUMBER_BYTE_LENGTH) {
+			buffer.writeUIntLE(Number(value), 0, byteLength);
+		} else {
+			buffer.writeBigUInt64LE(BigInt(value));
+		}
+	}
+
+	return buffer;
+};
 
 export const bufferToIntAsString = (buffer: Buffer): string =>
-	BigNum.fromBuffer(buffer).toString();
+	buffer.length <= MAX_NUMBER_BYTE_LENGTH
+		? buffer.readIntBE(0, buffer.length).toString()
+		: buffer.readBigUInt64BE().toString();
 
 /**
  * @deprecated Use intToBuffer instead
