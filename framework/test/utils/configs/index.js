757cee2918d0775133503121b1e93f6b996145c9
Nazar Hussain <nazar@lightcurve.io>
2020-01-30 11:07:13 +0100

    Update node configuration from modules.chain to app.node

--- framework/test/utils/configs/index.js
+++ framework/test/utils/configs/index.js
@@ -17,9 +17,11 @@
 const { storageConfig } = require('./config_storage');
 const { cacheConfig } = require('./config_cache');
 const { constantsConfig } = require('./config_constants');
+const { nodeConfig } = require('./config_node');
 
 module.exports = {
 	storageConfig,
 	cacheConfig,
 	constantsConfig,
+	nodeConfig,
 };
