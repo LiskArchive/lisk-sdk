954faf611874dc87a0184496d7ff627988e65482
mitsuaki-u <mitsuaki.uchimoto@gmail.com>
2020-03-02 15:37:39 +0100

    Remove unneeded flag

--- commander/src/commands/transaction/create.ts
+++ commander/src/commands/transaction/create.ts
@@ -98,8 +98,6 @@ export default class CreateCommand extends BaseCommand {
 		votes: flagParser.string(commonFlags.votes),
 		unvotes: flagParser.string(commonFlags.unvotes),
 		networkIdentifier: flagParser.string(commonFlags.networkIdentifier),
-		nonce: flagParser.string(commonFlags.nonce),
-		fee: flagParser.string(commonFlags.fee),
 	};
 
 	async run(): Promise<void> {
