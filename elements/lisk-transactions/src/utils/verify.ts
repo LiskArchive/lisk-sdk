2a3f5bec2a03485db912614938698c37c1827842
Pablo Vicente <pvicente@gmail.com>
2020-03-18 16:14:10 +0100

    Check for empty string when verifying mandatory signatures from multisignature account

--- elements/lisk-transactions/src/utils/verify.ts
+++ elements/lisk-transactions/src/utils/verify.ts
@@ -98,8 +98,20 @@ export const validateKeysSignatures = (
 	transactionBytes: Buffer,
 ) => {
 	const errors = [];
+
 	// tslint:disable-next-line: prefer-for-of no-let
 	for (let i = 0; i < keys.length; i += 1) {
+		if (signatures[i].length === 0) {
+			errors.push(
+				new TransactionError(
+					'All mandatory keys have to sign the transaction.',
+					undefined,
+					'.signatures',
+					signatures.join(','),
+				),
+			);
+			break;
+		}
 		const { error } = validateSignature(
 			keys[i],
 			signatures[i],
