04c53c7d0a0f6172ad3c9124ea0a9e1c649436da
shuse2 <shus.toda@gmail.com>
2020-03-24 16:16:41 +0100

    :bug: Fix to get last blocks with offset when deleting a block

--- elements/lisk-chain/src/chain.ts
+++ elements/lisk-chain/src/chain.ts
@@ -358,12 +358,12 @@ export class Chain {
 		this.dataAccess.resetBlockHeaderCache();
 	}
 
-	public async newStateStore(): Promise<StateStore> {
+	public async newStateStore(heightOffset: number = 0): Promise<StateStore> {
 		const fromHeight = Math.max(
 			1,
-			this._lastBlock.height - this.constants.stateBlockSize,
+			this._lastBlock.height - this.constants.stateBlockSize - heightOffset,
 		);
-		const toHeight = this._lastBlock.height;
+		const toHeight = Math.max(this._lastBlock.height - heightOffset, 1);
 		const lastBlockHeaders = await this.dataAccess.getBlockHeadersByHeightBetween(
 			fromHeight,
 			toHeight,
