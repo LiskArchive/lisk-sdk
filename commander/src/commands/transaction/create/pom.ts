1de7dd6333f64acccaecee89b2f68e28b3983c7b
shuse2 <shus.toda@gmail.com>
2020-04-02 17:15:30 +0200

    :seedling: Add command to create pom transaction

---
+++ commander/src/commands/transaction/create/pom.ts
@@ -0,0 +1,223 @@
+/*
+ * LiskHQ/lisk-commander
+ * Copyright © 2019 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import {
+	TransactionJSON,
+	utils as transactionUtils,
+} from '@liskhq/lisk-transactions';
+import { isValidFee, isValidNonce } from '@liskhq/lisk-validator';
+import { flags as flagParser } from '@oclif/command';
+
+import BaseCommand from '../../../base';
+import { ValidationError } from '../../../utils/error';
+import { flags as commonFlags } from '../../../utils/flags';
+import { getNetworkIdentifierWithInput } from '../../../utils/network_identifier';
+import { getPassphraseFromPrompt } from '../../../utils/reader';
+
+interface PomInput {
+	readonly nonce: string;
+	readonly fee: string;
+	readonly networkIdentifier: string;
+	readonly header1: RawHeader;
+	readonly header2: RawHeader;
+	readonly passphrase?: string;
+}
+
+const reportMisbehavior = ({
+	nonce,
+	fee,
+	networkIdentifier,
+	header1,
+	header2,
+	passphrase,
+}: PomInput): Partial<TransactionJSON> => {
+	// tslint:disable-next-line no-console
+	console.log({
+		nonce,
+		fee,
+		networkIdentifier,
+		header1,
+		header2,
+		passphrase,
+	});
+
+	// tslint:disable-next-line
+	return {} as Partial<TransactionJSON>;
+};
+
+interface RawHeader {
+	readonly id: string;
+	readonly height: number;
+	readonly version: number;
+	readonly timestamp: number;
+	readonly previousBlockId?: string | null;
+	readonly blockSignature: string;
+	readonly seedReveal: string;
+	readonly generatorPublicKey: string;
+	readonly numberOfTransactions: number;
+	readonly payloadLength: number;
+	readonly payloadHash: string;
+	readonly totalAmount: string;
+	readonly totalFee: string;
+	readonly reward: string;
+	readonly maxHeightPreviouslyForged: number;
+	readonly maxHeightPrevoted: number;
+}
+
+const processInputs = (
+	nonce: string,
+	fee: string,
+	networkIdentifier: string,
+	header1: RawHeader,
+	header2: RawHeader,
+	passphrase?: string,
+) =>
+	reportMisbehavior({
+		nonce,
+		fee,
+		networkIdentifier,
+		passphrase,
+		header1,
+		header2,
+	});
+
+interface Args {
+	readonly nonce: string;
+	readonly fee: string;
+	readonly header1: string;
+	readonly header2: string;
+}
+
+export default class PoMCommand extends BaseCommand {
+	static args = [
+		{
+			name: 'nonce',
+			required: true,
+			description: 'Nonce of the transaction.',
+		},
+		{
+			name: 'fee',
+			required: true,
+			description: 'Transaction fee in LSK.',
+		},
+		{
+			name: 'header1',
+			required: true,
+			description: 'Contradicting block header in JSON string.',
+		},
+		{
+			name: 'header2',
+			required: true,
+			description: 'Contradicting block header in JSON string.',
+		},
+	];
+	static description = `
+	Creates a transaction which will report misbehavior of delegate by providing contradicting 2 block headers.
+	`;
+
+	static examples = [
+		'transaction:create:pom 1 100 "{"height": 3, "version": 2, "maxHeightPrevoted": 30, "blockSignature": "xxx"}" "{"height": 3, "version": 2, "maxHeightPrevoted": 31, "blockSignature": "yyy"}"',
+	];
+
+	static flags = {
+		...BaseCommand.flags,
+		networkIdentifier: flagParser.string(commonFlags.networkIdentifier),
+		passphrase: flagParser.string(commonFlags.passphrase),
+		'no-signature': flagParser.boolean(commonFlags.noSignature),
+	};
+
+	async run(): Promise<void> {
+		const {
+			args,
+			flags: {
+				networkIdentifier: networkIdentifierSource,
+				passphrase: passphraseSource,
+				'no-signature': noSignature,
+			},
+		} = this.parse(PoMCommand);
+
+		const {
+			nonce,
+			fee,
+			header1: header1Str,
+			header2: header2Str,
+		} = args as Args;
+
+		if (!isValidNonce(nonce)) {
+			throw new ValidationError('Enter a valid nonce in number string format.');
+		}
+
+		if (Number.isNaN(Number(fee))) {
+			throw new ValidationError('Enter a valid fee in number string format.');
+		}
+
+		const normalizedFee = transactionUtils.convertLSKToBeddows(fee);
+
+		if (!isValidFee(normalizedFee)) {
+			throw new ValidationError('Enter a valid fee in number string format.');
+		}
+
+		// tslint:disable-next-line no-let
+		let header1: RawHeader;
+		try {
+			header1 = JSON.parse(header1Str);
+		} catch (error) {
+			throw new Error(
+				`Invalid block header 1. Fail to parse the input. ${error.message}`,
+			);
+		}
+
+		// tslint:disable-next-line no-let
+		let header2: RawHeader;
+		try {
+			header2 = JSON.parse(header2Str);
+		} catch (error) {
+			throw new Error(
+				`Invalid block header 1. Fail to parse the input. ${error.message}`,
+			);
+		}
+
+		const networkIdentifier = getNetworkIdentifierWithInput(
+			networkIdentifierSource,
+			this.userConfig.api.network,
+		);
+
+		if (noSignature) {
+			const noSignatureResult = processInputs(
+				nonce,
+				normalizedFee,
+				networkIdentifier,
+				header1,
+				header2,
+			);
+			this.print(noSignatureResult);
+
+			return;
+		}
+
+		const passphrase =
+			passphraseSource ?? (await getPassphraseFromPrompt('passphrase', true));
+
+		const result = processInputs(
+			nonce,
+			normalizedFee,
+			networkIdentifier,
+			header1,
+			header2,
+			passphrase,
+		);
+		this.print(result);
+	}
+}
---
+++ commander/test/commands/transaction/create/pom.test.ts
@@ -0,0 +1,191 @@
+/*
+ * LiskHQ/lisk-commander
+ * Copyright © 2020 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+import * as sandbox from 'sinon';
+import { expect, test } from '@oclif/test';
+import * as transactions from '@liskhq/lisk-transactions';
+import * as config from '../../../../src/utils/config';
+import * as printUtils from '../../../../src/utils/print';
+import * as readerUtils from '../../../../src/utils/reader';
+
+describe('transaction:create:pom', () => {
+	const defaultHeader1 = {
+		version: 2,
+		timestamp: 2000000,
+		previousBlockId: '10620616195853047363',
+		seedReveal: 'c8c557b5dba8527c0e760124128fd15c',
+		height: 300000,
+		maxHeightPreviouslyForged: 90000,
+		maxHeightPrevoted: 100000,
+		numberOfTransactions: 0,
+		totalAmount: '0',
+		totalFee: '10000000000',
+		reward: '10000000000',
+		payloadLength: 0,
+		payloadHash:
+			'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+		generatorPublicKey:
+			'addb0e15a44b0fdc6ff291be28d8c98f5551d0cd9218d749e30ddb87c6e31ca9',
+		blockSignature:
+			'd87f2eafb2f8022d4a1171fa5735466f5fa5749dfc6d2a3978d69d266aadfe4929a65925daaf380c1323d34049bc0aef5d5ad12916ab137c8829ef42f12d400b',
+	};
+	const defaultHeader2 = {
+		version: 2,
+		timestamp: 3000000,
+		previousBlockId: '10620616195853047363',
+		seedReveal: 'c8c557b5dba8527c0e760124128fd15c',
+		height: 200000,
+		maxHeightPreviouslyForged: 100000,
+		maxHeightPrevoted: 100000,
+		numberOfTransactions: 0,
+		totalAmount: '0',
+		totalFee: '10000000000',
+		reward: '10000000000',
+		payloadLength: 0,
+		payloadHash:
+			'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+		generatorPublicKey:
+			'addb0e15a44b0fdc6ff291be28d8c98f5551d0cd9218d749e30ddb87c6e31ca9',
+		blockSignature:
+			'c86e638c27d39c033cc8bb107c5a5a19f6e95361314d2d74b715d96ddfac6d156ea77d798dbd34203920e33525c5645ac01931043e3c0021a6da068048fa770a',
+	};
+
+	const defaultInputs = '123';
+	const defaultTransaction = {
+		nonce: '0',
+		fee: '10000000',
+		amount: '10000000000',
+		senderPublicKey: null,
+		timestamp: 66492418,
+		type: 15,
+		asset: {},
+	};
+	const testnetNetworkIdentifier =
+		'e48feb88db5b5cf5ad71d93cdcd1d879b6d5ed187a36b0002cc34e0ef9883255';
+
+	const printMethodStub = sandbox.stub();
+
+	const setupTest = () =>
+		test
+			.stub(printUtils, 'print', sandbox.stub().returns(printMethodStub))
+			.stub(
+				config,
+				'getConfig',
+				sandbox.stub().returns({ api: { network: 'test' } }),
+			)
+			.stub(
+				transactions,
+				'reportMisbehavior',
+				sandbox.stub().returns(defaultTransaction),
+			)
+			.stub(
+				readerUtils,
+				'getPassphraseFromPrompt',
+				sandbox.stub().resolves(defaultInputs),
+			)
+			.stdout();
+
+	describe('transaction:create:pom', () => {
+		setupTest()
+			.command(['transaction:create:pom'])
+			.catch(error => {
+				return expect(error.message).to.contain('Missing 4 required arg');
+			})
+			.it('should throw an error');
+	});
+
+	describe('transaction:create:delegate nonce fee header1 header2', () => {
+		setupTest()
+			.command([
+				'transaction:create:delegate',
+				'1',
+				'100',
+				JSON.stringify(defaultHeader1),
+				JSON.stringify(defaultHeader2),
+			])
+			.it('create a transaction with the header 1 and header 2', () => {
+				expect(readerUtils.getPassphraseFromPrompt).to.be.calledWithExactly(
+					'passphrase',
+					true,
+				);
+				expect(transactions.reportMisbehavior).to.be.calledWithExactly({
+					nonce: '1',
+					fee: '10000000000',
+					networkIdentifier: testnetNetworkIdentifier,
+					passphrase: defaultInputs,
+					header1: defaultHeader1,
+					header2: defaultHeader2,
+				});
+				return expect(printMethodStub).to.be.calledWithExactly(
+					defaultTransaction,
+				);
+			});
+	});
+
+	describe('transaction:create:pom nonce fee header1 header2 --passphrase=xxx', () => {
+		setupTest()
+			.command([
+				'transaction:create:pom',
+				'1',
+				'100',
+				JSON.stringify(defaultHeader1),
+				JSON.stringify(defaultHeader2),
+				'--passphrase=123',
+			])
+			.it(
+				'create a transaction with the headers with the passphrase from flag',
+				() => {
+					expect(readerUtils.getPassphraseFromPrompt).not.to.be.called;
+					expect(transactions.registerDelegate).to.be.calledWithExactly({
+						nonce: '1',
+						fee: '10000000000',
+						networkIdentifier: testnetNetworkIdentifier,
+						passphrase: defaultInputs,
+						header1: defaultHeader1,
+						header2: defaultHeader2,
+					});
+					return expect(printMethodStub).to.be.calledWithExactly(
+						defaultTransaction,
+					);
+				},
+			);
+	});
+
+	describe('transaction:create:pom nonce fee header1 header2 --no-signature', () => {
+		setupTest()
+			.command([
+				'transaction:create:delegate',
+				'1',
+				'100',
+				JSON.stringify(defaultHeader1),
+				JSON.stringify(defaultHeader2),
+				'--no-signature',
+			])
+			.it('create a transaction with the username without signature', () => {
+				expect(transactions.reportMisbehavior).to.be.calledWithExactly({
+					nonce: '1',
+					fee: '10000000000',
+					networkIdentifier: testnetNetworkIdentifier,
+					passphrase: undefined,
+					header1: defaultHeader1,
+					header2: defaultHeader2,
+				});
+				expect(readerUtils.getPassphraseFromPrompt).not.to.be.called;
+				return expect(printMethodStub).to.be.calledWithExactly(
+					defaultTransaction,
+				);
+			});
+	});
+});
