157146b0fe947e2e7123e67a487f329c02e50705
shuse2 <shus.toda@gmail.com>
2020-04-17 23:28:54 +0200

    :recycle: Remove eslint rule

--- .eslintrc.test.js
+++ .eslintrc.test.js
@@ -3,6 +3,7 @@ module.exports = {
 	rules: {
 		'arrow-body-style': 'off',
 		'@typescript-eslint/no-magic-numbers': 'off',
+		'@typescript-eslint/unbound-method': 'off',
 		'@typescript-eslint/no-require-imports': 'off',
 		'@typescript-eslint/no-explicit-any': 'off',
 		'@typescript-eslint/no-unsafe-assignment': 'off',
