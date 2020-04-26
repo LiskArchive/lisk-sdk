a9a9cd1e41bc311f48e9316d282b539116d36240
Manu Nelamane Siddalingegowda <manugowda.ns@gmail.com>
2020-04-26 20:40:51 +0200

    :seedling: Add typedef for framework
    Rename network files to ts

---
+++ framework/src/types.ts
@@ -0,0 +1,82 @@
+/*
+ * Copyright © 2020 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+import * as liskP2p from '@liskhq/lisk-p2p';
+
+export interface Channel<T> {
+	readonly publish: (procedure: string, params?: object) => void;
+	readonly subscribe: (procedure: string, callback: Function) => void;
+	readonly invoke: (eventName: string) => Promise<T>;
+	readonly invokePublic: (
+		procedure: string,
+		params?: object,
+	) => Promise<object>;
+}
+
+export interface Logger {
+	readonly trace: (data?: object | unknown, message?: string) => void;
+	readonly debug: (data?: object | unknown, message?: string) => void;
+	readonly info: (data?: object | unknown, message?: string) => void;
+	readonly error: (data?: object | unknown, message?: string) => void;
+	readonly fatal: (data?: object | unknown, message?: string) => void;
+}
+
+/* Start Database  */
+export interface Storage {
+	readonly entities: {
+		readonly NetworkInfo: NetworkInfoEntity;
+	};
+}
+
+export interface NetworkInfoEntity {
+	readonly getKey: (
+		key: string,
+		tx?: StorageTransaction,
+	) => Promise<string | undefined>;
+	readonly setKey: (
+		key: string,
+		value: string,
+		tx?: StorageTransaction,
+	) => Promise<void>;
+}
+
+export interface StorageTransaction {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	readonly batch: <T = any>(input: any[]) => Promise<T>;
+}
+/* End Database */
+
+/* Start P2P */
+type Modify<T, R> = Omit<T, keyof R> & R;
+export type P2PConfig = Modify<
+	liskP2p.p2pTypes.P2PConfig,
+	{
+		readonly advertiseAddress: boolean;
+		readonly seedPeers: ReadonlyArray<SeedPeerInfo>;
+		// readonly fixedPeers: ReadonlyArray<SeedPeerInfo>,
+		// readonly whitelistedPeers: ReadonlyArray<SeedPeerInfo>,
+		// readonly blacklistedIPs: ReadonlyArray<string>,
+	}
+>;
+
+export interface ProtocolPeerInfo {
+	readonly [key: string]: unknown;
+	readonly ipAddress: string;
+	readonly wsPort: number;
+}
+
+export interface SeedPeerInfo {
+	readonly ip: string | unknown;
+	readonly wsPort: number;
+}
+/* End P2P */
