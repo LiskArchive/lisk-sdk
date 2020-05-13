bde957bbcc311c282cfa2cf681988c76c94f79f8
shuse2 <shus.toda@gmail.com>
2020-05-13 21:41:22 +0200

    :recycle: Update integration tests

---
+++ framework/test/utils/kv_store.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright © 2018 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+import * as fs from 'fs-extra';
+import { KVStore } from '@liskhq/lisk-db';
+
+export const defaultPath = '/tmp/lisk-framework/test';
+
+const getPath = (name: string): string => `${defaultPath}/${name}`;
+
+// eslint-disable-next-line
+export const createDB = (name: string) => {
+	const path = getPath(name);
+	fs.ensureDirSync(path);
+	const forgerDBPath = getPath(name);
+	return {
+		path,
+		blockchainDB: new KVStore(`${path}/blockchain.db`),
+		forgerDBPath,
+		forgerDB: new KVStore(`${path}/forger.db`),
+	};
+};
+
+export const removeDB = (name: string): void => fs.removeSync(getPath(name));
