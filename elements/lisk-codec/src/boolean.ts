bad4401cd41e1316fb487827f560420165161ded
shuse2 <shus.toda@gmail.com>
2020-05-14 16:00:19 +0200

    :seedling: Add boolean read/writer

---
+++ elements/lisk-codec/src/boolean.ts
@@ -0,0 +1,26 @@
+/*
+ * Copyright © 2020 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+/* eslint-disable no-bitwise */
+/* eslint-disable no-param-reassign */
+
+
+interface SchemaProperty {
+	readonly dataType: string;
+}
+
+export const writeBoolean = (value: boolean, _schema: SchemaProperty): Buffer =>
+    value ? Buffer.from('01', 'hex') : Buffer.from('00', 'hex');
+
+export const readBoolean = (buffer: Buffer, _schema: SchemaProperty): boolean =>
+	buffer[0] === 0x01;
---
+++ elements/lisk-codec/test/boolean.spec.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright © 2020 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+import {
+	writeBoolean,
+	readBoolean,
+} from '../src/boolean';
+
+describe('boolean', () => {
+	describe('writer', () => {
+		it('should encode boolean', () => {
+			expect(writeBoolean(true, { dataType: 'boolean' })).toEqual(
+				Buffer.from('01', 'hex'),
+			);
+			expect(writeBoolean(false, { dataType: 'boolean' })).toEqual(
+				Buffer.from('00', 'hex'),
+			);
+		});
+	});
+
+	describe('reader', () => {
+		it('should decode boolean', () => {
+			expect(
+				readBoolean(Buffer.from('00', 'hex'), { dataType: 'boolean' }),
+			).toBeFalse();
+			expect(
+				readBoolean(Buffer.from('01', 'hex'), { dataType: 'boolean' }),
+			).toBeTrue();
+		});
+	});
+});
