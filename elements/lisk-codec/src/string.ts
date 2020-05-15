f1cabc56c81adae956849bc1a532a40e4b09a3d0
Ishan <ishantiw.quasar@gmail.com>
2020-05-15 11:07:43 +0200

    Add string reader/writer

---
+++ elements/lisk-codec/src/string.ts
@@ -0,0 +1,23 @@
+/*
+ * Copyright © 2020 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+interface SchemaProperty {
+	readonly dataType: string;
+}
+
+export const writeString = (value: string, _schema: SchemaProperty): Buffer =>
+	Buffer.from(value, 'utf8');
+
+export const readString = (buffer: Buffer, _schema: SchemaProperty): string =>
+	buffer.toString('utf8');
