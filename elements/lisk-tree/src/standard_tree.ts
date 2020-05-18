4ff491996c7d36c296876f6ac5a7d19afe65174b
Shusetsu Toda <shusetsu@lightcurve.io>
2020-05-18 18:04:35 +0200

    :seedling: Add lisk-tree package

---
+++ elements/lisk-tree/src/standard_tree.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright © 2020 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+import { Proof } from './types';
+
+export class StandardMarkleTree {
+	// eslint-disable-next-line
+	public constructor(_initValues: Buffer[]) {}
+
+	// eslint-disable-next-line
+	public append(_value: Buffer): { key: Buffer; value: Buffer }[] {
+		return [];
+	}
+
+	// eslint-disable-next-line
+	public generateProof(_queryData: ReadonlyArray<Buffer>): Proof {
+		// eslint-disable-next-line
+		return {} as any;
+	}
+}
---
+++ elements/lisk-tree/test/standard_tree.spec.ts
@@ -0,0 +1,27 @@
+/*
+ * Copyright © 2020 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+describe('', () => {
+	describe('constructor', () => {
+		it.todo('should have correct root value');
+	});
+
+	describe('append', () => {
+		it.todo('should append and update root');
+	});
+
+	describe('generateProof', () => {
+		it.todo('should generate proof that is verifiable');
+	});
+});
