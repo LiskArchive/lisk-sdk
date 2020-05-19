6cb403386caa1f1a333420117ed56a0a4eececf8
Manu Nelamane Siddalingegowda <manugowda.ns@gmail.com>
2020-05-19 17:22:14 +0200

    :recycle: Update protocol spec
    Add merkelRoot calculation for random ids

--- protocol-specs/generators/transaction_merkle_root_for_blocks/index.js
+++ protocol-specs/generators/transaction_merkle_root_for_blocks/index.js
@@ -14,27 +14,62 @@
 
 'use strict';
 
+const {
+	bufferToHex,
+	hash,
+	intToBuffer,
+	getRandomBytes,
+} = require('@liskhq/lisk-cryptography');
 const BaseGenerator = require('../base_generator');
-const defaultConfig = require('../../config/devnet');
 
-const { genesisBlock } = defaultConfig;
+const getRandomTransactionIds = () => {
+	const ids = [];
+	for (let index = 0; index < 303; index += 1) {
+		ids.push(getRandomBytes(32));
+	}
+	return ids;
+};
+
+const LEAFPREFIX = intToBuffer(0);
+const BRANCHPREFIX = intToBuffer(1);
+
+const concat = (m1, m2) => Buffer.concat([m1, m2]);
+
+const leafHash = m => hash(concat(LEAFPREFIX, m));
+
+const branchHash = m => hash(concat(BRANCHPREFIX, m));
+
+const merkleRoot = transactionIds => {
+	const len = transactionIds.length;
+	if (len === 0) return Buffer.from([]);
+	if (len === 1) return leafHash(transactionIds[0]);
+
+	const k = 2 ** Math.floor(Math.log2(len - 1));
+	const newData = transactionIds.splice(k);
+
+	return branchHash(concat(merkleRoot(transactionIds), merkleRoot(newData)));
+};
 
 const generateTransactionMerkleRoot = () => {
-	const merkleRoot = '';
+	const transactionIds = getRandomTransactionIds();
+	const ids = transactionIds.map(t => bufferToHex(t));
+	const transactionMerkleRoot = merkleRoot(transactionIds);
 
 	return {
 		description: 'Given a valid block',
 		input: {
-			block: genesisBlock,
+			transactionIds: ids,
 		},
-		output: { merkleRoot },
+		output: {
+			transactionMerkleRoot: transactionMerkleRoot.toString('hex'),
+		},
 	};
 };
 
 const transactionMerkleRootSuite = () => ({
 	title: 'Transaction Merkle tree root',
 	summary: 'Replace payload hash with Merkle tree root in block header',
-	config: { network: 'devnet' },
+	config: {},
 	runner: 'transaction_merkle_root',
 	handler: 'transaction_merkle_root',
 	testCases: [generateTransactionMerkleRoot()],
