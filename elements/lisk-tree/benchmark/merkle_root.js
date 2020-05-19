f4b7a4e500e328dc6bf4823039b929da177713c3
Shusetsu Toda <shusetsu@lightcurve.io>
2020-05-19 16:25:17 +0200

    :seedling: Add merkle tree root calculation
    :fire: Remove eslint ignore
    
    :recycle: Add eslintignore
    
    fix

---
+++ elements/lisk-tree/benchmark/merkle_root.js
@@ -0,0 +1,36 @@
+/*
+ * Copyright © 2020 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+const { Suite } = require('benchmark');
+const { getRandomBytes } = require('@liskhq/lisk-cryptography');
+const {
+    MerkleTree,
+} = require('../dist-node/merkle_tree');
+
+const suite = new Suite();
+const size = 150;
+const testSamples = [];
+
+for (let i = 0; i < size; i += 1) {
+	testSamples.push(getRandomBytes(32));
+}
+
+suite
+    .add('constructor', () => {
+        new MerkleTree(testSamples);
+    })
+    .on('cycle', function (event) {
+        console.log(String(event.target));
+    })
+    .run({ async: true })
---
+++ elements/lisk-tree/src/constant.ts
@@ -0,0 +1,17 @@
+/*
+ * Copyright © 2020 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+export const EMPTY_HASH = Buffer.alloc(0);
+export const LEAF_PREFIX = Buffer.from('00', 'hex');
+export const BRANCH_PREFIX = Buffer.from('01', 'hex');
