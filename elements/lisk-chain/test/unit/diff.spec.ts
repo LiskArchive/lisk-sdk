e8ed5dc44ebd899368f361cc1cf3be23f151b5fb
Ishan <ishantiw.quasar@gmail.com>
2020-05-25 14:56:14 +0200

    Add more unit tests to diff function

--- elements/lisk-chain/test/unit/diff.spec.ts
+++ elements/lisk-chain/test/unit/diff.spec.ts
@@ -14,9 +14,20 @@
 
 import { calculateDiff, undo } from '../../src/diff';
 
+const randomBytes = (size: number) => {
+	let result = '';
+	const characters = 'abcdef0123456789';
+	const charactersLength = characters.length;
+	for (let i = 0; i < 2 * size; i += 1) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength));
+	}
+
+	return Buffer.from(result, 'hex');
+};
+
 describe('diff', () => {
 	describe('calculateDiff', () => {
-		describe('Testing using simple arrays', () => {
+		describe('Testing using arrays and random bytes', () => {
 			const arr1 = [1, 2, 3, 5, 6];
 			const arr2 = [1, 3, 4, 5, 6];
 			const arr1Buffer = Buffer.from(arr1);
@@ -53,6 +64,27 @@ describe('diff', () => {
 				];
 				expect(diff).toEqual(editHistory);
 				expect(undo(arr1Buffer, diff)).toEqual(Buffer.from([]));
+			});
+
+			it('should return diff that can be use to undo buffer2 to buffer1', () => {
+				const randomBytes1 = randomBytes(115);
+				const randomBytes2 = randomBytes(815);
+				const randomBytes3 = randomBytes(16);
+				const randomBytes4 = randomBytes(12);
+
+				const buffer1 = Buffer.concat([
+					randomBytes1,
+					randomBytes3,
+					randomBytes2,
+				]);
+				const buffer2 = Buffer.concat([
+					randomBytes4,
+					randomBytes1,
+					randomBytes2,
+				]);
+
+				const diff = calculateDiff(buffer1, buffer2);
+				expect(undo(buffer2, diff)).toEqual(buffer1);
 			});
 		});
 
@@ -148,6 +180,76 @@ describe('diff', () => {
 				);
 
 				expect(restoredReceiver).toEqual(receiverAccount);
+			});
+
+			it('should return a valid diff when undo multisignature account', () => {
+				const multiSignatureAccount = {
+					...senderAccount,
+					keys: {
+						numberOfSignatures: 3,
+						mandatoryKeys: [
+							'0b211fce4b615083701cb8a8c99407e464b2f9aa4f367095322de1b77e5fcfbe',
+							'ff30ef40b7de42114137be46f1009d30e5c19809a73d5a162bc99f7e7681d63d',
+						],
+						optionalKeys: [
+							'57df5c3811961939f8dcfa858c6eaefebfaa4de942f7e703bf88127e0ee9cca4',
+							'fa406b6952d377f0278920e3eb8da919e4cf5c68b02eeba5d8b3334fdc0369b6',
+						],
+					},
+				};
+
+				const multiSignatureAccountBuffer = Buffer.from(
+					JSON.stringify(multiSignatureAccount),
+				);
+				const diff = calculateDiff(
+					previousSenderStateBuffer,
+					multiSignatureAccountBuffer,
+				);
+
+				const restoredSender = JSON.parse(
+					undo(multiSignatureAccountBuffer, diff).toString('utf8'),
+				);
+
+				expect(restoredSender).toEqual(senderAccount);
+			});
+
+			it('should return a diff even if there is no state change and undo makes no change', () => {
+				const diff = calculateDiff(
+					previousSenderStateBuffer,
+					previousSenderStateBuffer,
+				);
+
+				const restoredSender = JSON.parse(
+					undo(previousSenderStateBuffer, diff).toString('utf8'),
+				);
+
+				expect(restoredSender).toEqual(senderAccount);
+			});
+
+			it('should return a valid diff from a blank buffer to an account json', () => {
+				const diff = calculateDiff(
+					Buffer.from(JSON.stringify(''), 'utf8'),
+					previousSenderStateBuffer,
+				);
+
+				const restoredState = JSON.parse(
+					undo(previousSenderStateBuffer, diff).toString('utf8'),
+				);
+
+				expect(restoredState).toEqual('');
+			});
+
+			it('should return a valid diff from a valid account json to blank buffer on deletion', () => {
+				const diff = calculateDiff(
+					previousSenderStateBuffer,
+					Buffer.from(JSON.stringify(''), 'utf8'),
+				);
+
+				const restoredSender = JSON.parse(
+					undo(Buffer.from(JSON.stringify(''), 'utf8'), diff).toString('utf8'),
+				);
+
+				expect(restoredSender).toEqual(senderAccount);
 			});
 		});
 	});
