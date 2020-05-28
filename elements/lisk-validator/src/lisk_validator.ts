580f8d70afb6f2ed19624524558682dc9c7ebbf4
Nazar Hussain <nazar@lightcurve.io>
2020-05-28 15:08:46 +0200

    Update lisk-validator interface to expose more interfaces

--- elements/lisk-validator/src/lisk_validator.ts
+++ elements/lisk-validator/src/lisk_validator.ts
@@ -14,29 +14,29 @@
  */
 
 import * as Ajv from 'ajv';
-
+import { FormatDefinition, FormatValidator, ValidateFunction } from 'ajv';
 import * as formats from './formats';
 
 export type ErrorObject = Ajv.ErrorObject;
 
 class LiskValidator {
-	private readonly validator: Ajv.Ajv;
+	private readonly _validator: Ajv.Ajv;
 	public constructor() {
-		this.validator = new Ajv({
+		this._validator = new Ajv({
 			allErrors: true,
 			schemaId: 'auto',
 			useDefaults: false,
 		});
 
 		for (const formatName of Object.keys(formats)) {
-			this.validator.addFormat(
+			this._validator.addFormat(
 				formatName,
 				// eslint-disable-next-line import/namespace
 				formats[formatName as keyof typeof formats],
 			);
 		}
 
-		this.validator.addKeyword('uniqueSignedPublicKeys', {
+		this._validator.addKeyword('uniqueSignedPublicKeys', {
 			type: 'array',
 			// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 			compile: () => (data: ReadonlyArray<string>) =>
@@ -49,11 +49,41 @@ class LiskValidator {
 	}
 
 	public validate(schema: object, data: object): ReadonlyArray<ErrorObject> {
-		if (!this.validator.validate(schema, data)) {
-			return this.validator.errors as ReadonlyArray<ErrorObject>;
+		if (!this._validator.validate(schema, data)) {
+			return this._validator.errors as ReadonlyArray<ErrorObject>;
 		}
 
 		return [];
+	}
+
+	public validateSchema(schema: object | boolean): ReadonlyArray<ErrorObject> {
+		if (!this._validator.validateSchema(schema)) {
+			return this._validator.errors as ReadonlyArray<ErrorObject>;
+		}
+
+		return [];
+	}
+
+	public compile(schema: object | boolean): ValidateFunction {
+		return this._validator.compile(schema);
+	}
+
+	public addMetaSchema(schema: object, key?: string): Ajv.Ajv {
+		return this._validator.addMetaSchema(schema, key);
+	}
+
+	public addKeyword(
+		keyword: string,
+		definition: Ajv.KeywordDefinition,
+	): Ajv.Ajv {
+		return this._validator.addKeyword(keyword, definition);
+	}
+
+	public addFormat(
+		name: string,
+		format: FormatValidator | FormatDefinition,
+	): Ajv.Ajv {
+		return this._validator.addFormat(name, format);
 	}
 }
 
