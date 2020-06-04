bf31c6db235b208d2c5c497350433565d3011c67
Ishan <ishantiw.quasar@gmail.com>
2020-06-04 14:09:03 +0200

    Fix incorrect condition

--- elements/lisk-validator/src/keywords/data_type.ts
+++ elements/lisk-validator/src/keywords/data_type.ts
@@ -79,9 +79,9 @@ const compile = (
 		if (value === 'bytes') return isBytes(data as Buffer);
 		if (value === 'string') return isString(data);
 		if (value === 'uint32') return isUInt32(data);
-		if (value === 'sint32') return isSInt32(data);
-		if (value === 'uint64') return isSInt64(data);
 		if (value === 'uint64') return isUInt64(data);
+		if (value === 'sint32') return isSInt32(data);
+		if (value === 'sint64') return isSInt64(data);
 
 		// Either "dataType" or "type" can be presented in schema
 		return true;
