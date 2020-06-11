d22c3712aad6be180a0d6de1d16f8bd3b0b31be9
Ishan <ishantiw.quasar@gmail.com>
2020-06-11 16:10:42 +0200

    Add custom schema util

---
+++ elements/lisk-p2p/test/utils/schema.ts
@@ -0,0 +1,100 @@
+/*
+ * Copyright © 2020 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+export const customNodeInfoSchema = {
+	$id: '/nodeInfo',
+	type: 'object',
+	properties: {
+		networkId: {
+			dataType: 'string',
+			fieldNumber: 1,
+		},
+		protocolVersion: {
+			dataType: 'string',
+			fieldNumber: 2,
+		},
+		wsPort: {
+			dataType: 'uint32',
+			fieldNumber: 3,
+		},
+		nonce: {
+			dataType: 'string',
+			fieldNumber: 4,
+		},
+		advertiseAddress: {
+			dataType: 'boolean',
+			fieldNumber: 5,
+		},
+		os: {
+			dataType: 'string',
+			fieldNumber: 6,
+		},
+		height: {
+			dataType: 'uint32',
+			fieldNumber: 7,
+		},
+		maxHeightPrevoted: {
+			dataType: 'uint32',
+			fieldNumber: 8,
+		},
+		maxHeightPreviouslyForged: {
+			dataType: 'uint32',
+			fieldNumber: 9,
+		},
+	},
+	required: ['networkId', 'protocolVersion', 'wsPort', 'nonce'],
+};
+
+export const customPeerInfoSchema = {
+	$id: '/peerInfo',
+	type: 'object',
+	properties: {
+		ipAddress: {
+			dataType: 'string',
+			fieldNumber: 1,
+		},
+		wsPort: {
+			dataType: 'uint32',
+			fieldNumber: 2,
+		},
+		networkId: {
+			dataType: 'string',
+			fieldNumber: 3,
+		},
+		protocolVersion: {
+			dataType: 'string',
+			fieldNumber: 4,
+		},
+		nonce: {
+			dataType: 'string',
+			fieldNumber: 5,
+		},
+		os: {
+			dataType: 'string',
+			fieldNumber: 6,
+		},
+		height: {
+			dataType: 'uint32',
+			fieldNumber: 7,
+		},
+		maxHeightPrevoted: {
+			dataType: 'uint32',
+			fieldNumber: 8,
+		},
+		maxHeightPreviouslyForged: {
+			dataType: 'uint32',
+			fieldNumber: 9,
+		},
+	},
+	required: ['ipAddress', 'wsPort'],
+};
