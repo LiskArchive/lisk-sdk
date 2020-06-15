af7c8adaf0b9356d4f718bc766c04d51164e6e90
Shusetsu Toda <shusetsu@lightcurve.io>
2020-06-15 17:50:24 +0200

    :recycle: Fix process exit tests

--- framework/test/functional/specs/actions/blocks.spec.ts
+++ framework/test/functional/specs/actions/blocks.spec.ts
@@ -12,23 +12,14 @@
  * Removal or modification of this copyright notice is prohibited.
  */
 
-import { TransferTransaction } from '@liskhq/lisk-transactions';
-import {
-	createApplication,
-	closeApplication,
-	sendTransaction,
-	waitNBlocks,
-} from '../../utils/application';
+import { createApplication, closeApplication } from '../../utils/application';
 import { Application } from '../../../../src';
 
 describe('Block related actions', () => {
 	let app: Application;
-	let sentTx: TransferTransaction;
 
 	beforeAll(async () => {
 		app = await createApplication('actions-blocks');
-		sentTx = await sendTransaction(app);
-		await waitNBlocks(app, 1);
 	});
 
 	afterAll(async () => {
@@ -50,14 +41,15 @@ describe('Block related actions', () => {
 
 	describe('getBlocksByIDs', () => {
 		it('should return valid encoded blocks', async () => {
-			const encodedBlocks: string[] = await app[
-				'_channel'
-			].invoke('app:getBlocksByIDs', {
-				ids: [
-					app['_node']['_chain'].genesisBlock.header.id.toString('base64'),
-					app['_node']['_chain'].lastBlock.header.id.toString('base64'),
-				],
-			});
+			const encodedBlocks: string[] = await app['_channel'].invoke(
+				'app:getBlocksByIDs',
+				{
+					ids: [
+						app['_node']['_chain'].genesisBlock.header.id.toString('base64'),
+						app['_node']['_chain'].lastBlock.header.id.toString('base64'),
+					],
+				},
+			);
 			expect(encodedBlocks).toHaveLength(2);
 			const block = app['_node']['_chain'].dataAccess.decode(
 				Buffer.from(encodedBlocks[0], 'base64'),
@@ -90,34 +82,6 @@ describe('Block related actions', () => {
 				Buffer.from(encodedBlocks[0], 'base64'),
 			);
 			expect(block.header.height).toEqual(2);
-		});
-	});
-
-	describe('getTransactionByID', () => {
-		it('should return valid encoded transaction', async () => {
-			const encodedTx = await app['_channel'].invoke('app:getTransactionByID', {
-				id: sentTx.id.toString('base64'),
-			});
-			expect(encodedTx).toBeString();
-			const tx = app['_node']['_chain'].dataAccess.decodeTransaction(
-				Buffer.from(encodedTx as string, 'base64'),
-			);
-			expect(tx.senderPublicKey).toEqual(sentTx.senderPublicKey);
-		});
-	});
-
-	describe('getTransactionsByIDs', () => {
-		it('should return valid encoded transactions', async () => {
-			const encodedTxs: string[] = await app[
-				'_channel'
-			].invoke('app:getTransactionsByIDs', {
-				ids: [sentTx.id.toString('base64')],
-			});
-			expect(encodedTxs).toHaveLength(1);
-			const tx = app['_node']['_chain'].dataAccess.decodeTransaction(
-				Buffer.from(encodedTxs[0], 'base64'),
-			);
-			expect(tx.senderPublicKey).toEqual(sentTx.senderPublicKey);
 		});
 	});
 });
