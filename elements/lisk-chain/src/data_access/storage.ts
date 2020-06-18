07e6883eb263713dcf15402b2d5aac4d872571f8
ishantiw <ishan210788@gmail.com>
2020-06-18 16:32:29 +0200

    Expose getDiffByHeight on data access

--- elements/lisk-chain/src/data_access/storage.ts
+++ elements/lisk-chain/src/data_access/storage.ts
@@ -20,8 +20,9 @@ import {
 	getLastPrefix,
 	NotFoundError,
 } from '@liskhq/lisk-db';
+import { codec } from '@liskhq/lisk-codec';
 import { getAddressFromPublicKey, hash } from '@liskhq/lisk-cryptography';
-import { RawBlock } from '../types';
+import { RawBlock, StateDiff, DiffHistory } from '../types';
 import { StateStore } from '../state_store';
 
 import {
@@ -33,8 +34,11 @@ import {
 	DB_KEY_ACCOUNTS_ADDRESS,
 	DB_KEY_CHAIN_STATE,
 	DB_KEY_CONSENSUS_STATE,
+	DB_KEY_DIFF_STATE,
 } from './constants';
 import { keyString } from '../utils';
+import { stateDiffSchema } from '../schema';
+import { undo } from '../diff';
 
 export class Storage {
 	private readonly _db: KVStore;
@@ -435,8 +439,33 @@ export class Storage {
 		if (saveToTemp) {
 			batch.put(`${DB_KEY_TEMPBLOCKS_HEIGHT}:${heightStr}`, fullBlock);
 		}
+		// Take the diff to revert back states
+		const stateDiff = await this._getDiffByHeight(height);
+
+		const {
+			created: createdStates,
+			updated: updatedStates,
+		}: StateDiff = codec.decode(stateDiffSchema, stateDiff);
+		// Delete all the newly created states
+		for (const key of createdStates) {
+			batch.del(key);
+		}
+		for (const { key, value: diff } of updatedStates) {
+			const currentValue = await this._db.get(key);
+			const previousValue = undo(currentValue, diff as DiffHistory[]);
+			batch.put(key, previousValue);
+		}
+		// Delete stored diff at particular height
+		batch.del(`${DB_KEY_DIFF_STATE}:${height}`);
+
 		stateStore.finalize(heightStr, batch);
 		await batch.write();
+	}
+
+	private async _getDiffByHeight(height: number): Promise<Buffer> {
+		const diff = await this._db.get(`${DB_KEY_DIFF_STATE}:${height}`);
+
+		return diff;
 	}
 
 	private async _getTransactions(blockID: Buffer): Promise<Buffer[]> {
