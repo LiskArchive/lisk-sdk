d4567163da359ebc29ce2e088f3bb22031508283
Manu Nelamane Siddalingegowda <manugowda.ns@gmail.com>
2020-06-30 09:38:18 +0200

    :recycle: Fix failing framework tests

--- elements/lisk-dpos/src/types.ts
+++ elements/lisk-dpos/src/types.ts
@@ -69,6 +69,7 @@ export interface UnlockingAccountAsset {
 
 export interface Account {
 	readonly address: Buffer;
+	readonly publicKey: Buffer;
 	balance: bigint;
 	asset: {
 		delegate: DelegateAccountAsset;
