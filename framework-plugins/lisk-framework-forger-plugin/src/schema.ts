8d8be70b559fb8de6ace6f448481681624998221
Manu Nelamane Siddalingegowda <manugowda.ns@gmail.com>
2020-07-09 12:35:34 +0200

    :recycle: Add decodeRawBlock interface to calculate fee
    Update forger plugin fee calculation

--- framework-plugins/lisk-framework-forger-plugin/src/schema.ts
+++ framework-plugins/lisk-framework-forger-plugin/src/schema.ts
@@ -13,39 +13,39 @@
  */
 
 export const forgerInfoSchema = {
-  $id: '/forger/info',
-  type: 'object',
-  properties: {
-    totalProducedBlocks: {
-      dataType: 'uint32',
-      fieldNumber: 1,
-    },
-    totalReceivedFees: {
-      dataType: 'uint64',
-      fieldNumber: 2,
-    },
-    totalReceivedRewards: {
-      dataType: 'uint64',
-      fieldNumber: 3,
-    },
-    votesReceived: {
-      type: 'array',
-      items: {
-        type: 'object',
-        properties: {
-          address: {
-            dataType: 'bytes',
-            fieldNumber: 1,
-          },
-          amount: {
-            dataType: 'uint64',
-            fieldNumber: 2,
-          },
-        },
-      },
-      fieldNumber: 4,
-      required: ['address', 'amount'],
-    },
-  },
-  required: ['totalProducedBlocks', 'totalReceivedFees', 'totalReceivedRewards', 'votesReceived'],
+	$id: '/forger/info',
+	type: 'object',
+	properties: {
+		totalProducedBlocks: {
+			dataType: 'uint32',
+			fieldNumber: 1,
+		},
+		totalReceivedFees: {
+			dataType: 'uint64',
+			fieldNumber: 2,
+		},
+		totalReceivedRewards: {
+			dataType: 'uint64',
+			fieldNumber: 3,
+		},
+		votesReceived: {
+			type: 'array',
+			items: {
+				type: 'object',
+				properties: {
+					address: {
+						dataType: 'bytes',
+						fieldNumber: 1,
+					},
+					amount: {
+						dataType: 'uint64',
+						fieldNumber: 2,
+					},
+				},
+			},
+			fieldNumber: 4,
+			required: ['address', 'amount'],
+		},
+	},
+	required: ['totalProducedBlocks', 'totalReceivedFees', 'totalReceivedRewards', 'votesReceived'],
 };
