f960356736a2071cb755e12372a2698b1abe22eb
mitsuaki-u <mitsuaki.uchimoto@gmail.com>
2020-07-10 10:38:56 +0200

    Add controller for get transactions from transaction pool

--- framework-plugins/lisk-framework-http-api-plugin/src/controllers/node.ts
+++ framework-plugins/lisk-framework-http-api-plugin/src/controllers/node.ts
@@ -12,7 +12,8 @@
  * Removal or modification of this copyright notice is prohibited.
  */
 import { Request, Response, NextFunction } from 'express';
-import { BaseChannel } from 'lisk-framework';
+import { BaseChannel, PluginCodec } from 'lisk-framework';
+import { isNumberString } from '@liskhq/lisk-validator';
 
 export const getNodeInfo = (channel: BaseChannel) => async (
 	_req: Request,
@@ -25,4 +26,63 @@ export const getNodeInfo = (channel: BaseChannel) => async (
 	} catch (err) {
 		next(err);
 	}
+};
+
+export const getTransactions = (channel: BaseChannel, codec: PluginCodec) => async (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+): Promise<void> => {
+	const { limit, offset } = req.query;
+	let limitNumber = 0;
+	let offsetNumber = 0;
+	if (limit) {
+		if (!isNumberString(limit)) {
+			res.status(400).send({
+				errors: [{ message: 'The limit query parameter should be a number.' }],
+			});
+			return;
+		}
+		limitNumber = Number(limit);
+	}
+
+	if (offset) {
+		if (!isNumberString(offset)) {
+			res.status(400).send({
+				errors: [{ message: 'The offset query parameter should be a number.' }],
+			});
+			return;
+		}
+		offsetNumber = Number(offset);
+	}
+
+	let transactionsInPool;
+
+	try {
+		transactionsInPool = await channel.invoke<string>('app:getTransactionsFromPool');
+	} catch (err) {
+		next(err);
+		return;
+	}
+
+	const decodedTransactions = [];
+	for (const transaction of transactionsInPool) {
+		decodedTransactions.push(codec.decodeTransaction(transaction));
+	}
+
+	let data = decodedTransactions;
+
+	if (offset) {
+		data = decodedTransactions.slice(offsetNumber, decodedTransactions.length);
+	}
+
+	if (limit) {
+		data = data.slice(0, limitNumber);
+	}
+
+	// 200 - Response
+	res.status(200).json({
+		data,
+		meta: { limit: limitNumber, offset: offsetNumber, total: decodedTransactions.length },
+	});
 };
