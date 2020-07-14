903c62396dcbbbba6e86a07fc2bf56e79d0ff492
ishantiw <ishan210788@gmail.com>
2020-07-14 16:26:15 +0200

    Add api/delegates endpoint and controller

---
+++ framework-plugins/lisk-framework-http-api-plugin/src/controllers/delegates.ts
@@ -0,0 +1,65 @@
+/*
+ * Copyright © 2020 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+import { Request, Response, NextFunction } from 'express';
+import { validator, LiskValidationError } from '@liskhq/lisk-validator';
+import { BaseChannel, PluginCodec } from 'lisk-framework';
+import { paginateList } from '../utils';
+
+const getDelegatesQuerySchema = {
+	type: 'object',
+	properties: {
+		limit: {
+			type: 'string',
+			format: 'uint64',
+			description: 'Number of delegates to be returned',
+		},
+		offset: {
+			type: 'string',
+			format: 'uint64',
+			description: 'Offset to get delegates after a specific length in a peer list',
+		},
+	},
+	default: {
+		limit: 100,
+		offset: 0,
+	},
+};
+
+export const getDelegates = (channel: BaseChannel, codec: PluginCodec) => async (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+): Promise<void> => {
+	const errors = validator.validate(getDelegatesQuerySchema, req.query);
+	// 400 - Malformed query or parameters
+	if (errors.length) {
+		res.status(400).send({
+			errors: [{ message: new LiskValidationError([...errors]).message }],
+		});
+		return;
+	}
+
+	const { limit = 100, offset = 0 } = req.query;
+
+	try {
+		const encodedDelegates: string[] = await channel.invoke('app:getAllDelegates');
+		const decodedDelegates = encodedDelegates
+			.map(account => codec.decodeAccount(account))
+			.filter(account => account.asset.delegate.username);
+
+		res.status(200).send({ data: paginateList(decodedDelegates, +limit, +offset) });
+	} catch (err) {
+		next(err);
+	}
+};
