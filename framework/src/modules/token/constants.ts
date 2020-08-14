d9fe7dbf62f7aa19c71b611509b94fef5379ee5d
mitsuaki-u <mitsuaki.uchimoto@gmail.com>
2020-08-14 12:32:38 +0200

    Max transaction amount

--- framework/src/modules/token/constants.ts
+++ framework/src/modules/token/constants.ts
@@ -13,3 +13,4 @@
  */
 export const CHAIN_STATE_BURNT_FEE = 'burntFee';
 export const GENESIS_BLOCK_MAX_BALANCE = BigInt(2) ** BigInt(63) - BigInt(1);
+export const MAX_TRANSACTION_AMOUNT = '9223372036854775807';
