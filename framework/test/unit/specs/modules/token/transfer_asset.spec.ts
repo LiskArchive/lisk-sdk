f596a3c4b5da1588b7cd5d3ee71769cd518d59e6
mitsuaki-u <mitsuaki.uchimoto@gmail.com>
2020-08-20 11:30:57 +0200

    Fix typo

--- framework/test/unit/specs/modules/token/transfer_asset.spec.ts
+++ framework/test/unit/specs/modules/token/transfer_asset.spec.ts
@@ -11,20 +11,6 @@
  *
  * Removal or modification of this copyright notice is prohibited.
  */
-/*
- * Copyright © 2019 Lisk Foundation
- *
- * See the LICENSE file at the top-level directory of this distribution
- * for licensing information.
- *
- * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
- * no part of this software, including this file, may be copied, modified,
- * propagated, or distributed except according to the terms contained in the
- * LICENSE file.
- *
- * Removal or modification of this copyright notice is prohibited.
- *
- */
 import { Transaction, transactionSchema } from '@liskhq/lisk-chain';
 import { codec } from '@liskhq/lisk-codec';
 import { TransferAsset } from '../../../../../src/modules/token';
