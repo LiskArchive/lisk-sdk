00058c16d353ff95727281b156af776daa67e150
mitsuaki-u <mitsuaki.uchimoto@gmail.com>
2020-09-04 14:38:03 +0200

    Update error to show hex key

--- framework-plugins/lisk-framework-http-api-plugin/src/controllers/accounts.ts
+++ framework-plugins/lisk-framework-http-api-plugin/src/controllers/accounts.ts
@@ -35,7 +35,7 @@ export const getAccount = (channel: BaseChannel, codec: PluginCodec) => async (
 		});
 		res.status(200).send({ data: codec.decodeAccount(account) });
 	} catch (err) {
-		if (/^Specified key accounts:address:(.*)does not exist/.test((err as Error).message)) {
+		if ((err as Error).message.startsWith('Specified key accounts:address')) {
 			res.status(404).send({
 				errors: [{ message: `Account with address '${accountAddres}' was not found` }],
 			});
