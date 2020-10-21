d6ac80d2b3500af086baaaae9b5d143381b88dd1
Manu Nelamane Siddalingegowda <manugowda.ns@gmail.com>
2020-10-21 10:33:43 +0200

    :seedling: Add save block implementation subscribe to postBlock event and save block headers

--- framework-plugins/lisk-framework-report-misbehavior-plugin/src/db.ts
+++ framework-plugins/lisk-framework-report-misbehavior-plugin/src/db.ts
@@ -13,9 +13,30 @@
  */
 
 import { KVStore } from '@liskhq/lisk-db';
+import { codec } from '@liskhq/lisk-codec';
+import { RawBlockHeader } from '@liskhq/lisk-chain';
 import * as os from 'os';
 import { join } from 'path';
 import { ensureDir } from 'fs-extra';
+import { BlockHeaderJSON, blockHeaderSchema, RegisteredSchema } from 'lisk-framework';
+
+const blockHeadersSchema = {
+	$id: 'lisk/reportMisbehavior/blockHeaders',
+	type: 'object',
+	properties: {
+		blockHeaders: {
+			type: 'array',
+			fieldNumber: 1,
+			items: {
+				...blockHeaderSchema,
+			},
+		},
+	},
+};
+
+interface BlockHeaders {
+	readonly blockHeaders: RawBlockHeader[];
+}
 
 export const getDBInstance = async (
 	dataPath: string,
@@ -25,4 +46,42 @@ export const getDBInstance = async (
 	await ensureDir(dirPath);
 
 	return new KVStore(dirPath);
+};
+
+export const getBlockHeaders = async (
+	db: KVStore,
+	dbKeyBlockHeader: string,
+): Promise<BlockHeaders> => {
+	try {
+		const encodedBlockHeaders = await db.get(dbKeyBlockHeader);
+		return codec.decode(blockHeadersSchema, encodedBlockHeaders);
+	} catch (error) {
+		return { blockHeaders: [] };
+	}
+};
+
+const encodeBlockHeader = (schemas: RegisteredSchema, blockHeaderJSON: BlockHeaderJSON) => {
+	const { id, ...blockHeaderJSONWithoutID } = blockHeaderJSON;
+	const assetSchema = schemas.blockHeadersAssets[blockHeaderJSON.version];
+	const assetObject = codec.fromJSON(assetSchema, blockHeaderJSONWithoutID.asset);
+	const encodedAsset = codec.encode(assetSchema, assetObject);
+	const blockHeaderObject = codec.fromJSON(schemas.blockHeader, {
+		...blockHeaderJSONWithoutID,
+		asset: encodedAsset,
+	});
+	return codec.encode(schemas.blockHeader, blockHeaderObject);
+};
+
+export const saveBlockHeaders = async (
+	db: KVStore,
+	schemas: RegisteredSchema,
+	blockHeaderJSON: BlockHeaderJSON,
+): Promise<void> => {
+	const { generatorPublicKey, height } = blockHeaderJSON;
+	const dbKeyBlockHeader = `${generatorPublicKey}:${height}`;
+	const { blockHeaders } = await getBlockHeaders(db, dbKeyBlockHeader);
+	const encodedBlockHeader = codec.encode(blockHeadersSchema, {
+		blockHeaders: [...blockHeaders, encodeBlockHeader(schemas, blockHeaderJSON)],
+	});
+	await db.put(dbKeyBlockHeader, encodedBlockHeader);
 };
