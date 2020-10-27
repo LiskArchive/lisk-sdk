c8c8749da0e2106045b69335536bc702855253f6
Ishan <ishantiw.quasar@gmail.com>
2020-10-27 03:24:39 +0100

    🌱 Add majorityHeight calculation and property to networkStats

--- framework-plugins/lisk-framework-monitor-plugin/src/controllers/network.ts
+++ framework-plugins/lisk-framework-monitor-plugin/src/controllers/network.ts
@@ -13,6 +13,25 @@
  */
 import { Request, Response, NextFunction } from 'express';
 import { BaseChannel } from 'lisk-framework';
+import { PeerInfo } from '../types';
+
+const getMajorityHeight = (peers: PeerInfo[]): { height: number; count: number } => {
+	const heightHistogram = {} as { [key: number]: number };
+	const majority = {
+		height: 0,
+		count: 0,
+	};
+	for (const { options } of peers) {
+		const height = options.height as number;
+		heightHistogram[height] = heightHistogram[height] ? heightHistogram[height] + 1 : 1;
+		if (heightHistogram[height] > majority.count) {
+			majority.count = heightHistogram[height];
+			majority.height = height;
+		}
+	}
+
+	return majority;
+};
 
 export const getNetworkStats = (channel: BaseChannel) => async (
 	_req: Request,
@@ -20,9 +39,12 @@ export const getNetworkStats = (channel: BaseChannel) => async (
 	next: NextFunction,
 ): Promise<void> => {
 	try {
-		const networkStats = await channel.invoke('app:getNetworkStats');
+		const networkStats: { [key: string]: unknown } = await channel.invoke('app:getNetworkStats');
+		const connectedPeers = await channel.invoke('app:getConnectedPeers');
+		const majorityHeight = getMajorityHeight(connectedPeers as PeerInfo[]);
+		const data = { ...networkStats, majorityHeight };
 
-		res.status(200).send({ data: networkStats, meta: {} });
+		res.status(200).json({ data, meta: {} });
 	} catch (err) {
 		next(err);
 	}
