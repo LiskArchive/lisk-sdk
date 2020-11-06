8f3c4c54e39237fa111cf8ad1b16400e5ec660e6
Incede <33103370+Incede@users.noreply.github.com>
2020-11-06 10:21:37 +0100

    Fix unit tests with valid block fixture

--- elements/lisk-api-client/test/unit/block.spec.ts
+++ elements/lisk-api-client/test/unit/block.spec.ts
@@ -19,20 +19,48 @@ import {
 	blockHeaderAssetSchema,
 	transactionSchema,
 } from '@liskhq/lisk-chain';
-import { codec } from '@liskhq/lisk-codec';
 import { Channel } from '../../src/types';
 import { Block } from '../../src/block';
 
 describe('block', () => {
 	let channel: Channel;
 	let block: Block;
-
+	const sampleHeight = 1;
 	const encodedBlock =
-		'0acd01080210c38ec1fc0518a2012220c736b8cfb669ff453118230c71d7dc433797b5b30da6b9d89a14457f1b56faa12a20e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b85532209bcf519c9e0a8e66b3939f9d592b5a1728141ea3253b7d3a2424a44575c5f4e738004216087410001a1060fce85c0ca51ca1c72c589c9f651f574a40396edc8e940c5f5829d382c10cae3c2f7f24a5a9bb42ef8c545439e1a2e83951f87bc894816d7b90958b411a37b816c9e2d597dd52d7847d5a73f411ded65303';
+		'0ad601080210a7feddfc0518b0c90c2220e69286cc8efdfc794a3aabc7755415bd201832b80c29493d9f2c50e597aab4562a20e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b8553220e91e2241030b0dad7022ec05d98343394043e0f31bc63f296ed07cce35cbd3d53880cab5ee01421a08cdc80c10ecc80c1a10fe22dd75b833d917e98539f586954ba54a40fb67faefdb95a624a83cb02115c5aacd6d22bd7074e69f2b31f0a261570e27518e2d93eb2ffb1e0eaf9ab0ce5b69889672ea344ff6e4d4bdc315c299fc2d010e';
 	const encodedBlockBuffer = Buffer.from(encodedBlock, 'hex');
-
-	const blockId = '4f7e41f5744c0c2a434f13afb186b77fb4b176a5298f91ed866680ff5ef13a6d';
-	const id = Buffer.from(blockId, 'hex');
+	const sampleBlock = {
+		header: {
+			version: 2,
+			timestamp: 1603764007,
+			height: 206000,
+			previousBlockID: Buffer.from(
+				'e69286cc8efdfc794a3aabc7755415bd201832b80c29493d9f2c50e597aab456',
+				'hex',
+			),
+			transactionRoot: Buffer.from(
+				'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+				'hex',
+			),
+			generatorPublicKey: Buffer.from(
+				'e91e2241030b0dad7022ec05d98343394043e0f31bc63f296ed07cce35cbd3d5',
+				'hex',
+			),
+			reward: BigInt('500000000'),
+			asset: {
+				maxHeightPreviouslyForged: 205901,
+				maxHeightPrevoted: 205932,
+				seedReveal: Buffer.from('fe22dd75b833d917e98539f586954ba5', 'hex'),
+			},
+			signature: Buffer.from(
+				'fb67faefdb95a624a83cb02115c5aacd6d22bd7074e69f2b31f0a261570e27518e2d93eb2ffb1e0eaf9ab0ce5b69889672ea344ff6e4d4bdc315c299fc2d010e',
+				'hex',
+			),
+			id: Buffer.from('ff960b7762cad26b2b6e22e47e9aecbb933f7238ecb54ec36e212260dba82db7', 'hex'),
+		},
+		payload: [],
+	};
+	const blockId = sampleBlock.header.id;
 	const accountSchema = {
 		$id: 'accountSchema',
 		type: 'object',
@@ -82,10 +110,6 @@ describe('block', () => {
 		],
 	} as any;
 
-	const { header, payload } = codec.decode(schema.block, encodedBlockBuffer);
-	const blockHeader = codec.decode(schema.blockHeader, header);
-	const sampleBlock = { blockHeader, payload };
-
 	beforeEach(() => {
 		channel = {
 			connect: jest.fn(),
@@ -106,11 +130,13 @@ describe('block', () => {
 		describe('get', () => {
 			it('should invoke app:getBlockByID', async () => {
 				// Act
-				await block.get(id);
+				await block.get(blockId);
 
 				// Assert
 				expect(channel.invoke).toHaveBeenCalledTimes(1);
-				expect(channel.invoke).toHaveBeenCalledWith('app:getBlockByID', { id: blockId });
+				expect(channel.invoke).toHaveBeenCalledWith('app:getBlockByID', {
+					id: blockId.toString('hex'),
+				});
 			});
 		});
 
@@ -120,18 +146,20 @@ describe('block', () => {
 				await block.getByHeight(1);
 
 				// Assert
-				expect(channel.invoke).toHaveBeenCalledTimes(1);
-				expect(channel.invoke).toHaveBeenCalledWith('app:getBlockByHeight');
+				expect(channel.invoke).toHaveBeenCalledTimes(sampleHeight);
+				expect(channel.invoke).toHaveBeenCalledWith('app:getBlockByHeight', {
+					height: sampleHeight,
+				});
 			});
 		});
 
 		describe('encode', () => {
 			it('should return encoded block', () => {
 				// Act
-				const _encodedBlock = block.encode(sampleBlock as any);
+				const returnedBlock = block.encode(sampleBlock as any);
 
 				// Assert
-				expect(_encodedBlock).toEqual(encodedBlock);
+				expect(returnedBlock).toEqual(encodedBlockBuffer);
 			});
 		});
 
@@ -141,7 +169,7 @@ describe('block', () => {
 				const decodedBlock = block.decode(encodedBlockBuffer);
 
 				// Assert
-				expect(decodedBlock).toEqual(encodedBlock);
+				expect(decodedBlock).toEqual(sampleBlock);
 			});
 		});
 	});
