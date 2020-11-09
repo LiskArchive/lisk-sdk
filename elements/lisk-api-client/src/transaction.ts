1a093bd85f047730c37c4a64a8e39ad5f369e0c9
Incede <33103370+Incede@users.noreply.github.com>
2020-11-09 12:20:00 +0100

    Define namespace & utility functions for transactions in API client

---
+++ elements/lisk-api-client/src/transaction.ts
@@ -0,0 +1,172 @@
+/*
+ * Copyright © 2020 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+
+import {
+	signTransaction,
+	signMultiSignatureTransaction,
+	computeMinFee,
+} from '@liskhq/lisk-transactions';
+import { getAddressAndPublicKeyFromPassphrase } from '@liskhq/lisk-cryptography';
+import {
+	decodeTransaction,
+	encodeTransaction,
+	getTransactionAssetSchema,
+	decodeAccount,
+} from './codec';
+import { Channel, RegisteredSchemas, NodeInfo } from './types';
+
+export interface CreateTransactionOptions {
+	nonce?: bigint;
+	includeSenderSignature?: boolean;
+	multisignatureKeys?: {
+		mandatoryKeys: Buffer[];
+		optionalKeys: Buffer[];
+	};
+}
+
+export interface MultiSignatureKeys {
+	readonly mandatoryKeys: Buffer[];
+	readonly optionalKeys: Buffer[];
+	readonly numberOfSignatures: number;
+}
+
+export class Transaction {
+	private readonly _channel: Channel;
+	private readonly _schema: RegisteredSchemas;
+	private readonly _nodeInfo: NodeInfo;
+
+	public constructor(channel: Channel, registeredSchema: RegisteredSchemas, nodeInfo: NodeInfo) {
+		this._channel = channel;
+		this._schema = registeredSchema;
+		this._nodeInfo = nodeInfo;
+	}
+
+	public async create(
+		input: {
+			moduleID?: number; // id takes priority
+			moduleName?: number;
+			assetID?: number; // id takes priority
+			assetName?: number;
+			fee: bigint;
+			nonce: bigint;
+			senderPublicKey: Buffer;
+			asset: Record<string, unknown>;
+			signatures?: Buffer[];
+		},
+		passphrase: string,
+		options?: {
+			nonce?: bigint;
+			includeSenderSignature?: boolean;
+			multisignatureKeys?: {
+				mandatoryKeys: Buffer[];
+				optionalKeys: Buffer[];
+			};
+		},
+	): Promise<Record<string, unknown>> {
+		const txInput = input;
+		const networkIdentifier = Buffer.from(this._nodeInfo.networkIdentifier, 'hex');
+		// const registeredModules = this._nodeInfo.registeredModules;
+		const { publicKey, address } = getAddressAndPublicKeyFromPassphrase(passphrase);
+		const accountHex = await this._channel.invoke<string>('app:getAccount', {
+			address: address.toString('hex'),
+		});
+		const account = decodeAccount(Buffer.from(accountHex, 'hex'), this._schema);
+		if (!input.moduleID) {
+			if (!input.moduleName) {
+				throw new Error('Missing moduleId and moduleName');
+			}
+			// map moduleName to moduleId from registeredModules;
+		}
+		if (!input.assetID) {
+			if (!input.assetName) {
+				throw new Error('Missing moduleId and moduleName');
+			}
+			// map assetName to assetId from registeredModules;
+		}
+
+		if (!options?.nonce && !txInput.nonce) {
+			if (
+				typeof account.sequence !== 'object' ||
+				!(account.sequence as Record<string, unknown>).nonce
+			) {
+				throw new Error('Unspported account type.');
+			}
+			txInput.nonce = (account.sequence as { nonce: bigint }).nonce;
+		}
+		if (txInput.senderPublicKey) {
+			txInput.senderPublicKey = publicKey;
+		}
+		const assetSchema = getTransactionAssetSchema(txInput, this._schema);
+		if (account.keys && (account.keys as MultiSignatureKeys).numberOfSignatures > 0) {
+			return signMultiSignatureTransaction(
+				assetSchema,
+				txInput,
+				networkIdentifier,
+				passphrase,
+				account.keys as MultiSignatureKeys,
+				options?.includeSenderSignature,
+			);
+		}
+		if (options?.multisignatureKeys) {
+			return signMultiSignatureTransaction(
+				assetSchema,
+				txInput,
+				networkIdentifier,
+				passphrase,
+				options.multisignatureKeys,
+				options?.includeSenderSignature,
+			);
+		}
+		return signTransaction(assetSchema, txInput, networkIdentifier, passphrase);
+	}
+
+	public async get(id: Buffer): Promise<Record<string, unknown>> {
+		const transactionHex = await this._channel.invoke<string>('app:TransactionByID', {
+			id: id.toString('hex'),
+		});
+		return decodeTransaction(Buffer.from(transactionHex, 'hex'), this._schema);
+	}
+
+	public async getFromPool(): Promise<Record<string, unknown>[]> {
+		return this._channel.invoke('app:getTransactionsFromPool');
+	}
+
+	public sign(
+		transaction: Record<string, unknown>,
+		passphrases: string[],
+	): Record<string, unknown> {
+		const assetSchema = getTransactionAssetSchema(transaction, this._schema);
+		const networkIdentifier = Buffer.from(this._nodeInfo.networkIdentifier, 'hex');
+		return signTransaction(assetSchema, transaction, networkIdentifier, passphrases[0]);
+	}
+
+	public async send(transaction: Record<string, unknown>): Promise<void> {
+		const encodedTx = encodeTransaction(transaction, this._schema);
+		return this._channel.invoke('app:postTransaction', { transaction: encodedTx.toString('hex') });
+	}
+
+	public decode(transaction: Buffer): Record<string, unknown> {
+		return decodeTransaction(transaction, this._schema);
+	}
+
+	public encode(transaction: Record<string, unknown>): Buffer {
+		return encodeTransaction(transaction, this._schema);
+	}
+
+	public getMinFee(transaction: Record<string, unknown>): bigint {
+		const assetSchema = getTransactionAssetSchema(transaction, this._schema);
+		return computeMinFee(assetSchema, transaction);
+	}
+}
