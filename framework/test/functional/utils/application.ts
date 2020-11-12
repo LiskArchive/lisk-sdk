eaa45152e101262f77e0028e302599101da2c437
Nazar Hussain <nazar@lightcurve.io>
2020-11-12 13:17:22 +0100

    :white_check_mark: Fix broken funtional test

--- framework/test/functional/utils/application.ts
+++ framework/test/functional/utils/application.ts
@@ -15,7 +15,7 @@ import * as os from 'os';
 import * as fs from 'fs-extra';
 import * as path from 'path';
 import { Transaction } from '@liskhq/lisk-chain';
-import { EVENT_BLOCK_NEW } from '../../../src/constants';
+import { APP_EVENT_BLOCK_NEW } from '../../../src/constants';
 import * as genesisBlockJSON from '../../fixtures/config/devnet/genesis_block.json';
 import * as configJSON from '../../fixtures/config/devnet/config.json';
 import { Application, PartialApplicationConfig } from '../../../src';
@@ -52,7 +52,7 @@ export const createApplication = async (
 	// eslint-disable-next-line @typescript-eslint/no-floating-promises
 	await Promise.race([app.run(), new Promise(resolve => setTimeout(resolve, 3000))]);
 	await new Promise(resolve => {
-		app['_channel'].subscribe(EVENT_BLOCK_NEW, () => {
+		app['_channel'].subscribe(APP_EVENT_BLOCK_NEW, () => {
 			if (app['_node']['_chain'].lastBlock.header.height === 2) {
 				resolve();
 			}
@@ -74,7 +74,7 @@ export const getPeerID = (app: Application): string => `127.0.0.1:${app.config.n
 export const waitNBlocks = async (app: Application, n = 1): Promise<void> => {
 	const height = app['_node']['_chain'].lastBlock.header.height + n;
 	return new Promise(resolve => {
-		app['_channel'].subscribe(EVENT_BLOCK_NEW, () => {
+		app['_channel'].subscribe(APP_EVENT_BLOCK_NEW, () => {
 			if (app['_node']['_chain'].lastBlock.header.height >= height) {
 				resolve();
 			}
