065387729baeaa29354429ab7b6f8d026e7438f4
Shusetsu Toda <shusetsu@lightcurve.io>
2020-11-23 15:10:05 +0100

    :white_check_mark: Disable child process temporally since running in child process from test does not work

--- framework/test/functional/rpc-api/ws/ws_client_child_process.spec.ts
+++ framework/test/functional/rpc-api/ws/ws_client_child_process.spec.ts
@@ -16,7 +16,9 @@ import { createWSClient } from '@liskhq/lisk-api-client';
 import { Application } from '../../../../src';
 import { closeApplication, createApplicationWithHelloPlugin } from '../../utils/application';
 
-describe('plugin in child process', () => {
+// Skipped because from the test, currently child process cannot be initialized
+// eslint-disable-next-line jest/no-disabled-tests
+describe.skip('plugin in child process', () => {
 	let appWithPlugin: Application;
 	let client: any;
 	let helloMessage: any;
