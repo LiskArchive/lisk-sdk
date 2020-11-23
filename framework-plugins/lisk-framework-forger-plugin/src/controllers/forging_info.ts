2d2fdbbfd1767456ecb192a33c539adbbead9b1f
Ishan <ishantiw.quasar@gmail.com>
2020-11-23 17:52:29 +0100

    ♻️ Update forger plugin to use actions and events 🔥 Remove express api, swaggere and middlware related files and code 🌱 Add actions getForgingInfo and getVoters 🔥 Remove controller for /api/forging patch 🌱 Add events for block:created and block:missed 🔥 Remove webhooks setup and usage

--- framework-plugins/lisk-framework-forger-plugin/src/controllers/forging_info.ts
+++ framework-plugins/lisk-framework-forger-plugin/src/controllers/forging_info.ts
@@ -12,50 +12,51 @@
  * Removal or modification of this copyright notice is prohibited.
  */
 
-import { Request, Response, NextFunction } from 'express';
 import { BaseChannel, PluginCodec } from 'lisk-framework';
 import { KVStore } from '@liskhq/lisk-db';
 import { getForgerInfo } from '../db';
 import { Forger, DPoSAccountJSON } from '../types';
 
-export const getForgingInfo = (channel: BaseChannel, codec: PluginCodec, db: KVStore) => async (
-	_req: Request,
-	res: Response,
-	next: NextFunction,
-): Promise<void> => {
-	try {
-		const forgingDelegates = await channel.invoke<ReadonlyArray<Forger>>('app:getForgingStatus');
-		const encodedAccounts = await channel.invoke<string[]>('app:getAccounts', {
-			address: forgingDelegates.map(forger => forger.address),
-		});
-		const forgerAccounts = encodedAccounts.map(encodedAccount =>
-			codec.decodeAccount<DPoSAccountJSON>(encodedAccount),
-		);
+interface ForgerInfo extends Forger {
+	readonly username: string;
+	readonly totalReceivedFees: string;
+	readonly totalReceivedRewards: string;
+	readonly totalProducedBlocks: number;
+	readonly totalVotesReceived: string;
+	readonly consecutiveMissedBlocks: number;
+}
 
-		const data = [];
-		for (const forgerAccount of forgerAccounts) {
-			const forgerAddressBinary = Buffer.from(forgerAccount.address, 'hex').toString('binary');
-			const forgerInfo = await getForgerInfo(db, forgerAddressBinary);
-			const forger = forgingDelegates.find(aForger => aForger.address === forgerAccount.address);
+export const getForgingInfo = async (
+	channel: BaseChannel,
+	codec: PluginCodec,
+	db: KVStore,
+): Promise<ForgerInfo[]> => {
+	const forgingDelegates = await channel.invoke<ReadonlyArray<Forger>>('app:getForgingStatus');
+	const encodedAccounts = await channel.invoke<string[]>('app:getAccounts', {
+		address: forgingDelegates.map(forger => forger.address),
+	});
+	const forgerAccounts = encodedAccounts.map(encodedAccount =>
+		codec.decodeAccount<DPoSAccountJSON>(encodedAccount),
+	);
 
-			if (forger) {
-				data.push({
-					...forger,
-					username: forgerAccount.dpos.delegate.username,
-					totalReceivedFees: forgerInfo.totalReceivedFees.toString(),
-					totalReceivedRewards: forgerInfo.totalReceivedRewards.toString(),
-					totalProducedBlocks: forgerInfo.totalProducedBlocks,
-					totalVotesReceived: forgerAccount.dpos.delegate.totalVotesReceived,
-					consecutiveMissedBlocks: forgerAccount.dpos.delegate.consecutiveMissedBlocks,
-				});
-			}
+	const data: ForgerInfo[] = [];
+	for (const forgerAccount of forgerAccounts) {
+		const forgerAddressBinary = Buffer.from(forgerAccount.address, 'hex').toString('binary');
+		const forgerInfo = await getForgerInfo(db, forgerAddressBinary);
+		const forger = forgingDelegates.find(aForger => aForger.address === forgerAccount.address);
+
+		if (forger) {
+			data.push({
+				...forger,
+				username: forgerAccount.dpos.delegate.username,
+				totalReceivedFees: forgerInfo.totalReceivedFees.toString(),
+				totalReceivedRewards: forgerInfo.totalReceivedRewards.toString(),
+				totalProducedBlocks: forgerInfo.totalProducedBlocks,
+				totalVotesReceived: forgerAccount.dpos.delegate.totalVotesReceived,
+				consecutiveMissedBlocks: forgerAccount.dpos.delegate.consecutiveMissedBlocks,
+			});
 		}
-
-		res.status(200).json({
-			data,
-			meta: { count: data.length },
-		});
-	} catch (err) {
-		next(err);
 	}
+
+	return data;
 };
