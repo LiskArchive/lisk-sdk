440146af4bed2dbf6d6457706070f3b7cc65b25b
Nazar Hussain <nazar@lightcurve.io>
2020-11-24 11:43:37 +0100

    :art: Update event interface to use data directly and not info object

--- framework/src/controller/event.ts
+++ framework/src/controller/event.ts
@@ -16,22 +16,16 @@ import { strict as assert } from 'assert';
 import { eventWithModuleNameReg } from '../constants';
 import { NotificationRequest, VERSION } from './jsonrpc';
 
-export interface EventInfoObject {
-	readonly module: string;
-	readonly name: string;
-	readonly data?: object;
-}
-
-export type EventCallback = (event: EventInfoObject) => void | Promise<void>;
+export type EventCallback = (data?: Record<string, unknown>) => void | Promise<void>;
 
 export type EventsArray = ReadonlyArray<string>;
 
 export class Event {
 	public readonly module: string;
 	public readonly name: string;
-	public readonly data?: object;
+	public readonly data?: Record<string, unknown>;
 
-	public constructor(name: string, data?: object) {
+	public constructor(name: string, data?: Record<string, unknown>) {
 		assert(
 			eventWithModuleNameReg.test(name),
 			`Event name "${name}" must be a valid name with module name and event name.`,
@@ -55,14 +49,6 @@ export class Event {
 			jsonrpc: VERSION,
 			method: `${this.module}:${this.name}`,
 			params: this.data,
-		};
-	}
-
-	public toObject(): EventInfoObject {
-		return {
-			module: this.module,
-			name: this.name,
-			data: this.data,
 		};
 	}
 
