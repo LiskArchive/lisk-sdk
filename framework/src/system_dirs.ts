6d38bab43b4948bb034f2ad56dfe8ca4132aabba
Incede <33103370+Incede@users.noreply.github.com>
2020-12-08 13:35:52 +0100

    Apply PR feedback

--- framework/src/system_dirs.ts
+++ framework/src/system_dirs.ts
@@ -16,10 +16,10 @@ import * as path from 'path';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types,@typescript-eslint/explicit-function-return-type
 export const systemDirs = (appLabel: string, rootPath: string) => ({
-	dataPath: path.resolve(rootPath, appLabel),
-	data: path.resolve(rootPath, appLabel, 'data'),
-	tmp: path.resolve(rootPath, appLabel, 'tmp'),
-	logs: path.resolve(rootPath, appLabel, 'logs'),
-	sockets: path.resolve(rootPath, appLabel, 'tmp', 'sockets'),
-	pids: path.resolve(rootPath, appLabel, 'tmp', 'pids'),
+	dataPath: path.resolve(path.join(rootPath, appLabel)),
+	data: path.resolve(path.join(rootPath, appLabel, 'data')),
+	tmp: path.resolve(path.join(rootPath, appLabel, 'tmp')),
+	logs: path.resolve(path.join(rootPath, appLabel, 'logs')),
+	sockets: path.resolve(path.join(rootPath, appLabel, 'tmp', 'sockets')),
+	pids: path.resolve(path.join(rootPath, appLabel, 'tmp', 'pids')),
 });
