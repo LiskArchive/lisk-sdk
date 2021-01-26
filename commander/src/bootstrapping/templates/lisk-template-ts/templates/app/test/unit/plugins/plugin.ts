5dd8d698512c0e1610fbd824cab537d42fdf32ff
Mitsuaki Uchimoto <mitsuaki@binarymail.me>
2021-01-26 11:14:13 +0100

    Create Plugin test template

---
+++ commander/src/bootstrapping/templates/lisk-template-ts/templates/app/test/unit/plugins/plugin.ts
@@ -0,0 +1,33 @@
+/*
+ * LiskHQ/lisk-commander
+ * Copyright © 2021 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+
+// import * as plugins from '../../../src/app/plugins'
+
+describe('<%= className %>Plugin', () => {
+    beforeEach(() => {});
+    describe('alias', () => {
+		it.todo('should have valid alias');
+    });
+    describe('info', () => {
+        it.todo('should have author');
+        it.todo('should have name');
+        it.todo('should have version');
+    });
+    describe('events', () => {});
+    describe('actions', () => {});
+    describe('load', () => {});
+    describe('unload', () => {});
+});
