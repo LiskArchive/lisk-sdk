44a5903eafd1cd3e940726969cc24dc8c13dacda
Ishan <ishantiw.quasar@gmail.com>
2021-01-26 11:16:10 +0100

    🔥 Remove duplicate lisk:init from env

--- commander/src/bootstrapping/env.ts
+++ commander/src/bootstrapping/env.ts
@@ -19,7 +19,6 @@ import yeoman from 'yeoman-environment';
 const env = yeoman.createEnv();
 
 env.register(require.resolve('./generators/init_generator'), 'lisk:init');
-env.register(require.resolve('./generators/init'), 'lisk:init');
 env.register(require.resolve('./generators/generate/modules'), 'lisk:generate:module');
 
 export { env };
