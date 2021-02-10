6f385dc9e4045158a03e33808acecc63c4dc2f78
Ishan <ishantiw.quasar@gmail.com>
2021-02-10 17:40:10 +0100

    ♻️  Use IPC server only when IPC is enabled ♻️  consoleLogLevel->info as default value

--- framework/src/controller/bus.ts
+++ framework/src/controller/bus.ts
@@ -311,13 +311,15 @@ export class Bus {
 		this._emitter.emit(eventName, notification);
 
 		// Communicate through unix socket
-		try {
-			this._ipcServer.pubSocket.send(notification);
-		} catch (error) {
-			this.logger.debug(
-				{ err: error as Error },
-				`Failed to publish event: ${eventName} to ipc server.`,
-			);
+		if (this.config.rpc.enable) {
+			try {
+				this._ipcServer.pubSocket.send(notification);
+			} catch (error) {
+				this.logger.debug(
+					{ err: error as Error },
+					`Failed to publish event: ${eventName} to ipc server.`,
+				);
+			}
 		}
 
 		if (this.config.rpc.enable && this.config.rpc.mode === 'ws') {
