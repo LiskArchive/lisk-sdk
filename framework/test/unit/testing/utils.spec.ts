001223c3057e7df25df7f05a90ac6a4753670d01
Mitsuaki Uchimoto <mitsuaki@binarymail.me>
2021-02-22 16:36:30 +0100

    Add waitUntilBlockHeight tests

--- framework/test/unit/testing/utils.spec.ts
+++ framework/test/unit/testing/utils.spec.ts
@@ -11,9 +11,12 @@
  *
  * Removal or modification of this copyright notice is prohibited.
  */
-
 import { SequenceModule, TokenModule } from '../../../src';
-import { getAccountSchemaFromModules, getModuleInstance } from '../../../src/testing/utils';
+import {
+	getAccountSchemaFromModules,
+	getModuleInstance,
+	waitUntilBlockHeight,
+} from '../../../src/testing/utils';
 
 describe('utils', () => {
 	describe('getAccountSchemaFromModules()', () => {
@@ -22,7 +25,7 @@ describe('utils', () => {
 		});
 	});
 
-	describe('getModuleInstance()', () => {
+	describe('getModuleInstance', () => {
 		it('should create module instance with default mocks', () => {
 			const module = getModuleInstance(TokenModule);
 
@@ -65,6 +68,26 @@ describe('utils', () => {
 
 			expect(module).toBeInstanceOf(TokenModule);
 			expect(module['_channel']).toBe(channel);
+		});
+	});
+
+	describe('waitUntilBlockHeight', () => {
+		const apiClient = {
+			// eslint-disable-next-line @typescript-eslint/require-await
+			subscribe: jest.fn(async (_, callback) => callback({ block: 'blockdata' })),
+			block: {
+				decode: jest.fn().mockReturnValue({ header: { height: 2 } }),
+			},
+		} as any;
+
+		it('should resolve after input height', async () => {
+			await expect(waitUntilBlockHeight({ apiClient, height: 1 })).resolves.toBeUndefined();
+		});
+
+		it('should timeout', async () => {
+			await expect(
+				waitUntilBlockHeight({ apiClient, height: 1, timeout: 0 }),
+			).resolves.toBeUndefined();
 		});
 	});
 });
