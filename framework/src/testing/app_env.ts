383bd2da8301ed7a55487032b14a1cff8c70b52e
Ishan <ishantiw.quasar@gmail.com>
2021-03-04 14:30:08 +0100

    🌱 Create ApplicationEnv class for managing app for testing purpose

--- framework/src/testing/app_env.ts
+++ framework/src/testing/app_env.ts
@@ -23,51 +23,78 @@ import { PartialApplicationConfig } from '../types';
 import { Application } from '../application';
 import { DPoSModule } from '../modules/dpos';
 
-interface GetApplicationEnv {
+interface ApplicationEnvConfig {
 	modules: ModuleClass[];
 	plugins?: PluginClass[];
 	config?: PartialApplicationConfig;
 	genesisBlock?: Record<string, unknown>;
 }
 
-interface ApplicationEnv {
-	apiClient: APIClient;
-	application: Application;
-}
+export class ApplicationEnv {
+	private _application!: Application;
+	private _dataPath!: string;
+	private _ipcClient!: APIClient;
 
-export const getApplicationEnv = async (params: GetApplicationEnv): Promise<ApplicationEnv> => {
-	// As we can call this function with different configuration
-	// so we need to make sure existing schemas are already clear
-	codec.clearCache();
-
-	// TODO: Remove this dependency in future
-	if (!params.modules.includes(DPoSModule)) {
-		params.modules.push(DPoSModule);
+	public constructor(appConfig: ApplicationEnvConfig) {
+		this._initApplication(appConfig);
 	}
-	const { genesisBlockJSON } = createGenesisBlockWithAccounts(params.modules);
-	const config = params.config ?? (defaultConfig as PartialApplicationConfig);
-	const { label } = config;
 
-	const application = new Application(params.genesisBlock ?? genesisBlockJSON, config);
-	params.modules.map(module => application.registerModule(module));
-	params.plugins?.map(plugin => application.registerPlugin(plugin));
-	await Promise.race([application.run(), new Promise(resolve => setTimeout(resolve, 3000))]);
+	public get application(): Application {
+		return this._application;
+	}
 
-	const dataPath = join(application.config.rootPath, label as string);
-	const apiClient = await createIPCClient(dataPath);
+	public get ipcClient(): APIClient {
+		return this._ipcClient;
+	}
 
-	return {
-		apiClient,
-		application,
-	};
-};
+	public get dataPath(): string {
+		return this._dataPath;
+	}
 
-/* eslint-disable @typescript-eslint/prefer-ts-expect-error */
-/* eslint-disable dot-notation */
+	public get networkIdentifier(): Buffer {
+		return this._application.networkIdentifier;
+	}
 
-export const clearApplicationEnv = async (appEnv: ApplicationEnv): Promise<void> => {
-	await appEnv.application['_forgerDB'].clear();
-	await appEnv.application['_blockchainDB'].clear();
-	await appEnv.application['_nodeDB'].clear();
-	await appEnv.application.shutdown();
-};
+	public async startApplication(): Promise<void> {
+		await Promise.race([
+			this._application.run(),
+			new Promise(resolve => setTimeout(resolve, 3000)),
+		]);
+		this._ipcClient = await createIPCClient(this._dataPath);
+	}
+
+	public async stopApplication(options: { clearDB: boolean } = { clearDB: true }): Promise<void> {
+		if (options.clearDB) {
+			// eslint-disable-next-line dot-notation
+			await this._application['_forgerDB'].clear();
+			// eslint-disable-next-line dot-notation
+			await this._application['_blockchainDB'].clear();
+			// eslint-disable-next-line dot-notation
+			await this._application['_nodeDB'].clear();
+		}
+		await this._ipcClient.disconnect();
+		await this._application.shutdown();
+	}
+
+	private _initApplication(appConfig: ApplicationEnvConfig): Application {
+		// As we can call this function with different configuration
+		// so we need to make sure existing schemas are already clear
+		codec.clearCache();
+
+		// TODO: Remove this dependency in future
+		if (!appConfig.modules.includes(DPoSModule)) {
+			appConfig.modules.push(DPoSModule);
+		}
+		const { genesisBlockJSON } = createGenesisBlockWithAccounts(appConfig.modules);
+		const config = appConfig.config ?? (defaultConfig as PartialApplicationConfig);
+		const { label } = config;
+
+		const application = new Application(appConfig.genesisBlock ?? genesisBlockJSON, config);
+		appConfig.modules.map(module => application.registerModule(module));
+		appConfig.plugins?.map(plugin => application.registerPlugin(plugin));
+		this._dataPath = join(application.config.rootPath, label as string);
+
+		this._application = application;
+		return application;
+	}
+}
