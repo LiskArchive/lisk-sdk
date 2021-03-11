4a82408934933ec0189e8193095fa5c69b5042c0
Mitsuaki Uchimoto <mitsuaki@binarymail.me>
2021-03-11 10:37:37 +0100

    Add fund tokens action to faucet

--- framework-plugins/lisk-framework-faucet-plugin/src/plugin/faucet_plugin.ts
+++ framework-plugins/lisk-framework-faucet-plugin/src/plugin/faucet_plugin.ts
@@ -12,11 +12,14 @@
  * Removal or modification of this copyright notice is prohibited.
  */
 
+import { Transaction } from '@liskhq/lisk-chain';
+import { codec } from '@liskhq/lisk-codec';
 import { validator, LiskValidationError } from '@liskhq/lisk-validator';
 import {
 	decryptPassphraseWithPassword,
 	parseEncryptedPassphrase,
 	getAddressAndPublicKeyFromPassphrase,
+	signData,
 } from '@liskhq/lisk-cryptography';
 import { objects } from '@liskhq/lisk-utils';
 import {
@@ -33,7 +36,7 @@ import { FaucetPluginOptions, State } from './types';
 // eslint-disable-next-line
 const packageJSON = require('../../package.json');
 
-const actionParamsSchema = {
+const authorizeParamsSchema = {
 	$id: 'lisk/faucet/auth',
 	type: 'object',
 	required: ['password', 'enable'],
@@ -47,9 +50,22 @@ const actionParamsSchema = {
 	},
 };
 
+const fundParamsSchema = {
+	$id: 'lisk/faucet/fund',
+	type: 'object',
+	required: ['address'],
+	properties: {
+		address: {
+			type: 'string',
+			format: 'hex',
+		},
+	},
+};
+
 export class FaucetPlugin extends BasePlugin {
 	private _options!: FaucetPluginOptions;
-	private readonly _state: State = { publicKey: Buffer.alloc(0), passphrase: '' };
+	private _channel!: BaseChannel;
+	private readonly _state: State = { publicKey: undefined, passphrase: undefined };
 
 	// eslint-disable-next-line @typescript-eslint/class-literal-property-style
 	public static get alias(): string {
@@ -82,7 +98,7 @@ export class FaucetPlugin extends BasePlugin {
 	public get actions(): ActionsDefinition {
 		return {
 			authorize: (params?: Record<string, unknown>): { result: string } => {
-				const errors = validator.validate(actionParamsSchema, params as Record<string, unknown>);
+				const errors = validator.validate(authorizeParamsSchema, params as Record<string, unknown>);
 
 				if (errors.length) {
 					throw new LiskValidationError([...errors]);
@@ -120,14 +136,91 @@ export class FaucetPlugin extends BasePlugin {
 					throw new Error('Password given is not valid.');
 				}
 			},
+			fundTokens: async (params?: Record<string, unknown>): Promise<{ result: string }> => {
+				const errors = validator.validate(fundParamsSchema, params as Record<string, unknown>);
+
+				if (errors.length) {
+					throw new LiskValidationError([...errors]);
+				}
+
+				if (!this._state.publicKey || !this._state.passphrase) {
+					throw new Error('Faucet is not enabled.');
+				}
+
+				const { address } = params as Record<string, unknown>;
+				const encodedTransaction = await this._createTransferTransaction(address as string);
+				await this._channel.invoke<{
+					transactionId?: string;
+				}>('app:postTransaction', {
+					transaction: encodedTransaction,
+				});
+
+				return {
+					result: `Successfully funded account at address: ${address as string}.`,
+				};
+			},
 		};
 	}
 
-	// eslint-disable-next-line @typescript-eslint/require-await, class-methods-use-this, @typescript-eslint/no-empty-function
-	public async load(_channel: BaseChannel): Promise<void> {
+	// eslint-disable-next-line @typescript-eslint/require-await, class-methods-use-this
+	public async load(channel: BaseChannel): Promise<void> {
+		this._channel = channel;
 		this._options = objects.mergeDeep({}, defaults.config, this._options) as FaucetPluginOptions;
 	}
 
 	// eslint-disable-next-line class-methods-use-this, @typescript-eslint/no-empty-function
 	public async unload(): Promise<void> {}
+
+	private async _createTransferTransaction(address: string): Promise<string> {
+		// ModuleID: 2 (Token), AssetID:0 (TransferAsset)
+		const transferAssetInfo = this.schemas.transactionsAssets.find(
+			({ moduleID, assetID }) => moduleID === 2 && assetID === 0,
+		);
+
+		if (!transferAssetInfo) {
+			throw new Error('Transfer asset schema is not registered in the application.');
+		}
+
+		const encodedAccount = await this._channel.invoke<string>('app:getAccount', {
+			address,
+		});
+
+		const {
+			sequence: { nonce },
+		} = codec.decode<{ sequence: { nonce: bigint } }>(
+			this.schemas.account,
+			Buffer.from(encodedAccount, 'hex'),
+		);
+
+		const transferTransactionAsset = {
+			amount: BigInt(this._options.amount),
+			recipientAddress: Buffer.from(address, 'hex'),
+			data: '',
+		};
+
+		const { networkIdentifier } = await this._channel.invoke<{ networkIdentifier: string }>(
+			'app:getNodeInfo',
+		);
+
+		const encodedAsset = codec.encode(transferAssetInfo.schema, transferTransactionAsset);
+
+		const tx = new Transaction({
+			moduleID: transferAssetInfo.moduleID,
+			assetID: transferAssetInfo.assetID,
+			nonce,
+			senderPublicKey: this._state.publicKey as Buffer,
+			fee: BigInt(this._options.fee), // TODO: The static fee should be replaced by fee estimation calculation
+			asset: encodedAsset,
+			signatures: [],
+		});
+
+		(tx.signatures as Buffer[]).push(
+			signData(
+				Buffer.concat([Buffer.from(networkIdentifier, 'hex'), tx.getSigningBytes()]),
+				this._state.passphrase as string,
+			),
+		);
+
+		return tx.getBytes().toString('hex');
+	}
 }
