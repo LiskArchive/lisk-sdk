2028faffae06cc0cea63cc4a3fe0c2d4c8dd62c8
Nazar Hussain <nazar@lightcurve.io>
2021-03-22 16:17:14 +0100

    :sparkles: Add icon component

---
+++ framework-plugins/lisk-framework-dashboard-plugin/src/ui/components/Icon/index.tsx
@@ -0,0 +1,30 @@
+/*
+ * Copyright © 2021 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+import * as React from 'react';
+import * as styles from './Icon.module.scss';
+
+interface Props {
+	name: string;
+	size?: 's' | 'm' | 'l' | 'xl';
+}
+
+const Icon: React.FC<Props> = props => {
+	const { name } = props;
+	const size = props.size ?? 'm';
+	return (
+		<span className={` ${styles.default.icon} ${styles.default[`icon-${size}`]}`}>{name}</span>
+	);
+};
+
+export default Icon;
