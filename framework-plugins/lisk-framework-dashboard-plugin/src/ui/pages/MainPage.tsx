48e5a8548827a944847a5e0afddbd2f75c5f86b7
Mitsuaki Uchimoto <mitsuaki@binarymail.me>
2021-04-01 10:11:28 +0200

    Update MainPage.tsx

--- framework-plugins/lisk-framework-dashboard-plugin/src/ui/pages/MainPage.tsx
+++ framework-plugins/lisk-framework-dashboard-plugin/src/ui/pages/MainPage.tsx
@@ -40,25 +40,16 @@ const MainPage: React.FC = () => {
 	const [peersInfoDialog, setPeersInfoDialog] = React.useState(false);
 	const accounts = [
 		{
-			"passphrase": "gospel taxi search output south uncover gaze glue apple garden pill master",
-			"privateKey": "294752900c9bc77600d6d2f5b9f92d0bdece28d9405c3c40506b90c1afe2d607a6b97e7960647cf8123c7da31a6d9304843eb1c7a869e98fad169692eabd79a4",
 			"publicKey": "a6b97e7960647cf8123c7da31a6d9304843eb1c7a869e98fad169692eabd79a4",
 			"binaryAddress": "a76ede56e69333382c6d4fd721dee0fe328318a2",
-			"address": "lskkhw9jqwbroatu5c94g5rcwwzg2a3bbmvgomnsu"
 		},
 		{
-			"passphrase": "gospel taxi search output south uncover gaze glue apple garden pill master",
 			"privateKey": "294752900c9bc77600d6d2f5b9f92d0bdece28d9405c3c40506b90c1afe2d607a6b97e7960647cf8123c7da31a6d9304843eb1c7a869e98fad169692eabd79a4",
 			"publicKey": "a6b97e7960647cf8123c7da31a6d9304843eb1c7a869e98fad169692eabd79a4",
-			"binaryAddress": "a76ede56e69333382c6d4fd721dee0fe328318a2",
-			"address": "lskkhw9jqwbroatu5c94g5rcwwzg2a3bbmvgomnsu"
 		},
 		{
-			"passphrase": "gospel taxi search output south uncover gaze glue apple garden pill master",
-			"privateKey": "294752900c9bc77600d6d2f5b9f92d0bdece28d9405c3c40506b90c1afe2d607a6b97e7960647cf8123c7da31a6d9304843eb1c7a869e98fad169692eabd79a4",
 			"publicKey": "a6b97e7960647cf8123c7da31a6d9304843eb1c7a869e98fad169692eabd79a4",
 			"binaryAddress": "a76ede56e69333382c6d4fd721dee0fe328318a2",
-			"address": "lskkhw9jqwbroatu5c94g5rcwwzg2a3bbmvgomnsu"
 		},
 	];
 
