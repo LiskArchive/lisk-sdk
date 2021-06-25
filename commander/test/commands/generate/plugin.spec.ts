6c4e18a5a385021a157166bb015a413a7fb89531
Nazar Hussain <nazar@lightcurve.io>
2021-06-25 16:49:14 +0200

    :white_check_mark: Add unit tests for generate commands

--- commander/test/commands/generate/plugin.spec.ts
+++ commander/test/commands/generate/plugin.spec.ts
@@ -16,10 +16,10 @@
 
 import * as Config from '@oclif/config';
 import BaseBootstrapCommand from '../../../src/base_bootstrap_command';
-import ModuleCommand from '../../../src/commands/generate/module';
+import PluginCommand from '../../../src/commands/generate/plugin';
 import { getConfig } from '../../helpers/config';
 
-describe('generate:module command', () => {
+describe('generate:plugin command', () => {
 	let stdout: string[];
 	let stderr: string[];
 	let config: Config.IConfig;
@@ -32,34 +32,24 @@ describe('generate:module command', () => {
 		jest.spyOn(process.stderr, 'write').mockImplementation(val => stderr.push(val as string) > -1);
 	});
 
-	describe('generate:module', () => {
+	describe('generate:plugin', () => {
 		it('should throw an error when all arg is not provided', async () => {
-			await expect(ModuleCommand.run([], config)).rejects.toThrow('Missing 2 required arg');
-		});
-
-		it('should throw an error when a arg is not provided', async () => {
-			await expect(ModuleCommand.run(['nft'], config)).rejects.toThrow('Missing 1 required arg');
+			await expect(PluginCommand.run([], config)).rejects.toThrow('Missing 1 required arg');
 		});
 	});
 
-	describe('generate:module invalidModuleName invalidModuleID', () => {
+	describe('generate:plugin invalidAlias', () => {
 		it('should throw an error when module name is invalid', async () => {
-			await expect(ModuleCommand.run(['nft$5', '1001'], config)).rejects.toThrow(
-				'Invalid module name',
-			);
-		});
-
-		it('should throw an error when module ID is invalid', async () => {
-			await expect(ModuleCommand.run(['nft', '5r'], config)).rejects.toThrow('Invalid module ID');
+			await expect(PluginCommand.run(['http$5'], config)).rejects.toThrow('Invalid plugin alias');
 		});
 	});
 
-	describe('generate:module should check app directory', () => {
+	describe('generate:plugin should check app directory', () => {
 		it('should throw error if cwd is not a lisk app directory', async () => {
 			jest.spyOn<any, any>(BaseBootstrapCommand.prototype, '_isLiskAppDir').mockReturnValue(false);
 			jest.spyOn(process, 'cwd').mockReturnValue('/my/dir');
 
-			await expect(ModuleCommand.run(['nft', '1001'], config)).rejects.toThrow(
+			await expect(PluginCommand.run(['httpPlugin'], config)).rejects.toThrow(
 				'You can run this command only in lisk app directory. Run "lisk init --help" command for more details.',
 			);
 			expect(BaseBootstrapCommand.prototype['_isLiskAppDir']).toHaveBeenCalledWith('/my/dir');
@@ -72,11 +62,30 @@ describe('generate:module command', () => {
 				.mockResolvedValue(null as never);
 			jest.spyOn(process, 'cwd').mockReturnValue('/my/dir');
 
-			await expect(ModuleCommand.run(['nft', '1001'], config)).resolves.toBeNull();
+			await expect(PluginCommand.run(['httpPlugin'], config)).resolves.toBeNull();
 			expect(BaseBootstrapCommand.prototype['_isLiskAppDir']).toHaveBeenCalledWith('/my/dir');
 			expect(
 				BaseBootstrapCommand.prototype['_runBootstrapCommand'],
-			).toHaveBeenCalledWith('lisk:generate:module', { moduleID: '1001', moduleName: 'nft' });
+			).toHaveBeenCalledWith('lisk:generate:plugin', { alias: 'httpPlugin' });
+		});
+
+		it('should not throw error if cwd is not lisk app directory and --standalone was provided', async () => {
+			jest.spyOn<any, any>(BaseBootstrapCommand.prototype, '_isLiskAppDir');
+			jest
+				.spyOn<any, any>(BaseBootstrapCommand.prototype, '_runBootstrapCommand')
+				.mockResolvedValue(null as never);
+
+			await expect(
+				PluginCommand.run(['httpPlugin', '--standalone', '--output', '/my/dir'], config),
+			).resolves.toBeNull();
+			expect(BaseBootstrapCommand.prototype['_isLiskAppDir']).not.toHaveBeenCalled();
+			expect(BaseBootstrapCommand.prototype['_runBootstrapCommand']).toHaveBeenCalledWith(
+				'lisk:init:plugin',
+				{
+					alias: 'httpPlugin',
+					projectPath: '/my/dir',
+				},
+			);
 		});
 	});
 });
