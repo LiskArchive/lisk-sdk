4214ba9bfdac47e00a4732ad25194956874138e3
Mitsuaki Uchimoto <mitsuaki@binarymail.me>
2021-07-06 17:36:43 +0200

    Create utils.spec.ts

---
+++ elements/lisk-tree/test/merkle_tree/utils.spec.ts
@@ -0,0 +1,54 @@
+/*
+ * LiskHQ/lisk-commander
+ * Copyright © 2021 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+
+import * as fixture from '../fixtures/transaction_merkle_root/transaction_merkle_root.json';
+import { MerkleTree } from '../../src/merkle_tree/merkle_tree';
+import { calculateMerkleRoot } from '../../src/merkle_tree/utils';
+
+describe('utils', () => {
+        describe('calculateLightMerkleRoot', () => {
+            describe('should generate correct merkle root info', () => {
+                // Skip the case of empty tree since there is no value to pop
+                for (const test of fixture.testCases.slice(1)) {
+                    describe(test.description, () => {
+                        describe(test.description, () => {
+                            const fullTree = new MerkleTree();
+                            const partialTree = new MerkleTree();
+                            const transactionIds = test.input.transactionIds.map(hexString => Buffer.from(hexString, 'hex'));
+                            let valueToAppend: Buffer;
+
+                            beforeAll(async () => {
+                                await fullTree.init(transactionIds);
+                                valueToAppend = transactionIds.pop() as Buffer;
+                                await partialTree.init(transactionIds);
+                            });
+            
+                            it('should return correct merkle root appendPath', async () => {
+                                const previousAppendPath = await partialTree.getAppendPathHashes();
+                                const previousSize = partialTree.size;
+                                const { root, appendPath, size } = calculateMerkleRoot({ value: valueToAppend, appendPath: previousAppendPath, size: previousSize }); 
+                                const expectedAppendPath = await fullTree.getAppendPathHashes();
+
+                                expect(root).toEqual(fullTree.root);
+                                expect(appendPath).toEqual(expectedAppendPath);
+                                expect(size).toEqual(fullTree.size);
+                            });
+                        });
+                    });
+                }
+            });
+        });
+});
