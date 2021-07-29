3a99f77e9c66b2617582180350d82489c4f2e15f
Nazar Hussain <nazar@lightcurve.io>
2021-07-29 16:59:52 +0200

    :white_check_mark: Add and update tests and snapshots

--- framework/test/unit/schema/application_schema.spec.ts
+++ framework/test/unit/schema/application_schema.spec.ts
@@ -12,10 +12,37 @@
  * Removal or modification of this copyright notice is prohibited.
  */
 
-import * as applicationSchema from '../../../src/schema/application_config_schema';
+import { validator } from '@liskhq/lisk-validator';
+import { objects } from '@liskhq/lisk-utils';
+import { applicationConfigSchema } from '../../../src/schema/application_config_schema';
 
 describe('schema/application_config_schema.js', () => {
 	it('application config schema must match to the snapshot.', () => {
-		expect(applicationSchema).toMatchSnapshot();
+		expect(applicationConfigSchema).toMatchSnapshot();
+	});
+
+	it('should validate if module properties are objects', () => {
+		const config = objects.cloneDeep(applicationConfigSchema.default);
+		config.genesis.modules = { myModule: { myProp: 1 } };
+
+		const errors = validator.validate(applicationConfigSchema, config);
+
+		expect(errors).toHaveLength(0);
+	});
+
+	it('should not validate if module properties are not objects', () => {
+		const config = objects.cloneDeep(applicationConfigSchema.default);
+		config.genesis.modules = { myModule: 10 };
+
+		const errors2 = validator.validate(applicationConfigSchema, config);
+
+		expect(errors2).toHaveLength(1);
+		expect(errors2[0]).toEqual(
+			expect.objectContaining({
+				dataPath: '.genesis.modules.myModule',
+				keyword: 'type',
+				message: 'must be object',
+			}),
+		);
 	});
 });
