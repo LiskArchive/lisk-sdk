293bcde8ae86fbd272d2376d49ee55a7b9dd6a80
Ishan <ishantiw.quasar@gmail.com>
2021-08-18 12:25:55 +0200

    🌱 Update ipc socket, client and server to use zeroMQ - Update ipc socket to assign socket path - Update server to bind to pub/sub sockets - Update client to connect to pub/sub sockets

--- framework/src/controller/ipc/ipc_client.ts
+++ framework/src/controller/ipc/ipc_client.ts
@@ -12,96 +12,35 @@
  * Removal or modification of this copyright notice is prohibited.
  */
 
-// eslint-disable-next-line
-/// <reference path="../../../external_types/pm2-axon/index.d.ts" />
-// eslint-disable-next-line
-/// <reference path="../../../external_types/pm2-axon-rpc/index.d.ts" />
-
-import * as axon from 'pm2-axon';
-import { PushSocket, ReqSocket, SubSocket } from 'pm2-axon';
-import { Client as RPCClient } from 'pm2-axon-rpc';
+import { Publisher, Subscriber } from 'zeromq';
 import { IPCSocket } from './ipc_socket';
 
 const CONNECTION_TIME_OUT = 2000;
 
 export class IPCClient extends IPCSocket {
-	public rpcClient!: RPCClient;
-	protected readonly _actionRPCConnectingServerSocketPath: string;
 
-	public constructor(options: { socketsDir: string; name: string; rpcServerSocketPath: string }) {
+	public constructor(options: { socketsDir: string; name: string; }) {
 		super(options);
 
-		this._actionRPCConnectingServerSocketPath = options.rpcServerSocketPath;
-
-		this.pubSocket = axon.socket('push', {}) as PushSocket;
-		this.subSocket = axon.socket('sub', {}) as SubSocket;
-		this.rpcClient = new RPCClient(axon.socket('req') as ReqSocket);
+		this.pubSocket = new Publisher();
+		this.subSocket = new Subscriber();
 	}
 
 	public async start(): Promise<void> {
-		await super.start();
-
-		await new Promise<void>((resolve, reject) => {
-			const timeout = setTimeout(() => {
-				reject(
-					new Error('IPC Socket client connection timeout. Please check if IPC server is running.'),
-				);
-			}, CONNECTION_TIME_OUT);
-			this.pubSocket.on('connect', () => {
-				clearTimeout(timeout);
-				resolve();
-			});
-			this.pubSocket.on('error', reject);
-
-			// We switched the path here to establish communication
-			// The socket on which server is observing clients will publish
+		try {
+			this.pubSocket.connectTimeout = CONNECTION_TIME_OUT;
+			this.subSocket.connectTimeout = CONNECTION_TIME_OUT;
+			// Connect to sub socket of the server to publish
 			this.pubSocket.connect(this._eventSubSocketPath);
-		}).finally(() => {
-			this.pubSocket.removeAllListeners('connect');
-			this.pubSocket.removeAllListeners('error');
-		});
-
-		await new Promise<void>((resolve, reject) => {
-			const timeout = setTimeout(() => {
-				reject(
-					new Error('IPC Socket client connection timeout. Please check if IPC server is running.'),
-				);
-			}, CONNECTION_TIME_OUT);
-			this.subSocket.on('connect', () => {
-				clearTimeout(timeout);
-				resolve();
-			});
-			this.subSocket.on('error', reject);
-
-			// We switched the path here to establish communication
-			// The socket on which server is publishing clients will observer
+			// Connect to pub socket of the server to receive subscribed events
 			this.subSocket.connect(this._eventPubSocketPath);
-		}).finally(() => {
-			this.subSocket.removeAllListeners('connect');
-			this.subSocket.removeAllListeners('error');
-		});
-
-		await new Promise<void>((resolve, reject) => {
-			const timeout = setTimeout(() => {
-				reject(
-					new Error('IPC Socket client connection timeout. Please check if IPC server is running.'),
-				);
-			}, CONNECTION_TIME_OUT);
-			this.rpcClient.sock.on('connect', () => {
-				clearTimeout(timeout);
-				resolve();
-			});
-			this.rpcClient.sock.on('error', reject);
-
-			this.rpcClient.sock.connect(this._actionRPCConnectingServerSocketPath);
-		}).finally(() => {
-			this.rpcClient.sock.removeAllListeners('connect');
-			this.rpcClient.sock.removeAllListeners('error');
-		});
-	}
-
-	public stop(): void {
-		super.stop();
-		this.rpcClient.sock.close();
+			/* Wait briefly before publishing to avoid slow joiner syndrome,
+			   where the subscriber loses messages as it connects to the server’s socket */
+			await new Promise(resolve => setTimeout(resolve, 25));
+		} catch (error) {
+			this.pubSocket.close();
+			this.subSocket.close();
+			throw error;
+		}
 	}
 }
