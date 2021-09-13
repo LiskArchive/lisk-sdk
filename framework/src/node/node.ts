36b2ebf3322dfc8e6768d9900b6a3b60d3f0988c
shuse2 <shus.toda@gmail.com>
2021-09-13 18:23:11 +0200

    :recycle: Rename liskbft to bft module

--- framework/src/node/node.ts
+++ framework/src/node/node.ts
@@ -29,7 +29,7 @@ import { Endpoint } from './endpoint';
 import { getRegisteredModules, getSchema } from './utils/modules';
 import { getEndpointHandlers, mergeEndpointHandlers } from '../endpoint';
 import { ValidatorsAPI, ValidatorsModule } from '../modules/validators';
-import { LiskBFTAPI, LiskBFTModule } from '../modules/liskbft';
+import { LiskBFTAPI, LiskBFTModule } from '../modules/bft';
 import {
 	APP_EVENT_BLOCK_DELETE,
 	APP_EVENT_BLOCK_NEW,
