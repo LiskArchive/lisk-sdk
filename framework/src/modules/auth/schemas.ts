024f9e802ccfce6a35dda7e00e551712eac4dde9
Mehmet Egemen Albayrak <mehmetegemenalbayrak@gmail.com>
2021-09-14 15:32:17 +0300

    Add numerical constraints to regMultisig schema

--- framework/src/modules/auth/schemas.ts
+++ framework/src/modules/auth/schemas.ts
@@ -48,20 +48,30 @@ export const registerMultisignatureParamsSchema = {
 		numberOfSignatures: {
 			dataType: 'uint32',
 			fieldNumber: 1,
+			minimum: 1,
+			maximum: 64,
 		},
 		mandatoryKeys: {
 			type: 'array',
 			items: {
 				dataType: 'bytes',
+				minLength: 32,
+				maxLength: 32,
 			},
 			fieldNumber: 2,
+			minItems: 0,
+			maxItems: 64,
 		},
 		optionalKeys: {
 			type: 'array',
 			items: {
 				dataType: 'bytes',
+				minLength: 32,
+				maxLength: 32,
 			},
 			fieldNumber: 3,
+			minItems: 0,
+			maxItems: 64,
 		},
 	},
 	required: ['numberOfSignatures', 'mandatoryKeys', 'optionalKeys'],
