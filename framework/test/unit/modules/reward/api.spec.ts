7719f071b3bcb3ad9d78cc69685ec535bd9e23d3
Mehmet Egemen Albayrak <mehmetegemenalbayrak@gmail.com>
2021-09-17 07:07:50 +0300

    Add licence header to api.spec

--- framework/test/unit/modules/reward/api.spec.ts
+++ framework/test/unit/modules/reward/api.spec.ts
@@ -1,3 +1,17 @@
+/*
+ * Copyright © 2021 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
 import { getRandomBytes, hash } from '@liskhq/lisk-cryptography';
 import { RewardModule } from '../../../../src/modules/reward';
 import { EventQueue } from '../../../../src/node/state_machine';
