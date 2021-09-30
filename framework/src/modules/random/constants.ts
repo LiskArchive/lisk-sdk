da8d835316b9be2124ef1971db674012e8923d1b
Ishan <ishantiw.quasar@gmail.com>
2021-09-30 16:13:58 +0200

    🌱 Bootstrap random module - Add class files for module, api and endpoint - Add schema, types and constants files - Assign maxLengthRevealsMainchain constant from config in init()

---
+++ framework/src/modules/random/constants.ts
@@ -0,0 +1,16 @@
+/*
+ * Copyright © 2021 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+export const MODULE_ID_RANDOM = 14;
+export const MAX_LENGTH_REVEALS_MAINCHAIN = 206;
+export const STORE_PREFIX_RANDOM = 0x0000;
