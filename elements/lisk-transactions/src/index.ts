eaa4bc67689e85293a7da154fdd641994146f785
shuse2 <shus.toda@gmail.com>
2021-10-07 12:10:47 +0200

    :seedling: Expose new private key signing functions

--- elements/lisk-transactions/src/index.ts
+++ elements/lisk-transactions/src/index.ts
@@ -15,5 +15,12 @@
 
 export { computeMinFee } from './fee';
 export { convertBeddowsToLSK, convertLSKToBeddows } from './format';
-export { getBytes, getSigningBytes, signTransaction, signMultiSignatureTransaction } from './sign';
+export {
+	getBytes,
+	getSigningBytes,
+	signTransaction,
+	signMultiSignatureTransaction,
+	signMultiSignatureTransactionWithPrivateKey,
+	signTransactionWithPrivateKey,
+} from './sign';
 export { validateTransaction } from './validate';
