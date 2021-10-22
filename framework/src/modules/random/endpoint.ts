522d8bd36b08397b51ed094f8a6bb40a20725e11
Ishan <ishantiw.quasar@gmail.com>
2021-10-22 12:19:59 +0200

    🌱 Add Random module endpoint - add endpoint isSeedRevealValid

--- framework/src/modules/random/endpoint.ts
+++ framework/src/modules/random/endpoint.ts
@@ -12,6 +12,25 @@
  * Removal or modification of this copyright notice is prohibited.
  */
 
+import { ModuleEndpointContext } from '../..';
 import { BaseEndpoint } from '../base_endpoint';
+import { STORE_PREFIX_RANDOM, EMPTY_KEY } from './constants';
+import { seedRevealSchema } from './schemas';
+import { ValidatorReveals } from './types';
+import { isSeedRevealValidUtil } from './utils';
 
-export class RandomEndpoint extends BaseEndpoint {}
+export class RandomEndpoint extends BaseEndpoint {
+	public async isSeedRevealValid(
+		context: ModuleEndpointContext,
+		generatorAddress: Buffer,
+		seedReveal: Buffer,
+	): Promise<boolean> {
+		const randomDataStore = context.getStore(this.moduleID, STORE_PREFIX_RANDOM);
+		const { validatorReveals } = await randomDataStore.getWithSchema<ValidatorReveals>(
+			EMPTY_KEY,
+			seedRevealSchema,
+		);
+
+		return isSeedRevealValidUtil(generatorAddress, seedReveal, validatorReveals);
+	}
+}
