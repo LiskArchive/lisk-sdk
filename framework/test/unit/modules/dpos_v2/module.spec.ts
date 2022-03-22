81e58180ff63aebc133a3041fe315a34e8683420
Mehmet Egemen Albayrak <mehmetegemenalbayrak@gmail.com>
2022-03-22 17:51:40 +0300

    Implement Validators and DPoS updates

--- framework/test/unit/modules/dpos_v2/module.spec.ts
+++ framework/test/unit/modules/dpos_v2/module.spec.ts
@@ -1019,13 +1019,9 @@ describe('DPoS module', () => {
 				);
 
 				const missedBlocks: Record<string, number> = {};
-				for (let i = 0; i < 103; i += 1) {
-					// Generator address forged last block of previous round where
-					// previousTimestamp points out and also forging current end
-					// of round block. Because of that updateDelegateProductivity
-					// function will substract 2 from its missed blocks and hence
-					// we assign it 2, results in 0 meaning block is not forged yet.
-					missedBlocks[delegateAddresses[i].toString('binary')] = i === 102 ? 2 : 1;
+				// Make every delegate miss its block-slot except start and end slots
+				for (let i = 1; i < 102; i += 1) {
+					missedBlocks[delegateAddresses[i].toString('binary')] = 1;
 				}
 
 				when(validatorsAPI.getGeneratorsBetweenTimestamps)
@@ -1051,6 +1047,8 @@ describe('DPoS module', () => {
 					if (delegateAddress.equals(generatorAddress)) {
 						expect(currentDelegate.consecutiveMissedBlocks).toBe(0);
 						expect(currentDelegate.lastGeneratedHeight).toBe(nextForgedHeight);
+					} else if (delegateAddress.equals(delegateAddresses[0])) {
+						expect(currentDelegate.consecutiveMissedBlocks).toBe(0);
 					} else {
 						expect(currentDelegate.consecutiveMissedBlocks).toBe(1);
 					}
@@ -1087,18 +1085,8 @@ describe('DPoS module', () => {
 				);
 
 				const missedBlocks: Record<string, number> = {};
-				for (let i = 0; i < missedForgers.length + 1 + 1; i += 1) {
-					if (i === 0) {
-						// 1 will be subtracted from this in updateDelegateProductivity
-						missedBlocks[lastForgerAddress.toString('binary')] = 1;
-					} else if (i > 0 && i < missedForgers.length + 1) {
-						// We missed blocks, nothing will be subtracted from this
-						missedBlocks[missedForgers[2 - i].toString('binary')] = 1;
-					} else {
-						// Since end of range, 1 will be subtracted from this
-						// meaning block is not forged yet
-						missedBlocks[generatorAddress.toString('binary')] = 1;
-					}
+				for (let i = 1; i < missedForgers.length + 1; i += 1) {
+					missedBlocks[missedForgers[2 - i].toString('binary')] = 1;
 				}
 
 				when(validatorsAPI.getGeneratorsBetweenTimestamps)
@@ -1158,13 +1146,14 @@ describe('DPoS module', () => {
 
 				const missedBlocks: Record<string, number> = {};
 				for (const delegateAddress of delegateAddresses) {
+					if (delegateAddress.equals(lastForgerAddress)) {
+						continue;
+					}
 					missedBlocks[delegateAddress.toString('binary')] = 1;
 				}
 				for (const delegateAddress of missedMoreThan1Block) {
 					missedBlocks[delegateAddress.toString('binary')] += 1;
 				}
-				missedBlocks[generatorAddress.toString('binary')] = 2;
-				missedBlocks[lastForgerAddress.toString('binary')] = 2;
 
 				when(validatorsAPI.getGeneratorsBetweenTimestamps)
 					.calledWith(context.getAPIContext(), previousTimestamp, currentTimestamp)
@@ -1192,6 +1181,8 @@ describe('DPoS module', () => {
 						missedMoreThan1Block.some(missedForger => missedForger.equals(delegateAddress))
 					) {
 						expect(currentDelegate.consecutiveMissedBlocks).toBe(2);
+					} else if (delegateAddress.equals(lastForgerAddress)) {
+						expect(currentDelegate.consecutiveMissedBlocks).toBe(0);
 					} else {
 						expect(currentDelegate.consecutiveMissedBlocks).toBe(1);
 					}
@@ -1225,8 +1216,6 @@ describe('DPoS module', () => {
 				);
 
 				const missedBlocks: Record<string, number> = {};
-				missedBlocks[generatorAddress.toString('binary')] = 1;
-				missedBlocks[lastForgerAddress.toString('binary')] = 1;
 
 				when(validatorsAPI.getGeneratorsBetweenTimestamps)
 					.calledWith(context.getAPIContext(), previousTimestamp, currentTimestamp)
@@ -1284,9 +1273,7 @@ describe('DPoS module', () => {
 				);
 
 				const missedBlocks: Record<string, number> = {};
-				missedBlocks[generatorAddress.toString('binary')] = 1;
 				missedBlocks[missedDelegate.toString('binary')] = 1;
-				missedBlocks[lastForgerAddress.toString('binary')] = 1;
 
 				when(validatorsAPI.getGeneratorsBetweenTimestamps)
 					.calledWith(context.getAPIContext(), previousTimestamp, currentTimestamp)
@@ -1348,9 +1335,7 @@ describe('DPoS module', () => {
 				);
 
 				const missedBlocks: Record<string, number> = {};
-				missedBlocks[generatorAddress.toString('binary')] = 1;
 				missedBlocks[missedDelegate.toString('binary')] = 1;
-				missedBlocks[lastForgerAddress.toString('binary')] = 1;
 
 				when(validatorsAPI.getGeneratorsBetweenTimestamps)
 					.calledWith(context.getAPIContext(), previousTimestamp, currentTimestamp)
@@ -1412,9 +1397,7 @@ describe('DPoS module', () => {
 				);
 
 				const missedBlocks: Record<string, number> = {};
-				missedBlocks[generatorAddress.toString('binary')] = 1;
 				missedBlocks[missedDelegate.toString('binary')] = 1;
-				missedBlocks[lastForgerAddress.toString('binary')] = 1;
 
 				when(validatorsAPI.getGeneratorsBetweenTimestamps)
 					.calledWith(context.getAPIContext(), previousTimestamp, currentTimestamp)
