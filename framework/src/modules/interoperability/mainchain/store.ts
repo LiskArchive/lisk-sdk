92ed0747c3ef4e9d13c3faa64769e0c41af7a026
Mitsuaki Uchimoto <mitsuaki-u@users.noreply.github.com>
2022-03-23 11:29:53 +0100

    Update store.ts

--- framework/src/modules/interoperability/mainchain/store.ts
+++ framework/src/modules/interoperability/mainchain/store.ts
@@ -12,49 +12,10 @@
  * Removal or modification of this copyright notice is prohibited.
  */
 
-import { codec } from '@liskhq/lisk-codec';
-import { hash } from '@liskhq/lisk-cryptography';
-import { regularMerkleTree } from '@liskhq/lisk-tree';
 import { BaseInteroperabilityStore } from '../base_interoperability_store';
-import {
-	MODULE_ID_INTEROPERABILITY,
-	STORE_PREFIX_CHANNEL_DATA,
-	STORE_PREFIX_OUTBOX_ROOT,
-} from '../constants';
-import { ccmSchema, channelSchema, outboxRootSchema } from '../schema';
-import { CCMsg, CCUpdateParams, ChannelData, SendInternalContext } from '../types';
+import { CCMsg, CCUpdateParams, SendInternalContext } from '../types';
 
 export class MainchainInteroperabilityStore extends BaseInteroperabilityStore {
-	public async appendToOutboxTree(chainID: Buffer, appendData: Buffer) {
-		const channelSubstore = this.getStore(MODULE_ID_INTEROPERABILITY, STORE_PREFIX_CHANNEL_DATA);
-		const channel = await channelSubstore.getWithSchema<ChannelData>(chainID, channelSchema);
-		const updatedOutbox = regularMerkleTree.calculateMerkleRoot({
-			value: hash(appendData),
-			appendPath: channel.outbox.appendPath,
-			size: channel.outbox.size,
-		});
-		await channelSubstore.setWithSchema(
-			chainID,
-			{ ...channel, outbox: updatedOutbox },
-			channelSchema,
-		);
-
-		return true;
-	}
-
-	public async addToOutbox(chainID: Buffer, ccm: CCMsg) {
-		const serializedMessage = codec.encode(ccmSchema, ccm);
-		await this.appendToOutboxTree(chainID, serializedMessage);
-
-		const channelSubstore = this.getStore(MODULE_ID_INTEROPERABILITY, STORE_PREFIX_CHANNEL_DATA);
-		const channel = await channelSubstore.getWithSchema<ChannelData>(chainID, channelSchema);
-
-		const outboxRootSubstore = this.getStore(MODULE_ID_INTEROPERABILITY, STORE_PREFIX_OUTBOX_ROOT);
-		await outboxRootSubstore.setWithSchema(chainID, channel.outbox.root, outboxRootSchema);
-
-		return true;
-	}
-
 	// eslint-disable-next-line @typescript-eslint/require-await
 	public async appendToInboxTree(chainID: number, appendData: Buffer): Promise<void> {
 		// eslint-disable-next-line no-console
