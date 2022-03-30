960102fbcf3ffbadcc295be70d5fc2cada629f38
Ishan Tiwari <ishantiw.quasar@gmail.com>
2022-03-30 15:10:50 +0200

    🌱 Bootstrap cross chain commands - boostrap CCCommands classes for mainchain and sidechains - Add related schemas - Add related constants

---
+++ framework/src/modules/interoperability/cc_commands/channel_terminated.ts
@@ -0,0 +1,28 @@
+/*
+ * Copyright © 2022 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+import { BaseCrossChainCommand } from '../base_cross_chain_command';
+import { CROSS_CHAIN_COMMAND_ID_CHANNEL_TERMINATED } from '../constants';
+import { channelTerminatedCCMParamsSchema } from '../schema';
+
+export class CrossChainChannelTerminatedCommand extends BaseCrossChainCommand {
+	public ID = CROSS_CHAIN_COMMAND_ID_CHANNEL_TERMINATED;
+	public name = 'channelTerminated';
+	public schema = channelTerminatedCCMParamsSchema;
+	// TODO
+	// eslint-disable-next-line @typescript-eslint/require-await
+	public async execute(): Promise<void> {
+		throw new Error('Method not implemented.');
+	}
+}
