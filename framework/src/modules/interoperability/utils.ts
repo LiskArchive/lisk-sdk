d77acf612068e48dbbae69e3ab95e9c1d4f134fb
Mitsuaki Uchimoto <mitsuaki-u@users.noreply.github.com>
2022-04-28 17:30:37 +0200

    Implement computeValidatorsHash util

--- framework/src/modules/interoperability/utils.ts
+++ framework/src/modules/interoperability/utils.ts
@@ -13,11 +13,11 @@
  */
 
 import { codec } from '@liskhq/lisk-codec';
-import { intToBuffer } from '@liskhq/lisk-cryptography';
+import { hash, intToBuffer } from '@liskhq/lisk-cryptography';
 import { LiskValidationError, validator } from '@liskhq/lisk-validator';
 import { MAX_CCM_SIZE } from './constants';
 import { ActiveValidators, CCMsg, ChainAccount } from './types';
-import { ccmSchema, sidechainTerminatedCCMParamsSchema } from './schema';
+import { ccmSchema, sidechainTerminatedCCMParamsSchema, validatorsHashInputSchema } from './schema';
 
 // Returns the big endian uint32 serialization of an integer x, with 0 <= x < 2^32 which is 4 bytes long.
 export const getIDAsKeyForStore = (id: number) => intToBuffer(id, 4);
@@ -102,4 +102,17 @@ export const isValidName = (username: string): boolean => {
 	}
 
 	return /^[a-z0-9!@$&_.]+$/g.test(username);
+};
+
+export const computeValidatorsHash = (
+	initValidators: RegistrationParametersValidator[],
+	certificateThreshold: bigint,
+) => {
+	const input = {
+		activeValidators: initValidators,
+		certificateThreshold,
+	};
+
+	const encodedValidatorsHashInput = codec.encode(validatorsHashInputSchema, input);
+	return hash(encodedValidatorsHashInput);
 };
