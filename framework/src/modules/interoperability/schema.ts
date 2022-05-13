59aebf044936b8ee54aab8f37c890b59a51ea73e
Mitsuaki Uchimoto <mitsuaki-u@users.noreply.github.com>
2022-05-13 09:35:43 +0200

    Update schema.ts

--- framework/src/modules/interoperability/schema.ts
+++ framework/src/modules/interoperability/schema.ts
@@ -636,3 +636,52 @@ export const registrationSignatureMessageSchema = {
 		},
 	},
 };
+
+export const stateRecoveryParams = {
+	$id: '/modules/interoperability/mainchain/commands/state_recovery',
+	type: 'object',
+	required: ['chainID', 'moduleID', 'storeEntries', 'siblingHashes'],
+	properties: {
+		chainID: {
+			dataType: 'uint32',
+			fieldNumber: 1,
+		},
+		moduleID: {
+			dataType: 'uint32',
+			fieldNumber: 2,
+		},
+		storeEntries: {
+			type: 'array',
+			fieldNumber: 3,
+			items: {
+				type: 'object',
+				properties: {
+					storePrefix: {
+						dataType: 'uint32',
+						fieldNumber: 1,
+					},
+					storeKey: {
+						dataType: 'bytes',
+						fieldNumber: 2,
+					},
+					storeValue: {
+						dataType: 'bytes',
+						fieldNumber: 3,
+					},
+					bitmap: {
+						dataType: 'bytes',
+						fieldNumber: 4,
+					},
+				},
+				required: ['storePrefix', 'storeKey', 'storeValue', 'bitmap'],
+			},
+		},
+		siblingHashes: {
+			type: 'array',
+			items: {
+				dataType: 'bytes',
+			},
+			fieldNumber: 4,
+		},
+	},
+};
