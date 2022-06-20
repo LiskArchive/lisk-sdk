e5b89f6d334d272ec36b09aff178d9bfbfa1ed5e
Mitsuaki Uchimoto <mitsuaki-u@users.noreply.github.com>
2022-06-20 16:36:56 +0200

    Update endpoint.ts

--- framework/src/engine/generator/endpoint.ts
+++ framework/src/engine/generator/endpoint.ts
@@ -14,6 +14,7 @@
 
 import {
 	decryptPassphraseWithPassword,
+	EncryptedPassphraseObject,
 	generatePrivateKey,
 	getAddressFromPublicKey,
 	getPrivateAndPublicKeyFromPassphrase,
@@ -99,8 +100,10 @@ export class Endpoint {
 		}
 
 		try {
-			passphrase = decryptPassphraseWithPassword(
-				parseEncryptedPassphrase(encryptedGenerator.encryptedPassphrase),
+			passphrase = await decryptPassphraseWithPassword(
+				parseEncryptedPassphrase(
+					encryptedGenerator.encryptedPassphrase,
+				) as EncryptedPassphraseObject,
 				req.password,
 			);
 		} catch (e) {
