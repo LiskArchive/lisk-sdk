93bf2d528f888530ab94023a8157b4764d39b1a7
Mitsuaki Uchimoto <mitsuaki-u@users.noreply.github.com>
2022-07-07 16:59:20 +0200

    Update module.ts

--- framework/src/modules/interoperability/mainchain/module.ts
+++ framework/src/modules/interoperability/mainchain/module.ts
@@ -21,7 +21,8 @@ import { MainchainInteroperabilityEndpoint } from './endpoint';
 
 export class MainchainInteroperabilityModule extends BaseInteroperabilityModule {
 	public crossChainAPI = new MainchainCCAPI(this.id);
-	public api = new MainchainInteroperabilityAPI(this.id);
+	// TODO: inject interoperable cc apis
+	public api = new MainchainInteroperabilityAPI(this.id, new Map());
 	public endpoint = new MainchainInteroperabilityEndpoint(this.id);
 	// eslint-disable-next-line @typescript-eslint/no-empty-function
 	public registerInteroperableModule(): void {
