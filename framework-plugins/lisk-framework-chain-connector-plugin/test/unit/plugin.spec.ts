278f05f69742795f27c09206fc23502283ce6f5a
Incede <33103370+Incede@users.noreply.github.com>
2022-07-20 12:38:29 +0200

    Update config

--- framework-plugins/lisk-framework-chain-connector-plugin/test/unit/plugin.spec.ts
+++ framework-plugins/lisk-framework-chain-connector-plugin/test/unit/plugin.spec.ts
@@ -4,17 +4,6 @@ describe('ChainConnectorPlugin', () => {
 	describe('alias', () => {
 		it.todo('should have valid alias');
 	});
-	describe('info', () => {
-		it.todo('should have author');
-		it.todo('should have name');
-		it.todo('should have version');
-	});
-	describe('events', () => {
-		it.todo('should fire an event');
-	});
-	describe('actions', () => {
-		it.todo('should have an action');
-	});
 	describe('load', () => {
 		it.todo('should load plugin');
 	});
