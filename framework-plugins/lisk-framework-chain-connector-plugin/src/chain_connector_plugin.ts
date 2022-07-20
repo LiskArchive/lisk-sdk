1afb12e9fea4dc79f3fe7bec80e26b2f6c20e8b5
Ishan Tiwari <ishantiw.quasar@gmail.com>
2022-07-20 15:13:04 +0200

    🌱Setup configuration for chain connector plugin

--- framework-plugins/lisk-framework-chain-connector-plugin/src/chain_connector_plugin.ts
+++ framework-plugins/lisk-framework-chain-connector-plugin/src/chain_connector_plugin.ts
@@ -12,10 +12,29 @@
  * Removal or modification of this copyright notice is prohibited.
  */
 
-import { BasePlugin, PluginInitContext } from 'lisk-sdk';
+import { BasePlugin, PluginInitContext, Transaction, apiClient } from 'lisk-sdk';
+import { CCM_BASED_CCU_FREQUENCY, LIVENESS_BASED_CCU_FREQUENCY } from './constants';
+import { configSchema } from './schemas';
+import { ChainConnectorPluginConfig } from './types';
 
-export class ChainConnectorPlugin extends BasePlugin {
+interface CCUFrequencyConfig {
+	ccm: number;
+	liveness: number;
+}
+
+type SentCCUs = Transaction[];
+
+export class ChainConnectorPlugin extends BasePlugin<ChainConnectorPluginConfig> {
 	public name = 'chainConnector';
+	public configSchema = configSchema;
+	private _lastCertifiedHeight!: number;
+	private _ccuFrequency: CCUFrequencyConfig = {
+		ccm: CCM_BASED_CCU_FREQUENCY,
+		liveness: LIVENESS_BASED_CCU_FREQUENCY,
+	};
+	private _mainchainAPIClient!: apiClient.APIClient;
+	private _sidechainAPIClient?: apiClient.APIClient;
+	private readonly _sentCCUs: SentCCUs = [];
 
 	public get nodeModulePath(): string {
 		return __filename;
@@ -23,13 +42,29 @@ export class ChainConnectorPlugin extends BasePlugin {
 
 	// eslint-disable-next-line @typescript-eslint/require-await
 	public async init(context: PluginInitContext): Promise<void> {
+		await super.init(context);
+		this._ccuFrequency.ccm = this.config.ccmBasedFrequency || CCM_BASED_CCU_FREQUENCY;
+		this._ccuFrequency.liveness =
+			this.config.livenessBasedFrequency || LIVENESS_BASED_CCU_FREQUENCY;
+	}
+
+	public async load(): Promise<void> {
+		this._mainchainAPIClient = await apiClient.createIPCClient(this.config.mainchainIPCPath);
+		if (this.config.sidechainIPCPath) {
+			this._sidechainAPIClient = await apiClient.createIPCClient(this.config.sidechainIPCPath);
+		}
+		// TODO: After DB issue we need to fetch the last sent CCUs and assign it to _sentCCUs
 		// eslint-disable-next-line no-console
-		console.log(context);
+		console.log(this._lastCertifiedHeight, this._sentCCUs);
+		// TODO: Fetch the certificate height from last sent CCU and update the height
+		this._lastCertifiedHeight = 0;
 	}
 
 	// eslint-disable-next-line @typescript-eslint/no-empty-function
-	public async load(): Promise<void> {}
-
-	// eslint-disable-next-line @typescript-eslint/no-empty-function
-	public async unload(): Promise<void> {}
+	public async unload(): Promise<void> {
+		await this._mainchainAPIClient.disconnect();
+		if (this._sidechainAPIClient) {
+			await this._sidechainAPIClient.disconnect();
+		}
+	}
 }
