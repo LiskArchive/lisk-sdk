420834f05fc5d2f15c2247ddf4191cc36a2ee21b
Incede <33103370+Incede@users.noreply.github.com>
2022-07-20 10:22:26 +0200

    Create package

---
+++ framework-plugins/lisk-framework-chain-connector-plugin/test/unit/plugin.spec.ts
@@ -0,0 +1,24 @@
+// import * as plugins from '../../src/plugin';
+
+describe('ChainConnectorPlugin', () => {
+	describe('alias', () => {
+		it.todo('should have valid alias');
+	});
+	describe('info', () => {
+		it.todo('should have author');
+		it.todo('should have name');
+		it.todo('should have version');
+	});
+	describe('events', () => {
+		it.todo('should fire an event');
+	});
+	describe('actions', () => {
+		it.todo('should have an action');
+	});
+	describe('load', () => {
+		it.todo('should load plugin');
+	});
+	describe('unload', () => {
+		it.todo('should unload plugin');
+	});
+});
