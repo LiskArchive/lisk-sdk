e302f09f7ad8f91373253de961719320ab41f1cb
Ishan Tiwari <ishantiw.quasar@gmail.com>
2022-07-20 16:30:32 +0200

    🌱 Add license

--- framework-plugins/lisk-framework-chain-connector-plugin/test/unit/plugin.spec.ts
+++ framework-plugins/lisk-framework-chain-connector-plugin/test/unit/plugin.spec.ts
@@ -1,3 +1,17 @@
+/*
+ * Copyright © 2022 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
 import { testing, apiClient } from 'lisk-sdk';
 import * as plugins from '../../src/chain_connector_plugin';
 import { CCM_BASED_CCU_FREQUENCY, LIVENESS_BASED_CCU_FREQUENCY } from '../../src/constants';
