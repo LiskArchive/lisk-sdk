fc8046bdc48b2a958d8c6fb2e443b664d02b4f0d
Ishan Tiwari <ishantiw.quasar@gmail.com>
2022-08-04 18:28:50 +0200

    🌱 Add newBlockHandler for chain connector

--- framework-plugins/lisk-framework-chain-connector-plugin/src/types.ts
+++ framework-plugins/lisk-framework-chain-connector-plugin/src/types.ts
@@ -34,6 +34,75 @@ export interface ChainConnectorInfo {
 	blockHeaders: chain.BlockHeader[];
 	aggregateCommits: AggregateCommit[];
 	validatorsHashPreimage: ValidatorsData[];
+	crossChainMessages: CCMsg[];
+}
+
+export interface BFTParameters {
+	prevoteThreshold: bigint;
+	precommitThreshold: bigint;
+	certificateThreshold: bigint;
+	validators: BFTValidator[];
+	validatorsHash: Buffer;
+}
+
+export interface Inbox {
+	appendPath: Buffer[];
+	size: number;
+	root: Buffer;
+}
+
+export interface InboxJSON {
+	appendPath: string[];
+	size: number;
+	root: string;
+}
+
+export interface Outbox {
+	appendPath: Buffer[];
+	size: number;
+	root: Buffer;
+}
+
+export interface OutboxJSON {
+	appendPath: string[];
+	size: number;
+	root: string;
+}
+
+export interface MessageFeeTokenID {
+	chainID: Buffer;
+	localID: Buffer;
+}
+
+export interface MessageFeeTokenIDJSON {
+	chainID: string;
+	localID: string;
+}
+
+export interface ChannelData {
+	inbox: Inbox;
+	outbox: Outbox;
+	partnerChainOutboxRoot: Buffer;
+	messageFeeTokenID: MessageFeeTokenID;
+}
+
+export interface ChannelDataJSON {
+	inbox: InboxJSON;
+	outbox: OutboxJSON;
+	partnerChainOutboxRoot: string;
+	messageFeeTokenID: MessageFeeTokenIDJSON;
+}
+
+export interface CCMsg {
+	readonly nonce: bigint;
+	readonly moduleID: Buffer;
+	readonly crossChainCommandID: Buffer;
+	readonly sendingChainID: Buffer;
+	readonly receivingChainID: Buffer;
+	readonly fee: bigint;
+	readonly status: number;
+	readonly params: Buffer;
+	readonly id: Buffer;
 }
 
 export interface AggregateCommitJSON {
