e133f913bf318aad0d84d1e431465764f7e1477f
shuse2 <shus.toda@gmail.com>
2022-08-10 17:51:43 +0200

    :recycle: Update to use parcel so that browser field is used

--- framework-plugins/lisk-framework-faucet-plugin/src/ui/index.tsx
+++ framework-plugins/lisk-framework-faucet-plugin/src/ui/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable import/extensions */
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import '@csstools/normalize.css';
 import './index.scss';
 import { App } from './app';
 
