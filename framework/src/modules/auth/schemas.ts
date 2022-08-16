88269c7de471d0623fdf96eb6324bfc1f53a6a03
Ishan Tiwari <ishantiw.quasar@gmail.com>
2022-08-16 17:53:48 +0200

    ♻️  Add schema, constants and types for new MS registration transaction

--- framework/src/modules/auth/schemas.ts
+++ framework/src/modules/auth/schemas.ts
@@ -12,7 +12,52 @@
  * Removal or modification of this copyright notice is prohibited.
  */
 
-import { ED25519_PUBLIC_KEY_LENGTH, ED25519_SIGNATURE_LENGTH } from './constants';
+import {
+	ADDRESS_LENGTH,
+	ED25519_PUBLIC_KEY_LENGTH,
+	ED25519_SIGNATURE_LENGTH,
+	MAX_NUMBER_OF_SIGNATURES,
+} from './constants';
+
+export const authAccountSchema = {
+	$id: '/auth/account',
+	type: 'object',
+	properties: {
+		nonce: {
+			dataType: 'uint64',
+			fieldNumber: 1,
+		},
+		numberOfSignatures: {
+			dataType: 'uint32',
+			fieldNumber: 2,
+			minimum: 0,
+			maximum: MAX_NUMBER_OF_SIGNATURES,
+		},
+		mandatoryKeys: {
+			type: 'array',
+			items: {
+				dataType: 'bytes',
+				minLength: ED25519_PUBLIC_KEY_LENGTH,
+				maxLength: ED25519_PUBLIC_KEY_LENGTH,
+			},
+			minItems: 0,
+			maxItems: MAX_NUMBER_OF_SIGNATURES,
+			fieldNumber: 3,
+		},
+		optionalKeys: {
+			type: 'array',
+			items: {
+				dataType: 'bytes',
+				minLength: ED25519_PUBLIC_KEY_LENGTH,
+				maxLength: ED25519_PUBLIC_KEY_LENGTH,
+			},
+			minItems: 0,
+			maxItems: MAX_NUMBER_OF_SIGNATURES,
+			fieldNumber: 4,
+		},
+	},
+	required: ['nonce', 'numberOfSignatures', 'mandatoryKeys', 'optionalKeys'],
+};
 
 export const registerMultisignatureParamsSchema = {
 	$id: '/auth/command/regMultisig',
@@ -28,8 +73,8 @@ export const registerMultisignatureParamsSchema = {
 			type: 'array',
 			items: {
 				dataType: 'bytes',
-				minLength: 32,
-				maxLength: 32,
+				minLength: ED25519_PUBLIC_KEY_LENGTH,
+				maxLength: ED25519_PUBLIC_KEY_LENGTH,
 			},
 			fieldNumber: 2,
 			minItems: 0,
@@ -39,15 +84,64 @@ export const registerMultisignatureParamsSchema = {
 			type: 'array',
 			items: {
 				dataType: 'bytes',
-				minLength: 32,
-				maxLength: 32,
+				minLength: ED25519_PUBLIC_KEY_LENGTH,
+				maxLength: ED25519_PUBLIC_KEY_LENGTH,
 			},
 			fieldNumber: 3,
 			minItems: 0,
 			maxItems: 64,
 		},
+		signatures: {
+			type: 'array',
+			items: {
+				dataType: 'bytes',
+				minLength: ED25519_SIGNATURE_LENGTH,
+				maxLength: ED25519_SIGNATURE_LENGTH,
+			},
+			fieldNumber: 4,
+		},
 	},
-	required: ['numberOfSignatures', 'mandatoryKeys', 'optionalKeys'],
+	required: ['numberOfSignatures', 'mandatoryKeys', 'optionalKeys', 'signatures'],
+};
+
+export const multisigRegMsgSchema = {
+	$id: '/auth/command/regMultisigMsg',
+	type: 'object',
+	required: ['address', 'nonce', 'numberOfSignatures', 'mandatoryKeys', 'optionalKeys'],
+	properties: {
+		address: {
+			dataType: 'bytes',
+			fieldNumber: 1,
+			minLength: ADDRESS_LENGTH,
+			maxLength: ADDRESS_LENGTH,
+		},
+		nonce: {
+			dataType: 'uint64',
+			fieldNumber: 2,
+		},
+		numberOfSignatures: {
+			dataType: 'uint32',
+			fieldNumber: 3,
+		},
+		mandatoryKeys: {
+			type: 'array',
+			items: {
+				dataType: 'bytes',
+				minLength: ED25519_PUBLIC_KEY_LENGTH,
+				maxLength: ED25519_PUBLIC_KEY_LENGTH,
+			},
+			fieldNumber: 4,
+		},
+		optionalKeys: {
+			type: 'array',
+			items: {
+				dataType: 'bytes',
+				minLength: ED25519_PUBLIC_KEY_LENGTH,
+				maxLength: ED25519_PUBLIC_KEY_LENGTH,
+			},
+			fieldNumber: 5,
+		},
+	},
 };
 
 export const configSchema = {
