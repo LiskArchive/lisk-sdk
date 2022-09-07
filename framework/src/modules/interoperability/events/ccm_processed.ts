9408bc51eaeb9edca05292b98a444ad7ddeacbf9
Mitsuaki Uchimoto <mitsuaki-u@users.noreply.github.com>
2022-09-07 16:05:32 +0200

    Add interoperability events

---
+++ framework/src/modules/interoperability/events/ccm_processed.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright © 2022 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+import { BaseEvent, EventQueuer } from '../../base_event';
+
+export interface CcmProcessedEventData {
+	ccmID: Buffer;
+	status: number;
+}
+
+export const ccmProcessedEventSchema = {
+	$id: '/interoperability/events/ccmProcessed',
+	type: 'object',
+	required: ['ccmID', 'status'],
+	properties: {
+		ccmID: {
+			dataType: 'bytes',
+			fieldNumber: 1,
+		},
+		status: {
+			dataType: 'uint32',
+			fieldNumber: 2,
+		},
+	},
+};
+
+export class CcmProcessedEvent extends BaseEvent<CcmProcessedEventData> {
+	public schema = ccmProcessedEventSchema;
+
+	public log(
+		ctx: EventQueuer,
+		sendingChainID: Buffer,
+		receivingChainID: Buffer,
+		ccmID: Buffer,
+		data: CcmProcessedEventData,
+	): void {
+		this.add(ctx, data, [sendingChainID, receivingChainID, ccmID]);
+	}
+}
