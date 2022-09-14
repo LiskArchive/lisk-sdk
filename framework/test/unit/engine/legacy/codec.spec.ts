05fdf4df4549bbe93434faa4f8561446d0d88e23
shuse2 <shus.toda@gmail.com>
2022-09-14 19:25:19 +0200

    :seedling: Add legacy block validation and codec

--- framework/test/unit/engine/legacy/codec.spec.ts
+++ framework/test/unit/engine/legacy/codec.spec.ts
@@ -1,4 +1,3 @@
-/* eslint-disable max-classes-per-file */
 /*
  * Copyright © 2022 Lisk Foundation
  *
@@ -13,16 +12,52 @@
  * Removal or modification of this copyright notice is prohibited.
  */
 
+import { codec } from '@liskhq/lisk-codec';
+import { blockHeaderSchemaV2, blockSchemaV2 } from '../../../../src/engine/legacy/schemas';
+import { decodeBlock, decodeBlockJSON, encodeBlock } from '../../../../src/engine/legacy/codec';
+import { blockFixtures } from './fixtures';
+
 describe('Legacy codec', () => {
-	describe('decodeBlock', () => {
-		it.todo('test behaviors');
-	});
+	describe('v2 block', () => {
+		let encodedBlock: Buffer;
 
-	describe('decodeBlockJSON', () => {
-		it.todo('test behaviors');
-	});
+		beforeEach(() => {
+			encodedBlock = codec.encode(blockSchemaV2, {
+				header: codec.encode(blockHeaderSchemaV2, blockFixtures[0].header),
+				transactions: blockFixtures[0].transactions,
+			});
+		});
 
-	describe('encodeBlock', () => {
-		it.todo('test behaviors');
+		describe('decodeBlock', () => {
+			it('should decode valid v2 block', () => {
+				const decodedBlock = decodeBlock(encodedBlock);
+
+				expect(decodedBlock.block).toEqual(blockFixtures[0]);
+			});
+
+			it('should fail to decode invalid block', () => {
+				expect(() => decodeBlock(encodedBlock.slice(2))).toThrow();
+			});
+		});
+
+		describe('decodeBlockJSON', () => {
+			it('should decode the blocks to the JSON format', () => {
+				const decodedBlock = decodeBlockJSON(encodedBlock);
+
+				expect(decodedBlock.block.header.previousBlockID).toEqual(
+					blockFixtures[0].header.previousBlockID.toString('hex'),
+				);
+				expect(decodedBlock.block.header.height).toEqual(blockFixtures[0].header.height);
+				expect(decodedBlock.block.header.transactionRoot).toEqual(
+					blockFixtures[0].header.transactionRoot.toString('hex'),
+				);
+			});
+		});
+
+		describe('encodeBlock', () => {
+			it('should encode block', () => {
+				expect(encodeBlock(blockFixtures[0])).toEqual(encodedBlock);
+			});
+		});
 	});
 });
