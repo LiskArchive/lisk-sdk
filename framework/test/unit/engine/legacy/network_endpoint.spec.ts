985fe906dc7e6b7d7604e37a67abe398932d9bb3
Franco NG <franco.ng@lightcurve.io>
2022-09-22 17:09:42 +0200

    Implement function and test cases

--- framework/test/unit/engine/legacy/network_endpoint.spec.ts
+++ framework/test/unit/engine/legacy/network_endpoint.spec.ts
@@ -12,9 +12,80 @@
  *
  * Removal or modification of this copyright notice is prohibited.
  */
+import { utils } from '@liskhq/lisk-cryptography';
+import { Block, BlockAssets } from '@liskhq/lisk-chain';
+import { Database, InMemoryDatabase } from '@liskhq/lisk-db';
+import { codec } from '@liskhq/lisk-codec';
+import { LegacyNetworkEndpoint } from '../../../../src/engine/legacy/network_endpoint';
+import { loggerMock } from '../../../../src/testing/mocks';
+import { Network } from '../../../../src/engine/network';
+import {
+	getBlocksFromIdRequestSchema,
+	getBlocksFromIdResponseSchema,
+} from '../../../../src/engine/consensus/schema';
 
-describe('Legacy network endpoint', () => {
+import { createFakeBlockHeader } from '../../../fixtures';
+
+describe('Legacy P2P network endpoint', () => {
+	const defaultPeerId = 'peer-id';
+
+	let network: Network;
+	let db: Database;
+	let endpoint: LegacyNetworkEndpoint;
+
+	beforeEach(() => {
+		network = ({
+			applyPenaltyOnPeer: jest.fn(),
+		} as unknown) as Network;
+		db = (new InMemoryDatabase() as unknown) as Database;
+
+		endpoint = new LegacyNetworkEndpoint({
+			logger: loggerMock,
+			network,
+			db,
+		});
+	});
 	describe('handleRPCGetLegacyBlocksFromId', () => {
-		it.todo('test behaviors');
+		it('should apply penalty on the peer when data format is invalid', async () => {
+			const invalidBytes = Buffer.from([244, 21, 21]);
+			await expect(
+				endpoint.handleRPCGetLegacyBlocksFromId(invalidBytes, defaultPeerId),
+			).rejects.toThrow();
+			expect(network.applyPenaltyOnPeer).toHaveBeenCalledTimes(1);
+		});
+		it("should return empty list if id doesn't exist", async () => {
+			const blockId = utils.getRandomBytes(32);
+			const blockIds = codec.encode(getBlocksFromIdRequestSchema, {
+				blockId,
+			});
+			const blocks = await endpoint.handleRPCGetLegacyBlocksFromId(blockIds, defaultPeerId);
+			expect(blocks).toEqual(codec.encode(getBlocksFromIdResponseSchema, { blocks: [] }));
+		});
+		it('should return blocks from Id', async () => {
+			jest.spyOn(endpoint._dataAccess, 'getBlockHeaderByID').mockResolvedValue(
+				createFakeBlockHeader({
+					height: 10,
+				}) as never,
+			);
+			jest.spyOn(endpoint._dataAccess, 'getBlocksByHeightBetween').mockImplementation(
+				async (fromHeight: number, toHeight: number): Promise<Block[]> => {
+					const blocks = [];
+					for (let i = fromHeight; i < toHeight; i += 1) {
+						blocks.push(new Block(createFakeBlockHeader(), [], new BlockAssets()));
+					}
+					return Promise.resolve(blocks);
+				},
+			);
+			const blockId = utils.getRandomBytes(32);
+
+			const blockIds = codec.encode(getBlocksFromIdRequestSchema, {
+				blockId,
+			});
+
+			const blocks = await endpoint.handleRPCGetLegacyBlocksFromId(blockIds, defaultPeerId);
+			expect(
+				codec.decode<{ blocks: Block[] }>(getBlocksFromIdResponseSchema, blocks).blocks,
+			).toHaveLength(100);
+		});
 	});
 });
