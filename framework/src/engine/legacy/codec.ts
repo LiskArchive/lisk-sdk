c7313cdb2c645c36078a872ddc8c9b7f7c67350a
Ishan Tiwari <ishantiw.quasar@gmail.com>
2022-09-27 15:24:10 +0200

    ♻️  Add handler for getLegacyBlocksFromId RPC - Define interface for block with ID - Add unit tests for handler

--- framework/src/engine/legacy/codec.ts
+++ framework/src/engine/legacy/codec.ts
@@ -15,7 +15,7 @@
 import { codec, Schema } from '@liskhq/lisk-codec';
 import { utils } from '@liskhq/lisk-cryptography';
 import { blockHeaderSchemaV2, blockSchemaV2, legacyChainBracketInfoSchema } from './schemas';
-import { LegacyBlock, LegacyBlockJSON, LegacyChainBracketInfo, RawLegacyBlock } from './types';
+import { LegacyBlock, LegacyBlockJSON, LegacyChainBracketInfo, RawLegacyBlock, LegacyBlockWithID, LegacyBlockHeaderWithID} from './types';
 
 interface LegacyBlockSchema {
 	header: Schema;
@@ -32,7 +32,9 @@ export const blockSchemaMap: Record<number, LegacyBlockSchema> = {
 // Implement read version logic when adding more versions
 const readVersion = (): number => 2;
 
-export const decodeBlock = (data: Buffer): { block: LegacyBlock; schema: LegacyBlockSchema } => {
+export const decodeBlock = (
+	data: Buffer,
+): { block: LegacyBlockWithID; schema: LegacyBlockSchema } => {
 	const version = readVersion();
 	const blockSchema = blockSchemaMap[version];
 	if (!blockSchema) {
@@ -44,7 +46,7 @@ export const decodeBlock = (data: Buffer): { block: LegacyBlock; schema: LegacyB
 		block: {
 			...rawBlock,
 			header: {
-				...codec.decode(blockSchema.header, rawBlock.header),
+				...codec.decode<LegacyBlockHeaderWithID>(blockSchema.header, rawBlock.header),
 				id,
 			},
 		},
