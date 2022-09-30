2fe14446ed87d9f1be5453fb35611e82a7180c39
Mitsuaki Uchimoto <mitsuaki-u@users.noreply.github.com>
2022-09-30 13:23:45 +0200

    Rename ccm send fail event file name

---
+++ framework/src/modules/interoperability/events/ccm_send_fail.ts
@@ -0,0 +1,44 @@
+/*
+ * Copyright © 2022 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+import { BaseEvent, EventQueuer } from '../../base_event';
+import { ccmSchema } from '../schemas';
+
+export interface CcmSendFailEventData {
+	ccmID: Buffer;
+}
+
+export const ccmSendFailDataSchema = {
+	$id: '/interoperability/events/ccmSendFail',
+	type: 'object',
+	required: ['ccm', 'code'],
+	properties: {
+		ccm: {
+			...ccmSchema,
+			fieldNumber: 1,
+		},
+		code: {
+			dataType: 'uint32',
+			fieldNumber: 2,
+		},
+	},
+};
+
+export class CcmSendFailEvent extends BaseEvent<CcmSendFailEventData> {
+	public schema = ccmSendFailDataSchema;
+
+	public log(ctx: EventQueuer, data: CcmSendFailEventData): void {
+		this.add(ctx, data, []);
+	}
+}
