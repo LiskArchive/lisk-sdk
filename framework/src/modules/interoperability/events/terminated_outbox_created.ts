65cb7079d73b0434f40d037ee32ff3f1d5a7ec25
Franco NG <franco.ng@lightcurve.io>
2022-10-13 15:11:31 +0200

    Add Event

---
+++ framework/src/modules/interoperability/events/terminated_outbox_created.ts
@@ -0,0 +1,24 @@
+/*
+ * Copyright © 2022 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+import { BaseEvent, EventQueuer } from '../../base_event';
+import { TerminatedOutboxAccount, terminatedOutboxSchema } from '../stores/terminated_outbox';
+
+export class TerminatedOutboxCreated extends BaseEvent<TerminatedOutboxAccount> {
+	public schema = terminatedOutboxSchema;
+
+	public log(ctx: EventQueuer, chainID: Buffer, data: TerminatedOutboxAccount): void {
+		this.add(ctx, data, [chainID]);
+	}
+}
