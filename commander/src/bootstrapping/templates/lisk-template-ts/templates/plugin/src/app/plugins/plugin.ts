f2e4741ed7314816a40100d082dd0134f2ca7bda
Incede <33103370+Incede@users.noreply.github.com>
2022-12-01 03:47:09 +0100

    Fix plugin template

--- commander/src/bootstrapping/templates/lisk-template-ts/templates/plugin/src/app/plugins/plugin.ts
+++ commander/src/bootstrapping/templates/lisk-template-ts/templates/plugin/src/app/plugins/plugin.ts
@@ -4,25 +4,14 @@ import type { BaseChannel } from 'lisk-sdk';
  /* eslint-disable class-methods-use-this */
  /* eslint-disable  @typescript-eslint/no-empty-function */
  export class <%= className %> extends BasePlugin {
-	// private _channel!: BaseChannel;
 
 	public name: '<%= name %>';
 
 	public get nodeModulePath(): string {
-		return <%= __filename %>;
+		return  __filename;
 	}
 
-	public get events(): string[] {
-		return [
-			// 'block:created',
-			// 'block:missed'
-		];
-	}
-
-	public async load(_: BaseChannel): Promise<void> {
-		// this._channel = channel;
-		// this._channel.once('app:ready', () => {});
-	}
+	public async load(_: BaseChannel): Promise<void> {}
 
 	public async unload(): Promise<void> {}
 }
