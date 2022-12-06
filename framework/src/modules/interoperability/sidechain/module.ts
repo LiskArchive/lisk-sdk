68130698f20a3873e6b01b7a409134d1887729b8
shuse2 <shus.toda@gmail.com>
2022-12-06 15:34:43 +0100

    :seedling: Add store information to metadata

--- framework/src/modules/interoperability/sidechain/module.ts
+++ framework/src/modules/interoperability/sidechain/module.ts
@@ -179,6 +179,10 @@ export class SidechainInteroperabilityModule extends BaseInteroperabilityModule 
 					data: genesisInteroperabilitySchema,
 				},
 			],
+			stores: this.stores.values().map(v => ({
+				key: v.key.toString('hex'),
+				data: v.schema,
+			})),
 		};
 	}
 
