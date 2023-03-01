e961ca4ad10ca09c3f85194e09cfd3f15703cbfc
Mitsuaki Uchimoto <mitsuaki-u@users.noreply.github.com>
2023-03-01 13:46:58 +0100

    Implement isChainIDAvailable endpoint

--- framework/src/modules/interoperability/mainchain/endpoint.ts
+++ framework/src/modules/interoperability/mainchain/endpoint.ts
@@ -12,8 +12,15 @@
  * Removal or modification of this copyright notice is prohibited.
  */
 
+import { ModuleEndpointContext } from '../../../types';
 import { BaseInteroperabilityEndpoint } from '../base_interoperability_endpoint';
 import { CHAIN_REGISTRATION_FEE, MIN_RETURN_FEE_PER_BYTE_BEDDOWS } from '../constants';
+import { RegisteredNamesStore } from '../stores/registered_names';
+import { isValidName } from '../utils';
+
+export interface ResultJSON {
+	result: boolean;
+}
 
 export class MainchainInteroperabilityEndpoint extends BaseInteroperabilityEndpoint {
 	public getRegistrationFee(): { fee: string } {
@@ -23,6 +30,29 @@ export class MainchainInteroperabilityEndpoint extends BaseInteroperabilityEndpo
 	public getMinimumMessageFee(): { fee: string } {
 		return {
 			fee: MIN_RETURN_FEE_PER_BYTE_BEDDOWS.toString(),
+		};
+	}
+
+	public async isChainNameAvailable(context: ModuleEndpointContext): Promise<ResultJSON> {
+		const {
+			params: { name },
+		} = context;
+
+		if (typeof name !== 'string') {
+			throw new Error('Chain name must be a string.');
+		}
+
+		if (!isValidName(name)) {
+			throw new Error(
+				`Invalid name property. It should contain only characters from the set [a-z0-9!@$&_.].`,
+			);
+		}
+
+		const nameSubstore = this.stores.get(RegisteredNamesStore);
+		const nameExists = await nameSubstore.has(context, Buffer.from(name, 'ascii'));
+
+		return {
+			result: !nameExists,
 		};
 	}
 }
