ed307e6b033c71c7e14a8ee4af3a56f93d2afeb0
Ishan <ishantiw.quasar@gmail.com>
2023-03-07 16:27:36 +0100

    🐛 Add interopMethod dependency to token module ccMethod - Closes #8233

--- framework/src/modules/token/module.ts
+++ framework/src/modules/token/module.ts
@@ -125,6 +125,7 @@ export class TokenModule extends BaseInteroperableModule {
 
 	public addDependencies(interoperabilityMethod: InteroperabilityMethod, feeMethod: FeeMethod) {
 		this.method.addDependencies(interoperabilityMethod, this._internalMethod);
+		this.crossChainMethod.addDependencies(interoperabilityMethod);
 		this._internalMethod.addDependencies(feeMethod);
 	}
 
