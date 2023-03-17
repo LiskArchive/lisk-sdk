d18e05c15ab5912d27a0981e64f469124cd0fa87
Ishan <ishantiw.quasar@gmail.com>
2023-03-17 17:05:49 +0100

    🌱 Add pos-sidechain-fast for interop

---
+++ examples/interop/pos-sidechain-fast/jest.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+	testMatch: ['<rootDir>/test/**/?(*.)+(spec|test).+(ts|tsx|js)'],
+	setupFilesAfterEnv: ['<rootDir>/test/_setup.js'],
+	transform: {
+		'^.+\\.(ts|tsx)$': [
+			'ts-jest',
+			{
+				tsconfig: '<rootDir>/tsconfig.json',
+			},
+		],
+	},
+	verbose: false,
+	collectCoverage: false,
+	coverageReporters: ['json'],
+	coverageDirectory: '.coverage',
+	/**
+	 * restoreMocks [boolean]
+	 *
+	 * Default: false
+	 *
+	 * Automatically restore mock state between every test.
+	 * Equivalent to calling jest.restoreAllMocks() between each test.
+	 * This will lead to any mocks having their fake implementations removed
+	 * and restores their initial implementation.
+	 */
+	restoreMocks: true,
+	resetMocks: true,
+
+	/**
+	 * resetModules [boolean]
+	 *
+	 * Default: false
+	 *
+	 * By default, each test file gets its own independent module registry.
+	 * Enabling resetModules goes a step further and resets the module registry before running each individual test.
+	 * This is useful to isolate modules for every test so that local module state doesn't conflict between tests.
+	 * This can be done programmatically using jest.resetModules().
+	 */
+	resetModules: true,
+};
