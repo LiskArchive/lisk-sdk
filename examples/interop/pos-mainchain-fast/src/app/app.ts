f95c206c80c7adfbdc3f137da685e5c68d29b6c7
Ishan <ishantiw.quasar@gmail.com>
2023-03-20 14:47:29 +0100

    🌱 Add pm2 scripts

--- examples/interop/pos-mainchain-fast/src/app/app.ts
+++ examples/interop/pos-mainchain-fast/src/app/app.ts
@@ -4,7 +4,7 @@ import { registerModules } from './modules';
 import { registerPlugins } from './plugins';
 
 export const getApplication = (config: PartialApplicationConfig): Application => {
-	const { app } = Application.defaultApplication(config);
+	const { app } = Application.defaultApplication(config, true);
 
 	registerModules(app);
 	registerPlugins(app);
