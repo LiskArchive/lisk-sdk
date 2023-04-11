a3b5eebc35ef70e21ecab69fdff6d8280f531951
Ishan <ishantiw.quasar@gmail.com>
2023-04-11 12:46:40 +0200

    🌱 Add cross chain transfer scripts

---
+++ examples/interop/pos-sidechain-example-one/config/scripts/transfer_lsk_mainchain.ts
@@ -0,0 +1,82 @@
+import { apiClient, codec, cryptography, Schema, Transaction } from 'lisk-sdk';
+import { keys } from '../default/dev-validators.json';
+type ModulesMetadata = [
+	{
+		stores: { key: string; data: Schema }[];
+		events: { name: string; data: Schema }[];
+		name: string;
+		commands: { name: string; params: Schema }[];
+	},
+];
+(async () => {
+	const { address } = cryptography;
+
+	const nodeAlias = 'one';
+	const tokenID = Buffer.from('0400000000000000', 'hex');
+	const mainchainID = Buffer.from('04000000', 'hex');
+	const recipientAddress = address.getAddressFromLisk32Address(
+		'lskzjzeam6szx4a65sxgavr98m9h4kctcx85nvy7h',
+	);
+
+	const sidechainClient = await apiClient.createIPCClient(`~/.lisk/pos-sidechain-example-one`);
+
+	const mainchainNodeInfo = await sidechainClient.invoke('system_getNodeInfo');
+
+	const { modules: modulesMetadata } = await sidechainClient.invoke<{
+		modules: ModulesMetadata;
+	}>('system_getMetadata');
+
+	const tokenMetadata = modulesMetadata.find(m => m.name === 'token');
+
+	const ccTransferCMDSchema = tokenMetadata?.commands.filter(
+		cmd => cmd.name == 'transferCrossChain',
+	)[0].params as Schema;
+
+	const params = {
+		tokenID,
+		amount: BigInt('990000000'),
+		receivingChainID: mainchainID,
+		recipientAddress,
+		data: 'cc transfer testing',
+		messageFee: BigInt('10000000'),
+		messageFeeTokenID: tokenID,
+	};
+
+	const relayerkeyInfo = keys[48];
+
+	const { nonce } = await sidechainClient.invoke<{ nonce: string }>('auth_getAuthAccount', {
+		address: address.getLisk32AddressFromPublicKey(Buffer.from(relayerkeyInfo.publicKey, 'hex')),
+	});
+
+	const tx = new Transaction({
+		module: 'token',
+		command: 'transferCrossChain',
+		fee: BigInt(200000000),
+		params: codec.encode(ccTransferCMDSchema, params),
+		nonce: BigInt(nonce),
+		senderPublicKey: Buffer.from(relayerkeyInfo.publicKey, 'hex'),
+		signatures: [],
+	});
+
+	tx.sign(
+		Buffer.from(mainchainNodeInfo.chainID as string, 'hex'),
+		Buffer.from(relayerkeyInfo.privateKey, 'hex'),
+	);
+
+	const result = await sidechainClient.invoke<{
+		transactionId: string;
+	}>('txpool_postTransaction', {
+		transaction: tx.getBytes().toString('hex'),
+	});
+
+	console.log(
+		`Sent cross chain transfer transaction (amount: ${params.amount.toString()}, recipient: ${
+			params.recipientAddress
+		}) from sidechain (sidechainID: ${
+			params.receivingChainID
+		}) node ${nodeAlias} to mainchain. Result from transaction pool is: `,
+		result,
+	);
+
+	process.exit(0);
+})();
