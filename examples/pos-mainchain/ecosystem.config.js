6d93262b6cc28e244d3e085d31103af49ac75615
shuse2 <shus.toda@gmail.com>
2023-04-14 09:51:55 +0200

    :recycle: Expose api from followers

--- examples/pos-mainchain/ecosystem.config.js
+++ examples/pos-mainchain/ecosystem.config.js
@@ -4,12 +4,13 @@ const num = 10;
 const followers = new Array(num).fill(0).map((_, i) => ({
 	name: `follower_${i}`,
 	script: './bin/run',
-	args: 'start',
+	args: 'start --api-http --api-ws',
 	interpreter: 'node',
 	env: {
 		LISK_LOG_LEVEL: 'debug',
 		LISK_NETWORK: 'alphanet',
 		LISK_PORT: 7667 + i + 1,
+		LISK_API_WS_PORT: 7887 + i + 1,
 		LISK_SEED_PEERS: `127.0.0.1:7667`,
 		LISK_DATA_PATH: `${os.tmpdir()}/follower_${i}`,
 	},
