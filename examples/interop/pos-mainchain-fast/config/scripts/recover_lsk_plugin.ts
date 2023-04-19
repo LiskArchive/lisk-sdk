b289f6418bb41226d00d27a7ec364266b0a433e3
Ishan <ishantiw.quasar@gmail.com>
2023-04-19 17:38:48 +0200

    🌱 Add recovery plugin script

---
+++ examples/interop/pos-mainchain-fast/config/scripts/recover_lsk_plugin.ts
@@ -0,0 +1,340 @@
+import {
+	apiClient,
+	codec,
+	cryptography,
+	Transaction,
+	chain,
+	db,
+	ProveResponse,
+	OutboxRootWitness,
+	ChainAccountJSON,
+	ChainStatus,
+	stateRecoveryParamsSchema,
+} from 'lisk-sdk';
+import { keys } from '../default/dev-validators.json';
+import { ensureDir } from 'fs-extra';
+import { join } from 'path';
+import * as os from 'os';
+
+// Schemas
+export const inclusionProofsSchema = {
+	$id: `scripts/recovery/inclusionProofs`,
+	type: 'object',
+	properties: {
+		inclusionProofs: {
+			type: 'array',
+			fieldNumber: 1,
+			items: {
+				type: 'object',
+				properties: {
+					height: { dataType: 'uint32', fieldNumber: 1 },
+					inclusionProof: {
+						type: 'object',
+						fieldNumber: 2,
+						properties: {
+							siblingHashes: {
+								type: 'array',
+								fieldNumber: 1,
+								items: {
+									dataType: 'bytes',
+								},
+							},
+							bitmap: {
+								dataType: 'bytes',
+								fieldNumber: 2,
+							},
+							key: {
+								dataType: 'bytes',
+								fieldNumber: 3,
+							},
+							value: {
+								dataType: 'bytes',
+								fieldNumber: 4,
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+};
+
+// Types
+type KVStore = db.Database;
+interface Data {
+	readonly blockHeader: chain.BlockHeaderJSON;
+}
+type Primitive = string | number | bigint | boolean | null | undefined;
+type Replaced<T, TReplace, TWith, TKeep = Primitive> = T extends TReplace | TKeep
+	? T extends TReplace
+		? TWith | Exclude<T, TReplace>
+		: T
+	: {
+			[P in keyof T]: Replaced<T[P], TReplace, TWith, TKeep>;
+	  };
+
+export type JSONObject<T> = Replaced<T, bigint | Buffer, string>;
+interface InclusionProof {
+	height: number;
+	inclusionProof: OutboxRootWitness & { key: Buffer; value: Buffer };
+}
+interface StoreEntry {
+	substorePrefix: Buffer;
+	storeKey: Buffer;
+	storeValue: Buffer;
+	bitmap: Buffer;
+}
+interface StateRecoveryParams {
+	chainID: Buffer;
+	module: string;
+	storeEntries: StoreEntry[];
+	siblingHashes: Buffer[];
+}
+
+// Utils
+const getDBInstance = async (
+	dataPath: string,
+	dbName = 'lisk-framework-chain-connector-plugin.db',
+): Promise<KVStore> => {
+	const dirPath = join(dataPath.replace('~', os.homedir()), 'plugins/data', dbName);
+	await ensureDir(dirPath);
+
+	return new db.Database(dirPath);
+};
+
+export const proveResponseJSONToObj = (proveResponseJSON: ProveResponseJSON): ProveResponse => {
+	const {
+		proof: { queries, siblingHashes },
+	} = proveResponseJSON;
+
+	return {
+		proof: {
+			queries: queries.map(query => ({
+				bitmap: Buffer.from(query.bitmap, 'hex'),
+				key: Buffer.from(query.key, 'hex'),
+				value: Buffer.from(query.value, 'hex'),
+			})),
+			siblingHashes: siblingHashes.map(siblingHash => Buffer.from(siblingHash, 'hex')),
+		},
+	};
+};
+
+// Constants
+const DB_KEY_INCLUSION_PROOF = Buffer.from([1]);
+
+// DB class
+class RecoveryDB {
+	private readonly _db: KVStore;
+
+	public constructor(db: KVStore) {
+		this._db = db;
+	}
+
+	public close() {
+		this._db.close();
+	}
+
+	public async getAllInclusionProofs(): Promise<InclusionProof[]> {
+		let inclusionProofs: InclusionProof[] = [];
+		try {
+			const encodedInfo = await this._db.get(DB_KEY_INCLUSION_PROOF);
+			inclusionProofs = codec.decode<{ inclusionProofs: InclusionProof[] }>(
+				inclusionProofsSchema,
+				encodedInfo,
+			).inclusionProofs;
+		} catch (error) {
+			if (!(error instanceof db.NotFoundError)) {
+				throw error;
+			}
+		}
+		return inclusionProofs;
+	}
+
+	public async getInclusionProofByHeight(height: number): Promise<InclusionProof | undefined> {
+		const inclusionProofs = await this.getAllInclusionProofs();
+
+		return inclusionProofs.find(proofs => proofs.height === height);
+	}
+
+	public async deleteInclusionProofsUntilHeight(height: number) {
+		const inclusionProofs = await this.getAllInclusionProofs();
+		const encodedInfo = codec.encode(inclusionProofsSchema, {
+			inclusionProofs: inclusionProofs.filter(proofs => proofs.height > height),
+		});
+		await this._db.set(DB_KEY_INCLUSION_PROOF, encodedInfo);
+	}
+
+	public async setInclusionProof(inclusionProof: InclusionProof) {
+		const allInclusionProofs = await this.getAllInclusionProofs();
+		allInclusionProofs.push(inclusionProof);
+		const encodedInfo = codec.encode(inclusionProofsSchema, {
+			inclusionProofs: allInclusionProofs,
+		});
+		await this._db.set(DB_KEY_INCLUSION_PROOF, encodedInfo);
+	}
+}
+
+type ProveResponseJSON = JSONObject<ProveResponse>;
+(async () => {
+	console.log('Starting recovery plugin ...');
+
+	const { address, utils } = cryptography;
+
+	let recoveryDB: RecoveryDB;
+
+	try {
+		recoveryDB = new RecoveryDB(await getDBInstance('~/.lisk/auxiliary/recoveryLSK'));
+		console.log('Recovery DB is initialized successfully.');
+	} catch (error) {
+		console.log('Error occurred while initializing DB', error);
+
+		process.exit();
+	}
+	// const senderLSKAddress = 'lskxz85sur2yo22dmcxybe39uvh2fg7s2ezxq4ny9';
+	const sidechainBinaryAddress = address.getAddressFromPublicKey(
+		Buffer.from('2136cd87c5b60224291b0c374f315d325fd58ce10ca4d5989d1e2d371dc428ef', 'hex'),
+	);
+
+	const mainchainClient = await apiClient.createIPCClient(`~/.lisk/mainchain-node-one`);
+	const sidechainClient = await apiClient.createIPCClient(`~/.lisk/pos-sidechain-example-one`);
+	const mainchainNodeInfo = await mainchainClient.invoke('system_getNodeInfo');
+	const sidechainNodeInfo = await sidechainClient.invoke('system_getNodeInfo');
+
+	const getProofForMonitoringKey = async (client: apiClient.APIClient, monitoredKey) => {
+		const proof = await client.invoke<ProveResponseJSON>('state_prove', {
+			queryKeys: [monitoredKey.toString('hex')],
+		});
+
+		return proveResponseJSONToObj(proof);
+	};
+
+	// Collect inclusion proofs on sidechain and save it in recoveryDB
+	sidechainClient.subscribe('chain_newBlock', async (data?: Record<string, unknown>) => {
+		const { blockHeader: receivedBlock } = data as unknown as Data;
+
+		const newBlockHeader = chain.BlockHeader.fromJSON(receivedBlock).toObject();
+		console.log(
+			`\nReceived new block on sidechain ${sidechainNodeInfo.chainID} with height ${newBlockHeader.height}\n`,
+		);
+		const keyToBeRecovered = Buffer.concat([
+			Buffer.from('3c469e9d0000', 'hex'),
+			utils.hash(sidechainBinaryAddress),
+		]);
+
+		const { proof } = await getProofForMonitoringKey(sidechainClient, keyToBeRecovered);
+
+		const outboxRootWitness = {
+			key: proof.queries[0].key,
+			value: proof.queries[0].value,
+			bitmap: proof.queries[0].bitmap,
+			siblingHashes: proof.siblingHashes,
+		};
+
+		await recoveryDB.setInclusionProof({
+			inclusionProof: outboxRootWitness,
+			height: newBlockHeader.height,
+		});
+		console.log(`Successfully stored inclusion proof at height ${newBlockHeader.height}!\n`);
+	});
+
+	mainchainClient.subscribe('chain_newBlock', async (_data?: Record<string, unknown>) => {
+		const lastCertificateOfSidechain = await mainchainClient.invoke<ChainAccountJSON>(
+			'interoperability_getChainAccount',
+			{ chainID: sidechainNodeInfo.chainID },
+		);
+
+		if (lastCertificateOfSidechain.status !== ChainStatus.TERMINATED) {
+			// Delete all the inclusion proofs until lastCertificate height
+			await recoveryDB.deleteInclusionProofsUntilHeight(
+				lastCertificateOfSidechain.lastCertificate.height,
+			);
+			console.log(
+				`Successfully deleted all the inclusion proofs before last certifcate height ${lastCertificateOfSidechain.lastCertificate.height}!`,
+			);
+		} else {
+			// Create recovery transaction
+			const relayerkeyInfo = keys[4];
+			const { nonce } = await sidechainClient.invoke<{ nonce: string }>('auth_getAuthAccount', {
+				address: address.getLisk32AddressFromPublicKey(
+					Buffer.from(relayerkeyInfo.publicKey, 'hex'),
+				),
+			});
+
+			const siblingHashesAfterLastCertificate = await recoveryDB.getInclusionProofByHeight(
+				lastCertificateOfSidechain.lastCertificate.height + 1,
+			);
+			if (!siblingHashesAfterLastCertificate) {
+				throw new Error(
+					`No siblingHash exists at a given height: ${lastCertificateOfSidechain.lastCertificate.height}`,
+				);
+			}
+			const stateRecoveryParams: StateRecoveryParams = {
+				chainID: Buffer.from(sidechainNodeInfo.chainID as string, 'hex'),
+				module: 'token',
+				siblingHashes: siblingHashesAfterLastCertificate.inclusionProof.siblingHashes,
+				storeEntries: [
+					{
+						bitmap: siblingHashesAfterLastCertificate.inclusionProof.bitmap,
+						storeKey: siblingHashesAfterLastCertificate.inclusionProof.key,
+						storeValue: siblingHashesAfterLastCertificate.inclusionProof.value,
+						substorePrefix: Buffer.from('0000', 'hex'),
+					},
+				],
+			};
+
+			const tx = new Transaction({
+				module: 'interoperability',
+				command: 'recoverState',
+				fee: BigInt(500000000),
+				params: codec.encodeJSON(stateRecoveryParamsSchema, stateRecoveryParams),
+				nonce: BigInt(nonce),
+				senderPublicKey: Buffer.from(relayerkeyInfo.publicKey, 'hex'),
+				signatures: [],
+			});
+
+			tx.sign(
+				Buffer.from(mainchainNodeInfo.chainID as string, 'hex'),
+				Buffer.from(relayerkeyInfo.privateKey, 'hex'),
+			);
+
+			const result = await mainchainClient.invoke<{
+				transactionId: string;
+			}>('txpool_postTransaction', {
+				transaction: tx.getBytes().toString('hex'),
+			});
+
+			console.log('\nSent state recovery transaction. Result from transaction pool is: ', result);
+
+			cleanUp();
+		}
+	});
+
+	const cleanUp = async () => {
+		await sidechainClient.disconnect();
+		await mainchainClient.disconnect();
+		recoveryDB.close();
+		console.log(
+			'>>>>>> Closed recovery DB, disconnected API clients and exiting the app. <<<<<<\n',
+		);
+		process.exit(0);
+	};
+
+	process.on('uncaughtException', async err => {
+		// Handle error safely
+		console.log('uncaughtException: ', err);
+
+		await cleanUp();
+	});
+
+	process.on('unhandledRejection', async err => {
+		// Handle error safely
+		console.log('unhandledRejection: ', err);
+		await cleanUp();
+	});
+
+	//do something when app is closing
+	process.on('exit', cleanUp);
+
+	//catches ctrl+c event
+	process.on('SIGINT', cleanUp);
+})();
