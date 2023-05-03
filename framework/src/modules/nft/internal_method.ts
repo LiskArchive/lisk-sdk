afa3f9247b3691ec8a20c9430b5593fa2cb2df3f
shuse2 <shus.toda@gmail.com>
2023-05-03 17:20:08 +0200

    :seedling: Add template files

---
+++ framework/src/modules/nft/internal_method.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright © 2023 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+import { BaseMethod } from '../base_method';
+import { ModuleConfig } from './types';
+
+export class InternalMethod extends BaseMethod {
+	// @ts-expect-error TODO: unused error. Remove when implementing.
+	private _config!: ModuleConfig;
+
+	public init(config: ModuleConfig): void {
+		this._config = config;
+	}
+}
