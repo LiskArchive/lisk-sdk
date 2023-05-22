4c0e4899d4a4cda74f5684a3b2b0b4de82b0f0c5
Ishan <ishantiw.quasar@gmail.com>
2023-05-22 18:00:23 +0200

    🌱 Initialize stores

---
+++ framework/src/modules/poa/stores/validator.ts
@@ -0,0 +1,37 @@
+/*
+ * Copyright © 2023 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+import { BaseStore } from '../../base_store';
+import { MAX_LENGTH_NAME } from '../constants';
+
+export interface ValidatorObject {
+	name: string;
+}
+
+export const validatorObjectSchema = {
+	$id: 'lisk/poa/validatorObject',
+	type: 'object',
+	required: ['name'],
+	properties: {
+		name: {
+			type: 'string',
+			fieldNumber: 1,
+			minLength: 1,
+			maxLength: MAX_LENGTH_NAME,
+		},
+	},
+};
+
+export class ValidatorStore extends BaseStore<ValidatorObject> {
+	public schema = validatorObjectSchema;
+}
