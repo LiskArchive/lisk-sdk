5f29eab81ec13d038f39f0b385e923d0d1892b33
shuse2 <shus.toda@gmail.com>
2023-06-02 10:57:21 +0200

    :recycle: Update test setup to speedup encryption

--- commander/test/bootstrapping/commands/keys/create.spec.ts
+++ commander/test/bootstrapping/commands/keys/create.spec.ts
@@ -95,6 +95,24 @@ describe('keys:create command', () => {
 		jest.spyOn(cryptography.address, 'getAddressFromPublicKey');
 		jest.spyOn(cryptography.bls, 'getPrivateKeyFromPhraseAndPath');
 		jest.spyOn(cryptography.bls, 'getPublicKeyFromPrivateKey');
+		jest.spyOn(cryptography.encrypt, 'encryptMessageWithPassword').mockResolvedValue({
+			ciphertext:
+				'3735d984bd46e476019696afd973f6eaeb591e974f99d143d9292a01a4e65dabccb7fac3091e8c3340eeb16c83d5ecc9cac627d2154efc358d4d3318358eddbca9411e20bf77113252407fc94f4fbf0330a7102a7cd990e4952d9efdf20998f72b6a51f0c17a19ffa72118ebf1114b73ee7c2227ec4d1253ecd0df33cfaa72ee68ee0ddeff72db43a98646e1e55551c261dcf263f5bec55bb84672b5b7c234ffdf9551eeb9d80cb72480adef673d1b37ba12fca26638bd5e',
+			mac: '6f00f5a62b8c7e640f85e6c02c64479b5c9137fdd5e9fa38c2edcd11483ce743',
+			kdf: cryptography.encrypt.KDF.ARGON2,
+			kdfparams: {
+				parallelism: 4,
+				iterations: 1,
+				memorySize: 2097023,
+				salt: 'bd25ddc98eba7d8bf60a6738fca3ac23',
+			},
+			cipher: cryptography.encrypt.Cipher.AES256GCM,
+			cipherparams: {
+				iv: '1933be196b54d01fd0979294',
+				tag: '11398f4dcfd776d783a35721c326f007',
+			},
+			version: '1',
+		});
 		jest.spyOn(fs, 'ensureDirSync').mockReturnValue();
 		jest.spyOn(fs, 'writeJSONSync').mockReturnValue();
 		jest.spyOn(readerUtils, 'getPassphraseFromPrompt').mockImplementation(async (name?: string) => {
