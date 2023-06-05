53c4ae11374d4a7489f2a9e263f7b79920c50454
shuse2 <shus.toda@gmail.com>
2023-06-05 15:48:34 +0200

    :recycle: fix module template to be buildable when generated

--- commander/src/bootstrapping/templates/lisk-template-ts/templates/module/src/app/modules/module.ts
+++ commander/src/bootstrapping/templates/lisk-template-ts/templates/module/src/app/modules/module.ts
@@ -1,17 +1,19 @@
 /* eslint-disable class-methods-use-this */
+/* eslint-disable @typescript-eslint/member-ordering */
 
 import {
     BaseModule,
-    ModuleInitArgs,
-    InsertAssetContext,
-	BlockVerifyContext,
-	TransactionVerifyContext,
-	VerificationResult,
-	TransactionExecuteContext,
-	GenesisBlockExecuteContext,
 	ModuleMetadata,
-	BlockExecuteContext,
-	BlockAfterExecuteContext,
+	// ModuleInitArgs,
+	// InsertAssetContext,
+	// BlockVerifyContext,
+	// TransactionVerifyContext,
+	// VerificationResult,
+	// TransactionExecuteContext,
+	// GenesisBlockExecuteContext,
+	// BlockExecuteContext,
+	// BlockAfterExecuteContext,
+	// VerifyStatus,
 } from 'lisk-sdk';
 import { <%= moduleClass %>Endpoint } from './endpoint';
 import { <%= moduleClass %>Method } from './method';
@@ -21,64 +23,57 @@ export class <%= moduleClass %>Module extends BaseModule {
     public method = new <%= moduleClass %>Method(this.stores, this.events);
     public commands = [];
 
-	public constructor() {
-		super();
-		// registeration of stores and events
-	}
+	// public constructor() {
+	// 	super();
+	// 	// registeration of stores and events
+	// }
 
 	public metadata(): ModuleMetadata {
 		return {
-			name: '',
+			...this.baseMetadata(),
 			endpoints: [],
-			commands: this.commands.map(command => ({
-				name: command.name,
-				params: command.schema,
-			})),
-			events: this.events.values().map(v => ({
-				name: v.name,
-				data: v.schema,
-			})),
 			assets: [],
 		};
 	}
 
     // Lifecycle hooks
-    public async init(_args: ModuleInitArgs): Promise<void> {
-		// initialize this module when starting a node
-	}
+    // public async init(_args: ModuleInitArgs): Promise<void> {
+	// 	// initialize this module when starting a node
+	// }
 
-	public async insertAssets(_context: InsertAssetContext) {
-		// initialize block generation, add asset
-	}
+	// public async insertAssets(_context: InsertAssetContext) {
+	// 	// initialize block generation, add asset
+	// }
 
-	public async verifyAssets(_context: BlockVerifyContext): Promise<void> {
-		// verify block
-	}
+	// public async verifyAssets(_context: BlockVerifyContext): Promise<void> {
+	// 	// verify block
+	// }
 
     // Lifecycle hooks
-	public async verifyTransaction(_context: TransactionVerifyContext): Promise<VerificationResult> {
+	// public async verifyTransaction(_context: TransactionVerifyContext): Promise<VerificationResult> {
 		// verify transaction will be called multiple times in the transaction pool
-	}
+		// return { status: VerifyStatus.OK };
+	// }
 
-	public async beforeCommandExecute(_context: TransactionExecuteContext): Promise<void> {
-	}
+	// public async beforeCommandExecute(_context: TransactionExecuteContext): Promise<void> {
+	// }
 
-	public async afterCommandExecute(_context: TransactionExecuteContext): Promise<void> {
+	// public async afterCommandExecute(_context: TransactionExecuteContext): Promise<void> {
 
-	}
-	public async initGenesisState(_context: GenesisBlockExecuteContext): Promise<void> {
+	// }
+	// public async initGenesisState(_context: GenesisBlockExecuteContext): Promise<void> {
 
-	}
+	// }
 
-	public async finalizeGenesisState(_context: GenesisBlockExecuteContext): Promise<void> {
+	// public async finalizeGenesisState(_context: GenesisBlockExecuteContext): Promise<void> {
 
-	}
+	// }
 
-	public async beforeTransactionsExecute(_context: BlockExecuteContext): Promise<void> {
+	// public async beforeTransactionsExecute(_context: BlockExecuteContext): Promise<void> {
 
-	}
+	// }
 
-	public async afterTransactionsExecute(_context: BlockAfterExecuteContext): Promise<void> {
+	// public async afterTransactionsExecute(_context: BlockAfterExecuteContext): Promise<void> {
 
-	}
+	// }
 }
