eb82d3a490f5c8f135fff5a35a1b4da7b98a393d
Franco NG <franco.ng@lightcurve.io>
2023-06-05 11:16:35 +0200

    PoA Register Authority Command (#8496)
    * Implement Register Authority Commands
    
    * Renaming Commands
    
    * Remove extra files
    
    * Added Unit Test for RegisterAuthority
    
    * Put types back to PoA
    
    * Update code according to PR review and comments
    
    * Update chainID in test case to match with actual format
    
    * Update param names to be grammar-correct
    
    * Update AUTHORITY_REGISTRATION_FEE comments and test unit imports
    
    * Update $id for updateAuthority Schema
    
    * Change name of updateAuthoritySchema

--- framework/src/modules/poa/schemas.ts
+++ framework/src/modules/poa/schemas.ts
@@ -13,12 +13,126 @@
  */
 
 import {
-	NUM_BYTES_ADDRESS,
-	MAX_LENGTH_NAME,
-	LENGTH_PROOF_OF_POSSESSION,
 	LENGTH_BLS_KEY,
 	LENGTH_GENERATOR_KEY,
+	LENGTH_PROOF_OF_POSSESSION,
+	MAX_LENGTH_NAME,
+	NUM_BYTES_ADDRESS,
 } from './constants';
+
+const validator = {
+	type: 'object',
+	required: ['address', 'weight'],
+	properties: {
+		address: {
+			dataType: 'bytes',
+			minLength: NUM_BYTES_ADDRESS,
+			maxLength: NUM_BYTES_ADDRESS,
+			fieldNumber: 1,
+		},
+		weight: {
+			dataType: 'uint64',
+			fieldNumber: 2,
+		},
+	},
+};
+
+// https://github.com/LiskHQ/lips/blob/main/proposals/lip-0047.md#register-authority-command
+export const registerAuthoritySchema = {
+	$id: '/poa/command/registerAuthority',
+	type: 'object',
+	required: ['name', 'blsKey', 'proofOfPossession', 'generatorKey'],
+	properties: {
+		name: {
+			dataType: 'string',
+			minLength: 1,
+			maxLength: MAX_LENGTH_NAME,
+			fieldNumber: 1,
+		},
+		blsKey: {
+			dataType: 'bytes',
+			minLength: LENGTH_BLS_KEY,
+			maxLength: LENGTH_BLS_KEY,
+			fieldNumber: 2,
+		},
+		proofOfPossession: {
+			dataType: 'bytes',
+			minLength: LENGTH_PROOF_OF_POSSESSION,
+			maxLength: LENGTH_PROOF_OF_POSSESSION,
+			fieldNumber: 3,
+		},
+		generatorKey: {
+			dataType: 'bytes',
+			minLength: LENGTH_GENERATOR_KEY,
+			maxLength: LENGTH_GENERATOR_KEY,
+			fieldNumber: 4,
+		},
+	},
+};
+
+export const updateGeneratorKeySchema = {
+	$id: '/poa/command/updateGeneratorKey',
+	type: 'object',
+	required: ['generatorKey'],
+	properties: {
+		generatorKey: {
+			dataType: 'bytes',
+			minLength: LENGTH_GENERATOR_KEY,
+			maxLength: LENGTH_GENERATOR_KEY,
+			fieldNumber: 1,
+		},
+	},
+};
+
+export const updateAuthoritySchema = {
+	$id: '/poa/command/updateAuthority',
+	type: 'object',
+	required: ['newValidators', 'threshold', 'validatorsUpdateNonce', 'signature', 'aggregationBits'],
+	properties: {
+		newValidators: {
+			type: 'array',
+			fieldNumber: 1,
+			items: validator,
+		},
+		threshold: {
+			dataType: 'uint64',
+			fieldNumber: 2,
+		},
+		validatorsUpdateNonce: {
+			dataType: 'uint32',
+			fieldNumber: 3,
+		},
+		signature: {
+			dataType: 'bytes',
+			fieldNumber: 4,
+		},
+		aggregationBits: {
+			dataType: 'bytes',
+			fieldNumber: 5,
+		},
+	},
+};
+
+export const validatorSignatureMessageSchema = {
+	$id: '/poa/command/validatorSignatureMessage',
+	type: 'object',
+	required: ['newValidators', 'threshold', 'validatorsUpdateNonce'],
+	properties: {
+		newValidators: {
+			type: 'array',
+			fieldNumber: 1,
+			items: validator,
+		},
+		threshold: {
+			dataType: 'uint64',
+			fieldNumber: 2,
+		},
+		validatorsUpdateNonce: {
+			dataType: 'uint32',
+			fieldNumber: 3,
+		},
+	},
+};
 
 // https://github.com/LiskHQ/lips/blob/main/proposals/lip-0047.md#genesis-poa-store-schema
 export const genesisPoAStoreSchema = {
@@ -72,22 +186,7 @@ export const genesisPoAStoreSchema = {
 				activeValidators: {
 					type: 'array',
 					fieldNumber: 1,
-					items: {
-						type: 'object',
-						required: ['address', 'weight'],
-						properties: {
-							address: {
-								dataType: 'bytes',
-								minLength: NUM_BYTES_ADDRESS,
-								maxLength: NUM_BYTES_ADDRESS,
-								fieldNumber: 1,
-							},
-							weight: {
-								dataType: 'uint64',
-								fieldNumber: 2,
-							},
-						},
-					},
+					items: validator,
 				},
 				threshold: {
 					dataType: 'uint64',
