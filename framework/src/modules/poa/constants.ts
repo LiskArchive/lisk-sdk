31d626f387c51736361650c9b0afe349adb6c618
!shan <ishantiw.quasar@gmail.com>
2023-06-07 17:38:06 +0200

    Implement afterTransactionsExecute and _shuffleValidatorsList (#8523)
    * 🌱 Implement afterTransactionsExecute and _shuffleValidatorsList
    
    * ✅ Add unit tests for afterTransactionsExecute and shuffleValidatorList
    
    * ♻️ Use constants and simplify shuffleValidatorList
    
    * ✅ Adjust expectation setValidatorsParams to expect secondSnapshot threshold
    
    * 💅🏻 Rename snapshot keys
    
    * ♻️ Add missing logic and improve logic for afterTransactionsExecute
    
    * 💅🏻 Assert type on the arguments for readability
    
    * ♻️Naming issues and refine loop in test

--- framework/src/modules/poa/constants.ts
+++ framework/src/modules/poa/constants.ts
@@ -12,6 +12,8 @@
  * Removal or modification of this copyright notice is prohibited.
  */
 
+import { utils } from '@liskhq/lisk-cryptography';
+
 export enum UpdateAuthority {
 	SUCCESS = 0,
 	FAIL_INVALID_SIGNATURE,
@@ -35,3 +37,8 @@ export const EMPTY_BYTES = Buffer.alloc(0);
 export const COMMAND_REGISTER_AUTHORITY = 'registerAuthority';
 export const COMMAND_UPDATE_KEY = 'updateKey';
 export const COMMAND_UPDATE_AUTHORITY = 'updateAuthority';
+// Store key
+// https://github.com/LiskHQ/lips/blob/main/proposals/lip-0047.md#uint32be-function
+export const KEY_SNAPSHOT_0 = utils.intToBuffer(0, 4);
+export const KEY_SNAPSHOT_1 = utils.intToBuffer(1, 4);
+export const KEY_SNAPSHOT_2 = utils.intToBuffer(2, 4);
