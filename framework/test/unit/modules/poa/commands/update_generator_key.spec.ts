d109544c6e1c937a07a1778b6f2e226675dbde8d
Franco NG <franco.ng@lightcurve.io>
2023-06-08 12:22:48 +0200

    Implement PoA Update Generator Key (#8534)
    * Implement Register Authority Commands
    
    * Renaming Commands
    
    * Remove extra files
    
    * Added Unit Test for RegisterAuthority
    
    * Put types back to PoA
    
    * Update code according to PR review and comments
    
    * Update chainID in test case to match with actual format
    
    * Update param names to be grammar-correct
    
    * Update AUTHORITY_REGISTRATION_FEE comments and test unit imports
    
    * Update $id for updateAuthority Schema
    
    * Implement Update Authority and Test Cases
    
    * Change name of updateAuthoritySchema
    
    * Implement UpdateGeneratorKey and Test Cases
    
    * Update updateAuthoritySchema naming
    
    * Update update authority coding according to PR comments
    
    * Edited Buffer bits and test cases captions
    
    * Update cosmetic change on update_authority files
    
    * Update UpdateGeneratorKeyCommand to use senderAddress
    
    * Add comments and better error message to UpdateAuthorityCommand
    
    * Minor changes to error message captions
    
    * Update error message caption
    
    ---------
    
    Co-authored-by: !shan <ishantiw.quasar@gmail.com>

---
+++ framework/test/unit/modules/poa/commands/update_generator_key.spec.ts
@@ -0,0 +1,130 @@
+import { codec } from '@liskhq/lisk-codec';
+import { TransactionAttrs } from '@liskhq/lisk-chain';
+import { utils, address } from '@liskhq/lisk-cryptography';
+
+import {
+	PoAModule,
+	Transaction,
+	CommandVerifyContext,
+	CommandExecuteContext,
+	VerifyStatus,
+} from '../../../../../src';
+import { UpdateGeneratorKeyParams, ValidatorsMethod } from '../../../../../src/modules/poa/types';
+import { ValidatorStore } from '../../../../../src/modules/poa/stores';
+import {
+	AUTHORITY_REGISTRATION_FEE,
+	COMMAND_UPDATE_KEY,
+	LENGTH_GENERATOR_KEY,
+	MODULE_NAME_POA,
+} from '../../../../../src/modules/poa/constants';
+import { updateGeneratorKeySchema } from '../../../../../src/modules/poa/schemas';
+import { PrefixedStateReadWriter } from '../../../../../src/state_machine/prefixed_state_read_writer';
+import { InMemoryPrefixedStateDB } from '../../../../../src/testing';
+import * as testing from '../../../../../src/testing';
+import { UpdateGeneratorKeyCommand } from '../../../../../src/modules/poa/commands/update_generator_key';
+import { createStoreGetter } from '../../../../../src/testing/utils';
+
+describe('UpdateGeneratorKey', () => {
+	const poaModule = new PoAModule();
+	let updateGeneratorKeyCommand: UpdateGeneratorKeyCommand;
+	let stateStore: PrefixedStateReadWriter;
+	let mockValidatorsMethod: ValidatorsMethod;
+	let validatorStore: ValidatorStore;
+
+	const publicKey = utils.getRandomBytes(32);
+	const chainID = Buffer.from([0, 0, 0, 1]);
+
+	const updateGeneratorKeyParams: UpdateGeneratorKeyParams = {
+		generatorKey: utils.getRandomBytes(LENGTH_GENERATOR_KEY),
+	};
+
+	const buildTransaction = (transaction: Partial<TransactionAttrs>): Transaction => {
+		return new Transaction({
+			module: transaction.module ?? MODULE_NAME_POA,
+			command: transaction.command ?? COMMAND_UPDATE_KEY,
+			senderPublicKey: transaction.senderPublicKey ?? publicKey,
+			nonce: transaction.nonce ?? BigInt(0),
+			fee: transaction.fee ?? AUTHORITY_REGISTRATION_FEE,
+			params:
+				transaction.params ?? codec.encode(updateGeneratorKeySchema, updateGeneratorKeyParams),
+			signatures: transaction.signatures ?? [publicKey],
+		});
+	};
+
+	beforeEach(async () => {
+		updateGeneratorKeyCommand = new UpdateGeneratorKeyCommand(poaModule.stores, poaModule.events);
+		mockValidatorsMethod = {
+			setValidatorGeneratorKey: jest.fn(),
+			registerValidatorKeys: jest.fn(),
+			registerValidatorWithoutBLSKey: jest.fn(),
+			getValidatorKeys: jest.fn(),
+			getGeneratorsBetweenTimestamps: jest.fn(),
+			setValidatorsParams: jest.fn(),
+		};
+		updateGeneratorKeyCommand.addDependencies(mockValidatorsMethod);
+
+		stateStore = new PrefixedStateReadWriter(new InMemoryPrefixedStateDB());
+		validatorStore = poaModule.stores.get(ValidatorStore);
+
+		await validatorStore.set(
+			createStoreGetter(stateStore),
+			address.getAddressFromPublicKey(publicKey),
+			{
+				name: 'validator',
+			},
+		);
+	});
+
+	describe('verify', () => {
+		let context: CommandVerifyContext<UpdateGeneratorKeyParams>;
+		beforeEach(() => {
+			context = testing
+				.createTransactionContext({
+					stateStore,
+					transaction: buildTransaction({}),
+					chainID,
+				})
+				.createCommandVerifyContext<UpdateGeneratorKeyParams>(updateGeneratorKeySchema);
+		});
+
+		it('should return error when validator not exist', async () => {
+			await validatorStore.del(
+				createStoreGetter(stateStore),
+				address.getAddressFromPublicKey(publicKey),
+			);
+
+			await expect(updateGeneratorKeyCommand.verify(context)).rejects.toThrow(
+				'Validator does not exist.',
+			);
+		});
+
+		it('should return OK when transaction is valid', async () => {
+			const result = await updateGeneratorKeyCommand.verify(context);
+
+			expect(result.status).toBe(VerifyStatus.OK);
+		});
+	});
+
+	describe('execute', () => {
+		let context: CommandExecuteContext<UpdateGeneratorKeyParams>;
+		beforeEach(async () => {
+			context = testing
+				.createTransactionContext({
+					stateStore,
+					transaction: buildTransaction({}),
+					chainID,
+				})
+				.createCommandExecuteContext<UpdateGeneratorKeyParams>(updateGeneratorKeySchema);
+		});
+
+		it('should call setValidatorGeneratorKey', async () => {
+			await updateGeneratorKeyCommand.execute(context);
+
+			expect(mockValidatorsMethod.setValidatorGeneratorKey).toHaveBeenCalledWith(
+				expect.anything(),
+				address.getAddressFromPublicKey(publicKey),
+				context.params.generatorKey,
+			);
+		});
+	});
+});
