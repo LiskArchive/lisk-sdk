3ec3f89f2e291ff28c19fdc17c8af7bc69dc19d9
shuse2 <shus.toda@gmail.com>
2023-06-15 11:58:26 +0800

    :white_check_mark: Fix merge conflict

--- framework/test/unit/modules/pos/commands/register_validator.spec.ts
+++ framework/test/unit/modules/pos/commands/register_validator.spec.ts
@@ -296,37 +296,6 @@ describe('Validator registration command', () => {
 			expect(result.status).toBe(VerifyStatus.FAIL);
 			expect(result.error?.message).toInclude('Insufficient transaction fee.');
 		});
-
-		it('should throw error if name is empty', async () => {
-			const invalidParams = codec.encode(validatorRegistrationCommandParamsSchema, {
-				...transactionParams,
-				name: '',
-			});
-
-			const invalidTransaction = new Transaction({
-				module: 'pos',
-				command: 'registerValidator',
-				senderPublicKey: publicKey,
-				nonce: BigInt(0),
-				fee: BigInt(100000000),
-				params: invalidParams,
-				signatures: [publicKey],
-			});
-
-			const context = testing
-				.createTransactionContext({
-					transaction: invalidTransaction,
-					chainID,
-				})
-				.createCommandVerifyContext<ValidatorRegistrationParams>(
-					validatorRegistrationCommandParamsSchema,
-				);
-
-			const result = await validatorRegistrationCommand.verify(context);
-
-			expect(result.status).toBe(VerifyStatus.FAIL);
-			expect(result.error?.message).toInclude("'.name' must NOT have fewer than 1 characters");
-		});
 	});
 
 	describe('execute', () => {
