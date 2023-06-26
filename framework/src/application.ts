4e09d473f387b986a6e30878d6fdcd748895518a
Incede <33103370+Incede@users.noreply.github.com>
2023-06-26 14:47:33 +0200

    Add nft module to example app

--- framework/src/application.ts
+++ framework/src/application.ts
@@ -55,6 +55,7 @@ import {
 } from './modules/interoperability';
 import { DynamicRewardMethod, DynamicRewardModule } from './modules/dynamic_rewards';
 import { Engine } from './engine';
+import { NFTMethod, NFTModule } from './modules/nft';
 
 const isPidRunning = async (pid: number): Promise<boolean> =>
 	psList().then(list => list.some(x => x.pid === pid));
@@ -108,6 +109,7 @@ interface DefaultApplication {
 		validator: ValidatorsMethod;
 		auth: AuthMethod;
 		token: TokenMethod;
+		nft: NFTMethod;
 		fee: FeeMethod;
 		random: RandomMethod;
 		reward: DynamicRewardMethod;
@@ -163,6 +165,7 @@ export class Application {
 		// create module instances
 		const authModule = new AuthModule();
 		const tokenModule = new TokenModule();
+		const nftModule = new NFTModule();
 		const feeModule = new FeeModule();
 		const rewardModule = new DynamicRewardModule();
 		const randomModule = new RandomModule();
@@ -192,9 +195,11 @@ export class Application {
 			feeModule.method,
 		);
 		tokenModule.addDependencies(interoperabilityModule.method, feeModule.method);
+		nftModule.addDependencies(interoperabilityModule.method, feeModule.method, tokenModule.method);
 
 		// resolve interoperability dependencies
 		interoperabilityModule.registerInteroperableModule(tokenModule);
+		interoperabilityModule.registerInteroperableModule(nftModule);
 		interoperabilityModule.registerInteroperableModule(feeModule);
 
 		// register modules
@@ -202,6 +207,7 @@ export class Application {
 		application._registerModule(authModule);
 		application._registerModule(validatorModule);
 		application._registerModule(tokenModule);
+		application._registerModule(nftModule);
 		application._registerModule(rewardModule);
 		application._registerModule(randomModule);
 		application._registerModule(posModule);
@@ -212,6 +218,7 @@ export class Application {
 			method: {
 				validator: validatorModule.method,
 				token: tokenModule.method,
+				nft: nftModule.method,
 				auth: authModule.method,
 				fee: feeModule.method,
 				pos: posModule.method,
