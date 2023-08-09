4be282d754fdd63e24cdb5fdc1a9e3febdcde492
Incede <33103370+Incede@users.noreply.github.com>
2023-08-09 22:46:15 +0200

    Update example app

--- examples/interop/pos-sidechain-example-one/src/app/app.ts
+++ examples/interop/pos-sidechain-example-one/src/app/app.ts
@@ -4,7 +4,7 @@ import { registerModules } from './modules';
 import { registerPlugins } from './plugins';
 
 export const getApplication = (config: PartialApplicationConfig): Application => {
-	const { app, method } = Application.defaultApplication(config, true);
+	const { app, method } = Application.defaultApplication(config, false);
 
 	const nftModule = new NFTModule();
 	const testNftModule = new TestNftModule();
