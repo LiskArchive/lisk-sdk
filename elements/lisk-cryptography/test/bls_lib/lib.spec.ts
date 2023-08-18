4d8f44fc1680e55d4f5f772bb3faaa7000dbe2fc
Martin Macharia <mosmartin5@gmail.com>
2023-08-18 13:13:46 +0200

    Check that the sk is not a multiple of group order and is 32 bytes long

--- elements/lisk-cryptography/test/bls_lib/lib.spec.ts
+++ elements/lisk-cryptography/test/bls_lib/lib.spec.ts
@@ -13,8 +13,7 @@
  *
  */
 
-import { ErrorBLST, SecretKey } from '@chainsafe/blst';
-import { isSecretKeyNonZeroModOrder } from '../../src/bls_lib/lib';
+import { isNotMultipleOfGroupOrder } from '../../src/bls_lib/lib';
 import {
 	blsAggregate,
 	blsAggregateVerify,
@@ -60,231 +59,259 @@ interface EthFastAggrVerifySpec {
 }
 
 describe('bls_lib', () => {
-	const groupOrder = '0x73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001';
-
-	describe('isSecretKeyNonZeroModOrder', () => {
-		it('should return true when given a valid secret key', () => {
-			const secretKey = SecretKey.fromBytes(Buffer.alloc(32, 1));
-			expect(isSecretKeyNonZeroModOrder(secretKey)).toBe(true);
-		});
-
-		it('should throw error when given a zero secret key', () => {
-			expect(() => isSecretKeyNonZeroModOrder(SecretKey.fromBytes(Buffer.alloc(32, 0)))).toThrow(
-				'ZERO_SECRET_KEY',
+	describe('isNotMultipleOfGroupOrder', () => {
+		it('should return true if sk is not equal to groupOrder or groupOrderDouble', () => {
+			const sk = Buffer.from(
+				'73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000003',
+				'hex',
 			);
+			expect(isNotMultipleOfGroupOrder(sk)).toBe(true);
 		});
 
-		it('should return false for a secret key that is a multiple of the order of the group', () => {
-			const secretKey = SecretKey.fromBytes(
-				Buffer.from('73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001', 'hex'),
+		it('should return true if sk is 3*groupOrder', () => {
+			const sk = Buffer.from(
+				'15bc8f5f97cd877d899ad88181ce5880ffb38ec08fffb13fcfffffffd00000003',
+				'hex',
 			);
-			expect(isSecretKeyNonZeroModOrder(secretKey)).toBe(false);
+			expect(isNotMultipleOfGroupOrder(sk)).toBe(true);
 		});
 
-		describe('blsSkToPk', () => {
-			describe.each(getAllFiles(['bls_specs/sk_to_pk']))('%s', ({ path }) => {
-				it('should convert to valid pk', () => {
-					const { input, output } = loadSpecFile<{ input: string; output: string }>(path);
-
-					expect(blsSkToPk(hexToBuffer(input)).toString('hex')).toEqual(
-						hexToBuffer(output).toString('hex'),
-					);
-				});
-			});
-
-			it('should return a non-empty buffer when given a valid input buffer', () => {
-				const sk = Buffer.alloc(32, 1);
-				const pk = blsSkToPk(sk);
-
-				expect(pk).toBeDefined();
-				expect(pk.length).toBeGreaterThan(0);
-			});
-
-			it('should throw an error when given an input buffer with value equal to the group order', () => {
-				const sk = Buffer.from(groupOrder.slice(2), 'hex');
-
-				expect(() => blsSkToPk(sk)).toThrow('Secret key is not valid.');
-			});
-
-			it('should throw an error if the input buffer is zero', () => {
-				const sk = Buffer.alloc(32, 0);
-				expect(() => blsSkToPk(sk)).toThrow('ZERO_SECRET_KEY');
-			});
-
-			it('should throw an error if the input buffer is not 32 bytes long', () => {
-				const sk = Buffer.alloc(31, 1);
-				expect(() => blsSkToPk(sk)).toThrow(ErrorBLST);
-			});
+		it('should return true when the buffer is not a multiple of the group order and 32 bytes long', () => {
+			const sk = Buffer.from(
+				'0000000000000000000000000000000000000000000000000000000000000000',
+				'hex',
+			);
+			expect(isNotMultipleOfGroupOrder(sk)).toBe(true);
 		});
 
-		describe('blsSign', () => {
-			describe.each(getAllFiles(['eth2_bls_specs/sign', 'bls_specs/sign']))('%s', ({ path }) => {
-				const {
-					input: { privkey, message },
-					output,
-				} = loadSpecFile<EthSignSpec>(path);
+		it('should return true when the buffer is not a multiple of the group order and not 32 bytes long', () => {
+			const sk = Buffer.from(
+				'0000000000000000000000000000000000000000000000000000000000000001',
+				'hex',
+			);
+			expect(isNotMultipleOfGroupOrder(sk)).toBe(true);
+		});
 
-				if (privkey !== `0x${'00'.repeat(32)}`) {
-					it('should generate valid signature if private key is non zero', () => {
-						const signature = blsSign(hexToBuffer(privkey), hexToBuffer(message));
-						expect(signature.toString('hex')).toEqual(hexToBuffer(output).toString('hex'));
-					});
-				} else {
-					it('should throw an error if the private key is all zeros', () => {
-						expect(() => blsSign(hexToBuffer(privkey), hexToBuffer(message))).toThrow(
-							'ZERO_SECRET_KEY',
-						);
-					});
-				}
-			});
+		it('should return true when the buffer is a multiple of the group order but not 32 bytes long', () => {
+			const sk = Buffer.from(
+				'73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000002',
+				'hex',
+			);
+			expect(isNotMultipleOfGroupOrder(sk)).toBe(true);
+		});
 
-			it('should throw an error when given an input buffer with value equal to the group order', () => {
-				const sk = Buffer.from(groupOrder.slice(2), 'hex');
-				const message = Buffer.from('hello world');
+		it('should return false when the buffer is a multiple of the group order and 32 bytes long', () => {
+			const sk = Buffer.from(
+				'73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001',
+				'hex',
+			);
+			expect(isNotMultipleOfGroupOrder(sk)).toBe(false);
+		});
 
-				expect(() => blsSign(sk, message)).toThrow('Secret key is not valid.');
-			});
+		it('should return false if sk is equal to groupOrder', () => {
+			const sk = Buffer.from(
+				'73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001',
+				'hex',
+			);
+			expect(isNotMultipleOfGroupOrder(sk)).toBe(false);
+		});
 
-			it('should throw an error when a secret key that is non-zero but zero modulo the group order', () => {
-				// sk equals 2*r where r is order of the groups G1 and G2
-				const sk = Buffer.from(
-					'e7db4ea6533afa906673b0101343b00aa77b4805fffcb7fdfffffffe00000002',
-					'hex',
+		it('should return false if sk is equal to groupOrderDouble', () => {
+			const sk = Buffer.from(
+				'e7db4ea6533afa906673b0101343b00aa77b4805fffcb7fdfffffffe00000002',
+				'hex',
+			);
+			expect(isNotMultipleOfGroupOrder(sk)).toBe(false);
+		});
+	});
+
+	describe('blsSkToPk', () => {
+		describe.each(getAllFiles(['bls_specs/sk_to_pk']))('%s', ({ path }) => {
+			it('should convert to valid pk', () => {
+				const { input, output } = loadSpecFile<{ input: string; output: string }>(path);
+
+				expect(blsSkToPk(hexToBuffer(input)).toString('hex')).toEqual(
+					hexToBuffer(output).toString('hex'),
 				);
-				const message = Buffer.from('hello world', 'hex');
-
-				expect(() => blsSign(sk, message)).toThrow('Secret key is not valid.');
 			});
 		});
 
-		describe('blsVerify', () => {
-			describe.each(getAllFiles(['eth2_bls_specs/verify', 'bls_specs/verify']))(
-				'%s',
-				({ path }) => {
-					it('should verify signatures', () => {
-						const {
-							input: { pubkey, message, signature },
-							output,
-						} = loadSpecFile<EthVerifySpec>(path);
+		it('should return a non-empty buffer when given a valid input buffer', () => {
+			const sk = Buffer.alloc(32, 1);
+			const pk = blsSkToPk(sk);
 
-						const verify = blsVerify(
-							hexToBuffer(pubkey),
-							hexToBuffer(message),
-							hexToBuffer(signature),
-						);
-
-						expect(verify).toEqual(output);
-					});
-				},
-			);
+			expect(pk).toBeDefined();
+			expect(pk.length).toBeGreaterThan(0);
 		});
 
-		describe('blsAggregate', () => {
-			describe.each(getAllFiles(['eth2_bls_specs/aggregate', 'bls_specs/aggregate']))(
-				'%s',
-				({ path }) => {
-					it('should aggregate signatures', () => {
-						const { input, output } = loadSpecFile<EthAggrSpec>(path);
-
-						const signature = blsAggregate(input.map(hexToBuffer));
-
-						if (signature) {
-							expect(signature.toString('hex')).toEqual(hexToBuffer(output).toString('hex'));
-						} else {
-							// In one of eth2 specs, they refer null as INVALID case
-							const expectedOutput = output ?? false;
-							expect(signature).toEqual(expectedOutput);
-						}
-					});
-				},
-			);
+		it('should throw an error if the input buffer is zero', () => {
+			const sk = Buffer.alloc(32, 0);
+			expect(() => blsSkToPk(sk)).toThrow('ZERO_SECRET_KEY');
 		});
 
-		describe('blsAggregateVerify', () => {
-			describe.each(getAllFiles(['eth2_bls_specs/aggregate_verify']))('%s', ({ path }) => {
-				it('should verify messages', () => {
-					const {
-						input: { pubkeys, messages, signature },
-						output,
-					} = loadSpecFile<EthAggrVerifySpec>(path);
+		it('should throw an error if the input buffer is not 32 bytes long', () => {
+			const sk = Buffer.alloc(31, 1);
+			expect(() => blsSkToPk(sk)).toThrow('Input buffers must have the same byte length');
+		});
 
-					const verify = blsAggregateVerify(
-						pubkeys.map(hexToBuffer),
-						messages.map(hexToBuffer),
-						hexToBuffer(signature),
-					);
+		it('should throw an error when given a secret key that is not a 32-byte Buffer', () => {
+			const sk = Buffer.from('0123456789abcdef0123456789abcdef0123456789abcdef0123456789', 'hex');
+			expect(() => {
+				blsSkToPk(sk);
+			}).toThrow();
+		});
+	});
 
-					expect(verify).toEqual(output);
+	describe('blsSign', () => {
+		describe.each(getAllFiles(['eth2_bls_specs/sign', 'bls_specs/sign']))('%s', ({ path }) => {
+			const {
+				input: { privkey, message },
+				output,
+			} = loadSpecFile<EthSignSpec>(path);
+
+			if (privkey !== `0x${'00'.repeat(32)}`) {
+				it('should generate valid signature if private key is non zero', () => {
+					const signature = blsSign(hexToBuffer(privkey), hexToBuffer(message));
+					expect(signature.toString('hex')).toEqual(hexToBuffer(output).toString('hex'));
 				});
-			});
-		});
-
-		describe('blsFastAggregateVerify', () => {
-			// The ignored test case "fast_aggregate_verify_infinity_pubkey" contains pk at infinity (identify point)
-			// Since implementation standard https://tools.ietf.org/html/draft-irtf-cfrg-bls-signature-04#section-3.3.4
-			// specifies to not validate public keys in "FastAggregateVerify"
-			// so we why our implementation returns "true" and eth2 specs mentioned it as "false"
-			describe.each(
-				getAllFiles(
-					['eth2_bls_specs/fast_aggregate_verify', 'bls_specs/fast_aggregate_verify'],
-					/fast_aggregate_verify_infinity_pubkey/,
-				),
-			)('%s', ({ path }) => {
-				it('should verify message', () => {
-					const {
-						input: { pubkeys, message, signature },
-						output,
-					} = loadSpecFile<EthFastAggrVerifySpec>(path);
-
-					const verify = blsFastAggregateVerify(
-						pubkeys.map(hexToBuffer),
-						hexToBuffer(message),
-						hexToBuffer(signature),
-					);
-
-					expect(verify).toEqual(output);
-				});
-			});
-		});
-
-		describe('blsPopProve', () => {
-			describe.each(getAllFiles(['bls_specs/pop_prove']))('%s', ({ path }) => {
-				it('should create valid proof of possession', () => {
-					const { input, output } = loadSpecFile<{ input: string; output: string }>(path);
-
-					expect(blsPopProve(hexToBuffer(input)).toString('hex')).toEqual(
-						hexToBuffer(output).toString('hex'),
+			} else {
+				it('should throw an error if the private key is all zeros', () => {
+					expect(() => blsSign(hexToBuffer(privkey), hexToBuffer(message))).toThrow(
+						'ZERO_SECRET_KEY',
 					);
 				});
+			}
+		});
+
+		it('should throw an error when given a secret key Buffer of length other than 32', () => {
+			const sk = Buffer.from('0123456789abcdef0123456789abcdef0123456789abcdef', 'hex');
+
+			expect(() => {
+				blsSign(sk, Buffer.alloc(32, 1));
+			}).toThrow();
+		});
+	});
+
+	describe('blsVerify', () => {
+		describe.each(getAllFiles(['eth2_bls_specs/verify', 'bls_specs/verify']))('%s', ({ path }) => {
+			it('should verify signatures', () => {
+				const {
+					input: { pubkey, message, signature },
+					output,
+				} = loadSpecFile<EthVerifySpec>(path);
+
+				const verify = blsVerify(hexToBuffer(pubkey), hexToBuffer(message), hexToBuffer(signature));
+
+				expect(verify).toEqual(output);
 			});
+		});
+	});
 
-			it('should throw an error when given an input buffer with value equal to the group order', () => {
-				const sk = Buffer.from(groupOrder.slice(2), 'hex');
+	describe('blsAggregate', () => {
+		describe.each(getAllFiles(['eth2_bls_specs/aggregate', 'bls_specs/aggregate']))(
+			'%s',
+			({ path }) => {
+				it('should aggregate signatures', () => {
+					const { input, output } = loadSpecFile<EthAggrSpec>(path);
 
-				expect(() => {
-					blsPopProve(sk);
-				}).toThrow('Secret key is not valid.');
+					const signature = blsAggregate(input.map(hexToBuffer));
+
+					if (signature) {
+						expect(signature.toString('hex')).toEqual(hexToBuffer(output).toString('hex'));
+					} else {
+						// In one of eth2 specs, they refer null as INVALID case
+						const expectedOutput = output ?? false;
+						expect(signature).toEqual(expectedOutput);
+					}
+				});
+			},
+		);
+	});
+
+	describe('blsAggregateVerify', () => {
+		describe.each(getAllFiles(['eth2_bls_specs/aggregate_verify']))('%s', ({ path }) => {
+			it('should verify messages', () => {
+				const {
+					input: { pubkeys, messages, signature },
+					output,
+				} = loadSpecFile<EthAggrVerifySpec>(path);
+
+				const verify = blsAggregateVerify(
+					pubkeys.map(hexToBuffer),
+					messages.map(hexToBuffer),
+					hexToBuffer(signature),
+				);
+
+				expect(verify).toEqual(output);
 			});
+		});
+	});
 
-			it('should throw an error when a secret key is zero', () => {
-				const sk = Buffer.alloc(32, 0);
+	describe('blsFastAggregateVerify', () => {
+		// The ignored test case "fast_aggregate_verify_infinity_pubkey" contains pk at infinity (identify point)
+		// Since implementation standard https://tools.ietf.org/html/draft-irtf-cfrg-bls-signature-04#section-3.3.4
+		// specifies to not validate public keys in "FastAggregateVerify"
+		// so we why our implementation returns "true" and eth2 specs mentioned it as "false"
+		describe.each(
+			getAllFiles(
+				['eth2_bls_specs/fast_aggregate_verify', 'bls_specs/fast_aggregate_verify'],
+				/fast_aggregate_verify_infinity_pubkey/,
+			),
+		)('%s', ({ path }) => {
+			it('should verify message', () => {
+				const {
+					input: { pubkeys, message, signature },
+					output,
+				} = loadSpecFile<EthFastAggrVerifySpec>(path);
 
-				expect(() => {
-					blsPopProve(sk);
-				}).toThrow('ZERO_SECRET_KEY');
+				const verify = blsFastAggregateVerify(
+					pubkeys.map(hexToBuffer),
+					hexToBuffer(message),
+					hexToBuffer(signature),
+				);
+
+				expect(verify).toEqual(output);
+			});
+		});
+	});
+
+	describe('blsPopProve', () => {
+		describe.each(getAllFiles(['bls_specs/pop_prove']))('%s', ({ path }) => {
+			it('should create valid proof of possession', () => {
+				const { input, output } = loadSpecFile<{ input: string; output: string }>(path);
+
+				expect(blsPopProve(hexToBuffer(input)).toString('hex')).toEqual(
+					hexToBuffer(output).toString('hex'),
+				);
 			});
 		});
 
-		describe('blsPopVerify', () => {
-			describe.each(getAllFiles(['bls_specs/pop_verify']))('%s', ({ path }) => {
-				it('should verify proof of possession', () => {
-					const {
-						input: { pk, proof },
-						output,
-					} = loadSpecFile<{ input: { pk: string; proof: string }; output: boolean }>(path);
+		it('should throw an error when a secret key is zero', () => {
+			const sk = Buffer.alloc(32, 0);
 
-					expect(blsPopVerify(hexToBuffer(pk), hexToBuffer(proof))).toEqual(output);
-				});
+			expect(() => {
+				blsPopProve(sk);
+			}).toThrow('ZERO_SECRET_KEY');
+		});
+
+		it('should throw an error when given a secret key Buffer of length other than 32', () => {
+			const sk = Buffer.from('0123456789abcdef0123456789abcdef0123456789abcdef', 'hex');
+
+			expect(() => {
+				blsPopProve(sk);
+			}).toThrow();
+		});
+	});
+
+	describe('blsPopVerify', () => {
+		describe.each(getAllFiles(['bls_specs/pop_verify']))('%s', ({ path }) => {
+			it('should verify proof of possession', () => {
+				const {
+					input: { pk, proof },
+					output,
+				} = loadSpecFile<{ input: { pk: string; proof: string }; output: boolean }>(path);
+
+				expect(blsPopVerify(hexToBuffer(pk), hexToBuffer(proof))).toEqual(output);
 			});
 		});
 	});
