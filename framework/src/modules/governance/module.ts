328a65d616ddc5d556b5c0667d9a7cea0bea22b6
Boban Milošević <milosevic.boban@gmail.com>
2023-08-25 08:29:08 +0200

    Add Governance module foundation (#8888)
    * Add Governance module foundation
    
    * Create interfaces for methods used by Governance module
    
    * Rename Index store to ProposalIndex and declare enums as consts

---
+++ framework/src/modules/governance/module.ts
@@ -0,0 +1,67 @@
+/*
+ * Copyright © 2023 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+import { BaseModule, ModuleMetadata } from '../base_module';
+// import { FeeMethod, PoSMethod, TokenMethod } from './types';
+import { GovernanceEndpoint } from './endpoint';
+import { ProposalCreatedEvent } from './events/proposal_created';
+import { ProposalCreationFailedEvent } from './events/proposal_creation_failed';
+import { ProposalOutcomeEvent } from './events/proposal_outcome';
+import { ProposalQuorumCheckedEvent } from './events/proposal_quorum_checked';
+import { ProposalVotedEvent } from './events/proposal_voted';
+import { GovernanceMethod } from './method';
+import { ProposalIndexStore } from './stores/proposal_index';
+import { ConfigStore } from './stores/config';
+import { ProposalsStore } from './stores/proposals';
+import { VotesStore } from './stores/votes';
+
+export class GovernanceModule extends BaseModule {
+	public method = new GovernanceMethod(this.stores, this.events);
+	public endpoint = new GovernanceEndpoint(this.stores, this.offchainStores);
+
+	// private _tokenMethod!: TokenMethod;
+	// private _posMethod!: PoSMethod;
+	// private _feeMethod!: FeeMethod;
+
+	public constructor() {
+		super();
+
+		this.stores.register(ProposalsStore, new ProposalsStore(this.name, 0));
+		this.stores.register(VotesStore, new VotesStore(this.name, 1));
+		this.stores.register(ProposalIndexStore, new ProposalIndexStore(this.name, 2));
+		this.stores.register(ConfigStore, new ConfigStore(this.name, 3));
+
+		this.events.register(ProposalCreatedEvent, new ProposalCreatedEvent(this.name));
+		this.events.register(ProposalCreationFailedEvent, new ProposalCreationFailedEvent(this.name));
+		this.events.register(ProposalQuorumCheckedEvent, new ProposalQuorumCheckedEvent(this.name));
+		this.events.register(ProposalOutcomeEvent, new ProposalOutcomeEvent(this.name));
+		this.events.register(ProposalVotedEvent, new ProposalVotedEvent(this.name));
+	}
+
+	public metadata(): ModuleMetadata {
+		return {
+			endpoints: [],
+			commands: [],
+			events: [],
+			assets: [],
+			stores: [],
+		};
+	}
+
+	// public addDependencies(tokenMethod: TokenMethod, posMethod: PoSMethod, feeMethod: FeeMethod) {
+	// 	this._tokenMethod = tokenMethod;
+	// 	this._posMethod = posMethod;
+	// 	this._feeMethod = feeMethod;
+	// }
+}
---
+++ framework/src/modules/governance/types.ts
@@ -0,0 +1,72 @@
+/*
+ * Copyright © 2023 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ */
+
+import { ImmutableMethodContext, MethodContext } from '../../state_machine';
+
+export const enum ProposalType {
+	UNIVERSAL = 0,
+	FUNDING = 1,
+	CONFIG_UPDATE = 2,
+}
+
+export const enum ProposalStatus {
+	ACTIVE = 0,
+	FINISHED_ACCEPTED = 1,
+	ACCEPTED_ERROR = 2,
+	FINISHED_REJECTED = 3,
+	FAILED_QUORUM = 4,
+}
+
+export const enum ProposalDecision {
+	YES = 0,
+	NO = 1,
+	PASS = 2,
+}
+
+export interface ProposalDescription {
+	title: Buffer;
+	author: Buffer;
+	summary: Buffer;
+	discussionsTo: Buffer;
+	text: Buffer;
+}
+
+export interface CreateProposalParams {
+	type: ProposalType;
+	description: ProposalDescription;
+	data: Buffer;
+}
+
+export interface TokenMethod {
+	lock(
+		methodContext: MethodContext,
+		address: Buffer,
+		module: string,
+		tokenID: Buffer,
+		amount: bigint,
+	): Promise<void>;
+	getAvailableBalance(
+		methodContext: ImmutableMethodContext,
+		address: Buffer,
+		tokenID: Buffer,
+	): Promise<bigint>;
+}
+
+export interface FeeMethod {
+	payFee(methodContext: MethodContext, amount: bigint): void;
+}
+
+export interface PoSMethod {
+	getLockedStakedAmount(ctx: ImmutableMethodContext, address: Buffer): Promise<bigint>;
+}
