d68e637ce2651015840cb9c37782f1d0fcf7a783
Khalid Hameed <sitetester2010@gmail.com>
2023-09-06 17:38:47 +0300

    Update licence & update imports

--- framework/src/engine/legacy/utils.ts
+++ framework/src/engine/legacy/utils.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2022 Lisk Foundation
+ * Copyright © 2023 Lisk Foundation
  *
  * See the LICENSE file at the top-level directory of this distribution
  * for licensing information.
@@ -11,8 +11,6 @@
  *
  * Removal or modification of this copyright notice is prohibited.
  */
-// import { utils } from '@liskhq/lisk-cryptography';
-
 import { utils } from '@liskhq/lisk-cryptography';
 import {
 	DB_KEY_LEGACY_BRACKET,
