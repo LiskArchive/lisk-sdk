4abfe7f70c130aa3a66dd52a5788702aa842d77d
Boban Milošević <milosevic.boban@gmail.com>
2023-09-11 23:57:11 +0200

    Implement Proposals store for Governance module (#8916)
    * Implement Proposals store for Governance module
    
    * Update framework/src/modules/governance/stores/proposals.ts
    
    Co-authored-by: has5aan <50018215+has5aan@users.noreply.github.com>
    
    * Add missing properties to Proposal store interface
    
    ---------
    
    Co-authored-by: has5aan <50018215+has5aan@users.noreply.github.com>

--- framework/src/modules/governance/stores/proposals.ts
+++ framework/src/modules/governance/stores/proposals.ts
@@ -12,7 +12,14 @@
  * Removal or modification of this copyright notice is prohibited.
  */
 import { BaseStore } from '../../base_store';
-import { ProposalType, ProposalDescription } from '../types';
+import {
+	MAX_LENGTH_PROPOSAL_AUTHOR,
+	MAX_LENGTH_PROPOSAL_LINK,
+	MAX_LENGTH_PROPOSAL_SUMMARY,
+	MAX_LENGTH_PROPOSAL_TEXT,
+	MAX_LENGTH_PROPOSAL_TITLE,
+} from '../constants';
+import { ProposalType, ProposalDescription, ProposalStatus } from '../types';
 
 export interface ProposalsStoreData {
 	creator: Buffer;
@@ -23,6 +30,105 @@ export interface ProposalsStoreData {
 	votesPass: bigint;
 	type: ProposalType;
 	description: ProposalDescription;
+	data: Buffer;
+	status: ProposalStatus;
 }
 
-export class ProposalsStore extends BaseStore<ProposalsStoreData> {}
+export const proposalDescriptionSchema = {
+	type: 'object',
+	required: ['title', 'author', 'summary', 'discussionsTo', 'text'],
+	properties: {
+		title: {
+			dataType: 'bytes',
+			minLength: 1,
+			maxLength: MAX_LENGTH_PROPOSAL_TITLE,
+			fieldNumber: 1,
+		},
+		author: {
+			dataType: 'bytes',
+			minLength: 1,
+			maxLength: MAX_LENGTH_PROPOSAL_AUTHOR,
+			fieldNumber: 2,
+		},
+		summary: {
+			dataType: 'bytes',
+			minLength: 1,
+			maxLength: MAX_LENGTH_PROPOSAL_SUMMARY,
+			fieldNumber: 3,
+		},
+		discussionsTo: {
+			dataType: 'bytes',
+			maxLength: MAX_LENGTH_PROPOSAL_LINK,
+			fieldNumber: 4,
+		},
+		text: {
+			maxLength: MAX_LENGTH_PROPOSAL_TEXT,
+			fieldNumber: 5,
+			dataType: 'bytes',
+		},
+	},
+};
+
+export const proposalSchema = {
+	$id: '/governance/store/proposal',
+	type: 'object',
+	required: [
+		'creator',
+		'creationHeight',
+		'depositAmount',
+		'votesYes',
+		'votesNo',
+		'votesPass',
+		'type',
+		'description',
+		'data',
+		'status',
+	],
+	properties: {
+		creator: {
+			dataType: 'bytes',
+			format: 'lisk32',
+			fieldNumber: 1,
+		},
+		creationHeight: {
+			dataType: 'uint32',
+			fieldNumber: 2,
+		},
+		depositAmount: {
+			dataType: 'uint64',
+			fieldNumber: 3,
+		},
+		votesYes: {
+			dataType: 'uint64',
+			fieldNumber: 4,
+		},
+		votesNo: {
+			dataType: 'uint64',
+			fieldNumber: 5,
+		},
+		votesPass: {
+			dataType: 'uint64',
+			fieldNumber: 6,
+		},
+		type: {
+			dataType: 'uint32',
+			fieldNumber: 7,
+		},
+		description: {
+			fieldNumber: 8,
+			...proposalDescriptionSchema,
+		},
+		data: {
+			dataType: 'bytes',
+			fieldNumber: 9,
+		},
+		status: {
+			dataType: 'uint32',
+			fieldNumber: 10,
+		},
+	},
+};
+
+export class ProposalsStore extends BaseStore<ProposalsStoreData> {
+	public shema = proposalSchema;
+}
