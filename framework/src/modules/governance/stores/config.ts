e7dee2f16fa4fa53852b046dc66f4379083b37a1
has5aan <50018215+has5aan@users.noreply.github.com>
2023-09-11 23:53:45 +0200

    Adds Config, Index and Votes stores for Governance module (#8925)
    * Adds Config, Index and Votes stores for Governance module
    
    * Removes index.ts and updates ProposalIndexStore
    
    * Corrects schema defintion for ConfigStore
    
    * Updates schema id for ProposalStoreIndex

--- framework/src/modules/governance/stores/config.ts
+++ framework/src/modules/governance/stores/config.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2022 Lisk Foundation
+ * Copyright © 2023 Lisk Foundation
  *
  * See the LICENSE file at the top-level directory of this distribution
  * for licensing information.
@@ -11,6 +11,7 @@
  *
  * Removal or modification of this copyright notice is prohibited.
  */
+import { TOKEN_ID_LENGTH } from '../../../constants';
 import { BaseStore } from '../../base_store';
 
 export interface ConfigStoreData {
@@ -24,4 +25,57 @@ export interface ConfigStoreData {
 	treasuryTokensPerBlock: bigint;
 }
 
-export class ConfigStore extends BaseStore<ConfigStoreData> {}
+export const configSchema = {
+	$id: '/governance/store/config',
+	type: 'object',
+	required: [
+		'tokenIdTreasury',
+		'voteDuration',
+		'quorumDuration',
+		'proposalCreationFee',
+		'proposalCreationDeposit',
+		'proposalCreationMinBalance',
+		'quorumPercentage',
+		'treasuryTokensPerBlock',
+	],
+	properties: {
+		tokenIdTreasury: {
+			dataType: 'bytes',
+			minLength: TOKEN_ID_LENGTH,
+			maxLength: TOKEN_ID_LENGTH,
+			fieldNumber: 1,
+		},
+		voteDuration: {
+			dataType: 'uint32',
+			fieldNumber: 2,
+		},
+		quorumDuration: {
+			dataType: 'uint32',
+			fieldNumer: 3,
+		},
+		proposalCreationFee: {
+			dataType: 'uint64',
+			fieldNumber: 4,
+		},
+		proposalCreationDeposit: {
+			dataType: 'uint64',
+			fieldNumber: 5,
+		},
+		proposalCreationMinBalance: {
+			dataType: 'uint64',
+			fieldNumber: 6,
+		},
+		quorumPercentage: {
+			dataType: 'uint32',
+			fieldNumber: 7,
+		},
+		treasuryTokensPerBlock: {
+			dataType: 'uint64',
+			fieldNumber: 8,
+		},
+	},
+};
+
+export class ConfigStore extends BaseStore<ConfigStoreData> {
+	public schema = configSchema;
+}
