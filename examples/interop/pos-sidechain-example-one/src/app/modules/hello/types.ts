59007d4a0004e95d5c61a9bc77aff0e7d2929552
Mona Bärenfänger <mona@lightcurve.io>
2023-12-05 15:54:20 +0100

    Update interop example (#9144)
    * Update interop example
    
    * Update schema id
    
    * Refactor some sections
    
    * Adjust naming
    
    * Add interop setup diagram reference
    
    * Fix command example
    
    ---------
    
    Co-authored-by: Khalid Hameed <sitetester2010@gmail.com>
    Co-authored-by: !shan <ishantiw.quasar@gmail.com>

--- examples/interop/pos-sidechain-example-one/src/app/modules/hello/types.ts
+++ examples/interop/pos-sidechain-example-one/src/app/modules/hello/types.ts
@@ -7,3 +7,17 @@ export interface ModuleConfig {
 }
 
 export type ModuleConfigJSON = JSONObject<ModuleConfig>;
+
+export interface CreateHelloParams {
+	message: string;
+}
+
+// Parameters of the reactCrossChain CCM
+export interface CCReactMessageParams {
+	// A number indicating the type of the reaction.
+	reactionType: number;
+	// ID of the Hello message being reacted to.
+	helloMessageID: string;
+	// Optional field for data / messages.
+	data: string;
+}
--- examples/interop/pos-sidechain-example-two/src/app/modules/react/schemas.ts
+++ examples/interop/pos-sidechain-example-two/src/app/modules/react/schemas.ts
@@ -1,4 +1,21 @@
-// Schema for the parameters of the reactCrossChain CCM
+const reactionType = {
+	dataType: 'uint32',
+	fieldNumber: 1,
+};
+
+const helloMessageID = {
+	dataType: 'string',
+	fieldNumber: 2,
+};
+
+const data = {
+	dataType: 'string',
+	fieldNumber: 3,
+	minLength: 0,
+	maxLength: 64,
+};
+
+// Schema for the parameters of the crossChainReact CCM
 export const CCReactMessageParamsSchema = {
 	// The unique identifier of the schema.
 	$id: '/lisk/react/ccReactMessageParams',
@@ -7,24 +24,13 @@ export const CCReactMessageParamsSchema = {
 	required: ['reactionType', 'helloMessageID', 'data'],
 	// A list describing the required parameters for the CCM.
 	properties: {
-		reactionType: {
-			dataType: 'uint32',
-			fieldNumber: 1,
-		},
-		helloMessageID: {
-			dataType: 'string',
-			fieldNumber: 2,
-		},
-		data: {
-			dataType: 'string',
-			fieldNumber: 3,
-			minLength: 0,
-			maxLength: 64,
-		},
+		reactionType,
+		helloMessageID,
+		data,
 	},
 };
 
-// Schema for the parameters of the react reactCrossChain command
+// Schema for the parameters of the react crossChainReact command
 export const CCReactCommandParamsSchema = {
 	// The unique identifier of the schema.
 	$id: '/lisk/react/ccReactCommandParams',
@@ -33,20 +39,9 @@ export const CCReactCommandParamsSchema = {
 	required: ['reactionType', 'helloMessageID', 'receivingChainID', 'data', 'messageFee'],
 	// A list describing the available parameters for the command.
 	properties: {
-		reactionType: {
-			dataType: 'uint32',
-			fieldNumber: 1,
-		},
-		helloMessageID: {
-			dataType: 'string',
-			fieldNumber: 2,
-		},
-		data: {
-			dataType: 'string',
-			fieldNumber: 3,
-			minLength: 0,
-			maxLength: 64,
-		},
+		reactionType,
+		helloMessageID,
+		data,
 		receivingChainID: {
 			dataType: 'bytes',
 			fieldNumber: 4,
